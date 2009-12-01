jingpad-bsp/android_external_autotest

commit ef0ac74491f22827ab1359a06c7af867794dbffd

A script to start and stop programs as daemons. Creates pidfile Can redirect stderr/stdout to a log file Option to chdir to a directory before starting the process
Signed-off-by: Scott Zawalski <scottz@google.com>


git-svn-id: http://test.kernel.org/svn/autotest/trunk@3962 592f7852-d20e-0410-864c-8624ca9c26a4

client/bin/harness_standalone.py
@@ -36,9 +36,12 @@ class harness_standalone(harness.harness):
         if os.path.exists('/etc/event.d'):
             # NB: assuming current runlevel is default
             initdefault = utils.system_output('/sbin/runlevel').split()[1]
-        else:
+        else if os.path.exists('/etc/inittab'):
             initdefault = utils.system_output('grep :initdefault: /etc/inittab')
             initdefault = initdefault.split(':')[1]
+        else:
+            initdefault = '2'
+        
 
         try:
             utils.system('ln -sf %s /etc/init.d/autotest' % rc)

utils/service_helper.py
@@ -1,0 +1,67 @@
+#!/usr/bin/python
+"""Service launcher that creates pidfiles and can redirect output to a file."""
+import subprocess, sys, os, optparse, signal
+
+
+def stop_service(pidfile):
+    """Read the first line of a file for an integer that should refer to a pid,
+       send a SIGTERM to that pid #.
+       @param pidfile: file to read for the process id number
+    """
+    pidfh = open(pidfile)
+    pid = int(pidfh.readline())
+    os.kill(pid, signal.SIGTERM)
+
+
+def start_service(cmd, pidfile, logfile=os.devnull, chdir=None):
+    """Start cmd in the background and write the pid to pidfile.
+       @param cmd: command to run with arguments
+       @param pidfile: pidfile to write the pid to
+       @param logfile: file to write stderr/stdout to
+       @param chdir: Directory to change to before starting the application
+    """
+    logfh = open(logfile, 'a')
+    pidfh = open(pidfile, 'w')
+
+    proc = subprocess.Popen(cmd, stdout=logfh, stderr=logfh, cwd=chdir)
+    pidfh.write(str(proc.pid))
+    pidfh.close()
+
+
+def main():
+    parser = optparse.OptionParser()
+    parser.allow_interspersed_args = False
+    parser.add_option('-l', '--logfile', action='store',
+                      default=None,
+                      help='File to redirect stdout to')
+    parser.add_option('-c', '--chdir', action='store',
+                      default=None,
+                      help='Change to dir before starting the process')
+    parser.add_option('-s', '--start-service', action='store_true',
+                      default=False,
+                      help='Start service')
+    parser.add_option('-k', '--stop-service', action='store_true',
+                      default=False,
+                      help='Stop service')
+    parser.add_option('-p', '--pidfile', action='store',
+                      default=None,
+                      help='Pid file location (Required)')
+
+    options, args = parser.parse_args()
+
+    if not options.pidfile:
+        print 'A pidfile must always be supplied'
+        parser.print_help()
+        sys.exit(1)
+
+    if options.start_service:
+        start_service(args, options.pidfile, options.logfile, options.chdir)
+    elif options.stop_service:
+        stop_service(options.pidfile)
+    else:
+        print 'Nothing to do, you must specify to start or stop a service'
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()

commit 8374340639ac9a54f192f3310197ed9ce9e48062

Fixing autotest client stand alone runs
A bug introduced by r3962 is breaking stand alone runs
of the autotest client. This patch fixes the issue.

Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>


git-svn-id: http://test.kernel.org/svn/autotest/trunk@3980 592f7852-d20e-0410-864c-8624ca9c26a4

client/bin/harness_standalone.py
@@ -36,12 +36,11 @@ class harness_standalone(harness.harness):
         if os.path.exists('/etc/event.d'):
             # NB: assuming current runlevel is default
             initdefault = utils.system_output('/sbin/runlevel').split()[1]
-        else if os.path.exists('/etc/inittab'):
+        elif os.path.exists('/etc/inittab'):
             initdefault = utils.system_output('grep :initdefault: /etc/inittab')
             initdefault = initdefault.split(':')[1]
         else:
             initdefault = '2'
-        
 
         try:
             utils.system('ln -sf %s /etc/init.d/autotest' % rc)