9339316d3ac2cb3a14560416543c9c158111c8e0
Zhenyao Mo <zmo@google.com>
2010-01-15 14:18:01 -0800

    Lower the threshold from 1 Gb to 0.75 Gb.
    Review URL: http://codereview.chromium.org/543091

--- client/site_tests/memory_TotalSize/memory_TotalSize.py
+++ client/site_tests/memory_TotalSize/memory_TotalSize.py
@@ -11,11 +11,17 @@ class memory_TotalSize(test.test):
     version = 1
 
     def run_once(self):
-        # TODO(zmo@): this may not get total physical memory size on ARM.
+        # TODO(zmo@): this may not get total physical memory size on ARM
+        #             or some x86 machines.
         mem_size = utils.memtotal()
         gb = mem_size / 1024.0 / 1024.0
         self.write_perf_keyval({"gb_memory_total": gb})
         logging.info("MemTotal: %.3f GB" % gb)
 
-        if gb < 1.0:
+        # We intend to check if a machine has at least 1G memory.  However,
+        # taking into consideration that some machines reserve certain amount
+        # of memory and these won't show in '/proc/meminfo', we lower the
+        # threshold to 0.75 Gb.  Hopefully the reserved memory size is less
+        # than 256 Mb.
+        if gb <= 0.75:
             raise error.TestFail("total system memory size < 1G");
