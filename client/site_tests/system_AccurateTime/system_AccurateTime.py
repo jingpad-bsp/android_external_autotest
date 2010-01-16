60d08327e2d941b58ffa286fd4fb29ace8b06e4e
Ken Mixter <kmixter@google.com>
2010-01-15 16:19:11 -0800

    Improve AccurateTime error recovery and fix syntax error
    Review URL: http://codereview.chromium.org/552020

--- client/site_tests/system_AccurateTime/system_AccurateTime.py
+++ client/site_tests/system_AccurateTime/system_AccurateTime.py
@@ -1,4 +1,4 @@
-import commands, math, re
+import commands, logging, math, re
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 
@@ -13,6 +13,10 @@ class system_AccurateTime(test.test):
         return float(offset.group(1))
 
 
+    def __restart_ntp_server(self):
+        return commands.getstatusoutput('initctl start ntp')[0] == 0
+
+
     def run_once(self, max_offset):
         # Check ntpd is currently running
         if commands.getstatusoutput('pgrep ntpd')[0] != 0:
@@ -20,17 +24,23 @@ class system_AccurateTime(test.test):
         # Stop it since we cannot force ntp requests unless its not running
         if commands.getstatusoutput('initctl stop ntp')[0] != 0:
             raise error.TestError('NTP server could not be stopped')
-        # Now grab the current time and get its offset
-        (status, output) = commands.getstatusoutput('ntpd -g -u ntp:ntp -q')
-        if status != 0:
-            raise error.TestError('NTP current-time check failed')
-        server_offset = self.__get_offset(output)
-        logging.info("server time offset: %f" % server_ffset)
+        try:
+            # Now grab the current time and get its offset
+            (status, output) = commands.getstatusoutput('ntpd -g -u ntp:ntp -q')
+            if status != 0:
+                raise error.TestError('NTP current-time check failed')
+            server_offset = self.__get_offset(output)
+            logging.info("server time offset: %f" % server_offset)
 
-        if (abs(server_offset) > max_offset):
-            raise error.TestError(
-                'NTP server time offset was %fs > max offset %ds' %
-                (server_offset, max_offset))
+            if (abs(server_offset) > max_offset):
+                raise error.TestError(
+                    'abs NTP server time offset was %fs > max offset %ds' %
+                    (abs(server_offset), max_offset))
+        except error.TestError, e:
+            # In case of error, restart server and pass up original error.
+            self.__restart_ntp_server()
+            raise e
+
         # Restart server
-        if commands.getstatusoutput('initctl start ntp')[0] != 0:
+        if not self.__restart_ntp_server():
             raise error.TestError('NTP server could not be restarted')
