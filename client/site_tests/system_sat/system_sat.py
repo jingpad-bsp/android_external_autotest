092291e650f18938a76b6bcae33a0214b90c06cb
Darin Petkov <petkov@chromium.org>
2010-01-22 13:53:30 -0800

    Add stressapptest (SAT).
    This test could be upstreamed but will require different changes to the build
    setup especially in regard to cross-compiling. So, use site_tests for now.
    
    Review URL: http://codereview.chromium.org/543177

---
+++ client/site_tests/system_sat/system_sat.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging, re, os, shutil
+
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
+
+class system_sat(test.test):
+    version = 1
+
+    # http://stressapptest.googlecode.com/files/\
+    #   stressapptest-1.0.1_autoconf.tar.gz
+    def setup(self, tarball = 'stressapptest-1.0.1_autoconf.tar.gz'):
+        # clean
+        if os.path.exists(self.srcdir):
+            utils.system('rm -rf %s' % self.srcdir)
+
+        tarball = utils.unmap_url(self.bindir, tarball, self.tmpdir)
+        utils.extract_tarball_to_dir(tarball, self.srcdir)
+
+        os.chdir(os.path.join(self.srcdir, 'src'))
+        shutil.copyfile('../../Makefile', 'Makefile')
+        shutil.copyfile('../../stressapptest_config.h',
+                        'stressapptest_config.h')
+        utils.system('make')
+
+
+    def run_once(self):
+        os.chdir(os.path.join(self.srcdir, 'src'))
+        cpus = max(utils.count_cpus(), 1)
+        sat = utils.run('./stressapptest -v 20 -i %d -C %d -v 20' %
+                        (cpus, cpus))
+        logging.debug(sat.stdout)
+        if not re.search('Status: PASS', sat.stdout):
+            raise error.TestFail(sat.stdout)
