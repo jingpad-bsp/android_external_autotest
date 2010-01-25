09fa9ef202ab5d9258308e56102992d6bc007d59
Darin Petkov <petkov@chromium.org>
2010-01-25 11:05:55 -0800

    More stress threads. Style fixes. Fix configure options -- hopefully the options will go away once make_autotest.sh sets CC/CXX/etc appropriately.
    Review URL: http://codereview.chromium.org/552131

--- client/site_tests/system_sat/system_sat.py
+++ client/site_tests/system_sat/system_sat.py
@@ -2,7 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, re, os, shutil
+import logging
+import os
+import re
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
@@ -12,7 +14,7 @@ class system_sat(test.test):
 
     # http://stressapptest.googlecode.com/files/\
     #   stressapptest-1.0.1_autoconf.tar.gz
-    def setup(self, tarball = 'stressapptest-1.0.1_autoconf.tar.gz'):
+    def setup(self, tarball='stressapptest-1.0.1_autoconf.tar.gz'):
         # clean
         if os.path.exists(self.srcdir):
             utils.system('rm -rf %s' % self.srcdir)
@@ -21,15 +23,25 @@ class system_sat(test.test):
         utils.extract_tarball_to_dir(tarball, self.srcdir)
 
         os.chdir(self.srcdir)
-        utils.system('./configure --target=i686-linux-gnu')
-        utils.system('make')
+        utils.system('./configure --build=`./config.guess`'
+                     ' --host=i686-linux-gnu')
+        utils.system('make -j %d' % utils.count_cpus())
 
 
-    def run_once(self):
-        os.chdir(os.path.join(self.srcdir, 'src'))
+    def run_once(self, seconds=60):
         cpus = max(utils.count_cpus(), 1)
-        sat = utils.run('./stressapptest -v 20 -i %d -C %d -v 20' %
-                        (cpus, cpus))
+        mbytes = max(int(utils.freememtotal() * .95 / 1024), 512)
+        args = ' -M %d' % mbytes  # megabytes to test
+        args += ' -s %d' % seconds  # seconds to run
+        args += ' -m %d' % cpus  # memory copy threads
+        args += ' -i %d' % cpus  # memory invert threads
+        args += ' -c %d' % cpus  # memory check only threads
+        args += ' -C %d' % cpus  # CPU stress threads
+        args += ' -n 127.0.0.1 --listen'  # network thread
+        args += ' -f diskthread'  # disk thread
+
+        os.chdir(os.path.join(self.srcdir, 'src'))
+        sat = utils.run('./stressapptest' + args)
         logging.debug(sat.stdout)
         if not re.search('Status: PASS', sat.stdout):
             raise error.TestFail(sat.stdout)
