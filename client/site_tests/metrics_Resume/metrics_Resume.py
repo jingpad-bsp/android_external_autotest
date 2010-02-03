52d2ac01491673d9089de245560c95f8ac4730f2
Darin Petkov <petkov@chromium.org>
2010-02-02 16:46:41 -0800

    Use the base utilities for hwclock and suspend/resume.
    Review URL: http://codereview.chromium.org/565022

--- client/site_tests/metrics_Resume/metrics_Resume.py
+++ client/site_tests/metrics_Resume/metrics_Resume.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import commands, logging, os, re, time, utils
+import commands, logging, re, time, utils
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 
@@ -13,7 +13,7 @@ class metrics_Resume(test.test):
 
     def __get_last_msg_time(self, msg):
         data = commands.getoutput(
-            "cat /var/log/messages | grep '%s' | tail -n 1" % msg)
+            "grep '%s' /var/log/messages | tail -n 1" % msg)
         match = re.search(r' \[\s*([0-9.]+)\] ', data)
         if match is None:
             raise error.TestError('Failed to find log message: ' + msg)
@@ -31,27 +31,6 @@ class metrics_Resume(test.test):
         return self.__get_last_msg_time('CPU[0-9] is down')
 
 
-    def get_hwclock_seconds(self, utc=True):
-        """
-        Return the hardware clock in seconds as a floating point value.
-        Use Coordinated Universal Time if utc is True, local time otherwise.
-        Raise a ValueError if unable to read the hardware clock.
-        """
-        cmd = '/sbin/hwclock --debug'
-        if utc:
-            cmd += ' --utc'
-        hwclock_output = utils.system_output(cmd, ignore_status=True)
-        match = re.search(r'= ([0-9]+) seconds since .+ (-?[0-9.]+) seconds$',
-                          hwclock_output, re.DOTALL)
-        if match:
-            seconds = int(match.group(1)) + float(match.group(2))
-            logging.debug('hwclock seconds = %f' % seconds)
-            return seconds
-
-        raise ValueError('Unable to read the hardware clock -- ' +
-                         hwclock_output)
-
-
     def run_once(self, itersleep=None):
         if itersleep is not None:
             time.sleep(itersleep)
@@ -60,21 +39,15 @@ class metrics_Resume(test.test):
         time_to_sleep = 10
 
         # Set the alarm
-        alarm_time = int(self.get_hwclock_seconds()) + time_to_sleep
+        alarm_time = int(utils.get_hwclock_seconds()) + time_to_sleep
         logging.debug('alarm_time = %d' % alarm_time)
-        set_wake_command = 'echo ' + str(alarm_time) + \
-                           ' > /sys/class/rtc/rtc0/wakealarm'
-        if commands.getstatusoutput(set_wake_command)[0] != 0:
-            raise error.TestError('Failure to set wake alarm')
+        utils.set_wake_alarm(alarm_time)
 
         # Suspend the system to RAM
-        sleep_command = "echo mem > /sys/power/state"
-        (status, output) = commands.getstatusoutput(sleep_command)
-        if status != 0:
-            raise error.TestError('Failure to suspend to RAM')
+        utils.suspend_to_ram()
 
         # Calculate the suspend/resume times
-        resume_time = self.get_hwclock_seconds() - alarm_time
+        resume_time = utils.get_hwclock_seconds() - alarm_time
         suspend_time = \
             self.__get_end_suspend_time() - self.__get_start_suspend_time()
 
