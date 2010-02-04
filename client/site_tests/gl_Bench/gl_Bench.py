592b86247ac99992659354d80251a72be7dde5af
Alexey Marinichev <amarinichev@chromium.org>
2010-02-04 15:20:10 -0800

    Initial version of gl_Bench, to start the discussion.
    As of now this tests clears, fills and triangle setup.  The python script starts an X server at
    :1, runs the test there and terminates the server.
    
    Review URL: http://codereview.chromium.org/553081

---
+++ client/site_tests/gl_Bench/gl_Bench.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import utils
+
+class gl_Bench(test.test):
+  version = 1
+  preserve_srcdir = True
+
+  def setup(self):
+    os.chdir(self.srcdir)
+    utils.system('make clean')
+    utils.system('make')
+
+
+  def run_once(self):
+    exefile = os.path.join(self.bindir, 'gl_Bench')
+    cmd = "X :1 & sleep 1; DISPLAY=:1 %s; kill $!" % exefile
+    self.results = utils.system_output(cmd, retain_output=True)
+
+    for keyval in self.results.splitlines():
+      if keyval.strip().startswith('#'):
+	continue
+      key, val = keyval.split(':')
+      self.write_perf_keyval({key.strip(): val.strip()})
