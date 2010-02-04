5fa6d5919ce904345c75d3d0c4389850d2ec8e9c
Darin Petkov <petkov@chromium.org>
2010-02-03 17:58:25 -0800

    Add wlan0 sanity check and a GPU worker thread to the suspend/resume test.
    This adds some extra steps and checks to the metrics_Resume test -- let me know
    if you'd rather do this in a separate suspend/resume .py.
    
    Review URL: http://codereview.chromium.org/566040

--- client/site_tests/metrics_Resume/metrics_Resume.py
+++ client/site_tests/metrics_Resume/metrics_Resume.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import commands, logging, re, time, utils
+import commands, logging, random, re, time, utils
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 
@@ -31,9 +31,29 @@ class metrics_Resume(test.test):
         return self.__get_last_msg_time('CPU[0-9] is down')
 
 
-    def run_once(self, itersleep=None):
-        if itersleep is not None:
-            time.sleep(itersleep)
+    def __is_iface_up(self, name):
+        try:
+            out = utils.system_output('/sbin/ifconfig %s' % name)
+        except error.CmdError, e:
+            logging.info(e)
+            raise error.TestError('interface %s not found' % name)
+
+        match = re.search('UP', out, re.S)
+        return match
+
+
+    def __sanity_check_system(self):
+        time.sleep(3)
+
+        iface = 'wlan0'
+        if not self.__is_iface_up(iface):
+            raise error.TestFail('%s failed to come up' % iface)
+
+
+    def run_once(self):
+        # Some idle time before initiating suspend-to-ram
+        idle_time = random.randint(1, 10)
+        time.sleep(idle_time)
 
         # Safe enough number, can tweek if necessary
         time_to_sleep = 10
@@ -56,3 +76,6 @@ class metrics_Resume(test.test):
         results['seconds_system_suspend'] = suspend_time
         results['seconds_system_resume'] = resume_time
         self.write_perf_keyval(results)
+
+        # Finally, sanity check critical system components
+        self.__sanity_check_system()
