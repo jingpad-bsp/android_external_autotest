998a1458003cd51b034dcb5c73f9880b86232caa
Chris Sosa <sosa@google.com>
2010-02-05 17:06:33 -0800

    Some fixes to get it to work using run_remote_tests
    Adds browser_UrlFetch test from Chrome to autotest with binary
    
    Review URL: http://codereview.chromium.org/573039

---
+++ client/site_tests/browser_UrlFetch/required_environment.sh
@@ -0,0 +1,14 @@
+# Xorg vars
+export DISPLAY=:0.0
+export XAUTHORITY=/home/chronos/.Xauthority
+
+# Vars from session_manager_setup.sh
+export USER=chronos
+export LOGNAME=${USER}
+export SHELL=/bin/bash
+export HOME=/home/chronos
+export PATH=/bin:/usr/bin:/usr/local/bin:/usr/bin/X11
+export GTK_IM_MODULE=ibus
+
+export CHROME_DIR="/opt/google/chrome"
+export BROWSER="$CHROME_DIR/chrome"
