8e552d7a19bbb38de779936edece8fa488fc1a97
Zhurun Zhang <zhurunz@google.com>
2010-03-02 14:51:02 -0800

    Fix bug in GTalkunittest. Previous testing passed when machine is not in a "clean" stage.
    Review URL: http://codereview.chromium.org/660402

--- client/site_tests/realtimecomm_GTalkunittest/realtimecomm_GTalkunittest.py
+++ client/site_tests/realtimecomm_GTalkunittest/realtimecomm_GTalkunittest.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os
+import logging, os, shutil
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
@@ -27,19 +27,24 @@ class realtimecomm_GTalkunittest(test.test):
         utils.run('killall GoogleTalkPlugin', ignore_status=True)
  
         # Setup as appropriate
-        talk_path = os.path.join(self.tmpdir, 'talk')
-        utils.run('rm -rf %s' % talk_path)
-        utils.run('cp -r %s %s' % \
-            (os.path.join(self.bindir, 'talk'), talk_path))
+        talk_path = '/home/chronos/talk'
+        shutil.rmtree(talk_path, ignore_errors=True)
+        shutil.copytree(os.path.join(self.bindir, 'talk'), talk_path)
         utils.run('chown chronos %s -R' % talk_path)
+        utils.run('chown chronos \
+            /tmp/.google-talk-plugin-theuser.lock.testlock', ignore_status=True)
 
         # Run all unittests
         for test_exe in self.unittests:
             # TODO(zhurunz): Support ARM once available.
             x86_talk_path = os.path.join(talk_path, 'i686')
             # The unittest has to be run in 'talk' folder 
-            test_cmd = "cd %s && ./%s" % (x86_talk_path, test_exe)
+            test_cmd = "cd %s && su chronos -c \'./%s\'" % \
+                (x86_talk_path, test_exe)
             self.__run_one_test(test_cmd, self.unittests[test_exe])
+
+        # Clean up
+        shutil.rmtree(talk_path)
 
 
     def __run_one_test(self, test_cmd, number_of_testcases):
