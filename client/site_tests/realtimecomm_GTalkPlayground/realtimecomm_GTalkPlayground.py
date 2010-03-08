fafb79c0987e0f99397fd29fae641c70775c3a15
Zhurun Zhang <zhurunz@google.com>
2010-03-08 15:03:45 -0800

    Quick fix typo.
    Review URL: http://codereview.chromium.org/672017

--- client/site_tests/realtimecomm_GTalkPlayground/realtimecomm_GTalkPlayground.py
+++ client/site_tests/realtimecomm_GTalkPlayground/realtimecomm_GTalkPlayground.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os, re, shutil, time, 
+import os, re, shutil, time
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error, site_ui
