b5cb23983a378231dae4842aff40e93d755b0a57
Darin Petkov <petkov@chromium.org>
2010-03-10 11:11:29 -0800

    Add a basic enable/disable test for the Bluetooth hci0 interface. Add the interface enable/disable tests to the hwqual suite.
    Review URL: http://codereview.chromium.org/824001

--- client/site_tests/networking_DisableInterface/networking_DisableInterface.py
+++ client/site_tests/networking_DisableInterface/networking_DisableInterface.py
@@ -5,34 +5,39 @@ from autotest_lib.client.common_lib import error
 class networking_DisableInterface(test.test):
     version = 1
 
-    def run_once(self, iface_name = 'eth0'):
+    def run_once(self, iface_name='wlan0'):
         forced_up = False
+
+        # use the right interface configuration utility
+        self._ifconfig = 'ifconfig'
+        if iface_name.startswith('hci'):
+            self._ifconfig = 'hciconfig'
 
         # bring up the interface if its not already up
         if not self.is_iface_up(iface_name):
-            utils.system('ifconfig %s up' % iface_name)
+            utils.system('%s %s up' % (self._ifconfig, iface_name))
             if not self.is_iface_up(iface_name):
                 raise error.TestFail('interface failed to come up')
             forced_up = True
 
         # bring interface down
-        utils.system('ifconfig %s down' % iface_name)
+        utils.system('%s %s down' % (self._ifconfig, iface_name))
         if self.is_iface_up(iface_name):
             raise error.TestFail('interface failed to go down')
 
         # if initial interface state was down, don't bring it back up
         if forced_up:
             return
-        
+
         # bring interface back up
-        utils.system('ifconfig %s up' % iface_name)
+        utils.system('%s %s up' % (self._ifconfig, iface_name))
         if not self.is_iface_up(iface_name):
             raise error.TestFail('interface failed to come up')
 
 
     def is_iface_up(self, name):
         try:
-            out = utils.system_output('ifconfig %s' % name)
+            out = utils.system_output('%s %s' % (self._ifconfig, name))
         except error.CmdError, e:
             logging.info(e)
             raise error.TestNAError('test interface not found')
