4b6bf5db1ec48f0c4514c6f9fa2e7bdcad2550da
Darin Petkov <petkov@chromium.org>
2010-03-11 13:19:35 -0800

    More tests for approved components. Very few actually work (just CPU and audio). Checking in for swatis@, as-is. More work and cleanup may be necessary.
    Review URL: http://codereview.chromium.org/854003

--- client/site_tests/platform_Components/platform_Components.py
+++ client/site_tests/platform_Components/platform_Components.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os
+import logging, os, re
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 
@@ -10,6 +10,8 @@ from autotest_lib.client.common_lib import error
 class platform_Components(test.test):
     version = 1
     _syslog = '/var/log/messages'
+    _audio =  '/proc/asound/*'
+    result = ""
 
 
     def check_component(self, comp_key, comp_id):
@@ -21,18 +23,47 @@ class platform_Components(test.test):
         app_ids = self._approved[comp_key]
         if '*' in app_ids:
             return
-        if not comp_id in app_ids:
-            raise error.TestFail('%s="%s" is not approved' %
-                                 (comp_key, comp_id))
 
+        flag = 0
+        for name in app_ids:
+            if re.search(name, comp_id)==None:
+                flag += 1
+
+        if flag == len(app_ids):
+                self.result += comp_key + ': Not Approved '
+        else:
+                self.result += comp_key + ': Approved '
+
+    # More get methods go here...
+    def get_part_id_audio_codec(self):
+        cmd = 'grep -R Codec: %s | head -n 1 | sed s/.\*Codec://' % self._audio
+        part_id = utils.system_output(cmd).strip()
+        return part_id
+
+    def get_part_id_bluetooth(self):
+        cmd = 'grep -m 1 Bluetooth: %s | sed s/.\*Bluetooth://' % self._syslog
+        part_id = utils.system_output(cmd).strip()
+        return part_id
 
     def get_part_id_cpu(self):
         cmd = 'grep -i -m 1 CPU0: %s | sed s/.\*CPU0://' % self._syslog
         part_id = utils.system_output(cmd).strip()
         return part_id
 
+    def get_part_id_chipset(self):
+        cmd = 'grep -i -m 1 Chipset %s | sed s/.\*kernel://' % self._syslog
+        part_id = utils.system_output(cmd).strip()
+        return part_id
 
-    # More get methods go here...
+    def get_part_id_touchpad(self):
+        cmd = 'grep -i -m 1 touchpad %s | sed s/.\*kernel://' % self._syslog
+        part_id = utils.system_output(cmd).strip()
+        return part_id
+
+    def get_part_id_webcam(self):
+        cmd = 'grep -i -m 1 camera %s | sed s/.\*kernel://' % self._syslog
+        part_id = utils.system_output(cmd).strip()
+        return part_id
 
 
     def run_once(self, approved_db=None):
@@ -42,7 +73,13 @@ class platform_Components(test.test):
         db = os.path.join(self.bindir, approved_db)
         self._approved = eval(utils.read_file(db))
 
+        self.check_component('part_id_audio_codec', 
+                             self.get_part_id_audio_codec())
+        self.check_component('part_id_bluetooth', self.get_part_id_bluetooth())
         self.check_component('part_id_cpu', self.get_part_id_cpu())
+        self.check_component('part_id_chipset', self.get_part_id_chipset())
+        self.check_component('part_id_touchpad', self.get_part_id_touchpad())
+        self.check_component('part_id_webcam', self.get_part_id_webcam())
 
         # More get/check calls go here...
 
@@ -51,3 +88,7 @@ class platform_Components(test.test):
 
         outdb = os.path.join(self.resultsdir, 'system_components')
         utils.open_write_close(outdb, str(self._system))
+     
+        if self.result != "":
+            raise error.TestFail(self.result)
+
