e0b2e46109a014a411138516d683136486fe58b0
Darin Petkov <petkov@chromium.org>
2010-03-11 18:05:36 -0800

    Some cleanup of the test. Remove regular expressions for now otherwise we'll need to escape the generated database so that it can be properly matched later. More cleanup is necessary -- and some checks definitely don't work.
    Review URL: http://codereview.chromium.org/860003

--- client/site_tests/platform_Components/platform_Components.py
+++ client/site_tests/platform_Components/platform_Components.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, re
+import logging, os
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 
@@ -10,8 +10,14 @@ from autotest_lib.client.common_lib import error
 class platform_Components(test.test):
     version = 1
     _syslog = '/var/log/messages'
-    _audio =  '/proc/asound/*'
-    result = ""
+    _cids = [
+        'part_id_audio_codec',
+        'part_id_bluetooth',
+        'part_id_cpu',
+        'part_id_chipset',
+        'part_id_touchpad',
+        'part_id_webcam',
+    ]
 
 
     def check_component(self, comp_key, comp_id):
@@ -20,45 +26,44 @@ class platform_Components(test.test):
         if not self._approved.has_key(comp_key):
             raise error.TestFail('%s missing from database' % comp_key)
 
-        app_ids = self._approved[comp_key]
-        if '*' in app_ids:
+        app_cids = self._approved[comp_key]
+
+        if '*' in app_cids:
             return
 
-        flag = 0
-        for name in app_ids:
-            if re.search(name, comp_id)==None:
-                flag += 1
+        if not comp_id in app_cids:
+            self._failures[comp_key] = [ comp_id ]
 
-        if flag == len(app_ids):
-                self.result += comp_key + ': Not Approved '
-        else:
-                self.result += comp_key + ': Approved '
 
-    # More get methods go here...
     def get_part_id_audio_codec(self):
-        cmd = 'grep -R Codec: %s | head -n 1 | sed s/.\*Codec://' % self._audio
+        cmd = 'grep -R Codec: /proc/asound/* | head -n 1 | sed s/.\*Codec://'
         part_id = utils.system_output(cmd).strip()
         return part_id
+
 
     def get_part_id_bluetooth(self):
         cmd = 'grep -m 1 Bluetooth: %s | sed s/.\*Bluetooth://' % self._syslog
         part_id = utils.system_output(cmd).strip()
         return part_id
 
+
     def get_part_id_cpu(self):
         cmd = 'grep -i -m 1 CPU0: %s | sed s/.\*CPU0://' % self._syslog
         part_id = utils.system_output(cmd).strip()
         return part_id
+
 
     def get_part_id_chipset(self):
         cmd = 'grep -i -m 1 Chipset %s | sed s/.\*kernel://' % self._syslog
         part_id = utils.system_output(cmd).strip()
         return part_id
 
+
     def get_part_id_touchpad(self):
         cmd = 'grep -i -m 1 touchpad %s | sed s/.\*kernel://' % self._syslog
         part_id = utils.system_output(cmd).strip()
         return part_id
+
 
     def get_part_id_webcam(self):
         cmd = 'grep -i -m 1 camera %s | sed s/.\*kernel://' % self._syslog
@@ -68,27 +73,20 @@ class platform_Components(test.test):
 
     def run_once(self, approved_db=None):
         self._system = {}
+        self._failures = {}
         if approved_db is None:
             approved_db = 'approved_components'
         db = os.path.join(self.bindir, approved_db)
         self._approved = eval(utils.read_file(db))
+        logging.debug('Approved DB: %s', self._approved)
 
-        self.check_component('part_id_audio_codec', 
-                             self.get_part_id_audio_codec())
-        self.check_component('part_id_bluetooth', self.get_part_id_bluetooth())
-        self.check_component('part_id_cpu', self.get_part_id_cpu())
-        self.check_component('part_id_chipset', self.get_part_id_chipset())
-        self.check_component('part_id_touchpad', self.get_part_id_touchpad())
-        self.check_component('part_id_webcam', self.get_part_id_webcam())
+        for cid in self._cids:
+            self.check_component(cid, getattr(self, 'get_' + cid)())
 
-        # More get/check calls go here...
-
-        logging.debug(self._system)
-        logging.debug(self._approved)
+        logging.debug('System: %s', self._system)
 
         outdb = os.path.join(self.resultsdir, 'system_components')
         utils.open_write_close(outdb, str(self._system))
-     
-        if self.result != "":
-            raise error.TestFail(self.result)
 
+        if self._failures:
+            raise error.TestFail(self._failures)
