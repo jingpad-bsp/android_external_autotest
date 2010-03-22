f534ae40656093c8cf1d22582c60957968fdeb91
Chris Masone <cmasone@chromium.org>
2010-03-22 16:26:30 -0700

    Fix use of chromeos_utils
    Review URL: http://codereview.chromium.org/1072012

--- client/site_tests/desktopui_DoLogin/desktopui_DoLogin.py
+++ client/site_tests/desktopui_DoLogin/desktopui_DoLogin.py
@@ -3,29 +3,29 @@
 # found in the LICENSE file.
 
 import os, time
-from autotest_lib.client.bin import chromeos_utils, test
+from autotest_lib.client.bin import site_login, test
 from autotest_lib.client.common_lib import error
 
 class desktopui_DoLogin(test.test):
     version = 1
 
     def setup(self):
-        chromeos_utils.setup_autox(self)
+        site_login.setup_autox(self)
 
     def run_once(self):
-        logged_in = chromeos_utils.logged_in()
+        logged_in = site_login.logged_in()
 
         # Can't test login while logged in, so logout.
         if logged_in:
-            if not chromeos_utils.attempt_logout():
+            if not site_login.attempt_logout():
                 raise error.TestFail('Could not terminate existing session')
-            if not chromeos_utils.wait_for_login_manager():
+            if not site_login.wait_for_login_manager():
                 raise error.TestFail("Login manager didn't come back")
 
         # Test account information embedded into json file.
-        if not chromeos_utils.attempt_login(self, 'autox_script.json'):
+        if not site_login.attempt_login(self, 'autox_script.json'):
             raise error.TestFail('Could not login')
 
         # If we started logged out, log back out.
         if not logged_in:
-            chromeos_utils.attempt_logout()
+            site_login.attempt_logout()
