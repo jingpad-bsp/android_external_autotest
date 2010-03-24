3cccb0bf6a99a092ce947dbca5a85c6ebd9067bf
Tom Wai-Hong Tam <waihong@chromium.org>
2010-03-24 13:32:30 +0800

    Add a simple interaction to autotest library. hardware_ExternalDrives is one of the use cases.
    Review URL: http://codereview.chromium.org/1150006

--- client/common_lib/site_ui.py
+++ client/common_lib/site_ui.py
@@ -3,7 +3,7 @@
 # found in the LICENSE file.
 
 import logging, shutil
-from autotest_lib.client.common_lib import utils
+from autotest_lib.client.common_lib import site_httpd, utils
 
 
 def xcommand(cmd):
@@ -48,3 +48,80 @@ class ChromeSession(object):
         if self.job is not None:
             utils.nuke_subprocess(self.job.sp)
         self.job = None
+
+
+
+_HTML_HEADER_ = '''
+<html><head>
+<title>Question Dialog</title>
+<script language="Javascript">
+function do_submit(value) {
+    document.forms[0].result.value = value;
+    document.forms[0].submit();
+}
+</script>
+</head><body>
+<h3>%s</h3>
+<form action="/answer" method="post">
+    <input type="hidden" name="result" value="">
+'''
+
+_HTML_BUTTON_ = '''<input type="button" value="%s" onclick="do_submit('%s')">'''
+
+_HTML_FOOTER_ = '''</form></body></html>'''
+
+
+
+class Dialog(object):
+    """
+    A class to create a simple interaction with a user, like asking a question
+    and receiving the answer.
+    """
+
+    def __init__(self, question='', choices=['Pass', 'Fail'], timeout=60):
+        self.init(question, choices, timeout)
+
+
+    def init(self, question='', choices=['Pass', 'Fail'], timeout=60):
+        self._question = question
+        self._choices = choices
+        self._timeout = timeout
+
+
+    def return_html(self, server, args):
+        html = _HTML_HEADER_ % self._question
+        for choice in self._choices:
+            html += _HTML_BUTTON_ % (choice, choice)
+        html += _HTML_FOOTER_
+        server.wfile.write(html)
+
+
+    def get_result(self):
+        # Run a HTTP server.
+        url = 'http://localhost:8000/'
+        http_server = site_httpd.HTTPListener(8000)
+        http_server.run()
+
+        # Assign the handlers.
+        http_server.add_url_handler('/',
+            lambda server, form, o=self: o.return_html(server, form))
+        http_server.add_url_handler('/answer',
+            lambda server, form, o=self: o.return_html(server, form))
+
+        try:
+            # Start a Chrome session to load the page.
+            session = ChromeSession(url)
+            latch = http_server.add_wait_url('/answer')
+            latch.wait(self._timeout)
+        finally:
+            session.close()
+            http_server.stop()
+
+        # Return None if timeout.
+        if not latch.is_set():
+            http_server.stop()
+            return None
+
+        result = http_server.get_form_entries()['result']
+        http_server.stop()
+        return result
