509b3999269c20a1e7750c8c50dfcfe7b8b82a66
Kelly Lucas <kdlucas@chromium.org>
2010-03-26 10:57:58 -0700

    A simple test case that verifies some basic memory settings.
    Review URL: http://codereview.chromium.org/1374002

---
+++ client/site_tests/platform_MemCheck/platform_MemCheck.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+__author__ = 'kdlucas@chromium.org (Kelly Lucas)'
+
+import logging
+import os
+
+from autotest_lib.client.bin import utils, test
+from autotest_lib.client.common_lib import error
+
+
+class platform_MemCheck(test.test):
+    """
+    Verify memory usage looks correct.
+    """
+    version = 1
+
+    def run_once(self):
+        errors = 0
+
+        memref = 1024892
+        less_refs = ['MemTotal', 'MemFree', 'VmallocTotal']
+        equal_refs = ['SwapCached', 'SwapTotal']
+
+        ref = {'MemTotal': memref,
+               'MemFree': memref/2,
+               'SwapCached': 0,
+               'SwapTotal': 0,
+               'VmallocTotal': 102400,
+              }
+
+        for k in ref:
+            value = utils.read_from_meminfo(k)
+            if k in less_refs:
+                if value < ref[k]:
+                    logging.warn('%s is %d' % (k, value))
+                    logging.warn('%s should be at least %d' % (k, ref[k]))
+                    errors += 1
+            elif k in equal_refs:
+                if value != ref[k]:
+                    logging.warn('%s is %d' % (k, value))
+                    logging.warn('%s should be %d' % (k, ref[k]))
+                    errors += 1
+
+        # If self.error is not zero, there were errors.
+        if errors > 0:
+            raise error.TestFail('Found %d incorrect values' % errors)
