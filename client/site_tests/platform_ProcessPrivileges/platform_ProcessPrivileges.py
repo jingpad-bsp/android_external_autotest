1f493833e1a506b0dc40237c51f16ae7e2ef4f3d
Yusuke Sato <yusukes@chromium.org>
2010-03-26 10:22:04 +0900

    Merge desktopui_ChromeRunning.py into platform_ProcessPrivileges.py.
    - Merged desktopui_ChromeRunning.py into platform_ProcessPrivileges.py and deleted desktopui_ChromeRunning.py following petkov's suggestion in http://codereview.chromium.org/1108007.
    - Added tests for IBus (I18N text input) daemons to platform_ProcessPrivileges.py.
    - Temporarily disabled PROCESS FLAGS checks. See http://crosbug.com/2253 for details.
    
    Review URL: http://codereview.chromium.org/1332002

--- client/site_tests/platform_ProcessPrivileges/platform_ProcessPrivileges.py
+++ client/site_tests/platform_ProcessPrivileges/platform_ProcessPrivileges.py
@@ -2,48 +2,70 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from autotest_lib.client.bin import test, utils
+import time
+from autotest_lib.client.bin import site_login, test, utils
 from autotest_lib.client.common_lib import error
 
 class platform_ProcessPrivileges(test.test):
     version = 1
 
-    def run_once(self, process='X', user=None, run_as_root=False):
+    def setup(self):
+        site_login.setup_autox(self)
+
+
+    def run_once(self, process='X', user=None, run_as_root=False,
+                 do_login=False):
         """Check if the process is running as the specified user / root.
 
         Args:
             process: Process name to check.
             user: User process must run as; ignored if None.
             run_as_root: Is process allowed to run as root?
+            do_login: login before getting process information?
         """
+        logged_in = site_login.logged_in()
 
-        # Get the process information
-        pscmd = 'ps -o f,euser,ruser,suser,fuser,comm -C %s --no-headers'
-        ps = utils.system_output(pscmd % process, retain_output=True)
+        if do_login and not logged_in:
+            # Test account information embedded into json file.
+            if not site_login.attempt_login(self, 'autox_script.json'):
+                raise error.TestFail('Could not login')
+            # Wait for processes for user-session are started.
+            time.sleep(10)
 
-        pslines = ps.splitlines()
+        try:
+            # Get the process information
+            pscmd = 'ps -o f,euser,ruser,suser,fuser,comm -C %s --no-headers'
+            ps = utils.system_output(pscmd % process, retain_output=True)
 
-        # Fail if process is not running
-        if not len(pslines):
-            raise error.TestFail('Process %s is not running' % process)
+            pslines = ps.splitlines()
 
-        # Check all instances of the process
-        for psline in pslines:
-            ps = psline.split()
+            # Fail if process is not running
+            if not len(pslines):
+                raise error.TestFail('Process %s is not running' % process)
 
-            # Fail if not running as the specified user
-            if user is not None:
-                for uid in ps[1:5]:
-                    if uid != user:
+            # Check all instances of the process
+            for psline in pslines:
+                ps = psline.split()
+
+                # Fail if not running as the specified user
+                if user is not None:
+                    for uid in ps[1:5]:
+                        if uid != user:
+                            raise error.TestFail(
+                                'Process %s running as %s; expected %s' %
+                                (process, uid, user))
+
+                # Check if process has super-user privileges
+                if not run_as_root:
+                    # TODO(yusukes): Uncomment this once issue 2253 is resolved
+                    # if int(ps[0]) & 0x04:
+                    #    raise error.TestFail(
+                    #        'Process %s running with super-user flag' %
+                    #        process)
+                    if 'root' in ps:
                         raise error.TestFail(
-                            'Process %s running as %s; expected %s' %
-                            (uid, user))
-
-            # Check if process has super-user privileges
-            if not run_as_root:
-                if int(ps[0]) & 0x04:
-                    raise error.TestFail(
-                        'Process %s running with super-user flag' % process)
-                if 'root' in ps:
-                    raise error.TestFail(
-                        'Process %s running as root' % process)
+                            'Process %s running as root' % process)
+        finally:
+            # If we started logged out, log back out.
+            if do_login and not logged_in:
+                site_login.attempt_logout()
