a3b485e38cf50af56b35dab58d1457d1292d20ca
Tom Wai-Hong Tam <waihong@chromium.org>
2010-03-26 18:17:14 +0800

    Test system firmware size >= 2MB
    Review URL: http://codereview.chromium.org/1377002

---
+++ client/site_tests/firmware_RomSize/firmware_RomSize.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
+
+class firmware_RomSize(test.test):
+    version = 1
+
+    def run_once(self):
+        cmd = 'dmidecode | grep "ROM Size" | sed "s/.*: \([0-9]\+\) kB/\\1/"'
+        size = int(utils.system_output(cmd).strip())
+        self.write_perf_keyval({"kb_system_rom_size": size})
+
+        if size < 2048:
+            raise error.TestFail("System firmware size < 2MB");
