81ad616e9364b35415181f5b8a862e0bba324ddf
Will Drewry <wad@chromium.org>
2010-04-01 10:26:07 -0500

    Check if the mount is mounted in the cryptohomed process vfs namespace
    This updates the test to check in the cryptohomed VFS namespace.
    
    TEST=manually run
    BUG=none
    
    Review URL: http://codereview.chromium.org/1524004

--- client/bin/site_cryptohome.py
+++ client/bin/site_cryptohome.py
@@ -9,7 +9,8 @@ from autotest_lib.client.common_lib import error
 def is_mounted(device = chromeos_constants.CRYPTOHOME_DEVICE,
                expected_mountpt = chromeos_constants.CRYPTOHOME_MOUNT_PT,
                allow_fail = False):
-    mount_line = utils.system_output('/bin/mount | grep %s' % expected_mountpt,
-                                     ignore_status = allow_fail)
+    mount_line = utils.system_output(
+        'grep %s /proc/$(pgrep cryptohomed)/mounts' % expected_mountpt,
+        ignore_status = allow_fail)
     mount_parts = mount_line.split()
     return len(mount_parts) > 0 and device == mount_parts[0]
