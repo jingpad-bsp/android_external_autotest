91c9040ca00c77f9bf5d44184a48f42c83e3b552
Jie Sun <jiesun@google.com>
2010-04-02 17:44:37 -0700

    update test cases. more issues:
    1. libffmpegsumo.so is not correct in normal build. 2. ffmpeg_tests need to be fetched from build. 3. some clips on build.chromium.org is not correct.
    
    Review URL: http://codereview.chromium.org/1520013

--- client/site_tests/audiovideo_FFMPEG/audiovideo_FFMPEG.py
+++ client/site_tests/audiovideo_FFMPEG/audiovideo_FFMPEG.py
@@ -2,13 +2,24 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import glob, logging, os, re, stat, string, time, urllib
+import glob, logging, os, re, shutil, stat, string, time, urllib
 
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error, utils
 
 class audiovideo_FFMPEG(test.test):
     version = 1
+
+    def setup(self):
+        """ copy test asset to bindir. """
+        if not os.path.exists(self.srcdir):
+            os.mkdir(self.srcdir)
+        # sysroot = os.environ["SYSROOT"]
+        # testdir = os.path.join(sysroot, "usr/local/autotest-chrome")
+        # testbin = os.path.join(testdir, "ffmpeg_tests")
+	# TODO(jiesun): retrieve chrome test asset from build.
+        # shutil.copy(testbin, self.bindir)
+
 
     def run_once(self):
         """ Run FFMPEG performance test! """
@@ -27,7 +38,10 @@ class audiovideo_FFMPEG(test.test):
 
 
     def run_testcase(self, testcase):
-        executable = os.path.join(self.bindir, "ffmpeg_tests.i686")
+        if utils.get_arch() == 'i386':
+            executable = os.path.join(self.bindir, "ffmpeg_tests.i686")
+        else:  # TODO(jiesun): we only have ARM and i386.
+            executable = os.path.join(self.bindir, "ffmpeg_tests.arm")
         file_url = testcase[0]
 
         # TODO(jiesun): if url is not local, grab it from internet.
@@ -55,17 +69,32 @@ class audiovideo_FFMPEG(test.test):
                                  command_line,
                                  retain_output=True)
 
-        # what's the fps we measures.
-        fps_pattern = re.search(r"FPS:\s+([\d\.]+)", stdout)
-        if fps_pattern is None:
-            raise error.TestFail("ffmpeg_tests failed to exit normally!")
-        fps = float(fps_pattern.group(1))
         cpu_usage *= 100.0  # in percentage.
-        logging.info("Cpu Usage %s%%; FPS: %s" % (cpu_usage, fps))
 
-        # record the performance data for future analysis.
-        namekey = string.replace(string.lower(file_name), '.', '_')
-        self.performance_results['fps_' + namekey] = fps
-        self.performance_results['cpuusage_' + namekey] = cpu_usage
+        # what's the fps we measures for video.
+        fps_pattern = re.search(r"FPS:\s+([\d\.]+)", stdout)
+        # what's the time per frame for audio.
+        tpf_pattern = re.search(r"TIME PER FRAME \(MS\):\s+([\d\.]+)", stdout)
+        if fps_pattern:
+            fps = float(fps_pattern.group(1))
+            logging.info("CPU Usage %s%%; FPS: %s" % (cpu_usage, fps))
+            # record the performance data for future analysis.
+            namekey = file_name.lower().replace('.', '_')
+            self.performance_results['fps_' + namekey] = fps
+            self.performance_results['cpuusage_' + namekey] = cpu_usage
+        elif tpf_pattern:
+            tpf = float(tpf_pattern.group(1))
+            logging.info("CPU Usage %s%%; TimePerFrame: %s" % (cpu_usage, tpf))
+            # record the performance data for future analysis.
+            namekey = file_name.lower().replace('.', '_')
+            self.performance_results['timeperframe_' + namekey] = tpf
+            self.performance_results['cpuusage_' + namekey] = cpu_usage
+        else:
+            raise error.TestFail("ffmpeg_tests failed to exit normally!")
 
         # TODO(jiesun/fbarchard): what else need to be checked?
+
+        # remove file after test to save diskspace.
+        os.remove(file_path);
+
+
