11adeb3fb6461763a4f8ee8f887cdbba1910cd6f
rginda <rginda@chromium.org>
2010-04-07 17:50:56 -0700

    turn login_CryptohomeMounted into a site_ui_test.UITest
    Review URL: http://codereview.chromium.org/1558028

--- client/site_tests/login_CryptohomeMounted/login_CryptohomeMounted.py
+++ client/site_tests/login_CryptohomeMounted/login_CryptohomeMounted.py
@@ -4,29 +4,14 @@
 
 import os, utils, time
 from autotest_lib.client.bin import chromeos_constants
-from autotest_lib.client.bin import site_cryptohome, site_login, test
+from autotest_lib.client.bin import site_cryptohome, site_ui_test
 from autotest_lib.client.common_lib import error
 
-class login_CryptohomeMounted(test.test):
+class login_CryptohomeMounted(site_ui_test.UITest):
     version = 1
 
-    # TODO: figure the right fix for missing setup_autox
-    # def setup(self):
-    #     site_login.setup_autox(self)
-
-    def run_once(self, script='autox_script.json', is_control=False):
-        # Make sure that we're logged out initially -- this test is run
-        # multiple times, and we don't want to reuse the previous instance's
-        # session.
-        if site_login.logged_in():
-            site_login.attempt_logout()
-
-        # Test account information embedded into json file.
-        site_login.attempt_login(self, script)
-
+    def run_once(self, is_control=False):
         if (not is_control and
             not site_cryptohome.is_mounted(allow_fail=is_control)):
             raise error.TestFail('CryptohomeIsMounted should return %s' %
                                  (not is_control))
-
-        site_login.attempt_logout()
