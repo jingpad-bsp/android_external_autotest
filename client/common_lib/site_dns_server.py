f7ae5257c0de65006d0cc3002504b6c617ed411b
Chris Masone <cmasone@chromium.org>
2010-04-21 08:21:25 -0700

    Add wrapper for miniFakeDns that runs it in a separate thread
    Review URL: http://codereview.chromium.org/1706006

---
+++ client/common_lib/site_dns_server.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging, threading, time, utils
+
+class LocalDns(object):
+    """a wrapper around miniFakeDns that handles managing running the server
+    in a separate thread.
+    """
+
+    def __init__(self, fake_ip="127.0.0.1"):
+        import miniFakeDns  # So we don't need to install it in the chroot.
+        self._dns = miniFakeDns.DNSServer(fake_ip="127.0.0.1")
+        self._stopper = threading.Event()
+        self._thread = threading.Thread(target=self._dns.run,
+                                        args=(self._stopper,))
+
+
+    def run(self):
+        self._thread.start()
+
+
+    def stop(self):
+        self._stopper.set()
+        self._thread.join()
