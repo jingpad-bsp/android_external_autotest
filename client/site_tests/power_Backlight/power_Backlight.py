af1acfd563293f731f3bd4d07c5b3adba14d721f
David James <davidjames@google.com>
2010-04-21 13:42:23 -0700

    Update power_Backlight test to stop screen locker and dpms.
    Also update power_Backlight test to write out performance data
    before failing out.
    
    Review URL: http://codereview.chromium.org/1585045

--- client/site_tests/power_Backlight/power_Backlight.py
+++ client/site_tests/power_Backlight/power_Backlight.py
@@ -2,15 +2,22 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, time, utils
+import logging, os, time, utils
 from autotest_lib.client.bin import test
-from autotest_lib.client.common_lib import error, site_power_status
+from autotest_lib.client.common_lib import error, site_power_status, site_ui, \
+                                           utils
 
 
 class power_Backlight(test.test):
     version = 1
 
+
     def run_once(self, seconds=120):
+        # disable screen locker and screen blanking
+        # TODO(davidjames): Power manager should support this feature directly
+        os.system('stop screen-locker')
+        site_ui.xsystem('xset -dpms')
+
         status = site_power_status.get_status()
         if status.linepower[0].online:
             raise error.TestFail('Machine must be unplugged')
@@ -30,7 +37,15 @@ class power_Backlight(test.test):
             status.refresh()
             keyvals['w_bl_%d_rate' % i] = status.battery[0].energy_rate
             rates.append(status.battery[0].energy_rate)
-            if len(rates) > 1 and rates[-1] <= rates[-2]:
-              raise error.TestFail('Turning up the backlight ' \
-                                   'should increase energy consumption')
         self.write_perf_keyval(keyvals)
+        for i in range(1, len(levels)):
+            if rates[i] <= rates[i-1]:
+                raise error.TestFail('Turning up the backlight ' \
+                                     'should increase energy consumption')
+
+
+    def cleanup(self):
+        # re-enable screen locker and screen blanking
+        utils.system('start screen-locker')
+        site_ui.xsystem('xset +dpms')
+
