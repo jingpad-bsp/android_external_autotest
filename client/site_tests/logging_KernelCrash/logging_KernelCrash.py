b031da2fcc68c35c25285aedadd61fd7ffa0b3de
Ken Mixter <kmixter@google.com>
2010-05-06 17:19:30 -0700

    Add control file for logging_KernelCrash to give dashboard metadata
    Review URL: http://codereview.chromium.org/2031003

--- client/site_tests/logging_KernelCrash/logging_KernelCrash.py
+++ client/site_tests/logging_KernelCrash/logging_KernelCrash.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os, time
+import logging, os, time
 from autotest_lib.client.bin import site_log_reader, site_ui_test, test
 from autotest_lib.client.common_lib import error, utils
 
