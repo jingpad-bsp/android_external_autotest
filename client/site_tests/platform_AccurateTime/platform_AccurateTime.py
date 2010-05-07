da8e9e3d88519735a0f5e8f0bc653ccae2083b98
Ken Mixter <kmixter@google.com>
2010-05-07 09:19:51 -0700

    Add better diagnostics to platform_AccurateTime
    Review URL: http://codereview.chromium.org/1998011

--- client/site_tests/platform_AccurateTime/platform_AccurateTime.py
+++ client/site_tests/platform_AccurateTime/platform_AccurateTime.py
@@ -1,5 +1,9 @@
-import commands, logging, math, re
-from autotest_lib.client.bin import test
+# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging, math, re, utils
+from autotest_lib.client.bin import site_log_reader, test
 from autotest_lib.client.common_lib import error
 
 class platform_AccurateTime(test.test):
@@ -9,35 +13,27 @@ class platform_AccurateTime(test.test):
     def __get_offset(self, string):
         offset = re.search(r'(-?[\d+\.]+)s', string)
         if offset is None:
+            # If string is empty, check the sys logs dumped later.
             raise error.TestError('Unable to find offset in %s' % string)
         return float(offset.group(1))
 
 
-    def __restart_ntp_server(self):
-        return commands.getstatusoutput('initctl start ntp')[0] == 0
-
-
     def run_once(self):
+        reader = site_log_reader.LogReader()
+        reader.set_start_by_current()
         # Check ntpd is currently running
-        if commands.getstatusoutput('pgrep ntpd')[0] != 0:
+        if utils.system('pgrep ntpd', ignore_status=True) != 0:
             raise error.TestError('NTP server was not already running')
         # Stop it since we cannot force ntp requests unless its not running
-        if commands.getstatusoutput('initctl stop ntp')[0] != 0:
-            raise error.TestError('NTP server could not be stopped')
+        utils.system('initctl stop ntp')
         try:
             # Now grab the current time and get its offset
-            (status, output) = commands.getstatusoutput('ntpd -g -u ntp:ntp -q')
-            if status != 0:
-                raise error.TestError('NTP current-time check failed')
+            output = utils.system_output('ntpd -g -u ntp:ntp -q',
+                                         retain_output=True)
             server_offset = self.__get_offset(output)
             logging.info("server time offset: %f" % server_offset)
 
             self.write_perf_keyval({'seconds_offset': abs(server_offset)})
-        except error.TestError, e:
-            # In case of error, restart server and pass up original error.
-            self.__restart_ntp_server()
-            raise e
-
-        # Restart server
-        if not self.__restart_ntp_server():
-            raise error.TestError('NTP server could not be restarted')
+        finally:
+            utils.system('initctl start ntp')
+            logging.debug('sys logs emitted: %s' % reader.get_logs())
