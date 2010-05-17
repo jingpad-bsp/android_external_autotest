c908cf887b3ad501bd76d634075071bdf7e75036
Darin Petkov <petkov@chromium.org>
2010-05-17 10:18:56 -0700

    Get the firmware boot time from the file generated by dump-boot-stats.
    Review URL: http://codereview.chromium.org/2066007

--- client/site_tests/platform_BootPerf/platform_BootPerf.py
+++ client/site_tests/platform_BootPerf/platform_BootPerf.py
@@ -35,8 +35,18 @@ class platform_BootPerf(test.test):
         results['sectors_read_kernel_to_login'] = float(vals[2])
 
 
-    # Find both firmware time (on x86 only) and reboot/shutdown time
-    # if the last boot was a reboot.
+    def __parse_firmware_boot_time(self, results):
+        data = None
+        try:
+            # If the firmware boot time is not available, the file
+            # will not exists.
+            data = utils.read_one_line('/tmp/firmware-boot-time')
+        except IOError:
+            return
+        results['seconds_firmware_boot'] = float(data)
+
+
+    # Find the reboot/shutdown time if the last boot was a reboot.
     def __parse_syslog(self, results, last_boot_was_reboot):
         file_handle = None
         logfile = '/var/log/messages'
@@ -45,23 +55,19 @@ class platform_BootPerf(test.test):
         except:
             raise error.TestFail('Test is unable to read "%s"' % logfile)
         mhz = 0
-        ticks = 0
         startups_found = 0
         last_shutdown_time = None
         kernel_start_time = None
-        seconds_firmware_boot = 0
         datetime_re = r'(\d{4})-(\d{2})-(\d{2})[A-Z]' + \
                       r'(\d{2}):(\d{2}):(\d{2})\.(\d{6})'
         last_shutdown_re = re.compile(
             datetime_re + r'.*(klog|tty2) main process.*killed by TERM')
         startup_re = re.compile(datetime_re + r'.*000\] Linux version \d')
         mhz_re = re.compile(r'Detected (\d+\.\d+) MHz processor.')
-        initial_tsc_re = re.compile('Initial TSC value: (\d+)')
         for line in file_handle.readlines():
             match = startup_re.match(line)
             if match is not None:
                 mhz = 0
-                ticks = 0
                 datetime_args = tuple([int(x) for x in match.groups()[:7]])
                 kernel_start_time = datetime.datetime(*datetime_args)
                 startups_found += 1
@@ -72,14 +78,7 @@ class platform_BootPerf(test.test):
             match = mhz_re.search(line)
             if match is not None:
                 mhz = float(match.group(1))
-            match = initial_tsc_re.search(line)
-            if match is not None:
-                ticks = int(match.group(1))
-                logging.info('Found initial TSC: %d' % ticks)
         file_handle.close()
-        if mhz > 0 and startups_found > 0 and ticks > 0:
-            seconds_firmware_boot = float(ticks) / mhz / 1000000
-            results['seconds_firmware_boot'] = seconds_firmware_boot
         if (last_shutdown_time != None and last_boot_was_reboot and
             kernel_start_time != None):
             logging.info('Kernel start time: %s, last shutdown time: %s' %
@@ -95,7 +94,7 @@ class platform_BootPerf(test.test):
                            1000000.0)
             results['seconds_reboot_time'] = reboot_time
             results['seconds_shutdown_time'] = \
-                reboot_time - seconds_firmware_boot
+                reboot_time - results.get('seconds_firmware_boot', 0.0)
         results['reboots_in_syslog'] = startups_found
         results['mhz_primary_cpu'] = mhz
 
@@ -112,6 +111,7 @@ class platform_BootPerf(test.test):
         for resultname, filename in uptime_files:
             results[resultname] = self.__parse_uptime(filename)
 
+        self.__parse_firmware_boot_time(results)
         self.__parse_disk_login_prompt_ready(results)
         self.__parse_syslog(results, last_boot_was_reboot)
 
