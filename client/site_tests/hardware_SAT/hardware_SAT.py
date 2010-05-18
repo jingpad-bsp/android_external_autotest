3e80452f0fa9b7011751878d1a6c3c27ec498db8
Nick Sanders <nsanders@google.com>
2010-05-18 15:00:37 -0700

    update SAT version to 1.0.3
    * add 2GB support
    * optimize args for most memory, cpu stress.
    * add SSE support
    
    Review URL: http://codereview.chromium.org/2130002

--- client/site_tests/hardware_SAT/hardware_SAT.py
+++ client/site_tests/hardware_SAT/hardware_SAT.py
@@ -10,9 +10,8 @@ from autotest_lib.client.common_lib import error
 class hardware_SAT(test.test):
     version = 1
 
-    # http://stressapptest.googlecode.com/files/\
-    #   stressapptest-1.0.2_autoconf.tar.gz
-    def setup(self, tarball='stressapptest-1.0.2_autoconf.tar.gz'):
+    # http://code.google.com/p/stressapptest/ 
+    def setup(self, tarball='stressapptest-1.0.3_autoconf.tar.gz'):
         # clean
         if os.path.exists(self.srcdir):
             utils.system('rm -rf %s' % self.srcdir)
@@ -36,14 +35,34 @@ class hardware_SAT(test.test):
 
 
     def run_once(self, seconds=60):
+        # Allow shmem access to all of memory. This is used for 32 bit
+        # access to > 1.4G. Virtual address space limitation prevents
+        # directly mapping the memory.
+        utils.run('mount -o remount,size=100% /dev/shm')
         cpus = max(utils.count_cpus(), 1)
         mbytes = max(int(utils.freememtotal() * .95 / 1024), 512)
+        # SAT should use as much memory as possible, while still
+        # avoiding OOMs and allowing the kernel to run, so that
+        # the maximum amoun tof memory can be tested.
         args = ' -M %d' % mbytes  # megabytes to test
+        # The number of seconds under test can be chosen to fit into
+        # manufacturing or test flow. 60 seconds gives several
+        # passes and several patterns over each memory location
+        # and should catch clearly fautly memeory. 4 hours
+        # is an effective runin test, to catch lower frequency errors.
         args += ' -s %d' % seconds  # seconds to run
-        args += ' -m %d' % cpus  # memory copy threads
-        args += ' -i %d' % cpus  # memory invert threads
-        args += ' -C %d' % cpus  # CPU stress threads
-        args += ' -f diskthread'  # disk thread
+        # One memory copy thread per CPU should keep the memory bus
+        # as saturated as possible, while keeping each CPU busy as well.
+        args += ' -m %d' % cpus  # memory copy threads.
+        # SSE copy and checksum increases the rate at which the CPUs
+        # can drive memory, as well as stressing the CPU.
+        args += ' -W' % cpus  # Use SSE optimizatin in memory threads.
+        # File IO threads allow stressful transactions over the
+        # south bridge and SATA, as well as potentially finding SSD
+        # or disk cache problems. Two threads ensure multiple
+        # outstanding transactions to the disk, if supported.
+        args += ' -f sat.diskthread.a'  # disk thread
+        args += ' -f sat.diskthread.b'
 
         os.chdir(os.path.join(self.srcdir, 'src'))
         sat = utils.run('./stressapptest' + args)
