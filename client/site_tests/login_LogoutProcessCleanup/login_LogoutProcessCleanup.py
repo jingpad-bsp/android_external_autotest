5dd36fdc64704edf4f782e0812d650762ac093c0
Bryan Yeung <bryeung@chromium.org>
2010-06-18 14:56:34 -0400

    Revised login_LogoutProcessCleanup test
    TEST=manually
    BUG=none
    
    Patch from James MacLean <wjmaclean@chromium.org>.
    
    The file login_LogoutProcessCleanup.py file has been modified to:
    
    1) add a process that ignores SIGTERM, in order to test that such processes
       are killed properly, and
    
    2) before logout generate a list of 'chronos' processes in order that we can
       check to make sure they are all terminated properly during the logout.
    
    Step 2 is necessary since not all 'chronos' processes are created by the
    session_manager, and thus a test of descendancy from session_manager is
    not sufficient to ensure all 'chronos' processes have in fact been killed.
    
    This approach still allows for a race condition in which some 'chronos'
    process is created between generating the list, and logging out. Such a
    process, if it escaped being killed during logout, would constitute a
    false positive for the test (it should have failed). However, this seems
    to be the best we can do without inserting commands into the boot-script
    structure, which is not feasible. (Specifically, a list could be compiled
    during the time while the UI is not running, as controlled by
    /etc/init/ui.conf, to verify that no 'chronos' processes exist at this
    time.)
    
    Review URL: http://codereview.chromium.org/2779003

--- client/site_tests/login_LogoutProcessCleanup/login_LogoutProcessCleanup.py
+++ client/site_tests/login_LogoutProcessCleanup/login_LogoutProcessCleanup.py
@@ -2,9 +2,46 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, time, utils
+import logging, os, time, utils, signal
 from autotest_lib.client.bin import site_ui_test
-from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib import error, utils
+
+class TestProcess:
+
+    def __init__(self, command, pattern):
+      self.command = command
+      self.pattern = pattern
+      self.pid_su = ''
+      self.pid_bash = ''
+
+    def __wait_for_subprocess(self):
+        """Waits for 10 seconds for a process whose job args matches the
+           expression given by self.pattern, and indicates whether it did or
+           not. ASSUMES only one process will match pattern."""
+
+        tries_remaining = 100
+        pid = ''
+        while tries_remaining > 0:
+          pid = utils.system_output('ps -U chronos -o pid,args | grep %s'
+                                    % self.pattern, ignore_status = True)
+          if pid != '':
+              # lstrip() on next line important, as PID data sometimes has
+              # leading spaces when returned from ps.
+              self.pid_bash = pid.lstrip().split(' ')[0]
+              break
+          time.sleep(0.1)
+          tries_remaining -= 1
+
+    def run_me_as_chronos(self):
+        """Runs the command in self.command as user 'chronos'. Waits for bash
+           sub-process to start, and fails if this does not happen."""
+
+        # Start process as user chronos.
+        self.pid_su = utils.BgJob('su chronos -c "%s"' % self.command)
+        # Get pid of bash sub-process. Even though utils.BgJob() has exited,
+        # the su-process may not have created its sub-process yet.
+        self.__wait_for_subprocess()
+        return self.pid_bash != ''
 
 class login_LogoutProcessCleanup(site_ui_test.UITest):
     version = 1
@@ -78,10 +115,19 @@ class login_LogoutProcessCleanup(site_ui_test.UITest):
                 return True
         return False
 
-
     def run_once(self, is_control=False, timeout=10):
-        # Start a process as chronos.  This should get killed when logging out.
-        bg_job = utils.BgJob('su chronos -c "sleep 3600"')
+
+        test_processes = []
+        test_processes.append(
+            TestProcess('while :; do :; done ; # tst00','bash.*tst00'))
+        # Create a test command that ignores SIGTERM.
+        test_processes.append(
+            TestProcess('trap 15; while :; do :; done ; # tst01',
+                        'bash.*tst01'))
+
+        for test in test_processes:
+            if not test.run_me_as_chronos():
+                raise error.TestFail('Did not start: bash %s' % test.command)
 
         session_manager = self.__get_session_manager_pid()
         if session_manager == "":
@@ -90,6 +136,17 @@ class login_LogoutProcessCleanup(site_ui_test.UITest):
         if not self.__has_chronos_processes(session_manager):
             raise error.TestFail('Expected to find processes owned by chronos '
                 'that were not started by the session manager while logged in.')
+
+        cpids = self.__get_chronos_pids()
+
+        # Sanity checks: make sure test jobs are in the list & still running.
+        for test in test_processes:
+          if cpids.count(test.pid_bash) != 1:
+            raise error.TestFail('Job missing (%s - %s)' %
+                                 (test.pid_bash, test.command))
+          if self.__is_process_dead(test.pid_bash):
+            raise error.TestFail('Job prematurely dead (%s - %s)' %
+                                 (test.pid_bash, test.command))
 
         self.logout()
 
@@ -102,10 +159,20 @@ class login_LogoutProcessCleanup(site_ui_test.UITest):
             time.sleep(0.1)
             session_manager = self.__get_session_manager_pid()
 
-        if self.__has_chronos_processes(session_manager):
-            # Make sure the test job we started is dead.
-            if bg_job.sp.returncode == None:
-                bg_job.sp.kill()
-            raise error.TestFail('Expected NOT to find processes owned by '
-                'chronos that were not started by the session manager '
-                'while logged out.')
+        # Make sure all pre-logout chronos processes are now dead.
+        old_pid_count = 0
+        for p in cpids:
+          if not self.__is_process_dead(p):
+            old_pid_count += 1
+            proc_args = utils.system_output('ps -p %s -o args=' % p,
+                                            ignore_status = True)
+            logging.info('Found pre-logout chronos process pid=%s (%s) '
+                         'still alive.' % (p, proc_args))
+            # If p is something we started, kill it.
+            for test in test_processes:
+              if (p == test.pid_su or p == test.pid_bash):
+                utils.signal_pid(p, signal.SIGKILL)
+
+        if old_pid_count > 0:
+          raise error.TestFail('Found %s chronos processes that survived '
+                               'logout.' % old_pid_count)
