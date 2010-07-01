59d3bfbd4baa573e8ee55001839b5b5d26ea055e
J. Richard Barnette <jrbarnette@chromium.org>
2010-06-30 18:33:22 -0700

    Make the test paramaters for ProcessPrivileges private to the test
    This prevents duplicating a list of processes and their required
    properties in two different places; the duplication was causing bugs
    as sometimes only one list would be updated.
    
    The code motion also uncovered dead code to be removed and other
    opportunities for cleanup to be exploited.
    
    BUG=4388
    TEST=run each control file manually on an ST8; test error cases with temporary script change
    
    Review URL: http://codereview.chromium.org/2853021

--- client/site_tests/platform_ProcessPrivileges/platform_ProcessPrivileges.py
+++ client/site_tests/platform_ProcessPrivileges/platform_ProcessPrivileges.py
@@ -11,16 +11,17 @@ class platform_ProcessPrivileges(site_ui_test.UITest):
 
     auto_login = False
 
-    def run_once(self, process='X', user=None, run_as_root=False,
-                 do_login=False, any=False):
+    def log_error(self, process, command, message):
+        self.job.record('ERROR', None, command, message)
+        self._failed.append(process)
+
+    def check_process(self, process, user=None, do_login=False):
         """Check if the process is running as the specified user / root.
 
         Args:
             process: Process name to check.
             user: User process must run as; ignored if None.
-            run_as_root: Is process allowed to run as root?
             do_login: login before getting process information?
-            any: Test succeeds if any of processes satisfy the conditions.
         """
         if do_login:
             self.login()
@@ -29,51 +30,58 @@ class platform_ProcessPrivileges(site_ui_test.UITest):
 
         # Get the process information
         pscmd = 'ps -o f,euser,ruser,suser,fuser,comm -C %s --no-headers'
-        ps = utils.system_output(pscmd % process, retain_output=True)
+        pscmd = pscmd % process
+        ps = utils.system_output(pscmd,
+                                 ignore_status=True, retain_output=True)
 
         pslines = ps.splitlines()
 
         # Fail if process is not running
         if not len(pslines):
-            raise error.TestFail('Process %s is not running' % process)
+            self.log_error(process, pscmd,
+                           'Process %s is not running' % process)
+            return
 
         # Check all instances of the process
         for psline in pslines:
             ps = psline.split()
 
-            # Assume process meets conditions until proven otherwise
-            user_satisfied = True
-            run_as_root_satisfied = True
-
             # Fail if not running as the specified user
             if user is not None:
                 for uid in ps[1:5]:
                     if uid != user:
-                        if any:
-                            user_satisfied = False
-                            break
-                        raise error.TestFail(
+                        self.log_error(process, pscmd,
                             'Process %s running as %s; expected %s' %
                             (process, uid, user))
+                        return
 
             # Check if process has super-user privileges
-            if not run_as_root:
+            else:
                 # TODO(yusukes): Uncomment this once issue 2253 is resolved
                 # if int(ps[0]) & 0x04:
                 #    raise error.TestFail(
                 #        'Process %s running with super-user flag' %
                 #        process)
                 if 'root' in ps:
-                    if any:
-                        run_as_root_satisfied = False
-                        continue
-                    raise error.TestFail(
+                    self.log_error(process, pscmd,
                         'Process %s running as root' % process)
+                    return
 
-            # Check if conditions are met for "any" mode.
-            if any and user_satisfied and run_as_root_satisfied:
-                break
-        else:
-            if any:
-                raise error.TestFail(
-                    'Conditions are not met for any process %s' % process)
+
+    def run_once(self):
+        self._failed = []
+        self.check_process('candidate_window', user='chronos', do_login=True)
+        self.check_process('chrome')
+
+        # TODO(mazda): Change the user value to 'messagebus'.
+        # 201 is a uid of messagebus. ps command displays 201 as the
+        # user name for some reasons (bug of the ps command?).
+        self.check_process('dbus-daemon', user='201')
+
+        self.check_process('ibus-daemon', user='chronos', do_login=True)
+        self.check_process('ibus-memconf', user='chronos', do_login=True)
+        self.check_process('X', user='root')
+
+        if len(self._failed) != 0:
+            raise error.TestFail(
+                'Failed processes: %s' % ','.join(self._failed))
