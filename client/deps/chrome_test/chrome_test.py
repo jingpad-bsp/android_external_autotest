ed7f0f3b0cf7f8044cdc803dc87f16624bbbcc2e
Zelidrag Hornung <zelidrag@chromium.org>
2010-07-22 21:36:02 -0700

    Fixed chrome autotests setup script to work with SERVER_SOURCE as well.
    BUG=chromium-os:4828
    TEST=make sure desktopui_BrowserTest works on BTV machines
    
    Review URL: http://codereview.chromium.org/3020025

--- client/deps/chrome_test/chrome_test.py
+++ client/deps/chrome_test/chrome_test.py
@@ -11,7 +11,8 @@ version = 1
 
 def setup(top_dir):
     if 'CHROME_ORIGIN' not in os.environ.keys() or \
-       os.environ['CHROME_ORIGIN'] != 'LOCAL_SOURCE':
+       (os.environ['CHROME_ORIGIN'] != 'LOCAL_SOURCE' and
+        os.environ['CHROME_ORIGIN'] != 'SERVER_SOURCE'):
         logging.info('Skipping Chrome test resource setup for non-local builds')
         return
 
