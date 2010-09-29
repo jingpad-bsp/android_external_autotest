25fc6d1f28e54c46689f12d3b93c2540ef45323a
Eric Li <ericli@chromium.org>
2010-09-28 17:22:51 -0700

    Merge remote branch 'cros/upstream' into tempbranch2
    Merged to trunk@4816.
    
    BUG=
    TEST=we will build a new autotest server instance, and keep cautotest running and then later do a cname switch.
    
    Review URL: http://codereview.chromium.org/3511003
    
    Change-Id: Iee5f52f45f28f84927d6c6f9a74edc370d40288a

--- client/deps/systemtap/systemtap.py
+++ client/deps/systemtap/systemtap.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import os
-import common
 import shutil
 from autotest_lib.client.bin import utils
 
@@ -12,10 +11,10 @@ def setup(topdir):
 
     os.chdir(srcdir)
 
-    utils.system('./configure --with-elfutils=elfutils ' \
-                 '--prefix=%s/systemtap' % topdir)
-    utils.system('make -j %d' % utils.count_cpus())
-    utils.system('make install')
+    utils.configure('--with-elfutils=elfutils ' \
+                    '--prefix=%s/systemtap' % topdir)
+    utils.make('-j %d' % utils.count_cpus())
+    utils.make('install')
 
     os.chdir(topdir)
 
