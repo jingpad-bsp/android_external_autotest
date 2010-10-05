24618cb0796a9341f15bbd2ed0beeaba748251c5
Olof Johansson <olofj@chromium.org>
2010-10-05 13:32:22 -0700

    flashsanitycheck: enable on arm and change grep strings
    Change-Id: I49bf97ce5bf69cd94af8dafb9ad0ce82491be662
    
    BUG=none
    TEST=on seaboard: run once, see it pass. Run another time, "killall -11 chrome" during the 25-second sleep and see it report
    failure.
    
    Review URL: http://codereview.chromium.org/3576012

--- client/site_tests/desktopui_FlashSanityCheck/desktopui_FlashSanityCheck.py
+++ client/site_tests/desktopui_FlashSanityCheck/desktopui_FlashSanityCheck.py
@@ -13,9 +13,6 @@ class desktopui_FlashSanityCheck(test.test):
 
 
     def run_once(self, time_to_wait=25):
-        if utils.get_arch() != 'i386':
-            raise error.TestNAError('Only supported on x86')
-
         # take a snapshot from /var/log/messages.
         msg_linecount = utils.system_output('wc -l /var/log/messages')
         logging.debug(msg_linecount)
@@ -33,10 +30,9 @@ class desktopui_FlashSanityCheck(test.test):
                                       msg_linecount.split()[0])
 
         # any better pattern matching?
-        if re.search(r' chrome\[.* segfault at', new_msg):
+        if re.search(r' Received crash notification for ', new_msg):
             # well, there is a crash. sample crash message:
-            # 2010-04-21T18:17:21.181068+00:00 localhost kernel: [88602.303508]
-            # chrome[2961]: segfault at 6dc08030 ip 00192bcd sp 77d33f14 error 4
-            # in libpthread-2.10.1.so[18b000+14000]
+            # 2010-10-04T19:13:17.923673-07:00 localhost crash_reporter[30712]:
+            # Received crash notification for chrome[29888] sig 11 (ignoring)
             raise error.TestFail('Browser crashed during test.\nMessage '
                                  'from /var/log/messages:\n%s' % new_msg)
