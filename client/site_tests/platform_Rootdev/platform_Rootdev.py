32c6dc1d8afccf04a90955cbfe71bf65dc88e171
Kobi Cohen-Arazi <kobic@codeaurora.org>
2010-10-07 14:24:14 -0600

    Fix platform_Rootdev.py. Enable part 5
    BUG=chromium-os:7133
    
    TEST=Try when partition is 5 for arm and x86
    
    Review URL: http://codereview.chromium.org/3583005
    Patch from Kobi Cohen-Arazi <kobic@codeaurora.org>.

--- client/site_tests/platform_Rootdev/platform_Rootdev.py
+++ client/site_tests/platform_Rootdev/platform_Rootdev.py
@@ -15,45 +15,43 @@ from autotest_lib.client.common_lib import error
 class platform_Rootdev(test.test):
     version = 1
 
+    def test_no_partition(self, inputDev, cpuType):
+
+        # arm should be /dev/mmcblk0,  /dev/mmcblk1 etc 
+        # x86 should be /dev/sda, /dev/sdb
+
+        if (cpuType == "arm"):
+            m = re.match("/dev/mmcblk[0-9]$", inputDev)
+            if not m:
+                raise error.TestFail(
+                    "Rootdev test_no_partition arm failed != /dev/mmcblk[0-9]")
+
+        else:
+            m = re.match("/dev/sd[a-z]$", inputDev)
+            if not m:
+                raise error.TestFail(
+                    "Rootdev test_no_partition x86 failed != /dev/sd[a-z]")
+
+
     def run_once(self):
 
         cpuType = utils.get_cpu_arch()
         logging.debug("cpu type is %s" % cpuType)
 
         # test return values
-        result = utils.system("rootdev")
+        result = utils.system("rootdev -s")
         logging.debug("Rootdev test res: %d", result)
         if (result != 0):
             raise error.TestFail("Rootdev failed")
-        result = utils.system("rootdev -d")
+        result = utils.system("rootdev -s -d")
         logging.debug("Rootdev test -d switch res: %d", result)
         if (result != 0):
-            raise error.TestFail("Rootdev failed -d")
-
-        # test content
-        text = utils.system_output("rootdev 2>&1")
-        text=text.strip()
-        logging.debug("Rootdev txt is *%s*", text)
-        if(cpuType == "arm"):
-            if(text != "/dev/mmcblk0p3" and text != "/dev/mmcblk1p3"):
-                raise error.TestFail(
-                    "Rootdev arm failed != /dev/mmcblk0p3 and != mmcblk1p3")
-        else:
-            if(text != "/dev/sda3" and text != "/dev/sdb3"):
-                raise error.TestFail(
-                    "Rootdev x86 failed != /dev/sda3 and != sdb3")
+            raise error.TestFail("Rootdev failed -s -d")
 
         # test with -d Results should be without the partition device number
-        text = utils.system_output("rootdev -d 2>&1")
+        text = utils.system_output("rootdev -s -d 2>&1")
         text = text.strip()
-        logging.debug("Rootdev -d txt is *%s*", text)
-        if(cpuType == "arm"):
-            if(text != "/dev/mmcblk0" and text != "/dev/mmcblk1"):
-                raise error.TestFail(
-                    "Rootdev arm failed != /dev/mmcblk0 and != mmcblk1")
-        else:
-            if(text != "/dev/sda" and text != "/dev/sdb"):
-                raise error.TestFail(
-                    "Rootdev x86 failed != /dev/sda and != sdb")
+        logging.debug("Rootdev -s -d txt is *%s*", text)
+        self.test_no_partition(text, cpuType)
 
 
