b9f6a2e8629d4b1f3f530d9a6c7e710b91f7ada6
Hung-Te Lin <hungte@chromium.org>
2010-10-25 16:57:55 +0800

    factory_UploadLogs: allow filename customization from more properties, including VPD
    Vendors may perfer to rename the log files according to their serial number,
    which may be derrived from VPD.
    
    BUG=none
    TEST=manually executed on devices with VPD and using filename as
         %(vpd_serial_number)s.log%(gz)s, checked uploading works fine
    
    Change-Id: I426bbf3c6a9b15ba392faa4bddb1ce41ef6a8bcb
    
    Review URL: http://codereview.chromium.org/4014009

--- client/site_tests/factory_UploadLogs/factory_UploadLogs.py
+++ client/site_tests/factory_UploadLogs/factory_UploadLogs.py
@@ -2,6 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# This test provides uploading text-based factory log files to a FTP site.
+# To add extra information into log, please create a new standalone test
+# and output text-safe information via API "factory.log".
+# You can find a good example in test factory_LogVpd.
+
 import ftplib
 import gzip
 import hashlib
@@ -13,8 +18,9 @@ from autotest_lib.client.bin import test, utils
 from autotest_lib.client.bin import factory_error as error
 
 class factory_UploadLogs(test.test):
-    version = 1
+    version = 2
 
+    # Please check suite_Factory/test_list for the more information
     DEFAULT_DESTINATION_PARAM = {
         'method': 'ftp',
         'host': 'localhost',
@@ -23,16 +29,17 @@ class factory_UploadLogs(test.test):
         'passwd': '',
         'timeout': 3*60,
         'initdir': '',
+        'gzip': True,
+        'source_log_path': factory.LOG_PATH,
+        'filename_pattern': '%(hash)s.log%(gz)s',
     }
 
-    USE_GZIP = True
-
-    def prepare_source_object(self, text_source_filename):
-        ''' prepares the source file object from logs '''
+    def prepare_source_object(self, text_source_filename, use_gzip):
+        ''' Prepares the source file object from logs '''
         content = open(text_source_filename, 'r').read()
         hashval = hashlib.sha1(content).hexdigest()
-        if self.USE_GZIP:
-            buf = StringIO.StringIO()
+        buf = StringIO.StringIO()
+        if use_gzip:
             zbuf = gzip.GzipFile(fileobj=buf, mode='w')
             zbuf.write(content)
             zbuf.close()  # force gzip flush to stringIO buffer
@@ -42,7 +49,7 @@ class factory_UploadLogs(test.test):
         return hashval, buf
 
     def do_upload_file(self, dest, fileobj):
-        ''' uploads (via FTP) fileobj to dest '''
+        ''' Uploads (via FTP) fileobj to dest '''
         factory.log('upload destination: %s://%s:%s@%s:%s/%s' %
                     (dest['method'], dest['user'],
                      '*',
@@ -57,17 +64,34 @@ class factory_UploadLogs(test.test):
         ftp.storbinary('STOR %s' % dest['filename'], fileobj)
         ftp.quit()
 
+    def collect_vpd_values(self):
+        ''' Collects VPD properties and return as dictionary '''
+        vpd_key_values = {}
+        vpd_data = utils.system_output(
+                "mosys -l vpd print all", ignore_status=True).strip()
+        # The output of mosys VPD is "key | value" format.
+        for line in vpd_data.split('\n'):
+            if line.find('|') < 0:
+                continue
+            k, v = line.split('|', 1)
+            vpd_key_values['vpd_' + k.strip()] = v.strip()
+        return vpd_key_values
+
     def run_once(self, destination):
         assert 'filename' not in destination, "file names must be generated"
         if destination['host'] == '*':
             factory.log('WARNING: FACTORY LOG UPLOADING IS BYPASSED.')
             return
-        src_hash, src_obj = self.prepare_source_object(factory.LOG_PATH)
         dest = {}
         dest.update(self.DEFAULT_DESTINATION_PARAM)
         dest.update(destination)
-        dest_name = src_hash + '.log'
-        if self.USE_GZIP:
-            dest_name = dest_name + '.gz'
+        src_hash, src_obj = self.prepare_source_object(
+                dest['source_log_path'], dest['gzip'])
+        filename_params = {
+                'gz': '.gz' if dest['gzip'] else '',
+                'hash': src_hash, }
+        if dest['filename_pattern'].find('%(vpd_') >= 0:
+            filename_params.update(self.collect_vpd_values())
+        dest_name = dest['filename_pattern'] % filename_params
         dest['filename'] = os.path.join(dest['initdir'], dest_name)
         self.do_upload_file(dest, src_obj)
