981117a1c92160b8cbf4341913194392813d8948
Paul Stewart <pstew@chromium.org>
2010-10-25 13:36:16 -0700

    Monitor errno for EBUSY on write to waekalarm
    Some bug in later builds/firmware causes the wakealarm to still be set
    after we wake up from sleep using RTC alarm.  Work around this issue by
    monitoring the result of our write to the wakealarm, and attempt to
    clear if we get EBUSY.
    
    I submitted a bug to snanda for either adding a workaround to
    powerd_suspend, or perhaps running down the root cause.
    
    BUG=none
    TEST=Rerun WiFiRoaming test suite, which was hanging last week
    
    Review URL: http://codereview.chromium.org/4095001

--- client/common_lib/rtc.py
+++ client/common_lib/rtc.py
@@ -1,3 +1,5 @@
+import errno
+
 def get_seconds(utc=True):
     """
     Read the current time out of the RTC
@@ -5,10 +7,23 @@ def get_seconds(utc=True):
     return int(file('/sys/class/rtc/rtc0/since_epoch').readline())
 
 
+def write_wake_alarm(alarm_time):
+    """
+    Write a value to the wake alarm
+    """
+    f = file('/sys/class/rtc/rtc0/wakealarm', 'w')
+    f.write('%s\n' % str(alarm_time))
+    f.close()
+
 def set_wake_alarm(alarm_time):
     """
     Set the hardware RTC-based wake alarm to 'alarm_time'.
     """
-    file('/sys/class/rtc/rtc0/wakealarm', 'w').write("%s\n" % str(alarm_time))
-
-
+    try:
+        write_wake_alarm(alarm_time)
+    except IOError as (errnum, strerror):
+        if errnum != errno.EBUSY:
+            raise
+        write_wake_alarm('clear')
+        write_wake_alarm(alarm_time)
+        
