9f680480d55656443450806eb00155ab460ff8d2
Tracy Turchetto <tturchetto@chromium.org>
2010-10-27 15:07:28 -0700

    add security_rootfsOwners test to the suite
    Change-Id: I03e4c55463ad92726e0a0d3d4405839e9b42e068
    
    Adding a security_rootfsOwners test.
    
    Change-Id: I2b4441a99e2f046632ca5fa5747289a99ec6779d
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/4103008

---
+++ client/site_tests/security_RootfsOwners/security_RootfsOwners.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
+
+class security_RootfsOwners(test.test):
+    version = 1
+
+    def run_once(self):
+        """
+        This is a regression test for 7989.
+        Do a find on the system for rootfs files own by chronos.
+        Test fails if there is any.
+        """
+        cmd = 'find / -xdev -user chronos -print'
+        cmd_output = utils.system_output(cmd, ignore_status=True)
+        
+        if (cmd_output != '') :
+            logging.error(cmd_output)
+            raise error.TestFail('Rootfs should not contain any files owned by chronos')
+
