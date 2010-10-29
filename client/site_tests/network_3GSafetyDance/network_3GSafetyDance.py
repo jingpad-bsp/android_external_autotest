0645b5336d97a47722ae5d1350ee38cfdc16a992
Elly Jones <ellyjones@google.com>
2010-10-29 11:59:12 -0400

    autotest-tests: Add network_3GSafetyDance.
    This test performs connect, disconnect, enable and disable operations in random
    sequence, and requires that flimflam not crash or time out.
    
    BUG=chromium-os:7804
    TEST=Sure is
    
    Change-Id: If1712349e6c999e6b47a3c431e6ebbc636d598d4
    Signed-off-by: Elly Jones <ellyjones@chromium.org>
    Signed-off-by: Jason Glasgow <jglasgow@chromium.org>
    Reviewed-by: Heigan the Unclean
    
    Review URL: http://codereview.chromium.org/4205003

---
+++ client/site_tests/network_3GSafetyDance/network_3GSafetyDance.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.client.bin import site_backchannel, test, utils
+from autotest_lib.client.common_lib import error
+
+import logging, os, re, socket, string, sys, time, urllib2
+import dbus, dbus.mainloop.glib, gobject
+import random
+
+sys.path.append(os.environ.get("SYSROOT", "") + "/usr/local/lib/flimflam/test")
+import flimflam
+
+class network_3GSafetyDance(test.test):
+    version = 1
+
+    def filterexns(self, fn):
+        v = None
+        try:
+            v = fn()
+        except dbus.exceptions.DBusException, error:
+            if error._dbus_error_name in self.okerrors:
+                return v
+            else:
+                raise error
+        return v
+
+    def enable(self):
+        print 'Enable'
+        self.filterexns(lambda:
+            self.flim.EnableTechnology('cellular'))
+
+    def disable(self):
+        print 'Disable'
+        self.filterexns(lambda:
+            self.flim.DisableTechnology('cellular'))
+
+    def ignoring(self, status):
+        if 'AlreadyConnected' in status['reason']:
+            return True
+        if 'InProgress' in status['reason']:
+            return True
+        return False
+
+    def connect(self):
+        print 'Connect'
+        (success, status) = self.filterexns(lambda: 
+            self.flim.ConnectService(service = self.service,
+                                     config_timeout = 120))
+        if not success and not self.ignoring(status):
+            raise error.TestFail('Could not connect: %s' % status)
+
+    def disconnect(self):
+        print 'Disconnect'
+        (success, status) = self.filterexns(lambda:
+            self.flim.DisconnectService(service = self.service,
+                                        wait_timeout = 60))
+        if not success:
+            raise error.TestFail('Could not disconnect: %s' % status)
+
+    def op(self):
+        n = random.randint(0, len(self.ops) - 1)
+        self.ops[n]()
+        time.sleep(random.randint(0, 20) / 10.0)
+
+    def run_once(self, name='usb', ops=500, seed=None):
+        if not seed:
+            seed = int(time.time())
+        self.okerrors = [
+            'org.chromium.flimflam.Error.InProgress',
+            'org.chromium.flimflam.Error.AlreadyConnected',
+            'org.chromium.flimflam.Error.AlreadyEnabled'
+        ]
+        self.ops = [ self.enable,
+                     self.disable,
+                     self.connect,
+                     self.disconnect ]
+        self.flim = flimflam.FlimFlam()
+        self.manager = flimflam.DeviceManager(self.flim)
+        self.service = self.flim.FindElementByPropertySubstring('Service',
+                                                                'Type',
+                                                                'cellular')
+        self.device = self.flim.FindElementByNameSubstring('Device', name)
+        if not self.device:
+            self.device = self.flim.FindElementByPropertySubstring('Device',
+                                                                   'Interface',
+                                                                   name)
+        logging.info('Seed: %d' % seed)
+        random.seed(seed)
+        for _ in xrange(ops):
+            self.op()
