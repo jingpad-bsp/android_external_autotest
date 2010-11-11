5b21584556f7abd1b414369db40dd108698ccb31
Daniel Kurtz <djkurtz@chromium.org>
2010-11-11 14:53:37 -0800

    desktopui_FlashSanityCheck: use site_log_reader
    Unify /var/log/message access across suite_Smoke tests.
    
    BUG=chromium-os:8281
    TEST=Regression test: Run desktopui_FlashSanityCheck, should pass with identical results as before.
    
    Review URL: http://codereview.chromium.org/4808002
    
    Change-Id: I3c4b32115d77a79e5463a4b9bbdf697418a84980

--- client/site_tests/desktopui_FlashSanityCheck/desktopui_FlashSanityCheck.py
+++ client/site_tests/desktopui_FlashSanityCheck/desktopui_FlashSanityCheck.py
@@ -4,9 +4,8 @@
 
 
 import logging, re, time
-from autotest_lib.client.bin import test, utils
+from autotest_lib.client.bin import test, utils, site_log_reader
 from autotest_lib.client.common_lib import error, site_ui
-
 
 class desktopui_FlashSanityCheck(test.test):
     version = 1
@@ -14,23 +13,18 @@ class desktopui_FlashSanityCheck(test.test):
 
     def run_once(self, time_to_wait=25):
         # take a snapshot from /var/log/messages.
-        msg_linecount = utils.system_output('wc -l /var/log/messages')
-        logging.debug(msg_linecount)
+        self._log_reader = site_log_reader.LogReader()
+        self._log_reader.set_start_by_current()
 
         # open browser to youtube.com.
         session = site_ui.ChromeSession('http://www.youtube.com')
         # wait some time till the webpage got fully loaded.
         time.sleep(time_to_wait)
         session.close()
-        # Question: do we need to test with other popular flash website?
-
-        # take another snapshot from /var/log/message.
-        # there should be no messages.
-        new_msg = utils.system_output('tail -n +%s /var/log/messages' %
-                                      msg_linecount.split()[0])
+        # Question: do we need to test with other popular flash websites?
 
         # any better pattern matching?
-        if re.search(r' Received crash notification for ', new_msg):
+        if self._log_reader.can_find(' Received crash notification for '):
             # well, there is a crash. sample crash message:
             # 2010-10-04T19:13:17.923673-07:00 localhost crash_reporter[30712]:
             # Received crash notification for chrome[29888] sig 11 (ignoring)
