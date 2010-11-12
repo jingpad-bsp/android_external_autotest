31b78a23537d0784273d9d65290cb19417e5933d
Elly Jones <ellyjones@google.com>
2010-11-12 15:31:50 -0500

    autotest: add network_3GDormancyDance.
    This test steps the modem through a continuous enable/connect/disconnect/disable
    cycle, waiting for the modem to become dormant before disconnecting.
    
    This test is (so far) responsible for the discovery of chromium-os:8879 and
    chromium-os:8977.
    
    BUG=chromium-os:8481
    TEST=Yes
    
    Change-Id: I513514cf574263d975f3e2e11f5fd435af49a43b
    X-Where: locus chromeos-dogfood network_3GDormancyDance
    Signed-off-by: Elly Jones <ellyjones@chromium.org>
    Signed-off-by: Jason Glasgow <jglasgow@chromium.org>
    
    Review URL: http://codereview.chromium.org/4745003

---
+++ client/site_tests/network_3GDormancyDance/network_3GDormancyDance.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.client.bin import site_backchannel, test, utils
+from autotest_lib.client.common_lib import error
+
+import os, sys
+import dbus, dbus.mainloop.glib, gobject
+import glib
+
+sys.path.append(os.environ.get("SYSROOT", "") + "/usr/local/lib/flimflam/test")
+import flimflam
+
+class network_3GDormancyDance(test.test):
+    version = 1
+
+    def countdown(self):
+        self.opsleft -= 1
+        if self.opsleft == 0:
+            self.mainloop.quit()
+
+    def RequestDormancyEvents(self):
+        c = self.bus.get_object('org.chromium.ModemManager',
+                                '/org/chromium/ModemManager/Gobi/0')
+        c.RequestEvents('+dormancy',
+                        dbus_interface='org.chromium.ModemManager.Modem.Gobi')
+
+    def enable(self):
+        print 'Enable'
+        self.countdown()
+        self.flim.EnableTechnology('cellular')
+
+    def disable(self):
+        print 'Disable'
+        self.countdown()
+        self.flim.DisableTechnology('cellular')
+
+    def connect(self):
+        print 'Connect'
+        self.countdown()
+        self.flim.ConnectService(service=self.service, config_timeout=120)
+
+    def disconnect(self):
+        print 'Disconnect'
+        self.countdown()
+        self.flim.DisconnectService(service=self.service, wait_timeout=60)
+
+    def PropertyChanged(self, *args, **kwargs):
+        print 'PropertyChanged: %s %s' % (args, kwargs)
+        if args[0] == 'Powered':
+            if not args[1]:
+                self.enable()
+            else:
+                self.connect()
+        if args[0] == 'Connected':
+            if not args[1]:
+                self.disable()
+
+
+    def DormancyStatus(self, *args, **kwargs):
+        print 'DormancyStatus: %s %s' % (args, kwargs)
+        if args[0]:
+            self.disconnect()
+
+    def begin(self):
+        print 'Setup...'
+        self.disable()
+
+    def run_once(self, name='usb', ops=5000, seed=None):
+        self.opsleft = ops
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        self.bus = dbus.SystemBus()
+        self.RequestDormancyEvents()
+        self.flim = flimflam.FlimFlam()
+        self.manager = flimflam.DeviceManager(self.flim)
+        self.service = self.flim.FindElementByPropertySubstring('Service',
+                                                                'Type',
+                                                                'cellular')
+        self.device = self.flim.FindElementByNameSubstring('Device', name)
+        if not self.device:
+            self.device = self.flim.FindElementByPropertySubstring('Device',
+                                                                   'Interface',
+                                                                   name)
+        self.bus.add_signal_receiver(self.PropertyChanged,
+                                     signal_name='PropertyChanged')
+        self.bus.add_signal_receiver(self.DormancyStatus,
+                                     signal_name='DormancyStatus')
+        self.mainloop = gobject.MainLoop()
+        glib.idle_add(self.begin)
+        self.mainloop.run()
