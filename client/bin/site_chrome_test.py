c18e883438c6c73d13a7ffbedb75ad881d06228a
Chris Masone <cmasone@chromium.org>
2010-11-15 12:57:15 -0800

    [autotest] Change ChromeTestBase to work with pre-installed chrome test binaries.
    Instead of pulling chrome test binaries as a dep (which takes too long on VMs),
    we'll be pre-installing them on test images.  Update the ChromeTestBase class
    to expect this new behavior.
    
    BUG=8544
    TEST=Build a test image with chrome > -r35, run desktopui_BrowserTest
    
    Change-Id: I013b689b4ad7fe5e47760312da573a9a2b29d669
    
    Review URL: http://codereview.chromium.org/4557002

--- client/bin/site_chrome_test.py
+++ client/bin/site_chrome_test.py
@@ -2,9 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, shutil, tempfile, utils
+import logging, os, re, shutil, subprocess, tempfile, utils
 from autotest_lib.client.bin import test
-from autotest_lib.client.common_lib import error, site_ui
+from autotest_lib.client.common_lib import error, global_config, site_ui
 
 class ChromeTestBase(test.test):
     home_dir = None
@@ -16,30 +16,75 @@ class ChromeTestBase(test.test):
             os.mkdir(self.srcdir)
 
 
-    def run_chrome_test(self, test_to_run, extra_params = ''):
+    def initialize(self):
+        self.home_dir = tempfile.mkdtemp()
         dep = 'chrome_test'
         dep_dir = os.path.join(self.autodir, 'deps', dep)
+        repo = global_config.global_config.get_config_value(
+            'PACKAGES', 'fetch_location',
+            type=str, default='/usr/local/autotest-pkgs')
+
+        self.job.add_repository([repo])
         self.job.install_pkg(dep, 'dep', dep_dir)
-
-        chrome_dir = '/opt/google/chrome'
-        cr_source_dir = '%s/test_src' % dep_dir
-        test_binary_dir = '%s/test_src/out/Release' % dep_dir
-        self.home_dir = tempfile.mkdtemp()
-
+        self.cr_source_dir = '%s/test_src' % dep_dir
+        self.test_binary_dir = '%s/out/Release' % self.cr_source_dir
         try:
-            setup_cmd = '%s/%s' % (test_binary_dir,
-                                  'setup_test_links.sh')
-            utils.system(setup_cmd)
+            setup_cmd = '/bin/sh %s/%s' % (self.test_binary_dir,
+                                           'setup_test_links.sh')
+            utils.system(setup_cmd)  # this might raise an exception
+        except error.CmdError, e:
+            raise error.TestError(e)
 
-            cmd = '%s/%s %s' % (test_binary_dir, test_to_run, extra_params)
+
+    def filter_bad_tests(self, tests):
+        matcher = re.compile(".+\.(FLAKY|FAILS|DISABLED).+")
+        return filter(lambda(x): not matcher.match(x), tests)
+
+
+    def list_chrome_tests(self, test_binary):
+        all_tests = []
+        try:
+            cmd = '%s/%s --gtest_list_tests' % (self.test_binary_dir,
+                                                test_binary)
             cmd = 'HOME=%s CR_SOURCE_ROOT=%s %s' % (self.home_dir,
-                                                    cr_source_dir,
+                                                    self.cr_source_dir,
                                                     site_ui.xcommand(cmd))
-            logging.info("Running %s" % cmd)
+            logging.debug("Running %s" % cmd)
+            test_proc = subprocess.Popen(cmd,
+                                         shell=True,
+                                         stdout=subprocess.PIPE)
+            last_suite = None
+            skipper = re.compile('YOU HAVE')
+            for line in test_proc.stdout:
+                stripped = line.lstrip()
+                if stripped == '' or skipper.match(stripped):
+                    continue
+                elif (stripped == line):
+                    last_suite = stripped.rstrip()
+                else:
+                  all_tests.append(last_suite+stripped.rstrip())
+        except OSError, e:
+            logging.debug(e)
+            raise error.TestFail('Failed to list tests in %s!' % test_binary)
+        return all_tests
+
+
+    def run_chrome_test(self, test_to_run, extra_params=''):
+        try:
+            cmd = '%s/%s %s' % (self.test_binary_dir, test_to_run, extra_params)
+            cmd = 'HOME=%s CR_SOURCE_ROOT=%s %s' % (self.home_dir,
+                                                    self.cr_source_dir,
+                                                    site_ui.xcommand(cmd))
             utils.system(cmd)
         except error.CmdError, e:
             logging.debug(e)
             raise error.TestFail('%s failed!' % test_to_run)
+
+
+    def generate_test_list(self, binary, group, total_groups):
+        all_tests = self.list_chrome_tests(self.binary_to_run)
+        group_size = len(all_tests)/total_groups + 1  # to be safe
+        return all_tests[group*group_size:group*group_size+group_size]
 
 
     def cleanup(self):
