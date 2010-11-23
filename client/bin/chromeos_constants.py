4446e6da3dd9be90f9d35d0370ef66f16a984ec5
Ken Mixter <kmixter@google.com>
2010-11-23 13:29:16 -0800

    autotest: Test RestartEntd dbus call
    Change-Id: I0d921661416e22d65781b98b19950dbe2e6861a7
    
    BUG=9282
    TEST=login_DBusCalls and smoke suite
    
    Review URL: http://codereview.chromium.org/5216005

--- client/bin/chromeos_constants.py
+++ client/bin/chromeos_constants.py
@@ -4,12 +4,27 @@
 
 # The names of expected mount-points, devices, magic files, etc on chrome os.
 
-CHROME_LOG_DIR = '/var/log/chrome'
+# Constants used by other constants.
 USER_DATA_DIR = '/home/chronos'
+WHITELIST_DIR = '/var/lib/whitelist'
 
-LOGIN_PROFILE = USER_DATA_DIR+'/Default'
+
+# Rest of constants.
+BROWSER = 'chrome'
+
+CHROME_LOG_DIR = '/var/log/chrome'
+CHROME_WINDOW_MAPPED_MAGIC_FILE = \
+    '/var/run/state/windowmanager/initial-chrome-window-mapped'
 
 CLEANUP_LOGS_PAUSED_FILE = '/var/lib/cleanup_logs_paused'
+
+CLIENT_LOGIN_URL = '/accounts/ClientLogin'
+
+CREDENTIALS = {
+    '$default': ['performance.test.account@gmail.com', 'perfsmurf'],
+    '$backdoor': ['chronos@gmail.com', 'chronos'],
+}
+
 # TODO(fes): With the switch to ecryptfs, the cryptohome device is no longer
 # static--it includes a system-specific hash of the username whose vault is
 # mounted.  seano points out that this is no longer a constant, and we may want
@@ -20,35 +35,26 @@ CRYPTOHOME_MOUNT_PT = USER_DATA_DIR+'/user'
 
 CRYPTOHOMED_LOG = '/var/log/cryptohomed.log'
 
-LOGIN_TRUST_ROOTS = '/etc/login_trust_root.pem'
-
-BROWSER = 'chrome'
-SESSION_MANAGER = 'session_manager'
-WINDOW_MANAGER = 'chromeos-wm'
+DISABLE_BROWSER_RESTART_MAGIC_FILE = '/tmp/disable_chrome_restart'
 
 LOGGED_IN_MAGIC_FILE = '/var/run/state/logged-in'
 
-CHROME_WINDOW_MAPPED_MAGIC_FILE = \
-    '/var/run/state/windowmanager/initial-chrome-window-mapped'
-
-DISABLE_BROWSER_RESTART_MAGIC_FILE = '/tmp/disable_chrome_restart'
-
-LOGIN_PROMPT_READY_MAGIC_FILE = '/tmp/uptime-login-prompt-ready'
-
-CREDENTIALS = {
-    '$default': ['performance.test.account@gmail.com', 'perfsmurf'],
-    '$backdoor': ['chronos@gmail.com', 'chronos'],
-}
-
-WHITELIST_DIR = '/var/lib/whitelist'
-OWNER_KEY_FILE = WHITELIST_DIR+'/owner.key'
-SIGNED_PREFERENCES_FILE = WHITELIST_DIR+'/preferences'
-
-CLIENT_LOGIN_URL = '/accounts/ClientLogin'
-ISSUE_AUTH_TOKEN_URL = '/accounts/IssueAuthToken'
-TOKEN_AUTH_URL = '/accounts/TokenAuth'
-
+LOGIN_PROFILE = USER_DATA_DIR+'/Default'
 LOGIN_SERVICE = 'gaia'
 LOGIN_ERROR = 'Error=BadAuthentication'
+LOGIN_PROMPT_READY_MAGIC_FILE = '/tmp/uptime-login-prompt-ready'
+LOGIN_TRUST_ROOTS = '/etc/login_trust_root.pem'
 
+ISSUE_AUTH_TOKEN_URL = '/accounts/IssueAuthToken'
+
+OWNER_KEY_FILE = WHITELIST_DIR+'/owner.key'
+
+SESSION_MANAGER = 'session_manager'
+SIGNED_PREFERENCES_FILE = WHITELIST_DIR+'/preferences'
+
+TOKEN_AUTH_URL = '/accounts/TokenAuth'
+
+UI_LOG = '/var/log/ui/ui.LATEST'
 UPDATE_ENGINE_LOG = '/var/log/update_engine.log'
+
+WINDOW_MANAGER = 'chromeos-wm'
