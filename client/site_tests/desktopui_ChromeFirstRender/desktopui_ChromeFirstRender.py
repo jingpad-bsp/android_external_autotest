ce01ff4d43b8e7417c9976b39ca1a7a5e380c6a0
Sean O'Connor <seano@chromium.org>
2010-12-07 17:13:13 -0800

    Make desktopui_ChromeFirstRender tolerant of logfile race condition.
    Change-Id: I36944a5ac6022fa651c3af3a3bb8b40797229e44
    
    BUG=chromeos:8055
    TEST=Executed manually against a dev L13
    
    Review URL: http://codereview.chromium.org/5655002

--- client/site_tests/desktopui_ChromeFirstRender/desktopui_ChromeFirstRender.py
+++ client/site_tests/desktopui_ChromeFirstRender/desktopui_ChromeFirstRender.py
@@ -19,14 +19,20 @@ class desktopui_ChromeFirstRender(site_ui_test.UITest):
         return float(time)
 
 
+    def __check_logfile(self, target_file):
+        # The data log file is written with one write(), this should be safe.
+        return lambda: (os.access(target_file, os.F_OK)
+                        and os.path.getsize(target_file))
+
+
     def run_once(self):
         try:
             site_utils.poll_for_condition(
-                lambda: os.access(self._LOGIN_SUCCESS_FILE, os.F_OK),
-                site_login.TimeoutError('Timed out waiting for initial login'))
+                self.__check_logfile(self._LOGIN_SUCCESS_FILE),
+                site_login.TimeoutError('Timeout waiting for initial login'))
             site_utils.poll_for_condition(
-                lambda: os.access(self._FIRST_RENDER_FILE, os.F_OK),
-                site_login.TimeoutError('Timed out waiting for initial render'),
+                self.__check_logfile(self._FIRST_RENDER_FILE),
+                site_login.TimeoutError('Timeout waiting for initial render'),
                 timeout=60)
 
             start_time = self.__parse_uptime(self._LOGIN_SUCCESS_FILE)
