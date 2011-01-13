9fab90d36c39f8d435accf4e15eb29c8279c24cb
Chris Masone <cmasone@chromium.org>
2011-01-12 18:15:12 -0800

    [autotest] Re-Add some tests to suite_Smoke
    We have enabled tests to log in via Guest mode, so login_CryptohomeIncognitoMounted can be run again.  Also, we have added some new account name canonicalization logic that needs to be tested.
    
    BUG=10837
    TEST=suite_Smoke
    
    Change-Id: Ic692df01c0792c4201929f74cb90bf310ba4f62e
    
    Review URL: http://codereview.chromium.org/6234002

--- client/site_tests/login_CryptohomeIncognitoMounted/login_CryptohomeIncognitoMounted.py
+++ client/site_tests/login_CryptohomeIncognitoMounted/login_CryptohomeIncognitoMounted.py
@@ -10,20 +10,6 @@ class login_CryptohomeIncognitoMounted(cros_ui_test.UITest):
     version = 1
 
 
-    def __login_denier(self, handler, url_args):
-        handler.send_response(403)
-        handler.end_headers()
-        handler.wfile.write('Error=BadAuthentication.')
-
-
-    def start_authserver(self):
-        self._authServer = auth_server.GoogleAuthServer(
-            cl_responder=self.__login_denier)
-        self._authServer.run()
-
-        self.use_local_dns()
-
-
     def run_once(self):
         if (cryptohome.is_mounted(allow_fail=True) or
             not cryptohome.is_mounted_on_tmpfs(
