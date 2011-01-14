0418d6de981da05b309955ffa9d72c9909d66f44
Chris Masone <cmasone@chromium.org>
2011-01-14 14:26:01 -0800

    [autotest] Test ensuring only the first login gets ownership
    BUG=6086
    TEST=Run this test
    
    Change-Id: I88453bf609dfa65d02136a9193cceb074000c8f2
    
    [autotest] Add an autotest to ensure that ownership can be taken
    
    BUG=6085
    TEST=this is the test!
    
    Change-Id: I0382a279b158fe669951fd28b705d2ab7ce5074d
    
    Review URL: http://codereview.chromium.org/6340003

---
+++ client/site_tests/login_OwnershipNotRetaken/login_OwnershipNotRetaken.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import hashlib, logging, os, time, utils
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.cros import constants, cros_logging, cros_ui
+from autotest_lib.client.cros import cros_ui_test, cryptohome, login
+
+
+class login_OwnershipNotRetaken(cros_ui_test.UITest):
+    version = 1
+
+    _TEST_USER = 'example@chromium.org'
+    _TEST_PASS = 'testme'
+
+    def initialize(self, creds='$default'):
+        super(login_OwnershipNotRetaken, self).initialize(creds)
+
+
+    def run_once(self):
+        login.wait_for_ownership()
+        login.attempt_logout()
+        key = open(constants.OWNER_KEY_FILE, 'rb')
+        hash = hashlib.md5(key.read())
+        key.close()
+        mtime = os.stat(constants.OWNER_KEY_FILE).st_mtime
+        login.refresh_login_screen()
+        login.attempt_login(self._TEST_USER, self._TEST_PASS)
+        try:
+            utils.poll_for_condition(
+                lambda: os.stat(constants.OWNER_KEY_FILE).st_mtime > mtime,
+                login.TimeoutError(''),
+                20)
+            # If we DIDN'T time out...badness!
+            raise error.TestFail("Owner key was touched on second login!")
+        except login.TimeoutError, e:
+            pass
+
+        # Sanity check.
+        key2 = open(constants.OWNER_KEY_FILE, 'rb')
+        hash2 = hashlib.md5(key2.read())
+        key2.close()
+        if hash.hexdigest() != hash2.hexdigest():
+            raise error.TestFail("Owner key was touched on second login!")
+
+
+    def cleanup(self):
+        super(login_OwnershipNotRetaken, self).cleanup()
+        cryptohome.remove_vault(self._TEST_USER)
