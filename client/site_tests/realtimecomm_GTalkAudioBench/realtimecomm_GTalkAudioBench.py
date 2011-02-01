a8cd16014af2bdbfc65ac8bb2ac5401a76eb3a5c
Dale Curtis <dalecurtis@chromium.org>
2011-02-01 11:53:59 -0800

    Cleanup realtimecomm tests for new ebuild.
    With the new ebuild to bring tree built binaries in, we no longer need the checked in binaries. I've also cleaned the tests up and made them a bit more maintainable. Notably:
    
    1. Gtalkunittest no longer checks test counts. Relies on failed message or non-zero exit code to detect failed state.
    2. ARM/i686 binaries will target the same executable so tests should run on either platform without needing any if/else statements.
    
    Some of the cleanup involved removing unittests which no longer exist and adding new ones.
    
    Change-Id: I6eef36afa839cd3f72da70b310169d3c0b0f04e9
    
    BUG=chromium-os:11542
    TEST=Ran w/ run_remote tests. Not all tests are currently passing.
    
    Review URL: http://codereview.chromium.org/6329029

--- client/site_tests/realtimecomm_GTalkAudioBench/realtimecomm_GTalkAudioBench.py
+++ client/site_tests/realtimecomm_GTalkAudioBench/realtimecomm_GTalkAudioBench.py
@@ -4,6 +4,7 @@
 
 import os, shutil
 from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
 
 class realtimecomm_GTalkAudioBench(test.test):
     version = 1
@@ -26,38 +27,41 @@ class realtimecomm_GTalkAudioBench(test.test):
     ]
 
     def setup(self):
-        # TODO(zhurunz): Support ARM once available.
         self.gips_path = os.path.join(self.autodir, 'gips')
-        self.gips = os.path.join(self.gips_path, 'audiotesttool.i686')
+        self.gips = os.path.join(self.gips_path, 'gipstool')
 
 
     def run_once(self):
-        # Setup as appropriate
+        # Setup as appropriate.
         shutil.rmtree(self.gips_path, ignore_errors=True)
         shutil.copytree(self.bindir, self.gips_path)
         utils.run('chown chronos %s -R' % self.gips_path)
 
-        # Run all codecs
+        if not os.path.exists(self.gips):
+            raise error.TestFail('Missing gipstool binary. Make sure gtalk has '
+                                 'been emerged.')
+
+        # Run all codecs.
         for codec in self.codecs:
             self.__run_one_codec(codec.lower())
 
-        # Report perf
+        # Report perf.
         self.write_perf_keyval(self.performance_results)
 
 
     def __run_one_codec(self, codec):
-        # Encode
-        para = "--codec=%s encode source.wav output.rtp" % codec
-        cmd = "cd %s && su chronos -c '%s %s'" % \
-            (self.gips_path, self.gips, para)
+        # Encode.
+        para = '--codec=%s encode source.wav output.rtp' % codec
+        cmd = "cd %s && su chronos -c '%s %s'" %  (self.gips_path, self.gips,
+                                                   para)
         cpu_usage, stdout = utils.get_cpu_percentage(
             utils.system_output, cmd, retain_output=True)
         self.performance_results['utime_gtalk_%s_enc' % codec] = cpu_usage
 
-        # Decode
-        para = "--codec=%s decode output.rtp output.wav" % codec
-        cmd = "cd %s && su chronos -c '%s %s'" % \
-            (self.gips_path, self.gips, para)
+        # Decode.
+        para = '--codec=%s decode output.rtp output.wav' % codec
+        cmd = "cd %s && su chronos -c '%s %s'" % (self.gips_path, self.gips,
+                                                  para)
         cpu_usage, stdout = utils.get_cpu_percentage(
             utils.system_output, cmd, retain_output=True)
         self.performance_results['utime_gtalk_%s_dec' % codec] = cpu_usage
--- client/site_tests/realtimecomm_GTalkunittest/realtimecomm_GTalkunittest.py
+++ client/site_tests/realtimecomm_GTalkunittest/realtimecomm_GTalkunittest.py
@@ -9,50 +9,50 @@ from autotest_lib.client.common_lib import error
 
 class realtimecomm_GTalkunittest(test.test):
     version = 1
-    # { test_exe : number_of_testcases }
-    unittests = {
-        'base_unittest'         : '155',
-        'call_unittest'         : '2'  ,
-        'flute_unittest'        : '146',
-        'flutetesting_unittest' : '3'  ,
-        'media_unittest'        : '181',
-        'p2p_unittest'          : '228',
-        'plugin_unittest'       : '10' ,
-        'xmllite_unittest'      : '49' ,
-        'xmpp_unittest'         : '37' ,
-    }
+    unittests = [
+        'base_unittest',
+        'browserplugin_unittest',
+        'call_unittest',
+        'flash_unittest',
+        'flute_unittest',
+        'media_unittest',
+        'p2p_unittest',
+        'sound_unittest',
+        'tunnel_unittest',
+        'xmllite_unittest',
+        'xmpp_unittest',
+    ]
 
     def run_once(self):
-        # Stop Google Talk Plugin
+        # Stop Google Talk Plugin.
         utils.run('pkill GoogleTalkPlugin', ignore_status=True)
 
-        # Setup as appropriate
+        # Setup as appropriate.
         talk_path = os.path.join(self.autodir, 'talk')
         shutil.rmtree(talk_path, ignore_errors=True)
         shutil.copytree(os.path.join(self.bindir, 'talk'), talk_path)
         utils.run('chown chronos %s -R' % talk_path)
-        utils.run('chown chronos \
-            /tmp/.google-talk-plugin-theuser.lock.testlock', ignore_status=True)
+        utils.run(
+            'chown chronos /tmp/.google-talk-plugin-theuser.lock.testlock',
+            ignore_status=True)
 
-        # Run all unittests
+        # Run all unittests.
         for test_exe in self.unittests:
-            # TODO(zhurunz): Support ARM once available.
-            x86_talk_path = os.path.join(talk_path, 'i686')
-            # The unittest has to be run in 'talk' folder
-            test_cmd = "cd %s && su chronos -c \'./%s\'" % \
-                (x86_talk_path, test_exe)
-            self.__run_one_test(test_cmd, self.unittests[test_exe])
+            if not os.path.exists(os.path.join(talk_path, test_exe)):
+                raise error.TestFail('Missing test binary %s. Make sure gtalk '
+                                     'has been emerged.' % test_exe)
+            # The unittest has to be run in 'talk' folder.
+            test_cmd = "cd %s && su chronos -c \'./%s\'" %  (talk_path,
+                                                             test_exe)
+            self.__run_one_test(test_cmd)
 
-        # Clean up
+        # Clean up.
         shutil.rmtree(talk_path)
 
 
-    def __run_one_test(self, test_cmd, number_of_testcases):
-        logging.info("Running %s" % test_cmd)
-        result = utils.run(test_cmd, ignore_status=True)
-        if "[  FAILED  ]" in result.stdout:
+    def __run_one_test(self, test_cmd):
+        logging.info('Running %s' % test_cmd)
+        result = utils.run(test_cmd)
+        if '[  FAILED  ]' in result.stdout:
             raise error.TestFail(result.stdout)
-        expected = "[  PASSED  ] %s tests." % number_of_testcases
-        if not expected in result.stdout:
-            raise error.TestFail(result.stdout)
-        logging.info("%s passed." % test_cmd)
+        logging.info('%s passed.' % test_cmd)
