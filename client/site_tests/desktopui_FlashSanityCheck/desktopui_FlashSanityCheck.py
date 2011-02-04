a67d46353a6c0ec2d70c39d9f2a79ac683d5404e
Chris Sosa <sosa@chromium.org>
2011-02-04 15:00:13 -0800

    Convert FlashSanityCheck to login and use the same chrome instance.
    Change-Id: Id559167cea902e3c9d86559f1eaa8c713dd82fcb
    
    BUG=chromium-os:11656
    TEST=Ran it 3x....saw it happen.  Also ran suite_Smoke 2x with no failures
    
    Review URL: http://codereview.chromium.org/6410097

--- client/site_tests/desktopui_FlashSanityCheck/desktopui_FlashSanityCheck.py
+++ client/site_tests/desktopui_FlashSanityCheck/desktopui_FlashSanityCheck.py
@@ -1,30 +1,33 @@
-# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-
-import logging, re, time
-from autotest_lib.client.bin import test, utils
+import os, time
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros import cros_logging, cros_ui
+from autotest_lib.client.cros import constants, cros_logging, cros_ui
+from autotest_lib.client.cros import cros_ui_test, login
 
-class desktopui_FlashSanityCheck(test.test):
-    version = 1
+
+class desktopui_FlashSanityCheck(cros_ui_test.UITest):
+    version = 2
 
 
     def run_once(self, time_to_wait=25):
-        # take a snapshot from /var/log/messages.
         self._log_reader = cros_logging.LogReader()
         self._log_reader.set_start_by_current()
 
-        # open browser to youtube.com.
-        session = cros_ui.ChromeSession('http://www.youtube.com')
-        # wait some time till the webpage got fully loaded.
+        # Make sure that we don't have the initial browser window popping up in
+        # the middle of the test.
+        login.wait_for_initial_chrome_window()
+        # TODO(sosa): Check in flash video that is a solid test.
+        session = cros_ui.ChromeSession(
+            '--user-data-dir=%s %s' % (constants.CRYPTOHOME_MOUNT_PT,
+                                       'http://www.youtube.com'),
+            clean_state=False, suid=True)
+        # Wait some time till the webpage got fully loaded.
         time.sleep(time_to_wait)
         session.close()
-        # Question: do we need to test with other popular flash websites?
 
-        # any better pattern matching?
+        # Any better pattern matching?
         if self._log_reader.can_find(' Received crash notification for '):
-            # well, there is a crash.
             raise error.TestFail('Browser crashed during test.')
