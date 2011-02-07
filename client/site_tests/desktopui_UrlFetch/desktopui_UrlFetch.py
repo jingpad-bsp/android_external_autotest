73865b094ce2413d971b43aa4f5db8c6e17800ef
Simon Glass <sjg@chromium.org>
2011-02-07 08:00:58 -0800

    Clean up UrlFetch a little and make it support ARM
    This test causes a reboot at the end. I modified it to exit cleanly and allow
    testing to continue without a reboot.
    
    Added newer binary for x86, and added an ARM binary so it runs on Seaboard.
    
    BUG=chromiumos-5620
    TEST=try on mario, qemu, seaboard
    
    Change-Id: I0178a79d380dcbff90d821bf9025c23c27007b05
    
    Review URL: http://codereview.chromium.org/6174008

--- client/site_tests/desktopui_UrlFetch/desktopui_UrlFetch.py
+++ client/site_tests/desktopui_UrlFetch/desktopui_UrlFetch.py
@@ -2,9 +2,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, utils
+import logging, os, time, utils
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
+from autotest_lib.client.cros import constants, cros_ui, login
 
 class desktopui_UrlFetch(test.test):
     version = 1
@@ -15,22 +16,47 @@ class desktopui_UrlFetch(test.test):
 
         os.chdir(self.bindir)
 
+        # Select correct binary.
+        cpuType = utils.get_cpu_arch()
+        url_fetch_test = 'url_fetch_test'
+        if cpuType == "arm":
+            url_fetch_test += '.arm'
+
+        # Stop chrome from restarting and kill login manager.
         try:
-          # Stop chrome from restarting and kill login manager
-          utils.system('touch /tmp/disable_chrome_restart')
-          utils.system('pkill chrome')
+            orig_pid = utils.system_output('pgrep session_manager')
+            open(constants.DISABLE_BROWSER_RESTART_MAGIC_FILE, 'w').close()
+        except IOError, e:
+            logging.debug(e)
+            raise error.TestError('Failed to disable browser restarting.')
 
-          # Copy over chrome, chrome.pak, etc needed for test
-          utils.system('cp -r %s/* .' % '/opt/google/chrome')
+        # We kill with signal 9 so that the session manager doesn't exit.
+        # If the session manager sees Chrome exit normally, it exits.
+        login.nuke_process_by_name(name=constants.BROWSER, with_prejudice=True)
 
-          # Setup environment
-          make_env = 'source ./required_environment.sh'
-          utils.system('%s && ./url_fetch_test --url=%s --wait_cookie_name=%s'
-                       % (make_env, url, cookie_expected))
+        clean_exit = False
+        try:
+            new_pid = utils.system_output('pgrep session_manager')
+            if orig_pid != new_pid:
+                raise error.TestFail(
+                    'session_manager restarted when chrome was killed')
 
-          #TODO(sosa@chromium.org) - Find a way to clean up besides rebooting
-          utils.system('shutdown -r 1 &')
+            # Copy over chrome, chrome.pak, locales, chromeos needed for test.
+            utils.system('cp -r %s/* .' % '/opt/google/chrome')
+
+            cros_ui.xsystem('./%s --url=%s --wait_cookie_name=%s'
+                                % (url_fetch_test, url, cookie_expected))
+            clean_exit = True
+
         except error.CmdError, e:
             logging.debug(e)
             raise error.TestFail('Url Fetch test was unsuccessful for %s %s %s'
                                  % (url, cookie_expected, os.getcwd()))
+        finally:
+            # Allow chrome to be restarted again.
+            os.unlink(constants.DISABLE_BROWSER_RESTART_MAGIC_FILE)
+
+            # Reset the UI but only if we need to (avoid double reset).
+            if not clean_exit:
+                login.nuke_login_manager()
+                login.refresh_login_screen()
