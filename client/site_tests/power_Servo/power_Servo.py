64309ab9b7add21bbe904e06086001893f35d162
Sameer Nanda <snanda@chromium.org>
2011-02-25 09:21:33 -0800

    autotest: added initial cut of servo library and autotest
    A very early cut of servo board support for autotest.
    
    TEST=ran an emulated servod daemon for testing. The real servod is in the
    process of being checked in by tbroch.
    BUG=12452
    
    Change-Id: I9b736cfc939cf47b8f0e7f066252f7f2519748a6
    
    Review URL: http://codereview.chromium.org/6576061

---
+++ client/site_tests/power_Servo/power_Servo.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import error, site_servo
+
+
+class power_Servo(test.test):
+    version = 1
+
+    def run_once(self, servo_host='', servo_port='', action='', **kwargs):
+        if servo_host is None or servo_port is None:
+            raise error.TestFail('servo_host or servo_port not specified')
+
+        try:
+            self.servo = site_servo.Servo()
+            self.servo.initialize(servo_host=servo_host,
+                                  servo_port=servo_port)
+        except:
+            raise error.TestFail('Could not connect to servod')
+
+        if action == 'get_gpio':
+            self._get_gpio(*kwargs)
+        elif action == 'set_gpio':
+            self._set_gpio(*kwargs)
+        else:
+            raise error.TestFail('Unsupported action: %s', action)
+
+
+    def _get_gpio(self, gpio_name='', gpio_expected_value=''):
+        value = self.servo.get_gpio(gpio_name=gpio_name)
+        if gpio_expected_value and str(value) != gpio_expected_value:
+            raise error.TestFail('gpio value did not match expected value')
+
+
+    def _set_gpio(self, gpio_name='', gpio_value=''):
+        self.servo.set_gpio(gpio_name=gpio_name, gpio_value=gpio_value)
