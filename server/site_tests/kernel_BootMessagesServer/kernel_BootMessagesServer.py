f30b8664daaa1e6105d1473e63678b9e788638fd
Doug Anderson <dianders@google.com>
2011-02-24 16:48:31 -0800

    Code cleanup for kernel_BootMessagesServer.
    Change-Id: I4d9146c9f628c6fb226cd2d389983abcdd7b4762
    
    BUG=chromium-os:12233
    TEST=./run_remote_tests.sh --board=${BOARD} --remote ${CLIENT_IP} kernel_BootMessagesServer
    
    Review URL: http://codereview.chromium.org/6574051

--- server/site_tests/kernel_BootMessagesServer/kernel_BootMessagesServer.py
+++ server/site_tests/kernel_BootMessagesServer/kernel_BootMessagesServer.py
@@ -11,57 +11,71 @@ from autotest_lib.server import test
 _KERN_WARNING = 4
 
 _WHITELIST = [
-  "Kernel-defined memdesc doesn't match the one from EFI!",
-  "Warning only 1919MB will be used.",
-  "Use a HIGHMEM enabled kernel.",
-  "pnp 00:01: io resource (0x164e-0x164f) overlaps 0000:00:1c.0 "
-  "BAR 7 (0x1000-0x1fff), disabling",
-  "i915 0000:00:02.0: Invalid ROM contents",
-  "[drm:intel_init_bios] *ERROR* VBT signature missing",
-  "usb 1-2: config 1 has an invalid interface number: 1 but max is 0",
-  "usb 1-2: config 1 has no interface number 0",
-  "device-mapper: verity: Failed to acquire device 'ROOT_DEV': -1",
-  "device-mapper: table: 254:0: verity: Device lookup failed",
-  "dm: starting dm-0 (vroot) failed",
-  "EXT3-fs warning: maximal mount count reached, running e2fsck is "
-  "recommended",
-  "i2c i2c-2: The new_device interface is still experimental and may change "
-  "in a near future",
-  "industrialio: module is from the staging directory, "
-  "the quality is unknown, you have been warned.",
-  "tsl2563: module is from the staging directory, the quality is unknown, "
-  "you have been warned.",
+    "Kernel-defined memdesc doesn't match the one from EFI!",
+    "Warning only 1919MB will be used.",
+    "Use a HIGHMEM enabled kernel.",
+    "pnp 00:01: io resource (0x164e-0x164f) overlaps 0000:00:1c.0 "
+    "BAR 7 (0x1000-0x1fff), disabling",
+    "i915 0000:00:02.0: Invalid ROM contents",
+    "[drm:intel_init_bios] *ERROR* VBT signature missing",
+    "usb 1-2: config 1 has an invalid interface number: 1 but max is 0",
+    "usb 1-2: config 1 has no interface number 0",
+    "device-mapper: verity: Failed to acquire device 'ROOT_DEV': -1",
+    "device-mapper: table: 254:0: verity: Device lookup failed",
+    "dm: starting dm-0 (vroot) failed",
+    "EXT3-fs warning: maximal mount count reached, running e2fsck is "
+    "recommended",
+    "i2c i2c-2: The new_device interface is still experimental and may change "
+    "in a near future",
+    "industrialio: module is from the staging directory, "
+    "the quality is unknown, you have been warned.",
+    "tsl2563: module is from the staging directory, the quality is unknown, "
+    "you have been warned.",
 ]
 
 class kernel_BootMessagesServer(test.test):
-  version = 1
+    version = 1
 
 
-  def read_dmesg(self, filename):
-    f = open(filename, 'w')
-    self.client.run('dmesg -r', stdout_tee=f)
-    f.close()
+    def _read_dmesg(self, filename):
+        """Put the contents of 'dmesg -r' into the given file.
+
+        @param filename: The file to write 'dmesg -r' into.
+        """
+        f = open(filename, 'w')
+        self._client.run('dmesg -r', stdout_tee=f)
+        f.close()
 
 
-  def reboot_machine(self):
-    self.client.run('reboot')
-    self.client.wait_down()
-    self.client.wait_up()
+    def _reboot_machine(self):
+        """Reboot the client machine.
+
+        We'll wait until the client is down, then up again.
+        """
+        self._client.run('reboot')
+        self._client.wait_down()
+        self._client.wait_up()
 
 
-  def run_once(self, host=None):
-    self.client = host
-    self.client_test = 'kernel_BootMessages'
-    dmesg_filename = os.path.join(self.resultsdir, 'dmesg')
+    def run_once(self, host=None):
+        """Run the test.
 
-    self.reboot_machine()
-    self.read_dmesg(dmesg_filename)
-    dmesg = utils.read_file(dmesg_filename)
-    unexpected = utils.check_raw_dmesg(dmesg, _KERN_WARNING, _WHITELIST)
-    if unexpected:
-      f = open(os.path.join(self.resultsdir, 'dmesg.err'), 'w')
-      for line in unexpected:
-          logging.error('UNEXPECTED DMESG: %s' % line)
-          f.write('%s\n' % line)
-      f.close()
-      raise error.TestFail("Unexpected dmesg warnings and/or errors.")
+        @param host: The client machine to connect to; should be a Host object.
+        """
+        assert host is not None, "The host must be specified."
+
+        self._client = host
+        dmesg_filename = os.path.join(self.resultsdir, 'dmesg')
+
+        self._reboot_machine()
+        self._read_dmesg(dmesg_filename)
+        dmesg = utils.read_file(dmesg_filename)
+        unexpected = utils.check_raw_dmesg(dmesg, _KERN_WARNING, _WHITELIST)
+
+        if unexpected:
+            f = open(os.path.join(self.resultsdir, 'dmesg.err'), 'w')
+            for line in unexpected:
+                logging.error('UNEXPECTED DMESG: %s' % line)
+                f.write('%s\n' % line)
+            f.close()
+            raise error.TestFail("Unexpected dmesg warnings and/or errors.")
