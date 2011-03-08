a2e8d3b465cd49051be4fa8cf2c8f0608da94721
Dale Curtis <dalecurtis@chromium.org>
2011-03-07 20:38:57 -0800

    Double ownership timeout to 60 seconds.
    Owner key generation can exceed the alotted timeout on machines which have insufficient entropy for key generation. Lab machines with rare phyical interaction are especially susceptible.
    
    Some /sbin/keygen numbers for a Cr-48 over 3 runs on 28 machines: min: 1.519s, max: 32.524s, avg 8.48s. Other boards had numbers ranging up to 42 seconds.
    
    Change-Id: I24c11deb20b44b2e81df89e3a0f77dbce903d0a2
    
    BUG=chromium-os:12846
    TEST=Ran several login based tests.
    
    Review URL: http://codereview.chromium.org/6628075

--- client/cros/login.py
+++ client/cros/login.py
@@ -10,6 +10,7 @@ from autotest_lib.client.common_lib import error
 
 
 _DEFAULT_TIMEOUT = 30
+_DEFAULT_OWNERSHIP_TIMEOUT = 60
 
 # Priority increase to use when logging in to make sure we aren't descheduled at
 # inopportune times.
@@ -329,7 +330,7 @@ def wait_for_initial_chrome_window(timeout=_DEFAULT_TIMEOUT):
         crash_msg='Chrome crashed before first tab rendered.')
 
 
-def wait_for_ownership(timeout=_DEFAULT_TIMEOUT):
+def wait_for_ownership(timeout=_DEFAULT_OWNERSHIP_TIMEOUT):
     log_reader = cros_logging.LogReader()
     log_reader.set_start_by_current()
     wait_for_condition(
