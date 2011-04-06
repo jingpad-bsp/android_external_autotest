a2e1853bcbead588929567a1c6acf978e8ce288a
Mike Truty <truty@google.com>
2011-04-06 15:11:22 -0700

    Stop warnings that autoserv is improperly exiting.
    Change-Id: Id7d83df110f9f46b6def29a15feb795c6bcff948
    
    BUG=none
    TEST=/home/truy/tmp/site_drone_utility_withtest.py
    TEST=quick check on cautotest
    
    Review URL: http://codereview.chromium.org/6803023

---
+++ scheduler/site_drone_utility.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+# Override default parser with our site parser.
+# This is coordinated with site_monitor_db.py.
+def check_parse(process_info):
+    return process_info['comm'] == 'site_parse'
