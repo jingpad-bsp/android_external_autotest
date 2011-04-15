e630f9d8ce46db962f8927014a69927fd653952f
Dale Curtis <dalecurtis@chromium.org>
2011-04-15 13:51:09 -0700

    Let cros_ui_test take care of login flow.
    As titled. The login flow for cros_ui_test has been honed to be more correct. No need to manually resolve creds or worry about refreshing the login screen prior to login.
    
    Hopefully this will fix the login timeout issues.
    
    Change-Id: Idd55b232fea2cb6362eb60c1744cbb4d7d1c89c2
    
    BUG=none
    TEST=Ran w/ run_remote_tests. Ensured BWSI test case still failed.
    
    Review URL: http://codereview.chromium.org/6869022

--- client/site_tests/security_ProfilePermissions/security_ProfilePermissions.py
+++ client/site_tests/security_ProfilePermissions/security_ProfilePermissions.py
@@ -6,40 +6,31 @@ import logging
 import os
 import stat
 
-from autotest_lib.client.bin import test, utils
+from autotest_lib.client.bin import utils
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import constants, cros_ui_test, login
+
 
 class security_ProfilePermissions(cros_ui_test.UITest):
     version = 1
     _HOMEDIR_MODE = 040700
-    auto_login = False
-    username = None
-    password = None
 
-    def run_once(self, creds=None):
-        """
-        Check permissions within cryptohome for anything too permissive.
-        """
-        if creds:
-            self.username = constants.CREDENTIALS[creds][0]
-            self.password = constants.CREDENTIALS[creds][1]
-
-        self.login(self.username, self.password)
+    def run_once(self):
+        """Check permissions within cryptohome for anything too permissive."""
         login.wait_for_initial_chrome_window()
 
         homepath = constants.CRYPTOHOME_MOUNT_PT
         homemode = os.stat(homepath)[stat.ST_MODE]
 
-        if (homemode != self._HOMEDIR_MODE):
+        if homemode != self._HOMEDIR_MODE:
             raise error.TestFail('%s permissions were %s' % (homepath,
                                                              oct(homemode)))
 
         # Writable by anyone else is bad, as is owned by anyone else.
         cmd = 'find -L "%s" \\( -perm /022 -o \\! -user chronos \\) -ls'
-        cmd = cmd % homepath
+        cmd %= homepath
         cmd_output = utils.system_output(cmd, ignore_status=True)
 
-        if (cmd_output != '') :
+        if cmd_output:
             logging.error(cmd_output)
             raise error.TestFail('Bad permissions found on cryptohome files')
