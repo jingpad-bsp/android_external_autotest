1f9a066a031a0fff6c8d3ea46b9e010f0675fa61
Simon Glass <sjg@chromium.org>
2011-04-29 14:34:57 -0700

    Rewrite UrlFetch test to use PyAuto
    Now that the PyAuto framework is in, we should use this for a number of
    tests. This test is the simplest to convert.
    
    BUG=chromium-os:13288
    TEST=(make sure pyauto_dep is in your autotest/files/client/deps)
    ./run_remote_tests.sh --remote=seaboard0 UrlFetch
    
    Change-Id: Ic896a164fa7b4596bc1951b0fe893ec29acb8ae6
    
    Committed: http://chrome-svn/viewvc/chromeos?view=rev&revision=47eb843
    
    Review URL: http://codereview.chromium.org/6904068

--- client/site_tests/desktopui_UrlFetch/desktopui_UrlFetch.py
+++ client/site_tests/desktopui_UrlFetch/desktopui_UrlFetch.py
@@ -1,64 +1,21 @@
-# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, time, utils
-from autotest_lib.client.bin import test
-from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros import constants, cros_ui, login
+from autotest_lib.client.cros import pyauto_test
 
-class desktopui_UrlFetch(test.test):
+
+class desktopui_UrlFetch(pyauto_test.PyAutoTest):
     version = 1
 
     def run_once(self):
         url = 'http://www.youtube.com'
-        cookie_expected = 'VISITOR_INFO1_LIVE'
+        cookie_expected = 'VISITOR_INFO1_LIVE2'
 
-        os.chdir(self.bindir)
+        self.pyauto.NavigateToURL(url)
+        if self.pyauto.GetActiveTabTitle().split()[0] != 'YouTube':
+            raise error.TestError('Unexpected web site title for YouTube')
 
-        # Select correct binary.
-        cpuType = utils.get_cpu_arch()
-        url_fetch_test = 'url_fetch_test'
-        if cpuType == "arm":
-            url_fetch_test += '.arm'
-
-        # Stop chrome from restarting and kill login manager.
-        try:
-            orig_pid = utils.system_output('pgrep %s' %
-                constants.SESSION_MANAGER)
-            open(constants.DISABLE_BROWSER_RESTART_MAGIC_FILE, 'w').close()
-        except IOError, e:
-            logging.debug(e)
-            raise error.TestError('Failed to disable browser restarting.')
-
-        # We kill with signal 9 so that the session manager doesn't exit.
-        # If the session manager sees Chrome exit normally, it exits.
-        login.nuke_process_by_name(name=constants.BROWSER, with_prejudice=True)
-
-        clean_exit = False
-        try:
-            new_pid = utils.system_output('pgrep %s' %
-                constants.SESSION_MANAGER)
-            if orig_pid != new_pid:
-                raise error.TestFail(
-                    'session_manager restarted when chrome was killed')
-
-            # Copy over chrome, chrome.pak, locales, chromeos needed for test.
-            utils.system('cp -r %s/* .' % '/opt/google/chrome')
-
-            cros_ui.xsystem('./%s --url=%s --wait_cookie_name=%s'
-                                % (url_fetch_test, url, cookie_expected))
-            clean_exit = True
-
-        except error.CmdError, e:
-            logging.debug(e)
-            raise error.TestFail('Url Fetch test was unsuccessful for %s %s %s'
-                                 % (url, cookie_expected, os.getcwd()))
-        finally:
-            # Allow chrome to be restarted again.
-            os.unlink(constants.DISABLE_BROWSER_RESTART_MAGIC_FILE)
-
-            # Reset the UI but only if we need to (avoid double reset).
-            if not clean_exit:
-                login.nuke_login_manager()
-                login.refresh_login_screen()
+        cookie = self.pyauto.GetCookie(self.pyauto.GURL(url))
+        if cookie != cookie_expected:
+            raise error.TestError('Unexpected cookie from YouTube')
