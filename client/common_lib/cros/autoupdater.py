5c32c72beab5784abff82d07bf2618946c2445ec
Dale Curtis <dalecurtis@chromium.org>
2011-05-04 19:24:23 -0700

    Cleanup and slight refactoring of autoupdater.
    After having to test many autoupdate changes recently, I've run into a
    few headaches which require some cleanup.
    
    Most notably, the hack we use to make sure stateful_update is recent is
    a PITA. I've changed this to fail quietly and fall back to whatever
    version of stateful_update is available on the host.
    
    I've also made some various code cleanups.
    
    BUG=none
    TEST=Ran run_remote_tests.
    
    Change-Id: Iebbbe89ff8483f412f1ce489ab7a9b13c03f3e90
    Reviewed-on: http://gerrit.chromium.org/gerrit/801
    Tested-by: Eric Li(李咏竹) <ericli@chromium.org>
    Reviewed-by: Eric Li(李咏竹) <ericli@chromium.org>

--- client/common_lib/cros/autoupdater.py
+++ client/common_lib/cros/autoupdater.py
@@ -1,20 +1,19 @@
-# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 import httplib
 import logging
+import os
 import re
-import socket
 import urlparse
 
-from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib import error, global_config
 
-# TODO(dalecurtis): HACK to bootstrap stateful updater until crosbug.com/8960 is
-# fixed.
-LOCAL_STATEFULDEV_UPDATER = ('/home/chromeos-test/chromeos-src/chromeos/src'
-                             '/platform/dev/stateful_update')
-STATEFULDEV_UPDATER = '/tmp/stateful_update'
+# Local stateful update path is relative to the CrOS source directory.
+LOCAL_STATEFUL_UPDATE_PATH = 'src/platform/dev/stateful_update'
+REMOTE_STATEUL_UPDATE_PATH = '/usr/local/bin/stateful_update'
+STATEFUL_UPDATE = '/tmp/stateful_update'
 UPDATER_BIN = '/usr/bin/update_engine_client'
 UPDATER_IDLE = 'UPDATE_STATUS_IDLE'
 UPDATER_NEED_REBOOT = 'UPDATE_STATUS_UPDATED_NEED_REBOOT'
@@ -44,31 +43,33 @@ class ChromiumOSUpdater():
 
 
     def check_update_status(self):
-        update_status_cmd = ' '.join([UPDATER_BIN, '-status', '2>&1',
-                                      '| grep CURRENT_OP'])
-        update_status = self._run(update_status_cmd)
+        """Return current status from update-engine."""
+        update_status = self._run(
+            '%s -status 2>&1 | grep CURRENT_OP' % UPDATER_BIN)
         return update_status.stdout.strip().split('=')[-1]
 
 
     def reset_update_engine(self):
-        logging.info('Resetting update-engine.')
+        """Restarts the update-engine service."""
         self._run('rm -f %s' % UPDATED_MARKER)
         try:
             self._run('initctl stop update-engine')
-        except error.AutoservRunError, e:
+        except error.AutoservRunError:
             logging.warn('Stopping update-engine service failed. Already dead?')
         self._run('initctl start update-engine')
-        # May need to wait if service becomes slow to restart.
+
         if self.check_update_status() != UPDATER_IDLE:
             raise ChromiumOSError('%s is not in an installable state' %
                                   self.host.hostname)
 
 
     def _run(self, cmd, *args, **kwargs):
+        """Abbreviated form of self.host.run(...)"""
         return self.host.run(cmd, *args, **kwargs)
 
 
     def rootdev(self, options=''):
+        """Returns the stripped output of rootdev <options>."""
         return self._run('rootdev %s' % options).stdout.strip()
 
 
@@ -80,7 +81,7 @@ class ChromiumOSUpdater():
         elif active_root == self.KERNEL_B['root']:
             return self.KERNEL_B, self.KERNEL_A
         else:
-            raise ChromiumOSError('Encountered unknown root partition: %s',
+            raise ChromiumOSError('Encountered unknown root partition: %s' %
                                   active_root)
 
 
@@ -111,77 +112,80 @@ class ChromiumOSUpdater():
         return self._run('/postinst %s 2>&1' % part)
 
 
+    def _update_root(self):
+        # Reset update_engine's state & check that update_engine is idle.
+        self.reset_update_engine()
+
+        # Run update_engine using the specified URL.
+        try:
+            autoupdate_cmd = '%s --update --omaha_url=%s 2>&1' % (
+                UPDATER_BIN, self.update_url)
+            self._run(autoupdate_cmd, timeout=900)
+        except error.AutoservRunError:
+            raise ChromiumOSError('update-engine failed on %s' %
+                                  self.host.hostname)
+
+        # Check that the installer completed as expected.
+        status = self.check_update_status()
+        if status != UPDATER_NEED_REBOOT:
+            raise ChromiumOSError('update-engine error on %s: %s' %
+                                  (self.host.hostname, status))
+
+
+    def _update_stateful(self):
+        # Attempt stateful partition update; this must succeed so that the newly
+        # installed host is testable after update.
+        statefuldev_url = self.update_url.replace('update', 'static/archive')
+
+        # Load the Chrome OS source tree location.
+        stateful_update_path = os.path.join(
+            global_config.global_config.get_config_value(
+                'CROS', 'source_tree', default=''),
+            LOCAL_STATEFUL_UPDATE_PATH)
+
+        if os.path.exists(stateful_update_path):
+            self.host.send_file(
+                stateful_update_path, STATEFUL_UPDATE, delete_dest=True)
+            statefuldev_cmd = [STATEFUL_UPDATE]
+        else:
+            logging.warn('Could not find local stateful_update script, falling'
+                         ' back on client copy.')
+            statefuldev_cmd = [REMOTE_STATEUL_UPDATE_PATH]
+
+        statefuldev_cmd += [statefuldev_url, '--stateful_change=clean', '2>&1']
+        try:
+            self._run(' '.join(statefuldev_cmd), timeout=600)
+        except error.AutoservRunError:
+            self.revert_boot_partition()
+            raise ChromiumOSError('stateful_update failed on %s' %
+                                  self.host.hostname)
+
+
     def run_update(self, force_update):
         booted_version = self.get_booted_version()
         if booted_version in self.update_version and not force_update:
             logging.info('System is already up to date. Skipping update.')
             return False
 
-        # Check that devserver is accepting connections (from autoserv's host)
+        # Check that Dev Server is accepting connections (from autoserv's host).
         # If we can't talk to it, the machine host probably can't either.
         auserver_host = urlparse.urlparse(self.update_url)[1]
         try:
             httplib.HTTPConnection(auserver_host).connect()
-        except socket.error:
-            raise ChromiumOSError('Update server at %s not available' %
-                                  auserver_host)
+        except IOError:
+            raise ChromiumOSError(
+                'Update server at %s not available' % auserver_host)
 
-        logging.info('Installing from %s to: %s' % (self.update_url,
-                                                    self.host.hostname))
-        # Reset update_engine's state & check that update_engine is idle.
-        self.reset_update_engine()
+        logging.info(
+            'Installing from %s to: %s', self.update_url, self.host.hostname)
 
-        # Run autoupdate command. This tells the autoupdate process on
-        # the host to look for an update at a specific URL and version
-        # string.
-        autoupdate_cmd = ' '.join([UPDATER_BIN,
-                                   '--update',
-                                   '--omaha_url=%s' % self.update_url,
-                                   ' 2>&1'])
-        logging.info(autoupdate_cmd)
-        try:
-            self._run(autoupdate_cmd, timeout=900)
-        except error.AutoservRunError, e:
-            # Either a runtime error occurred on the host, or
-            # update_engine_client exited with > 0.
-            raise ChromiumOSError('update_engine failed on %s' %
-                                  self.host.hostname)
+        logging.info('Updating root partition...')
+        self._update_root()
 
-        # Check that the installer completed as expected.
-        status = self.check_update_status()
-        if status != UPDATER_NEED_REBOOT:
-            raise ChromiumOSError('update-engine error on %s: '
-                                  '"%s" from update-engine' %
-                                  (self.host.hostname, status))
+        logging.info('Updating stateful partition...')
+        self._update_stateful()
 
-        # Attempt dev & test tools update (which don't live on the
-        # rootfs). This must succeed so that the newly installed host
-        # is testable after we run the autoupdater.
-        statefuldev_url = self.update_url.replace('update', 'static/archive')
-
-        # TODO(dalecurtis): HACK to bootstrap stateful updater until
-        # crosbug.com/8960 is fixed.
-        self.host.send_file(LOCAL_STATEFULDEV_UPDATER, STATEFULDEV_UPDATER,
-                            delete_dest=True)
-        statefuldev_cmd = [STATEFULDEV_UPDATER, statefuldev_url]
-
-        # TODO(dalecurtis): HACK necessary until R10 builds are out of testing.
-        if int(self.update_version.split('.')[1]) > 10:
-            statefuldev_cmd.append('--stateful_change=clean')
-
-        statefuldev_cmd.append('2>&1')
-        statefuldev_cmd = ' '.join(statefuldev_cmd)
-
-        logging.info(statefuldev_cmd)
-        try:
-            self._run(statefuldev_cmd, timeout=600)
-        except error.AutoservRunError, e:
-            # TODO(seano): If statefuldev update failed, we must mark
-            # the update as failed, and keep the same rootfs after
-            # reboot.
-            self.revert_boot_partition()
-            raise ChromiumOSError('stateful_update failed on %s.' %
-                                  self.host.hostname)
+        logging.info('Update complete.')
         return True
 
 
