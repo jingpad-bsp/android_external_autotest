4db70188ba021eb305bd6ebe0efb269fca9dbfb0
Jim Hebert <jimhebert@chromium.org>
2011-05-18 11:46:06 -0700

    Add security_RootCA test for validating roots shipped in CrOS
    BUG=chromium-os:9017
    TEST=run_remote_tests ... this test currently passes on 0.13.525
    The test currently passes, unsurprisingly, since the current baseline
    is derived from current shipping reality. This has been previously manually
    inspected vs the "golden" list and approved, but it wouldn't
    hurt to do that one more time coinciding with this check in, since it's been
    a few months. I'll take care of that and log any discrepancies with
    bugs.
    
    Change-Id: I0cb0edbd9e8911c47145fc9e36bacf340005b54b
    Reviewed-on: http://gerrit.chromium.org/gerrit/1122
    Reviewed-by: Chris Masone <cmasone@chromium.org>
    Reviewed-by: Jim Hebert <jimhebert@chromium.org>
    Tested-by: Jim Hebert <jimhebert@chromium.org>

---
+++ client/site_tests/security_RootCA/security_RootCA.py
@@ -0,0 +1,136 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging, os, re
+
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib import pexpect
+
+
+FINGERPRINT_RE = re.compile(r'Fingerprint \(SHA1\):\n\s+(\b[:\w]+)\b')
+NSS_ISSUER_RE = re.compile(r'Object Token:(.+\b)\s+[CGA]*,[CGA]*,[CGA]*\n')
+
+NSSCERTUTIL = '/usr/local/bin/nsscertutil'
+NSSMODUTIL = '/usr/local/bin/nssmodutil'
+OPENSSL = '/usr/bin/openssl'
+
+OPENSSL_CERT_DIR = '/usr/share/ca-certificates/chromeos'
+
+
+class security_RootCA(test.test):
+    version = 1
+
+    def get_baseline_sets(self):
+        """Returns a dictionary of sets. The keys are the names of
+           the ssl components and the values are the sets of fingerprints
+           we expect to find in that component's Root CA list.
+        """
+        baselines = {'nss': set([]), 'openssl': set([])}
+        baseline_file = open(os.path.join(self.bindir, 'baseline'))
+        for line in baseline_file:
+            (lib, fingerprint) = line.rstrip().split()
+            if lib == 'both':
+                baselines['nss'].add(fingerprint)
+                baselines['openssl'].add(fingerprint)
+            else:
+                baselines[lib].add(fingerprint)
+        return baselines
+
+    def get_nss_certs(self):
+        """Returns the set of certificate fingerprints observed in nss."""
+        tmpdir = self.tmpdir
+
+        # Create new empty cert DB.
+        child = pexpect.spawn('"%s" -N -d %s' % (NSSCERTUTIL, tmpdir))
+        child.expect('Enter new password:')
+        child.sendline('foo')
+        child.expect('Re-enter password:')
+        child.sendline('foo')
+        child.close()
+
+        # Add the certs found in the compiled NSS shlib to a new module in DB.
+        cmd = ('"%s" -add testroots '
+               '-libfile /usr/lib/libnssckbi.so'
+               ' -dbdir %s' % (NSSMODUTIL, tmpdir))
+        nssmodutil = pexpect.spawn(cmd)
+        nssmodutil.expect('\'q <enter>\' to abort, or <enter> to continue:')
+        nssmodutil.sendline('\n')
+        ret = utils.system_output(NSSMODUTIL + ' -list '
+                                  '-dbdir %s' % tmpdir)
+        self.assert_('2. testroots' in ret)
+
+        # Dump out the list of root certs.
+        all_certs = utils.system_output(NSSCERTUTIL +
+                                        ' -L -d %s -h all' % tmpdir)
+        certdict = {}  # A map of {SHA1_Fingerprint : CA_Nickname}.
+        for cert in NSS_ISSUER_RE.findall(all_certs):
+            cert_dump = utils.system_output(NSSCERTUTIL +
+                                            ' -L -d %s -n '
+                                            '\"Builtin Object Token:%s\"' %
+                                            (tmpdir, cert))
+            f = FINGERPRINT_RE.search(cert_dump)
+            certdict[f.group(1)] = cert
+        return set(certdict)
+
+
+    def get_openssl_certs(self):
+        """Returns the set of certificate fingerprints observed in openssl."""
+        fingerprint_cmd = ' '.join([OPENSSL, 'x509', '-fingerprint',
+                                    '-issuer', '-noout',
+                                    '-in %s/%%s' % OPENSSL_CERT_DIR])
+        certdict = {}  # A map of {SHA1_Fingerprint : CA_Nickname}.
+
+        for certfile in os.listdir(OPENSSL_CERT_DIR):
+            f, i = utils.system_output(fingerprint_cmd % certfile).splitlines()
+            fingerprint = f.split('=')[1]
+            if (fingerprint != certfile.split('.crt')[0]):
+                logging.warning('Filename %s doesn\'t match fingerprint %s' %
+                                (certfile, fingerprint))
+            for field in i.split('/'):
+                items = field.split('=')
+                # Compensate for stupidly malformed issuer fields.
+                if len(items) > 1:
+                    if items[0] == 'CN':
+                        certdict[fingerprint] = items[1]
+                        break
+                    elif items[0] == 'O':
+                        certdict[fingerprint] = items[1]
+                        break
+                else:
+                    logging.warning('Malformed issuer string %s' % i)
+            # Check that we found a name for this fingerprint.
+            if not fingerprint in certdict:
+                raise error.TestFail('Couldn\'t find issuer string for %s' %
+                                     fingerprint)
+        return set(certdict)
+
+
+    def run_once(self):
+        """Enforce exact-matches (no extra, no missing) for the installed
+           Root CA's vs their baselines. Cover both nss and openssl.
+        """
+        testfail = False
+
+        # Dump certificate info and run comparisons.
+        seen = {}
+        seen['nss'] = self.get_nss_certs()
+        seen['openssl'] = self.get_openssl_certs()
+        expected = self.get_baseline_sets()
+
+        for lib in seen.keys():
+            missing = expected[lib].difference(seen[lib])
+            unexpected = seen[lib].difference(expected[lib])
+            if missing or unexpected:
+                testfail = True
+                logging.error('Results for %s' % lib)
+                logging.error('Missing')
+                for i in missing:
+                    logging.error(i)
+                logging.error('Unexpected')
+                for i in unexpected:
+                    logging.error(i)
+
+        if testfail:
+            raise error.TestFail('Root CA Baseline mismatches')
