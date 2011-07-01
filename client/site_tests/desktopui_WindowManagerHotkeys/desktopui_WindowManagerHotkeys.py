cb1aa8de2f24ed301637521daad388298470db07
Daniel Erat <derat@chromium.org>
2011-07-01 16:56:55 -0700

    autotest: Remove screenshot test from WindowManagerHotkeys.
    I'm making the window manager write logged-in screenshots to
    /home/chronos/user/Downloads instead of
    /home/chronos/user/Downloads/Screenshots.  I don't see much
    value in continuing to test the screenshot hotkey here --
    there's already another test for Ctrl-Alt-T.
    
    BUG=chromium-os:16883
    TEST=still passes
    
    Change-Id: I0cc9d65da42bdc312ee7d0efe9d7b938b67facd2
    Reviewed-on: http://gerrit.chromium.org/gerrit/3678
    Reviewed-by: Daniel Erat <derat@chromium.org>
    Tested-by: Daniel Erat <derat@chromium.org>

--- client/site_tests/desktopui_WindowManagerHotkeys/desktopui_WindowManagerHotkeys.py
+++ client/site_tests/desktopui_WindowManagerHotkeys/desktopui_WindowManagerHotkeys.py
@@ -2,9 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os, random, re, shutil, time
-from autotest_lib.client.bin import utils
-from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import cros_ui_test, login
 
 class desktopui_WindowManagerHotkeys(cros_ui_test.UITest):
@@ -28,14 +25,3 @@ class desktopui_WindowManagerHotkeys(cros_ui_test.UITest):
         ax.await_condition(
             lambda: ax.get_active_window_property() != orig_active_win_xid,
             desc='Waiting for terminal to become active window')
-
-        # Press the Print Screen key and check that a screenshot is written.
-        screenshot_dir = '/home/chronos/user/Downloads/Screenshots'
-        shutil.rmtree(screenshot_dir, ignore_errors=True)
-        ax.send_hotkey('Print')
-        utils.poll_for_condition(
-            lambda: os.access(screenshot_dir, os.F_OK) and \
-                    os.listdir(screenshot_dir),
-            error.TestFail(
-                'Waiting for screenshot in %s' % screenshot_dir))
-        shutil.rmtree(screenshot_dir, ignore_errors=True)
