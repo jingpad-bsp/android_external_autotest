3030b56b89fdf897f1418495ab55f7e2391fa524
Chris Masone <cmasone@chromium.org>
2011-08-01 10:46:28 -0700

    [autotest] Make desktopui_FlashSanityCheck hermetic
    Still don't do a great job at ensuring we load the flash content,
    but nirnimesh will address that by migrating to pyauto.  We want
    to run a local server and serve the flash content locally, tho.
    
    BUG=chromium-os:18475
    TEST=run this test.
    
    Change-Id: Ieaf057885cb304e30b5cc63c560346b4b2d7aa30
    Reviewed-on: http://gerrit.chromium.org/gerrit/5080
    Tested-by: Chris Masone <cmasone@chromium.org>
    Reviewed-by: Chris Sosa <sosa@chromium.org>

--- client/site_tests/desktopui_FlashSanityCheck/desktopui_FlashSanityCheck.py
+++ client/site_tests/desktopui_FlashSanityCheck/desktopui_FlashSanityCheck.py
@@ -5,11 +5,17 @@
 import os, time
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import constants, cros_logging, cros_ui
-from autotest_lib.client.cros import cros_ui_test, login
+from autotest_lib.client.cros import cros_ui_test, login, httpd
 
 
 class desktopui_FlashSanityCheck(cros_ui_test.UITest):
-    version = 2
+    version = 3
+
+    def initialize(self):
+        self._test_url = 'http://localhost:8000/index.html'
+        self._testServer = httpd.HTTPListener(8000, docroot=self.srcdir)
+        self._testServer.run()
+        super(desktopui_FlashSanityCheck, self).initialize()
 
 
     def run_once(self, time_to_wait=25):
@@ -19,12 +25,16 @@ class desktopui_FlashSanityCheck(cros_ui_test.UITest):
         # Make sure that we don't have the initial browser window popping up in
         # the middle of the test.
         login.wait_for_initial_chrome_window()
-        # TODO(sosa): Check in flash video that is a solid test.
-        session = cros_ui.ChromeSession(args='http://www.youtube.com')
-        # Wait some time till the webpage has (probably) fully loaded.
+        session = cros_ui.ChromeSession(args=self._test_url)
+        # TODO(nirnimesh) use pyauto to accurately wait.
         time.sleep(time_to_wait)
 
         # Any better pattern matching?
         msg = ' Received crash notification for ' + constants.BROWSER
         if self._log_reader.can_find(msg):
             raise error.TestFail('Browser crashed during test.')
+
+
+    def cleanup(self):
+        self._testServer.stop()
+        super(desktopui_FlashSanityCheck, self).cleanup()
