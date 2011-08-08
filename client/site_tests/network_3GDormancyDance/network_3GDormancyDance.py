005cc6b1a61eb3c1200499fd9437c210440b5e2c
ttuttle <ttuttle@chromium.org>
2011-08-08 16:20:42 -0400

    network_3GDormancyDance: Add a real state machine, finally.
    DormancyDance has been in dire need of some actual logic for tracking
    what the modem is doing, instead of just knee-jerk responding to each
    incoming event.
    
    I've added an enumeration of States, split out the handling of each
    incoming event, and added exceptions for unexpected transitions.
    The test still hangs on exceptions, because it's not using an
    exception-forwarding main loop; I'll change it to use one in a future
    patch.
    
    BUG=chromium-os:17848
    TEST=DormancyDance passed with ops=500 as a cursory smoketest
    
    Change-Id: I50de82795a16ea6a47d5cb9635482abcb482502e
    Reviewed-on: http://gerrit.chromium.org/gerrit/5488
    Reviewed-by: Jason Glasgow <jglasgow@chromium.org>
    Tested-by: ttuttle <ttuttle@chromium.org>

--- client/site_tests/network_3GDormancyDance/network_3GDormancyDance.py
+++ client/site_tests/network_3GDormancyDance/network_3GDormancyDance.py
@@ -11,11 +11,20 @@ import glib
 from autotest_lib.client.cros import flimflam_test_path
 import flimflam, mm
 
+class State:
+    ENABLING = 0
+    REGISTERING = 1
+    CONNECTING = 2
+    WAITING = 3
+    DISCONNECTING = 4
+    DISABLING = 5
+
 class network_3GDormancyDance(test.test):
     version = 1
 
     def countdown(self):
         self.opsleft -= 1
+        print 'Countdown: %d' % (self.opsleft,)
         if self.opsleft == 0:
             self.mainloop.quit()
 
@@ -36,75 +45,144 @@ class network_3GDormancyDance(test.test):
         modem.RequestEvents('+dormancy')
 
     def enable(self):
-        print 'Enable'
+        print 'Enabling...'
         self.countdown()
+        self.state = State.ENABLING
         self.flim.EnableTechnology('cellular')
 
     def disable(self):
-        print 'Disable'
+        print 'Disabling...'
         self.countdown()
+        self.state = State.DISABLING
         self.flim.DisableTechnology('cellular')
 
     def connect(self):
-        print 'Connect'
+        print 'Connecting...'
         self.countdown()
+        self.state = State.CONNECTING
         self.flim.ConnectService(service=self.service, config_timeout=120)
 
     def disconnect(self):
-        print 'Disconnect'
+        print 'Disconnecting...'
         self.countdown()
+        self.state = State.DISCONNECTING
         self.flim.DisconnectService(service=self.service, wait_timeout=60)
 
     def PropertyChanged(self, *args, **kwargs):
-        if args[0] in ['Powered', 'Connected', 'Services']:
-            print 'PropertyChanged: %s %s' % (args, kwargs)
         if args[0] == 'Powered':
             if not args[1]:
-                self.enable()
+                self.HandleDisabled()
             else:
-                print 'Waiting for service...'
-                self.waiting_for_service = True
-        if args[0] == 'Connected':
+                self.HandleEnabled()
+        elif args[0] == 'Connected':
             if not args[1]:
-                self.disable()
-        if args[0] == 'Services':
-            self.FindService()
-            if self.waiting_for_service and self.service:
-                self.waiting_for_service = False
-                self.connect()
+                self.HandleDisconnected()
+            else:
+                self.HandleConnected()
+        elif args[0] == 'Services':
+            self.CheckService()
 
     def DormancyStatus(self, *args, **kwargs):
-        print 'DormancyStatus: %s %s' % (args, kwargs)
         if args[0]:
-            self.disconnect()
-
-    def begin(self):
-        print 'Setup...'
-        self.FindService()
-        if self.service:
-            self.disable()
-        else:
-            self.enable()
+            self.HandleDormant()
 
     def FindService(self):
         self.service = self.flim.FindElementByPropertySubstring('Service',
                                                                 'Type',
                                                                 'cellular')
-        print 'Service: %s' % (self.service,)
 
-    def run_once(self, name='usb', ops=5000, seed=None):
+    def CheckService(self):
+        self.FindService()
+        if self.state == State.REGISTERING and self.service:
+            self.HandleRegistered()
+
+    def HandleDisabled(self):
+        if self.state != State.DISABLING:
+            raise error.TestFail('Disabled while not in state Disabling')
+        print 'Disabled'
+        self.enable()
+
+    def HandleEnabled(self):
+        if self.state != State.ENABLING:
+            raise error.TestFail('Enabled while not in state Enabling')
+        print 'Enabled'
+        self.state = State.REGISTERING
+        print 'Waiting for registration...'
+        self.CheckService()
+
+    def HandleRegistered(self):
+        if self.state != State.REGISTERING:
+            raise error.TestFail('Registered while not in state Registering')
+        print 'Registered'
+        self.connect()
+
+    def HandleConnected(self):
+        if self.state != State.CONNECTING:
+            raise error.TestFail('Connected while not in state Connecting')
+        print 'Connected'
+        self.state = State.WAITING
+        print 'Waiting for dormancy...'
+
+    def HandleDormant(self):
+        if self.state != State.WAITING:
+            raise error.TestFail('Dormant while not in state Waiting')
+        print 'Dormant'
+        self.disconnect()
+
+    def HandleDisconnected(self):
+        if self.state != State.DISCONNECTING:
+            raise error.TestFail(
+                'Disconnected while not in state Disconnecting')
+        print 'Disconnected'
+        self.disable()
+
+    def begin(self):
+        connected = False
+        powered = False
+
+        self.FindService()
+        if self.service:
+            service_props = self.service.GetProperties(utf8_strings = True)
+            if service_props['State'] in ['online', 'portal', 'ready']:
+                connected = True
+            print 'Service exists, and state is %s.' % (service_props['State'],)
+        else:
+            print 'Service does not exist.'
+
+        device_props = self.device.GetProperties(utf8_strings = True)
+        if device_props['Powered']:
+            print 'Device is powered.'
+            powered = True
+        else:
+            print 'Device is unpowered.'
+
+        if powered and connected:
+            print 'Starting with Disconnect.'
+            self.disconnect()
+        elif powered and (not connected):
+            print 'Starting with Disable.'
+            self.disable()
+        elif (not powered) and (not connected):
+            print 'Starting with Enable.'
+            self.enable()
+        else:
+            raise error.TestFail('Service online but device unpowered!')
+
+    def run_once(self, name='usb', ops=500, seed=None):
         self.opsleft = ops
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
         self.bus = dbus.SystemBus()
+
         modem_path = self.FindModemPath()
-        print 'Modem: %s' % (modem_path,)
         if not modem_path:
             raise error.TestFail('No Gobi modem found.')
+        print 'Modem: %s' % (modem_path,)
         self.RequestDormancyEvents(modem_path)
+
         self.flim = flimflam.FlimFlam()
         self.manager = flimflam.DeviceManager(self.flim)
         self.device = self.flim.FindElementByNameSubstring('Device', name)
-        self.waiting_for_service = False
+
         if not self.device:
             self.device = self.flim.FindElementByPropertySubstring('Device',
                                                                    'Interface',
