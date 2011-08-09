76fd307c017408f76ca0be2b61ad3d91eeec039e
Nick Sanders <nsanders@chromium.org>
2011-08-08 22:38:06 -0700

    Fix factory_Lightsensor UI
    Allow countdown to actually count down
    Allow status to report test item status
    Print current light sensor reading
    
    BUG=chromium-os:18713
    TEST=see countdown, reading, status
    
    Change-Id: Icab55065669f49713705a3c8ed7b10215855b0bf
    Reviewed-on: http://gerrit.chromium.org/gerrit/6471
    Tested-by: Nick Sanders <nsanders@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- client/site_tests/factory_LightSensor/factory_LightSensor.py
+++ client/site_tests/factory_LightSensor/factory_LightSensor.py
@@ -102,7 +102,7 @@ class iio_generic():
             error.ValueError('Illegal value %s for type' % type)
 
 class factory_LightSensor(test.test):
-    version = 1
+    version = 2
 
     def key_release_callback(self, widget, event):
         if event.keyval == ord('Q'):
@@ -110,29 +110,6 @@ class factory_LightSensor(test.test):
         return True
 
     def timer_event(self, countdown_label):
-        val = self._als.read('mean',samples=5, delay=0)
-        #factory.log("avg_val = %d" % val)
-
-        passed = 0
-        for name in _SUBTEST_LIST:
-            cfg = _SUBTEST_CFG[name]
-            if 'max' in cfg:
-                if val > cfg['max']:
-                    factory.log("Passed checking max %d > %d" % \
-                                    (val, cfg['max']))
-                    self._status_map[name] = ful.PASSED
-            elif 'min' in cfg:
-                if val < cfg['min']:
-                    factory.log("Passed checking min %d < %d" % \
-                                    (val, cfg['min']))
-                    self._status_map[name] = ful.PASSED
-            if self._status_map[name] is ful.PASSED:
-                passed += 1
-
-        if passed is len(_SUBTEST_LIST):
-            factory.log('Passed all sensor tests')
-            gtk.main_quit()
-
         time_remaining = max(0, self._deadline - time.time())
         if time_remaining is 0:
             factory.log('deadline reached')
@@ -140,12 +117,49 @@ class factory_LightSensor(test.test):
 
         countdown_label.set_text('%d' % time_remaining)
         countdown_label.queue_draw()
+        gtk.main_iteration(False)
         return True
 
-    def label_status_expose(self, widget, event, name=None):
+    def sensor_event(self, sensor_value):
+        val = self._als.read('mean',samples=5, delay=0)
+
+        passed = 0
+        for name in _SUBTEST_LIST:
+            cfg = _SUBTEST_CFG[name]
+            if self._status_map[name] is ful.PASSED:
+                # No need to recheck, we already passed.
+                pass
+            elif 'max' in cfg:
+                if val > cfg['max']:
+                    factory.log("Passed checking max %d > %d" % \
+                                    (val, cfg['max']))
+                    self._status_map[name] = ful.PASSED
+                    self._status_label[name].queue_draw()
+            elif 'min' in cfg:
+                if val < cfg['min']:
+                    factory.log("Passed checking min %d < %d" % \
+                                    (val, cfg['min']))
+                    self._status_map[name] = ful.PASSED
+                    self._status_label[name].queue_draw()
+            if self._status_map[name] is ful.PASSED:
+                passed += 1
+
+        if passed is len(_SUBTEST_LIST):
+            factory.log('Passed all sensor tests')
+            gtk.main_quit()
+
+        sensor_value.set_text('%d' % val)
+        sensor_value.queue_draw()
+        gtk.main_iteration(False)
+        return True
+
+
+    def label_status_expose(self, widget, event, name):
         status = self._status_map[name]
         widget.set_text(status)
         widget.modify_fg(gtk.STATE_NORMAL, ful.LABEL_COLORS[status])
+        return False
+
 
     def make_subtest_label_box(self, name):
         eb = gtk.EventBox()
@@ -153,8 +167,7 @@ class factory_LightSensor(test.test):
         label_status = ful.make_label(ful.UNTESTED, size=_LABEL_STATUS_SIZE,
                                       alignment=(0, 0.5),
                                       fg=ful.LABEL_COLORS[ful.UNTESTED])
-        expose_cb = lambda *x: self.label_status_expose(*x, **{'name':name})
-        label_status.connect('expose_event', expose_cb)
+        label_status.connect('expose_event', self.label_status_expose, name)
         label_en = ful.make_label(name, alignment=(1,0.5))
         label_sep = ful.make_label(' : ', alignment=(0.5, 0.5))
         hbox = gtk.HBox()
@@ -183,14 +196,25 @@ class factory_LightSensor(test.test):
 
         vbox = gtk.VBox()
         prompt_label = ful.make_label(
-            'Cover Sensor with Finger\n' + '请遮蔽光传感器\n\n' +
-            'Shine flashlight at Sensor\n' + '请以灯光照射光传感器\n\n',
+            'Cover Sensor with Finger (Input < %s)\n' % lux_min +
+            '请遮蔽光传感器\n\n' +
+            'Shine flashlight at Sensor (Input > %s)\n' % lux_max +
+            '请以灯光照射光传感器\n',
             fg=ful.WHITE)
         vbox.pack_start(prompt_label, False, False)
 
+        hbox = gtk.HBox()
+        sensor_label = ful.make_label('Input: ', fg=ful.WHITE)
+        sensor_value = ful.make_label('     ', fg=ful.WHITE)
+        hbox.pack_start(sensor_label, False, False)
+        hbox.pack_start(sensor_value, False, False)
+        vbox.pack_start(hbox, False, False)
+
+        self._status_label = {}
         for name in _SUBTEST_LIST:
             label_box = self.make_subtest_label_box(name)
             vbox.pack_start(label_box, False, False)
+            self._status_label[name] = label_box
             cfg = _SUBTEST_CFG[name]
             # change defaults per factory setup
             if 'max' in cfg and lux_max is not None:
@@ -200,7 +224,8 @@ class factory_LightSensor(test.test):
 
         countdown_widget, countdown_label = ful.make_countdown_widget()
         vbox.pack_start(countdown_widget, False, False)
-        gobject.timeout_add(200, self.timer_event, countdown_label)
+        gobject.timeout_add(1000, self.timer_event, countdown_label)
+        gobject.timeout_add(300, self.sensor_event, sensor_value)
 
         self._test_widget = vbox
         ful.run_test_widget(self.job, vbox,
