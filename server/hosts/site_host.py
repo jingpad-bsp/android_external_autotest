aa5eedb042bc7d3470e3ca66b00e599a81f01cb9
Dale Curtis <dalecurtis@chromium.org>
2011-08-23 16:18:52 -0700

    Attempt to fix dying SSH connections.
    Lots of jobs are showing up with the server connection dying yet the
    test completing normally on the client. The issue could be related to
    the flimflam restart we're issuing at the end of tests to clear the DNS
    settings.
    
    It's also possible there are some intermittent network issues
    in the lab.
    
    In either case extending the ServerAliveInterval should resolve the
    problem.
    
    BUG=none
    TEST=running in production.
    
    Change-Id: I0ba5142d93db8158230ea422faf915c42fc48ba6
    Reviewed-on: http://gerrit.chromium.org/gerrit/6549
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Tested-by: Dale Curtis <dalecurtis@chromium.org>

--- server/hosts/site_host.py
+++ server/hosts/site_host.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 def make_ssh_command(user='root', port=22, opts='', hosts_file=None,
                      connect_timeout=None, alive_interval=None):
     """Override default make_ssh_command to use options tuned for Chrome OS.
@@ -6,9 +10,10 @@ def make_ssh_command(user='root', port=22, opts='', hosts_file=None,
       - ConnectTimeout=10; maximum of 10 seconds allowed for an SSH connection
       failure.
 
-      - ServerAliveInterval=60; which causes SSH to ping connection every
-      60 seconds. In conjunction with ServerAliveCountMax ensures that if the
-      connection dies, Autotest will bail out quickly.
+      - ServerAliveInterval=180; which causes SSH to ping connection every
+      180 seconds. In conjunction with ServerAliveCountMax ensures that if the
+      connection dies, Autotest will bail out quickly. Originally tried 60 secs,
+      but saw frequent job ABORTS where the test completed successfully.
 
       - ServerAliveCountMax=1; only allow a single keep alive failure.
 
@@ -17,6 +22,6 @@ def make_ssh_command(user='root', port=22, opts='', hosts_file=None,
     """
     base_command = ('/usr/bin/ssh -a -x %s -o StrictHostKeyChecking=no'
                     ' -o UserKnownHostsFile=/dev/null -o BatchMode=yes'
-                    ' -o ConnectTimeout=10 -o ServerAliveInterval=60'
+                    ' -o ConnectTimeout=10 -o ServerAliveInterval=180'
                     ' -o ServerAliveCountMax=1 -l %s -p %d')
     return base_command % (opts, user, port)
