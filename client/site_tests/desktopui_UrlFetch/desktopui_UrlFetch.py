6c2883de1d6f892704a3126472c0aa5679a98520
Mandeep Singh Baines <msb@chromium.org>
2011-08-26 11:10:46 -0700

    Revert "Add a non-live version of desktopui_UrlFetch test"
    This reverts commit cfe8d8079b3bcf7d2f026f7422b566039c9c99e3
    
    This test is breaking the buildbot:
    
    http://build.chromium.org/p/chromiumos/builders/x86%20generic%20chrome%20PFQ/builds/97
    Change-Id: I48b352afe788e449beefa565effa70ecccf45904
    Reviewed-on: http://gerrit.chromium.org/gerrit/6759
    Reviewed-by: Satoru Takabayashi <satorux@chromium.org>
    Tested-by: Mandeep Singh Baines <msb@chromium.org>

--- client/site_tests/desktopui_UrlFetch/desktopui_UrlFetch.py
+++ client/site_tests/desktopui_UrlFetch/desktopui_UrlFetch.py
@@ -3,44 +3,25 @@
 # found in the LICENSE file.
 
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros import httpd, pyauto_test
+from autotest_lib.client.cros import pyauto_test
 
 
 class desktopui_UrlFetch(pyauto_test.PyAutoTest):
     version = 1
 
-
-    def initialize(self, live=True):
-        super(desktopui_UrlFetch, self).initialize()
-        if live:
-            self._test_url = 'http://www.noaa.gov/'
-            self._expected_title = \
-                'NOAA - National Oceanic and Atmospheric Administration'
-        else:
-            self._test_url = 'http://localhost:8000/hello.html'
-            self._expected_title = 'Hello World'
-            self._testServer = httpd.HTTPListener(8000, docroot=self.bindir)
-            self._testServer.run()
-
-
-    def cleanup(self, live=True):
-        if not live:
-            self._testServer.stop()
-        super(desktopui_UrlFetch, self).cleanup()
-
-
     def run_once(self):
+        url = 'http://www.noaa.gov/'
+        title = 'NOAA - National Oceanic and Atmospheric Administration'
         import pyauto
 
-        assert not self.pyauto.GetCookie(pyauto.GURL(self._test_url))
+        assert not self.pyauto.GetCookie(pyauto.GURL(url))
 
-        self.pyauto.NavigateToURL(self._test_url)
+        self.pyauto.NavigateToURL(url)
         tab_title = self.pyauto.GetActiveTabTitle()
-        if tab_title != self._expected_title:
-            raise error.TestError(
-                'Unexpected web site title.  Expected: %s. '
-                'Returned: %s' % (self._expected_title, tab_title))
+        if tab_title != title:
+            raise error.TestError('Unexpected web site title.  Expected: %s. '
+                                  'Returned: %s' % (title, tab_title))
 
-        cookie = self.pyauto.GetCookie(pyauto.GURL(self._test_url))
+        cookie = self.pyauto.GetCookie(pyauto.GURL(url))
         if not cookie:
-            raise error.TestError('Expected cookie for %s' % self._test_url)
+            raise error.TestError('Expected cookie for %s' % url)
