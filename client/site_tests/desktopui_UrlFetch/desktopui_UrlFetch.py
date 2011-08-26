f247d084f97fb863b09b939e4cdd7f1e9541df7f
Nirnimesh <nirnimesh@chromium.org>
2011-08-26 12:03:01 -0700

    Reland desktopui_UrlFetch with fix
    This relands the CL in http://gerrit.chromium.org/gerrit/#change,6473
    with fixes.
    
    In the original CL the test derives from pyauto_test. I've switched
    to have it derive from cros_ui_test, so that the test can do fake login
    in non-live mode. Only the non-live mode runs in smoke suite.
    
    Original CL description:
    Add a non-live version of desktopui_UrlFetch test
    
    The non-live version uses the local http server and is included in the BVT suite.
    This should prevent bugs like crosbug.com/19468
    
    The live version uses real internet and is part of pyauto_basic suite.
    
    BUG=None
    TEST=desktopui_UrlFetch
    
    Change-Id: I91d2b5106304b00516386369af25ef7c0811f3b8
    Reviewed-on: http://gerrit.chromium.org/gerrit/6769
    Reviewed-by: Satoru Takabayashi <satorux@chromium.org>
    Tested-by: Nirnimesh <nirnimesh@chromium.org>

--- client/site_tests/desktopui_UrlFetch/desktopui_UrlFetch.py
+++ client/site_tests/desktopui_UrlFetch/desktopui_UrlFetch.py
@@ -3,25 +3,52 @@
 # found in the LICENSE file.
 
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros import pyauto_test
+from autotest_lib.client.cros import cros_ui_test, httpd
 
 
-class desktopui_UrlFetch(pyauto_test.PyAutoTest):
+class desktopui_UrlFetch(cros_ui_test.UITest):
     version = 1
 
+
+    def initialize(self, live=True):
+        self._live = live
+        super(desktopui_UrlFetch, self).initialize(creds='$default')
+        if live:
+            self._test_url = 'http://www.noaa.gov/'
+            self._expected_title = \
+                'NOAA - National Oceanic and Atmospheric Administration'
+        else:
+            self._test_url = 'http://localhost:8000/hello.html'
+            self._expected_title = 'Hello World'
+            self._testServer = httpd.HTTPListener(8000, docroot=self.bindir)
+            self._testServer.run()
+
+
+    def cleanup(self):
+        if not self._live:
+            self._testServer.stop()
+        super(desktopui_UrlFetch, self).cleanup()
+
+
+    def start_authserver(self):
+        # Do nothing in live mode so that we do real login.
+        # In non-live mode, setup local authserver which handles login.
+        if not self._live:
+            super(desktopui_UrlFetch, self).start_authserver()
+
+
     def run_once(self):
-        url = 'http://www.noaa.gov/'
-        title = 'NOAA - National Oceanic and Atmospheric Administration'
         import pyauto
 
-        assert not self.pyauto.GetCookie(pyauto.GURL(url))
+        assert not self.pyauto.GetCookie(pyauto.GURL(self._test_url))
 
-        self.pyauto.NavigateToURL(url)
+        self.pyauto.NavigateToURL(self._test_url)
         tab_title = self.pyauto.GetActiveTabTitle()
-        if tab_title != title:
-            raise error.TestError('Unexpected web site title.  Expected: %s. '
-                                  'Returned: %s' % (title, tab_title))
+        if tab_title != self._expected_title:
+            raise error.TestError(
+                'Unexpected web site title.  Expected: %s. '
+                'Returned: %s' % (self._expected_title, tab_title))
 
-        cookie = self.pyauto.GetCookie(pyauto.GURL(url))
+        cookie = self.pyauto.GetCookie(pyauto.GURL(self._test_url))
         if not cookie:
-            raise error.TestError('Expected cookie for %s' % url)
+            raise error.TestError('Expected cookie for %s' % self._test_url)
