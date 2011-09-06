3f4e4bf02ccc89a22c257ed4ed6df4730bdeaa2a
Daniel Erat <derat@chromium.org>
2011-09-06 14:49:29 -0700

    autotest: Simplify desktopui_WindowManagerFocusNewWindows.
    This makes the test just check that the _NET_ACTIVE_WINDOW
    X property is set, which acts as a good proxy for "is the
    window manager running and focusing windows?".
    
    I'm doing this to avoid flakiness that I suspect is being
    caused by a race between Chrome's first window getting
    painted and the windows created by the testing code being
    mapped.  The additional logic that this test was checking
    (namely, that newly-mapped windows take the focus from
    existing windows) is already covered in much more detail by
    layout_manager2_test in the window manager repository.
    
    BUG=chromium-os:19786
    TEST=ran it successfully
    
    Change-Id: I0bf089018776a7a7e856fbaf4f91066453c14ee5
    Reviewed-on: http://gerrit.chromium.org/gerrit/7292
    Reviewed-by: David James <davidjames@chromium.org>
    Tested-by: Daniel Erat <derat@chromium.org>

--- client/site_tests/desktopui_WindowManagerFocusNewWindows/desktopui_WindowManagerFocusNewWindows.py
+++ client/site_tests/desktopui_WindowManagerFocusNewWindows/desktopui_WindowManagerFocusNewWindows.py
@@ -1,59 +1,17 @@
-# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, time
-from autotest_lib.client.bin import test
-from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import cros_ui, cros_ui_test, login
 
 class desktopui_WindowManagerFocusNewWindows(cros_ui_test.UITest):
     version = 1
 
-
     def initialize(self, creds='$default'):
         cros_ui_test.UITest.initialize(self, creds)
 
-
-    def __check_active_window(self, id, info):
-        """Check that a particular window is active.
-
-        Args:
-            id: int window ID
-            info: AutoX.WindowInfo object corresponding to 'id'
-
-        Raises:
-            error.TestFail: if a condition timed out
-        """
-        try:
-            self.autox.await_condition(
-                lambda: self.autox.get_active_window_property() == id,
-                desc='Waiting for _NET_ACTIVE_WINDOW to contain 0x%x' % id)
-            self.autox.await_condition(
-                lambda: info.is_focused,
-                desc='Waiting for window 0x%x to be focused' % id)
-
-        except self.autox.ConditionTimeoutError as exception:
-            raise error.TestFail(
-                'Timed out on condition: %s' % exception.__str__())
-
     def run_once(self):
-        # Make sure that we don't have the initial browser window popping up in
-        # the middle of the test.
-        login.wait_for_initial_chrome_window()
-
-        self.autox = cros_ui.get_autox()
-
-        # Create a window and check that we switch to it.
-        win = self.autox.create_and_map_window(
-            width=200, height=200, title='test')
-        logging.info('Created first window 0x%x' % win.id)
-        info = self.autox.get_window_info(win.id)
-        self.__check_active_window(win.id, info)
-
-        # Create a second window.
-        win2 = self.autox.create_and_map_window(
-            width=200, height=200, title='test 2')
-        logging.info('Created second window 0x%x' % win2.id)
-        info2 = self.autox.get_window_info(win2.id)
-        self.__check_active_window(win2.id, info2)
+        autox = cros_ui.get_autox()
+        autox.await_condition(
+            lambda: autox.get_active_window_property(),
+            desc='Waiting for _NET_ACTIVE_WINDOW to be set');
