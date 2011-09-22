c874db4a912b61555a1848406c73571edddcfe52
Hung-Te Lin <hungte@chromium.org>
2011-09-22 14:28:34 +0800

    suite_Factory: change factory xauth file to cros default path
    Having the xauth file for factory using same path as system default make it
    easier to use other system services like HDMI.
    
    The comments for how to enabling HDMI is also removed - we should fix the system
    chrontel.conf instead of starting it inside factory initialization code.
    
    BUG=chrome-os-partner:6066
    TEST=start factory test image, see X & factory UI started
    
    Change-Id: I112b8f388ae86c68f148500c65b5deb0e250ba7e
    Reviewed-on: http://gerrit.chromium.org/gerrit/8122
    Reviewed-by: Nick Sanders <nsanders@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>

--- client/site_tests/suite_Factory/startx.sh
+++ client/site_tests/suite_Factory/startx.sh
@@ -5,7 +5,7 @@
 # found in the LICENSE file.
 
 XAUTH=/usr/bin/xauth
-XAUTH_FILE="/var/run/factory_ui.auth"
+XAUTH_FILE=/home/chronos/.Xauthority
 SERVER_READY=
 DISPLAY=":0"
 
@@ -29,14 +29,6 @@ done
 
 export DISPLAY=${DISPLAY}
 export XAUTHORITY=${XAUTH_FILE}
-
-# TODO : move this to ExtDisplay test
-# TODO : currently necessary as ch7036 stuff only launches if upstart ui but in
-# future it will be tied to udev
-#if [ -e /usr/bin/ch7036_monitor ] ; then
-# /sbin/modprobe i2c-dev
-#  /usr/bin/ch7036_monitor -v > /var/log/factory_ch7036_monitor.log &
-#fi
 
 /sbin/initctl emit factory-ui-started
 cat /proc/uptime > /tmp/uptime-x-started
