c10b0716005fe9ab8cd6695239bda39d90b04039
Tom Wai-Hong Tam <waihong@chromium.org>
2011-09-22 14:05:40 +0800

    Implement a test case for firmware B corruption.
    BUG=chromium-os:19710
    TEST=run_remote_tests.sh --remote=$REMOTE_IP -a \
         "servo_vid=0x18d1 servo_pid=0x5001" firmware_CorruptFwB
    
    Change-Id: If3b6da2701f950291d48a7d611bc5fd12a8acb09
    Reviewed-on: http://gerrit.chromium.org/gerrit/8121
    Commit-Ready: Tom Wai-Hong Tam <waihong@chromium.org>
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

---
+++ server/site_tests/firmware_CorruptFwB/firmware_CorruptFwB.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.server.cros.faftsequence import FAFTSequence
+
+
+class firmware_CorruptFwB(FAFTSequence):
+    """
+    Servo based firmware B corruption test.
+    """
+    version = 1
+
+
+    def confirm_fw_a_boot(self):
+        if not self.crossystem_checker({'mainfw_act': 'A', 'tried_fwb': '0'}):
+            self.faft_client.run_shell_command(
+                    'chromeos-firmwareupdate --mode recovery')
+            self.faft_client.software_reboot()
+            self.wait_for_client_offline()
+            self.wait_for_client()
+
+
+    def setup(self):
+        super(firmware_CorruptFwB, self).setup()
+        self.confirm_fw_a_boot()
+
+
+    def cleanup(self):
+        self.confirm_fw_a_boot()
+        super(firmware_CorruptFwB, self).cleanup()
+
+
+    def run_once(self, host=None):
+        self.register_faft_sequence((
+            {   # Step 1, expected firmware A boot and corrupt firmware B
+                'state_checker': (self.crossystem_checker, {
+                    'mainfw_act': 'A',
+                    'tried_fwb': '0',
+                }),
+                'userspace_action': (self.faft_client.corrupt_firmware, 'b'),
+            },
+            {   # Step 2, expected firmware A boot and set try_fwb flag
+                'state_checker': (self.crossystem_checker, {
+                    'mainfw_act': 'A',
+                    'tried_fwb': '0',
+                }),
+                'userspace_action': self.faft_client.set_try_fw_b,
+            },
+            {   # Step 3, expected firmware A boot and restore firmware B
+                'state_checker': (self.crossystem_checker, {
+                    'mainfw_act': 'A',
+                    'tried_fwb': '1',
+                }),
+                'userspace_action': (self.faft_client.restore_firmware, 'b'),
+            },
+            {   # Step 4, final check and done
+                'state_checker': (self.crossystem_checker, {
+                    'mainfw_act': 'A',
+                    'tried_fwb': '0',
+                }),
+            },
+        ))
+        self.run_faft_sequence()
