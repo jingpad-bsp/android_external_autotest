e8559171c135f436e6094cfb8c15f4ddf80b4a78
Tom Wai-Hong Tam <waihong@chromium.org>
2011-09-22 14:01:33 +0800

    Implement a test case for setting firmware B boot.
    BUG=chromium-os:19710
    TEST=run_remote_tests.sh --remote=$REMOTE_IP -a \
         "servo_vid=0x18d1 servo_pid=0x5001" firmware_TryFwB
    
    Change-Id: I360b794b74459e3241488de8aff2206026a7c38c
    Reviewed-on: http://gerrit.chromium.org/gerrit/8119
    Commit-Ready: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>

---
+++ server/site_tests/firmware_TryFwB/firmware_TryFwB.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.server.cros.faftsequence import FAFTSequence
+
+
+class firmware_TryFwB(FAFTSequence):
+    """
+    Servo based RW firmware B boot test.
+    """
+    version = 1
+
+    def confirm_fw_a_boot(self):
+        if not self.crossystem_checker({'mainfw_act': 'A', 'tried_fwb': '0'}):
+            self.faft_client.software_reboot()
+            self.wait_for_client_offline()
+            self.wait_for_client()
+
+
+    def setup(self):
+        super(firmware_TryFwB, self).setup()
+        self.confirm_fw_a_boot()
+
+
+    def cleanup(self):
+        self.confirm_fw_a_boot()
+        super(firmware_TryFwB, self).cleanup()
+
+
+    def run_once(self, host=None):
+        self.register_faft_sequence((
+            {   # Step 1, set fwb_tries flag
+                'state_checker': (self.crossystem_checker, {
+                    'mainfw_act': 'A',
+                    'tried_fwb': '0',
+                }),
+                'userspace_action': self.faft_client.set_try_fw_b,
+            },
+            {   # Step 2, expected firmware B boot, reboot
+                'state_checker': (self.crossystem_checker, {
+                    'mainfw_act': 'B',
+                    'tried_fwb': '1',
+                }),
+            },
+            {   # Step 3, expected firmware A boot, done
+                'state_checker': (self.crossystem_checker, {
+                    'mainfw_act': 'A',
+                    'tried_fwb': '0',
+                }),
+            },
+        ))
+        self.run_faft_sequence()
