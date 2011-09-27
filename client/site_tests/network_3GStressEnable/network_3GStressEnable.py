b29611e0e316f2acf433b32b5728668ece0a1d92
ttuttle <ttuttle@chromium.org>
2011-09-27 13:42:11 -0400

    autotest: 3GStressEnable: find wwan%d as well as usb%d
    When we switch to kernel 3.0, gobi modems are going to be (properly) named
    wwan%d instead of usb%d or eth%d; network_3GStressEnable was hard-coded to
    look for a usb%d-named modem.
    
    BUG=chromium-os:20842
    TEST=network_3GStressEnable passes with kernel-next
    
    Change-Id: I53465f934ade5234f5b0effa5e3d17d278024609
    Reviewed-on: http://gerrit.chromium.org/gerrit/8362
    Commit-Ready: ttuttle <ttuttle@chromium.org>
    Reviewed-by: ttuttle <ttuttle@chromium.org>
    Tested-by: ttuttle <ttuttle@chromium.org>

--- client/site_tests/network_3GStressEnable/network_3GStressEnable.py
+++ client/site_tests/network_3GStressEnable/network_3GStressEnable.py
@@ -18,28 +18,39 @@ class network_3GStressEnable(test.test):
         'org.chromium.flimflam.Error.InProgress'
     ]
 
-    def SetPowered(self, device, state):
+    def SetPowered(self, state):
         try:
-            device.SetProperty('Powered', dbus.Boolean(state))
+            self.device.SetProperty('Powered', dbus.Boolean(state))
         except dbus.exceptions.DBusException, err:
             if err._dbus_error_name in network_3GStressEnable.okerrors:
                 return
             else:
                 raise error.TestFailed(err)
 
-    def test(self, device, settle):
-        self.SetPowered(device, 1)
+    def test(self, settle):
+        self.SetPowered(1)
         time.sleep(settle)
-        self.SetPowered(device, 0)
+        self.SetPowered(0)
         time.sleep(settle)
 
-    def run_once(self, name='usb', cycles=3, min=15, max=25):
-        flim = flimflam.FlimFlam(dbus.SystemBus())
-        device = flim.FindElementByNameSubstring('Device', name)
-        if device is None:
-            device = flim.FindElementByPropertySubstring('Device', 'Interface',
-                                                         name)
-        service = flim.FindElementByNameSubstring('Service', 'cellular')
+    def FindDevice(self, names):
+        for name in names:
+            self.device = self.flim.FindElementByNameSubstring('Device',
+                                                               name)
+            if self.device:
+                return True
+            self.device = self.flim.FindElementByPropertySubstring('Device',
+                                                              'Interface',
+                                                               name)
+            if self.device:
+                return True
+        return False
+
+    def run_once(self, names=['usb', 'wwan'], cycles=3, min=15, max=25):
+        self.flim = flimflam.FlimFlam(dbus.SystemBus())
+        if not self.FindDevice(names):
+            raise error.TestFail('No device found.')
+        service = self.flim.FindElementByNameSubstring('Service', 'cellular')
         if service:
             # If cellular's already up, take it down to start.
             try:
@@ -51,10 +62,10 @@ class network_3GStressEnable(test.test):
                 if err._dbus_error_name != 'org.chromium.flimflam.'\
                                              'Error.InvalidService':
                     raise err
-            self.SetPowered(device, 0)
+            self.SetPowered(0)
         for t in xrange(max, min, -1):
             for n in xrange(cycles):
                 # deciseconds are an awesome unit.
                 print 'Cycle %d: %f seconds delay.' % (n, t / 10.0)
-                self.test(device, t / 10.0)
+                self.test(t / 10.0)
         print 'Done.'
