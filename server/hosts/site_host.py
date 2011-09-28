f7fcd6e5b6d89544e356a763f39eb8c55a8a1040
Chris Sosa <sosa@chromium.org>
2011-09-27 17:30:47 -0700

    Update site_host to have consistent values with remote_access.sh in crosutils.
    This updates our defaults to make our ssh connections more robust through
    testing.
    
    BUG=chromium-os:20427
    TEST=Ran cros_run_vm_test suite_Smoke with latest x86 test/vm image.
    
    Change-Id: I458e75cf767ddc6b3cdf2d9232ee8691ddef19da
    Reviewed-on: http://gerrit.chromium.org/gerrit/8393
    Commit-Ready: Chris Sosa <sosa@chromium.org>
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Tested-by: Chris Sosa <sosa@chromium.org>

--- server/hosts/site_host.py
+++ server/hosts/site_host.py
@@ -7,21 +7,27 @@ def make_ssh_command(user='root', port=22, opts='', hosts_file=None,
     """Override default make_ssh_command to use options tuned for Chrome OS.
 
     Tuning changes:
-      - ConnectTimeout=10; maximum of 10 seconds allowed for an SSH connection
-      failure.
+      - ConnectTimeout=30; maximum of 30 seconds allowed for an SSH connection
+      failure.  Consistency with remote_access.sh.
 
       - ServerAliveInterval=180; which causes SSH to ping connection every
       180 seconds. In conjunction with ServerAliveCountMax ensures that if the
       connection dies, Autotest will bail out quickly. Originally tried 60 secs,
       but saw frequent job ABORTS where the test completed successfully.
 
-      - ServerAliveCountMax=1; only allow a single keep alive failure.
+      - ServerAliveCountMax=3; consistency with remote_access.sh.
+
+      - ConnectAttempts=4; reduce flakiness in connection errors; consistency
+      with remote_access.sh.
 
       - UserKnownHostsFile=/dev/null; we don't care about the keys. Host keys
       change with every new installation, don't waste memory/space saving them.
+
+      - SSH protocol forced to 2; needed for ServerAliveInterval.
     """
     base_command = ('/usr/bin/ssh -a -x %s -o StrictHostKeyChecking=no'
                     ' -o UserKnownHostsFile=/dev/null -o BatchMode=yes'
-                    ' -o ConnectTimeout=10 -o ServerAliveInterval=180'
-                    ' -o ServerAliveCountMax=1 -l %s -p %d')
+                    ' -o ConnectTimeout=30 -o ServerAliveInterval=180'
+                    ' -o ServerAliveCountMax=3 -o ConnectionAttempts=4'
+                    ' -o Protocol=2 -l %s -p %d')
     return base_command % (opts, user, port)
