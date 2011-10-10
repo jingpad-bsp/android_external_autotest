56042e3b85bf1c609d711c0a7e8084ee692d48fb
Stuart Abercrombie <sabercrombie@chromium.org>
2011-10-10 14:52:05 -0700

    Restrict hardware_Resolution check to LVDS connections.
    The test was expecting all resolutions, regardless of connection type, to match one of 2 LVDS resolutions associated with existing products.  This produced a failure on Stumpy.
    
    BUG=chrome-os-partner:5858
    TEST=run on Alex and Stumpy
    
    Change-Id: I58e5078e242dc76859c26bcb3e4fc4df3950787e
    Reviewed-on: http://gerrit.chromium.org/gerrit/9815
    Tested-by: Stuart Abercrombie <sabercrombie@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Ready: Stuart Abercrombie <sabercrombie@chromium.org>

--- client/site_tests/hardware_Resolution/hardware_Resolution.py
+++ client/site_tests/hardware_Resolution/hardware_Resolution.py
@@ -6,7 +6,7 @@
 
 __author__ = 'kdlucas@chromium.org (Kelly Lucas)'
 
-import os
+import os, re
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
@@ -18,11 +18,9 @@ class hardware_Resolution(test.test):
     """
     version = 1
 
-    def get_resolution(self):
+    def get_xrandr_output(self):
         """
-        Get the current video resolution.
-        Returns:
-            string: represents the video resolution.
+        Retrieves the output of xrandr as a list of strings.
         """
         cmd = 'xrandr'
         # TODO:remove oldxauth when slim is deprecated.
@@ -38,8 +36,29 @@ class hardware_Resolution(test.test):
         environment = 'DISPLAY=:0.0 XAUTHORITY=%s' % xauth
         output = utils.system_output('%s %s' % (environment, cmd))
 
-        linesout = output.split('\n')
-        for line in linesout:
+        return output.split('\n')
+
+
+    def is_lvds_res(self, res, xrandr_output):
+        """
+        Returns True if the supplied resolution is associated with
+        an LVDS connection.
+        """
+        search_str = r'LVDS\d+ connected ' + res
+        for line in xrandr_output:
+            if re.match(search_str, line):
+                return True;
+
+        return False
+
+
+    def get_current_res(self, xrandr_output):
+        """
+        Get the current video resolution.
+        Returns:
+            string: represents the video resolution.
+        """
+        for line in xrandr_output:
             if 'Screen 0' in line:
                 sections = line.split(',')
                 for item in sections:
@@ -51,9 +70,13 @@ class hardware_Resolution(test.test):
 
 
     def run_once(self):
+        xrandr_output = self.get_xrandr_output()
 
-        supported_resolutions = ['1280x800', '1366x768']
-        res = self.get_resolution()
+        res = self.get_current_res(xrandr_output)
+        if not res or not re.match(r'\d+x\d+$', res):
+            raise error.TestFail('%s is not a valid resolution' % res)
 
-        if res not in supported_resolutions:
-            raise error.TestFail('%s is not a supported resoltuion' % res)
+        supported_lvds_resolutions = ['1280x800', '1366x768']
+        if self.is_lvds_res(res, xrandr_output) and \
+           res not in supported_lvds_resolutions:
+            raise error.TestFail('%s is not a supported LVDS resolution' % res)
