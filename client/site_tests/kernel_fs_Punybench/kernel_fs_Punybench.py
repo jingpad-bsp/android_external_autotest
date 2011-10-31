834665e7cb32c9ad6d7a053c7c62bcc5d8729169
Paul Taysom <taysom@google.com>
2011-10-31 13:20:17 -0700

    Added rest of performance benchmarks.
    Added in the performance benchmarks that are in the initial
    performance benchmark table.
    
    BUG=chromium-os:18097
    TEST=ran on alex
    
    Change-Id: Iee11dfe799dfd6dbedaa6805e303f3420f8448b0
    Reviewed-on: https://gerrit.chromium.org/gerrit/10918
    Tested-by: Paul Taysom <taysom@google.com>
    Reviewed-by: Mandeep Singh Baines <msb@chromium.org>
    Commit-Ready: Paul Taysom <taysom@google.com>

--- client/site_tests/kernel_fs_Punybench/kernel_fs_Punybench.py
+++ client/site_tests/kernel_fs_Punybench/kernel_fs_Punybench.py
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os, shutil, re
+import os, shutil, re, string
 from autotest_lib.client.bin import utils, test
 
 class kernel_fs_Punybench(test.test):
@@ -14,19 +14,43 @@ class kernel_fs_Punybench(test.test):
     version = 1
     Bin = '/usr/local/opt/punybench/bin/'
 
+
     def initialize(self):
         self.results = []
         self.job.drop_caches_between_iterations = True
+
 
     def run(self, cmd, args):
         return utils.system_output(
             os.path.join(self.Bin, cmd) + ' ' + args)
 
-    def threadtree(self):
-        result = self.run('threadtree', '-d /usr/local/_Dir -k 3')
-        r1 = re.search("timer avg= \d*.\d*.*$", result)
-        r2 = re.search("\d*\.\d*", r1.group())
-        self.write_perf_keyval({'threadtree': r2.group() + ' secs'})
+    def find_max(self, tag, text):
+        r1 = re.search(tag + ".*\n(\d.*\n)+", text)
+        r2 = re.search(r"(\d+\. \d+\.\d+.*\n)+", r1.group(0))
+        runs = string.split(r2.group(0), '\n')
+        max = 0.0
+        for r in runs:
+            a = re.search('\d+.\d+', r)
+            if (a):
+                b = float(a.group(0))
+                if (b > max):
+                    max = b
+        return max
+
+
+    def memcpy(self):
+        size = '0x4000000'
+        loops = '4'
+        iterations = '10'
+        args  = '-z' + size
+        args += ' -i' + iterations
+        args += ' -l' + loops
+        result = self.run('memcpy', args)
+
+        for tag in ['memcpy', '32bit', '64bit']:
+           max = self.find_max(tag, result)
+           self.write_perf_keyval({tag: max})
+
 
     def memcpy_test(self):
         result = self.run('memcpy_test', "")
@@ -34,6 +58,73 @@ class kernel_fs_Punybench(test.test):
         r2 = re.search("\d+\.\d+ MiB/s$", r1.group())
         self.write_perf_keyval({'L1cache': r2.group()})
 
+        r1 = re.search("L2 cache.*\n.*\n.*", result)
+        r2 = re.search("\d+\.\d+ MiB/s$", r1.group(0))
+        self.write_perf_keyval({'L2cache': r2.group()})
+
+        r1 = re.search("SDRAM.*\n.*\n.*", result)
+        r2 = re.search("\d+\.\d+ MiB/s$", r1.group(0))
+        self.write_perf_keyval({'SDRAM': r2.group()})
+
+
+    def threadtree(self):
+        directory = '/usr/local/_Dir'
+        iterations = '4'
+        tasks = '2'
+        width = '3'
+        depth = '5'
+        args  = '-d' + directory
+        args += ' -i' + iterations
+        args += ' -t' + tasks
+        args += ' -w' + width
+        args += ' -k' + depth
+
+        result = self.run('threadtree', args)
+        r1 = re.search("timer avg= \d*.\d*.*$", result)
+        r2 = re.search("\d*\.\d*", r1.group())
+        p = int(tasks) * pow(int(width), int(depth) + 1) / float(r2.group(0))
+        self.write_perf_keyval({'threadtree': p})
+
+
+    def uread(self):
+        file = '/usr/local/xyzzy'
+        size = '0x200000000'
+        loops = '4'
+        iterations = '1'
+        args = '-f' + file
+        args += ' -z' + size
+        args += ' -i' + iterations
+        args += ' -l' + loops
+        args += ' -b12'
+        result = self.run('uread', args)
+
+        r1 = re.search("timer avg=\d*.\d*.*$", result)
+        r2 = re.search("\d*\.\d*", r1.group(0))
+        p = int(size, 0) * int(iterations) / float(r2.group(0)) \
+            / 1024.0 / 1024.0
+        self.write_perf_keyval({'uread': p})
+
+
+    def ureadrand(self):
+        file = '/usr/local/xyzzy'
+        size = '0x200000000'
+        loops = '4'
+        iterations = '100000'
+        args = '-f' + file
+        args += ' -z' + size
+        args += ' -i' + iterations
+        args += ' -l' + loops
+        args += ' -b12'
+        result = self.run('ureadrand', args)
+
+        r1 = re.search("timer avg=\d*.\d*.*$", result)
+        r2 = re.search("\d*\.\d*", r1.group(0))
+        p =  4096.0 * int(iterations) / float(r2.group(0)) / 1024.0 / 1024.0
+        self.write_perf_keyval({'ureadrand': p})
+
     def run_once(self):
-        self.threadtree()
         self.memcpy_test()
+        self.memcpy()
+        self.threadtree()
+        self.uread()
+        self.ureadrand()
