28a3b17d6ceb777edee7842dcb2182a104955d0e
Taylor Hutt <thutt@chromium.org>
2011-11-01 12:23:41 -0700

    autotests: sound_infrastructure: Use data files rather than initialized data
    Details
    
      The size of the initialized data in this test is getting large
      enough, and requiring enough manual changes that it warrants making
      data files out of the control names and device files.
    
      This change moves all the codec-specific controls into their own
      data files, and places instructions in the source code on how to
      create the data files.
    
    Testing
    
      Ran test against each codec; all passed.
      Verified that each test is using the data file by modifying the file
      to have a non-existent control.
    
    BUG=chromium-os:22369
    TEST=See above
    
    Change-Id: I5f1a1a2fa6e771d63118862dc3102752062e5047
    Signed-off-by: Taylor Hutt <thutt@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/10983
    Reviewed-by: Terry Lambert <tlambert@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- client/tests/sound_infrastructure/sound_infrastructure.py
+++ client/tests/sound_infrastructure/sound_infrastructure.py
@@ -91,86 +91,7 @@ class sound_infrastructure(test.test):
             },
 
         'WM8903': {
-            'controls': [       # Produced from 'amixer controls'
-                "numid=1,iface=MIXER,name='Left Input PGA Switch'",
-                "numid=2,iface=MIXER,name='Left Input PGA Volume'",
-                "numid=3,iface=MIXER,name='Left Input PGA Common Mode Switch'",
-                "numid=4,iface=MIXER,name='Right Input PGA Switch'",
-                "numid=5,iface=MIXER,name='Right Input PGA Volume'",
-                "numid=6,iface=MIXER,name='Right Input PGA Common Mode Switch'",
-                "numid=7,iface=MIXER,name='ADC OSR'",
-                "numid=8,iface=MIXER,name='HPF Switch'",
-                "numid=9,iface=MIXER,name='HPF Mode'",
-                "numid=10,iface=MIXER,name='DRC Switch'",
-                "numid=11,iface=MIXER,name='DRC Compressor Slope R0'",
-                "numid=12,iface=MIXER,name='DRC Compressor Slope R1'",
-                "numid=13,iface=MIXER,name='DRC Compressor Threshold Volume'",
-                "numid=14,iface=MIXER,name='DRC Volume'",
-                "numid=15,iface=MIXER,name='DRC Minimum Gain Volume'",
-                "numid=16,iface=MIXER,name='DRC Maximum Gain Volume'",
-                "numid=17,iface=MIXER,name='DRC Attack Rate'",
-                "numid=18,iface=MIXER,name='DRC Decay Rate'",
-                "numid=19,iface=MIXER,name='DRC FF Delay'",
-                "numid=20,iface=MIXER,name='DRC Anticlip Switch'",
-                "numid=21,iface=MIXER,name='DRC QR Switch'",
-                "numid=22,iface=MIXER,name='DRC QR Threshold Volume'",
-                "numid=23,iface=MIXER,name='DRC QR Decay Rate'",
-                "numid=24,iface=MIXER,name='DRC Smoothing Switch'",
-                "numid=25,iface=MIXER,name='DRC Smoothing Hysteresis Switch'",
-                "numid=26,iface=MIXER,name='DRC Smoothing Threshold'",
-                "numid=27,iface=MIXER,name='DRC Startup Volume'",
-                "numid=28,iface=MIXER,name='Digital Capture Volume'",
-                "numid=29,iface=MIXER,name='ADC Companding Mode'",
-                "numid=30,iface=MIXER,name='ADC Companding Switch'",
-                "numid=31,iface=MIXER,name='Digital Sidetone Volume'",
-                "numid=32,iface=MIXER,name='DAC OSR'",
-                "numid=33,iface=MIXER,name='Digital Playback Volume'",
-                "numid=34,iface=MIXER,name='DAC Soft Mute Rate'",
-                "numid=35,iface=MIXER,name='DAC Mute Mode'",
-                "numid=36,iface=MIXER,name='DAC Mono Switch'",
-                "numid=37,iface=MIXER,name='DAC Companding Mode'",
-                "numid=38,iface=MIXER,name='DAC Companding Switch'",
-                "numid=39,iface=MIXER,name='Playback Deemphasis Switch'",
-                "numid=40,iface=MIXER,name='Headphone Switch'",
-                "numid=41,iface=MIXER,name='Headphone ZC Switch'",
-                "numid=42,iface=MIXER,name='Headphone Volume'",
-                "numid=43,iface=MIXER,name='Line Out Switch'",
-                "numid=44,iface=MIXER,name='Line Out ZC Switch'",
-                "numid=45,iface=MIXER,name='Line Out Volume'",
-                "numid=46,iface=MIXER,name='Speaker Switch'",
-                "numid=47,iface=MIXER,name='Speaker ZC Switch'",
-                "numid=48,iface=MIXER,name='Speaker Volume'",
-                "numid=49,iface=MIXER,name='Int Spk Switch'",
-                "numid=50,iface=MIXER,name='Right Speaker Mixer DACL Switch'",
-                "numid=51,iface=MIXER,name='Right Speaker Mixer DACR Switch'",
-                "numid=52,iface=MIXER,name='Right Speaker Mixer Left Bypass Switch'",
-                "numid=53,iface=MIXER,name='Right Speaker Mixer Right Bypass Switch'",
-                "numid=54,iface=MIXER,name='Left Speaker Mixer DACL Switch'",
-                "numid=55,iface=MIXER,name='Left Speaker Mixer DACR Switch'",
-                "numid=56,iface=MIXER,name='Left Speaker Mixer Left Bypass Switch'",
-                "numid=57,iface=MIXER,name='Left Speaker Mixer Right Bypass Switch'",
-                "numid=58,iface=MIXER,name='Right Output Mixer DACL Switch'",
-                "numid=59,iface=MIXER,name='Right Output Mixer DACR Switch'",
-                "numid=60,iface=MIXER,name='Right Output Mixer Left Bypass Switch'",
-                "numid=61,iface=MIXER,name='Right Output Mixer Right Bypass Switch'",
-                "numid=62,iface=MIXER,name='Left Output Mixer DACL Switch'",
-                "numid=63,iface=MIXER,name='Left Output Mixer DACR Switch'",
-                "numid=64,iface=MIXER,name='Left Output Mixer Left Bypass Switch'",
-                "numid=65,iface=MIXER,name='Left Output Mixer Right Bypass Switch'",
-                "numid=66,iface=MIXER,name='Right Playback Mux'",
-                "numid=67,iface=MIXER,name='Left Playback Mux'",
-                "numid=68,iface=MIXER,name='DACR Sidetone Mux'",
-                "numid=69,iface=MIXER,name='DACL Sidetone Mux'",
-                "numid=70,iface=MIXER,name='Right Capture Mux'",
-                "numid=71,iface=MIXER,name='Left Capture Mux'",
-                "numid=72,iface=MIXER,name='ADC Input'",
-                "numid=73,iface=MIXER,name='Right Input Mode Mux'",
-                "numid=74,iface=MIXER,name='Right Inverting Input Mux'",
-                "numid=75,iface=MIXER,name='Right Input Mux'",
-                "numid=76,iface=MIXER,name='Left Input Mode Mux'",
-                "numid=77,iface=MIXER,name='Left Inverting Input Mux'",
-                "numid=78,iface=MIXER,name='Left Input Mux'",
-                ],
+            'controls': [ ], # Create: amixer controls >WM8903.controls
             'files': [
                 '/dev/snd/by-path',
                 '/dev/snd/controlC0',
@@ -213,21 +134,7 @@ class sound_infrastructure(test.test):
             },
 
         'ALC271X': {
-            'controls': [
-                "numid=1,iface=MIXER,name='Master Playback Volume'",
-                "numid=2,iface=MIXER,name='Master Playback Switch'",
-                "numid=3,iface=MIXER,name='Speaker Playback Switch'",
-                "numid=4,iface=MIXER,name='Headphone Playback Switch'",
-                "numid=5,iface=MIXER,name='Capture Volume'",
-                "numid=6,iface=MIXER,name='Capture Switch'",
-                "numid=7,iface=MIXER,name='Mic Boost Volume'",
-                "numid=8,iface=MIXER,name='IEC958 Playback Con Mask'",
-                "numid=9,iface=MIXER,name='IEC958 Playback Pro Mask'",
-                "numid=10,iface=MIXER,name='IEC958 Playback Default'",
-                "numid=11,iface=MIXER,name='IEC958 Playback Switch'",
-                "numid=12,iface=MIXER,name='IEC958 Default PCM Playback Switch'",
-                "numid=13,iface=MIXER,name='PCM Playback Volume'",
-                ],
+            'controls': [ ], # Create: amixer controls >ALC271X.controls
             'files': [
                 "/dev/snd/by-path",
                 "/dev/snd/controlC0",
@@ -282,29 +189,7 @@ class sound_infrastructure(test.test):
             },
 
         'Cirrus Analog': {
-            'controls': [
-                "numid=1,iface=MIXER,name='Master Playback Switch'",
-                "numid=2,iface=MIXER,name='Master Playback Volume'",
-                "numid=21,iface=MIXER,name='PCM Playback Volume'",
-                "numid=8,iface=MIXER,name='Mic Capture Volume'",
-                "numid=6,iface=MIXER,name='Capture Switch'",
-                "numid=7,iface=MIXER,name='Capture Volume'",
-                "numid=9,iface=MIXER,name='IEC958 Playback Con Mask'",
-                "numid=13,iface=MIXER,name='IEC958 Playback Con Mask',index=1",
-                "numid=17,iface=MIXER,name='IEC958 Playback Con Mask',index=2",
-                "numid=10,iface=MIXER,name='IEC958 Playback Pro Mask'",
-                "numid=14,iface=MIXER,name='IEC958 Playback Pro Mask',index=1",
-                "numid=18,iface=MIXER,name='IEC958 Playback Pro Mask',index=2",
-                "numid=11,iface=MIXER,name='IEC958 Playback Default'",
-                "numid=15,iface=MIXER,name='IEC958 Playback Default',index=1",
-                "numid=19,iface=MIXER,name='IEC958 Playback Default',index=2",
-                "numid=12,iface=MIXER,name='IEC958 Playback Switch'",
-                "numid=16,iface=MIXER,name='IEC958 Playback Switch',index=1",
-                "numid=20,iface=MIXER,name='IEC958 Playback Switch',index=2",
-                "numid=3,iface=MIXER,name='HP/Speakers Playback Switch'",
-                "numid=4,iface=MIXER,name='HP/Speakers Playback Volume'",
-                "numid=5,iface=MIXER,name='Speaker Boost Playback Volume'",
-                ],
+            'controls': [ ], # Create: amixer controls >Cirrus_Analog.controls
             'files': [
                 '/dev/snd/by-path',
                 '/dev/snd/by-path/pci-0000:00:1b.0',
@@ -384,22 +269,7 @@ class sound_infrastructure(test.test):
             },
 
         'ALC272': {
-            'controls': [
-                "numid=1,iface=MIXER,name='Speaker Playback Volume'",
-                "numid=2,iface=MIXER,name='Speaker Playback Switch'",
-                "numid=3,iface=MIXER,name='Headphone Playback Volume'",
-                "numid=4,iface=MIXER,name='Headphone Playback Switch'",
-                "numid=5,iface=MIXER,name='Mic Playback Volume'",
-                "numid=6,iface=MIXER,name='Mic Playback Switch'",
-                "numid=7,iface=MIXER,name='Mic Boost Volume'",
-                "numid=8,iface=MIXER,name='Capture Switch'",
-                "numid=9,iface=MIXER,name='Capture Switch',index=1",
-                "numid=10,iface=MIXER,name='Capture Volume'",
-                "numid=11,iface=MIXER,name='Capture Volume',index=1",
-                "numid=12,iface=MIXER,name='Master Playback Volume'",
-                "numid=13,iface=MIXER,name='Master Playback Switch'",
-                "numid=14,iface=MIXER,name='PCM Playback Volume'",
-                ],
+            'controls': [ ], # Create: amixer controls >ALC272.controls
             'files': [
                     '/dev/snd/by-path',
                     '/dev/snd/by-path/pci-0000:00:1b.0',
@@ -467,6 +337,12 @@ class sound_infrastructure(test.test):
                 return c
         raise error.TestError('Unable to determine sound codec.')
 
+    def get_codec_basename(self, codec):
+        return codec.replace(' ', '_')
+
+    def get_data_pathname(self, filename):
+        return os.path.join(self.bindir, filename)
+
     def validate_files(self, files_list):
         for f in files_list:
             self.pathname_must_exist(f)
@@ -479,8 +355,17 @@ class sound_infrastructure(test.test):
         self.validate_files(codec['files'])
         self.validate_controls(codec['controls'])
 
+    def read_codec_data(self, codec):
+        codec_basename = self.get_codec_basename(codec)
+
+        # Read controls which must be present.
+        pathname = self.get_data_pathname(codec_basename + ".controls")
+        self.codec_info[codec]['controls'] = [line.strip() for line in
+                                              open(pathname)]
+
     def run_once(self):
         codec = self.get_codec()
+        self.read_codec_data(codec)
         if codec in self.codec_info:
             self.validate_codec(self.codec_info['ALL'])
             self.validate_codec(self.codec_info[codec])
