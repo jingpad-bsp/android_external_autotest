61f7891745a13d5773bdfcc41e005aab1329ab7f
Scott James Remnant <keybuk@chromium.org>
2011-11-09 10:42:11 -0800

    Revert "cros_ui_test: pass additional arguments to super initialize"
    This reverts commit 8d3f089e59b8543080042a87ddee09638324ca1f.
    
    Change-Id: Ifa6af5789a7f25b9a7692a71e4f4878706e2d7e7
    Reviewed-on: https://gerrit.chromium.org/gerrit/11393
    Reviewed-by: Scott James Remnant <keybuk@chromium.org>
    Tested-by: Scott James Remnant <keybuk@chromium.org>

--- client/cros/cros_ui_test.py
+++ client/cros/cros_ui_test.py
@@ -28,10 +28,9 @@ class UITest(pyauto_test.PyAutoTest):
     UITest.initialize for more details.
 
     If your subclass overrides the initialize() or cleanup() methods, it
-    should make sure to invoke this class' version of those methods as well,
-    and accept any unknown keyword arguments passing them to this class'
-    version. The standard super(...) function cannot be used for this,
-    since the base test class is not a 'new style' Python class.
+    should make sure to invoke this class' version of those methods as well.
+    The standard super(...) function cannot be used for this, since the base
+    test class is not a 'new style' Python class.
     """
     version = 1
 
@@ -201,7 +200,7 @@ class UITest(pyauto_test.PyAutoTest):
 
 
     def initialize(self, creds=None, is_creating_owner=False,
-                   extra_chrome_flags=[], **dargs):
+                   extra_chrome_flags=[]):
         """Overridden from test.initialize() to log out and (maybe) log in.
 
         If self.auto_login is True, this will automatically log in using the
@@ -227,7 +226,6 @@ class UITest(pyauto_test.PyAutoTest):
             is_creating_owner: If the test case is creating a new device owner.
             extra_chrome_flags: Extra chrome flags to pass to chrome, if any.
 
-        Unknown arguments are passed to the super class' initialize method.
         """
         # Mark /var/log/messages now; we'll run through all subsequent
         # log messages at the end of the test and log info about processes that
@@ -265,8 +263,7 @@ class UITest(pyauto_test.PyAutoTest):
         cros_ui.start()
 
         pyauto_test.PyAutoTest.initialize(self, auto_login=False,
-                                          extra_chrome_flags=extra_chrome_flags,
-                                          **dargs)
+                                          extra_chrome_flags=extra_chrome_flags)
         if self.auto_login:
             self.login(self.username, self.password)
             if is_creating_owner:
