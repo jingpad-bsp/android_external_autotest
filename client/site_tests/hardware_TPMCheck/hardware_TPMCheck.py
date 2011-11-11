c5bc881c1bef35b2e742014d34cee634d482df2c
Luigi Semenzato <semenzato@chromium.org>
2011-11-11 15:46:52 -0800

    Require that all systems with a valid firmware version support the TPM.
    BUG=chrome-os-partner:6820
    TEST=run this test on a Lumpy
    
    Change-Id: I456c8e073494df2c4e9a08cb6748800361968939
    Reviewed-on: https://gerrit.chromium.org/gerrit/11566
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Bill Richardson <wfrichar@chromium.org>
    Commit-Ready: Luigi Semenzato <semenzato@chromium.org>

--- client/site_tests/hardware_TPMCheck/hardware_TPMCheck.py
+++ client/site_tests/hardware_TPMCheck/hardware_TPMCheck.py
@@ -6,21 +6,12 @@ import logging, os, re
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 
-def old_or_missing_firmware_version():
+def missing_firmware_version():
     f = os.popen("crossystem fwid")
     if not f:
         return True
-    version = f.readline().strip()
     f.close()
-    logging.info("firmware version: %s", version)
-    # Expect a dot-separated list of 6 elements.  Discard 1st element.
-    v = re.split("\.", version)[1:]
-    w = re.split("\.", "any-nickname.03.60.1118.0036.")[1:]
-    if len(v) == 4:
-        return False  # assume ARM platform, or other newer firmware
-    if len(v) != len(w):
-        raise error.TestError("malformed firmware version %s" % version)
-    return v < w
+    return False
 
 def dict_from_command(command):
     dict = {}
@@ -52,9 +43,8 @@ class hardware_TPMCheck(test.test):
 
     def run_once(self):
 
-        if old_or_missing_firmware_version():
-            logging.warning("skipping test because firmware " +
-                            "version missing or deemed too old")
+        if missing_firmware_version():
+            logging.warning("no firmware version, skipping test")
             return
 
         try:
