05d24a654698c75467990016d08cda8e5ef847d3
David Rochberg <rochberg@chromium.org>
2011-11-18 14:21:32 -0500

    cellular:  Throughput test for emulated cell networks
    Uses iperf to test TCP throughput
    
    Future work:
           run over all technologies supported by the modem
           pass/fail based on throughput
    
    TEST=This is a test.  It passes on Gobi2k.
    BUG=chromium-os:22298
    Change-Id: I2d55c21361d233a08c4889398fafe4bc0ad654d4
    Reviewed-on: https://gerrit.chromium.org/gerrit/11925
    Reviewed-by: Jason Glasgow <jglasgow@chromium.org>
    Commit-Ready: David Rochberg <rochberg@chromium.org>
    Tested-by: David Rochberg <rochberg@chromium.org>

---
+++ client/site_tests/cellular_Throughput/cellular_Throughput.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import logging, time
+
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.cros import backchannel, iperf, network
+
+from autotest_lib.client.cros.cellular import cellular, cell_tools
+from autotest_lib.client.cros.cellular import emulator_config, labconfig
+
+from autotest_lib.client.cros import flimflam_test_path
+import flimflam
+
+
+class cellular_Throughput(test.test):
+    version = 1
+
+    def run_once(self, config):
+        with backchannel.Backchannel():
+            flim = flimflam.FlimFlam()
+            with cell_tools.OtherDeviceShutdownContext('cellular', flim):
+                bs, verifier = emulator_config.GetDefaultBasestation(
+                    config, cellular.Technology.WCDMA)
+                network.ResetAllModems(flim)
+                # TODO(rochberg): Figure out whether it's just Gobi 2k
+                # that requires this or all modems
+                time.sleep(10)
+
+                (service, _) = cell_tools.ConnectToCellular(flim, verifier)
+                cell_tools.CheckHttpConnectivity(config)
+
+                # The control file has started iperf at this address
+                perftarget = config['perfserver']['rf_address']
+                (client, perf) = iperf.BuildClientCommand(perftarget)
+                iperf_output = utils.system_output(client, retain_output=True)
+
+                # Add in conditions from BuildClientCommand
+                perf.update(iperf.ParseIperfOutput(iperf_output))
+                cell_tools.DisconnectFromCellularService(bs, flim, service)
+                self.write_perf_keyval(perf)
