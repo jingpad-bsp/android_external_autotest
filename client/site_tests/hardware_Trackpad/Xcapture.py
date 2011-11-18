d04a0d9ecf95ad28bdc41a82a0a2f2270cf9753b
Joseph Hwang <josephsih@chromium.org>
2011-11-18 15:55:34 +0800

    hardware_Trackpad: use mtplot to display device files
    Use the colorful mtplot to display finger tracking when playing
    back the trackpad device files instead of using an empty screen.
    
    BUG=chromium-os:22569
    TEST=hardware_Trackpad autotest should run normally.
    
    Change-Id: Ib436b4d49eaa9aaf9ca1eefb6af2cd13f64a2a82
    Reviewed-on: https://gerrit.chromium.org/gerrit/15085
    Commit-Ready: Joseph Shyh-In Hwang <josephsih@chromium.org>
    Reviewed-by: Joseph Shyh-In Hwang <josephsih@chromium.org>
    Tested-by: Joseph Shyh-In Hwang <josephsih@chromium.org>

--- client/site_tests/hardware_Trackpad/Xcapture.py
+++ client/site_tests/hardware_Trackpad/Xcapture.py
@@ -6,14 +6,18 @@
 
 import logging
 import os
+import re
 import subprocess
 import tempfile
 import time
 
+import common_util
+import trackpad_util
+
 from trackpad_util import Display, read_trackpad_test_conf
 
 
-def create_popup(display, ax):
+def create_popup_window(display, ax):
     ''' Create a popup window with override-redirect so that we can
         manipulate its geometry.
     '''
@@ -50,6 +54,110 @@ def create_popup(display, ax):
     return popup_win
 
 
+class Mtplot:
+    ''' Create a mtplot window for Xcapture to listen to '''
+
+    def __init__(self, display):
+        display.set_environ()
+        self._create_mtplot_window(display)
+        self.id = self._get_mtplot_win_id()
+        if self.id is None:
+            raise error.TestError('Failure on deriving window id in focus.')
+
+    def _create_mtplot_window(self, display):
+        trackpad_device_file, msg = trackpad_util.get_trackpad_device_file()
+        mtplot_cmd = 'mtplot %s' % trackpad_device_file
+        self.null_file = open('/dev/null')
+
+        try:
+            self.proc = subprocess.Popen(mtplot_cmd.split(),
+                                         stdout=self.null_file)
+        except:
+            err_msg = 'Cannot start program: %s' % self.mtplot_cmd
+            raise self.error.TestError(err_msg)
+
+        time.sleep(0.1)
+        if self.proc.poll() is not None:
+            raise error.TestError('Failure on "%s" [%d]' %
+                                  (self.mtplot_cmd, self.proc.returncode))
+
+        logging.info('"%s" has been launched.' % mtplot_cmd)
+
+    def _get_mtplot_win_id(self):
+        ''' Use xwininfo to make sure that the focus window is not Chrome '''
+        count = 0
+        max_count = 50
+        while count <= max_count:
+            focus_win_id = self._get_focus_window_id()
+            if (self._is_full_screen(focus_win_id) and
+                not self._is_chrome(focus_win_id)):
+                return int(focus_win_id, 16)
+            logging.info('    Waiting mtplot window to get in focus (%d)...' %
+                         count)
+            time.sleep(0.1)
+            count += 1
+        raise error.TestError('Fail to get mtplot window in focus')
+
+    def _is_chrome(self, win_id):
+        ''' Use xwininfo to get window name '''
+        cmd = 'xwininfo -root -tree | grep %s' % win_id
+        win_info = common_util.simple_system_output(cmd)
+        # The win_info for a chrome browser looks like
+        #     0x600016 "chrome": ("chrome" "Chrome")  1366x768+-1366+0 +-1366+0
+        if win_info is not None:
+            res = re.search('chrome', win_info, re.I)
+            if res is not None:
+                return True
+        return False
+
+    def _is_full_screen(self, win_id):
+        ''' Does the window occupy the whole screen? '''
+        cmd = 'xwininfo -root -tree | grep %s' % win_id
+        win_info = common_util.simple_system_output(cmd)
+        full_screen_dim = self._get_screen_dimensions()
+        if full_screen_dim is None:
+            raise error.TestError('Failure on deriving screen dimensions.')
+
+        # The win_info for a chrome browser looks like
+        #     0x600016 "chrome": ("chrome" "Chrome")  1366x768+-1366+0 +-1366+0
+        if win_info is not None:
+            res = re.search(full_screen_dim, win_info, re.I)
+            if res is not None:
+                return True
+        return False
+
+    def _get_screen_dimensions(self):
+        ''' Use xdpyinfo to derive the screen dimensions such as 1366x768 '''
+        cmd = 'xdpyinfo | grep dimensions'
+        dim = common_util.simple_system_output(cmd)
+        if dim is not None:
+            # An example output:
+            #   "  dimensions:    1366x768 pixels (361x203 millimeters)"
+            res = re.search('\s*dimensions:\s*(\d+)x(\d+)\s*pixels', dim, re.I)
+            if res is not None:
+                return '%sx%s' % (res.group(1), res.group(2))
+        return None
+
+
+    def _get_focus_window_id(self):
+        ''' Use xdpyinfo to derive the window id currently in focus '''
+        cmd = 'xdpyinfo | grep focus'
+        win_id = common_util.simple_system_output(cmd)
+        if win_id is not None:
+            # An example output: "focus:  window 0x600017, revert to Parent"
+            res = re.search('focus:\s+window\s+0x(\w+),', win_id, re.I)
+            if res is not None:
+                return res.group(1)
+        return None
+
+    def destroy(self):
+        ''' Destroy the mtplot process. '''
+        self.proc.terminate()
+        self.proc.kill()
+        self.proc.wait()
+        self.null_file.close()
+
+
 class Xcapture:
     ''' A class to capture X events '''
 
@@ -77,11 +185,15 @@ class Xcapture:
         self.fd_all = tempfile.NamedTemporaryFile()
         self.xcapture_file_all = self.fd_all.name
 
-        # Create a popup window to listen to the X events
-        self.popup_win = create_popup(self.display, self.ax)
+        # Create a window to listen to the X events
+        mtplot_gui = read_trackpad_test_conf('mtplot_gui', conf_path)
+        if mtplot_gui:
+            self.win = Mtplot(self.display)
+        else:
+            self.win = create_popup_window(self.display, self.ax)
 
         # Launch the capture process
-        self.xcapture_cmd = 'xev -id 0x%x' % int(self.popup_win.id)
+        self.xcapture_cmd = 'xev -id 0x%x' % int(self.win.id)
         self._launch(self.fd_all)
 
         logging.info('X events will be saved in %s' % self.xcapture_dir)
@@ -179,5 +291,5 @@ class Xcapture:
         self.proc.wait()
 
         # Destroy the popup window
-        self.popup_win.destroy()
+        self.win.destroy()
         self.ax.sync()
