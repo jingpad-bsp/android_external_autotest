804c1df2241bdca3c769155ffcfa75283e8ac67c
David Rochberg <rochberg@chromium.org>
2011-11-18 11:44:10 -0500

    cellular:  Add a context for iptables manipulation
    This object lets you add iptables rules to a host and then cleans them
    up automatically (if you use it with a 'with' clause).  It is for
    future cell autotests testing incoming connections.
    
    Based on code from site_wifitest.py
    
    BUG=chromium-os:22298
    TEST=manual, including trying to add the same rule twice
    
    Change-Id: I6036a842e1baaced8b78ae073809313ba72c1e4f
    Reviewed-on: https://gerrit.chromium.org/gerrit/11920
    Reviewed-by: David Rochberg <rochberg@chromium.org>
    Tested-by: David Rochberg <rochberg@chromium.org>
    Commit-Ready: David Rochberg <rochberg@chromium.org>

--- client/cros/network.py
+++ client/cros/network.py
@@ -4,6 +4,7 @@
 
 import logging
 from autotest_lib.client.cros import flimflam_test_path
+from autotest_lib.client.common_lib import utils
 import flimflam, mm
 
 
@@ -37,3 +38,44 @@ def ClearGobiModemFaultInjection():
     gobi = modem_manager.GobiModem(gobi_path)
     if gobi:
         gobi.InjectFault('ClearFaults',1);
+
+
+class IpTablesContext(object):
+    """Context manager that manages iptables rules."""
+    IPTABLES='/sbin/iptables'
+
+    def __init__(self):
+        self.rules = []
+
+    def _IpTables(self, command):
+        # Run, log, return output
+        return utils.system_output('%s %s' % (self.IPTABLES, command),
+                                   retain_output=True)
+
+    def _RemoveRule(self, rule):
+        self._IpTables('-D ' + rule)
+        self.rules.remove(rule)
+
+    def AllowHost(self, host):
+        for proto in ['tcp', 'udp']:
+            rule = 'INPUT -s %s/32 -p %s -m %s -j ACCEPT' % (host, proto, proto)
+            output = self._IpTables('-S INPUT')
+            current = [x.rstrip() for x in output.splitlines()]
+            logging.error('current: %s' % current)
+            if '-A ' + rule in current:
+                # Already have the rule
+                logging.info('Not adding redundant %s' % rule)
+                continue
+            self._IpTables('-A '+ rule)
+            self.rules.append(rule)
+
+    def _CleanupRules(self):
+        for rule in self.rules:
+            self._RemoveRule(rule)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception, value, traceback):
+        self._CleanupRules()
+        return False
