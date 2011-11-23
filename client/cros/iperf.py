326fd856546af2cf81bed808eb413d4b4699c88a
David Rochberg <rochberg@chromium.org>
2011-11-23 15:19:40 -0500

    Add bidirectional (tradeoff) operation to iperf.
    Increase unit test coverage.
    
    BUG=chromium-os:23392
    TEST=unit tests, cellular_Throughput
    
    Change-Id: I740a538314def91262cd53fcdce844cfbd267bbc
    Reviewed-on: https://gerrit.chromium.org/gerrit/12085
    Reviewed-by: David Rochberg <rochberg@chromium.org>
    Tested-by: David Rochberg <rochberg@chromium.org>
    Commit-Ready: David Rochberg <rochberg@chromium.org>

--- client/cros/iperf.py
+++ client/cros/iperf.py
@@ -54,6 +54,9 @@ def BuildClientCommand(server, params=None, command="/usr/local/bin/iperf"):
         client_args += ' -b %s' % params['bandwidth']
         perf['bandwidth'] = params['bandwidth']
 
+    if 'tradeoff' in params:
+        client_args += ' --tradeoff'
+
     return (command + client_args, perf)
 
 
@@ -62,12 +65,107 @@ def BuildServerCommand(params, command="/usr/bin/iperf"):
     return command + ' -s' + common_args
 
 
-MULTIPLIERS={
-    'Kbits/sec': 1.0e3,
-    'Mbits/sec': 1.0e6,
-    'Gbits/sec': 1.0e9,
-}
+def ApplyMultiplier(quantity, multiplier):
+    """Given a quantity and multiplier of 'Xbits/sec', return bits/sec."""
 
+    MULTIPLIERS={
+        'bits/sec': 1.0e0,
+        'Kbits/sec': 1.0e3,
+        'Mbits/sec': 1.0e6,
+        'Gbits/sec': 1.0e9,
+    }
+
+    if multiplier not in MULTIPLIERS:
+        raise Error('Could not parse multiplier %s' % multiplier)
+    return float(quantity) * MULTIPLIERS[multiplier]
+
+
+def _ParseOneTcpLine(line):
+    """Parses a line of TCP output, returns bandwidth.
+    Args:
+        line:  a line like "[  3]  0.0- 5.2 sec  0.06 MBytes  0.10 Mbits/sec"
+    """
+    tcp_tokens = line.split()
+    if len(tcp_tokens) >= 6 and 'bits/sec' in tcp_tokens[-1]:
+        return ApplyMultiplier(tcp_tokens[-2], tcp_tokens[-1])
+    else:
+        raise Error('Could not parse TCP line')
+
+
+def _ParseTcpOutput(lines):
+    """Parses the following and returns a single dictionary with
+    uplink throughput, and, if available, downlink throughput.
+
+    ------------------------------------------------------------
+    Client connecting to localhost, TCP port 5001
+    TCP window size: 49.4 KByte (default)
+    ------------------------------------------------------------
+    [  3] local 127.0.0.1 port 57936 connected with 127.0.0.1 port 5001
+    [ ID] Interval       Transfer     Bandwidth
+    [  3]  0.0-10.0 sec  2.09 GBytes  1.79 Gbits/sec
+    """
+    perf = {}
+
+    uplink = _ParseOneTcpLine(lines[6])
+    perf['uplink_throughput'] = uplink
+    if len(lines) > 13:
+        perf['downlink_throughput'] = _ParseOneTcpLine(lines[13])
+
+    return perf
+
+def _ParseOneUdpLine(line, prefix):
+    udp_tokens = line.replace('/', ' ').split()
+    # Find the column ending with "...Bytes"
+    mb_col = [col for col, data in enumerate(udp_tokens)
+              if data.endswith('Bytes')]
+
+    if len(mb_col) > 0 and len(udp_tokens) >= mb_col[0] + 9:
+        # Make a sublist starting after the column named "MBytes"
+        stat_tokens = udp_tokens[mb_col[0]+1:]
+        # Rebuild Mbits/sec out of Mbits sec
+        multiplier = '%s/%s' % tuple(stat_tokens[1:3])
+        return {prefix + 'throughput':
+                    ApplyMultiplier(stat_tokens[0], multiplier),
+                prefix + 'jitter':float(stat_tokens[3]),
+                prefix + 'lost':float(stat_tokens[7].strip('()%'))}
+    else:
+        raise Error('Could not parse UDP line: %s' % line)
+
+
+
+def _ParseUdpOutput(lines):
+    """Parses iperf output, returns throughput, jitter, and loss.
+------------------------------------------------------------
+Client connecting to 172.31.206.152, UDP port 5001
+Sending 1470 byte datagrams
+UDP buffer size:   110 KByte (default)
+------------------------------------------------------------
+[  3] local 172.31.206.145 port 39460 connected with 172.31.206.152 port 5001
+[ ID] Interval       Transfer     Bandwidth
+[  3]  0.0- 3.0 sec    386 KBytes  1.05 Mbits/sec
+[  3] Sent 269 datagrams
+[  3] Server Report:
+[ ID] Interval       Transfer     Bandwidth       Jitter   Lost/Total Datagrams
+[  3]  0.0- 3.0 sec    386 KBytes  1.05 Mbits/sec  0.010 ms    1/  270 (0.37%)
+------------------------------------------------------------
+Server listening on UDP port 5001
+Receiving 1470 byte datagrams
+UDP buffer size:   110 KByte (default)
+------------------------------------------------------------
+[  3] local 172.31.206.145 port 5001 connected with 172.31.206.152 port 57416
+[ ID] Interval       Transfer     Bandwidth       Jitter   Lost/Total Datagrams
+[  3]  0.0- 3.0 sec    386 KBytes  1.05 Mbits/sec  0.085 ms    1/  270 (0.37%)
+"""
+    byte_lines = [line for line in lines
+                  if 'Bytes' in line]
+    if len(byte_lines) < 2 or len(byte_lines) > 3:
+        raise Error('Wrong number of byte report lines: %d' % len(byte_lines))
+
+    out = _ParseOneUdpLine(byte_lines[1], 'uplink_')
+    if len(byte_lines) > 2:
+        out.update(_ParseOneUdpLine(byte_lines[2], 'downlink_'))
+
+    return out
 
 def ParseIperfOutput(input):
     if not isinstance(input, list):
@@ -90,63 +188,9 @@ def ParseIperfOutput(input):
     else:
         raise Error('Could not parse header line %s' % lines[1])
 
-    perf = {}
-
     if protocol == 'TCP':
-        """Parses the following and returns a single element dictionary
-        with throughput value.
-
-        ------------------------------------------------------------
-        Client connecting to localhost, TCP port 5001
-        TCP window size: 49.4 KByte (default)
-        ------------------------------------------------------------
-        [  3] local 127.0.0.1 port 57936 connected with 127.0.0.1 port 5001
-        [ ID] Interval       Transfer     Bandwidth
-        [  3]  0.0-10.0 sec  2.09 GBytes  1.79 Gbits/sec
-        """
-        tcp_tokens = lines[6].split()
-        if len(tcp_tokens) >= 6 and tcp_tokens[-1] in MULTIPLIERS:
-            return {'throughput':
-                        MULTIPLIERS[tcp_tokens[-1]] * float(tcp_tokens[-2])}
-        else:
-            raise Error('Could not parse throughput line: %s' % lines[6])
-
+        return _ParseTcpOutput(lines)
     elif protocol == 'UDP':
-        """Parses the following and returns a dictionary of performance values.
-
-        ------------------------------------------------------------
-        Client connecting to localhost, UDP port 5001
-        Sending 1470 byte datagrams
-        UDP buffer size:   108 KByte (default)
-        ------------------------------------------------------------
-        [  3] local 127.0.0.1 port 54244 connected with 127.0.0.1 port 5001
-        [ ID] Interval       Transfer     Bandwidth
-        [  3]  0.0-10.0 sec  1.25 MBytes  1.05 Mbits/sec
-        [  3] Sent 893 datagrams
-        [  3] Server Report:
-        [ ID] Interval       Transfer     Bandwidth       Jitter   Lost/Total Da
-        [  3]  0.0-10.0 sec  1.25 MBytes  1.05 Mbits/sec  0.032 ms 1/894 (0.11%)
-        [  3]  0.0-15.0 sec  14060 datagrams received out-of-order
-        """
-        # Search for the last row containing the word 'Bytes'
-        mb_row = [row for row,data in enumerate(lines)
-                  if 'Bytes' in data][-1]
-        udp_tokens = lines[mb_row].replace('/', ' ').split()
-        # Find the column ending with "...Bytes"
-        mb_col = [col for col,data in enumerate(udp_tokens)
-                  if data.endswith('Bytes')]
-        if len(mb_col) > 0 and len(udp_tokens) >= mb_col[0] + 9:
-            # Make a sublist starting after the column named "MBytes"
-            stat_tokens = udp_tokens[mb_col[0]+1:]
-            # Rebuild Mbits/sec out of Mbits sec
-            multiplier = '%s/%s' % tuple(stat_tokens[1:3])
-            if multiplier not in MULTIPLIERS:
-                raise Error('Could not parse multiplier in %s' % mb_row)
-            return {'throughput':
-                        float(stat_tokens[0]) * MULTIPLIERS[multiplier],
-                    'jitter':float(stat_tokens[3]),
-                    'lost':float(stat_tokens[7].strip('()%'))}
-        else:
-            raise Error('Could not parse UDP test output: %s' % lines)
+        return _ParseUdpOutput(lines)
     else:
         raise Error('Unhandled protocol %s' % lines)
--- client/cros/iperf_unittest.py
+++ client/cros/iperf_unittest.py
@@ -74,10 +74,50 @@ UDP buffer size: 0.11 MByte (default)
 [ ID] Interval       Transfer     Bandwidth       Jitter   Lost/Total Datagrams
 [  3]  0.0- 5.2 sec  0.06 MBytes  0.10 Mbits/sec  0.014 ms    1/   45 (2.2%)
 """.lstrip().rstrip()
-        self.assertEqual({'jitter': 0.014,
-                          'throughput': 100000,
-                          'lost': 2.2},
+        expected = {'jitter': 0.014,
+                    'throughput': 100000.0,
+                    'lost': 2.2}
+        expected.update(dict([('uplink_' + a, b)
+                              for (a,b) in expected.items()]))
+        self.assertEqual(expected,
                          iperf.ParseIperfOutput(text))
+
+
+    def test_udp_tradeoff(self):
+        text = """------------------------------------------------------------
+Client connecting to 172.31.206.152, UDP port 5001
+Sending 1470 byte datagrams
+UDP buffer size: 0.11 MByte (default)
+------------------------------------------------------------
+[  3] local 172.31.206.145 port 51532 connected with 172.31.206.152 port 5001
+[ ID] Interval       Transfer     Bandwidth
+[  3]  0.0- 5.2 sec  0.06 MBytes  0.10 Mbits/sec
+[  3] Sent 44 datagrams
+[  3] Server Report:
+[ ID] Interval       Transfer     Bandwidth       Jitter   Lost/Total Datagrams
+[  3]  0.0- 5.2 sec  0.06 MBytes  0.10 Mbits/sec  0.014 ms    1/   45 (2.2%)
+------------------------------------------------------------
+Server listening on UDP port 5001
+Receiving 1470 byte datagrams
+UDP buffer size:   110 KByte (default)
+------------------------------------------------------------
+[  3] local 172.31.206.145 port 5001 connected with 172.31.206.152 port 57416
+[ ID] Interval       Transfer     Bandwidth       Jitter   Lost/Total Datagrams
+[  3]  0.0- 3.0 sec    386 KBytes  1.05 Mbits/sec  0.085 ms    1/  270 (0.37%)
+""".lstrip().rstrip()
+        expected = {'jitter': 0.014,
+                    'throughput': 100000.0,
+                    'lost': 2.2}
+        expected.update(dict([('uplink_' + a, b)
+                              for (a,b) in expected.items()]))
+        expected.update({'downlink_throughput': 1.05e6,
+                         'downlink_jitter': 0.085,
+                         'downlink_lost': 0.37})
+
+        self.assertEqual(expected,
+                         iperf.ParseIperfOutput(text))
+
+
 
     def test_tcp(self):
         text = """------------------------------------------------------------
@@ -87,17 +127,37 @@ TCP window size: 0.02 MByte (default)
 [  3] local 172.31.206.145 port 38376 connected with 172.31.206.152 port 5001
 [ ID] Interval       Transfer     Bandwidth
 [  3]  0.0- 3.0 sec  34.1 MBytes  95.4 Mbits/sec""".lstrip().rstrip()
-        self.assertEqual({'throughput': 95.4e6}, iperf.ParseIperfOutput(text))
-
-    def test_K(self):
-         text = """------------------------------------------------------------
+        self.assertEqual({'throughput': 95.4e6,
+                          'uplink_throughput': 95.4e6},
+                         iperf.ParseIperfOutput(text))
+    def test_tcp_tradeoff(self):
+        text = """------------------------------------------------------------
 Client connecting to 172.31.206.152, TCP port 5001
 TCP window size: 0.02 MByte (default)
 ------------------------------------------------------------
 [  3] local 172.31.206.145 port 38376 connected with 172.31.206.152 port 5001
 [ ID] Interval       Transfer     Bandwidth
-[  3]  0.0- 3.0 sec  34.1 MBytes  95.4 Kbits/sec""".lstrip().rstrip()
-         self.assertEqual({'throughput': 95400}, iperf.ParseIperfOutput(text))
+[  3]  0.0- 3.0 sec  34.1 MBytes  95.4 Mbits/sec
+------------------------------------------------------------
+Server listening on TCP port 5001
+TCP window size: 85.3 KByte (default)
+------------------------------------------------------------
+[  4] local 172.31.206.145 port 5001 connected with 172.31.206.152 port 56542
+[ ID] Interval       Transfer     Bandwidth
+[  4]  0.0- 3.0 sec  34.1 MBytes  94.7 Mbits/sec""".lstrip().rstrip()
+        self.assertEqual({'throughput': 95.4e6,
+                          'uplink_throughput': 95.4e6,
+                          'downlink_throughput': 94.7e6},
+                         iperf.ParseIperfOutput(text))
+
+
+    def test_Multipliers(self):
+        self.assertEqual(1, iperf.ParseMultiplier(1, 'bits/sec'))
+        self.assertEqual(1, iperf.ParseMultiplier('1', 'bits/sec'))
+        self.assertEqual(1000, iperf.ParseMultiplier('1', 'Kbits/sec'))
+        self.assertEqual(1000000, iperf.ParseMultiplier('1', 'Mbits/sec'))
+        self.assertEqual(1000000000, iperf.ParseMultiplier('1', 'Gbits/sec'))
+        self.assertEqual(1000000000, iperf.ParseMultiplier('1000', 'Mbits/sec'))
 
 
 if __name__ == '__main__':
