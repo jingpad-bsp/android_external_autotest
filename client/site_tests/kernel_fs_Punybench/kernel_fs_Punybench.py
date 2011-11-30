cff4ed0f600dc7f73322714548a25b1efdbb4a32
Paul Taysom <taysom@google.com>
2011-11-30 14:43:19 -0800

    Fixed uread results.
    BUG=chromium-os:23601
    TEST=control.kernel_fs_performance
    
    Change-Id: I4f87275bc8f1f6cd77fc032cf228b497d71ed510
    Reviewed-on: https://gerrit.chromium.org/gerrit/12281
    Commit-Ready: Paul Taysom <taysom@google.com>
    Reviewed-by: Paul Taysom <taysom@google.com>
    Tested-by: Paul Taysom <taysom@google.com>

--- client/site_tests/kernel_fs_Punybench/kernel_fs_Punybench.py
+++ client/site_tests/kernel_fs_Punybench/kernel_fs_Punybench.py
@@ -4,13 +4,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 import optparse
 import os, shutil, re, string
 from autotest_lib.client.bin import utils, test
 
 class kernel_fs_Punybench(test.test):
-    """
-    Run selected puny benchmarks
+    """Run a selected subset of the puny benchmarks
     """
     version = 1
     Bin = '/usr/local/opt/punybench/bin/'
@@ -21,54 +21,80 @@ class kernel_fs_Punybench(test.test):
         self.job.drop_caches_between_iterations = True
 
 
-    def run(self, cmd, args):
-        return utils.system_output(
+    def _run(self, cmd, args):
+        """Run a puny benchmark
+
+        Prepends the path to the puny benchmark bin.
+        """
+        result = utils.system_output(
             os.path.join(self.Bin, cmd) + ' ' + args)
+        logging.debug(result)
+        return result
 
-    def find_max(self, tag, text):
+
+    @staticmethod
+    def _find_max(tag, text):
+        """Find the max in a memcpy result.
+
+        Args:
+          tag: name of sub-test to select from text.
+          text: output from memcpy test.
+        Returns:
+          Best result from that sub-test.
+        """
+        re_float = r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?"
         r1 = re.search(tag + ".*\n(\d.*\n)+", text)
-        r2 = re.search(r"(\d+\. \d+\.\d+.*\n)+", r1.group(0))
-        runs = string.split(r2.group(0), '\n')
-        max = 0.0
-        for r in runs:
-            a = re.search('\d+.\d+', r)
-            if (a):
-                b = float(a.group(0))
-                if (b > max):
-                    max = b
-        return max
+        r2 = re.findall(r"\d+\. (" + re_float + r") M.*\n", r1.group(0))
+        return max(float(result) for result in r2)
 
 
-    def memcpy(self):
+    def _memcpy(self):
+        """Measure memory to memory copy.
+
+        The size has to be large enough that it doesn't fit
+        in the cache. We then take the best of serveral runs
+        so we have a guarenteed not to exceed number.
+
+        Several different ways are used to move memory.
+        """
         size = '0x4000000'
         loops = '4'
         iterations = '10'
         args  = '-z' + size
         args += ' -i' + iterations
         args += ' -l' + loops
-        result = self.run('memcpy', args)
+        result = self._run('memcpy', args)
 
         for tag in ['memcpy', '32bit', '64bit']:
-           max = self.find_max(tag, result)
-           self.write_perf_keyval({tag: max})
+            max = self._find_max(tag, result)
+            self.write_perf_keyval({tag: max})
 
 
-    def memcpy_test(self):
-        result = self.run('memcpy_test', "")
-        r1 = re.search("L1 cache.*\n.*\n.*", result)
-        r2 = re.search("\d+\.\d+ MiB/s$", r1.group())
+    def _memcpy_test(self):
+        """Test the various caches and alignments
+
+        WARNING: test will have to be changed if cache sizes change.
+        """
+        result = self._run('memcpy_test', "")
+        r1 = re.search(r"L1 cache.*\n.*\n.*", result)
+        r2 = re.search(r"[^\s]+ MiB/s$", r1.group(0))
         self.write_perf_keyval({'L1cache': r2.group()})
 
-        r1 = re.search("L2 cache.*\n.*\n.*", result)
-        r2 = re.search("\d+\.\d+ MiB/s$", r1.group(0))
+        r1 = re.search(r"L2 cache.*\n.*\n.*", result)
+        r2 = re.search(r"[^\s]+ MiB/s$", r1.group(0))
         self.write_perf_keyval({'L2cache': r2.group()})
 
-        r1 = re.search("SDRAM.*\n.*\n.*", result)
-        r2 = re.search("\d+\.\d+ MiB/s$", r1.group(0))
+        r1 = re.search(r"SDRAM.*\n.*\n.*", result)
+        r2 = re.search(r"[^\s]+ MiB/s$", r1.group(0))
         self.write_perf_keyval({'SDRAM': r2.group()})
 
 
-    def threadtree(self):
+    def _threadtree(self):
+        """Create and manipulate directory trees.
+
+        Threadtree creates a directory tree with files for each task.
+        It then copies that tree then deletes it.
+        """
         directory = '/usr/local/_Dir'
         iterations = '4'
         tasks = '2'
@@ -79,15 +105,19 @@ class kernel_fs_Punybench(test.test):
         args += ' -t' + tasks
         args += ' -w' + width
         args += ' -k' + depth
-
-        result = self.run('threadtree', args)
-        r1 = re.search("timer avg= \d*.\d*.*$", result)
-        r2 = re.search("\d*\.\d*", r1.group())
-        p = int(tasks) * pow(int(width), int(depth) + 1) / float(r2.group(0))
+        result = self._run('threadtree', args)
+        r1 = re.search(r"timer avg= *([^\s]*).*$", result)
+        timer_avg = float(r1.groups()[0])
+        p = int(tasks) * pow(int(width), int(depth) + 1) / timer_avg
         self.write_perf_keyval({'threadtree': p})
 
 
-    def uread(self):
+    def _uread(self):
+        """Read a large file.
+
+        The size should be picked so the file will
+        not fit in memory.
+        """
         file = '/usr/local/xyzzy'
         size = '0x200000000'
         loops = '4'
@@ -97,16 +127,15 @@ class kernel_fs_Punybench(test.test):
         args += ' -i' + iterations
         args += ' -l' + loops
         args += ' -b12'
-        result = self.run('uread', args)
-
-        r1 = re.search("timer avg=\d*.\d*.*$", result)
-        r2 = re.search("\d*\.\d*", r1.group(0))
-        p = int(size, 0) * int(iterations) / float(r2.group(0)) \
-            / 1024.0 / 1024.0
-        self.write_perf_keyval({'uread': p})
+        result = self._run('uread', args)
+        r1 = re.search(r"([^\s]+ MiB/s).*$", result)
+        value = r1.groups()[0]
+        self.write_perf_keyval({'uread': value})
 
 
-    def ureadrand(self):
+    def _ureadrand(self):
+        """Read randomly a large file
+        """
         file = '/usr/local/xyzzy'
         size = '0x200000000'
         loops = '4'
@@ -116,14 +145,13 @@ class kernel_fs_Punybench(test.test):
         args += ' -i' + iterations
         args += ' -l' + loops
         args += ' -b12'
-        result = self.run('ureadrand', args)
+        result = self._run('ureadrand', args)
+        r1 = re.search(r"([^\s]+ MiB/s).*$", result)
+        value = r1.groups()[0]
+        self.write_perf_keyval({'ureadrand': value})
 
-        r1 = re.search("timer avg=\d*.\d*.*$", result)
-        r2 = re.search("\d*\.\d*", r1.group(0))
-        p =  4096.0 * int(iterations) / float(r2.group(0)) / 1024.0 / 1024.0
-        self.write_perf_keyval({'ureadrand': p})
 
-    def parse_args(self, args):
+    def _parse_args(self, args):
         """Parse input arguments to this autotest.
 
         Args:
@@ -145,6 +173,7 @@ class kernel_fs_Punybench(test.test):
         # be stripped before they are parsed.
         return parser.parse_args(map(lambda arg: arg.strip('\'\"'), args))
 
+
     def run_once(self, args=[]):
         """Run the PyAuto performance tests.
 
@@ -154,16 +183,18 @@ class kernel_fs_Punybench(test.test):
               it to get a list.  The list will be sent to optparse for parsing.
         """
         if isinstance(args, str):
-          args = args.split()
-        options, test_args = self.parse_args(args)
+            args = args.split()
+        options, test_args = self._parse_args(args)
 
         if test_args:
             raise error.TestFail("Unknown args: %s" % repr(test_args))
 
+        utils.system_output('stop ui')
         if options.want_mem_tests:
-            self.memcpy_test()
-            self.memcpy()
+            self._memcpy_test()
+            self._memcpy()
         if options.want_disk_tests:
-            self.threadtree()
-            self.uread()
-            self.ureadrand()
+            self._threadtree()
+            self._uread()
+            self._ureadrand()
+        utils.system_output('start ui')
