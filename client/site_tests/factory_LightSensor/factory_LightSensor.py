8f43c7e89fa595f763410e48467b8864d5d87dd3
Vic Yang <victoryang@chromium.org>
2011-12-09 14:48:07 +0800

    Improve ALS Functional Test
    Add a subtest mode to accept specific luminescence range.
    Make subtests run in sequence instead of in parallel.
    Modify UI to allow display operator instruction for each subtest.
    Allow subtest list to be passed in.
    
    BUG=chromium-os-partner:6615
    TEST=pass all subtests, tested subtest time-out, see UI changes
    
    Change-Id: I212ca96b669c9ba69f0a0bc395e0f648e229f1a1
    Reviewed-on: https://gerrit.chromium.org/gerrit/12674
    Reviewed-by: Dave Parker <dparker@chromium.org>
    Commit-Ready: Vic Yang <victoryang@chromium.org>
    Tested-by: Vic Yang <victoryang@chromium.org>

--- client/site_tests/factory_LightSensor/factory_LightSensor.py
+++ client/site_tests/factory_LightSensor/factory_LightSensor.py
@@ -27,22 +27,30 @@ from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib import utils
 
 _LABEL_STATUS_SIZE = (140, 30)
-_LABEL_RESPONSE_STR = ful.USER_PASS_FAIL_SELECT_STR + '\n'
 
-_SUBTEST_LIST = ['Light sensor dark', 'Light sensor light']
-_SUBTEST_CFG = {'Light sensor dark':{'min':4},
-                'Light sensor light':{'max':1000}}
+_SUBTEST_LIST = ['Light sensor dark',
+                 'Light sensor exact',
+                 'Light sensor light']
+_SUBTEST_CFG = {'Light sensor dark': {'below': 4},
+                'Light sensor exact': {'between': (10, 15)},
+                'Light sensor light': {'above': 200}}
+
+_SUBTEST_INSTRUCTION = {
+    'Light sensor dark': 'Cover light sensor with finger',
+    'Light sensor exact': 'Remove finger from light sensor',
+    'Light sensor light': 'Shine light sensor with flashlight'}
+
 
 class iio_generic():
     '''
     Object to interface to ambient light sensor over iio.
     '''
-    PARAMS = {'rd':'/sys/bus/iio/devices/device0/illuminance0_input',
+    PARAMS = {'rd': '/sys/bus/iio/devices/device0/illuminance0_input',
               'init': '',
-              'min':0,
-              'max':math.pow(2,16),
+              'min': 0,
+              'max': math.pow(2, 16),
               # in seconds
-              'mindelay':0.178,
+              'mindelay': 0.178,
               }
 
     def __init__(self):
@@ -52,7 +60,7 @@ class iio_generic():
         if not os.path.isfile(self.PARAMS['rd']):
             self.cfg()
 
-        self.ambient = self.read('mean',delay=0,samples=10)
+        self.ambient = self.read('mean', delay=0, samples=10)
         factory.log('ambient light sensor = %d' % self.ambient)
 
     def cfg(self):
@@ -60,10 +68,10 @@ class iio_generic():
         utils.system(cmd)
         time.sleep(1)
         if not os.path.isfile(self.PARAMS['rd']):
-            raise error.TestError(cmd + 'did not create ' + self.PARAMS['rd'])
-        val = self.read('first',samples=1)
+            raise error.TestError(cmd + ' did not create ' + self.PARAMS['rd'])
+        val = self.read('first', samples=1)
         if val <= self.PARAMS['min'] or val >= self.PARAMS['max']:
-            raise error.TestError("Failed initial read\n")
+            raise error.TestError('Failed initial read\n')
 
 
     def read(self, type, delay=None, samples=1):
@@ -101,58 +109,94 @@ class iio_generic():
         else:
             error.ValueError('Illegal value %s for type' % type)
 
-class factory_LightSensor(test.test):
-    version = 2
 
-    def key_release_callback(self, widget, event):
-        if event.keyval == ord('Q'):
+class factory_LightSensor(test.test):
+    version = 3
+
+    def next_subtest(self):
+        self._tested += 1
+        if self._tested is len(self._subtest_list):
+            gtk.main_iteration(False)
             gtk.main_quit()
-        return True
+        self._active_subtest = self._subtest_list[self._tested]
+        self._status_map[self._active_subtest] = ful.ACTIVE
+        self._status_label[self._active_subtest].queue_draw()
+        self._deadline = time.time() + self._timeout_per_subtest
+        self._current_iter_remained = self._iter_req_per_subtest
+        self._cumulative_val = 0
 
     def timer_event(self, countdown_label):
         time_remaining = max(0, self._deadline - time.time())
         if time_remaining is 0:
-            factory.log('deadline reached')
-            gtk.main_quit()
+            self._status_map[self._active_subtest] = ful.FAILED
+            self._status_label[self._active_subtest].queue_draw()
+            factory.log('Timeout on subtest "%s"' % self._active_subtest)
+            self.next_subtest()
 
         countdown_label.set_text('%d' % time_remaining)
         countdown_label.queue_draw()
-        gtk.main_iteration(False)
         return True
 
-    def sensor_event(self, sensor_value):
-        val = self._als.read('mean',samples=5, delay=0)
-
-        passed = 0
-        for name in _SUBTEST_LIST:
-            cfg = _SUBTEST_CFG[name]
-            if self._status_map[name] is ful.PASSED:
-                # No need to recheck, we already passed.
-                pass
-            elif 'max' in cfg:
-                if val > cfg['max']:
-                    factory.log("Passed checking max %d > %d" % \
-                                    (val, cfg['max']))
-                    self._status_map[name] = ful.PASSED
-                    self._status_label[name].queue_draw()
-            elif 'min' in cfg:
-                if val < cfg['min']:
-                    factory.log("Passed checking min %d < %d" % \
-                                    (val, cfg['min']))
-                    self._status_map[name] = ful.PASSED
-                    self._status_label[name].queue_draw()
-            if self._status_map[name] is ful.PASSED:
-                passed += 1
-
-        if passed is len(_SUBTEST_LIST):
-            factory.log('Passed all sensor tests')
+    def key_release_callback(self, widget, event):
+        if event.keyval == ord('Q'):
             gtk.main_quit()
+        if event.keyval == ord(' ') and not self._started:
+            self._started = True
+            self._active_subtest = self._subtest_list[0]
+            self._status_map[self._active_subtest] = ful.ACTIVE
+            self._status_label[self._active_subtest].queue_draw()
+            self._deadline = time.time() + self._timeout_per_subtest
+            gobject.timeout_add(1000, self.timer_event, self._countdown_label)
+
+        return True
+
+    def pass_one_iter(self, name):
+        self._current_iter_remained -= 1
+        if self._current_iter_remained is 0:
+            self._status_map[name] = ful.PASSED
+            self._status_label[name].queue_draw()
+            self.next_subtest()
+            self._current_iter_remained = self._iter_req_per_subtest
+            mean_val = self._cumulative_val / self._iter_req_per_subtest
+            factory.log('Passed subtest "%s" with mean value %d.' %
+                        (name, mean_val))
+
+    def sensor_event(self, sensor_value):
+        val = self._als.read('mean', samples=5, delay=0)
+
+        if self._started:
+            name = self._active_subtest
+            cfg = self._subtest_cfg[name]
+            passed = 0
+            if 'above' in cfg:
+                if val > cfg['above']:
+                    factory.log('Passed checking "above" %d > %d' %
+                                (val, cfg['above']))
+                    passed = 1
+            elif 'below' in cfg:
+                if val < cfg['below']:
+                    factory.log('Passed checking "below" %d < %d' %
+                                (val, cfg['below']))
+                    passed = 1
+            elif 'between' in cfg:
+                lb, ub = cfg['between']
+                if val > lb and val < ub:
+                    factory.log('Passed checking "between" %d < %d < %d' %
+                                (lb, val, ub))
+                    passed = 1
+            if passed is 1:
+                self._cumulative_val += val
+                self.pass_one_iter(name)
+            else:
+                if self._current_iter_remained != self._iter_req_per_subtest:
+                    factory.log('Resetting iter count.')
+                self._cumulative_val = 0
+                self._current_iter_remained = self._iter_req_per_subtest
 
         sensor_value.set_text('%d' % val)
         sensor_value.queue_draw()
         gtk.main_iteration(False)
         return True
-
 
     def label_status_expose(self, widget, event, name):
         status = self._status_map[name]
@@ -160,6 +204,15 @@ class factory_LightSensor(test.test):
         widget.modify_fg(gtk.STATE_NORMAL, ful.LABEL_COLORS[status])
         return False
 
+    def calc_cfg_description(self, cfg):
+        if 'above' in cfg:
+            return 'Input > %d' % cfg['above']
+        elif 'below' in cfg:
+            return 'Input < %d' % cfg['below']
+        elif 'between' in cfg:
+            return '%d < Input < %d' % cfg['between']
+        else:
+            raise error.ValueError('Unknown type in subtest configuration')
 
     def make_subtest_label_box(self, name):
         eb = gtk.EventBox()
@@ -168,63 +221,96 @@ class factory_LightSensor(test.test):
                                       alignment=(0, 0.5),
                                       fg=ful.LABEL_COLORS[ful.UNTESTED])
         label_status.connect('expose_event', self.label_status_expose, name)
-        label_en = ful.make_label(name, alignment=(1,0.5))
+        label_desc = ful.make_label(self._subtest_instruction[name],
+                                    alignment=(0.5, 0.5), fg=ful.WHITE)
+        cfg_desc = self.calc_cfg_description(self._subtest_cfg[name])
+        label_en = ful.make_label("%s (%s)" % (name, cfg_desc),
+                                  alignment=(1, 0.5))
         label_sep = ful.make_label(' : ', alignment=(0.5, 0.5))
         hbox = gtk.HBox()
         hbox.pack_end(label_status, False, False)
         hbox.pack_end(label_sep, False, False)
         hbox.pack_end(label_en, False, False)
-        eb.add(hbox)
+        vbox = gtk.VBox()
+        vbox.pack_start(label_desc, False, False)
+        vbox.pack_start(hbox, False, False)
+        eb.add(vbox)
         return eb
 
     def register_callbacks(self, window):
         window.connect('key-release-event', self.key_release_callback)
         window.add_events(gtk.gdk.KEY_RELEASE_MASK)
 
+    def get_subtest(self, subtest_list, subtest_cfg, subtest_instruction):
+        has_specified = (subtest_list is not None or
+                         subtest_cfg is not None or
+                         subtest_instruction is not None)
+        all_specified = (subtest_list is not None and
+                         subtest_cfg is not None and
+                         subtest_instruction is not None)
+        if has_specified and not all_specified:
+            raise error.ValueError('Missing parameters of subtests.')
+        if all_specified:
+            self._subtest_list = subtest_list
+            self._subtest_cfg = subtest_cfg
+            self._subtest_instruction = subtest_instruction
+        else:
+            self._subtest_list = _SUBTEST_LIST
+            self._subtest_cfg = _SUBTEST_CFG
+            self._subtest_instruction = _SUBTEST_INSTRUCTION
+
     def run_once(self,
-                 lux_min=None,
-                 lux_max=None,
-                 timeout=60):
+                 timeout_per_subtest=10,
+                 subtest_list=None,
+                 subtest_cfg=None,
+                 subtest_instruction=None):
 
         factory.log('%s run_once' % self.__class__)
 
-        self._als = iio_generic()
-        self._deadline = time.time() + timeout
+        self.get_subtest(subtest_list, subtest_cfg, subtest_instruction)
 
-        self._subtest_queue = [x for x in reversed(_SUBTEST_LIST)]
-        self._status_map = dict((n, ful.ACTIVE) for n in _SUBTEST_LIST)
+        self._als = iio_generic()
+
+        self._timeout_per_subtest = timeout_per_subtest
+
+        self._iter_req_per_subtest = 3
+        self._current_iter_remained = self._iter_req_per_subtest
+        self._cumulative_val = 0
+
+        self._status_map = dict((n, ful.UNTESTED) for n in self._subtest_list)
+        self._tested = 0
+
+        self._started = False
 
         vbox = gtk.VBox()
         prompt_label = ful.make_label(
-            'Cover Sensor with Finger (Input < %s)\n' % lux_min +
-            '请遮蔽光传感器\n\n' +
-            'Shine flashlight at Sensor (Input > %s)\n' % lux_max +
-            '请以灯光照射光传感器\n',
+            'Use indicated light source to pass each subtest\n'
+            'Hit "space" to begin...\n',
             fg=ful.WHITE)
         vbox.pack_start(prompt_label, False, False)
+
+        padding_size = 15
+
+        self._status_label = {}
+        for name in self._subtest_list:
+            label_box = self.make_subtest_label_box(name)
+            vbox.pack_start(label_box, False, False, padding_size)
+            self._status_label[name] = label_box
+
+        vbox.pack_start(ful.make_hsep(), False, False, padding_size)
 
         hbox = gtk.HBox()
         sensor_label = ful.make_label('Input: ', fg=ful.WHITE)
         sensor_value = ful.make_label('     ', fg=ful.WHITE)
         hbox.pack_start(sensor_label, False, False)
         hbox.pack_start(sensor_value, False, False)
+
+        countdown_widget, self._countdown_label = ful.make_countdown_widget()
+        self._countdown_label.set_text('%d' % timeout_per_subtest)
+        hbox.pack_end(countdown_widget, False, False)
+
         vbox.pack_start(hbox, False, False)
 
-        self._status_label = {}
-        for name in _SUBTEST_LIST:
-            label_box = self.make_subtest_label_box(name)
-            vbox.pack_start(label_box, False, False)
-            self._status_label[name] = label_box
-            cfg = _SUBTEST_CFG[name]
-            # change defaults per factory setup
-            if 'max' in cfg and lux_max is not None:
-                cfg['max'] = lux_max
-            if 'min' in cfg and lux_min is not None:
-                cfg['min'] = lux_min
-
-        countdown_widget, countdown_label = ful.make_countdown_widget()
-        vbox.pack_start(countdown_widget, False, False)
-        gobject.timeout_add(1000, self.timer_event, countdown_label)
         gobject.timeout_add(300, self.sensor_event, sensor_value)
 
         self._test_widget = vbox
@@ -234,7 +320,7 @@ class factory_LightSensor(test.test):
         failed_set = set(name for name, status in self._status_map.items()
                          if status is not ful.PASSED)
         if failed_set:
-            raise error.TestFail('some subtests failed (%s)' %
+            raise error.TestFail('some subtests timed out (%s)' %
                                  ', '.join(failed_set))
 
         factory.log('%s run_once finished' % self.__class__)
