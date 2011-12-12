38da5a22f6af9e2ac65b588c937ff2a9d8d800c6
Raymes Khoury <raymes@chromium.org>
2011-12-12 15:54:29 -0800

    Add profile_type option to cros_perf profiler.
    This allows one to run 'perf record' or 'perf stat' (or other perf
    commands) and collect different types of data from perf.
    
    BUG=none
    TEST=Ran with profile_type='record' and profile_type='stat' and checked
    that the expected data was collected.
    
    Change-Id: I4a66dc6d79d7e7aac9de5519da8e8155d5c49f74
    Reviewed-on: https://gerrit.chromium.org/gerrit/12788
    Reviewed-by: Mike Truty <truty@chromium.org>
    Tested-by: Raymes Khoury <raymes@chromium.org>
    Commit-Ready: Raymes Khoury <raymes@chromium.org>

--- client/profilers/cros_perf/cros_perf.py
+++ client/profilers/cros_perf/cros_perf.py
@@ -5,10 +5,11 @@
 """
 This is a profiler class for the perf profiler in ChromeOS. It differs from
 the existing perf profiler in autotset by directly substituting the options
-passed to the initialize function into the "perf record" command line. It also
-does not produce a perf report on the client (where there are no debug
-symbols) but instead copies the perf.data file back to the server for
-analysis.
+passed to the initialize function into the "perf" command line. It allows one
+to specify which perf command to run and thus what type of profile to collect
+(e.g. "perf record" or "perf stat"). It also does not produce a perf report
+on the client (where there are no debug symbols) but instead copies
+the perf.data file back to the server for analysis.
 """
 
 import os, signal, subprocess
@@ -19,15 +20,29 @@ from autotest_lib.client.common_lib import error
 class cros_perf(profiler.profiler):
     version = 1
 
-    def initialize(self, options='-e cycles'):
+    def initialize(self, options='-e cycles', profile_type='record'):
+        # The two supported options for profile_type are 'record' and 'stat'.
         self.options = options
         self.perf_bin = os_dep.command('perf')
+        self.profile_type = profile_type
 
 
     def start(self, test):
-        self.logfile = os.path.join(test.profdir, 'perf.data')
-        cmd = ('exec %s record -a -o %s %s' %
-               (self.perf_bin, self.logfile, self.options))
+        if self.profile_type == 'record':
+            # perf record allows you to specify where to place the output.
+            # perf stat does not.
+            logfile = os.path.join(test.profdir, 'perf.data')
+            self.options += ' -o %s' % logfile
+
+        if self.profile_type == 'stat':
+            # Unfortunately we need to give perf stat a command or process even
+            # when running in system-wide mode.
+            self.options += ' -p 1'
+
+        outfile = os.path.join(test.profdir, 'perf.out')
+
+        cmd = ('exec %s %s -a %s > %s 2>&1' %
+               (self.perf_bin, self.profile_type, self.options, outfile))
 
         self._process = subprocess.Popen(cmd, shell=True,
                                          stderr=subprocess.STDOUT)
@@ -41,4 +56,3 @@ class cros_perf(profiler.profiler):
 
         self._process.send_signal(signal.SIGINT)
         self._process.wait()
-
