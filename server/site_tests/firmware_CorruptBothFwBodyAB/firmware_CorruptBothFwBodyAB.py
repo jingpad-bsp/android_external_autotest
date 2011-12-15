9cc6b3517f971cf8d7c0763fa05c6ce2d7138b2f
Tom Wai-Hong Tam <waihong@chromium.org>
2011-12-15 11:49:12 +0800

    A FAFT test case of corrupting both firmware body A and B.
    This test corrupts both firmware body A and B and checks the next boot results.
    
    The expected behavior is different if the firmware preamble USE_RO_NORMAL
    flag is enabled. In the case USE_RO_NORMAL ON, the firmware corruption
    doesn't hurt the boot results since it boots the RO path directly and does
    not load and verify the RW firmware body. In the case USE_RO_NORMAL OFF,
    the firmware verification fails on loading RW firmware and enters recovery
    mode. In this case, it requires a USB disk plugged-in, which contains a
    Chrome OS test image (built by "build_image --test").
    
    BUG=chromium-os:19710;chrome-os-partner:6882
    TEST=tested on both USE_RO_NORMAL enabled and disabled devices:
    $ run_remote_tests.sh --remote=$REMOTE_IP -a "xml_config=$OVERLAY_XML \
          servo_vid=0x18d1 servo_pid=0x5001" CorruptBothFwBodyAB/control.both
    
    Change-Id: I37661366e41196abacbf78756032ac6af3f1805e
    Reviewed-on: https://gerrit.chromium.org/gerrit/12973
    Commit-Ready: Tom Wai-Hong Tam <waihong@chromium.org>
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

---
+++ server/site_tests/firmware_CorruptBothFwBodyAB/firmware_CorruptBothFwBodyAB.py
@@ -0,0 +1,111 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+
+from autotest_lib.server.cros.faftsequence import FAFTSequence
+
+
+class firmware_CorruptBothFwBodyAB(FAFTSequence):
+    """
+    Servo based both firmware body A and B corruption test.
+
+    The expected behavior is different if the firmware preamble USE_RO_NORMAL
+    flag is enabled. In the case USE_RO_NORMAL ON, the firmware corruption
+    doesn't hurt the boot results since it boots the RO path directly and does
+    not load and verify the RW firmware body. In the case USE_RO_NORMAL OFF,
+    the firmware verification fails on loading RW firmware and enters recovery
+    mode. In this case, it requires a USB disk plugged-in, which contains a
+    Chrome OS test image (built by "build_image --test").
+    """
+    version = 1
+
+    PREAMBLE_USE_RO_NORMAL = 1
+
+    use_ro = False
+
+
+    def ensure_normal_boot(self):
+        """Ensure normal boot this time.
+
+        If not, it may be a test failure during step 2, try to recover to
+        normal mode by recovering the firmware and rebooting.
+        """
+        if self.crossystem_checker({'mainfw_type': 'recovery'}):
+            self.run_faft_step({
+                'userspace_action': (self.faft_client.run_shell_command,
+                    'chromeos-firmwareupdate --mode recovery')
+            })
+
+
+    def setup(self, dev_mode=False):
+        super(firmware_CorruptBothFwBodyAB, self).setup()
+        if (self.faft_client.get_firmware_flags('a') &
+                self.PREAMBLE_USE_RO_NORMAL):
+            self.use_ro = True
+            self.servo.set('usb_mux_sel1', 'servo_sees_usbkey')
+        else:
+            self.assert_test_image_in_usb_disk()
+            self.servo.set('usb_mux_sel1', 'dut_sees_usbkey')
+        self.setup_dev_mode(dev_mode)
+
+
+    def cleanup(self):
+        self.ensure_normal_boot()
+        super(firmware_CorruptBothFwBodyAB, self).cleanup()
+
+
+    def run_once(self, host=None):
+        if self.use_ro:
+            # USE_RO_NORMAL flag is ON. Firmware body corruption doesn't
+            # hurt the booting results.
+            logging.info('The firmware USE_RO_NORMAL flag is enabled.')
+            self.register_faft_sequence((
+                {   # Step 1, corrupt both firmware body A and B
+                    'state_checker': (self.crossystem_checker, {
+                        'mainfw_type': ('normal', 'developer'),
+                        'recoverysw_boot': '0',
+                    }),
+                    'userspace_action': (self.faft_client.corrupt_firmware_body,
+                                         ('a', 'b')),
+                },
+                {   # Step 2, still expected normal/developer boot and restore
+                    'state_checker': (self.crossystem_checker, {
+                        'mainfw_type': ('normal', 'developer'),
+                        'recoverysw_boot': '0',
+                    }),
+                    'userspace_action': (self.faft_client.restore_firmware_body,
+                                         ('a', 'b')),
+                },
+            ))
+        else:
+            self.register_faft_sequence((
+                {   # Step 1, corrupt both firmware body A and B
+                    'state_checker': (self.crossystem_checker, {
+                        'mainfw_type': ('normal', 'developer'),
+                        'recoverysw_boot': '0',
+                    }),
+                    'userspace_action': (self.faft_client.corrupt_firmware_body,
+                                         ('a', 'b')),
+                    'firmware_action': self.wait_fw_screen_and_plug_usb,
+                    'install_deps_after_boot': True,
+                },
+                {   # Step 2, expected recovery boot and restore firmware
+                    'state_checker': (self.crossystem_checker, {
+                        'mainfw_type': 'recovery',
+                        'recovery_reason':
+                            self.RECOVERY_REASON['RO_INVALID_RW'],
+                        'recoverysw_boot': '0',
+                    }),
+                    'userspace_action': (self.faft_client.restore_firmware_body,
+                                         ('a', 'b')),
+                },
+                {   # Step 3, expected normal boot, done
+                    'state_checker': (self.crossystem_checker, {
+                        'mainfw_type': ('normal', 'developer'),
+                        'recoverysw_boot': '0',
+                    }),
+                },
+            ))
+        self.run_faft_sequence()
