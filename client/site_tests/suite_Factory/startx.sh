555ef622d6e11f3a5c243b06810e004c2e993521
Joseph Hwang <josephsih@chromium.org>
2011-12-26 12:07:51 +0800

    suite_Factory: initialize touchpad device in startx.sh adequately
    On factory image, X is started by startx.sh which is different
    from that of a ToT normal image which invokes /sbin/xstart.sh
    to initialize the cmt driver. This CL copies some initialization
    code from /sbin/xstart.sh into startx.sh to bring up the cmt driver
    correctly.
    
    factory_Touchpad works as before. This CL is to fix the initialization
    of the cmt driver on factory image.
    
    BUG=chrome-os-partner:7330
    TEST=In factory test, press "ctrl-t" to perform touchpad test successfully.
    
    Change-Id: I2685411fd8276255ba41a516794b2d6d229fecf6
    Reviewed-on: https://gerrit.chromium.org/gerrit/13484
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Joseph Shyh-In Hwang <josephsih@chromium.org>
    Commit-Ready: Joseph Shyh-In Hwang <josephsih@chromium.org>

--- client/site_tests/suite_Factory/startx.sh
+++ client/site_tests/suite_Factory/startx.sh
@@ -19,6 +19,28 @@ trap user1_handler USR1
 MCOOKIE=$(head -c 8 /dev/urandom | openssl md5)
 ${XAUTH} -q -f ${XAUTH_FILE} add ${DISPLAY} . ${MCOOKIE}
 
+# The following logic is copied from /sbin/xstart.sh to initialize the
+# touchpad device adequately.
+if [ -d /home/chronos -a ! -f /home/chronos/.syntp_enable ] ; then
+  # serio_rawN devices come from the udev rules for the Synaptics binary
+  # driver throwing the PS/2 interface into raw mode.  If we find any
+  # that are TP devices, put them back into normal mode to use the default
+  # driver instead.
+  for P in /sys/class/misc/serio_raw*
+  do
+    # Note: It's OK if globbing fails, since the next line will drop us out
+    udevadm info -q env -p $P | grep -q ID_INPUT_TOUCHPAD=1 || continue
+    # Rescan everything; things that don't have another driver will just
+    # restart in serio_rawN mode again, since they won't be looking for
+    # the disable in their udev rules.
+    SERIAL_DEVICE=/sys$(dirname $(dirname $(udevadm info -q path -p $P)))
+    echo -n "rescan" > ${SERIAL_DEVICE}/drvctl
+  done
+else
+  udevadm trigger
+  udevadm settle
+fi
+
 /bin/sh -c "\
 trap '' USR1 TTOU TTIN
 exec /usr/bin/X -nolisten tcp vt01 -auth ${XAUTH_FILE} \
