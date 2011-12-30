f7c4aa319afb5532ef330c250c2c7d80c84f6d16
Tom Wai-Hong Tam <waihong@chromium.org>
2011-12-30 14:41:46 +0800

    Support shellballs in platform_InstallFW and able to install the local one.
    This change makes platform_InstallFW accept a firmware shellball in addition
    to a raw binary. Specifying bios_path=local can also install the local
    shellball (/usr/sbin/chromeos-firmwareupdate) in the current client image.
    
    BUG=chromium-os:19710;chromium-os:22339
    TEST=run_remote_tests.sh --remote=$REMOTE_IP -a "xml_config=$OVERLAY_XML \
             servo_vid=0x18d1 servo_pid=0x5001 bios_path=local" platform_InstallFW
    
    Change-Id: I52018504a76b9aa9c19011aa7b7aaf47e262d49b
    Reviewed-on: https://gerrit.chromium.org/gerrit/13576
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Commit-Ready: Tom Wai-Hong Tam <waihong@chromium.org>

--- server/site_tests/platform_InstallFW/platform_InstallFW.py
+++ server/site_tests/platform_InstallFW/platform_InstallFW.py
@@ -3,20 +3,32 @@
 # found in the LICENSE file.
 
 import logging
-import os
-from autotest_lib.server import test, autotest
+from autotest_lib.server import test, autotest, utils
 
 class platform_InstallFW(test.test):
     version = 1
 
     def run_once(self, host=None, bios_path=None, bios_name=None):
-      # Copy binary from server to client.
-      host.send_file("%s/%s" % (bios_path, bios_name),
-                     "/tmp/%s" % bios_name)
-      # Install bios on a client.
-      host.run("sudo /usr/sbin/flashrom -w /tmp/%s" % bios_name)
-      # Reboot client after installing the binary.
-      host.reboot()
-      # Get the versions of binaries.
-      bios_info = host.run("sudo mosys -k smbios info bios")
-      logging.info("Firmware version info: %s" % bios_info)
+        if bios_path == "local":
+            fw_dst = "/usr/sbin/chromeos-firmwareupdate"
+            is_shellball = True
+        else:
+            fw_src = "%s/%s" % (bios_path, bios_name)
+            # Determine the firmware file is a shellball or a raw binary.
+            is_shellball = (utils.system_output("file %s" % fw_src).find(
+                    "shell script") != -1)
+            fw_dst = "/tmp/%s" % bios_name
+            # Copy binary from server to client.
+            host.send_file(fw_src, fw_dst)
+
+        # Install bios on a client.
+        if is_shellball:
+            host.run("sudo /bin/sh %s --mode factory_install" % fw_dst)
+        else:
+            host.run("sudo /usr/sbin/flashrom -w %s" % fw_dst)
+
+        # Reboot client after installing the binary.
+        host.reboot()
+        # Get the versions of binaries.
+        bios_info = host.run("sudo mosys -k smbios info bios")
+        logging.info("Firmware version info: %s" % bios_info)
