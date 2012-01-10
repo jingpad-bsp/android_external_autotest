722b10be01ed39573f6b6e130a18e04c7bc5e3f6
Hsinyu Chao <hychao@chromium.org>
2012-01-10 21:15:01 +0800

    Add --without-sndfile flag to prevent libsndfile.so.1 dependency not found error when executing sox.
    BUG=None
    TEST=emerge autotest-deps then execute sox binary successfully on
    Chromebook
    Change-Id: Ic247b14d20ae1e56d497aeb8a973e03bc7bb2617
    Reviewed-on: https://gerrit.chromium.org/gerrit/13978
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Tested-by: Hsinyu Chao <hychao@chromium.org>
    Commit-Ready: Hsinyu Chao <hychao@chromium.org>

--- client/deps/sox/sox.py
+++ client/deps/sox/sox.py
@@ -14,7 +14,8 @@ def setup(tarball, topdir):
     utils.extract_tarball_to_dir(tarball, srcdir)
     os.chdir(srcdir)
     utils.system('patch < ../configure_no_gsm.patch')
-    utils.configure('--disable-gomp --without-magic --prefix=%s' % topdir)
+    utils.configure('--disable-gomp --without-sndfile --without-magic' +
+            ' --prefix=%s' % topdir)
     utils.make()
     utils.system('make install')
     os.chdir(topdir)
