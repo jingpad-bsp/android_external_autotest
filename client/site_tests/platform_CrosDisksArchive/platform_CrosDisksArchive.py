e089ea23fab4c9b196d2a43a1c3fd6ec6b98bc8d
Ben Chan <benchan@chromium.org>
2012-02-01 06:21:31 -0800

    Add tar, tar.bz2 and tar.gz tests to platform_CrosDisksArchive.
    This CL generalizes the code for testing zip support in
    platform_CrosDisksArchive such that it can test other archive formats.
    tar, tar.bz2 and tar.gz tests are added but currently disabled until
    cros-disks supports these archive formats.
    
    BUG=chromium-os:25802
    TEST=Ran platform_CrosDisksArchive with a modified version of cros-disks
    that supports tar, tar.bz2 and tar.gz.
    
    Change-Id: I53b61a1ba5c1eea815fc1028f7e7df65ef4148c2
    Reviewed-on: https://gerrit.chromium.org/gerrit/15185
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Commit-Ready: Ben Chan <benchan@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>

--- client/site_tests/platform_CrosDisksArchive/platform_CrosDisksArchive.py
+++ client/site_tests/platform_CrosDisksArchive/platform_CrosDisksArchive.py
@@ -4,6 +4,7 @@
 
 import logging
 import os
+import tarfile
 import zipfile
 
 from autotest_lib.client.bin import test
@@ -17,10 +18,11 @@ from collections import deque
 class CrosDisksArchiveTester(CrosDisksTester):
     """A tester to verify archive support in CrosDisks.
     """
-    def __init__(self, test):
+    def __init__(self, test, archive_types):
         super(CrosDisksArchiveTester, self).__init__(test)
+        self._archive_types = archive_types
 
-    def find_all_files(self, root_dir):
+    def _find_all_files(self, root_dir):
         """Returns all files under a directory and its sub-directories.
 
            This is a generator that performs a breadth-first-search of
@@ -42,7 +44,26 @@ class CrosDisksArchiveTester(CrosDisksTester):
                 else:
                     yield relative_path
 
-    def make_zip_archive(self, archive_path, root_dir,
+    def _make_tar_archive(self, archive_path, root_dir, compression=None):
+        """Archives a specified directory into a tar file.
+
+           The created tar file contains all files and sub-directories
+           under the specified root directory, but not the root directory
+           itself.
+
+        Args:
+            archive_path: Path of the output archive.
+            root_dir: The root directory to archive.
+            compression: The compression method: None, 'gz', 'bz2'
+        """
+        mode = 'w:' + compression if compression else 'w'
+        # TarFile in Python 2.6 does not work with the 'with' statement.
+        archive = tarfile.open(archive_path, mode)
+        for path in self._find_all_files(root_dir):
+            archive.add(os.path.join(root_dir, path), path)
+        archive.close()
+
+    def _make_zip_archive(self, archive_path, root_dir,
                          compression=zipfile.ZIP_DEFLATED):
         """Archives a specified directory into a ZIP file.
 
@@ -57,30 +78,54 @@ class CrosDisksArchiveTester(CrosDisksTester):
         """
         # ZipFile in Python 2.6 does not work with the 'with' statement.
         archive = zipfile.ZipFile(archive_path, 'w', compression)
-        for path in self.find_all_files(root_dir):
+        for path in self._find_all_files(root_dir):
             archive.write(os.path.join(root_dir, path), path)
         archive.close()
 
-    def test_zip_archive(self):
-        # Create the ZIP file content in a temporary directory.
-        zip_dir = autotemp.tempdir(unique_id='CrosDisks')
+    def _make_archive(self, archive_type, archive_path, root_dir):
+        """Archives a specified directory into an archive of specified type.
+
+           The created archive file contains all files and sub-directories
+           under the specified root directory, but not the root directory
+           itself.
+
+        Args:
+            archive_type: Type of the output archive.
+            archive_path: Path of the output archive.
+            root_dir: The root directory to archive.
+        """
+        if archive_type in ['zip']:
+            self._make_zip_archive(archive_path, root_dir)
+        elif archive_type in ['tar']:
+            self._make_tar_archive(archive_path, root_dir)
+        elif archive_type in ['tar.gz', 'tgz']:
+            self._make_tar_archive(archive_path, root_dir, 'gz')
+        elif archive_type in ['tar.bz2', 'tbz', 'tbz2']:
+            self._make_tar_archive(archive_path, root_dir, 'bz2')
+        else:
+            raise error.TestFail("Unsupported archive type " + archive_type)
+
+    def _test_archive(self, archive_type):
+        # Create the archive file content in a temporary directory.
+        archive_dir = autotemp.tempdir(unique_id='CrosDisks')
         test_content = DefaultFilesystemTestContent()
-        if not test_content.create(zip_dir.name):
+        if not test_content.create(archive_dir.name):
             raise error.TestFail("Failed to create archive test content")
 
-        # Create a FAT-formatted virtual filesystem image containing a ZIP
-        # file to help stimulate mounting a ZIP file on a removable drive.
+        # Create a FAT-formatted virtual filesystem image containing an
+        # archive file to help stimulate mounting an archive file on a
+        # removable drive.
         with VirtualFilesystemImage(
                 block_size=1024,
                 block_count=65536,
                 filesystem_type='vfat',
-                mkfs_options=[ '-F', '32', '-n', 'ZIPTEST' ]) as image:
+                mkfs_options=[ '-F', '32', '-n', 'ARCHIVE' ]) as image:
             image.format()
             image.mount(options=['sync'])
-            # Create the ZIP file on the virtual filesystem image.
-            archive_name = 'test.zip'
+            # Create the archive file on the virtual filesystem image.
+            archive_name = 'test.' + archive_type
             archive_path = os.path.join(image.mount_dir, archive_name)
-            self.make_zip_archive(archive_path, zip_dir.name)
+            self._make_archive(archive_type, archive_path, archive_dir.name)
             image.unmount()
 
             # Mount the virtual filesystem image via CrosDisks.
@@ -92,7 +137,7 @@ class CrosDisksArchiveTester(CrosDisksTester):
                 'source_path': device_file
             })
 
-            # Mount the ZIP file on the mounted filesystem via CrosDisks.
+            # Mount the archive file on the mounted filesystem via CrosDisks.
             archive_path = os.path.join(result['mount_path'], archive_name)
             expected_mount_path = os.path.join('/media/archive', archive_name)
             self.cros_disks.mount(archive_path)
@@ -102,20 +147,24 @@ class CrosDisksArchiveTester(CrosDisksTester):
                 'mount_path': expected_mount_path
             })
 
-            # Verify the content of the mounted ZIP file.
+            # Verify the content of the mounted archive file.
             if not test_content.verify(expected_mount_path):
                 raise error.TestFail("Failed to verify filesystem test content")
 
             self.cros_disks.unmount(expected_mount_path, ['force'])
             self.cros_disks.unmount(device_file, ['force'])
 
+    def test_archives(self):
+        for archive_type in self._archive_types:
+            self._test_archive(archive_type)
+
     def get_tests(self):
-        return [self.test_zip_archive]
+        return [self.test_archives]
 
 
 class platform_CrosDisksArchive(test.test):
     version = 1
 
     def run_once(self, *args, **kwargs):
-        tester = CrosDisksArchiveTester(self)
+        tester = CrosDisksArchiveTester(self, kwargs['archive_types'])
         tester.run(*args, **kwargs)
