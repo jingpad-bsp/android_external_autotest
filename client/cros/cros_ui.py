f9b93111b0577baa4edc58afa43a1c4d4b98e79c
Joao da Silva <joaodasilva@chromium.org>
2012-02-02 11:54:42 +0100

    Fixed policy autotests.
    The tests restart the UI but fail when trying to stop it if it is
    already stopped.
    
    BUG=chromium-os:24260, chromium-os:24302
    TEST=run_remote_tests.sh control.desktopui_pyauto_basic
    
    Change-Id: If183d861a12a5fa1034b0ee03b0cd6abcfaf5a1e
    Reviewed-on: https://gerrit.chromium.org/gerrit/15183
    Reviewed-by: Nirnimesh <nirnimesh@chromium.org>
    Reviewed-by: Joao da Silva <joaodasilva@chromium.org>
    Tested-by: Joao da Silva <joaodasilva@chromium.org>
    Commit-Ready: Joao da Silva <joaodasilva@chromium.org>

--- client/cros/cros_ui.py
+++ client/cros/cros_ui.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -105,7 +105,7 @@ def _wait_for_login_prompt(timeout=DEFAULT_TIMEOUT):
 
 
 def stop(allow_fail=False):
-    return utils.system("stop ui")
+    return utils.system("stop ui", ignore_status=allow_fail)
 
 
 def start(allow_fail=False):
