f3ff6e4d2acd849f6494e1f6ab0d96cb47d145cf
Hung-Te Lin <hungte@chromium.org>
2012-02-08 17:47:18 +0800

    factory: Add shopfloor system into factory flow (factory_Start).
    The first part of using shop floor system - allow inputting serial number in
    factory_Start (The logic of server communication is not finished yet).
    
    BUG=chrome-os-partner:6911
    TEST=manually tested factory_Start in factory test image.
    
    Change-Id: Ie0244da97a587882164dace5dd281299c67b5d3c
    Reviewed-on: https://gerrit.chromium.org/gerrit/16026
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>

--- client/site_tests/factory_Start/factory_Start.py
+++ client/site_tests/factory_Start/factory_Start.py
@@ -8,122 +8,227 @@
 # This factory test is run a the start of a test sequence to verify the DUT has
 # been setup correctly.
 #
-# It prompts and waits for a keypress (SPACE) to start testing.
-# Pass 'press_to_continue' = False as a darg in the test_list to disable.
-#
-# It optionally prompts and waits for external power to be applied.
-# Pass 'require_external_power' = False as a darg in the test_list to disable.
+# The start provides several settings (set via darg):
+# 'require_external_power': Prompts and waits for external power to be applied.
+# 'require_shop_floor': Prompts and waits for serial number as input.  The
+#       server is default to the host running mini-omaha, unless you specify an
+#       URL by 'shop_floor_server_url' darg.
+# 'press_to_continue': Prompts and waits for a key press (SPACE) to continue
 
 import glob
-import gobject
-import gtk
 import os
-import pango
 import sys
 
+import gobject
+import gtk
+import pango
 from gtk import gdk
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import factory
 from autotest_lib.client.cros.factory import ui as ful
+from autotest_lib.client.cros.factory import shopfloor
 
+
+# Messages for tasks
+_MSG_TASK_POWER = (
+        'Plug in external power to continue.\n'
+        '請插上外接電源以繼續。')
+_MSG_TASK_SERIAL = (
+        'Enter valid serial number:\n'
+        '請輸入有效的序號:'
+        )
+_MSG_TASK_SPACE = (
+        'Hit SPACE to start testing...\n'
+        '按 "空白鍵" 開始測試...')
 _LABEL_FONT = pango.FontDescription('courier new condensed 24')
-_PLUG_IN_POWER_FMT_STR = \
-    'Plug in external power to continue.\n插電才繼續.'
-_SPACE_TO_START_FMT_STR = \
-    'Hit SPACE to start testing...\n按 "空白鍵" 開始測試...'
 
-class Adapter_State:
-    CONNECTED = 1
-    DISCONNECTED = 2
+
+class Task(object):
+    def __init__(self, ui):
+        self._ui = ui
+
+    def start(self):
+        """Initializes task and returns a widget for display, if available."""
+        return None
+
+    def stop(self):
+        """Notifies the test backend current task is finished."""
+        return self._ui.stop_task(self)
+
+    def get_window(self):
+        """Returns UI top level window."""
+        return self._ui.get_window()
+
+    def set_error_and_quit(self, message):
+        """Sets error message and requests for quiting entire test."""
+        return self._ui.set_error_and_quit(message)
+
+
+class PressSpaceTask(Task):
+    def start(self):
+        window = self._ui.get_window()
+        window.add_events(gdk.KEY_PRESS_MASK)
+        self.callback = (window, window.connect('key-press-event',
+                                                self.check_space))
+        return ful.make_label(_MSG_TASK_SPACE, font=ful.LABEL_LARGE_FONT)
+
+    def stop(self):
+        (window, callback_id) = self.callback
+        window.disconnect(callback_id)
+        Task.stop(self)
+
+    def check_space(self, window, event):
+        if event.keyval == gtk.keysyms.space:
+            self.stop()
+        else:
+            factory.log('PressSpaceTask: non-space hit: %d' % event.keyval)
+        return True
+
+
+class ExternalPowerTask(Task):
+
+    AC_CONNECTED = 1
+    AC_DISCONNECTED = 2
+    AC_CHECK_PERIOD = 500
+
+    def start(self):
+        widget = ful.make_label(_MSG_TASK_POWER, font=ful.LABEL_LARGE_FONT)
+        self._timeout = gobject.timeout_add(self.AC_CHECK_PERIOD,
+                                            self.check_event, widget)
+        self._active = True
+        return widget
+
+    def stop(self):
+        self._active = False
+        gobject.source_remove(self._timeout)
+        Task.stop(self)
+
+    def check_event(self, label):
+        if not self._active:
+            return True
+        try:
+            state = self.get_external_power_state()
+        except (IOError, ValueError) as details:
+            self.set_error_and_quit('ExternalPowerTask: %s' % details)
+        except:
+            self.set_error_and_quit('ExternalPowerTask: Unknown exception: %s' %
+                                    sys.exc_info()[0])
+        else:
+            if state == self.AC_CONNECTED:
+                self.stop()
+        return True
+
+    def get_external_power_state(self):
+        for type_file in glob.glob('/sys/class/power_supply/*/type'):
+            type_value = utils.read_one_line(type_file).strip()
+            if type_value == 'Mains':
+                status_file = os.path.join(os.path.dirname(type_file), 'online')
+                try:
+                    status = int(utils.read_one_line(status_file).strip())
+                except ValueError as details:
+                    raise ValueError('Invalid external power state in %s: %s' %
+                                     (status_file, details))
+                if status == 0:
+                    return self.AC_DISCONNECTED
+                elif status == 1:
+                    return self.AC_CONNECTED
+                else:
+                    raise ValueError('Invalid external power state "%s" in %s' %
+                                     (status, status_file))
+        raise IOError('Unable to determine external power state.')
+
+
+class ShopFloorTask(Task):
+    def __init__(self, ui, server_url):
+        Task.__init__(self, ui)
+        self.server_url = server_url or shopfloor.detect_default_server_url()
+        shopfloor.set_enabled(True)
+        shopfloor.set_server_url(self.server_url)
+
+    def start(self):
+        return ful.make_input_window(
+                prompt=_MSG_TASK_SERIAL,
+                on_validate=self.validate_serial_number,
+                on_complete=self.complete_serial_task)
+
+    def validate_serial_number(self, serial):
+        # TODO(hungte) Queries server to see if serial number is valid.
+        return serial.strip()
+
+    def complete_serial_task(self, serial):
+        serial = serial.strip()
+        factory.log('Serial number: %s' % serial)
+        # TODO(hungte) Wipe all cached shopfloor values.
+        # TODO(hungte) Move shared data manipulation into shopfloor module
+        factory.set_shared_data(shopfloor.KEY_ENABLED, True)
+        factory.set_shared_data(shopfloor.KEY_SERVER_URL, self.server_url)
+        factory.set_shared_data(shopfloor.KEY_SERIAL_NUMBER, serial)
+        self.stop()
+        return True
+
 
 class factory_Start(test.test):
-    version = 1
-
-    def initialize(self):
-        self._error = False
-        self._error_message = 'An unspecified error occurred.'
+    version = 2
 
     def set_error_and_quit(self, message):
         self._error = True
         self._error_message = message
         gtk.main_quit()
 
-    def key_release_callback(self, widget, event):
-        char = event.keyval in range(32,127) and chr(event.keyval) or None
-        factory.log('key_release %s(%s)' % (event.keyval, char))
-        if event.keyval == ord(' '):
-            if not self.require_external_power:
-                gtk.main_quit()
-            elif self.get_external_power_state() == Adapter_State.CONNECTED:
-                gtk.main_quit()
-        return True
+    def get_window(self):
+        return self._window
 
-    def register_callbacks(self, window):
-        window.connect('key-release-event', self.key_release_callback)
-        window.add_events(gdk.KEY_RELEASE_MASK)
+    def stop_task(self, task):
+        # Remove all active widgets and registered callbacks
+        factory.log('factory_Start: Stopping task: %s' %
+                    task.__class__.__name__)
+        for widget in self._test_widget.get_children():
+            self._test_widget.remove(widget)
+        self._task_list.remove(task)
+        self.find_next_task()
 
-    def get_external_power_state(self):
-        for type_file in glob.glob('/sys/class/power_supply/*/type'):
-            try:
-                type = utils.read_one_line(type_file).strip()
-            except IOError as details:
-                self.set_error_and_quit('%s' % details)
-            if type == 'Mains':
-                status_file = os.path.join(
-                        os.path.dirname(type_file), 'online')
-                try:
-                    status = int(utils.read_one_line(status_file).strip())
-                except ValueError as details:
-                    self.set_error_and_quit(
-                            'Invalid external power state in %s: %s' %
-                            (status_file, details))
-                except IOError as details:
-                    self.set_error_and_quit('%s' % details)
-                if status == 0:
-                    return Adapter_State.DISCONNECTED
-                elif status == 1:
-                    return Adapter_State.CONNECTED
-                else:
-                    self.set_error_and_quit(
-                            'Invalid external power state "%s" in %s.' %
-                            (status, status_file))
-        self.set_error_and_quit('Unable to determine external power state.')
-
-    def external_power_event(self, label):
-        if self.get_external_power_state() == Adapter_State.DISCONNECTED:
-            label.modify_fg(gtk.STATE_NORMAL, ful.LABEL_COLORS[ful.ACTIVE])
-            label.set_label(_PLUG_IN_POWER_FMT_STR)
-        elif not self.press_to_continue:
-            gtk.main_quit()
+    def find_next_task(self):
+        if self._task_list:
+            task = self._task_list[0]
+            factory.log('factory_Start: Starting task: %s' %
+                        task.__class__.__name__)
+            self._test_widget.add(task.start())
+            self._test_widget.show_all()
         else:
-            label.modify_fg(gtk.STATE_NORMAL, ful.LABEL_COLORS[ful.PASSED])
-            label.set_label(_SPACE_TO_START_FMT_STR)
-        gtk.main_iteration(False)
+            gtk.main_quit()
+
+    def register_window(self, window):
+        """Registers top-level window for tasks."""
+        self._window = window
+        self.find_next_task()
         return True
 
-    def run_once(self, press_to_continue=True, require_external_power=False):
+    def run_once(self,
+                 press_to_continue=True,
+                 require_external_power=False,
+                 require_shop_floor=False,
+                 shop_floor_server_url=None):
         factory.log('%s run_once' % self.__class__)
 
-        self.press_to_continue = press_to_continue
-        self.require_external_power = require_external_power
+        self._error = False
+        self._error_message = 'An unspecified error occurred.'
 
-        label = ful.make_label('', font=_LABEL_FONT)
-        test_widget = gtk.EventBox()
-        test_widget.modify_bg(gtk.STATE_NORMAL, ful.BLACK)
-        test_widget.add(label)
+        self._task_list = []
+        if require_shop_floor:
+            self._task_list.append(ShopFloorTask(self, shop_floor_server_url))
+        if require_external_power:
+            self._task_list.append(ExternalPowerTask(self))
+        if press_to_continue:
+            self._task_list.append(PressSpaceTask(self))
 
-        if self.require_external_power:
-                gobject.timeout_add(500, self.external_power_event, label)
-
-        if self.press_to_continue and not self.require_external_power:
-            label.set_label(_SPACE_TO_START_FMT_STR)
-
-        if self.require_external_power or self.press_to_continue:
+        if self._task_list:
+            # Creates user interface.
+            self._test_widget = gtk.VBox()
             ful.run_test_widget(
-                    self.job, test_widget,
-                    window_registration_callback=self.register_callbacks)
+                    self.job, self._test_widget,
+                    window_registration_callback=self.register_window)
 
         if self._error:
             raise error.TestError(self._error_message)
