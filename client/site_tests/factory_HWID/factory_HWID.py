f58ab056ab5677732fee33d525e8a487f0f07278
Hung-Te Lin <hungte@chromium.org>
2012-02-21 16:13:59 +0800

    factory: new test "factory_HWID".
    By new factory flow design, "HWID probing" (factory_ProbeHWID, restart,
    factory_WriteGBB) is deprecated. The new flow trusts shop floor system, and
    writes the given string as-is.
    
    For RMA and factories not using shop floor system, factory_HWID should provide a
    simple user interface so that operator can manually select installed components.
    This part is not implemented yet and pending on the new "hwidtool" parsing code
    (They can also use the old flow - factoryProbeHWID).
    
    BUG=chrome-os-partner:6911
    TEST=Manually executed with sample shop floor system.
    
    Change-Id: If23affc3084e025b22a5f709711cd1739da32cc8
    Reviewed-on: https://gerrit.chromium.org/gerrit/16269
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>

---
+++ client/site_tests/factory_HWID/factory_HWID.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import os
+import sys
+import tempfile
+import thread
+
+import gobject
+import gtk
+from gtk import gdk
+
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import error, utils
+from autotest_lib.client.cros import factory
+from autotest_lib.client.cros.factory import shopfloor
+from autotest_lib.client.cros.factory import ui as ful
+
+
+class factory_HWID(test.test):
+    version = 1
+
+    def write_hwid(self, hwid):
+        # TODO(hungte) Replace this by gooftool, plus partial matching.
+        # "Partial matching" means: given HWID string from shop floor system
+        # contains only BOM and VARIANT names, select and derive the complete ID
+        # from active HWIDs in current database. Ex: hwid="BLUE A-A" => matched
+        # to "MARIO BLUE A-A 6868".
+        def shell(command):
+            factory.log(command)
+            utils.system(command)
+        with tempfile.NamedTemporaryFile() as temp_file:
+            name = temp_file.name
+            shell("flashrom -i GBB -r '%s'" % name)
+            shell("gbb_utility -s --hwid='%s' '%s'" % (hwid, name))
+            # TODO(hungte) If the HWID is already set correctly, no need to
+            # write again.
+            shell("flashrom -i GBB -w '%s' --fast-verify" % name)
+
+    def shop_floor_thread(self):
+        """Task thread for writing HWID by shop floor system."""
+        def update_label(text):
+            with gtk.gdk.lock:
+                self.label.set_text(text)
+        try:
+            update_label("Fetching HWID information...")
+            hwid = shopfloor.get_hwid()
+
+            update_label("Writing HWID: [%s]" % hwid)
+            self.write_hwid(hwid)
+        except:
+            self._fail_msg = "Failed writing HWID: %s" % sys.exc_info()[1]
+            logging.exception("Execption when writing HWID by shop floor")
+        finally:
+            gobject.idle_add(gtk.main_quit)
+
+    def run_shop_floor(self):
+        """Runs with shop floor system."""
+        self.label = ful.make_label('Preparing HWID...')
+        test_widget = self.label
+        thread.start_new_thread(self.shop_floor_thread, ())
+        ful.run_test_widget(self.job, test_widget)
+
+    def run_interactively(self):
+        """Runs interactively (without shop floor system)."""
+        # TODO(hungte) A complete user interface to select modules
+        self._fail_msg = "Interactive mode HWID probing is not implemented yet."
+
+    def run_once(self):
+        factory.log('%s run_once' % self.__class__)
+        self._fail_msg = None
+
+        gtk.gdk.threads_init()
+        if shopfloor.is_enabled():
+            self.run_shop_floor()
+        else:
+            self.run_interactively()
+
+        factory.log('%s run_once finished' % repr(self.__class__))
+        if self._fail_msg:
+            raise error.TestFail(self._fail_msg)
