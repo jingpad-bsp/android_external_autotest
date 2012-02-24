2be7f65429ed8c2b1002345e3b1258e40cb5c227
Vic Yang <victoryang@chromium.org>
2012-02-24 14:13:57 -0800

    Keyboard Backlight functional test.
    This test uses ectool to control keyboard backlight. Requires operator
    inspection on whether backlight is lit on and turned off.
    
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    
    BUG=chrome-os-partner:6613
    TEST=Tested UI behaviour on alex.
    Tested ectool is actually invoked.
    
    Change-Id: If4ab28ee52222e180c47745bb40b2a7067bc6c71
    Reviewed-on: https://gerrit.chromium.org/gerrit/16746
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Ready: Vic Yang <victoryang@chromium.org>
    Tested-by: Vic Yang <victoryang@chromium.org>

---
+++ client/site_tests/factory_KeyboardBacklight/factory_KeyboardBacklight.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+# DESCRIPTION :
+#
+# This is a factory test to test the keyboard backlight. Keyboard backlight will
+# light up and operator will check if he/she can see the light. Then backlight
+# will dim and operator will check if the light is off.
+
+
+import gtk
+import logging
+
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib import utils
+from autotest_lib.client.cros import factory
+from autotest_lib.client.cros.factory import ui as ful
+
+
+_LABEL_BIG_SIZE = (280, 60)
+_LABEL_STATUS_SIZE = (140, 30)
+_LABEL_START_STR = ('hit SPACE to start keyboard backlight test\n'
+                    '按空白鍵開始各項鍵盤背光測試\n\n')
+_SUBTEST_LIST = ['Light up', 'Dim']
+
+
+class factory_KeyboardBacklight(test.test):
+    version = 1
+
+    def kblight_subtest_widget(self, name):
+        vb = gtk.VBox()
+        ebh = gtk.EventBox()
+        ebh.modify_bg(gtk.STATE_NORMAL, ful.LABEL_COLORS[ful.ACTIVE])
+        ebh.add(ful.make_label(name, size=_LABEL_BIG_SIZE,
+                               fg=ful.BLACK))
+        vb.pack_start(ebh)
+        vb.pack_start(ful.make_vsep(3), False, False)
+        if name == "Light up":
+            lab_str = ('Check if keyboard backlight lights up.\n'
+                       '請檢查鍵盤背光是否點亮\n')
+        else:
+            lab_str = ('Check if keyboard backlight turns off.\n'
+                       '請檢查鍵盤背光是否熄滅\n')
+        vb.pack_start(ful.make_label(lab_str, fg=ful.WHITE))
+        vb.pack_start(ful.make_vsep(3), False, False)
+        vb.pack_start(ful.make_label(ful.USER_PASS_FAIL_SELECT_STR,
+                                     fg=ful.WHITE))
+
+        # Need event box to effect bg color.
+        eb = gtk.EventBox()
+        eb.modify_bg(gtk.STATE_NORMAL, ful.BLACK)
+        eb.add(vb)
+
+        self._subtest_widget = eb
+
+        self._test_widget.remove(self._top_level_test_list)
+        self._test_widget.add(self._subtest_widget)
+        self._test_widget.show_all()
+
+    def goto_next_subtest(self):
+        if not self._subtest_queue:
+            gtk.main_quit()
+            return
+        self._current_subtest = self._subtest_queue.pop()
+        name = self._current_subtest
+        self._status_map[name] = ful.ACTIVE
+        if name == "Light up":
+                result = utils.system_output('ectool pwmsetkblight 100')
+        else:
+                result = utils.system_output('ectool pwmsetkblight 0')
+        logging.info(result)
+
+    def cleanup_subtest(self):
+        self._test_widget.remove(self._subtest_widget)
+        self._subtest_widget = None
+        self._test_widget.add(self._top_level_test_list)
+        self._test_widget.show_all()
+        self.goto_next_subtest()
+
+    def key_release_callback(self, widget, event):
+        name = self._current_subtest
+        # Make sure we capture more advanced key events only when
+        # entered a subtest.
+        if self._subtest_widget is None:
+            if event.keyval == gtk.keysyms.space:
+                # Start subtest.
+                self.kblight_subtest_widget(name)
+            return True
+        if event.keyval == gtk.keysyms.Tab:
+            self._status_map[name] = ful.FAILED
+            self.cleanup_subtest()
+        elif event.keyval == gtk.keysyms.Return:
+            self._status_map[name] = ful.PASSED
+            self.cleanup_subtest()
+        elif event.keyval == ord('Q'):
+            gtk.main_quit()
+        self._test_widget.queue_draw()
+        return True
+
+    def label_status_expose(self, widget, event, label=None, name=None):
+        status = self._status_map[name]
+        if label:
+            label.set_text(status)
+            label.modify_fg(gtk.STATE_NORMAL, ful.LABEL_COLORS[status])
+
+    def make_subtest_label_box(self, name):
+        eb = gtk.EventBox()
+        eb.modify_bg(gtk.STATE_NORMAL, ful.BLACK)
+        label_status = ful.make_label(ful.UNTESTED, size=_LABEL_STATUS_SIZE,
+                                      alignment=(0, 0.5),
+                                      fg=ful.LABEL_COLORS[ful.UNTESTED])
+        eb.connect('expose_event', self.label_status_expose, label_status, name)
+        label_en = ful.make_label(name, alignment=(1,0.5))
+        label_sep = ful.make_label(' : ', alignment=(0.5, 0.5))
+        hbox = gtk.HBox()
+        hbox.pack_end(label_status, False, False)
+        hbox.pack_end(label_sep, False, False)
+        hbox.pack_end(label_en, False, False)
+        eb.add(hbox)
+        return eb
+
+    def register_callbacks(self, window):
+        window.connect('key-release-event', self.key_release_callback)
+        window.add_events(gtk.gdk.KEY_RELEASE_MASK)
+
+    def run_once(self):
+
+        factory.log('%s run_once' % self.__class__)
+
+        self._subtest_queue = [x for x in reversed(_SUBTEST_LIST)]
+        self._status_map = dict((n, ful.UNTESTED) for n in _SUBTEST_LIST)
+
+        prompt_label = ful.make_label(_LABEL_START_STR, alignment=(0.5, 0.5))
+
+        self._top_level_test_list = gtk.VBox()
+        self._top_level_test_list.pack_start(prompt_label, False, False)
+
+        for name in _SUBTEST_LIST:
+            label_box = self.make_subtest_label_box(name)
+            self._top_level_test_list.pack_start(label_box, False, False)
+
+        self._test_widget = gtk.EventBox()
+        self._test_widget.modify_bg(gtk.STATE_NORMAL, ful.BLACK)
+        self._test_widget.add(self._top_level_test_list)
+
+        self._subtest_widget = None
+
+        self.goto_next_subtest()
+
+        ful.run_test_widget(self.job, self._test_widget,
+            window_registration_callback=self.register_callbacks)
+
+        failed_set = set(name for name, status in self._status_map.items()
+                         if status is not ful.PASSED)
+        if failed_set:
+            raise error.TestFail('some subtest failed (%s)' %
+                                 ', '.join(failed_set))
+
+        factory.log('%s run_once finished' % self.__class__)
