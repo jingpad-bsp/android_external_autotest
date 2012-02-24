184c40f9ffc95f954ebe355fa4c51461d76fd764
Chris Masone <cmasone@chromium.org>
2012-02-23 20:21:43 -0800

    [autotest] RPC retry logic shouldn't retry RPCs that can't succeed
    RPC retry logic now sends a noop RPC upon failure of the chosen RPC.
    If the noop succeeds, we know that autotest is functioning, but that
    our RPC failed.  It's not worth retrying, because it's unlikely that
    there was a transient failure; rather, there was probably something
    wrong with the RPC we were trying to send.
    
    BUG=chromium-os:26768
    TEST=unit
    TEST=run a job, stop the AFE.  It should continue retrying.
    TEST=run a suite job with an incorrect board name.  It should not retry.
    STATUS=Fixed
    
    Change-Id: I30cdc5fb43881c86efc585cbc556a4bcca5ba0b8
    Reviewed-on: https://gerrit.chromium.org/gerrit/16608
    Tested-by: Chris Masone <cmasone@chromium.org>
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Commit-Ready: Chris Masone <cmasone@chromium.org>

--- server/cros/frontend_wrappers_unittest.py
+++ server/cros/frontend_wrappers_unittest.py
@@ -23,6 +23,7 @@ class FrontendWrappersTest(mox.MoxTestBase):
     def setUp(self):
         super(FrontendWrappersTest, self).setUp()
         self._FLAKY_FLAG = False
+        self.mox.StubOutWithMock(frontend_wrappers, 'try_noop_rpc')
 
 
     def testRetryDecoratorSucceeds(self):
@@ -54,11 +55,13 @@ class FrontendWrappersTest(mox.MoxTestBase):
             raise Exception
 
         deadline = time.time() + timeout_sec
+        frontend_wrappers.try_noop_rpc().AndReturn(False)
+        self.mox.ReplayAll()
         self.assertTrue(flaky_succeed())
         self.assertTrue(time.time() < deadline)
 
 
-    def testRetryDecoratorFailss(self):
+    def testRetryDecoratorFails(self):
         """Tests that a wrapped function retries til the timeout, then fails."""
         timeout_min = .01
         timeout_sec = timeout_min * 60
@@ -69,5 +72,24 @@ class FrontendWrappersTest(mox.MoxTestBase):
             raise Exception()
 
         deadline = time.time() + timeout_sec
+        frontend_wrappers.try_noop_rpc().AndReturn(False)
+        self.mox.ReplayAll()
         self.assertRaises(Exception, fail)
         self.assertTrue(time.time() >= deadline)
+
+
+    def testRetryDecoratorBails(self):
+        """Tests that a wrapped function bails when the NOOP RPC succeeds."""
+        timeout_min = .1
+        timeout_sec = timeout_min * 60
+        @frontend_wrappers.retry(Exception,
+                                 timeout_min=timeout_min,
+                                 delay_sec=1)
+        def fail():
+            raise Exception()
+
+        deadline = time.time() + timeout_sec
+        frontend_wrappers.try_noop_rpc().AndReturn(True)
+        self.mox.ReplayAll()
+        self.assertRaises(Exception, fail)
+        self.assertTrue(time.time() < deadline)
