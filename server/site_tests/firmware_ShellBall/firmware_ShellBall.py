fe6d97c5b4ac6fdedf38a3a6ef1bc52784cdadf5
Rajesh Chenna <rchenna@chromium.org>
2012-03-06 12:33:16 -0800

    Functional tests for shellball using FAFT.
    TEST=FAFT
    BUG=None
    
    Change-Id: Ic02ad11265c99a9263c9ecaaa6a91712e8363617
    Reviewed-on: https://gerrit.chromium.org/gerrit/17424
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Commit-Ready: Rajesh Chenna <rchenna@chromium.org>
    Tested-by: Rajesh Chenna <rchenna@chromium.org>

---
+++ server/site_tests/firmware_ShellBall/firmware_ShellBall.py
@@ -0,0 +1,88 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+from autotest_lib.server import test, autotest
+from autotest_lib.server.cros.faftsequence import FAFTSequence
+
+class firmware_ShellBall(FAFTSequence):
+    """
+    chromeos-firmwareupdate functional tests.
+
+    Checks the mode condition and enables or disables developement mode
+    accordingly and runs all shellball functioanl tests.
+    """
+    version = 1
+
+    _shellball_name = None
+
+    def update_firmware(self, mode):
+        self.faft_client.run_shell_command('%s --mode %s' %
+            (self._shellball_name, mode))
+        # Enalbe dev mode if the mode is todev.
+        if mode == 'todev':
+            self.servo.enable_development_mode()
+        # Disable dev mode if the mode is tonormal.
+        elif mode == 'tonormal':
+            self.servo.disable_development_mode()
+
+    def install_original_firmware(self):
+        self.faft_client.run_shell_command('sudo chromeos-firmwareupdate' \
+                                           ' --mode=factory_install')
+
+    def setup(self, host=None, shellball_path=None, shellball_name=None):
+        super(firmware_ShellBall, self).setup()
+        self._shellball_name = "/home/chronos/%s" % self._shellball_name
+        host.send_file("%s/%s" %(shellball_path, shellball_name),
+                       self._shellball_name)
+        self.faft_client.run_shell_command('chmod +x %s' %
+                                           self._shellball_name)
+        self.setup_dev_mode(dev_mode=False)
+        # Get crossystem fwid.
+        [self._current_fwid] = self.faft_client.run_shell_command_get_output(
+                                               'crossystem fwid')
+        # Get BIOS version from shellball.
+        [self._shellball_fwid] = self.faft_client.run_shell_command_get_output(
+                                                 '%s -V | grep "BIOS version"' \
+                                                 ' | sed "s/BIOS version:' \
+                                                 '\(.*\)/\\1/" '
+                                                 % self._shellball_name)
+
+    def cleanup(self):
+        if os.path.exists(self._shellball_name):
+            os.remove(self._shellball_name)
+        super(firmware_ShellBall, self).cleanup()
+
+
+    def run_once(self, host=None):
+        self.register_faft_sequence((
+            { # Step 1, change to devmode.
+                'state_checker': (self.crossystem_checker, {
+                    'dev_boot_usb': '0',
+                 }),
+                'userspace_action': (self.update_firmware, 'todev'),
+                'firmware_action': (self.wait_fw_screen_and_ctrl_d),
+            },
+            { # Step 2, check mainfw_type and run autoupdate.
+                'state_checker': (self.crossystem_checker, {
+                     'mainfw_type': 'developer'
+                 }),
+                'userspace_action': (self.update_firmware, 'autoupdate'),
+                'firmware_action': (self.wait_fw_screen_and_ctrl_d),
+            },
+            { # Step 3, verify fwid and install system firmware.
+                'state_checker': (self.crossystem_checker, {
+                    'fwid': self._shellball_fwid
+                }),
+                'userspace_action': (self.install_original_firmware),
+                'firmware_action': (self.wait_fw_screen_and_ctrl_d),
+            },
+            { # Step 4, verify the old firmware id.
+                'state_checker': (self.crossystem_checker, {
+                    'fwid': self._current_fwid
+                }),
+            }
+        ))
+        self.run_faft_sequence()
