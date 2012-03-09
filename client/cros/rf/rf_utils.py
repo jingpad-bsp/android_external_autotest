abb028eb66762c97232287f431fb19afc81b99b5
Jon Salz <jsalz@chromium.org>
2012-03-09 17:32:32 +0800

    Don't set IP address of interface if it is already set.
    BUG=chrome-os-partner:6623
    TEST=Manual
    
    Change-Id: I073a160ab2323a9091b58aa3d489f3f54c960ba0
    Reviewed-on: https://gerrit.chromium.org/gerrit/17682
    Reviewed-by: Chun-Ta Lin <itspeter@chromium.org>
    Commit-Ready: Jon Salz <jsalz@chromium.org>
    Tested-by: Jon Salz <jsalz@chromium.org>

--- client/cros/rf/rf_utils.py
+++ client/cros/rf/rf_utils.py
@@ -2,9 +2,23 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
+import re
 import rf_common
 
 from autotest_lib.client.bin import utils
 
 def SetInterfaceIp(interface, ip):
+    '''Sets the IP address for a network interface.
+
+    The address is set only if the interface does not already have an
+    assigned IP address.
+    '''
+    ip_output = utils.system_output(
+        'ip addr show dev %s' % interface)
+    match = re.search('^\s+inet ([.0-9]+)', ip_output, re.MULTILINE)
+    if match:
+        logging.info('Not setting IP address for interface %s: '
+                     'already set to %s' % (interface, match.group(1)))
+        return
     utils.system('ifconfig %s %s' % (interface, ip))
