6d86aecef3e2a0123d12c239c14c978543eef18f
Mike Frysinger <vapier@chromium.org>
2012-03-09 11:09:12 -0500

    ltp fixes from upstream
    Building ltp for x86 boards with PIE/PIC fails due to poor code in the
    LTP project.  This has been fixed upstream, so this is just adding the
    patches to our local cache.
    
    BUG=None
    TEST=`emerge-x86-alex autotest-deps` works
    
    Change-Id: I985df47504e64c5b523adfd5a42d5372a6db9457
    Reviewed-on: https://gerrit.chromium.org/gerrit/17838
    Reviewed-by: Mike Truty <truty@chromium.org>
    Commit-Ready: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- client/tests/ltp/ltp.py
+++ client/tests/ltp/ltp.py
@@ -40,6 +40,8 @@ class ltp(test.test):
         ltpbin_dir = os.path.join(self.srcdir, 'bin')
         os.mkdir(ltpbin_dir)
 
+        utils.system('patch -p1 < ../getdents.patch')
+        utils.system('patch -p1 < ../cpuid.patch')
         utils.make('autotools')
         utils.configure('--prefix=%s' % ltpbin_dir)
         utils.make('-j %d all' % utils.count_cpus())
