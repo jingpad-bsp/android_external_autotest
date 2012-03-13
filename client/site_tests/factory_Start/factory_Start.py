2e6becdcbc848608b2e91464f1327be5e39331ee
Hung-Te Lin <hungte@chromium.org>
2012-03-13 22:10:47 +0800

    factory: Improve factory_Start error messages.
    factory_Start, as the first test and the one to initial shop floor server
    testing, should provide better information when:
     - user forgot to set shop floor server url (or not using mini-omaha)
     - didn't attach network (for name / address lookup failure)
     - set wrong url (network or i/o failure)
    
    BUG=chrome-os-partner:6911
    TEST=Manually performed factory_Start, w/o network, and w/o mini-omaha.
    
    Change-Id: Ie3949b34d27d04a6631d4eb338d02919f4af8865
    Reviewed-on: https://gerrit.chromium.org/gerrit/17962
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>

--- client/site_tests/factory_Start/factory_Start.py
+++ client/site_tests/factory_Start/factory_Start.py
@@ -16,13 +16,14 @@
 # 'press_to_continue': Prompts and waits for a key press (SPACE) to continue.
 
 import glob
+import logging
 import os
+import socket
 import sys
 
 import gobject
 import gtk
 import pango
-from gtk import gdk
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
@@ -41,6 +42,18 @@ _MSG_TASK_SERIAL = (
 _MSG_TASK_SPACE = (
         'Hit SPACE to start testing...\n'
         '按 "空白鍵" 開始測試...')
+
+_MSG_NO_SHOP_FLOOR_SERVER_URL = (
+        'No shop floor server URL. Auto-testing stopped.\n\n'
+        'Please install the factory test image using the mini-Omaha server\n'
+        'rather than booting from a USB drive.\n\n'
+        'For debugging or development, use the listed hot-keys to start\n'
+        'individual tests.\n\n'
+        '未指定 Shop Floor 伺服器位址，停止自動測試。\n\n'
+        '請使用完整的 mini-Omaha 伺服器安裝測試程式，\n'
+        '不要直接從 USB 碟開機執行。\n\n'
+        '若想除錯或執行部份測試，請直接按下對應熱鍵。')
+
 _LABEL_FONT = pango.FontDescription('courier new condensed 24')
 
 
@@ -68,7 +81,7 @@ class Task(object):
 class PressSpaceTask(Task):
     def start(self):
         window = self._ui.get_window()
-        window.add_events(gdk.KEY_PRESS_MASK)
+        window.add_events(gtk.gdk.KEY_PRESS_MASK)
         self.callback = (window, window.connect('key-press-event',
                                                 self.check_space))
         return ful.make_label(_MSG_TASK_SPACE, font=ful.LABEL_LARGE_FONT)
@@ -143,22 +156,44 @@ class ShopFloorTask(Task):
     def __init__(self, ui, server_url):
         Task.__init__(self, ui)
         self.server_url = server_url or shopfloor.detect_default_server_url()
-        shopfloor.set_server_url(self.server_url)
 
     def start(self):
+        # Many developers will try to run factory test image directly without
+        # mini-omaha server, so we should either alert and fail, or ask for
+        # server address.
+        if not self.server_url:
+            return ful.make_label(_MSG_NO_SHOP_FLOOR_SERVER_URL,
+                    fg=ful.RED)
+
+        shopfloor.set_server_url(self.server_url)
         return ful.make_input_window(
                 prompt=_MSG_TASK_SERIAL,
                 on_validate=self.validate_serial_number,
                 on_complete=self.complete_serial_task)
 
     def validate_serial_number(self, serial):
+        # This is a callback function for widgets created by make_input_window.
+        # When the input is not valid (or temporary network failure), either
+        # return False or raise a ValueError with message to be displayed in
+        # bottom status line of input window.
         try:
+            # All exceptions
             shopfloor.check_serial_number(serial.strip())
             return True
         except shopfloor.Fault as e:
-            factory.log("Server Error: %s" % e)
+            logging.exception("ServerFault:")
+            raise ful.InputError("Server error:\n%s" %
+                             e.faultString.partition(':')[2])
+        except ValueError as e:
+            logging.exception("ValueError:")
+            raise ful.InputError(e.message)
+        except socket.gaierror as e:
+            raise ful.InputError("Network failure (address error).")
+        except socket.error as e:
+            raise ful.InputError("Network failure:\n%s" % e[1])
         except:
-            factory.log("Unknown exception: %s" % repr(sys.exc_info()))
+            logging.exception("UnknownException:")
+            raise ful.InputError(sys.exc_info()[1])
         return False
 
     def complete_serial_task(self, serial):
