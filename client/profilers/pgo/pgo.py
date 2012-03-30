4ac8cb9f6af6c36bfd9ad4214131ad63d24a36b5
Ahmad Sharif <asharif@chromium.org>
2012-03-29 17:32:11 -0700

    Added a profiler called pgo.
    This autotest copies files dumped by Chrome when built with -fprofile-generate.
    These files can then be fed back into the build process to build an optimized
    Chrome.
    
    BUG=chromium-os:25188
    TEST=./run_remote_tests --profile --profilers="pgo" desktopui_PageCyclerTests
    Verified that PGO data is copied back to the host.
    
    Change-Id: I7e77c049718f1dfe4957093915280d01231dc1a8
    Reviewed-on: https://gerrit.chromium.org/gerrit/19342
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Tested-by: asharif <asharif@chromium.org>
    Commit-Ready: asharif <asharif@chromium.org>

---
+++ client/profilers/pgo/pgo.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+This is a profiler class for copying Profile-Guided-Optimization (PGO) data
+files back to the host. When Chrome is built with -fprofile-generate, it dumps
+its PGO data in a directory that this test copies back to test.profdir.
+
+The PGO data is found where the build happens in the chroot, which is hardcoded
+as the source_dir below.
+"""
+
+import logging
+import os
+import shutil
+from autotest_lib.client.bin import profiler
+
+
+class pgo(profiler.profiler):
+    version = 1
+
+    def initialize(self,
+                   source_dir='var/cache/distfiles/target/chrome-src/src/c'):
+        self._source_dir = source_dir
+
+
+    def start(self, test):
+        # Remove the .gcda files first.
+        if os.path.exists(self._source_dir):
+            shutil.rmtree(self._source_dir)
+
+
+    def stop(self, test):
+        if os.path.isdir(self._source_dir):
+            shutil.copytree(self._source_dir,
+                            os.path.join(test.profdir, 'pgo'))
+        else:
+            logging.error('PGO dir: %s not found', self._source_dir)
