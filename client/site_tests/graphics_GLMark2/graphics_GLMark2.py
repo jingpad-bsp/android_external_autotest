372fd8ae01e724dbfc0812bbecf6e1e3516d2c7d
Chinyue Chen <chinyue@chromium.org>
2012-04-06 12:03:57 +0800

    A new autotest to run glmark2 on target machine.
    This test could be used as both functional and performance test.
    
    BUG=chrome-os-partner:8585
    TEST=./run_remote_tests.sh --board=lumpy --remote=${REMOTE_IP}
                               --use_emerged graphics_GLMark2
    
    Change-Id: I15bcb36c9e7ddc2a6568fd5e43d118e389d2e9a1
    Reviewed-on: https://gerrit.chromium.org/gerrit/19719
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Commit-Ready: Chinyue Chen <chinyue@chromium.org>
    Tested-by: Chinyue Chen <chinyue@chromium.org>

---
+++ client/site_tests/graphics_GLMark2/graphics_GLMark2.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Run the test with validation_mode=True to invoke glmark2 to do quick
+# validation that runs in a second. When glmark2 is run in normal mode, it
+# outputs a final performance score, and the test checks the performance score
+# against minimum requirement if min_score is set.
+
+import logging
+import os
+import re
+
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
+
+
+GLMARK2_SCORE_RE = 'glmark2 Score: (\d+)'
+
+
+class graphics_GLMark2(test.test):
+    version = 1
+    preserve_srcdir = True
+
+    def setup(self):
+        self.job.setup_dep(['glmark2'])
+
+    def run_once(self, size='800x600', validation_mode=False, min_score=None):
+        dep = 'glmark2'
+        dep_dir = os.path.join(self.autodir, 'deps', dep)
+        self.job.install_pkg(dep, 'dep', dep_dir)
+        glmark2 = os.path.join(dep_dir, 'bin/glmark2')
+        options = []
+        options.append('--size %s' % size)
+        if validation_mode:
+           options.append('--validate')
+        else:
+           options.append('--annotate')
+        cmd = '%s %s' % (glmark2, ' '.join(options))
+        if not os.getenv('DISPLAY'):
+            cmd = 'X :1 & sleep 1; DISPLAY=:1 %s; kill $!' % cmd
+
+        result = utils.run(cmd)
+        for line in result.stderr.splitlines():
+            if line.startswith('Error:'):
+                raise error.TestFail(line)
+
+        if not validation_mode:
+            score = None
+            for line in result.stdout.splitlines():
+                # glmark2 output the final performance score as:
+                #   glmark2 Score: 530
+                match = re.findall(GLMARK2_SCORE_RE, line)
+                if match:
+                    score = int(match[0])
+            if score is None:
+                raise error.TestFail('Unable to read benchmark score')
+            logging.info('GLMark2 score: %d', score)
+
+            if min_score is not None and score < min_score:
+                raise error.TestFail('Benchmark score %d < %d (minimum score '
+                                     'requirement)' % (score, min_score))
