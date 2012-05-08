01310f99e1aa2862af087583a0815c3271fc10c1
Scott Zawalski <scottz@chromium.org>
2012-05-08 09:13:07 -0700

    Revert "[autotest] Re-land "Add logic to symbolicate dumps using dev server""
    Cherrypy is hanging when we send multiple POSTs using request to it in a short
    amount of time
    
    BUG=30650
    
    This reverts commit 043bf47e7946366517757cdb981de99eb81adcb6
    
    Change-Id: I6f91b730289d14502eeffcf672ccb36494d5f847
    Reviewed-on: https://gerrit.chromium.org/gerrit/22115
    Commit-Ready: Scott Zawalski <scottz@chromium.org>
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Tested-by: Scott Zawalski <scottz@chromium.org>

--- server/site_crashcollect.py
+++ server/site_crashcollect.py
@@ -4,9 +4,7 @@
 
 import logging
 import os
-import urllib2
 from autotest_lib.client.common_lib import utils as client_utils
-from autotest_lib.client.common_lib.cros import dev_server
 from autotest_lib.client.cros import constants
 from autotest_lib.server import utils
 
@@ -28,26 +26,6 @@ def generate_minidump_stacktrace(minidump_path):
     return rc
 
 
-def symbolicate_minidump_with_devserver(minidump_path):
-    """
-    Generates a stack trace for the specified minidump by consulting devserver.
-
-    This function assumes the debug symbols have been staged on the devserver.
-    @return HTTP response code of the attempt.
-    """
-    devserver = dev_server.DevServer.create()
-    try:
-        trace_text = devserver.symbolicate_dump(minidump_path)
-        if not trace_text:
-            return 400
-        with open(minidump_path + '.txt', 'w') as trace_file:
-            trace_file.write(trace_text)
-    except urllib2.HTTPError as e:
-        logging.warn('HTTPError while symbolicating minidump: \n%r', e)
-        return e.code
-    return 200
-
-
 def find_and_generate_minidump_stacktraces(host_resultdir):
     """
     Finds all minidump files and generates a stack trace for each.
@@ -66,16 +44,9 @@ def find_and_generate_minidump_stacktraces(host_resultdir):
             if rc == 0:
                 logging.info('Generated stack trace for dump %s', minidump)
                 return
-
-            http_rc = symbolicate_minidump_with_devserver(minidump)
-            if http_rc == 200:
-                logging.info('Generated stack trace for dump %s', minidump)
-                return
-
-            logging.warn('Failed to generate stack trace locally for ' \
-                         'dump %s (rc=%d)', minidump, rc)
-            logging.warn('Failed to generate stack trace on devserver for ' \
-                         'dump %s (rc=%d)', minidump, http_rc)
+            else:
+                logging.warn('Failed to generate stack trace for ' \
+                             'dump %s (rc=%d)' % (minidump, rc))
 
 
 def fetch_orphaned_crashdumps(host, host_resultdir):
