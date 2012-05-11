df05bd99b42d727f42d2752418869dd82154eb52
Elly Jones <ellyjones@chromium.org>
2012-05-11 14:50:18 -0400

    [autotest] update security_OpenSSLBlacklist
    Generate new certs specifically for the test with longer expiration times, and
    update the test to use the new blacklist file format instead of the old one.
    
    TEST=Yes
    BUG=chromium-os:21100
    
    Change-Id: I297991652915f58ce98ed69e651547c41f32555f
    Signed-off-by: Elly Jones <ellyjones@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/22477
    Reviewed-by: Kees Cook <keescook@chromium.org>

--- client/site_tests/security_OpenSSLBlacklist/security_OpenSSLBlacklist.py
+++ client/site_tests/security_OpenSSLBlacklist/security_OpenSSLBlacklist.py
@@ -7,53 +7,26 @@ import os
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 
-
 OPENSSL = '/usr/bin/openssl'
 VERIFY = OPENSSL + ' verify'
-BLACKLIST = '/etc/ssl/blacklist'
 
 class security_OpenSSLBlacklist(test.test):
     version = 1
 
-    def blacklist(self, fingerprint):
-        f = open(BLACKLIST, 'a+')
-        f.write('%s\n' % fingerprint)
-
-    def unblacklist(self, fingerprint):
-        with open(BLACKLIST, 'r') as f:
-            lines = f.readlines()
-            lines = [x.strip() for x in lines]
-            lines = [x for x in lines if x != fingerprint]
-        for line in lines:
-            print "'%s' != '%s'" % (line, fingerprint)
-        with open(BLACKLIST, 'w') as f:
-            f.writelines(lines)
-
-    def verify(self):
-        r = os.system('%s -CAfile %s %s' % (VERIFY, self.ca, self.cert))
+    def verify(self, blacklist='/dev/null'):
+        r = os.system('OPENSSL_BLACKLIST_PATH=%s %s -CAfile %s %s' %
+            (blacklist, VERIFY, self.ca, self.cert))
         return r == 0
 
     def run_once(self, opts=None):
+        self.blacklist = '%s/blacklist' % self.srcdir
+        self.bogus_blacklist = '%s/bogus_blacklist' % self.srcdir
         self.ca = '%s/ca.pem' % self.srcdir
         self.cert = '%s/cert.pem' % self.srcdir
-        # This fingerprint comes from 'openssl x509 -in foo.pem -fingerprint -sha256'
-        self.certfp = 'f641c36cfef49bc071359ecf88eed9317b738b5989416ad401720c0a4e2e6352'
-        # ... and this one comes from 'head -c 16 /dev/urandom | sha256sum' :)
-        self.bogus_certfp = 'bb708578662b7202b7ac3f420013a8a765e0b8687109a2cbba2b5a625358788f'
 
-        try:
-            os.system('mv %s %s.old' % (BLACKLIST, BLACKLIST))
-            os.system('touch %s' % BLACKLIST)
-            if not self.verify():
-                raise error.TestFail('Certificate does not verify normally.')
-            self.blacklist(self.certfp)
-            if self.verify():
-                raise error.TestFail('Certificate verified when blacklisted.')
-            self.unblacklist(self.certfp)
-            if not self.verify():
-                raise error.TestFail('Certificate does not verify when unblacklisted.')
-            self.blacklist(self.bogus_certfp)
-            if not self.verify():
-                raise error.TestFail('Certificate does not verify with nonempty blacklist.')
-        finally:
-            os.system('mv %s.old %s' % (BLACKLIST, BLACKLIST))
+        if not self.verify():
+            raise error.TestFail('Certificate does not verify normally.')
+        if self.verify(blacklist=self.blacklist):
+            raise error.TestFail('Certificate verified when blacklisted.')
+        if not self.verify(blacklist=self.bogus_blacklist):
+            raise error.TestFail('Certificate does not verify with nonempty blacklist.')
