cdf9890c2a478617383e419a9dbd61c89364e70d
Taylor Hutt <thutt@chromium.org>
2012-05-14 14:54:20 -0700

    autotest: sound_infrastructure: Remove non-useful sections of test
    Details
    
      This change removes portions of the sound_infrastructure test which
      are not providing useful testing because the Alsa controls exported
      by some drivers are going through rapid change.
    
    BUG=none
    TEST=Ran test against stumpy, kaen
    Signed-off-by: Taylor Hutt <thutt@chromium.org>
    
    Change-Id: Ib825685844e67d4db66bae3d4d4ac98fdf06a88a
    Reviewed-on: https://gerrit.chromium.org/gerrit/22649
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Tested-by: Taylor Hutt <thutt@chromium.org>
    Commit-Ready: Taylor Hutt <thutt@chromium.org>

--- client/tests/sound_infrastructure/sound_infrastructure.py
+++ client/tests/sound_infrastructure/sound_infrastructure.py
@@ -10,47 +10,9 @@ class sound_infrastructure(test.test):
     """
     Tests that the expected sound infrastructure is present.
 
-    If a control or file is found to be missing, that would imply that
-    the driver has changed, and this will have an impact on the entire
-    sound system.
-
-     o Missing control
-
-       Regenerate '/etc/asound.state' for each board which is missing
-       the control.
-
-         - Log in to console on device.
-         - Set all controls to desired levels.
-         - Execute 'alsactl --file /tmp/asound.state store 0'.
-         - Copy new '/tmp/asound.state' from device to correct place
-           in source tree.  The correct place should normally
-           correspond to the 'audioconfig-board' ebuild file for that
-           specific board.
-
-       Search the Chromium OS & Chromium sources for that missing
-       control name.  If there are references to that control name,
-       adjust the sources as necessary.
-
-     o Added control (TODO(thutt): added controls not yet checked)
-
-       Regenerate '/etc/asound.state' for each affected board.
-
-       Validate that sound controls still work on each affected target
-       device:
-
-          Volume Up, Volume Down, Mute
-          Any UI for controlling AV output
-
-       Notify Chrome developers, if necessary, of new control.
-       Notify GTalk developers, if necessary, of new control.
-
-     o Missing file, added file
-
-       I don't know what implications this might have, so use caution
-       here and update these instructions in the case that this
-       occurs.
-
-       At the very least, verify that everything still functions.
+    If a file is found to be missing, that would imply that the driver
+    has changed, and this will have an impact on the entire sound
+    system.
 
     Before submitting the fixes, test & verify:
 
@@ -65,20 +27,6 @@ class sound_infrastructure(test.test):
           Internal Speakers
           HDMI
           Internal Mic
-
-     o Notify Chrome developers, if necessary, of removed control.
-     o Notify GTalk developers, if necessary, of removed control.
-     o Notify Flash player developers, if necessary, of removed control.
-
-     o To create 'controls' data file:
-
-        Execute the following on the DUT:
-
-          amixer -c0 controls | cut -d ',' -f 2- | sort >{codec-name}.controls
-
-        {codec-name} should be replaced with the name of the codec.
-        Replace spaces in the codec name with '_'.
-        See get_codec() to help determine the name of the codec.
 
      o To create 'files' data file:
 
@@ -95,7 +43,6 @@ class sound_infrastructure(test.test):
 
     codec_info = {
         'ALL': {                # Things common to all sound codecs
-            'controls': [ ],
             'files': [
                 '/etc/init/adhd.conf', # Upstart script, from ADHD package
                 '/etc/asound.state',   # Factory defaults.  From ADHD.
@@ -112,22 +59,18 @@ class sound_infrastructure(test.test):
             },
 
         'WM8903': {
-            'controls': [ ],  # See above for creating 'controls' and 'files'.
             'files'   : [ ],
             },
 
         'ALC271X': {
-            'controls': [ ],
             'files'   : [ ],
             },
 
         'Cirrus Analog': {
-            'controls': [ ],
             'files'   : [ ],
             },
 
         'ALC272': {
-            'controls': [ ],
             'files'   : [ ],
             }
         }
@@ -138,12 +81,6 @@ class sound_infrastructure(test.test):
     def pathname_must_exist(self, pathname):
         if not os.path.exists(pathname):
             logging.error("File missing: '%s'", pathname)
-            return False
-        return True
-
-    def control_must_exist(self, control):
-        if self.exec_cmd("amixer -c0 controls|grep -e \"%s\"" % (control)) != 0:
-            logging.error("Control missing: '%s'", control)
             return False
         return True
 
@@ -175,33 +112,18 @@ class sound_infrastructure(test.test):
                 errors += 1
         return errors
 
-    def validate_controls(self, controls_list):
-        errors = 0
-        for c in controls_list:
-            if not self.control_must_exist(c):
-                errors += 1
-        return errors
-
     def validate_codec(self, codec):
         err_str = ''
         errors = self.validate_files(codec['files'])
         if errors:
             err_str += " files: %d" % errors
 
-        errors = self.validate_controls(codec['controls'])
-        if errors:
-            err_str += " controls: %d" % errors
         if err_str != '':
             err_str = "(%s)%s" % (self._codec_basename, err_str)
         return err_str
 
     def read_codec_data(self, codec):
         self._codec_basename = self.get_codec_basename(codec)
-
-        # Read controls which must be present.
-        pathname = self.get_data_pathname(self._codec_basename + ".controls")
-        self.codec_info[codec]['controls'] = [line.strip() for line in
-                                              open(pathname)]
 
         # Read files which must be present.
         pathname = self.get_data_pathname(self._codec_basename + ".files")
@@ -224,4 +146,3 @@ class sound_infrastructure(test.test):
                                       (err_str))
         else:
             raise error.TestError("No test info for codec '%s'." % (codec))
-        self.load_asound_state()
