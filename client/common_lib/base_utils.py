d4e72c40c906ae602a0979fc3c8db6bf720a6a54
Chris Masone <cmasone@chromium.org>
2012-05-14 16:36:28 -0700

    [autotest] Remove obsolete TODO()
    To help diagnose http://crosbug.com/22019, we added some kernel stack
    logging code on failed process kill.  We actually think that's a good
    idea, so we decided to leave it and just take out the TODO telling us
    to remove it.
    
    BUG=chromium-os:22019
    TEST=suite:bvt should still run
    
    Change-Id: Ibfce0a05009c007fb5c833ae88150ea845978600
    Reviewed-on: https://gerrit.chromium.org/gerrit/22611
    Tested-by: Chris Masone <cmasone@chromium.org>
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Commit-Ready: Chris Masone <cmasone@chromium.org>

--- client/common_lib/base_utils.py
+++ client/common_lib/base_utils.py
@@ -819,7 +819,6 @@ def _wait_for_commands(bg_jobs, start_time, timeout):
                      bg_job.command)
         if nuke_subprocess(bg_job.sp) is None:
             # If process could not be SIGKILL'd, log kernel stack.
-            # TODO(cmasone): Remove when http://crosbug.com/220189 is sorted.
             logging.warn(utils.read_file('/proc/' + bg_job.sp.pid + '/stack'))
         bg_job.result.exit_status = bg_job.sp.poll()
         bg_job.result.duration = time.time() - start_time
