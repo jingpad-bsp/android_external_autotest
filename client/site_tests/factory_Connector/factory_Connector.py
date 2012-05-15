e573e473dda378a039228e25c17398249ae393c0
Chun-Ta Lin <itspeter@chromium.org>
2012-05-15 19:37:59 +0800

    factory: Let factory_Connector work without additional fixtures.
    In order to support various situations, we add an option to examine the
    components without external fixtures(i.e. usb to i2c chipset). To enable such
    option with 'is_internal' set as True in the configuration file.
    
    This CL also add several features, including:
    1) Expose the configuration filename as an autotest argument.
    2) A LCD panel test with brightness control.
    3) Automatic camera probing.
    4) Individual control over sub test items. (camera, display, probing)
    
    BUG=chrome-os-partner:9126
    TEST=Manually test it on Alex.
    
    Change-Id: I6c464887e51cc0ec9d794b6318e7c08064d0db8e
    Reviewed-on: https://gerrit.chromium.org/gerrit/22697
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Chun-Ta Lin <itspeter@chromium.org>
    Commit-Ready: Chun-Ta Lin <itspeter@chromium.org>

--- client/site_tests/factory_Connector/factory_Connector.py
+++ client/site_tests/factory_Connector/factory_Connector.py
@@ -7,6 +7,7 @@ import datetime
 import gtk
 import os
 import pprint
+import re
 import StringIO
 
 from autotest_lib.client.cros import factory
@@ -17,7 +18,7 @@ from autotest_lib.client.cros.factory.media_util import MediaMonitor
 from autotest_lib.client.cros.factory.media_util import MountedMedia
 from autotest_lib.client.cros.i2c import usb_to_i2c
 from autotest_lib.client.cros.rf.config import PluggableConfig
-
+from autotest_lib.client.common_lib import utils
 
 _MESSAGE_USB = (
     'Please insert the usb stick, adapter board and golden reference.\n'
@@ -32,25 +33,35 @@ _MESSAGE_ENTER_SN_HINT = ('Please scan SN on LCD.\n請掃描本體上S/N:')
 _MESSAGE_CAMERA_CHECK = (
     'hit TAB to pass and ENTER to fail.\n' +
     '成功請按 TAB, 錯誤請按 ENTER.\n')
-_MESSAGE_I2C_TESTING = (
-    'I2C testing...\n'
+_MESSAGE_PROBING = (
+    'Probing components...\n'
     '測試中...\n')
+_MESSAGE_DISPLAY = (
+    'Hit TAB if DUT have displayed this string.\n' +
+    'Otherwise, hit ENTER.\n' +
+    '成功在測試品看到此字符請按 TAB, 否則請按 ENTER.\n')
 _MESSAGE_WRITING_LOGS = (
     'Writing logs...\n'
     '紀錄中...\n')
 _MESSAGE_RESULT_TAB = (
     'Results are listed below.\n'
-    'Please disconnect the panel.\n'
+    'Remove the pnael after backlight dimmed.\n'
     '測試結果顯示如下\n'
-    '請將AB Panel移除\n')
+    '確認背光關閉後, 請將Panel移除\n')
+
 
 _TEST_SN_NUMBER = 'TEST-SN-NUMBER'
 COLOR_MAGENTA = gtk.gdk.color_parse('magenta1')
+BRIGHTNESS_CONTROL_CMD = (
+    'echo %s > /sys/class/backlight/intel_backlight/brightness')
 
 class factory_Connector(state_machine.FactoryStateMachine):
-    version = 3
+    version = 4
 
     def setup_tests(self):
+        # Register more states for test procedure with configuration data
+        # loaded from external media device (USB/SD).
+
         # Setup serial number input widget.
         self.sn_input_widget = self.make_serial_number_widget(
             message=_MESSAGE_ENTER_SN_HINT,
@@ -58,29 +69,36 @@ class factory_Connector(state_machine.FactoryStateMachine):
             on_complete=self.on_sn_complete,
             on_keypress=self.on_sn_keypress,
             generate_status_row=True)
-        self.i2c_testing_widget = self.make_decision_widget(
-            message=_MESSAGE_I2C_TESTING, key_action_mapping={})
+        self.register_state(self.sn_input_widget)
+
+        if 'display' in self.config:
+            self.setup_display_test()
+            self.register_state(
+                self.display_widget, None, self.perform_display_test)
+
+        if 'camera' in self.config:
+            self.setup_camera_preview()
+            self.register_state(self.preview_widget, None,
+                                self.perform_camera_preview)
+
+        if 'probing' in self.config:
+            self.setup_probing()
+            self.probing_widget = self.make_decision_widget(
+                message=_MESSAGE_PROBING, key_action_mapping={})
+            self.register_state(self.probing_widget, None,
+                                self.perform_probing)
+
         self.writing_widget = self.make_decision_widget(
             message=_MESSAGE_WRITING_LOGS,
             key_action_mapping={},
             fg_color=COLOR_MAGENTA)
-
-        self.setup_camera_preview()
-        self.setup_i2c_bus()
-        # TODO(itspeter): setup the external display related test.
+        self.register_state(self.writing_widget, None, self.write_log)
 
         # Setup result tab.
         self.result_widget = self.make_result_widget(
             _MESSAGE_RESULT_TAB,
             key_action_mapping={
                 gtk.keysyms.Return: (self.advance_state, [])})
-        # Register more states for test procedure with configuration data
-        # loaded from external media device (USB/SD).
-        self.register_state(self.sn_input_widget)
-        self.register_state(self.preview_widget)
-        self.register_state(self.i2c_testing_widget, None,
-                            self.perform_i2c_bus_test)
-        self.register_state(self.writing_widget, None, self.write_log)
         # After this state (result), go back to "prepare panel" state.
         self.register_state(self.result_widget, None, None,
                             self._STATE_PREPARE_PANEL)
@@ -97,6 +115,7 @@ class factory_Connector(state_machine.FactoryStateMachine):
                 gtk.keysyms.Tab: self.pass_preview_test,
                 gtk.keysyms.Return: self.fail_preview_test},
             msg=_MESSAGE_CAMERA_CHECK,
+            device_index=int(self.camera_config['DEVICE_INDEX']),
             width=int(self.camera_config['WIDTH']),
             height=int(self.camera_config['HEIGHT']))
         self.preview_widget = self.camera_preview.widget
@@ -113,17 +132,58 @@ class factory_Connector(state_machine.FactoryStateMachine):
         self.update_status('camera', False)
         self.advance_state()
 
-    def setup_i2c_bus(self):
-        self.bus_config = self.config['bus']
-        factory.log('I2C bus config is [%s]' % self.bus_config)
-        self.i2c_list = self.bus_config['i2c_list']
-        self.chipset = self.bus_config['chipset']
+    def perform_camera_preview(self):
+        try:
+            if self.camera_config['IS_AUTO']:
+                self.camera_preview.init_device(start_capture=False)
+                ret, cvImg = self.camera_preview.capture_single()
+                self.pass_preview_test()
+            else:
+                self.camera_preview.init_device()
+        except IOError as e:
+            factory.log('Cannot get cammera - %s' % e)
+            self.fail_preview_test()
+
+    def setup_probing(self):
+        self.probing_config = self.config['probing']
+        factory.log('Probing config is [%s]' % self.probing_config)
+        if self.is_internal:
+            self.setup_internal_probing()
+        else:
+            self.setup_external_probing()
+
+    def setup_internal_probing(self):
         # Prepare the status row.
-        for test_name, test_label, _ in self.i2c_list:
+        self.probe_list = self.probing_config['items']
+        for test_name in self.probe_list:
+            self._status_rows.append((test_name, test_name, True))
+            self._results_to_check.append(test_name)
+
+    def setup_external_probing(self):
+        self.probe_list = self.probing_config['i2c_list']
+        self.chipset = self.probing_config['chipset']
+        # Prepare the status row.
+        for test_name, test_label, _ in self.probe_list:
             self._status_rows.append((test_name, test_label, True))
             self._results_to_check.append(test_name)
 
-    def perform_i2c_bus_test(self):
+    def perform_probing(self):
+        if self.is_internal:
+            self.perform_internal_probing()
+        else:
+            self.perform_external_probing()
+        self.advance_state()
+
+    def perform_internal_probing(self):
+        for test_name in self.probe_list:
+            commands = self.probe_list[test_name]
+            self.update_status(test_name, True)
+            for cmd, expect_re in commands:
+                if not self.run_cmd(cmd, expect_re):
+                    self.update_status(test_name, False)
+                    break
+
+    def perform_external_probing(self):
         try:
             controller = usb_to_i2c.create_i2c_controller(self.chipset)
             # Turn on the led light to indicate test in progress.
@@ -131,7 +191,7 @@ class factory_Connector(state_machine.FactoryStateMachine):
             controller.send_and_check_status(
                 98, [0x11, 0x97, 0x80, 0x00, 0x40, 0xE1])
             # Probe the peripherals.
-            for test_name, _, i2c_port in self.i2c_list:
+            for test_name, _, i2c_port in self.probe_list:
                 bus_status = controller.send_and_check_status(
                     eval(i2c_port), [])
                 factory.log("[%s] at port [%s] tested, returned %d" % (
@@ -142,6 +202,37 @@ class factory_Connector(state_machine.FactoryStateMachine):
                     self.update_status(test_name, False)
         except Exception as e:
             factory.log('Exception - %s' % e)
+
+    def set_brightness(self, brightness):
+        if not self.run_cmd(BRIGHTNESS_CONTROL_CMD % brightness, '^$'):
+            raise error.TestNAError('Failed to set brightness')
+
+    def setup_display_test(self):
+        # TODO(itspeter): setup the external display related test.
+        self.display_config = self.config['display']
+        if 'brightness_control' in self.display_config:
+            self.brightness_control = int(
+                self.display_config['brightness_control'])
+        else:
+            self.brightness_control = None
+        # Prepare the status row.
+        self._status_rows.append(('display', 'Display panel', True))
+        self._results_to_check.append('display')
+
+        self.display_widget = self.make_decision_widget(
+            message=_MESSAGE_DISPLAY,
+            key_action_mapping={
+                gtk.keysyms.Return: (self.end_display_test, [False]),
+                gtk.keysyms.Tab: (self.end_display_test, [True])})
+
+    def perform_display_test(self):
+        if self.brightness_control:
+            self.set_brightness(self.brightness_control)
+
+    def end_display_test(self, result):
+        self.update_status('display', result)
+        if self.brightness_control:
+            self.set_brightness(0)
         self.advance_state()
 
     def on_usb_insert(self, dev_path):
@@ -149,8 +240,12 @@ class factory_Connector(state_machine.FactoryStateMachine):
             self.dev_path = dev_path
             with MountedMedia(dev_path, 1) as config_dir:
                 # Load configuration.
-                config_path = os.path.join(config_dir, 'connectivity.params')
+                config_path = os.path.join(config_dir, self.config_file)
                 self.config = self.base_config.Read(config_path)
+                if 'is_internal' in self.config:
+                    self.is_internal = True
+                else:
+                    self.is_internal = False
                 # Configure the items
                 factory.log('Config loaded. Setup the UI...')
                 self.setup_tests()
@@ -171,16 +266,28 @@ class factory_Connector(state_machine.FactoryStateMachine):
         self.log_to_file.write('Serial_number : %s\n' % serial_number)
         self.log_to_file.write('Started at : %s\n' % datetime.datetime.now())
         self.update_status('sn', serial_number)
-        self.perform_camera_preview()
+        self.advance_state()
 
-    def perform_camera_preview(self):
+    def run_cmd(self, cmd, expect_result):
+        '''Runs a command line and compare with expect_result.
+
+        Args:
+            cmd: The command to execute.
+            expect_result: Expected result in regular expression.
+
+        Return:
+            True if the result matches.
+        '''
+        factory.log('Running command [%s]' % cmd)
         try:
-            self.camera_preview.init_device(
-                int(self.camera_config['DEVICE_INDEX']))
-        except IOError as e:
-            factory.log('Cannot get cammera - %s' % e)
-        finally:
-            self.advance_state()
+            ret = utils.system_output(cmd)
+            factory.log('Command returns [%s, expecting %s]' % (
+                ret, expect_result))
+            if re.search(expect_result, ret, re.MULTILINE):
+                return True
+        except Exception as e:
+            factory.log('Command failed with exception - %s' % e)
+        return False
 
     def write_log(self):
         self.generate_final_result()
@@ -209,9 +316,10 @@ class factory_Connector(state_machine.FactoryStateMachine):
         self.sn_input_widget.get_entry().set_text('')
         factory.log('Data reseted.')
 
-    def run_once(self):
+    def run_once(self, config_file):
         factory.log('%s run_once' % self.__class__)
         # Initialize variables.
+        self.config_file = config_file
         self.base_config = PluggableConfig({})
 
         # Set up the USB prompt widgets.
