0fe27c084a2a327c0868b54d7f65d303614bd21f
Ilja H. Friedel <ihf@google.com>
2012-05-16 13:30:45 -0700

    autotest: make graphics_ui_test ARM aware.
    For now that means skipping the GPU sanity checks when we are running
    on an ARM GPU.
    
    TEST=Ran on lumpy.
    
    Change-Id: I538eba18aee502d66c5114d5b8cfceec40c74f8b
    Reviewed-on: https://gerrit.chromium.org/gerrit/22850
    Reviewed-by: Stuart Abercrombie <sabercrombie@chromium.org>
    Commit-Ready: Ilja H. Friedel <ihf@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Tested-by: Ilja H. Friedel <ihf@chromium.org>

--- client/cros/graphics_ui_test.py
+++ client/cros/graphics_ui_test.py
@@ -23,43 +23,45 @@ class GraphicsUITest(cros_ui_test.UITest):
 
     def initialize(self, creds=None, is_creating_owner=False,
                    extra_chrome_flags=[], subtract_extra_chrome_flags=[]):
-        cmd = 'glxinfo | grep "OpenGL renderer string"'
-        cmd = cros_ui.xcommand(cmd)
-        output = utils.run(cmd)
-        result = output.stdout.splitlines()[0]
-        logging.info('glxinfo: %s', result)
-        # TODO(ihf): Find exhaustive error conditions (especially ARM).
-        if 'llvm' in result.lower() or 'soft' in result.lower():
-          raise error.TestFail('Refusing to run on SW rasterizer: ' + result)
-        logging.info('Initialize: Checking for old GPU hangs...')
-        f = open(self._MESSAGES_FILE, 'r')
-        for line in f:
-          if self._HANGCHECK in line:
-            logging.info(line)
-            self.hangs[line] = line
-        f.close()
+        if utils.get_cpu_arch() != 'arm':
+          cmd = 'glxinfo | grep "OpenGL renderer string"'
+          cmd = cros_ui.xcommand(cmd)
+          output = utils.run(cmd)
+          result = output.stdout.splitlines()[0]
+          logging.info('glxinfo: %s', result)
+          # TODO(ihf): Find exhaustive error conditions (especially ARM).
+          if 'llvmpipe' in result.lower() or 'soft' in result.lower():
+            raise error.TestFail('Refusing to run on SW rasterizer: ' + result)
+          logging.info('Initialize: Checking for old GPU hangs...')
+          f = open(self._MESSAGES_FILE, 'r')
+          for line in f:
+            if self._HANGCHECK in line:
+              logging.info(line)
+              self.hangs[line] = line
+          f.close()
 
         cros_ui_test.UITest.initialize(self, creds, is_creating_owner,
             extra_chrome_flags, subtract_extra_chrome_flags)
 
     def cleanup(self):
-        logging.info('Cleanup: Checking for new GPU hangs...')
-        f = open(self._MESSAGES_FILE, 'r')
-        for line in f:
-          if self._HANGCHECK in line:
-            if not line in self.hangs.keys():
-              logging.info(line)
-              self.job.record('WARN', None, 'Saw GPU hang during test.')
-        f.close()
+        if utils.get_cpu_arch() != 'arm':
+          logging.info('Cleanup: Checking for new GPU hangs...')
+          f = open(self._MESSAGES_FILE, 'r')
+          for line in f:
+            if self._HANGCHECK in line:
+              if not line in self.hangs.keys():
+                logging.info(line)
+                self.job.record('WARN', None, 'Saw GPU hang during test.')
+          f.close()
 
-        cmd = 'glxinfo | grep "OpenGL renderer string"'
-        cmd = cros_ui.xcommand(cmd)
-        output = utils.run(cmd)
-        result = output.stdout.splitlines()[0]
-        logging.info('glxinfo: %s', result)
-        # TODO(ihf): Find exhaustive error conditions (especially ARM).
-        if 'llvm' in result.lower() or 'soft' in result.lower():
-          logging.info('Finished test on SW rasterizer.')
-          raise error.TestFail('Finished test on SW rasterizer: ' + result)
+          cmd = 'glxinfo | grep "OpenGL renderer string"'
+          cmd = cros_ui.xcommand(cmd)
+          output = utils.run(cmd)
+          result = output.stdout.splitlines()[0]
+          logging.info('glxinfo: %s', result)
+          # TODO(ihf): Find exhaustive error conditions (especially ARM).
+          if 'llvmpipe' in result.lower() or 'soft' in result.lower():
+            logging.info('Finished test on SW rasterizer.')
+            raise error.TestFail('Finished test on SW rasterizer: ' + result)
 
         cros_ui_test.UITest.cleanup(self)
