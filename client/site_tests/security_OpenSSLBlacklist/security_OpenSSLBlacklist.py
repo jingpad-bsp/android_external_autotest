ab36c7abefeccc7c0b49bd967b77922f320acb62
Elly Jones <ellyjones@chromium.org>
2012-05-17 16:43:56 -0400

    [autotest] security_OpenSSLBlacklist: use curl
    Use curl to test cert blacklisting end-to-end.
    
    TEST=Yes
    BUG=chromium-os:21100
    
    Change-Id: I900d3c9e068ce0e3ae85d9371f68da9df200a0d3
    Signed-off-by: Elly Jones <ellyjones@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/22958
    Reviewed-by: Kees Cook <keescook@chromium.org>

--- client/site_tests/security_OpenSSLBlacklist/security_OpenSSLBlacklist.py
+++ client/site_tests/security_OpenSSLBlacklist/security_OpenSSLBlacklist.py
@@ -3,6 +3,8 @@
 # found in the LICENSE file.
 
 import os
+import subprocess
+import time
 
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
@@ -18,18 +20,42 @@ class security_OpenSSLBlacklist(test.test):
             (blacklist, VERIFY, self.ca, self.cert))
         return r == 0
 
+    def fetch(self, blacklist='/dev/null'):
+        r = os.system('OPENSSL_BLACKLIST_PATH=%s curl --cacert %s '
+                      'https://127.0.0.1:4433/ca.pem' % (blacklist, self.ca))
+        return r == 0
+
     def run_once(self, opts=None):
         self.hash_blacklist = '%s/hash_blacklist' % self.srcdir
         self.serial_blacklist = '%s/serial_blacklist' % self.srcdir
         self.bogus_blacklist = '%s/bogus_blacklist' % self.srcdir
         self.ca = '%s/ca.pem' % self.srcdir
         self.cert = '%s/cert.pem' % self.srcdir
+        self.key = '%s/cert.key' % self.srcdir
 
         if not self.verify():
             raise error.TestFail('Certificate does not verify normally.')
-        if self.verify(blacklist=self.hash_blacklist):
+        if self.verify(self.hash_blacklist):
             raise error.TestFail('Certificate verified when blacklisted by hash.')
-        if self.verify(blacklist=self.serial_blacklist):
+        if self.verify(self.serial_blacklist):
             raise error.TestFail('Certificate verified when blacklisted by serial.')
-        if not self.verify(blacklist=self.bogus_blacklist):
+        if not self.verify(self.bogus_blacklist):
             raise error.TestFail('Certificate does not verify with nonempty blacklist.')
+
+        # Fire up an openssl s_server and have curl fetch from it
+        server = subprocess.Popen([OPENSSL, 's_server', '-HTTP',
+                                   '-CAfile', self.ca, '-cert', self.cert,
+                                   '-key', self.key, '-port', '4433'],
+                                  cwd=self.srcdir)
+        # Give openssl time to start up. Without this, this test sometimes
+        # deadlocks with curl unable to connect to the server.
+        time.sleep(3)
+        if not self.fetch():
+            raise error.TestFail('Fetch without blacklist fails.')
+        if self.fetch(self.hash_blacklist):
+            raise error.TestFail('Fetch with hash blacklisted succeeds.')
+        if self.fetch(self.serial_blacklist):
+            raise error.TestFail('Fetch with serial blacklisted succeeds.')
+        if not self.fetch(self.bogus_blacklist):
+            raise error.TestFail('Fetch with nonempty blacklist fails.')
+        server.terminate()
