5ffa03c0e8b8cb7d794cb21a057556b223addb67
Elly Jones <ellyjones@chromium.org>
2012-05-17 12:58:11 -0400

    [autotest] security_OpenSSLBlacklist: test serial
    Test blacklist-by-serial as well as blacklist-by-hash.
    
    TEST=Yes
    BUG=chromium-os:21100
    
    Change-Id: Ief5d41d2e55c27d3cf3cc5255e4a1ec4621a59cd
    Signed-off-by: Elly Jones <ellyjones@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/22941

--- client/site_tests/security_OpenSSLBlacklist/security_OpenSSLBlacklist.py
+++ client/site_tests/security_OpenSSLBlacklist/security_OpenSSLBlacklist.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -19,14 +19,17 @@ class security_OpenSSLBlacklist(test.test):
         return r == 0
 
     def run_once(self, opts=None):
-        self.blacklist = '%s/blacklist' % self.srcdir
+        self.hash_blacklist = '%s/hash_blacklist' % self.srcdir
+        self.serial_blacklist = '%s/serial_blacklist' % self.srcdir
         self.bogus_blacklist = '%s/bogus_blacklist' % self.srcdir
         self.ca = '%s/ca.pem' % self.srcdir
         self.cert = '%s/cert.pem' % self.srcdir
 
         if not self.verify():
             raise error.TestFail('Certificate does not verify normally.')
-        if self.verify(blacklist=self.blacklist):
-            raise error.TestFail('Certificate verified when blacklisted.')
+        if self.verify(blacklist=self.hash_blacklist):
+            raise error.TestFail('Certificate verified when blacklisted by hash.')
+        if self.verify(blacklist=self.serial_blacklist):
+            raise error.TestFail('Certificate verified when blacklisted by serial.')
         if not self.verify(blacklist=self.bogus_blacklist):
             raise error.TestFail('Certificate does not verify with nonempty blacklist.')
