cd3a87cbbc80eaa9bcd1df36b6231763663190fa
Elly Jones <ellyjones@chromium.org>
2012-06-06 13:18:05 -0400

    [autotest] security_OpenSSLBlacklist: fix
    Fix a couple of issues in this test:
    1) ensure the server process is terminated even if we fail
    2) make curl not try to write to /
    3) don't bother serving random files out of src/, and instead use s_server's
       builtin info page
    
    TEST=Yes
    BUG=chromium-os:31445
    
    Change-Id: Ia1514e5acc525292e7a00c395e190c88f0a342af
    Signed-off-by: Elly Jones <ellyjones@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/24623
    Reviewed-by: Jim Hebert <jimhebert@chromium.org>

--- client/site_tests/security_OpenSSLBlacklist/security_OpenSSLBlacklist.py
+++ client/site_tests/security_OpenSSLBlacklist/security_OpenSSLBlacklist.py
@@ -6,7 +6,7 @@ import os
 import subprocess
 import time
 
-from autotest_lib.client.bin import test
+from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 
 OPENSSL = '/usr/bin/openssl'
@@ -21,8 +21,8 @@ class security_OpenSSLBlacklist(test.test):
         return r == 0
 
     def fetch(self, blacklist='/dev/null'):
-        r = os.system('OPENSSL_BLACKLIST_PATH=%s curl --cacert %s '
-                      'https://127.0.0.1:4433/ca.pem' % (blacklist, self.ca))
+        r = os.system('OPENSSL_BLACKLIST_PATH=%s curl --cacert %s -o /dev/null '
+                      'https://127.0.0.1:4433/' % (blacklist, self.ca))
         return r == 0
 
     def run_once(self, opts=None):
@@ -43,19 +43,18 @@ class security_OpenSSLBlacklist(test.test):
             raise error.TestFail('Certificate does not verify with nonempty blacklist.')
 
         # Fire up an openssl s_server and have curl fetch from it
-        server = subprocess.Popen([OPENSSL, 's_server', '-HTTP',
+        server = subprocess.Popen([OPENSSL, 's_server', '-www',
                                    '-CAfile', self.ca, '-cert', self.cert,
-                                   '-key', self.key, '-port', '4433'],
-                                  cwd=self.srcdir)
-        # Give openssl time to start up. Without this, this test sometimes
-        # deadlocks with curl unable to connect to the server.
-        time.sleep(3)
-        if not self.fetch():
-            raise error.TestFail('Fetch without blacklist fails.')
-        if self.fetch(self.hash_blacklist):
-            raise error.TestFail('Fetch with hash blacklisted succeeds.')
-        if self.fetch(self.serial_blacklist):
-            raise error.TestFail('Fetch with serial blacklisted succeeds.')
-        if not self.fetch(self.bogus_blacklist):
-            raise error.TestFail('Fetch with nonempty blacklist fails.')
-        server.terminate()
+                                   '-key', self.key, '-port', '4433'])
+        try:
+            # Need to wait for openssl to be ready to talk to us
+            if not utils.poll_for_condition(self.fetch):
+                raise error.TestFail('Fetch without blacklist fails.')
+            if self.fetch(self.hash_blacklist):
+                raise error.TestFail('Fetch with hash blacklisted succeeds.')
+            if self.fetch(self.serial_blacklist):
+                raise error.TestFail('Fetch with serial blacklisted succeeds.')
+            if not self.fetch(self.bogus_blacklist):
+                raise error.TestFail('Fetch with nonempty blacklist fails.')
+        finally:
+            server.terminate()
