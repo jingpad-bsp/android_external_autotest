9f36058d89b7315b41421053bca963d29884770b
Olof Johansson <olofj@chromium.org>
2012-06-11 16:39:08 -0700

    kernel_HdParm: check for arm, use mmcblk0
    BUG=chromium-os:29514
    TEST=run testcase
    
    Change-Id: I13d253c7c550360ea8be2bd1c526a616afec8d30
    Reviewed-on: https://gerrit.chromium.org/gerrit/25058
    Tested-by: Olof Johansson <olofj@chromium.org>
    Commit-Ready: Mike Truty <truty@chromium.org>
    Reviewed-by: Mike Truty <truty@chromium.org>

--- client/site_tests/kernel_HdParm/kernel_HdParm.py
+++ client/site_tests/kernel_HdParm/kernel_HdParm.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011-2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import re
+import logging, re
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
@@ -16,11 +16,17 @@ class kernel_HdParm(test.test):
     """
     version = 1
 
-
     def run_once(self):
-        result = utils.system_output('hdparm -T /dev/sda')
+        if (utils.get_cpu_arch() == "arm"):
+            disk = '/dev/mmcblk0'
+        else:
+            disk = '/dev/sda'
+
+        logging.debug("Using device %s", disk)
+
+        result = utils.system_output('hdparm -T %s' % disk)
         match = re.search('(\d+\.\d+) MB\/sec', result)
         self.write_perf_keyval({'cache_throughput': match.groups()[0]})
-        result = utils.system_output('hdparm -t /dev/sda')
+        result = utils.system_output('hdparm -t %s' % disk)
         match = re.search('(\d+\.\d+) MB\/sec', result)
         self.write_perf_keyval({'disk_throughput': match.groups()[0]})
