285d0bcd54c138e68156ba6c6207e203fce99e1a
Ilja H. Friedel <ihf@google.com>
2012-06-16 21:26:23 -0700

    autotest: let graphics tests record Chrome version
    Graphics results really need to record which version of Chrome they
    are running on. As an experiment for the perf dashboard record this.
    In the long term this should be part of the autotest infrastructure.
    
    TEST=Works on lumpy in local testing.
    
    Change-Id: I4c546377e90d90631d9f793fcf044f3b19023de1
    Reviewed-on: https://gerrit.chromium.org/gerrit/25507
    Reviewed-by: Stuart Abercrombie <sabercrombie@chromium.org>
    Commit-Ready: Ilja H. Friedel <ihf@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Tested-by: Ilja H. Friedel <ihf@chromium.org>

--- client/cros/graphics_ui_test.py
+++ client/cros/graphics_ui_test.py
@@ -2,9 +2,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os
+"""The functionality in this class is used whenever one of the graphics_*
+tests runs. It provides a sanity check on GPU failures and emits warnings
+on recovered GPU hangs and errors on fallback to software rasterization.
+"""
 
-import common, cros_ui, cros_ui_test
+import logging, re
+
+import cros_ui, cros_ui_test
 from autotest_lib.client.bin import utils
 from autotest_lib.client.common_lib import error
 
@@ -18,11 +23,13 @@ class GraphicsUITest(cros_ui_test.UITest):
 
     hangs = {}
 
-    _MESSAGES_FILE = '/var/log/messages'
+    _BROWSER_VERSION_COMMAND = '/opt/google/chrome/chrome --version'
     _HANGCHECK = 'drm:i915_hangcheck_elapsed'
+    _MESSAGES_FILE = '/var/log/messages'
 
     def initialize(self, creds=None, is_creating_owner=False,
                    extra_chrome_flags=[], subtract_extra_chrome_flags=[]):
+        """Analyzes the state of the GPU and log history."""
         if utils.get_cpu_arch() != 'arm':
           cmd = 'glxinfo | grep "OpenGL renderer string"'
           cmd = cros_ui.xcommand(cmd)
@@ -44,6 +51,10 @@ class GraphicsUITest(cros_ui_test.UITest):
             extra_chrome_flags, subtract_extra_chrome_flags)
 
     def cleanup(self):
+        """Analyzes the state of the GPU, log history and emits warnings or
+        errors if the state changed since initialize. Also makes a note of the
+        Chrome version for later usage in the perf-dashboard.
+        """
         if utils.get_cpu_arch() != 'arm':
           logging.info('Cleanup: Checking for new GPU hangs...')
           f = open(self._MESSAGES_FILE, 'r')
@@ -63,5 +74,22 @@ class GraphicsUITest(cros_ui_test.UITest):
           if 'llvmpipe' in result.lower() or 'soft' in result.lower():
             logging.info('Finished test on SW rasterizer.')
             raise error.TestFail('Finished test on SW rasterizer: ' + result)
+
+        # Graphics tests really care about the Chrome version installed.
+        output = utils.run(self._BROWSER_VERSION_COMMAND)
+        logging.info(output.stdout)
+        prog = re.compile('(Chrome|Chromium) (\d+)\.(\d+)\.(\d+)\.(\d+)')
+        m = prog.findall(output.stdout)
+        if len(m) > 0:
+            version = (int(m[0][1]), int(m[0][2]), int(m[0][3]), int(m[0][4]))
+            keyvals = {}
+            # Microsoft naming convention (Major, Minor, Build, Revision).
+            keyvals['Google_Chrome_Version0'] = version[0]
+            keyvals['Google_Chrome_Version1'] = version[1]
+            keyvals['Google_Chrome_Version2'] = version[2]
+            keyvals['Google_Chrome_Version3'] = version[3]
+            self.write_perf_keyval(keyvals)
+            logging.info('Found %s %d.%d.%d.%d.', m[0][0], version[0],
+                version[1], version[2], version[3])
 
         cros_ui_test.UITest.cleanup(self)
