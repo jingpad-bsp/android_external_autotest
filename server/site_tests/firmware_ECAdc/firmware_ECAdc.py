df6e9cc61fb10a16afa8d518ab775ac29addf8aa
Vic Yang <victoryang@chromium.org>
2012-06-17 16:34:56 +0800

    Add a test of check EC ADC functionality
    This test repeatedly read EC internal temperature through EC ADC module
    and checks no error happens.
    
    BUG=chrome-os-partner:10615
    TEST=Test passed.
    
    Change-Id: I661685e9e7babfedec96938f5a5206efdc4a9153
    Reviewed-on: https://gerrit.chromium.org/gerrit/25514
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Commit-Ready: Vic Yang <victoryang@chromium.org>
    Tested-by: Vic Yang <victoryang@chromium.org>

---
+++ server/site_tests/firmware_ECAdc/firmware_ECAdc.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import pexpect
+
+from autotest_lib.client.common_lib import error
+from autotest_lib.server.cros.faftsequence import FAFTSequence
+
+class firmware_ECAdc(FAFTSequence):
+    """
+    Servo based EC ADC test.
+    """
+    version = 1
+
+    # Repeat read count
+    READ_COUNT = 200
+
+    def _check_read(self):
+        """Read EC internal temperature by EC ADC.
+
+        Raises:
+          error.TestFail: Raised when read fails.
+        """
+        try:
+            t = int(self.send_uart_command_get_output("ectemp",
+                    ["EC temperature is (\d+) K"])[0].group(1))
+            if t < 273 or t > 373:
+                raise error.TestFail("Abnormal EC temperature %d K" % t)
+        except pexpect.TIMEOUT:
+            raise error.TestFail("Error reading EC internal temperature")
+
+
+    def run_once(self, host=None):
+        logging.info("Reading EC internal temperature for %d times." %
+                     self.READ_COUNT)
+        for i in xrange(self.READ_COUNT):
+            self._check_read()
