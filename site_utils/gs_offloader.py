9523eaa3c3fea15563a3d32dbc0cdda2453ee492
Simran Basi <sbasi@chromium.org>
2012-06-28 17:18:45 -0700

    Autotest: Clean up gs_offloader.
    Changed the structure of gs_offloader so that it will now loop
    continuously over the results directly.
    
    Added logging to a file and replaced all print out's with logging
    messages.
    
    Added a timeout to shelling out commands. Set the timeout to three
    hours and utilized signal and SIGALRM so that the offloader command will
    be terminated once the timeout expires.
    
    BUG=chromium-os:29571
    TEST=Ran the script (without folder deletion and is_job_complete) on
    chromeos-autotest.cbf
    
    Change-Id: I97b47db93d2f5e373dff9c9fa037f13f38f146cb
    Reviewed-on: https://gerrit.chromium.org/gerrit/26370
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Commit-Ready: Simran Basi <sbasi@google.com>
    Tested-by: Simran Basi <sbasi@google.com>

--- site_utils/gs_offloader.py
+++ site_utils/gs_offloader.py
@@ -12,85 +12,167 @@ successful copy, the local results directory is deleted.
 
 __author__ = 'dalecurtis@google.com (Dale Curtis)'
 
+import logging
 import os
 import re
 import shutil
+import signal
 import subprocess
 import sys
+import tempfile
+import time
 
 import is_job_complete
-
 
 # Google Storage bucket URI to store results in.
 GS_URI = 'gs://chromeos-autotest-results'
 
 # Set this to True to enable rsync otherwise results are offloaded to GS.
 USE_RSYNC = False
-RSYNC = "chromeos-sam1:/usr/local/autotest/results/"
+RSYNC_HOST_PATH = 'chromeos-sam1:/usr/local/autotest/results/'
 
 # Nice setting for process, the higher the number the lower the priority.
 NICENESS = 10
 
+# Setting timeout to 3 hours.
+TIMEOUT = 3 * 60 * 60
+
 # Location of Autotest results on disk.
 RESULTS_DIR = '/usr/local/autotest/results'
+
+# Success constant to check if a job is completed.
+JOB_COMPLETED = 0
+
+LOG_FILENAME_FORMAT = ('/usr/local/autotest/logs/'
+                       'gs_offloader_log_%Y%m%d_%H%M%S.txt')
+LOGGING_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+
+CLEAN_CMD = 'find %s -iname chrome_20[0-9][0-9]\* -exec rm {} \;'
+
+
+class TimeoutException(Exception):
+  pass
+
+
+def timeout_handler(_signum, _frame):
+  """
+  Called by the SIGALRM if the offloading process has timed out.
+
+  @raise TimeoutException: Automatically raises so that the time out is caught
+                           by the try/except surrounding the Popen call.
+  """
+  raise TimeoutException('Process Timed Out')
+
+
+def get_cmd_list(dir_entry):
+  """
+  Generate the cmd_list for the specified directory entry.
+
+  @param dir_entry: Directory entry/path that which we need a cmd_list to
+                    offload.
+
+  @return: A command list to be executed by Popen.
+  """
+  if USE_RSYNC:
+    logging.debug('Using rsync for offloading %s to %s.', dir_entry,
+                  RSYNC_HOST_PATH)
+    return ['rsync', '-a', dir_entry, RSYNC_HOST_PATH]
+  else:
+    logging.debug('Using google storage for offloading %s to %s.',
+                  dir_entry, GS_URI)
+    return ['gsutil', '-m', 'cp', '-eR', '-a', 'project-private', dir_entry,
+            GS_URI]
+
+
+def offload_dir(dir_entry):
+  """
+  Offload the specified directory entry to the Google storage or the RSYNC host,
+  but timeout if it takes too long.
+
+  @param dir_entry: Directory entry to offload.
+  """
+  try:
+    error = False
+    signal.alarm(TIMEOUT)
+    stdout_file = tempfile.TemporaryFile('w+')
+    stderr_file = tempfile.TemporaryFile('w+')
+    process = subprocess.Popen(get_cmd_list(dir_entry), stdout=stdout_file,
+                               stderr=stderr_file)
+    process.wait()
+    signal.alarm(0)
+    if process.returncode == 0:
+      shutil.rmtree(dir_entry)
+    else:
+      error = True
+  except TimeoutException:
+    process.terminate()
+    logging.error('Offloading %s timed out after waiting %d seconds.',
+                  dir_entry, TIMEOUT)
+    error = True
+  finally:
+    signal.alarm(0)
+    if error:
+      # Rewind the log files for stdout and stderr and log their contents.
+      stdout_file.seek(0)
+      stderr_file.seek(0)
+      logging.error('Stdout:\n%s \nStderr:\n%s', stdout_file.read(),
+                    stderr_file.read())
+    stdout_file.close()
+    stderr_file.close()
 
 
 def offload_files(results_dir):
   """
-  Offload files to Google Storage or the RSYNC host if USE_RSYNC is True.
+  Offload files to Google Storage or the RSYNC_HOST_PATH host if USE_RSYNC is
+  True.
+
+  To ensure that the offloading times out properly we utilize a SIGALRM by
+  assigning a simple function, timeout_handler, to be called if the SIGALRM is
+  raised. timeout_handler will raise an exception that we can catch so that we
+  know the timeout has occured and can react accordingly.
 
   @param results_dir: The Autotest results dir to look for dirs to offload.
   """
   # Nice our process (carried to subprocesses) so we don't kill the system.
   os.nice(NICENESS)
-
+  logging.debug('Set process to nice value: %d', NICENESS)
   # os.listdir returns relative paths, so change to where we need to be to avoid
   # an os.path.join on each loop.
-  os.chdir(RESULTS_DIR)
-
+  os.chdir(results_dir)
+  logging.debug('Looking for Autotest results in %s', results_dir)
   # Only pick up directories of the form <job #>-<job user>.
   job_matcher = re.compile('^\d+-\w+')
-
-  # Iterate over all directories in RESULTS_DIR.
-  for d in os.listdir('.'):
-    print 'Processing %s' % d
-    if not job_matcher.match(d):
-      print 'Skipping %s' % d
-      continue
-    job_id = os.path.basename(d).split('-')[0]
-    if is_job_complete.is_job_complete(job_id) != 0:
-      print 'Job %s is not yet complete skipping' % d
-      continue
-    if (job_matcher.match(d) and os.path.isdir(d)):
-      # Update command list.
-      print 'Offloading %s' % d
-      # The way we collect results, currently, is naive and resulst in a lot of
-      # extra data collection. Clear these for now until we can be more
-      # exact about what logs we care about. crosbug.com/26784.
-      print 'Cleaning'
-      cmd_clean = 'find %s -iname chrome_2012\* -exec rm {} \;' % d
-      os.system(cmd_clean)
-
-      if USE_RSYNC:
-        cmd_list = ['rsync', '-a', d, RSYNC]
-      else:
-        cmd_list = ['gsutil', '-m', 'cp', '-eR', '-a', 'project-private', d,
-                    GS_URI]
-      # Save stdout and stderr in case of failure.
-      process = subprocess.Popen(
-          cmd_list, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-      output = process.communicate()[0]
-
-      if process.returncode == 0:
-        # Everything copied okay, so remove the local directory.
-        shutil.rmtree(d)
-      else:
-        # Copy failed, dump logs and continue to the next directory.
-        print output
+  signal.signal(signal.SIGALRM, timeout_handler)
+  while True:
+    # Iterate over all directories in results_dir.
+    for dir_entry in os.listdir('.'):
+      logging.debug('Processing %s', dir_entry)
+      if not job_matcher.match(dir_entry):
+        logging.debug('Skipping dir %s', dir_entry)
+        continue
+      # Directory names are in the format of <job #>-<job user>. We want just
+      # the job # to see if it has completed.
+      job_id = os.path.basename(dir_entry).split('-')[0]
+      if is_job_complete.is_job_complete(job_id) is not JOB_COMPLETED:
+        logging.debug('Job %s is not yet complete; skipping.', dir_entry)
+        continue
+      if (job_matcher.match(dir_entry) and os.path.isdir(dir_entry)):
+        # The way we collect results currently is naive and results in a lot
+        # of extra data collection. Clear these for now until we can be more
+        # exact about what logs we care about. crosbug.com/26784.
+        logging.debug('Cleaning %s of extra data.', dir_entry)
+        os.system(CLEAN_CMD % dir_entry)
+        offload_dir(dir_entry)
 
 
-def main():
-  if len(sys.argv) > 1:
+def _check_args_and_print_usage(args):
+  """
+  Check that no args have been passed to gs_offloader, and if so print out the
+  proper usage.
+
+  @param args: Command line args passed into gs_offloader.
+  """
+  if len(args) > 1:
     print __help__
     print 'Defaults:'
     print '  Destination: ' + GS_URI
@@ -99,6 +181,12 @@ def main():
     print '  ./gs_offloader.py\n'
     sys.exit(0)
 
+
+def main():
+  _check_args_and_print_usage(sys.argv)
+  log_filename = time.strftime(LOG_FILENAME_FORMAT)
+  logging.basicConfig(filename=log_filename, level=logging.DEBUG,
+                      format=LOGGING_FORMAT)
   offload_files(RESULTS_DIR)
 
 
