eac3cacb49d40af993e3fff8e85113a29ac9f601
Lucian Cojocar <cojocar@chromium.org>
2012-07-09 11:06:13 -0700

    power_Resume: use LogReader for parsing the dmesg messages
    Use the LogReader class to parse messages from /var/log/messages.
    
    BUG=chromium-os:31434
    TEST=works on a Samsung Series 5 550 machine
    
    Change-Id: Ifff845a5771643602e56f7eba3ea89c020f6fba5
    Signed-off-by: Lucian Cojocar <cojocar@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/26491
    Reviewed-by: Mark Koudritsky <kamrik@chromium.org>
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Sameer Nanda <snanda@chromium.org>

--- client/site_tests/power_Resume/power_Resume.py
+++ client/site_tests/power_Resume/power_Resume.py
@@ -5,72 +5,15 @@
 import commands, logging, random, re, time
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros import rtc, sys_power
+from autotest_lib.client.cros import rtc, sys_power, cros_logging
 
 from autotest_lib.client.cros import flimflam_test_path
 import flimflam
 
-START_SUSPEND_MESSAGES = [ 'Freezing user space' ]
-END_SUSPEND_MESSAGES = [ 'Back to C!', 'Low-level resume complete',
-                         'Entering suspend state', 'sleep: irq wakeup masks:' ]
-START_RESUME_MESSAGES = [ 'Back to C!', 'Low-level resume complete',
-                          'Suspended for', 'Resume caused by',
-                          'post sleep, preparing to return']
-END_RESUME_MESSAGES = [ 'Restarting tasks' ]
 
 class power_Resume(test.test):
     version = 1
     preserve_srcdir = True
-
-
-    def _get_command_output(self, cmd):
-        """Try to execute a command, until we get some output or a limit is
-        reached.
-
-        The order in which processes are un-frozen is indeterminate
-        and therfore this test may get resumed before the system has gotten
-        a chance to finalize writes to logfile. Sleep a bit to take care of
-        this race.
-
-        Args:
-            cmd: The command to execute.
-
-        Returns:
-            The output of the command that was executed, or None, if the
-            command didn't printed anything to standard output.
-        """
-        count = 0
-        data = commands.getoutput(cmd)
-        while len(data) == 0 and count < 5:
-            count +=1
-            time.sleep(1)
-            data = commands.getoutput(cmd)
-
-        if count == 5:
-            return None
-
-        return data
-
-
-    def _get_last_msg(self, pattern):
-        """Search for the last message in /var/log/messages, that matches a
-        given pattern.
-
-            Args:
-                pattern: The pattern for which we search.
-
-            Return:
-                If the pattern is found in /var/log/messages, the last line
-                that matches that pattern.
-
-            Raises:
-                TestError: If the pattern isn't found.
-        """
-        cmd = "grep -a '%s' /var/log/messages | tail -n 1" % pattern
-        data = self._get_command_output(cmd)
-        if data is None:
-            raise error.TestError("Did not find log message: " + pattern)
-        return data
 
 
     def _get_max_time_device(self, start, stop, action):
@@ -89,27 +32,25 @@ class power_Resume(test.test):
                 device could be found.
 
             Raise:
-                TestError: If no device was found in *any* interval or if the
-                log is corrupted.
+                TestError: If the log is corrupted.
         """
-        cmd = "grep -a 'call [^ ]\+ returned 0 after [0-9]\+ usecs' " + \
-            "/var/log/messages"
-        data = self._get_command_output(cmd)
-
-        if data is None:
-            raise error.TestError("Did not find any device")
 
         max_time = 0
         max_device_name = None
         call_regexp = re.compile(r'call ([^ ]+) returned 0 after ([0-9]+) '
                 'usecs')
-        for dev_line in data.splitlines():
+
+        logging.debug("Searching in interval: %f - %f for %s times\n" %
+                (start, stop, action))
+
+        for dev_line in self._log_msg.get_logs().splitlines():
             # find the time stamp for each message
-            match_ts = re.search(r' \[\s*([0-9.]+)\] ', dev_line)
-            if match_ts is None:
-                raise error.TestError('Did not find timestamp for log message: '
-                        + dev_line)
-            ts = float(match_ts.group(1))
+            ts = -1
+            try:
+                ts = cros_logging.extract_kernel_timestamp(dev_line)
+            except error.TestError:
+                # probabily not an interesting message
+                continue
 
             if not (ts >= start and ts <= stop):
                 # skip this message, because it's in a different interval
@@ -122,10 +63,10 @@ class power_Resume(test.test):
                 # this line doesn't contains a call string
                 continue
 
-            (device_name, time) = search_groups.groups()
-            device_time = float(time)
+            (device_name, time_matched) = search_groups.groups()
+            device_time = float(time_matched)
 
-            logging.debug("Device %s took %s to %s" % \
+            logging.debug("Device %s took %f usecs to %s" % \
                     (device_name, device_time, action))
 
             # calculate the maximal time
@@ -136,64 +77,82 @@ class power_Resume(test.test):
         # convert from usec to seconds
         return (max_device_name, max_time / 1e6)
 
-    def _get_last_msg_time(self, msg):
-        data = self._get_last_msg(msg)
-        match = re.search(r' \[\s*([0-9.]+)\] ', data)
-        if match is None:
-            raise error.TestError('Did not find timestamp for log message: '
-                                  + msg)
 
-        msg_time = float(match.group(1))
-        logging.debug("Last message '%s' time = %f" % (msg, msg_time))
-        return msg_time
+    def _get_last_msg(self, patterns):
+        return self._log_msg.get_last_msg(patterns,
+                retries=5, sleep_seconds=1)
 
-    def _get_last_msg_time_multiple(self, msgs):
-        time = -1
-        for msg in msgs:
-            try:
-                time = self._get_last_msg_time(msg)
-                break
-            except error.TestError as e:
-                logging.info("%s, trying next message" % str(e))
 
-        return time
+    def _get_last_msg_time(self, patterns):
+        """Extract timestamp from a message which matches a certain pattern(s).
 
-    def _get_start_suspend_time(self):
-        time = self._get_last_msg_time_multiple(START_SUSPEND_MESSAGES)
-        if time == -1:
-            raise error.TestError("Could not find start suspend time message.")
+        Args:
+            patterns: Pattern or list of patterns that the message will match.
 
-        return time
+        Returns:
+            The timestamp from the last matched message or -1 if we couldn't
+            match a message.
+
+        Raises:
+            TestError: If the timestamp could not be extracted from the matched
+            pattern.
+        """
+        msg = self._get_last_msg(patterns)
+
+        if msg is None:
+            # we didn't find the pattern
+            return -1
+        else:
+            return cros_logging.extract_kernel_timestamp(msg)
+
 
     def _get_end_cpu_resume_time(self):
         # systems with only one logical CPU won't have this message, return -1
-        try:
-            time = self._get_last_msg_time('CPU[0-9]\+ is up')
-        except error.TestError:
-            time = -1
+        time = self._get_last_msg_time('CPU[0-9]+ is up')
+        logging.debug('END cpu resume time %f' % time)
 
         return time
+
+
+    def _get_start_suspend_time(self):
+        time = self._get_last_msg_time(
+                sys_power.SUSPEND_RESUME_MESSAGES['START_SUSPEND'])
+        if time == -1:
+            raise error.TestError("Could not find start suspend time message.")
+        logging.debug('START suspend time: %f' % time)
+
+        return time
+
 
     def _get_end_suspend_time(self):
-        time = self._get_last_msg_time_multiple(END_SUSPEND_MESSAGES)
+        time = self._get_last_msg_time(
+                sys_power.SUSPEND_RESUME_MESSAGES['END_SUSPEND'])
         if time == -1:
             raise error.TestError("Could not find end suspend time message.")
+        logging.debug('END suspend time %f' % time)
 
         return time
+
 
     def _get_start_resume_time(self):
-        time = self._get_last_msg_time_multiple(START_RESUME_MESSAGES)
+        time = self._get_last_msg_time(
+                sys_power.SUSPEND_RESUME_MESSAGES['START_RESUME'])
         if time == -1:
             raise error.TestError("Could not find start resume time message.")
+        logging.debug('START resume time %f' % time)
 
         return time
+
 
     def _get_end_resume_time(self):
-        time = self._get_last_msg_time_multiple(END_RESUME_MESSAGES)
+        time = self._get_last_msg_time(
+                sys_power.SUSPEND_RESUME_MESSAGES['END_RESUME'])
         if time == -1:
             raise error.TestError("Could not find end resume time message.")
+        logging.debug('END resume time %f' % time)
 
         return time
+
 
     def _get_device_resume_time(self):
         data = self._get_last_msg("PM: resume of devices complete after")
@@ -202,8 +161,11 @@ class power_Resume(test.test):
         if match is None:
             raise error.TestError('Failed to find device resume time on line: '
                                   + data)
+        logging.debug('device resume time: %f' % (float(match.group(1))/1e3))
+
         # convert from msec to sec
         return float(match.group(1)) / 1000
+
 
     def _get_hwclock_seconds(self):
         """
@@ -229,6 +191,7 @@ class power_Resume(test.test):
         raise ValueError('Unable to read the hardware clock -- ' +
                          hwclock_output)
 
+
     def _set_pm_print_times(self, enabled):
         cmd = 'echo %s > /sys/power/pm_print_times' % int(bool(enabled))
         (status, output) = commands.getstatusoutput(cmd)
@@ -237,11 +200,14 @@ class power_Resume(test.test):
         else:
             logging.info('Device resume times set to %s' % bool(enabled))
 
+
     def _enable_pm_print_times(self):
         self._set_pm_print_times(True)
 
+
     def _disable_pm_print_times(self):
         self._set_pm_print_times(False)
+
 
     def run_once(self):
         # Disconnect from 3G network to take out the variability of
@@ -281,6 +247,10 @@ class power_Resume(test.test):
 
             # Suspend the system to RAM
             sys_power.suspend_to_ram()
+
+            # initialize LogReader object
+            self._log_msg = \
+                cros_logging.LogReader(include_rotated_logs=False)
 
             # Get suspend and resume times from /var/log/messages
             start_suspend_time = self._get_start_suspend_time()
