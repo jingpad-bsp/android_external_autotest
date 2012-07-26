a12a986bbae03075117eff9b96d6f3a49d280edb
Tom Wai-Hong Tam <waihong@chromium.org>
2012-07-26 17:22:41 +0800

    faft: Fix the missing library of logging in DevTriggerRecovery
    Forgot to import it before. Add it back and remove the unused time library.
    
    BUG=chrome-os-partner:10787
    TEST=run firmware_DevTriggerRecovery on Link passed.
    
    Change-Id: Ib04c7729044393cc379232ec4327da25d914d650
    Reviewed-on: https://gerrit.chromium.org/gerrit/28484
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Ready: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

--- server/site_tests/firmware_DevTriggerRecovery/firmware_DevTriggerRecovery.py
+++ server/site_tests/firmware_DevTriggerRecovery/firmware_DevTriggerRecovery.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import time
+import logging
 
 from autotest_lib.server.cros.faftsequence import FAFTSequence
 
