e03c32a4277b19cd0e5b0d68eaa5e44f0308daba
Tom Wai-Hong Tam <waihong@chromium.org>
2012-07-26 16:22:32 +0800

    faft: Remove the Alex/ZGB legacy dev transition state in DevMode test
    The dev transition state is deprecated and only used in Alex/ZGB firmware.
    This CL removes that checks and simplify the DevMode test. It uses the new
    dev/normal switching methods which support keyboard controlled dev mode.
    
    BUG=chrome-os-partner:9223
    TEST=run firmware_DevMode on Link passed.
    
    Change-Id: I9d2eaf4e2d57945e186d5dc2c6b7266ba75f82da
    Reviewed-on: https://gerrit.chromium.org/gerrit/28481
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Ready: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

--- server/site_tests/firmware_DevMode/firmware_DevMode.py
+++ server/site_tests/firmware_DevMode/firmware_DevMode.py
@@ -2,8 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import time
-
 from autotest_lib.server.cros.faftsequence import FAFTSequence
 
 
@@ -13,55 +11,10 @@ class firmware_DevMode(FAFTSequence):
     """
     version = 1
 
-    # True if Alex/ZGB which needs a transition state to enter dev mode.
-    need_dev_transition = False
-
-
-    # The devsw off->on transition states are different based on platforms.
-    # For Alex/ZGB, it is dev switch on but normal firmware boot.
-    # For other platforms, it is dev switch on and developer firmware boot.
-    def check_devsw_on_transition(self, fwb_tries='0'):
-        if self.faft_client.get_platform_name() in ('Alex', 'ZGB'):
-            self.need_dev_transition = True
-            return self.crossystem_checker({
-                    'devsw_boot': '1',
-                    'mainfw_act': 'A' if fwb_tries == '0' else 'B',
-                    'mainfw_type': 'normal',
-                    'tried_fwb': fwb_tries,
-                })
-        else:
-            return self.crossystem_checker({
-                    'devsw_boot': '1',
-                    'mainfw_act': 'A' if fwb_tries == '0' else 'B',
-                    'mainfw_type': 'developer',
-                    'tried_fwb': fwb_tries,
-                })
-
-
-    # The devsw on->off transition states are different based on platforms.
-    # For Alex/ZGB, it is firmware B normal boot. Firmware A is still developer.
-    # For other platforms, it is directly firmware A normal boot.
-    def check_devsw_off_transition(self, fwb_tries='0'):
-        if self.need_dev_transition:
-            return self.crossystem_checker({
-                    'devsw_boot': '0',
-                    'mainfw_act': 'B',
-                    'mainfw_type': 'normal',
-                    'tried_fwb': fwb_tries,
-                })
-        else:
-            return self.crossystem_checker({
-                    'devsw_boot': '0',
-                    'mainfw_act': 'A' if fwb_tries == '0' else 'B',
-                    'mainfw_type': 'normal',
-                    'tried_fwb': fwb_tries,
-                })
-
 
     def setup(self):
         super(firmware_DevMode, self).setup()
         self.setup_dev_mode(dev_mode=False)
-        self.setup_tried_fwb(tried_fwb=False)
 
 
     def run_once(self, host=None):
@@ -69,53 +22,23 @@ class firmware_DevMode(FAFTSequence):
             {   # Step 1, enable dev mode
                 'state_checker': (self.crossystem_checker, {
                     'devsw_boot': '0',
-                    'mainfw_act': 'A',
                     'mainfw_type': 'normal',
                 }),
-                'userspace_action': self.servo.enable_development_mode,
-                'firmware_action': self.wait_fw_screen_and_ctrl_d,
-            },
-            {   # Step 2, expected values based on platforms (see above),
-                # and set fwb_tries flag.
-                'state_checker': (self.check_devsw_on_transition, '0'),
-                'userspace_action': self.faft_client.set_try_fw_b,
-                'firmware_action': self.wait_fw_screen_and_ctrl_d,
-            },
-            {
-                # Step 3, expected the same state,
-                # and run "chromeos-firmwareupdate --mode todev && reboot".
-                'state_checker': (self.check_devsw_on_transition, '1'),
-                'userspace_action': (self.faft_client.run_shell_command,
-                    'chromeos-firmwareupdate --mode todev && reboot'),
-                # Ignore the default reboot_action here because the
-                # userspace_action (firmware updater) will reboot the system.
+                'userspace_action': self.enable_dev_mode_and_fw,
                 'reboot_action': None,
                 'firmware_action': self.wait_fw_screen_and_ctrl_d,
             },
-            {   # Step 4, expected developer mode boot and disable dev switch
+            {   # Step 2, expected developer mode boot and enable normal mode
                 'state_checker': (self.crossystem_checker, {
                     'devsw_boot': '1',
-                    'mainfw_act': 'A',
                     'mainfw_type': 'developer',
                 }),
-                'userspace_action': self.servo.disable_development_mode,
-            },
-            {   # Step 5, expected values based on platforms (see above),
-                # and set fwb_tries flag.
-                'state_checker': (self.check_devsw_off_transition, '0'),
-                'userspace_action': self.faft_client.set_try_fw_b,
-            },
-            {   # Step 6, expected the same state,
-                # and run "chromeos-firmwareupdate --mode tonormal && reboot".
-                'state_checker': (self.check_devsw_off_transition, '1'),
-                'userspace_action': (self.faft_client.run_shell_command,
-                    'chromeos-firmwareupdate --mode tonormal && reboot'),
+                'userspace_action': self.enable_normal_mode_and_fw,
                 'reboot_action': None,
             },
-            {   # Step 7, expected normal mode boot, done
+            {   # Step 3, expected normal mode boot, done
                 'state_checker': (self.crossystem_checker, {
                     'devsw_boot': '0',
-                    'mainfw_act': 'A',
                     'mainfw_type': 'normal',
                 }),
             }
