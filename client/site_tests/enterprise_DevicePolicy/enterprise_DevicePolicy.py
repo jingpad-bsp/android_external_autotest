cf6a7ce9765f4fd08d5d7b8c4d8c122df3b938e8
Nirnimesh <nirnimesh@chromium.org>
2012-08-08 18:35:05 -0700

    [autotest] Add a test for enterprise device policy in BVT
    Currently behind the experimental flag
    
    CQ-DEPEND=CL:29708
    
    BUG=None
    TEST=None
    
    Change-Id: Ic981d3c0e0980c490d38541988bd60690d6bb88b
    Reviewed-on: https://gerrit.chromium.org/gerrit/29710
    Reviewed-by: Nirnimesh <nirnimesh@chromium.org>
    Tested-by: Nirnimesh <nirnimesh@chromium.org>
    Commit-Ready: Nirnimesh <nirnimesh@chromium.org>

---
+++ client/site_tests/enterprise_DevicePolicy/enterprise_DevicePolicy.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.client.cros import chrome_test
+
+
+class enterprise_DevicePolicy(chrome_test.PyAutoFunctionalTest):
+    version = 1
+
+
+    def run_once(self):
+        self.run_pyauto_functional(tests=['chromeos_device_policy'])
