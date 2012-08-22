cee14733a30e652e084d34d3149a10ba9c61dc67
Chris Sosa <sosa@chromium.org>
2012-08-21 19:08:53 -0700

    Wget should retry even on connection refused.
    Connection refused can happen if the network stack crashes. We should be able
    to continue once it comes back up.
    
    BUG=chromium-os:33725
    TEST=Pending
    
    Change-Id: Iff84c73e0a1bc7c3c6a5e4bbd7a8b27649780da0
    Reviewed-on: https://gerrit.chromium.org/gerrit/31077
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Reviewed-by: Thieu Le <thieule@chromium.org>
    Commit-Ready: Chris Sosa <sosa@chromium.org>
    Tested-by: Chris Sosa <sosa@chromium.org>

--- client/common_lib/site_packages.py
+++ client/common_lib/site_packages.py
@@ -2,6 +2,9 @@ from autotest_lib.client.common_lib import base_packages
 
 
 class SiteHttpFetcher(base_packages.HttpFetcher):
+    wget_cmd_pattern = ('wget --connect-timeout=15 --retry-connrefused '
+                        '--wait=5 -nv %s -O %s')
+
     # shortcut quick http test for now since our dev server does not support
     # this operation.
     def _quick_http_test(self):
