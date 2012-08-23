9f761ba0ad014b207541d57c9134e16262f7cfb5
John Dung Hong <johndhong@google.com>
2012-08-23 11:19:58 -0700

    Changed to point to the correct binary by dropping the omx_ prefix.
    BUG=chromium-os:32702
    TEST=./run_remote_tests.sh --remote=chromeos1-rack3-host3.cros OMX and ./bin/autotest_client tests/desktopui_OMXTest/control on the DUT
    
    Change-Id: I31cdbee60b06b240b0911580296abba3bbb11416
    Reviewed-on: https://gerrit.chromium.org/gerrit/31236
    Commit-Ready: John D Hong <johndhong@google.com>
    Tested-by: John D Hong <johndhong@google.com>
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Reviewed-by: Ami Fischman <fischman@chromium.org>

--- client/site_tests/desktopui_OMXTest/desktopui_OMXTest.py
+++ client/site_tests/desktopui_OMXTest/desktopui_OMXTest.py
@@ -15,5 +15,5 @@ class desktopui_OMXTest(chrome_test.ChromeBinaryTest):
     # the bots green.
     cmd_line_params = ('--test_video_data="%s:320:240:250:258:35:150:1"' %
                        test_video_file)
-    self.run_chrome_binary_test('omx_video_decode_accelerator_unittest',
+    self.run_chrome_binary_test('video_decode_accelerator_unittest',
                                 cmd_line_params)
