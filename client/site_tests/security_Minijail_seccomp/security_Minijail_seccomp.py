049102fa8534cc91ad7c38ae445721f7bfdd24df
Jorge Lucangeli Obes <jorgelo@chromium.org>
2012-08-23 16:33:32 -0700

    More comprehensive testing for seccomp filter in Minijail.
    BUG=chromium-os:32619
    TEST=run_remote_tests security_Minijail_seccomp against lumpy, alex, daisy.
    
    Change-Id: Ic86cab4dc668bae6767657f11843df7108ce6abf
    Reviewed-on: https://gerrit.chromium.org/gerrit/31289
    Tested-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Jim Hebert <jimhebert@chromium.org>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Commit-Ready: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- client/site_tests/security_Minijail_seccomp/security_Minijail_seccomp.py
+++ client/site_tests/security_Minijail_seccomp/security_Minijail_seccomp.py
@@ -5,8 +5,12 @@
 import logging
 import os
 
+from collections import namedtuple
+
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
+
+Jail = namedtuple("Jail", "user policy nnp")
 
 class security_Minijail_seccomp(test.test):
     version = 1
@@ -18,10 +22,26 @@ class security_Minijail_seccomp(test.test):
         utils.make()
 
 
-    def run_test(self, exe, expected_ret, pretty_msg):
-        cmdline = '/sbin/minijail0 -S %s/policy %s/%s' % (self.bindir,
-                                                          self.bindir,
-                                                          exe)
+    def get_arch(self):
+        full_arch = utils.get_arch()
+        if "arm" in full_arch:
+            return "arm"
+        else:
+            return full_arch
+
+
+    def run_test(self, exe, jail, expected_ret, pretty_msg):
+        cmdline = '/sbin/minijail0'
+
+        if jail.user:
+            cmdline += ' -u %s' % jail.user
+
+        if jail.nnp:
+            cmdline += ' -n'
+
+        cmdline += ' -S %s/%s %s/%s' % (self.bindir, jail.policy,
+                                        self.bindir, exe)
+
         logging.info("Command line: " + cmdline)
         ret = utils.system(cmdline, ignore_status=True)
 
@@ -31,8 +51,24 @@ class security_Minijail_seccomp(test.test):
 
 
     def run_once(self):
-        case_ok = ("ok", 0, "Allowed system calls failed")
-        case_fail = ("fail", 253, "Blocked system calls succeeded")
+        privdrop_policy = "policy-privdrop_" + self.get_arch()
 
-        for exe, expected_ret, pretty_msg in [case_ok, case_fail]:
-            self.run_test(exe, expected_ret, pretty_msg)
+        case_ok = ("ok",
+                   Jail(None, "policy", nnp=False),
+                   0, "Allowed system calls failed")
+        case_block_privdrop = ("ok",
+                               Jail("chronos", "policy", nnp=False),
+                               253, "Blocked priv-drop system calls succeeded")
+        case_allow_privdrop = ("ok",
+                               Jail("chronos", privdrop_policy, nnp=False),
+                               0, "Allowed system calls failed")
+        case_no_new_privs = ("ok",
+                             Jail("chronos", "policy", nnp=True),
+                             0, "Allowed system calls failed")
+        case_fail = ("fail",
+                     Jail(None, "policy", nnp=False),
+                     253, "Blocked system calls succeeded")
+
+        for case in [case_ok, case_block_privdrop, case_allow_privdrop,
+                     case_no_new_privs, case_fail]:
+            self.run_test(*case)
