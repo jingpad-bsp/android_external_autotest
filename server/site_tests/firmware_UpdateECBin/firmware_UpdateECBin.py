9b917b81b622f6838cda42e3bdef8b7ac5beaee6
Tom Wai-Hong Tam <waihong@chromium.org>
2012-08-24 16:21:10 +0800

    faft: Implement a new test for Software Sync EC update
    This test verified the EC software sync. The EC binary is embedded in
    the BIOS image. On AP startup, AP verifies the EC by comparing the
    hash of the current EC with the hash of the embedded EC image. So
    updating EC is simple and we just need to change the EC binary on
    the BIOS image.
    
    The test steps are:
     1. check EC is RO boot and AP is RO boot,
        extract the original EC binary on current AP RW A and save it locally,
        copy the new_ec binary to the EC area on the current AP RW A,
        disable RO_BOOT preamble flag,
        reboot;
     2. check EC is RW boot and AP is RW A boot,
        compare the current EC with the new_ec binary,
        copy the original EC binary to the EC area on the current AP RW A,
        reboot;
     3. check EC is RW boot and AP is RW A boot,
        compare the current EC with the original EC binary,
        enable RO_BOOT preamble flag,
        reboot;
     4. check EC is RO boot and AP is RW A boot.
    
    BUG=chrome-os-partner:12997
    TEST=After merging some EC update test changes, run:
    run_remote_test.sh -a "new_ec=ec_autest_image.bin" firmware_UpdateECBin/control$
    
    Software sync still has some issue (http://crosbug.com/p/12680) that RO-Boot ->
    TwoStop update is failed. So this is expected to be failed.
    
    Change-Id: I0801266f5554129c17fc39312cfcb3163650f227
    Reviewed-on: https://gerrit.chromium.org/gerrit/31328
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Commit-Ready: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

---
+++ server/site_tests/firmware_UpdateECBin/firmware_UpdateECBin.py
@@ -0,0 +1,128 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import logging, os
+
+from autotest_lib.client.common_lib import error, utils
+from autotest_lib.server.cros.faftsequence import FAFTSequence
+
+
+class firmware_UpdateECBin(FAFTSequence):
+    """
+    This test verified the EC software sync. The EC binary is embedded in
+    the BIOS image. On AP startup, AP verifies the EC by comparing the
+    hash of the current EC with the hash of the embedded EC image. So
+    updating EC is simple and we just need to change the EC binary on
+    the BIOS image.
+
+    A new EC image should be specified by the new_ec argument, like:
+      -a "new_ec=ec_autest_image.bin"
+
+    The test covers RONORMAL->TWOSTOP, TWOSTOP->TWOSTOP, and
+    TWOSTOP->RONORMAL updates.
+    """
+    version = 1
+
+    PREAMBLE_USE_RO_NORMAL = 1
+
+
+    def ensure_fw_a_boot(self):
+        """Ensure firmware A boot this time."""
+        if not self.crossystem_checker({'mainfw_act': 'A', 'tried_fwb': '0'}):
+            self.run_faft_step({
+                'userspace_action': (self.faft_client.run_shell_command,
+                    'chromeos-firmwareupdate --mode recovery')
+            })
+
+
+    def initialize(self, host, cmdline_args, use_pyauto=False, use_faft=True):
+        # Parse arguments from command line
+        dict_args = utils.args_to_dict(cmdline_args)
+        if 'new_ec' not in dict_args:
+            raise error.TestError('Should specify a new_ec image for update, '
+                                  'e.g. -a "new_ec=ec_autest_image.bin".')
+        self.arg_new_ec = dict_args['new_ec']
+        logging.info('The EC image to-be-updated is: %s' % self.arg_new_ec)
+        super(firmware_UpdateECBin, self).initialize(host, cmdline_args,
+                                                     use_pyauto, use_faft)
+
+
+    def setup(self, host, dev_mode=False):
+        super(firmware_UpdateECBin, self).setup()
+        self.setup_dev_mode(dev_mode)
+        self.ensure_fw_a_boot()
+
+        temp_path = self.faft_client.get_temp_path()
+        self.faft_client.setup_firmwareupdate_temp_dir()
+        self.old_ec_path = os.path.join(temp_path, 'old_ec.bin')
+        self.new_ec_path = os.path.join(temp_path, 'new_ec.bin')
+
+        self.faft_client.dump_EC_firmware(self.old_ec_path)
+        self.old_ec_sha = self.faft_client.get_EC_firmware_sha()
+
+        host.send_file(self.arg_new_ec, self.new_ec_path)
+        self.faft_client.setup_EC_image(self.new_ec_path)
+        self.new_ec_sha = self.faft_client.get_EC_image_sha()
+
+
+    def cleanup(self):
+        self.ensure_fw_a_boot()
+        super(firmware_UpdateECBin, self).cleanup()
+
+
+    def run_once(self, host=None):
+        flags = self.faft_client.get_firmware_flags('a')
+        if flags & self.PREAMBLE_USE_RO_NORMAL == 0:
+            logging.info('The firmware USE_RO_NORMAL flag is disabled.')
+            return
+
+        self.register_faft_sequence((
+            {   # Step 1, expected EC RO boot, update EC and disable RO flag
+                'state_checker': (self.crossystem_checker, {
+                    'mainfw_act': 'A',
+                    'tried_fwb': '0',
+                    'ecfw_act': 'RO',
+                }),
+                'userspace_action': (self.faft_client.update_EC_from_image,
+                                     'a', flags ^ self.PREAMBLE_USE_RO_NORMAL),
+                'reboot_action': self.sync_and_cold_reboot,
+            },
+            {   # Step 2, expected new EC and RW boot, restore the original EC
+                'state_checker': (
+                    lambda: self.crossystem_checker({
+                                'mainfw_act': 'A',
+                                'tried_fwb': '0',
+                                'ecfw_act': 'RW',
+                            }) and (
+                               self.faft_client.get_EC_firmware_sha() ==
+                               self.new_ec_sha)),
+                'userspace_action': (
+                    lambda: (self.faft_client.setup_EC_image(self.old_ec_path),
+                             self.faft_client.update_EC_from_image(
+                                 'a', flags ^ self.PREAMBLE_USE_RO_NORMAL))),
+                'reboot_action': self.sync_and_cold_reboot,
+            },
+            {   # Step 3, expected original EC and RW boot, enable RO flag
+                'state_checker': (
+                    lambda: self.crossystem_checker({
+                                'mainfw_act': 'A',
+                                'tried_fwb': '0',
+                                'ecfw_act': 'RW',
+                            }) and (
+                               self.faft_client.get_EC_firmware_sha() ==
+                               self.old_ec_sha)),
+                'userspace_action': (self.faft_client.update_ecbin_from_image,
+                                     'a', flags),
+                'reboot_action': self.sync_and_cold_reboot,
+            },
+            {   # Step 4, expected EC RO boot, done
+                'state_checker': ({
+                    'mainfw_act': 'A',
+                    'tried_fwb': '0',
+                    'ecfw_act': 'RO',
+                }),
+            },
+        ))
+        self.run_faft_sequence()
