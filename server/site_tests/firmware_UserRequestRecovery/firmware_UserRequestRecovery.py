500922f726ba583c0e19b7c864117c6d5a5c0fda
Tom Wai-Hong Tam <waihong@chromium.org>
2012-08-31 15:01:43 +0800

    faft: Try pressing Ctrl-D + Enter on user-requested REC_INSERT screen
    This change verifies that user-requested REC_INSERT screen should not trigger
    TO_DEV screen. Only pressing Power + Esc + Refresh can trigger TO_DEV screen.
    
    BUG=chrome-os-partner:13439
    TEST=run_remote_test.sh --remote=$IP firmware_UserRequestRecovery/control$
         run_remote_test.sh --remote=$IP firmware_UserRequestRecovery/control.dev
    
    Change-Id: I59ffa7a5ad17d37ddc033ffebaf5906c89706ba3
    Reviewed-on: https://gerrit.chromium.org/gerrit/32018
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Commit-Ready: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Tom Wai-Hong Tam <waihong@chromium.org>

--- server/site_tests/firmware_UserRequestRecovery/firmware_UserRequestRecovery.py
+++ server/site_tests/firmware_UserRequestRecovery/firmware_UserRequestRecovery.py
@@ -33,12 +33,24 @@ class firmware_UserRequestRecovery(FAFTSequence):
         super(firmware_UserRequestRecovery, self).setup()
         self.assert_test_image_in_usb_disk()
         self.setup_dev_mode(dev_mode)
-        self.servo.set('usb_mux_sel1', 'dut_sees_usbkey')
+        self.servo.enable_usb_hub(host=True)
 
 
     def cleanup(self):
         self.ensure_normal_boot()
         super(firmware_UserRequestRecovery, self).cleanup()
+
+
+    def try_dev_switching_and_plug_usb(self, dev_mode):
+        """Try pressing Ctrl-D and enter to check its firmware behavior."""
+        # Pressing Ctrl-D + Enter / Enter + Enter should not trigger
+        # dev / normal mode switching.
+        if self.client_attr.keyboard_dev:
+            self.wait_fw_screen_and_switch_keyboard_dev_mode(not dev_mode)
+            if not dev_mode:
+                self.wait_fw_screen_and_ctrl_d()
+
+        self.wait_fw_screen_and_plug_usb()
 
 
     def run_once(self, host=None, dev_mode=False):
@@ -48,17 +60,26 @@ class firmware_UserRequestRecovery(FAFTSequence):
                     'mainfw_type': 'developer' if dev_mode else 'normal',
                 }),
                 'userspace_action': self.faft_client.request_recovery_boot,
-                'firmware_action': None if dev_mode else
-                                   self.wait_fw_screen_and_plug_usb,
+                'firmware_action': (self.try_dev_switching_and_plug_usb,
+                                    dev_mode),
                 'install_deps_after_boot': True,
             },
-            {   # Step 2, expected recovery boot
+            {   # Step 2, expected recovery boot, request recovery again
+                'state_checker': (self.crossystem_checker, {
+                    'mainfw_type': 'recovery',
+                    'recovery_reason' : self.RECOVERY_REASON['US_TEST'],
+                }),
+                'userspace_action': self.faft_client.request_recovery_boot,
+                'firmware_action': None if dev_mode else
+                                   self.wait_fw_screen_and_plug_usb,
+            },
+            {   # Step 3, expected recovery boot
                 'state_checker': (self.crossystem_checker, {
                     'mainfw_type': 'recovery',
                     'recovery_reason' : self.RECOVERY_REASON['US_TEST'],
                 }),
             },
-            {   # Step 3, expected normal boot
+            {   # Step 4, expected normal boot
                 'state_checker': (self.crossystem_checker, {
                     'mainfw_type': 'developer' if dev_mode else 'normal',
                 }),
