4c8a6ed1b409eaba0a93ef9913effb9790e72ba0
Joseph Hwang <josephsih@chromium.org>
2012-09-04 17:17:58 +0800

    firmware_TouchpadMTB: add the test configuration file
    This module defines the test configuration. It includes the gestures
    and the related variations for a user to perform. It also describes
    what validators are to be applied for every gesture.
    
    BUG=chromium-os:34115
    TEST=None.
    
    Change-Id: I1690ffd2c8d4a8974da58450d262e398007fcc89
    Reviewed-on: https://gerrit.chromium.org/gerrit/32140
    Reviewed-by: Andrew de los Reyes <adlr@chromium.org>
    Commit-Ready: Joseph Shyh-In Hwang <josephsih@chromium.org>
    Tested-by: Joseph Shyh-In Hwang <josephsih@chromium.org>

---
+++ client/site_tests/firmware_TouchpadMTB/test_conf.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""This configuration file defines the gestures to perform."""
+
+from firmware_utils import Gesture
+from validators import (CountTrackingIDValidator,
+                        LinearityValidator,
+                        RangeValidator,
+                        StationaryFingerValidator,
+)
+
+# Include some constants
+execfile('firmware_constants.py', globals())
+
+
+# Define which score aggregator is to be used. A score aggregator collects
+# the scores from every tests and calculates the final score for the touchpad
+# firmware test suite.
+score_aggregator = 'fuzzy.average'
+
+
+# Define the gesture list that the user needs to perform in the test suite.
+gesture_list = [
+    Gesture(
+        name='stationary_finger_not_affected_by_2nd_moving_finger',
+        variations=(HORIZONTAL, VERTICAL, DIAGONAL),
+        prompt='Place your left finger on the {0} side. '
+               'And use 2nd finger to draw {1}.',
+        subprompt={
+            HORIZONTAL: ('lower half', 'a horizontal line from left to right'),
+            VERTICAL: ('left hand', 'a vertical line from top to bottom'),
+            DIAGONAL: ('near center', 'a right half circle'),
+        },
+        validators=(
+            StationaryFingerValidator('<= 20, ~ +20', slot=0),
+        ),
+    ),
+
+    Gesture(
+        name='one_finger_tracking',
+        variations=(HORIZONTAL, VERTICAL, DIAGONAL),
+        prompt='Draw a {0} line {1}.',
+        subprompt={
+            HORIZONTAL: ('horizontal', 'from left edge to right edge'),
+            VERTICAL: ('vertical', 'from top edge to bottom edge'),
+            DIAGONAL: ('diagonal',
+                       'from lower left corner to upper right corner'),
+        },
+        validators=(
+            CountTrackingIDValidator('== 1'),
+            LinearityValidator('<= 0.03, ~ +0.07'),
+            RangeValidator('<= 0.05, ~ +0.05'),
+        ),
+    ),
+
+    Gesture(
+        name='two_finger_tracking',
+        variations=(HORIZONTAL, VERTICAL, DIAGONAL),
+        prompt='Use two fingers to draw {0} lines {1}.',
+        subprompt={
+            HORIZONTAL: ('horizontal', 'from left edge to right edge'),
+            VERTICAL: ('vertical', 'from top edge to bottom edge'),
+            DIAGONAL: ('diagonal',
+                       'from lower left corner to upper right corner'),
+        },
+        validators=(
+            CountTrackingIDValidator('== 2'),
+            LinearityValidator('<= 0.03, ~ +0.07', fingers=2),
+        ),
+    ),
+]
+
+
+class FileName:
+    """A dummy class to hold the attributes in a test file name."""
+    pass
+filename = FileName()
+filename.sep = '-'
+filename.ext = 'dat'
