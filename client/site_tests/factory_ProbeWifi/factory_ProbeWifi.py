41e116e60a6d2e99da18ec86bd7baaf512ebef00
Chun-Ta Lin <itspeter@google.com>
2012-09-04 19:13:41 +0800

    refactor factory_ProbeWifi
    Refactor factory_ProbeWifi to reduce redundant code.
    
    BUG=chrome-os-partner:12448
    TEST=Manually on DUT
    
    Change-Id: I09df32572ca42885c85fb1f7af8d04876277bdb5
    Reviewed-on: https://gerrit.chromium.org/gerrit/32132
    Reviewed-by: Jon Salz <jsalz@chromium.org>
    Commit-Ready: Chun-Ta Lin <itspeter@google.com>
    Tested-by: Chun-Ta Lin <itspeter@google.com>

--- client/site_tests/factory_ProbeWifi/factory_ProbeWifi.py
+++ client/site_tests/factory_ProbeWifi/factory_ProbeWifi.py
@@ -12,52 +12,63 @@ from cros.factory.test import factory
 from cros.factory.event_log import EventLog
 from cros.factory.test.test_ui import UI
 
-_HTML = '''
+_HTML_PREFIX = '''
 <h1>LAN & Bluetooth MAC address probing</h1><br>
-<h2>%s</h2><br>
-<h2>%s</h2><br>
-<input type="button" value="Finished" id="pass"
-  onClick="test.pass()">
 '''
-
-_JS = '''
-document.getElementById("pass").focus();
+_HTML_POSTFIX = '''
+<input type="button" value="Finished"
+  onClick="test.pass()">
 '''
 
 LAN_MAC_PATH = "/sys/class/net/%s/address"
 BT_MAC_PATH = "/sys/class/bluetooth/%s/address"
 
 class factory_ProbeWifi(test.test):
-    version = 1
+    version = 2
+
+    def probe_devices(self, probe_list):
+        '''Probe devices' MAC address.
+
+        Args:
+            probe_list: A list of tuples in the format:
+            (type recorded in eventlog,
+             device path,
+             label to display in English,
+             label to display in Chinese)
+        '''
+        display_strings = []
+        for mac_type, device_path, label_en, label_zh in probe_list:
+             if os.path.exists(device_path):
+                 mac = open(device_path).read().strip()
+                 display_strings.append(u'Mac address of %s(%s): %s' % (
+                     label_en, label_zh, mac))
+             else:
+                 raise error.TestFail(
+                     '%s device %s does not exist' % (mac_type, device_path))
+
+             factory.console.info(display_str)
+             self.event_log.Log('mac', mac_type=mac_type, mac=mac)
+        return display_strings
 
     def run_once(self, lan_device='wlan0', bt_device='hci0', display=False):
-        event_log = EventLog.ForAutoTest()
+        self.event_log = EventLog.ForAutoTest()
         self.ui = UI()
-        lan_mac = bt_mac = ''
-
+        probe_list = []
         if lan_device:
-            path = LAN_MAC_PATH % lan_device
-            if os.path.exists(path):
-                lan_mac = 'LAN[%s](网路) MAC address: [%s]' % (
-                    lan_device, open(path).read().strip())
-            else:
-                raise error.TestFail(
-                    'LAN device %s does not exist' % lan_device)
-            factory.console.info(lan_mac)
-            event_log.Log('lan_mac', mac=lan_mac)
+            probe_list.append(('lan',
+                               LAN_MAC_PATH % lan_device,
+                               u'LAN', u'網路'))
 
         if bt_device:
-            path = BT_MAC_PATH % bt_device
-            if os.path.exists(path):
-                bt_mac = 'Bluetooth[%s](蓝牙) MAC address: [%s]' % (
-                    bt_device, open(path).read().strip())
-            else:
-                raise error.TestFail(
-                    'Bluetooth device %s does not exist' % bt_device)
-            factory.console.info(bt_mac)
-            event_log.Log('bluetooth_mac', mac=bt_mac)
+            probe_list.append(('bt',
+                               BT_MAC_PATH % bt_device,
+                               u'Bluetooth', u'藍芽'))
 
+        display_strings = tuple(self.probe_devices(probe_list))
+        factory.log(display_strings)
         if display:
-            self.ui.SetHTML(_HTML % (lan_mac, bt_mac))
-            self.ui.RunJS(_JS)
+            html = _HTML_PREFIX
+            for display_string in display_strings:
+                html += "<h2>%s</h2><br>" % display_string
+            self.ui.SetHTML(html + _HTML_POSTFIX)
             self.ui.Run()
