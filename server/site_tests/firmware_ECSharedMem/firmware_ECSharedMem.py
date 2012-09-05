57cb4b618b53184f2bdda43b555985a0468a3143
Vic Yang <victoryang@chromium.org>
2012-09-06 03:27:03 +0800

    Add a test of Chrome EC shared memory
    This test checks EC has enough shared memory under following conditions:
      - Normal operation
      - Reboot after crashing
      - Jumped between images
    
    BUG=chrome-os-partner:13529
    TEST=Test passed.
    
    Change-Id: I89a6da4b6cf59daaaddf692a04b1db92fd332bf8
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/32252
    Reviewed-by: Randall Spangler <rspangler@chromium.org>

---
+++ server/site_tests/firmware_ECSharedMem/firmware_ECSharedMem.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import time
+
+from autotest_lib.client.common_lib import error
+from autotest_lib.server.cros.faftsequence import FAFTSequence
+
+class firmware_ECSharedMem(FAFTSequence):
+    """
+    Servo based EC shared memory test.
+    """
+    version = 1
+
+    # Delay for EC boot
+    EC_BOOT_DELAY = 0.5
+
+
+    def shared_mem_checker(self):
+        match = self.send_uart_command_get_output("shmem",
+                                                  ["Size:\s+([0-9-]+)\r"])[0]
+        shmem_size = int(match.group(1))
+        logging.info("EC shared memory size if %d bytes", shmem_size)
+        if shmem_size <= 0:
+            return False
+        elif shmem_size <= 256:
+            logging.warning("EC shared memory is less than 256 bytes")
+        return True
+
+
+    def jump_checker(self):
+        self.send_uart_command("sysjump RW")
+        time.sleep(self.EC_BOOT_DELAY)
+        return self.shared_mem_checker()
+
+
+    def run_once(self, host=None):
+        if not self.check_ec_capability():
+            return
+        self.register_faft_sequence((
+            {   # Step 1, check shared memory in normal operation and crash EC
+                'state_checker': self.shared_mem_checker,
+                'reboot_action': (self.send_uart_command, "crash unaligned")
+            },
+            {   # Step 2, Check shared memory after crash and system jump
+                'state_checker': (lambda: self.shared_mem_checker() and
+                                          self.jump_checker()),
+                'reboot_action': self.sync_and_ec_reboot,
+            },
+            {   # Step 3, dummy step to make step 2 reboot so as to clean EC
+                #         state.
+            }
+        ))
+        self.run_faft_sequence()
