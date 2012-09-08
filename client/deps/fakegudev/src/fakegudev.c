f13c14093db9a5f07af0837740f241fac0818e60
Ben Chan <benchan@chromium.org>
2012-09-07 21:50:07 -0700

    autotest: Fix compilation errors when compiling with glib 2.32.
    This CL makes the following changes to make the code compatible with
    glib 2.30 and 2.32:
    - Remove unnecessary inclusion of glib/gtypes.h in fakegudev.c as
      glib/gtypes.h should not be included directly.
    - Use g_io_channel_shutdown() instead of the deprecated
      g_io_channel_close().
    
    BUG=chromium-os:34104
    TEST=emerge autotest and autotest-deps with glib 2.30.2 and 2.32.4
    
    Change-Id: I2f98d1fd1c7fdaa26aedf4595176c8d613f1fa13
    Reviewed-on: https://gerrit.chromium.org/gerrit/32654
    Commit-Ready: Ben Chan <benchan@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>
    Reviewed-by: Darin Petkov <petkov@chromium.org>

--- client/deps/fakegudev/src/fakegudev.c
+++ client/deps/fakegudev/src/fakegudev.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -7,7 +7,6 @@
 #define _GNU_SOURCE /* for RTLD_NEXT in dlfcn.h */
 
 #include <glib.h>
-#include <glib/gtypes.h>
 #include <glib-object.h>
 
 #include <dlfcn.h>
--- client/deps/fakemodem/src/fakemodem.c
+++ client/deps/fakemodem/src/fakemodem.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -159,7 +159,7 @@ parse_pattern_files(char **pattern_files, GError **error)
       }
       g_free (line);
     }
-    g_io_channel_close (pf);
+    g_io_channel_shutdown (pf, TRUE, NULL);
   }
 
   g_regex_unref (skip);
