48bcdc050963972a7c349edff1839977efd3a1cc
J. Richard Barnette <jrbarnette@chromium.org>
2012-10-04 14:09:55 -0700

    Correctly print milliseconds in log timestamps.
    Millisecond in log timestamps were being shown without leading
    0's when the millisecond value was less than 100.
    
    BUG=None
    TEST=run_remote_tests.sh, look at autoserv.DEBUG timestamps
    
    Change-Id: I2ba860f5b31420c241b8b10d7dab1bcb53ded207
    Reviewed-on: https://gerrit.chromium.org/gerrit/34675
    Tested-by: Richard Barnette <jrbarnette@chromium.org>
    Reviewed-by: Simran Basi <sbasi@chromium.org>
    Commit-Ready: Richard Barnette <jrbarnette@chromium.org>

--- client/common_lib/logging_config.py
+++ client/common_lib/logging_config.py
@@ -24,7 +24,7 @@ class LoggingConfig(object):
     stderr_level = logging.ERROR
 
     file_formatter = logging.Formatter(
-        fmt='%(asctime)s.%(msecs)d %(levelname)-5.5s|%(module)10.10s:'
+        fmt='%(asctime)s.%(msecs)03d %(levelname)-5.5s|%(module)10.10s:'
             '%(lineno)4.4d| %(message)s',
         datefmt='%m/%d %H:%M:%S')
 
