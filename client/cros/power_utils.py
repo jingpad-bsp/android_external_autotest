8848c0c397d96073322ce427e1c2f97049249a5b
Todd Broch <tbroch@chromium.org>
2012-10-11 16:40:06 -0700

    autotest: hardware_Backlight: Test keyboard backlight.
    BUG=chrome-os-partner:15055
    TEST=run test on systems w/ & w/o keyboard backlight successfully.
    
    Change-Id: Ia1b02ad0e460e4f24e617afad94fba723f038112
    Reviewed-on: https://gerrit.chromium.org/gerrit/35351
    Tested-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Simon Que <sque@chromium.org>
    Commit-Ready: Todd Broch <tbroch@chromium.org>

--- client/cros/power_utils.py
+++ client/cros/power_utils.py
@@ -1,8 +1,7 @@
 # Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-import logging
-import re
+import logging, os, re
 from autotest_lib.client.bin import utils
 
 def get_x86_cpu_arch():
@@ -42,3 +41,72 @@ def has_rapl_support():
     if cpu_arch and ((cpu_arch is 'Celeron') or (cpu_arch is 'Core')):
         return True
     return False
+
+
+class KbdBacklightException(Exception):
+    """Class for KbdBacklight exceptions."""
+
+
+class KbdBacklight(object):
+    """Class for control of keyboard backlight.
+
+    Example code:
+        kblight = power_utils.KbdBacklight()
+        kblight.set(10)
+        print "kblight % is %.f" % kblight.get()
+
+    Public methods:
+        set: Sets the keyboard backlight to a percent.
+        get: Get current keyboard backlight percentage.
+
+    Private functions:
+        _get_max: Retrieve maximum integer setting of keyboard backlight
+
+    Private attributes:
+        _path: filepath to keyboard backlight controls in sysfs
+        _max: cached value of 'max_brightness' integer
+
+    TODO(tbroch): deprecate direct sysfs access if/when these controls are
+    integrated into a userland tool such as backlight-tool in power manager.
+    """
+    DEFAULT_PATH = "/sys/class/leds/chromeos::kbd_backlight"
+
+    def __init__(self, path=DEFAULT_PATH):
+        if not os.path.exists(path):
+            raise KbdBacklightException('Unable to find path "%s"' % path)
+        self._path = path
+        self._max = None
+
+
+    def _get_max(self):
+        """Get maximum absolute value of keyboard brightness.
+
+        Returns:
+            integer, maximum value of keyboard brightness
+        """
+        if self._max is None:
+            self._max = int(utils.read_one_line(os.path.join(self._path,
+                                                             'max_brightness')))
+        return self._max
+
+
+    def get(self):
+        """Get current keyboard brightness setting.
+
+        Returns:
+            float, percentage of keyboard brightness.
+        """
+        current = int(utils.read_one_line(os.path.join(self._path,
+                                                       'brightness')))
+        return (current * 100 ) / self._get_max()
+
+
+    def set(self, percent):
+        """Set keyboard backlight percent.
+
+        Args:
+            percent: percent to set keyboard backlight to.
+        """
+        value = int((percent * self._get_max()) / 100)
+        cmd = "echo %d > %s" % (value, os.path.join(self._path, 'brightness'))
+        utils.system(cmd)
--- client/site_tests/hardware_Backlight/hardware_Backlight.py
+++ client/site_tests/hardware_Backlight/hardware_Backlight.py
@@ -1,10 +1,12 @@
 # Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import logging
 
 from autotest_lib.client.bin import test
 from autotest_lib.client.bin import utils
 from autotest_lib.client.common_lib import error
+from autotest_lib.client.cros import power_utils
 
 def backlight_tool(args):
     cmd = 'backlight-tool %s' % args
@@ -22,13 +24,37 @@ class hardware_Backlight(test.test):
         else:
             powerd_running = False
 
+        # optionally test keyboard backlight
+        kblight = None
+        kblight_errs = 0
+        try:
+            kblight = power_utils.KbdBacklight()
+        except power_utils.KbdBacklightException as e:
+            logging.info("Assuming no keyboard backlight due to %s", str(e))
+
+        if kblight:
+            init_percent = kblight.get()
+            try:
+                for i in xrange(100, -1, -1):
+                    kblight.set(i)
+                    result = int(kblight.get())
+                    if i != result:
+                        logging.error('keyboard backlight set %d != %d get',
+                                      i, result)
+                        kblight_errs += 1
+            finally:
+                kblight.set(init_percent)
+
+        if kblight_errs:
+            raise error.TestFail("%d errors testing keyboard backlight." % \
+                                     kblight_errs)
         try:
             brightness = int(backlight_tool("--get_brightness").rstrip())
         except error.CmdError, e:
             raise error.TestFail('Cannot get brightness with backlight-tool')
         max_brightness = int(backlight_tool("--get_max_brightness").rstrip())
         try:
-            for i in range(max_brightness + 1):
+            for i in xrange(max_brightness + 1):
                 backlight_tool("--set_brightness %d" % i)
                 result = int(backlight_tool("--get_brightness").rstrip())
                 if i != result:
