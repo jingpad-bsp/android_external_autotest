facf51195e190c9786862940f0924f2074dbfbf1
Scott Zawalski <scottz@google.com>
2012-10-12 17:51:00 -0400

    Update site_parse.py to work with recent changes to generate_test_report.
    site_parse.py uses genereate_test_report to create a json file that is then
    consumed by the Autotest BVT dashboard. Recent changes modified the behavior
    of generate_test_report to a point where site_parse.py could no longer use it.
    This CL fixes that.
    
    Since we no longer use site_parse.py to generate stacktraces that code has also
    been stripped out. The stacktraces code has been moved over to a separate process
    that is handled by the devserver and crashcollection in autoserv itself.
    
    TEST=tko/site_parse -r -l2 -o /usr/local/autotest/results/784982-scottz/ and verified json file
    was properly created.
    BUG=chromium-os:35196
    
    Change-Id: Ie6703b0bd97cb018366a472f4f90370bcf474756
    Reviewed-on: https://gerrit.chromium.org/gerrit/35452
    Reviewed-by: Chris Masone <cmasone@chromium.org>
    Commit-Ready: Scott Zawalski <scottz@chromium.org>
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Tested-by: Scott Zawalski <scottz@chromium.org>
    Reviewed-by: Mike Truty <truty@chromium.org>

--- tko/site_parse.py
+++ tko/site_parse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python -u
 #
-# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
@@ -65,9 +65,6 @@ class StackTrace(object):
     # Maximum time to wait for another instance to finish processing symbols.
     _SYMBOL_WAIT_TIMEOUT = 10 * 60
 
-    # Path to JSON test config relative to Autotest root.
-    _TEST_CONFIG_PATH = 'utils/dashboard/chromeos_test_config.json'
-
 
     def __init__(self, results_dir, cros_src_dir):
         """Initializes class variables.
@@ -80,15 +77,6 @@ class StackTrace(object):
         self._results_dir = results_dir
         self._cros_src_dir = cros_src_dir
         self._chroot_dir = os.path.join(self._cros_src_dir, 'chroot')
-
-        # Figure out the location of the test config JSON. Code is modeled after
-        # Autotest's standard common.py.
-        dirname = os.path.dirname(sys.modules[__name__].__file__)
-        autotest_dir = os.path.abspath(os.path.join(dirname, '..'))
-        test_config_path = os.path.join(autotest_dir, self._TEST_CONFIG_PATH)
-
-        with open(test_config_path) as f:
-            self._test_config = json.load(f)
 
 
     def _get_cache_dir(self):
@@ -135,220 +123,6 @@ class StackTrace(object):
         version = job_name.rsplit('-', 3)[1].split('_')[-1]
         arch, board, rev = job_name.split('-', 3)[:3]
         return '-'.join([arch, board]), rev, version
-
-
-    def _get_symbol_dir(self, job_name):
-        """Returns a path to the symbols dir relative to the chroot.
-
-        Retrieves symbols from Google Storage if they're not available in the
-        local cache. Requires gsutil to be present in the path.
-
-        Args:
-            job_name: Either a 3-tuple or 4-tuple job name as detailed in
-                _parse_job_name.
-
-        Returns:
-            A path to the symbols dir relative to the chroot.
-
-        Raises:
-            TimeoutError: If unable to acquire processing "lock" for the
-                requested symbols. An exception here indicates another instance
-                failed unexpectedly.
-            ValueError: If the gsutil command is not available.
-        """
-        cache_dir = self._get_cache_dir()
-        board, rev, version = self._parse_job_name(job_name)
-
-        # Symbols are stored under a "<board-rev-version>" folder in the cache.
-        symbol_dir = os.path.join(cache_dir, '-'.join([board, rev, version]))
-
-        # We need to return a path relative to the chroot. relpath will strip
-        # the leading slash so we need to add it back to ensure the path works
-        # inside the chroot.
-        symbol_dir_rel_path = os.sep + os.path.relpath(
-            symbol_dir, self._chroot_dir)
-
-        # Flag file used to indicate that these symbols have been downloaded and
-        # are ready for use.
-        symbols_complete_file = os.path.join(symbol_dir, self._COMPLETE_FILE)
-
-        # See if the symbols already exist and have been extracted.
-        if os.path.exists(symbols_complete_file):
-            # Symbols exist, so return a path relative to the chroot.
-            return symbol_dir_rel_path
-
-        # No .completed file available. Maybe another instance is processing
-        # the symbols already. Try to make the directory, ensuring either we
-        # acquire the "lock" for these symbols or know another instance is
-        # handling them in one atomic call.
-        try:
-            os.mkdir(symbol_dir)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-
-            # Another instance is probably processing these symbols, so wait
-            # until timeout for them to complete. Raises a TimeoutError if time
-            # elapses and the .completed file is not available.
-            utils.poll_for_condition(
-                lambda: os.path.exists(symbols_complete_file),
-                timeout=self._SYMBOL_WAIT_TIMEOUT, sleep_interval=5,
-                desc='Symbols for %s' % '-'.join([board, rev, version]))
-
-            # The other instance finished processing these symbols, so return a
-            # path relative to the chroot.
-            return symbol_dir_rel_path
-
-        # We've now acquired the 'lock' for these symbols. Now we need to figure
-        # out the remote url and start downloading.
-        try:
-            symbol_file = 'debug-%s.tgz' % board
-
-            # Build Google Storage URL from the test config.
-            board_key = '-'.join([board, rev])
-            remote_symbol_url = '/'.join([
-                self._test_config['boards'][board_key]['archive_server'],
-                self._test_config['boards'][board_key]['archive_path'].rsplit(
-                    '/', 1)[0] % {'board': board, 'build_version': version},
-                symbol_file])
-
-            if not remote_symbol_url.lower().startswith('gs://'):
-                raise ValueError(
-                    'Invalid symbols URL encountered. Only Google Storage URLs,'
-                    ' gs://, are supported.')
-
-            # Use gsutil to copy the file into the cache dir.
-            gsutil_cmd = os_dep.command('gsutil')
-            utils.run('%s cp %s %s' % (
-                gsutil_cmd, remote_symbol_url, symbol_dir))
-
-            # Unpack symbols.
-            utils.run('tar zxf %s -C %s %s' % (
-                os.path.join(symbol_dir, symbol_file), symbol_dir,
-                self._SYMBOL_DIR))
-
-            # No need to keep the tarball around...
-            os.remove(os.path.join(symbol_dir, symbol_file))
-
-            # Let any other instances know we've completed processing.
-            open(symbols_complete_file, 'w').close()
-        except:
-            # Something bad happened, remove the symbols directory so another
-            # instance can retry.
-            shutil.rmtree(symbol_dir)
-            raise
-
-        # Return a path relative to the chroot.
-        return symbol_dir_rel_path
-
-
-    def _trim_cache(self):
-        """Removes all symbol directories from the cache older than x days.
-
-        Cache age is controlled via _MAX_CACHE_AGE_DAYS constant.
-        """
-        cache_dir = self._get_cache_dir()
-
-        max_age = time.time() - 60 * 60 * 24 * self._MAX_CACHE_AGE_DAYS
-        for symbols in os.listdir(cache_dir):
-            symbol_path = os.path.join(cache_dir, symbols)
-            if (os.path.isdir(symbol_path)
-                    and os.stat(symbol_path).st_mtime < max_age):
-                # Multiple callers may be trying to clear the cache at once, so
-                # ignore any errors from this call.
-                shutil.rmtree(symbol_path, ignore_errors=True)
-
-
-    def _setup_results_in_chroot(self):
-        """Returns a path to the results dir relative to the chroot.
-
-        Uses mount --bind to setup the results directory inside chroot.
-
-        Returns:
-            A path to the results dir relative to the chroot.
-        """
-        # Create temporary directory inside chroot.
-        chroot_results_dir = tempfile.mkdtemp(dir=os.path.join(
-            self._chroot_dir, 'tmp'))
-
-        utils.run('mkdir -p %s; sudo mount --bind %s %s' % (
-            chroot_results_dir, self._results_dir, chroot_results_dir))
-
-        # Return a path relative to the chroot.
-        return os.sep + os.path.relpath(chroot_results_dir, self._chroot_dir)
-
-
-    def _cleanup_results_in_chroot(self, chroot_results_dir):
-        """Uses umount to remove mount --bind from inside chroot.
-
-        Args:
-            chroot_results_dir: Path to results dir relative to the chroot. Uses
-                the path returned by _setup_results_in_chroot().
-        """
-        # Create directory inside chroot based on results folder name.
-        full_chroot_results_dir = os.path.join(
-            self._chroot_dir, chroot_results_dir.lstrip(os.sep))
-
-        # unmount results directory from chroot.
-        utils.run(
-            'sudo umount %s' % full_chroot_results_dir, ignore_status=True)
-
-        # cleanup mount point. Use os.rmdir instead of shutil.rmtree in case the
-        # unmount failed. Should only be an empty directory left.
-        os.rmdir(full_chroot_results_dir)
-
-
-    def _generate_stack_traces(self, chroot_symbols_dir, chroot_results_dir):
-        """Enters the chroot, finds core dumps, and generate stack traces.
-
-        Args:
-            chroot_symbols_dir: Path to symbols dir relative to the chroot.
-            chroot_results_dir: Path to results dir relative to the chroot.
-
-        Raises:
-            ValueError: If the cros_sdk command is not available.
-        """
-        # Change to CrOS directory, enter chroot, then find and symbolize dumps.
-        cros_sdk_cmd = os_dep.command('cros_sdk')
-        utils.run(
-            "cd %s; %s -- find %s -name *.dmp -exec "
-            "sh -c 'minidump_stackwalk {} %s > {}.txt 2>/dev/null' \;" % (
-                self._cros_src_dir, cros_sdk_cmd, chroot_results_dir,
-                os.path.join(chroot_symbols_dir, self._SYMBOL_DIR)))
-
-
-    def generate(self):
-        """Main method. Retrieves symbols, manages cache, generate stack traces.
-
-        Given the results directory and Chrome OS source directory provided by
-        __init__, generates stack traces for all core dumps in the results dir.
-
-        Stack traces are stored in the same directory as the core dump under the
-        name <core dump file>.dmp.txt.
-        """
-        job_name = self._get_job_name()
-        if not job_name:
-            tko_utils.dprint(
-                'Unable to retrieve label keyval for this job. Stack traces can'
-                ' not be generated.')
-            return
-
-        # Retrieves symbols from local cache or Google Storage. Path returned is
-        # relative to the chroot.
-        chroot_symbols_dir = self._get_symbol_dir(job_name)
-
-        # Keep the cache healthy.
-        self._trim_cache()
-
-        # Setup links for results dir inside of chroot.
-        chroot_results_dir = self._setup_results_in_chroot()
-
-        # Generate stack traces.
-        try:
-            self._generate_stack_traces(chroot_symbols_dir, chroot_results_dir)
-        finally:
-            # Cleanup results directory inside of chroot.
-            self._cleanup_results_in_chroot(chroot_results_dir)
 
 
 def parse_reason(path):
@@ -412,31 +186,31 @@ def main():
     # Collect results using the standard Chrome OS test report generator. Doing
     # so allows us to use the same crash white list and reporting standards the
     # VM based test instances use.
-    results = ResultCollector().CollectResults(results_dir)
-
+    # TODO(scottz): Reevaluate this code usage. crosbug.com/35282
+    results = ResultCollector().RecursivelyCollectResults(results_dir)
     # We don't care about successful tests. We only want failed or crashing.
-    # Note: .items() generates a copy of the dictionary, so it's safe to delete.
-    crashes = False
-    for k, v in results.items():
-        if v['crashes']:
-            crashes = True
-        elif v['status'] == 'PASS':
-            del results[k]
+    # Note: list([]) generates a copy of the dictionary, so it's safe to delete.
+    for test_status in list(results):
+        if test_status['crashes']:
+            continue
+        elif test_status['status'] == 'PASS':
+            results.remove(test_status)
 
     # Filter results and collect logs. If we can't find a log for the test, skip
     # it. The Emailer will fill in the blanks using Database data later.
     filtered_results = {}
-    for test in results:
+    for test_dict in results:
         result_log = ''
-        test_name = os.path.basename(test)
-        error = os.path.join(test, 'debug', '%s.ERROR' % test_name)
+        test_name = os.path.basename(test_dict['testdir'])
+        error = os.path.join(
+                test_dict['testdir'], 'debug', '%s.ERROR' % test_name)
 
         # If the error log doesn't exist, we don't care about this test.
         if not os.path.isfile(error):
             continue
 
         # Parse failure reason for this test.
-        for t, r in parse_reason(test).iteritems():
+        for t, r in parse_reason(test_dict['testdir']).iteritems():
             # Server tests may have subtests which will each have their own
             # reason, so display the test name for the subtest in that case.
             if t != test_name:
@@ -463,7 +237,7 @@ def main():
                 '[...displaying only the last %d error log lines...]\n%s' % (
                     _ERROR_LOG_LIMIT, short_log))
 
-        filtered_results[test_name] = results[test]
+        filtered_results[test_name] = test_dict
         filtered_results[test_name]['log'] = '%s\n\n%s' % (
             short_result_log, short_log)
 
@@ -471,10 +245,6 @@ def main():
     json_file = open(os.path.join(results_dir, _JSON_REPORT_FILE), 'w')
     json.dump(filtered_results, json_file)
     json_file.close()
-
-    # If crashes occurred we need to generate stack traces for the cores.
-    if crashes:
-        StackTrace(results_dir, cros_src_dir).generate()
 
 
 if __name__ == '__main__':
