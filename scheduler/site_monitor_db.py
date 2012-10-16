bf2e21f65ba1e665554f7cb917bb06fdef238ad2
Simran Basi <sbasi@chromium.org>
2012-10-16 11:36:52 -0700

    Autotest: Hosts or Host Queue Entry in incorrect states fix.
    Currently Host Queue Entries will only start if they are in a correct
    state and their hosts are also in a correct state. Discrepancies can
    occur if the scheduler dies poorly or if changes to the database don't
    'stick'. This can cause a scheduler crashing loop and will impose
    downtime in the lab.
    
    In order to prevent this, I have updated the codepath to log and email
    an error message when this occurs and just abort the job attached to the
    Host/Host Queue Entry. This will prevent the scheduler from going down,
    and will allow us to keep processing other jobs while taking a hit of
    losing one job's results.
    
    BUG=chromium-os:35366
    TEST=On my local machine by recreating the db inconsistency and witnessing
         monitordb fail to launch, applying the fix and seeing the job complete.
    
    Change-Id: I24c5eaca5a25fa869ee69570044929b9c98332c6
    Reviewed-on: https://gerrit.chromium.org/gerrit/35743
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Commit-Ready: Simran Basi <sbasi@chromium.org>
    Reviewed-by: Simran Basi <sbasi@chromium.org>
    Tested-by: Simran Basi <sbasi@chromium.org>

--- scheduler/site_monitor_db.py
+++ scheduler/site_monitor_db.py
@@ -7,6 +7,7 @@ import logging
 
 from autotest_lib.client.common_lib import global_config
 from autotest_lib.frontend.afe import models
+from autotest_lib.scheduler import email_manager
 from autotest_lib.scheduler import scheduler_config, scheduler_models
 
 # Override default parser with our site parser.
@@ -39,6 +40,43 @@ class SiteAgentTask(object):
 
         for queue_entry in self.queue_entries:
             queue_entry.set_status(status)
+
+
+    def _check_queue_entry_statuses(self, queue_entries, allowed_hqe_statuses,
+                                    allowed_host_statuses=None):
+        """
+        Forked from monitor_db.py
+        """
+        class_name = self.__class__.__name__
+        for entry in queue_entries:
+            if entry.status not in allowed_hqe_statuses:
+                # In the orignal code, here we raise an exception. In an
+                # effort to prevent downtime we will instead abort the job and
+                # send out an email notifying us this has occured.
+                error_message = ('%s attempting to start entry with invalid '
+                                 'status %s: %s. Aborting Job: %s.'
+                                 % (class_name, entry.status, entry,
+                                    entry.job))
+                logging.error(error_message)
+                email_manager.manager.enqueue_notify_email(
+                    'Job Aborted - Invalid Host Queue Entry Status',
+                    error_message)
+                entry.job.request_abort()
+            invalid_host_status = (
+                    allowed_host_statuses is not None
+                    and entry.host.status not in allowed_host_statuses)
+            if invalid_host_status:
+                # In the orignal code, here we raise an exception. In an
+                # effort to prevent downtime we will instead abort the job and
+                # send out an email notifying us this has occured.
+                error_message = ('%s attempting to start on queue entry with '
+                                 'invalid host status %s: %s. Aborting Job: %s'
+                                 % (class_name, entry.host.status, entry,
+                                    entry.job))
+                logging.error(error_message)
+                email_manager.manager.enqueue_notify_email(
+                    'Job Aborted - Invalid Host Status', error_message)
+                entry.job.request_abort()
 
 
 class SiteDispatcher(object):
