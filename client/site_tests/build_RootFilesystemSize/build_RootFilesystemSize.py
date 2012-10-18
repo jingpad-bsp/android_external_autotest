33befb110a9925ed0f5c62035421074842cb7903
Don Garrett <dgarrett@google.com>
2012-10-18 13:17:31 -0700

    Make build_RootFilesystemSize ensure we have at least 100M free.
    In addition to recording how much we use, error out if there isn't a
    reasonable buffer of free space.
    
    BUG=chromium-os:35004
    TEST=Trybot run
    
    Change-Id: I6858be604867eb8c96363a07dc304d3d9606ab86
    Reviewed-on: https://gerrit.chromium.org/gerrit/35967
    Tested-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Richard Barnette <jrbarnette@chromium.org>
    Commit-Ready: Don Garrett <dgarrett@chromium.org>

--- client/site_tests/build_RootFilesystemSize/build_RootFilesystemSize.py
+++ client/site_tests/build_RootFilesystemSize/build_RootFilesystemSize.py
@@ -14,8 +14,22 @@ class build_RootFilesystemSize(test.test):
 
         # Report the current (test) size
         (status, output) = commands.getstatusoutput(
-            'df -B1 / | tail -1 | awk \'{ print $3 }\'')
+            'df -B1 / | tail -1')
         if status != 0:
             raise error.TestFail('Could not get size of rootfs')
 
-        self.write_perf_keyval({'bytes_rootfs_test': float(output)})
+        # Expected output format:
+        # rootfs    1056858112 768479232 288378880 73% /
+        output_columns = output.split()
+        used = output_columns[2]
+        free = output_columns[3]
+
+        self.write_perf_keyval({'bytes_rootfs_test': float(used)})
+
+        # Make sure we have at least 100M free on rootfs as warning if
+        # we are running out.
+        required_free_space = 100 * 1024 * 1024
+
+        if int(free) < required_free_space:
+          raise error.TestFail('%s bytes free is less than the %s required.' %
+                               (free, required_free_space))
