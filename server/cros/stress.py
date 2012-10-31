922fcb9c6e4d1c3cacd8d36c99805ad27a271f3a
Craig Harrison <craigdh@chromium.org>
2012-10-31 09:06:22 -0700

    Add support for passing exception up from the child thread.
    BUG=None
    TEST=stress_unittest.py
    
    Change-Id: I1e46e91bc2c26902d7d325d16b97393170122232
    Reviewed-on: https://gerrit.chromium.org/gerrit/37087
    Tested-by: Craig Harrison <craigdh@chromium.org>
    Reviewed-by: Dennis Jeffrey <dennisjeffrey@chromium.org>
    Reviewed-by: Prashanth Balasubramanian <beeps@chromium.org>
    Commit-Ready: Craig Harrison <craigdh@chromium.org>

--- server/cros/stress.py
+++ server/cros/stress.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
+import sys
 import threading
 import time
 
@@ -12,15 +14,19 @@ class BaseStressor(threading.Thread):
 
     @var stressor: callable which performs a single stress event.
     """
-    def __init__(self, stressor):
+    def __init__(self, stressor, escalate_exceptions=True):
         """
         Initialize the ControlledStressor.
 
         @param stressor: callable which performs a single stress event.
+        @param escalate_exceptions: whether to escalate exceptions to the parent
+            thread; defaults to True.
         """
         super(BaseStressor, self).__init__()
         self.daemon = True
         self.stressor = stressor
+        self._escalate_exceptions = escalate_exceptions
+        self._exc_info = None
 
 
     def start(self, start_condition=None):
@@ -30,7 +36,7 @@ class BaseStressor(threading.Thread):
         Optionally takes a wait condition before the stressor loop. Returns
         immediately.
 
-        @param start_condition: the new thread will wait to until this optional
+        @param start_condition: the new thread will wait until this optional
             callable returns True before running the stressor.
         """
         self._start_condition = start_condition
@@ -47,7 +53,12 @@ class BaseStressor(threading.Thread):
         if self._start_condition:
             while not self._start_condition():
                 time.sleep(1)
-        self._loop_stressor()
+        try:
+            self._loop_stressor()
+        except Exception as e:
+            if self._escalate_exceptions:
+                self._exc_info = sys.exc_info()
+            raise
 
 
     def _loop_stressor(self):
@@ -59,25 +70,42 @@ class BaseStressor(threading.Thread):
         raise NotImplementedError
 
 
+    def reraise(self):
+        """
+        Reraise an exception raised in the thread's stress loop.
+
+        This is a No-op if no exception was raised.
+        """
+        if self._exc_info:
+            exc_info = self._exc_info
+            self._exc_info = None
+            raise exc_info[0], exc_info[1], exc_info[2]
+
+
 class ControlledStressor(BaseStressor):
     """
     Run a stressor in loop on a separate thread.
 
     Creates a new thread and calls |stressor| in a loop until stop() is called.
     """
-    def __init__(self, stressor):
+    def __init__(self, stressor, escalate_exceptions=True):
         """
         Initialize the ControlledStressor.
 
         @param stressor: callable which performs a single stress event.
+        @param escalate_exceptions: whether to escalate exceptions to the parent
+            thread; defaults to True.
         """
         self._complete = threading.Event()
-        super(ControlledStressor, self).__init__(stressor)
+        super(ControlledStressor, self).__init__(stressor, escalate_exceptions)
 
 
     def _loop_stressor(self):
         """Overloaded from parent."""
+        iteration_num = 0
         while not self._complete.is_set():
+            iteration_num += 1
+            logging.info('Stressor iteration: %d' % iteration_num)
             self.stressor()
 
 
@@ -102,32 +130,35 @@ class ControlledStressor(BaseStressor):
         """
         self._complete.set()
         self.join(timeout)
+        self.reraise()
 
 
 class CountedStressor(BaseStressor):
     """
     Run a stressor in a loop on a separate thread a given number of times.
 
-    Creates a new thread and calls |stressor| in a loop |count| times. The
+    Creates a new thread and calls |stressor| in a loop |iterations| times. The
     calling thread can use wait() to block until the loop completes.
     """
     def _loop_stressor(self):
         """Overloaded from parent."""
-        for _ in xrange(self._count):
+        for iteration_num in xrange(1, self._iterations + 1):
+            logging.info('Stressor iteration: %d of %d' % (iteration_num,
+                                                           self._iterations))
             self.stressor()
 
 
-    def start(self, count, start_condition=None):
+    def start(self, iterations, start_condition=None):
         """
         Apply the stressor a given number of times.
 
         Overloaded from parent.
 
-        @param count: number of times to apply the stressor.
+        @param iterations: number of times to apply the stressor.
         @param start_condition: the new thread will wait to until this optional
             callable returns True before running the stressor.
         """
-        self._count = count
+        self._iterations = iterations
         super(CountedStressor, self).start(start_condition)
 
 
@@ -138,3 +169,4 @@ class CountedStressor(BaseStressor):
             never times out.
         """
         self.join(timeout)
+        self.reraise()
