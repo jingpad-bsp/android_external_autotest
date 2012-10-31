2e556a18f086d090419d1c2366ec53a2868f0075
Vic Yang <victoryang@chromium.org>
2012-10-31 10:39:32 +0800

    faft: Static declaration of faft_client_attribute members
    Dynamic declaration by __dict__ is not recognized by pylint. Let's move
    to static declaration for easier maintenance.
    
    BUG=chromium-os:35902
    TEST=Run FAFT suite and passed.
    
    Change-Id: Ic040f55e1b201fe32a29933b3bbcfa004e34a944
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/36994

--- server/cros/faft_client_attribute.py
+++ server/cros/faft_client_attribute.py
@@ -6,66 +6,57 @@ class FAFTClientAttribute(object):
     """Class that tests platform name and gives client machine attributes."""
     version = 1
 
-    DEFAULT_SETTING = {'broken_warm_reset': False,
-                       'chrome_ec': False,
-                       'has_lid': True,
-                       'has_keyboard': True,
-                       'keyboard_dev': True,
-                       'ec_capability': list(),
-                       'wp_voltage': 'pp1800',
-                       'key_matrix_layout': 0}
+    # Default settings
+    broken_warm_reset = False
+    chrome_ec = False
+    has_lid = True
+    has_keyboard = True
+    keyboard_dev = True
+    ec_capability = list()
+    wp_voltage = 'pp1800'
+    key_matrix_layout = 0
 
     def __init__(self, platform):
-        """Initialized.
+        """Initialized. Set up platform-dependent attributes.
 
         Args:
           platform: Platform name returned by FAFT client.
         """
-        self.__dict__.update(self.DEFAULT_SETTING)
-        self.__dict__.update(self._get_platform_setting(platform))
-
-
-    def _get_platform_setting(self, platform):
-        """Return platform-specific settings."""
-        setting = dict()
-
-        setting['platform'] = platform
+        self.platform = platform
 
         # Set 'broken_warm_reset'
         if platform in ['Parrot', 'Butterfly']:
-            setting['broken_warm_reset'] = True
+            self.broken_warm_reset = True
 
         # Set 'chrome_ec'
         if platform in ['Link', 'Snow']:
-            setting['chrome_ec'] = True
+            self.chrome_ec = True
 
         # Set 'has_lid'
         if platform in ['Stumpy', 'Kiev']:
-            setting['has_lid'] = False
+            self.has_lid = False
 
         # Set 'has_keyboard'
         if platform in ['Stumpy', 'Kiev']:
-            setting['has_keyboard'] = False
+            self.has_keyboard = False
 
         # Set 'keyboard_dev'
         if platform in ['Aebl', 'Alex', 'Kaen', 'Kiev', 'Lumpy', 'Mario',
                         'Seaboard', 'Stumpy', 'ZGB']:
-            setting['keyboard_dev'] = False
+            self.keyboard_dev = False
 
         # Set 'ec_capability'
         if platform == 'Link':
-            setting['ec_capability'] = ['adc_ectemp', 'battery', 'charging',
+            self.ec_capability = ['adc_ectemp', 'battery', 'charging',
                                         'keyboard', 'lid', 'x86', 'thermal',
                                         'usb', 'peci']
         elif platform == 'Snow':
-            setting['ec_capability'] = ['battery', 'keyboard', 'arm']
+            self.ec_capability = ['battery', 'keyboard', 'arm']
 
         # Set 'wp_voltage'
         if platform == 'Link':
-            setting['wp_voltage'] = 'pp3300'
+            self.wp_voltage = 'pp3300'
 
         # Set 'key_matrix_layout'
         if platform == 'Parrot':
-            setting['key_matrix_layout'] = 1
-
-        return setting
+            self.key_matrix_layout = 1
