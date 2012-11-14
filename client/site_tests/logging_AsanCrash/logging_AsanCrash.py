ede13da2769ac3d6b197dc13ccbb8c24b89e7f2c
Denis Glotov <glotov@chromium.org>
2012-11-14 22:20:47 +0400

    Redirect ASAN output to the /var/log/chrome/asan_log.
    This is because Chrome stderr is /dev/null now (crbug.com/156308).
    
    BUG=chromium-os:26249
    TEST=manual
    DEPEND_ON:https://gerrit.chromium.org/gerrit/#/c/38024/
    
    Change-Id: I3f24270e884bea24faf216a4c60bc100d4dad594
    Reviewed-on: https://gerrit.chromium.org/gerrit/38045
    Reviewed-by: Ken Mixter <kmixter@chromium.org>
    Tested-by: Denis Glotov <glotov@chromium.org>
    Commit-Ready: Denis Glotov <glotov@chromium.org>

--- client/site_tests/logging_AsanCrash/logging_AsanCrash.py
+++ client/site_tests/logging_AsanCrash/logging_AsanCrash.py
@@ -5,7 +5,7 @@
 import logging
 from autotest_lib.client.bin import utils
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros import constants, cros_logging, cros_ui_test
+from autotest_lib.client.cros import cros_logging, cros_ui_test
 
 class logging_AsanCrash(cros_ui_test.UITest):
     version = 1
@@ -17,11 +17,16 @@ class logging_AsanCrash(cros_ui_test.UITest):
         if not 'asan' in utils.read_file('/etc/session_manager_use_flags.txt'):
             raise error.TestFail('Current image not built with ASAN')
 
-        ui_log = cros_logging.LogReader(constants.UI_LOG)
-        ui_log.set_start_by_current()
+        pid = self.pyauto.GetBrowserInfo()['browser_pid']
+        asan_log_name = '/var/log/chrome/asan_log.%d' % pid
+        logging.info('Browser PID under pyauto control is %d. '
+                     'So ASAN log is expected at %s.' % (pid, asan_log_name))
 
         logging.info('Initiate simulating memory bug to be caught by ASAN...')
         self.pyauto.SimulateAsanMemoryBug()
+
+        ui_log = cros_logging.LogReader(asan_log_name)
+        ui_log.read_all_logs()
 
         # We must wait some time until memory bug is simulated (happens
         # immediately after the return on the call) and caught by ASAN.
@@ -30,7 +35,7 @@ class logging_AsanCrash(cros_ui_test.UITest):
                 lambda: ui_log.can_find('ERROR: AddressSanitizer'),
                 timeout=10,
                 exception=error.TestFail(
-                    'Found no ui log message about Address Sanitizer catch'))
+                    'Found no asan log message about Address Sanitizer catch'))
 
             utils.poll_for_condition(
                 lambda: ui_log.can_find("'testarray'"),
@@ -39,6 +44,5 @@ class logging_AsanCrash(cros_ui_test.UITest):
                     'ASAN caught bug but did not mentioned the cause in log'))
 
         except:
-            logging.debug('UI log: ' + ui_log.get_logs())
+            logging.debug('ASAN log content: ' + ui_log.get_logs())
             raise
-
