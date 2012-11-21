2707961254706817025b0121c992e9d73fb7bfaa
Michael Spang <spang@chromium.org>
2012-11-20 19:34:18 -0500

    Add test for suspend/resume with "no_console_suspend" option set
    Developers may have the "no_console_suspend" option set in order to
    see console messages while the system is suspending. This adds a test
    for this configuration.
    
    The following scenarios will cause this test to fail:
    
     - A device driver fails to suspend. This is reported as "suspend
       failed", with errno containing the value reported by the driver.
    
     - The system takes too long to suspend (more than 10 seconds). This
       will cause the wakealarm to fire before the system has actually
       suspended. This is reported as "suspend took too long".
    
     - The system wakes from suspend before 10 seconds of real time has
       elapsed. This is reported as "work from suspend early"
    
     - Any wake event fires while the test is running, such as input from
       the keyboard, touchpad, touchscreen, or lid switch. This is reported
       as either "spurious wake events" or "suspend failed" (EBUSY).
    
    BUG=chromium-os:36016
    TEST=run_remote_tests.sh -b power_NoConsoleSuspend
      - Normal run passes.
      - Touchpad input during test causes failure
        (woke from suspend early).
      - Simulated suspend error causes failure
        ("mount --bind /dev/full /sys/power/state").
      - Simulated suspend delay causes failure
        ("import time; time.sleep(10)" before suspend_to_ram()).
    
    Change-Id: If7f7af591f79c1c160824cb64ab2bd2bb956be3c
    Signed-off-by: Michael Spang <spang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/38436
    Reviewed-by: Mark Koudritsky <kamrik@chromium.org>
    Reviewed-by: Jon Kliegman <kliegs@chromium.org>

---
+++ client/site_tests/power_NoConsoleSuspend/power_NoConsoleSuspend.py
@@ -0,0 +1,80 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging, errno, shutil, os
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.cros import rtc, sys_power
+from autotest_lib.client.common_lib import error
+
+SLEEP_TIME = 10
+
+SYSFS_CONSOLE_SUSPEND = '/sys/module/printk/parameters/console_suspend'
+SYSFS_WAKEUP_COUNT = '/sys/power/wakeup_count'
+
+class power_NoConsoleSuspend(test.test):
+    """Test suspend/resume with no_console_suspend option set."""
+
+    version = 1
+
+    def initialize(self):
+        # Save & disable console_suspend module param
+        self.old_console_suspend = utils.read_file(SYSFS_CONSOLE_SUSPEND)
+        utils.write_one_line(SYSFS_CONSOLE_SUSPEND, 'N')
+
+    def run_once(self):
+        # Save wakeup count. If the wakealarm (set below) fires before
+        # suspend completes, this forces the system to wake back up rather
+        # than sleep forever ("suspend took too long" case below).
+        try:
+            wakeup_count = utils.read_file(SYSFS_WAKEUP_COUNT)
+            utils.open_write_close(SYSFS_WAKEUP_COUNT, wakeup_count)
+        except IOError as e:
+            logging.exception('failed to write %s', SYSFS_WAKEUP_COUNT)
+            if e.errno != errno.EINVAL:
+                raise
+            # Some wakeup source incremented wakeup_count. This could happen
+            # due to unexpected input from the keyboard/touchpad/lid switch.
+            raise error.TestError('spurious wake events')
+
+        # Set wake alarm
+        start_time = rtc.get_seconds()
+        alarm_time = start_time + SLEEP_TIME
+        rtc.set_wake_alarm(alarm_time)
+
+        # Kick off the suspend
+        try:
+            utils.suspend_to_ram()
+        except IOError as e:
+            logging.exception('suspend failed')
+            if e.errno == errno.EBUSY and rtc.get_seconds() >= alarm_time:
+                # The wakealarm probably fired on the way to suspend.
+                raise error.TestError('suspend took too long')
+            else:
+                # Some driver probably failed to suspend properly.
+                raise error.TestError('suspend failed')
+
+        # Save performance stats
+        time_in_suspend = rtc.get_seconds() - start_time
+        results = {
+            'time_in_suspend': time_in_suspend,
+        }
+        self.write_perf_keyval(results)
+
+        # Sanity check: slept for enough time?
+        if time_in_suspend < SLEEP_TIME:
+            raise error.TestError('woke from suspend early')
+
+    def save_log_file(self, path):
+        logging.info('saving log file %s', path)
+        shutil.copyfile(path,
+            os.path.join(self.outputdir, os.path.basename(path)))
+
+    def cleanup(self):
+        # Restore old console_suspend module param
+        logging.info('restoring value for console_suspend: %s',
+                     self.old_console_suspend)
+        utils.open_write_close(SYSFS_CONSOLE_SUSPEND, self.old_console_suspend)
+
+        # Save diagnostic logs
+        self.save_log_file('/sys/kernel/debug/suspend_stats')
