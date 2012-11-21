dcff425403bff79cbd6a8733839e89e8d6dea711
Simran Basi <sbasi@chromium.org>
2012-11-20 16:13:20 -0800

    Autotest: Refactor RPM Client code into its own module
    Instead of having everything in autotest need a host to interact with
    an RPM, this code is now refactored into its own module.
    
    BUG=None
    TEST=local system
    
    Change-Id: If9b173009eeb9d791e41947a103b59cc78fc3806
    CQ-DEPENDS: I0a21086a6d33adce456bcb512e9a3823e7584058
    Reviewed-on: https://gerrit.chromium.org/gerrit/38434
    Commit-Ready: Simran Basi <sbasi@chromium.org>
    Reviewed-by: Simran Basi <sbasi@chromium.org>
    Tested-by: Simran Basi <sbasi@chromium.org>

--- server/hosts/site_host.py
+++ server/hosts/site_host.py
@@ -18,15 +18,7 @@ from autotest_lib.server import autotest
 from autotest_lib.server import site_host_attributes
 from autotest_lib.server.cros import servo
 from autotest_lib.server.hosts import remote
-
-
-RPM_FRONTEND_URI = global_config.global_config.get_config_value('CROS',
-        'rpm_frontend_uri', type=str, default='')
-
-
-class RemotePowerException(Exception):
-    """This is raised when we fail to set the state of the device's outlet."""
-    pass
+from autotest_lib.site_utils.rpm_control_system import rpm_client
 
 
 def make_ssh_command(user='root', port=22, opts='', hosts_file=None,
@@ -553,26 +545,16 @@ class SiteHost(remote.RemoteHost):
         return SiteHost.check_for_rpm_support(self.hostname)
 
 
-    def _set_power(self, new_state):
-        client = xmlrpclib.ServerProxy(RPM_FRONTEND_URI, verbose=False)
-        if not client.queue_request(self.hostname, new_state):
-            error_msg = ('Failed to change outlet status for host: %s to '
-                         'state: %s.' % (self.hostname, new_state))
-            logging.error(error_msg)
-            raise RemotePowerException(error_msg)
-
-
-
     def power_off(self):
-        self._set_power('OFF')
+        rpm_client.set_power(self.hostname, 'OFF')
 
 
     def power_on(self):
-        self._set_power('ON')
+        rpm_client.set_power(self.hostname, 'ON')
 
 
     def power_cycle(self):
-        self._set_power('CYCLE')
+        rpm_client.set_power(self.hostname, 'CYCLE')
 
 
     def get_platform(self):
---
+++ site_utils/rpm_control_system/rpm_client.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import xmlrpclib
+
+import common
+
+from autotest_lib.client.common_lib import global_config
+
+
+RPM_FRONTEND_URI = global_config.global_config.get_config_value('CROS',
+        'rpm_frontend_uri', type=str, default='')
+
+
+class RemotePowerException(Exception):
+    """This is raised when we fail to set the state of the device's outlet."""
+    pass
+
+
+def set_power(hostname, new_state):
+    client = xmlrpclib.ServerProxy(RPM_FRONTEND_URI, verbose=False)
+    if not client.queue_request(hostname, new_state):
+        error_msg = ('Failed to change outlet status for host: %s to '
+                     'state: %s.' % (self.hostname, new_state))
+        logging.error(error_msg)
+        raise RemotePowerException(error_msg)
