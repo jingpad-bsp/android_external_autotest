5b8d925de2875a868e330927645330e40da28713
Simon Que <sque@chromium.org>
2012-12-10 10:55:28 -0800

    client: hardware_Xrandr option to run 500 times.
    Also print min and mean xrandr times.
    
    BUG=chromium-os:35967
    TEST=Run hardware_Xrandr test successfully.
    
    Change-Id: I2f2dbfca0a593f33709c32951c1cb56a9049a800
    Signed-off-by: Simon Que <sque@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/39495
    Reviewed-by: Todd Broch <tbroch@chromium.org>

--- client/site_tests/hardware_Xrandr/hardware_Xrandr.py
+++ client/site_tests/hardware_Xrandr/hardware_Xrandr.py
@@ -10,7 +10,7 @@ from autotest_lib.client.common_lib import base_utils, error
 class hardware_Xrandr(test.test):
     version = 1
 
-    def run_once(self):
+    def run_once(self, num_iterations=1):
         # Start ui if not started.  Restart if already started.  This is needed
         # because:
         #   1. X is running only when ui is running.
@@ -30,35 +30,58 @@ class hardware_Xrandr(test.test):
         # Read the Xrandr outputs.
         xrandr_state = base_utils.get_xrandr_output_state()
 
-        # Toggle each output twice and verify the output has turned on/off.
+        # Also create lists of on/off times for each output.  Initialize them to
+        # zeros to avoid the overhead of appending values.
+        on_times = {}
+        off_times = {}
         for output in xrandr_state:
-            is_enabled = xrandr_state[output]
+            # Attempt to identify the internal display.  Note that there may not
+            # be such a display, e.g. on a desktop system.
+            if self._is_internal(output):
+                keyvals['internal_output_name'] = output
+            on_times[output] = [0] * num_iterations
+            off_times[output] = [0] * num_iterations
 
-            for output_state in [not is_enabled, is_enabled]:
-                start_time = time.time();
-                base_utils.set_xrandr_output(output, output_state)
-                end_time = time.time();
+        # Toggle each output twice and verify the output has turned on/off.
+        for index in xrange(num_iterations):
+            for output in xrandr_state:
+                is_enabled = xrandr_state[output]
 
-                output_state_string = 'on' if output_state else 'off'
-                xrandr_time_s = end_time - start_time
+                for output_state in [not is_enabled, is_enabled]:
+                    start_time = time.time()
+                    base_utils.set_xrandr_output(output, output_state)
+                    end_time = time.time()
 
-                if self._is_internal(output):
-                    keyvals['internal_output_name'] = output
-                    keyvals['s_internal_%s_time' % (output_state_string)] = \
-                            xrandr_time_s
-                else:
-                    keyvals['s_%s_%s_time' % (output, output_state_string)] = \
-                            xrandr_time_s
+                    new_xrandr_state = base_utils.get_xrandr_output_state()
+                    if new_xrandr_state[output] != output_state:
+                        logging.error('Failed to turn %s %s.' %
+                                      (output, 'on' if output_state else 'off'))
+                        num_errors += 1
+                        break
 
-                new_xrandr_state = base_utils.get_xrandr_output_state()
-                if new_xrandr_state[output] != output_state:
-                    logging.error('Failed to turn %s %s.' %
-                                  (output, output_state_string))
-                    num_errors += 1
-                    break
+                    time_list = \
+                            (on_times if output_state else off_times)[output]
+                    time_list[index] = end_time - start_time
 
-                if output_state == is_enabled:
-                    logging.info('Successfully cycled output %s' % output)
+                    if output_state == is_enabled:
+                        logging.info('Successfully cycled output %s' % output)
+
+        stats = { 'mean'    : lambda list: sum(list) / num_iterations,
+                  'max'     : max,
+                  'min'     : min }
+        for output in xrandr_state:
+            output_name = output
+            if self._is_internal(output):
+                output_name = 'internal'
+
+            for func_name in stats:
+                keyvals['s_%s_on_%s_time' % (output_name, func_name)] = \
+                        stats[func_name](on_times[output])
+                keyvals['s_%s_off_%s_time' % (output_name, func_name)] = \
+                        stats[func_name](off_times[output])
+
+            logging.info(str(on_times[output]))
+            logging.info(str(off_times[output]))
 
         self.write_perf_keyval(keyvals)
 
