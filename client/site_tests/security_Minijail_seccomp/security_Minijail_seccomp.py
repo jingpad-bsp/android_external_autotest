71da3127391f8de95b454bd2a2b9115e1d764447
Jorge Lucangeli Obes <jorgelo@chromium.org>
2012-12-12 17:33:47 -0800

    security_Minijail_seccomp: add argument inspection tests.
    Now that Minijail supports policies that inspect arguments,
    add tests for that functionality.
    
    BUG=None
    TEST=security_Minijail_seccomp passes on lumpy, x86-alex, daisy
    
    Change-Id: Id226072f7c48ea06f6c2ade0f77c55e2e7068bdc
    Reviewed-on: https://gerrit.chromium.org/gerrit/39629
    Tested-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Jim Hebert <jimhebert@chromium.org>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Commit-Ready: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- client/site_tests/security_Minijail_seccomp/security_Minijail_seccomp.py
+++ client/site_tests/security_Minijail_seccomp/security_Minijail_seccomp.py
@@ -30,7 +30,7 @@ class security_Minijail_seccomp(test.test):
             return full_arch
 
 
-    def run_test(self, exe, jail, expected_ret, pretty_msg):
+    def run_test(self, exe, args, jail, expected_ret, pretty_msg):
         cmdline = '/sbin/minijail0'
 
         if jail.user:
@@ -41,6 +41,9 @@ class security_Minijail_seccomp(test.test):
 
         cmdline += ' -S %s/%s %s/%s' % (self.bindir, jail.policy,
                                         self.bindir, exe)
+
+        if len(args) > 0:
+            cmdline += ' %s' % ' '.join(args)
 
         logging.info("Command line: " + cmdline)
         ret = utils.system(cmdline, ignore_status=True)
@@ -53,22 +56,37 @@ class security_Minijail_seccomp(test.test):
     def run_once(self):
         privdrop_policy = "policy-privdrop_" + self.get_arch()
 
-        case_ok = ("ok",
+        case_ok = ("ok", [],
                    Jail(None, "policy", nnp=False),
                    0, "Allowed system calls failed")
-        case_block_privdrop = ("ok",
+        case_block_privdrop = ("ok", [],
                                Jail("chronos", "policy", nnp=False),
                                253, "Blocked priv-drop system calls succeeded")
-        case_allow_privdrop = ("ok",
+        case_allow_privdrop = ("ok", [],
                                Jail("chronos", privdrop_policy, nnp=False),
                                0, "Allowed system calls failed")
-        case_no_new_privs = ("ok",
+        case_no_new_privs = ("ok", [],
                              Jail("chronos", "policy", nnp=True),
                              0, "Allowed system calls failed")
-        case_fail = ("fail",
+        case_fail = ("fail", [],
                      Jail(None, "policy", nnp=False),
                      253, "Blocked system calls succeeded")
 
+        case_arg_equals_ok = ("open", ["0"],
+                              Jail(None, "policy-rdonly", nnp=False),
+                              0, "Allowing system calls via args == failed")
+        case_arg_equals_fail = ("open", ["1"],
+                                Jail(None, "policy-rdonly", nnp=False),
+                                253, "Blocking system calls via args == failed")
+        case_arg_flags_ok = ("open", ["1"],
+                             Jail(None, "policy-wronly", nnp=False),
+                             0, "Allowing system calls via args & failed")
+        case_arg_flags_ok = ("open", ["2"],
+                             Jail(None, "policy-wronly", nnp=False),
+                             253, "Blocking system calls via args & failed")
+
         for case in [case_ok, case_block_privdrop, case_allow_privdrop,
-                     case_no_new_privs, case_fail]:
+                     case_no_new_privs, case_fail,
+                     case_arg_equals_ok, case_arg_equals_fail,
+                     case_arg_flags_ok]:
             self.run_test(*case)
