f1bbb0a16a3f246f5e56199e5d5c687294e2f23b
Bryan Freed <bfreed@chromium.org>
2012-12-18 15:09:38 -0800

    Test low memory notification.
    Make this part of platform_CompressedSwap since they are so similar
    in functionality.
    Remove all of the old kernel_LowMemNotify test (.py and .c) except for control.
    
    BUG=chromium-os:34586
    TEST=run_remote_tests.sh with platform_CompressedSwap and kernel_LowMemNotify
         both with and without swap enabled (/home/chronos/.swap_enabled)
         on x86 and arm.
         Failures are appropriate for platform_CompressedSwap when swap is disabled.
    
    Change-Id: Ica4ea463544bc4ff8e6ccbbd454959231e2c00d7
    Reviewed-on: https://gerrit.chromium.org/gerrit/39980
    Tested-by: Bryan Freed <bfreed@chromium.org>
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    Commit-Queue: Bryan Freed <bfreed@chromium.org>
    Reviewed-by: Bryan Freed <bfreed@chromium.org>

--- client/site_tests/platform_CompressedSwap/platform_CompressedSwap.py
+++ client/site_tests/platform_CompressedSwap/platform_CompressedSwap.py
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, signal, subprocess, time
+import logging, os, select, signal, subprocess, time
 
 from autotest_lib.client.bin import utils, test
 from autotest_lib.client.common_lib import error
@@ -30,53 +30,104 @@ class platform_CompressedSwap(test.test):
                 return True
         return False
 
-    def run_once(self):
+    # Check for low memory notification by polling /dev/chromeos-low-mem.
+    def getting_low_mem_notification(self):
+        lowmem_fd = open('/dev/chromeos-low-mem', 'r')
+        lowmem_poller = select.poll()
+        lowmem_poller.register(lowmem_fd, select.POLLIN)
+        events=lowmem_poller.poll(0)
+        lowmem_fd.close()
+        for fd, flag in events:
+            if flag & select.POLLIN:
+                return True
+        return False
 
-        # Verify we set /sys/block/zram0/disksize to approximately 3/2 of
-        # MemTotal as per /etc/swap.conf.
+    def run_once(self, just_checking_lowmem=False):
+
         memtotal = utils.read_from_meminfo('MemTotal')
         swaptotal = utils.read_from_meminfo('SwapTotal')
-        swap_target = memtotal * 3 / 2
-        if swaptotal == 0:
-            raise error.TestFail('SwapTotal is 0, swap is configured off.')
-        if swaptotal < swap_target / 2 or swaptotal > swap_target * 3 / 2:
-            raise error.TestFail('SwapTotal %d is nowhere near our ' \
-                'target of %d.' % (swaptotal, swap_target))
+        if just_checking_lowmem:
+            # When just checking for low mem notification, swaptotal is
+            # allowed to be 0.  If it is, set swaptotal to memtotal.
+            if swaptotal == 0:
+                swaptotal = memtotal
+        else:
+            # Verify we set /sys/block/zram0/disksize to approximately 3/2 of
+            # MemTotal as per /etc/swap.conf.
+            swap_target = memtotal * 3 / 2
+            if swaptotal == 0:
+                raise error.TestFail('SwapTotal is 0, swap is configured off.')
+            if swaptotal < swap_target / 2 or swaptotal > swap_target * 3 / 2:
+                raise error.TestFail('SwapTotal %d is nowhere near our ' \
+                    'target of %d.' % (swaptotal, swap_target))
+
+        got_low_mem_notification = False
+        cleared_low_mem_notification = False
 
         # Loop over hog creation until MemFree+SwapFree approaches 0.
-        # Confirm we do not see any OOMs (procs killed due to Out Of Memory)
-        # until we are "reasonably close to 0" (say, 10% of SwapFree).
+        # Confirm we do not see any OOMs (procs killed due to Out Of Memory).
         hogs = []
-        cmd = [ self.srcdir + '/' + self.executable, '100' ]
+        cmd = [ self.srcdir + '/' + self.executable, '50' ]
         logging.debug('Memory hog command line is %s' % cmd)
-        while len(hogs) < 100:
+        while len(hogs) < 200:
             memfree = utils.read_from_meminfo('MemFree')
             swapfree = utils.read_from_meminfo('SwapFree')
             total_free = memfree + swapfree
             logging.debug('nhogs %d: memfree %d, swapfree %d' %
                           (len(hogs), memfree, swapfree))
-            if total_free < swaptotal * 0.1:
+            if total_free < swaptotal * 0.03:
                 break;
+
+            p = subprocess.Popen(cmd)
+            utils.write_one_line('/proc/%d/oom_score_adj' % p.pid, '1000')
+            hogs.append(p)
+
+            time.sleep(2)
 
             if self.check_for_oom(hogs):
                 utils.system("killall -TERM hog")
                 raise error.TestFail('Oom detected after %d hogs created' %
                                      len(hogs))
 
-            p = subprocess.Popen(cmd)
-            utils.write_one_line('/proc/%d/oom_score_adj' % p.pid, '1000')
-            hogs.append(p)
-            time.sleep(2)
+            # Check for low memory notification.
+            if self.getting_low_mem_notification():
+                if not got_low_mem_notification:
+                    first_notification = len(hogs)
+                got_low_mem_notification = True
+                logging.info('Got low memory notification after hog %d' %
+                             len(hogs))
 
-        logging.info('completed after %d hogs created' % len(hogs))
+        logging.info('Finished creating %d hogs, SwapFree %d, MemFree %d' %
+                     (len(hogs), swapfree, memfree))
 
-        # Clean up our hogs since they otherwise live forever.
-        # Sending signals via p.send_signal() is insufficient to
-        # kill the hogs because it only kills the 'nice' parents,
-        # not the actual 'hog' children.
-        swapfree1 = utils.read_from_meminfo('SwapFree')
+        # Before cleaning up all the hogs, verify that killing hogs back to
+        # our initial low memory notification causes notification to end.
+        if got_low_mem_notification:
+            hogs_killed = 0;
+            for p in hogs:
+                if not self.getting_low_mem_notification():
+                    cleared_low_mem_notification = True
+                    logging.info('Cleared low memory notification after %d '
+                                 'hogs were killed' % hogs_killed)
+                    break;
+                p.kill()
+                hogs_killed += 1
+                time.sleep(2)
+
+        # Clean up the rest of our hogs since they otherwise live forever.
         utils.system("killall -TERM hog")
         time.sleep(5)
         swapfree2 = utils.read_from_meminfo('SwapFree')
         logging.info('SwapFree was %d before cleanup, %d after.' %
-                     (swapfree1, swapfree2))
+                     (swapfree, swapfree2))
+
+        # Raise exceptions due to low memory notification failures.
+        if not got_low_mem_notification:
+            raise error.TestFail('We did not get low memory notification!')
+        elif not cleared_low_mem_notification:
+            raise error.TestFail('We did not clear low memory notification!')
+        elif len(hogs) - hogs_killed < first_notification - 3:
+            raise error.TestFail('We got low memory notification at hog %d, '
+                                 'but we did not clear it until we dropped to '
+                                 'hog %d' %
+                                 (first_notification, len(hogs) - hogs_killed))
