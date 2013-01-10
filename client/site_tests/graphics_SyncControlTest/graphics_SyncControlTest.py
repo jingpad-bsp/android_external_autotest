aa6dea876d0bf7f393e16505408bc3767a3ce7ef
Ryan Harrison <rharrison@chromium.org>
2013-01-10 11:35:05 -0500

    Add in graphics_SyncControlTest to autotests
    This test checks that sync control extension behave as expected. Currently this
    only implemented for EGL, with the GLX implementation being a place holder for
    future work. For EGL there is only one function tested,
    eglGetsyncValuesCHROMIUM, since the rest of the spec has not yet been defined
    and implemented.
    
    This CL add the implementation of the test as a dep in the glbench dep. There
    are other standalone binaries in this dep already, so this seems like the most
    resonable place for it to be.
    
    BUG=chrome-os-partner:16624
    TEST=Tested binary as a standalone on a EGL device, daisy:
          - `emerge-daisy autotest-deps-glbench`
          - Copied synccontroltest from /build/daisy/... to device
          - `export DISPLAY=:0`
          - `synccontroltest`
          - Confirmed no errors reported
         Confirmed that binary built for a GLX device, link:
          - `emerge-link autotest-deps-glbench`
         Built and ran the autotest, on daisy:
          - `emerge-daisy autotest-deps-glbench`
          - `emerge-daisy autotest-tests`
          - `run_remote_tests.sh --use_emerged --remote=$REMOTE
            graphics_SyncControlTest`
         Confirmed that autotest built for a GLX device, link:
          - `emerge-link autotest-deps-glbench`
          - `emerge-link autotest-tests`
    
    Change-Id: Ia0d737cbd939920cf1c2055e3158901f6094be38
    Reviewed-on: https://gerrit.chromium.org/gerrit/41290
    Commit-Queue: Ryan Harrison <rharrison@chromium.org>
    Reviewed-by: Ryan Harrison <rharrison@chromium.org>
    Tested-by: Ryan Harrison <rharrison@chromium.org>

---
+++ client/site_tests/graphics_SyncControlTest/graphics_SyncControlTest.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os, re, time
+
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.cros import power_utils
+
+class graphics_SyncControlTest(test.test):
+    """Confirms that infrastructure for aligning graphics operations in the
+    browser with vsync is present
+    """
+    version = 1
+
+    def setup(self):
+        self.job.setup_dep(['glbench'])
+
+    def run_once(self):
+        dep = 'glbench'
+        dep_dir = os.path.join(self.autodir, 'deps', dep)
+        self.job.install_pkg(dep, 'dep', dep_dir)
+
+        hz = {'daisy' : 59.9}
+
+        exefile = os.path.join(self.autodir, 'deps/glbench/synccontroltest')
+        cmd = "DISPLAY=:1 " + exefile
+        board = utils.get_current_board()
+        if board in hz:
+            cmd = cmd + " --vsync {0:.2f}".format(hz[board])
+
+        self._services = power_utils.ManageServices(['ui'])
+        self._services.stop_services()
+        x_summary = utils.system_output('X :1 & echo "XPID=$!"',
+                                            ignore_status=True)
+        self.x_pid = re.match(r"XPID=(\d+)", x_summary).group(0)
+        time.sleep(1)
+
+        # synccontroltest exits with a non zero status if a deviation above
+        # 200uS us is detected.
+        ret = utils.system(cmd, ignore_status=True)
+        if ret != 0:
+            raise error.TestFail(
+                "Failed: graphics_SyncControlTest with {0}".format(ret))
+
+    def cleanup(self):
+        if self.x_pid:
+            utils.system("kill {0}".format(self.x_pid), ignore_status=True)
+        if self._services:
+            self._services.restore_services()
