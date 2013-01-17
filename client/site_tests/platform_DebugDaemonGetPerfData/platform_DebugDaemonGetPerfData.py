92a5952ebf378d5e1bcb24c7400ba1c786978ffc
Ahmad Sharif <asharif@chromium.org>
2013-01-17 11:32:25 -0800

    debugd: added test for GetPerfData() function.
    BUG=chromium-os:37970
    TEST=./run_remote_tests.sh platform_DebugDaemonGetPerf
    
    Change-Id: If5883764135af4ce8389f17200de0b41d7bfe6ce
    Reviewed-on: https://gerrit.chromium.org/gerrit/41547
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Elly Jones <ellyjones@chromium.org>
    Tested-by: asharif <asharif@chromium.org>
    Commit-Queue: asharif <asharif@chromium.org>

---
+++ client/site_tests/platform_DebugDaemonGetPerfData/platform_DebugDaemonGetPerfData.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import dbus
+import logging
+
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import error
+
+
+class platform_DebugDaemonGetPerfData(test.test):
+    version = 1
+
+
+    def run_once(self, *args, **kwargs):
+        bus = dbus.SystemBus()
+        proxy = bus.get_object('org.chromium.debugd', '/org/chromium/debugd')
+        self.iface = dbus.Interface(proxy,
+                                    dbus_interface='org.chromium.debugd')
+        profile_duration_seconds = 2
+        result = self.iface.GetPerfData(profile_duration_seconds)
+        logging.info('Result: %s', result)
+        if not result:
+            raise error.TestFail('No perf output found: %s' % result)
+        if len(result) < 10:
+            raise error.TestFail('Perf output too small')
+        magic_prefix = "\nX\nN"
+        for i in range(len(magic_prefix)):
+            if magic_prefix[i] != chr(result[i]):
+                raise error.TestFail('Magic signature missing in output')
