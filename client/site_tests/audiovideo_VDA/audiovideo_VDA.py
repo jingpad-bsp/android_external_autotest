0dae2e2710db1b309c8bd14917f98c38ccd93dd7
Ilja Friedel <ihf@chromium.org>
2013-01-17 17:34:07 -0800

    Rename desktopui_OMXTest to audiovideo_VDA.
    Also put a check in so that it skips running on alex, mario and zgb.
    
    BUG=chromium-os:32702
    TEST=Made sure blacklist works and failure on lumpy is same as before.
    
    Change-Id: I4b1bbc5663bf6c02f3407236d583afdd8620c590
    Reviewed-on: https://gerrit.chromium.org/gerrit/41608
    Reviewed-by: Ami Fischman <fischman@chromium.org>
    Reviewed-by: Owen Lin <owenlin@chromium.org>
    Reviewed-by: Stuart Abercrombie <sabercrombie@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Tested-by: Ilja H. Friedel <ihf@chromium.org>
    Commit-Queue: Ilja H. Friedel <ihf@chromium.org>

--- client/site_tests/audiovideo_VDA/audiovideo_VDA.py
+++ client/site_tests/audiovideo_VDA/audiovideo_VDA.py
@@ -2,13 +2,22 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
+import logging, os
+from autotest_lib.client.bin import utils
 from autotest_lib.client.cros import chrome_test
 
-class desktopui_OMXTest(chrome_test.ChromeBinaryTest):
+class audiovideo_VDA(chrome_test.ChromeBinaryTest):
   version = 1
+  # Keep a current list of machines that don't support video acceleration.
+  boards_without_VDA = ['ALEX', 'MARIO', 'ZGB']
 
   def run_once(self):
+    board = utils.get_board()
+    if board in self.boards_without_VDA:
+        logging.info('Found board known not to support video acceleration.')
+        logging.info('Skip calling video_decode_accelerator_unittest.')
+        return
+
     test_video_file = os.path.join(self.cr_source_dir, 'content', 'common',
                                    'gpu', 'testdata', 'test-25fps.h264')
     # The FPS expectations here are lower than observed in most runs to keep
