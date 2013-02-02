2eed1122a005d38b9259267f4f1330ab2055c19b
Scott Zawalski <scottz@google.com>
2013-02-02 17:32:33 -0500

    Fix incorrect exception catch in host cleanup.
    In cleanup error.AutoservRunError was being caught to check whether or not
    the 'restart ui' pathway on a DUT was successful. This was a typoe and should have
    been error.AutotestRunError.
    
    BUG=None
    TEST=Ran cleanup on a host that is currently down in the lab and watched it reboot.
    
    Change-Id: I036fe8f6293fadc49c66a430ebe3813a8c4f7f66
    Reviewed-on: https://gerrit.chromium.org/gerrit/42508
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Tested-by: Scott Zawalski <scottz@chromium.org>

--- server/hosts/site_host.py
+++ server/hosts/site_host.py
@@ -366,7 +366,7 @@ class SiteHost(remote.RemoteHost):
             self.run('restart ui')
             client_at.run_static_method('autotest_lib.client.cros.cros_ui',
                                         '_wait_for_login_prompt')
-        except error.AutoservRunError:
+        except error.AutotestRunError:
             logging.warn('Unable to restart ui, rebooting device.')
             # Since restarting the UI fails fall back to normal Autotest
             # cleanup routines, i.e. reboot the machine.
