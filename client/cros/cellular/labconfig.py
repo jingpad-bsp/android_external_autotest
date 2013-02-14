jingpad-bsp/android_external_autotest

commit 3987b40ba3b4180487493aeb4d6d6bfbaab4dad0

Cellular:  Use RF switch to connect DUT to emulator
Also, some refactoring to make this possible:

Change GetDefaultBasestation to StartDefault.  I'm not happy with
this; I think the correct solution may be to make there be one test
environment object that encapsulates the configuration and has a
StartDefault method.

Check eth_test in addition to eth0

TEST=cellular_Smoke, cellular_ThroughputController
BUG=chromium-os:24703

Change-Id: I530570a9e968f195b34fb9ca7ca3bbf9a92c843e
Reviewed-on: https://gerrit.chromium.org/gerrit/13648
Reviewed-by: David Rochberg <rochberg@chromium.org>
Tested-by: David Rochberg <rochberg@chromium.org>
Commit-Ready: Jason Glasgow <jglasgow@chromium.org>

client/cros/cellular/labconfig.py
@@ -11,7 +11,7 @@ class LabConfigError(Exception):
 
 
 def get_interface_ip(interface='eth0'):
-    """Returns the IP address for an interface."""
+    """Returns the IP address for an interface, or None if not found."""
 
     # We'd like to use
     #  utils.system_output('ifconfig eth0 2>&1', retain_output=True)
@@ -22,8 +22,7 @@ def get_interface_ip(interface='eth0'):
 
     match = re.search(r'inet addr:([0-9.]+)', ifconfig)
     if not match:
-        raise LabConfigError('Could not parse interface address from ' +
-                             ifconfig)
+        return None
     return match.group(1)
 
 
@@ -35,18 +34,23 @@ class Configuration(object):
     and the DUT."""
 
     def __init__(self, args):
+        # For server tests, this constructor runs as part of the
+        # server control file, on whatever machine the test was
+        # started on.
         parser = optparse.OptionParser()
 
         # Record our args so we can serialize ourself.
         self.args = args
+
+        self.ip = None
 
         parser.add_option('--cell', dest='cell', default=None,
                           help='Cellular test cell to use')
         parser.add_option(
             '--technology', dest='technology', default='all',
             help='Radio access technologies to use (e.g. "WCDMA")')
-
         (self.options, _) = parser.parse_args(args)
+
         self.cell = self._get_cell(self.options.cell)
 
     def _get_cell(self, name):
@@ -64,20 +68,35 @@ class Configuration(object):
         return labconfig_data.CELLS[name]
 
     def _get_dut(self, machine=None):
-        """Returns the DUT record for machine from cell["duts"].
+        """Returns the DUT record for machine from cell["duts"]
+        Args:
+            machine:  name or IP of machine.  None: for "the current machine".
 
         Right now, we use the interface of eth0 to figure out which
         machine we're running on.  The important thing is that this
         matches the IP address in the cell duts configuration.  We'll
         have to come up with a better way if this proves brittle."""
+        if self.ip and not machine:
+            machine = self.ip
 
         if not machine:
             machine = get_interface_ip('eth0')
+            self.ip = machine
+        if not machine:
+            machine = get_interface_ip('eth_test')
+            self.ip = machine
+        if not machine:
+            raise LabConfigError(
+                'Could not determine which machine we are.  Cell =  %s' %
+                self.options.cell)
 
         for dut in self.cell["duts"]:
             if machine == dut["address"] or machine == dut["name"]:
                 return dut
-        return None
+
+        raise LabConfigError(
+            'Could not find DUT in lab configuration.  Cell = %s' %
+            self.options.cell)
 
     def get_technologies(self, machine=None):
         """Gets technologies to use for machine; defaults to all available."""
@@ -95,6 +114,16 @@ class Configuration(object):
                 'Could not understand a technology in %s' % technologies_list)
 
         return enums
+
+    def get_rf_switch_port(self, machine=None):
+        """Get the RF Switch Port for the specified machine."""
+        dut = self._get_dut(machine)
+
+        rf_switch = self.cell.get('rf_switch', None)
+        if rf_switch:
+            return rf_switch['ports'].index(dut['name'])
+        else:
+            return None
 
     def get_pickle(self):
         return pickle.dumps(self)

client/cros/cellular/labconfig_test.py
@@ -67,6 +67,10 @@ class TestLabConfig(unittest.TestCase):
     def test_get_interface_ip(self):
         self.assertEqual('127.0.0.1', labconfig.get_interface_ip('lo'))
 
+    def test_get_switch_port(self):
+        c = labconfig.Configuration(['--cell', 'test', '--technology=all'])
+        self.assertEqual(0,
+                         c.get_switch_port('one_two_three_four'))
 
 if __name__ == '__main__':
   unittest.main()

client/cros/cellular/emulator_config.py
@@ -3,9 +3,10 @@
 # found in the LICENSE file.
 
 """Configure cellular data emulation setup."""
-import time
+import logging, time
 
 from autotest_lib.client.cros.cellular import base_station_8960, cellular
+from autotest_lib.client.cros.cellular import ether_io_rf_switch
 from autotest_lib.client.cros.cellular import prologix_scpi_driver, scpi
 
 class Error(Exception):
@@ -25,13 +26,30 @@ def _CreateBaseStation(c):
     return base_station_8960.BaseStation8960(s)
 
 
-def GetDefaultBasestation(config, technology):
+def _CreateRfSwitch(config):
+    if 'rf_switch' not in config.cell:
+        return None
+    switch_config = config.cell['rf_switch']
+    if switch_config['type'] != 'ether_io':
+        raise KeyError('Could not configure switch of type %s' %
+                       switch_config['type'])
+    return ether_io_rf_switch.RfSwitch(switch_config['address'])
+
+
+def StartDefault(config, technology):
     """Set up a base station and turn it on.  Return BS and verifier."""
     if len(config.cell['basestations']) > 1:
         raise Error('Cannot (yet) handle >1 base station')
 
     c = config.cell['basestations'][0]
     bs = _CreateBaseStation(c)
+
+    rf_switch = _CreateRfSwitch(config)
+    if rf_switch:
+        port = config.get_rf_switch_port()
+        logging.info(
+            'Changing switch port from %s to %s' % (rf_switch.Query(), port))
+        rf_switch.SelectPort(port)
 
     with bs.checker_context:
         bs.SetBsNetmaskV4(c['bs_netmask'])

client/site_tests/cellular_Smoke/cellular_Smoke.py
@@ -34,8 +34,8 @@ class cellular_Smoke(test.test):
             # This shuts down other network devices on the host.  Again,
             # this is to ensure that test traffic goes over the modem
             with cell_tools.OtherDeviceShutdownContext('cellular', flim):
-                bs, verifier = emulator_config.GetDefaultBasestation(config,
-                                                                     technology)
+                bs, verifier = emulator_config.StartDefault(config, technology)
+
                 network.ResetAllModems(flim)
                 cell_tools.PrepareModemForTechnology('', technology)
 

client/site_tests/cellular_Throughput/cellular_Throughput.py
@@ -22,8 +22,7 @@ class cellular_Throughput(test.test):
         with backchannel.Backchannel():
             flim = flimflam.FlimFlam()
             with cell_tools.OtherDeviceShutdownContext('cellular', flim):
-                bs, verifier = emulator_config.GetDefaultBasestation(
-                    config, technology)
+                bs, verifier = emulator_config.StartDefault(config, technology)
                 network.ResetAllModems(flim)
                 cell_tools.PrepareModemForTechnology('', technology)
                 # TODO(rochberg): Figure out whether it's just Gobi 2k

commit c94926b4f59acebbbc0ac8b230b59fbd5fe71118

[autotest] Fix unittests in client/cros/cellular/labconfig.py
Unit test was broken in commit 3987b40ba3b4180487493aeb4d6d6bfbaab4dad0,
and further broken in commit 91d57f0fd6adbafac8c8e90a28097f8f1edad427.

BUG=chromium-os:37989
TEST=Unit test now passes.

Change-Id: Ia32c52b00939cd41872d543e500f00ee6716f14f
Reviewed-on: https://gerrit.chromium.org/gerrit/43203
Commit-Queue: Aviv Keshet <akeshet@chromium.org>
Reviewed-by: Aviv Keshet <akeshet@chromium.org>
Tested-by: Aviv Keshet <akeshet@chromium.org>

client/cros/cellular/labconfig.py
@@ -7,11 +7,14 @@ import optparse, pickle, re, subprocess
 import cellular, labconfig_data
 
 class LabConfigError(Exception):
-  pass
+    """Exception thrown on bad lab configuration"""
+    pass
 
 
 def get_interface_ip(interface='eth0'):
-    """Returns the IP address for an interface, or None if not found."""
+    """Returns the IP address for an interface, or None if not found.
+    @param interface: the interface to request IP address for.
+    """
 
     # We'd like to use
     #  utils.system_output('ifconfig eth0 2>&1', retain_output=True)
@@ -99,7 +102,9 @@ class Configuration(object):
             self.options.cell)
 
     def get_technologies(self, machine=None):
-        """Gets technologies to use for machine; defaults to all available."""
+        """Gets technologies to use for machine; defaults to all available.
+        @param machine: Machine to get technologies for.
+        """
         technologies_list = self.options.technology.split(',')
 
         if 'all' in technologies_list:
@@ -116,9 +121,13 @@ class Configuration(object):
         return enums
 
     def get_rf_switch_port(self, machine=None):
-        """Get the RF Switch Port for the specified machine."""
+        """Get the RF Switch Port for the specified machine.
+        @param machine: machine to get rf switch port for
+        """
         dut = self._get_dut(machine)
+        print dut
         return dut['rf_switch_port']
 
     def get_pickle(self):
+        """Get pickled object."""
         return pickle.dumps(self)

client/cros/cellular/labconfig_test.py
@@ -3,6 +3,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# pylint: disable-msg=C0111
+
 import unittest
 import cellular
 import labconfig
@@ -15,6 +17,7 @@ TEST_CELL = {
             'address': '1.2.3.4',
             'name': 'one_two_three_four',
             'technologies': ['CDMA_2000'],
+            'rf_switch_port': 0
             },
         {
             'address': '5.6.7.8',
@@ -25,9 +28,6 @@ TEST_CELL = {
     'rf_switch': {
         'type': 'ether_io',
         'address':  '172.31.206.172',
-        # Ports maps from port index to name as specified in
-        # duts.name
-        'ports': ['one_two_three_four', None, None, 'five_six_seven_eight'],
         }
     }
 
@@ -67,10 +67,10 @@ class TestLabConfig(unittest.TestCase):
     def test_get_interface_ip(self):
         self.assertEqual('127.0.0.1', labconfig.get_interface_ip('lo'))
 
-    def test_get_switch_port(self):
+    def test_get_rf_switch_port(self):
         c = labconfig.Configuration(['--cell', 'test', '--technology=all'])
         self.assertEqual(0,
-                         c.get_switch_port('one_two_three_four'))
+                         c.get_rf_switch_port('one_two_three_four'))
 
 if __name__ == '__main__':
   unittest.main()