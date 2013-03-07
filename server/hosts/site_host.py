13eb7c0c08459a94e49acaa2261b553af53f99dd
J. Richard Barnette <jrbarnette@chromium.org>
2013-03-07 12:06:29 -0800

    Limit the polling rate in xmlrpc_connect().
    In the code in xmlrpc_connect() that waits for the server to
    be ready, each failed attempt went into the log, at a rate of 10
    per second.  At two lines per attempt, that could easily produce
    more than 150 lines of log output.  Really, that's too much output,
    and polling 10 times per second is more than is necessary.
    
    This change limits the polling rate so that the number of log
    entries won't exceed 20 (meaning <=40 lines) for typical timeout
    values.
    
    BUG=None
    TEST=platform_ServoPyAuto (changed to use the new retry loop)
    
    Change-Id: Ia4e06aab45a4b12fc3db162464fe8e8c47630998
    Reviewed-on: https://gerrit.chromium.org/gerrit/44866
    Reviewed-by: Christopher Wiley <wiley@chromium.org>
    Commit-Queue: Richard Barnette <jrbarnette@chromium.org>
    Tested-by: Richard Barnette <jrbarnette@chromium.org>

--- server/hosts/site_host.py
+++ server/hosts/site_host.py
@@ -802,9 +802,11 @@ class SiteHost(remote.RemoteHost):
         rpc_url = 'http://localhost:%d' % local_port
         proxy = xmlrpclib.ServerProxy(rpc_url, allow_none=True)
         if ready_test_name is not None:
+            # retry.retry logs each attempt; calculate delay_sec to
+            # keep log spam to a dull roar.
             @retry.retry((socket.error, xmlrpclib.ProtocolError),
                          timeout_min=timeout_seconds/60.0,
-                         delay_sec=0.1)
+                         delay_sec=min(max(timeout_seconds/20.0, 0.1), 1))
             def ready_test():
                 """ Call proxy.ready_test_name(). """
                 getattr(proxy, ready_test_name)()
