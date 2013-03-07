ab4cc8d14a918fb49f52c28edb8a32577d0c3a3e
J. Richard Barnette <jrbarnette@chromium.org>
2013-03-07 12:22:25 -0800

    Update how pyauto_proxy waits for the XMLRPC proxy to be ready.
    The host xmlrpc_connect() method now has a feature to do the waiting
    on behalf of the client.  Change create_pyauto_proxy() to use that
    method, rather than carry duplicative logic.
    
    BUG=None
    TEST=platform_ServoPyAuto
    
    Change-Id: Ia417a22a81547eee16f5aa31da517356dc122768
    Reviewed-on: https://gerrit.chromium.org/gerrit/44865
    Tested-by: Richard Barnette <jrbarnette@chromium.org>
    Reviewed-by: Christopher Wiley <wiley@chromium.org>
    Commit-Queue: Richard Barnette <jrbarnette@chromium.org>

--- server/cros/pyauto_proxy.py
+++ server/cros/pyauto_proxy.py
@@ -2,13 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import errno
-import logging
-import socket
-import time
-import xmlrpclib
-
-from autotest_lib.client.common_lib import error
 from autotest_lib.server import autotest
 
 _PYAUTO_INSTALLER = 'desktopui_PyAutoInstall'
@@ -18,7 +11,6 @@ _PYAUTO_PORT = 9988
 _PYAUTO_CLEANUP_NAME = 'remote_pyauto'
 
 _PYAUTO_TIMEOUT = 10
-_PYAUTO_POLL_INTERVAL = _PYAUTO_TIMEOUT / 20.0
 
 
 def create_pyauto_proxy(host, auto_login=False):
@@ -45,25 +37,7 @@ def create_pyauto_proxy(host, auto_login=False):
     autotest_client = autotest.Autotest(host)
     autotest_client.run_test(_PYAUTO_INSTALLER, auto_login=auto_login)
 
-    pyauto = host.xmlrpc_connect(_PYAUTO_REMOTE, _PYAUTO_PORT,
-                                 _PYAUTO_CLEANUP_NAME)
-
-    endtime = time.time() + _PYAUTO_TIMEOUT
-    exc = None
-    while time.time() < endtime:
-        should_log = True
-        try:
-            pyauto.IsChromeOS()
-            break
-        except BaseException, exc:
-            pass
-
-        logging.debug('Exception: %s', str(exc))
-        time.sleep(_PYAUTO_POLL_INTERVAL)
-    else:
-        host.xmlrpc_disconnect(_PYAUTO_PORT)
-        logging.error('PyAuto server on host %s not ready'
-                      ' after %d seconds', host.hostname, _PYAUTO_TIMEOUT)
-        raise exc
-
-    return pyauto
+    return host.xmlrpc_connect(_PYAUTO_REMOTE, _PYAUTO_PORT,
+                               command_name=_PYAUTO_CLEANUP_NAME,
+                               ready_test_name='IsChromeOS',
+                               timeout_seconds=_PYAUTO_TIMEOUT)
