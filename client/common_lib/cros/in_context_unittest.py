8d6cd54b45bc68afdb92c328aa2c84dd9aaf9872
Christopher Wiley <wiley@chromium.org>
2013-03-11 11:10:44 -0700

    autotest: Add context manager wrapper
    Add a decorator that grabs a named context manager from self before
    calling the wrapped function.
    
    BUG=None
    TEST=Included unit test
    
    Change-Id: Id8271c36c98f393a6d0617f0eeb881217f5b3a8d
    Reviewed-on: https://gerrit.chromium.org/gerrit/45114
    Commit-Queue: Christopher Wiley <wiley@chromium.org>
    Reviewed-by: Christopher Wiley <wiley@chromium.org>
    Tested-by: Christopher Wiley <wiley@chromium.org>

---
+++ client/common_lib/cros/in_context_unittest.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import mox
+import threading
+import time
+
+from autotest_lib.client.common_lib.cros.in_context import in_context
+
+
+class InContextTest(mox.MoxTestBase):
+    """ Unit tests for the in_context decorator. """
+
+    @in_context('lock')
+    def inc_count(self):
+        """ Do a slow, racy read/write. """
+        temp = self.count
+        time.sleep(0.0001)
+        self.count = temp + 1
+
+
+    def testDecorator(self):
+        """ Test that the decorator works by using it with a lock. """
+        self.count = 0
+        self.lock = threading.RLock()
+        iters = 100
+        num_threads = 20
+        # Note that it is important for us to go through all this bother to call
+        # a method in_context N times rather than call a method in_context that
+        # does something N times, because by doing the former, we acquire the
+        # context N times (1 time for the latter).
+        thread_body = lambda f, n: [f() for i in xrange(n)]
+        threads = [threading.Thread(target=thread_body,
+                                    args=(self.inc_count, iters))
+                   for i in xrange(num_threads)]
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+        self.assertEquals(iters * num_threads, self.count)
