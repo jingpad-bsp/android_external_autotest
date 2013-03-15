fed99762df01cbf0a44f18489964d075c675045a
Owen Lin <owenlin@chromium.org>
2013-03-15 13:02:14 +0800

    Let audiovideo_VDA tests both h.264 and vp8 format.
    BUG=chromium-os:38495
    TEST=build and run the test on daisy
    
    Change-Id: I3d0c462438439a2b1c7bdf99f6cc1ca675da13ba
    Reviewed-on: https://gerrit.chromium.org/gerrit/45719
    Reviewed-by: Yuli Huang <yuli@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Commit-Queue: Owen Lin <owenlin@chromium.org>
    Tested-by: Owen Lin <owenlin@chromium.org>

--- client/site_tests/audiovideo_VDA/audiovideo_VDA.py
+++ client/site_tests/audiovideo_VDA/audiovideo_VDA.py
@@ -2,33 +2,30 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os
-from autotest_lib.client.bin import utils
+import os
 from autotest_lib.client.cros import chrome_test
 
+SKIP_DEPS_ARG = 'skip_deps'
+
 class audiovideo_VDA(chrome_test.ChromeBinaryTest):
-  version = 1
-  # Keep a current list of machines that don't support video acceleration.
-  boards_without_VDA = ['x86-alex', 'x86-mario', 'x86-zgb']
-  # Keep a current list of machines that use Exynos.
-  boards_with_exynos_VDA = ['daisy', 'daisy_spring']
+    """
+    This test is a wrapper of the chrome binary test:
+    video_decode_accelerator_unittest.
+    """
 
-  def run_once(self):
-    board = utils.get_board()
-    if board in self.boards_without_VDA:
-        logging.info('Found board known not to support video acceleration.')
-        logging.info('Skip calling video_decode_accelerator_unittest.')
-        return
+    version = 1
+    binary = 'video_decode_accelerator_unittest'
 
-    test_video_file = os.path.join(self.cr_source_dir, 'content', 'common',
-                                   'gpu', 'testdata', 'test-25fps.h264')
-    # The FPS expectations here are lower than observed in most runs to keep
-    # the bots green.
-    binary_test = 'video_decode_accelerator_unittest'
-    cmd_line_params = ('--test_video_data="%s:320:240:250:258:35:150:1"' %
-                       test_video_file)
-    self.run_chrome_binary_test(binary_test, cmd_line_params)
-    # Also run tests with EVDA for Exynos.
-    if board in self.boards_with_exynos_VDA:
-        cmd_line_params = ('%s --use-exynos-vda' % cmd_line_params)
-        self.run_chrome_binary_test(binary_test, cmd_line_params)
+
+    def initialize(self, arguments=[]):
+        chrome_test.ChromeBinaryTest.initialize(
+            self, nuke_browser_norestart=False,
+            skip_deps=bool(SKIP_DEPS_ARG in arguments))
+
+
+    def run_once(self, video_file, test_params):
+        video_file = os.path.join(self.cr_source_dir, 'content', 'common',
+                                  'gpu', 'testdata', video_file)
+
+        cmd_line = ('--test_video_data="%s:%s"' % (video_file, test_params))
+        self.run_chrome_binary_test(self.binary, cmd_line)
