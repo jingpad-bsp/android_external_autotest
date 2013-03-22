a55e204d8628ebb466209b281be78a37495cc7e8
Ismail Noorbasha <ismail@chromium.org>
2013-03-22 15:26:37 -0700

    Added Install EC functionality from shellball or raw binary.
    This change adds the functionality of Installing EC by getting binary from
    shellball of the installed Image or raw binary which is passed from command
    line argument.Added "fw_type" additional argument to differentiate the FW from
    BIOS and EC and direct the host commands accordingly.
    
    BUG=None.
    TEST=BIOS_local:./run_remote_tests.sh --args='fw_type=bios fw_path=local'
    --remote=<ip> --board=<> platform_InstallFW
    BIOS_raw binary:./run_remote_tests.sh --args='fw_type=bios
    fw_path=~/trunk/src/fac_input fw_name=<bios.bin>' --remote=<ip> --board=<>
    platform_InstallFW
    
    EC_local:./run_remote_tests.sh --args='fw_type=ec fw_path=local' --remote=<ip>
    --board=<> platform_InstallFW
    EC_raw binary:./run_remote_tests.sh --args='fw_type=ec
    fw_path=~/trunk/src/fac_input fw_name=<ec.bin>' --remote=<ip> --board=<>
    platform_InstallFW
    
    Change-Id: Ifccbc19ff57ba23b37f6471b867b1dfb6adfa394
    Reviewed-on: https://gerrit.chromium.org/gerrit/46322
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Ismail Noorbasha <ismail@chromium.org>
    Commit-Queue: Ismail Noorbasha <ismail@chromium.org>

--- server/site_tests/platform_InstallFW/platform_InstallFW.py
+++ server/site_tests/platform_InstallFW/platform_InstallFW.py
@@ -8,27 +8,38 @@ from autotest_lib.server import test, autotest, utils
 class platform_InstallFW(test.test):
     version = 1
 
-    def run_once(self, host=None, bios_path=None, bios_name=None):
-        if bios_path == "local":
+    def run_once(self, host=None, fw_type=None, fw_path=None, fw_name=None):
+        if fw_path == "local":
             fw_dst = "/usr/sbin/chromeos-firmwareupdate"
             is_shellball = True
         else:
-            fw_src = "%s/%s" % (bios_path, bios_name)
+            fw_src = "%s/%s" % (fw_path, fw_name)
             # Determine the firmware file is a shellball or a raw binary.
             is_shellball = (utils.system_output("file %s" % fw_src).find(
                     "shell script") != -1)
-            fw_dst = "/tmp/%s" % bios_name
+            fw_dst = "/tmp/%s" % fw_name
             # Copy binary from server to client.
             host.send_file(fw_src, fw_dst)
 
-        # Install bios on a client.
-        if is_shellball:
-            host.run("sudo /bin/sh %s --mode factory_install" % fw_dst)
-        else:
-            host.run("sudo /usr/sbin/flashrom -w %s" % fw_dst)
-
+        # Install bios/ec on a client.
+        if fw_type == "bios":
+            if is_shellball:
+                host.run("sudo /bin/sh %s --mode recovery --update_main "
+                         "--noupdate_ec" % fw_dst)
+            else:
+                host.run("sudo /usr/sbin/flashrom -p internal:bus=spi -w %s"
+                         % fw_dst)
+        if fw_type == "ec":
+            if is_shellball:
+                host.run("sudo /bin/sh %s --mode recovery --update_ec "
+                         "--noupdate_main" % fw_dst)
+            else:
+                host.run("sudo /usr/sbin/flashrom -p internal:bus=lpc -w %s"
+                         % fw_dst)
         # Reboot client after installing the binary.
         host.reboot()
-        # Get the versions of binaries.
+        # Get the versions of BIOS and EC binaries.
         bios_info = host.run("sudo mosys -k smbios info bios")
-        logging.info("Firmware version info: %s" % bios_info)
+        logging.info("BIOS version info: %s" % bios_info)
+        ec_info = host.run("sudo mosys -k ec info")
+        logging.info("EC version info: %s" % ec_info)
