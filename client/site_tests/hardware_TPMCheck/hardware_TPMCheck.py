fae481d5329169bed279b579a85ba4873afca3d5
Mike Truty <truty@chromium.org>
2013-03-25 12:40:14 -0500

    Refactor TPMCheck and show test+suspend+test pattern.
    hardware_TPMCheck and hardware_TPMCheckS3 had duplicated but
    different code for checking TPM state.  This factors all
    checks into TPMCheck which is then invoked by TPMCheckS3
    (which has been renamed power_CheckAfterSuspend).
    Also, rewrites TPMCheck to show a more pythonic pattern that
    makes run_once() more readable by reviewers.
    
    Updated power_CheckAfterSuspend includes first control file
    with call to hardware_TPMCheck.
    
    power_CheckAfterSuspend supports use of iterations for
    longer-running experiments.  Have tested upto 20 iterations.
    
    BUG=chromium:218123
    TEST=tested on daisy and snow, with no, 1, 2, and 3 iterations.
    TEST=able to get 200iterations on link but cannot get >20 on my old snow.
    TEST=./run_remote_tests.sh --board=<board> --remote=w.x.y.z \
         client/site_tests/power_CheckAfterSuspend/control
    TEST=./run_remote_tests.sh -a "iterations=3" --remote=w.x.y.z \
         client/site_tests/power_CheckAfterSuspend/control
    
    Change-Id: Ib185186ae482f4cbfa65fa83b74047b1953d4307
    Reviewed-on: https://gerrit.chromium.org/gerrit/46412
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Mark Koudritsky <kamrik@chromium.org>
    Tested-by: Mike Truty <truty@chromium.org>
    Commit-Queue: Mike Truty <truty@chromium.org>

--- client/site_tests/hardware_TPMCheck/hardware_TPMCheck.py
+++ client/site_tests/hardware_TPMCheck/hardware_TPMCheck.py
@@ -2,81 +2,116 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, re
+import logging, re
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
+from autotest_lib.client.cros import service_stopper
+
+
+KV_MATCH_RE = re.compile('([^ ]+) (.*)')
+
+# Expected results of 'tpmc getX' commands.
+TPMC_EXPECTED = {
+    'getvf': # volatile (ST_CLEAR) flags
+     set([('deactivated', '0'), ('physicalPresence', '0'),
+          ('physicalPresenceLock', '1'), ('bGlobalLock', '1')]),
+    'getpf': # permanent flags
+     set([('disable', '0'), ('ownership', '1'), ('deactivated', '0'),
+          ('physicalPresenceHWEnable', '0'), ('physicalPresenceCMDEnable', '1'),
+          ('physicalPresenceLifetimeLock', '1'), ('nvLocked', '1')])}
+
 
 def missing_firmware_version():
-    f = os.popen("crossystem fwid")
-    if not f:
-        return True
-    f.close()
-    return False
+    """Check for empty fwid.
 
-def dict_from_command(command):
-    dict = {}
-    out = os.popen(command)
-    for linecr in out.readlines():
-        line = linecr.strip()
-        match = re.match("([^ ]+) (.*)", line)
-        k = match.group(1)
-        v = match.group(2)
-        dict[k] = v
-    out.close()
-    return dict
+    @return True if no fwid else False.
+    """
+    cmd = 'crossystem fwid'
+    return not utils.system_output(cmd, ignore_status=True).strip()
 
-def expect(d, key, value):
-    if (d[key] != value):
-        raise error.TestError("expecting %s = %s, observing %s = %s" %
-                              (key, value, key, d[key]))
 
-def checkp(space, permission):
-    c = "tpmc getp %s" % space
-    out = os.popen(c)
-    l = out.readline()
-    out.close()
-    if (not re.match(".*%s" % permission, l)):
-        raise error.TestError("invalid response to %s: %s" % (c, l))
+def __run_tpmc_cmd(subcommand):
+    """Make this test more readable by simplifying commonly used tpmc command.
+
+    @param subcommand: String of the tpmc subcommand (getvf, getpf, getp, ...)
+    @return String output (which may be empty).
+    """
+    cmd = 'tpmc %s' % subcommand
+    return utils.system_output(cmd, ignore_status=True).strip()
+
+
+def set_from_keyval_output(out):
+    """Parse space-separated key-val output into a set of tuples.
+
+    Stuff the key-vals into tuples in a set to be later compared.
+
+    e.g.  deactivated 0
+          disableForceClear 0
+          ==>  set(('deactivated', '0'), ('disableForceClear', '0'))
+
+    @param out: multiple lines of space-separated key-val pairs.
+    @return set of key-val tuples.
+    """
+    results = set()
+    for linecr in out.splitlines():
+        match = KV_MATCH_RE.match(linecr.strip())
+        if match:
+            results.add((match.group(1), match.group(2)))
+    return results
+
+
+def check_tpmc(subcommand, expected):
+    """Runs tpmc command and checks the output against an expected result.
+
+    The expected results take 2 different forms:
+    1. A regular expression that is matched.
+    2. A set of tuples that are matched.
+
+    @param subcommand: String of the tpmc subcommand (getvf, getpf, getp, ...)
+    @param expected: Either a String re or the set of expected tuples.
+    @raises error.TestError() for invalidly matching expected.
+    """
+    error_msg = 'invalid response to tpmc %s' % subcommand
+    if isinstance(expected, str):
+        out = __run_tpmc_cmd(subcommand)
+        if (not re.match(expected, out)):
+            raise error.TestError('%s: %s' % (error_msg, out))
+    else:
+        result_set = set_from_keyval_output(__run_tpmc_cmd(subcommand))
+        if set(expected) <= result_set:
+            return
+        raise error.TestError('%s: expected=%s.' %
+                              (error_msg, sorted(set(expected) - result_set)))
+
 
 class hardware_TPMCheck(test.test):
+    """Check that the state of the TPM is as expected."""
     version = 1
 
-    def run_once(self):
 
+    def run_once(self):
+        """Run a few TPM state checks."""
         if missing_firmware_version():
-            logging.warning("no firmware version, skipping test")
+            logging.warning('no firmware version, skipping test')
             return
 
+        # Must stop the TCSD process to be able to collect TPM status,
+        # then restart TCSD process to leave system in a known good state.
+        # Due to crbug.com/219129 even though TCSD is restarted when this
+        # completes, cryptohomed never reconnects with TCSD so logins are hosed.
+        services = service_stopper.ServiceStopper(['tcsd'])
+        services.stop_services()
         try:
-            utils.system("stop tcsd", ignore_status=True)
-
-            # Check volatile (ST_CLEAR) flags
-            d = dict_from_command("tpmc getvf");
-            expect(d, "deactivated", "0")
-            expect(d, "physicalPresence", "0")
-            expect(d, "physicalPresenceLock", "1")
-            expect(d, "bGlobalLock", "1")
-
-            # Check permanent flags
-            d = dict_from_command("tpmc getpf");
-            expect(d, "disable", "0")
-            expect(d, "ownership", "1")
-            expect(d, "deactivated", "0")
-            expect(d, "physicalPresenceHWEnable", "0")
-            expect(d, "physicalPresenceCMDEnable", "1")
-            expect(d, "physicalPresenceLifetimeLock", "1")
-            expect(d, "nvLocked", "1")
+            # Check volatile and permanent flags
+            for subcommand in ['getvf', 'getpf']:
+                check_tpmc(subcommand, TPMC_EXPECTED[subcommand])
 
             # Check space permissions
-            checkp("0x1007", "0x8001")
-            checkp("0x1008", "0x1")
+            check_tpmc('getp 0x1007', '.*0x8001')
+            check_tpmc('getp 0x1008', '.*0x1')
 
             # Check kernel space UID
-            out = os.popen("tpmc read 0x1008 0x5")
-            l = out.readline()
-            if (not re.match(".* 4c 57 52 47$", l)):
-                raise error.TestError("invalid kernel space UID: %s" % l)
-            out.close()
+            check_tpmc('read 0x1008 0x5', '.* 4c 57 52 47$')
 
         finally:
-            utils.system("start tcsd")
+            services.restore_services()
