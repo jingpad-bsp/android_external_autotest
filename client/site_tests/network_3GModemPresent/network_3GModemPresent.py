93dd1d5d2581bb47053afadd3c48904e32a0c91b
Arman Uguray <armansito@chromium.org>
2013-03-27 14:54:40 -0700

    network_3GModemPresent: Add control.pseudomodem.
    Added support for running the test with the pseudomodem.
    
    BUG=chromium:217680
    TEST=Run network_3GModemPresent/control.pseudomodem on link and lumpy
    with Gobi3k. It should pass.
    
    Change-Id: If43522ce37233a1f65a0ec41dbf8810e96f2333c
    Reviewed-on: https://gerrit.chromium.org/gerrit/46690
    Commit-Queue: Arman Uguray <armansito@chromium.org>
    Reviewed-by: Arman Uguray <armansito@chromium.org>
    Tested-by: Arman Uguray <armansito@chromium.org>

--- client/site_tests/network_3GModemPresent/network_3GModemPresent.py
+++ client/site_tests/network_3GModemPresent/network_3GModemPresent.py
@@ -7,17 +7,18 @@ from autotest_lib.client.common_lib import error
 
 import dbus
 
+from autotest_lib.client.cros.cellular.pseudomodem import mm1, pseudomodem, sim
 from autotest_lib.client.cros import flimflam_test_path
 import flimflam
 
 class network_3GModemPresent(test.test):
     version = 1
 
-    def run_once(self):
-        flim = flimflam.FlimFlam()
+    def run_once(self, pseudo_modem=False):
+        with pseudomodem.TestModemManagerContext(pseudo_modem,
+                                                 ['cromo', 'modemmanager']):
+            flim = flimflam.FlimFlam()
 
-        device = flim.FindElementByPropertySubstring("Device",
-                                                     "Type",
-                                                     "cellular")
-        if not device:
-            raise error.TestFail("Could not find cellular device")
+            device = flim.FindCellularDevice()
+            if not device:
+                raise error.TestFail("Could not find cellular device")
