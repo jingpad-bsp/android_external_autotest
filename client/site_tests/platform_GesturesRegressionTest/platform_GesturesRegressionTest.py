58d4de283a919ebccefcd93df29471cb1a1bda9e
Dennis Kempin <denniskempin@chromium.org>
2013-03-28 16:00:09 -0700

    platform_GesturesRegressionTest: use TestRunner directly
    Instead of calling the command line interface and parsing it's output
    this CL updates the test script to use the TestRunner directly.
    
    BUG=chromium:224121
    TEST=run autotests for platform_GesturesRegressionTest
    
    Change-Id: I09d6b1f1a79b78b8e877cbaefefc10453e41de07
    Reviewed-on: https://gerrit.chromium.org/gerrit/46819
    Reviewed-by: Chung-yih Wang <cywang@chromium.org>
    Commit-Queue: Dennis Kempin <denniskempin@chromium.org>
    Tested-by: Dennis Kempin <denniskempin@chromium.org>

--- client/site_tests/platform_GesturesRegressionTest/platform_GesturesRegressionTest.py
+++ client/site_tests/platform_GesturesRegressionTest/platform_GesturesRegressionTest.py
@@ -3,24 +3,9 @@
 # found in the LICENSE file.
 
 import os
-import re
-from autotest_lib.client.bin import test, utils
-from autotest_lib.client.common_lib import error, site_utils
-
-#  Parse the score result lines, e.g. output of regression tests for mario
-#
-#  | mario/base_click   |          |  success (1.0000)  |         |
-#  | mario/base_tap     |          |  failure           |         |
-#
-#  We would like to retrieve the test name and its score if it is a success
-#  pattern and give the score of 0.0 if it is a failure pattern.
-SUCCESS_PATTERN = '\|\s*([\w\d.]*/\w+)\s*\|\s*\|\s*\w+\s\((\d.\d+)\)\s*\|'
-SUCCESS_RE = re.compile(SUCCESS_PATTERN)
-FAILURE_PATTERN = '\|\s*([\w\d.]*/\w+)\s*\|\s*\|\s*failure\s*\|'
-FAILURE_RE = re.compile(FAILURE_PATTERN)
-COLOR_CODE_RE = re.compile("\x1B\[[0-9;]*[mK]")
-TOUCHPAD_TEST = os.path.join('touchpad-tests', 'touchtests')
-
+import sys
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import site_utils
 
 class platform_GesturesRegressionTest(test.test):
     """ Wrapper of regression test of gestures library.
@@ -43,26 +28,23 @@ class platform_GesturesRegressionTest(test.test):
         if board == 'daisy':
             board = 'snow'
 
-        touchtests = os.path.join(self.autodir, 'deps', TOUCHPAD_TEST)
-        cmd = '%s --autotest %s*/*' % (touchtests, board)
-        output = COLOR_CODE_RE.sub('', utils.system_output(cmd))
-        if not output:
-            raise error.TestError('Can not run the touchtests')
+        # find paths for touchpad tests
+        root = os.path.join(self.autodir, 'deps', 'touchpad-tests')
+        framework_dir = os.path.join(root, 'framework')
+        tests_dir = os.path.join(root, 'tests')
+        xorg_dir = os.path.join(root, 'xorg-conf-files')
 
+        # create test runner
+        sys.path.append(framework_dir)
+        from test_runner import ParallelTestRunner as TestRunner
+        runner = TestRunner(tests_dir, xorg_dir)
+
+        # run all tests for this board and extract results
+        results = runner.RunAll('%s*/*' % board)
         self.test_results = {}
-        for line in output.splitlines():
-            result = SUCCESS_RE.search(line)
-            score = ''
-            if result:
-                score = result.group(2)
-            else:
-                result = FAILURE_RE.search(line)
-                if result:
-                    # set the score of failure test to 0.0
-                    score = '0.0'
-            if score:
-                # '/' is not a valid character for a key name
-                self.test_results[result.group(1).replace('/', '-')] = score
+        for key, value in results.items():
+            self.test_results[key.replace('/', '-')] = value["score"]
+
         # write converted test results out
         if self.test_results:
             self.write_perf_keyval(self.test_results)
