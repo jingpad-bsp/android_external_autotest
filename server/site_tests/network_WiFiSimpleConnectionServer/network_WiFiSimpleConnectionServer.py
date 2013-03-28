3f260a43aa69985b62afd3d83c2e2a2c4019be24
Tan Gao <tgao@chromium.org>
2013-03-28 14:59:24 -0700

    Refactor network_WiFiSimpleConnectionServer wifi test.
     - break up loop_ap_configs_and_test() into smaller methods
     - distinguish between use of 'ap' (an APConfigurator object) vs.
       'ap_info' (a dict of strings containing AP config info)
     - simplify band and channel selection/mapping logic
     - fixed style-related lint issues
    
    BUG=chromium:224770
    TEST=Manual (modified test case to use only 2 APs and 2 tries)
    
    Change-Id: Ifc53e47e49aded28363cb9c51c280689b94193ae
    Reviewed-on: https://gerrit.chromium.org/gerrit/46810
    Reviewed-by: Kris Rambish <krisr@chromium.org>
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Commit-Queue: Tan Gao <tgao@chromium.org>
    Tested-by: Tan Gao <tgao@chromium.org>

--- server/site_tests/network_WiFiSimpleConnectionServer/network_WiFiSimpleConnectionServer.py
+++ server/site_tests/network_WiFiSimpleConnectionServer/network_WiFiSimpleConnectionServer.py
@@ -8,13 +8,14 @@ import pprint
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.server import autotest, test
+from autotest_lib.server.cros.chaos_ap_configurators import ap_cartridge
 from autotest_lib.server.cros.chaos_ap_configurators import ap_configurator_factory
 from autotest_lib.server.cros.chaos_ap_configurators import download_chromium_prebuilt
-from autotest_lib.server.cros.chaos_ap_configurators import ap_cartridge
 from autotest_lib.server.cros.chaos_config import ChaosAP
 from autotest_lib.server.cros.wlan import connector, disconnector
 
 class network_WiFiSimpleConnectionServer(test.test):
+    """ Dynamic Chaos test. """
 
     version = 1
 
@@ -27,30 +28,28 @@ class network_WiFiSimpleConnectionServer(test.test):
         self.error_list = []
 
 
-    def run_connect_disconnect_test(self, ap, iteration):
+    def run_connect_disconnect_test(self, ap_info, iteration):
         """ Connects to the AP and Navigates to URL.
 
-        Args:
-            ap: the ap object
-            iteration: the current iteration
+        @param ap_info: a dict of attributes of a specific AP.
+        @param iteration: an integer, 1-indexed current iteration.
 
-        Returns:
-            None if there are no errors
-            The error messages if there are any errors.
+        @return None if there are no errors.
+                The error messages if there are any errors.
         """
         error = None
 
-        ssid = ap['ssid']
-        frequency = ap['frequency']
-        bss = ap['bss']
+        ssid = ap_info['ssid']
+        frequency = ap_info['frequency']
+        bss = ap_info['bss']
 
         self.d.disconnect(ssid)
         self.c.set_frequency(frequency)
         log_folder = os.path.join(self.outputdir, '%s' % bss)
         if not os.path.exists(log_folder):
             os.mkdir(log_folder)
-        self.c.set_filename(os.path.join(log_folder, 'connect_try_%d'
-                                         % (iteration+1)))
+        self.c.set_filename(
+            os.path.join(log_folder, 'connect_try_%d' % iteration))
         error = None
         try:
             self.c.connect(ssid, frequency=frequency)
@@ -64,6 +63,12 @@ class network_WiFiSimpleConnectionServer(test.test):
 
 
     def supported_band_and_channel(self, ap, band, channel):
+        """ Checks if specified band and channel is supported by ap.
+
+        @param ap: an APConfigurable object.
+        @param band: a string.
+        @param channel: a string.
+        """
         bands = ap.get_supported_bands()
         for current_band in bands:
             if (current_band['band'] == band and
@@ -72,75 +77,107 @@ class network_WiFiSimpleConnectionServer(test.test):
         return False
 
 
-    def loop_ap_configs_and_test(self, all_aps, tries):
-        """ Configures AP to all supported radio permuations runs a test.
+    def _run_ap_test(self, ap_info, tries):
+        """ Runs test on a configured AP.
 
-        Args:
-            ap: List of APs to run the test on.
-            tries: The number of times to connect/disconnect.
+        @param ap_info: a dict.
+        @param tries: an integer, number of times to connect/disconnect.
         """
-        # We need to go through the APs and pull out the common bands and
-        # then channels, the factory can do this a better way.
+        ap_info['failed_iterations'] = []
+        for iteration in range(1, tries+1):
+            logging.info('Connection try %d', iteration)
+            resp = self.run_connect_disconnect_test(ap_info, iteration)
+            if resp:
+                ap_info['failed_iterations'].append({'error': resp,
+                                                     'try': iteration})
+        if ap_info['failed_iterations']:
+            self.error_list.append(ap_info)
 
-        bands_and_channels = {}
-        for ap in all_aps:
-            bands = ap.get_supported_bands()
-            for band in bands:
-                if band['band'] not in bands_and_channels:
-                    bands_and_channels[band['band']] = set(band['channels'])
-                else:
-                    bands_and_channels[band['band']].union(band['channels'])
 
+    def _config_ap(self, ap, band, channel):
+        """ Configures an AP for the test.
+
+        @param ap: an APConfigurator object.
+        @param band: a string, 2.4GHz or 5GHz.
+        @param channel: an integer.
+
+        @returns a dict representing one band of a configured AP.
+        """
+        # Setting the band gets you the bss
+        ap.set_band(band)
+        ssid = '_'.join([ap.get_router_short_name(),
+                         str(channel),
+                         str(band).replace('.', '_')])
+        logging.info('Using ssid %s', ssid)
+
+        ap.power_up_router()
+        ap.set_channel(channel)
+        ap.set_radio(enabled=True)
+        ap.set_ssid(ssid)
+        ap.set_visibility(visible=True)
+        # Testing open system, i.e. no security
+        ap.set_security_disabled()
+        # DO NOT apply_settings() here. Cartridge is used to apply config
+        # settings to multiple APs in parallel, see _config_all_aps().
+
+        return {
+            'bss': ap.get_bss(),
+            'band': band,
+            'channel': channel,
+            'frequency': ChaosAP.FREQUENCY_TABLE[channel],
+            'radio': True,
+            'ssid': ssid,
+            'visibility': True,
+            'security': None,
+            }
+
+
+    def _config_all_aps(self, all_aps):
+        """ Configures all APs in Chaos lab.
+
+        @param all_aps: a list of APConfigurator objects, returned by factory.
+
+        @returns a list of dicts, each a return by _config_ap().
+        """
         bands = [all_aps[0].band_2ghz, all_aps[0].band_5ghz]
+        # TODO(tgao): support passing in channel params?
         channels = [5, 48]
+        configured_aps = []
+
         cartridge = ap_cartridge.APCartridge()
-        for band, channel in zip(bands, channels):
-            logging.info('Testing band %s and channel %s' % (band, channel))
-            configured_aps = []
-            for ap in all_aps:
-                if self.supported_band_and_channel(ap, band, channel):
-                    # Setting the band gets you the bss
-                    ap.set_band(band)
-                    ap_info = {
-                        'bss': ap.get_bss(),
-                        'band': band,
-                        'channel': channel,
-                        'frequency': ChaosAP.FREQUENCY_TABLE[channel],
-                        'radio': True,
-                        'ssid': '_'.join([ap.get_router_short_name(),
-                                          str(channel),
-                                          str(band).replace('.', '_')]),
-                        'visibility': True,
-                        'security': None,
-                    }
+        for ap in all_aps:
+            for band, channel in zip(bands, channels):
+                if not self.supported_band_and_channel(ap, band, channel):
+                    continue
+                logging.debug('AP %s supports band %s and channel %s',
+                              ap.get_bss(), band, channel)
+                ap_info = self._config_ap(ap, band, channel)
+                configured_aps.append(ap_info)
+                cartridge.push_configurator(ap)
 
-                    logging.info('Using ssid %s', ap_info['ssid'])
-                    ap.power_up_router()
-                    ap.set_channel(ap_info['channel'])
-                    ap.set_radio(enabled=ap_info['radio'])
-                    ap.set_ssid(ap_info['ssid'])
-                    ap.set_visibility(visible=ap_info['visibility'])
-                    ap.set_security_disabled()
-                    configured_aps.append(ap_info)
-                    cartridge.push_configurator(ap)
-            cartridge.run_configurators()
+        # Apply config settings to all APs in parallel.
+        cartridge.run_configurators()
+        return configured_aps
 
-            for ap in configured_aps:
-                logging.info('Client connecting to ssid %s' % ap['ssid'])
-                ap_info['failed_iterations'] = []
-                failure = False
-                for iteration in range(tries):
-                    logging.info('Connection try %d' % (iteration + 1))
-                    resp = self.run_connect_disconnect_test(ap, iteration)
-                    if resp:
-                        failure = True
-                        ap_info['failed_connections'].append({'error': resp,
-                                                             'try': iteration})
-                if failure:
-                    self.error_list.append(ap_info)
+
+    def _deconfig_all_aps(self, all_aps):
+        """ Powers down the APs used for a test run.
+
+        @param all_aps: a list of APConfigurator objects, returned by factory.
+        """
+        cartridge = ap_cartridge.APCartridge()
+        for ap in all_aps:
+            ap.power_down_router()
+            cartridge.push_configurator(ap)
+
+        cartridge.run_configurators()
 
 
     def run_once(self, tries=1):
+        """ Entry point of this test.
+
+        @param tries: an integer, number of times to connect/disconnect.
+        """
         if download_chromium_prebuilt.download_chromium_prebuilt_binaries():
             raise error.TestError('The binaries were just downloaded.  Please '
                                   'run: (outside-chroot) <path to chroot tmp '
@@ -150,11 +187,12 @@ class network_WiFiSimpleConnectionServer(test.test):
         self.client_at.install()
         factory = ap_configurator_factory.APConfiguratorFactory()
         all_aps = factory.get_ap_configurators()
-        self.loop_ap_configs_and_test(all_aps, tries)
-        logging.info('Client test complete, powering down router')
-        for ap in all_aps:
-            ap.power_down_router()
-            ap.apply_settings()
+        configured_aps = self._config_all_aps(all_aps)
+        for ap_info in configured_aps:
+            self._run_ap_test(ap_info, tries)
+
+        logging.info('Client test complete, powering down routers.')
+        self._deconfig_all_aps(all_aps)
 
         # Test failed if any of the intermediate tests failed.
         if self.error_list:
