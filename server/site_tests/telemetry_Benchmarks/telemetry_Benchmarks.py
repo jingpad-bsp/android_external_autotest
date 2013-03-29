8bb2e3d514e545751628372f18f2fd33d3285e75
Simran Basi <sbasi@chromium.org>
2013-03-29 10:09:11 -0700

    Perf Testing: Add more telemetry benchmarks to the lab.
    Added a bunch of telemetry benchmarks to the perf_v2 suite.
    
    * Dom Benchmark (ran through page_cycler)
    * Kraken Benchmark
    * RoboHornet Benchmark
    * Sunspider Benchmark
    * Dromaeo Benchmarks
    
    BUG=chromium:223997,chromium:224000,chromium:223998,chromium:223996,
    chromium:223995
    TEST=Ran all new tests locally
    
    Change-Id: I5bc8cb568161d73dd9dcf1266e347104234e1245
    Reviewed-on: https://gerrit.chromium.org/gerrit/46895
    Tested-by: Simran Basi <sbasi@chromium.org>
    Reviewed-by: Richard Barnette <jrbarnette@chromium.org>
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Reviewed-by: Dennis Jeffrey <dennisjeffrey@chromium.org>
    Reviewed-by: Simran Basi <sbasi@chromium.org>
    Commit-Queue: Simran Basi <sbasi@chromium.org>

---
+++ server/site_tests/telemetry_Benchmarks/telemetry_Benchmarks.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.client.common_lib import error
+from autotest_lib.server import test
+from autotest_lib.server.cros import telemetry_runner
+
+
+class telemetry_Benchmarks(test.test):
+    """Run a telemetry benchmark."""
+    version = 1
+
+
+    def run_once(self, host=None, benchmark=None, page_sets=None):
+        """Run a telemetry benchmark over a given list of page_sets.
+
+        @param host: host we are running telemetry on.
+        @param benchmark: benchmark we want to run.
+        @param page_sets: list of page sets we want to use with this benchmark.
+        """
+        failed_page_sets = []
+        warned_page_sets = []
+        telemetry = telemetry_runner.TelemetryRunner(host)
+        for page_set in page_sets:
+            try:
+                telemetry.run_telemetry_benchmark(benchmark,
+                                                  page_set,
+                                                  keyval_writer=self)
+            except error.TestWarn:
+                logging.warn('%s exited with warning for page_set: %s',
+                             benchmark, page_set)
+                warned_page_sets.append(page_set)
+            except error.TestFail:
+                logging.error('%s failed for page_set: %s', benchmark,
+                              page_set)
+                failed_page_sets.append(page_set)
+        if failed_page_sets:
+            raise error.TestFail('%s failed for page_sets: %s. And warned'
+                                 ' for page_sets: %s.', benchmark,
+                                 failed_page_sets, warned_page_sets)
+        if warned_page_sets:
+            raise error.TestWarn('%s exited with warnings for page_sets:'
+                                 ' %s.', benchmark, warned_page_sets)
