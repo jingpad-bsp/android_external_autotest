b2ef090c3504927ae0d7a20000316065d901fe24
Darren Krahn <dkrahn@chromium.org>
2013-04-09 18:25:17 -0700

    Updated the test PCA URL and added a name for the certified key.
    BUG=None
    TEST=Ran the test
    
    Change-Id: I8ad72e9974a8de2f097163894fee228450240ee9
    Reviewed-on: https://gerrit.chromium.org/gerrit/51193
    Reviewed-by: David Yu <davidyu@chromium.org>
    Tested-by: Darren Krahn <dkrahn@chromium.org>
    Commit-Queue: Darren Krahn <dkrahn@chromium.org>

--- client/site_tests/platform_Attestation/platform_Attestation.py
+++ client/site_tests/platform_Attestation/platform_Attestation.py
@@ -18,7 +18,7 @@ class platform_Attestation(test.test):
                      ' --data-binary "@/tmp/__attestation_enroll_request"' +
                      ' -o "/tmp/__attestation_enroll_response"' +
                      ' -H "Content-Type: application/octet-stream"' +
-                     ' https://chromeos-pca.appspot.com/enroll')
+                     ' https://chromeos-ca.gstatic.com/enroll')
         utils.system(cryptohome.CRYPTOHOME_CMD +
                      ' --action=tpm_attestation_finish_enroll' +
                      ' --file=/tmp/__attestation_enroll_response')
@@ -30,10 +30,11 @@ class platform_Attestation(test.test):
         utils.system('curl --data-binary "@/tmp/__attestation_cert_request"' +
                      ' -o "/tmp/__attestation_cert_response"' +
                      ' -H "Content-Type: application/octet-stream"' +
-                     ' https://chromeos-pca.appspot.com/sign')
+                     ' https://chromeos-ca.gstatic.com/sign')
         utils.system(cryptohome.CRYPTOHOME_CMD +
                      ' --action=tpm_attestation_finish_cert_request' +
-                     ' --file=/tmp/__attestation_cert_response')
+                     ' --file=/tmp/__attestation_cert_response' +
+                     ' --name=test')
 
     def run_once(self):
         status = cryptohome.get_tpm_attestation_status()
