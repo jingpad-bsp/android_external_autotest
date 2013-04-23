0f2e71a2895caa6af08ed9d41531b3b2384cf4b8
Deepak Gopal <deepakg@chromium.org>
2013-04-23 14:34:36 -0700

    Netgear R6200 gets its own _set_mode
    TEST=unittest
    BUG=chromium:234005, chromium:232026
    
    Change-Id: Ia590ceae42ae0d8a9d49670098b8fbc615a04bcb
    Reviewed-on: https://gerrit.chromium.org/gerrit/48976
    Reviewed-by: Kris Rambish <krisr@chromium.org>
    Commit-Queue: Deepak Gopal <deepakg@chromium.org>
    Tested-by: Deepak Gopal <deepakg@chromium.org>

--- server/cros/chaos_ap_configurators/netgearR6200_ap_configurator.py
+++ server/cros/chaos_ap_configurators/netgearR6200_ap_configurator.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 
 import netgear_WNDR_dual_band_configurator
+import logging
 
 
 class NetgearR6200APConfigurator(netgear_WNDR_dual_band_configurator.
@@ -10,9 +11,23 @@ class NetgearR6200APConfigurator(netgear_WNDR_dual_band_configurator.
     """Derived class to control Netgear R6200 router."""
 
 
+    def _alert_handler(self, alert):
+        """Checks for any modal dialogs which popup to alert the user and
+        either raises a RuntimeError or ignores the alert.
+
+        @params alert: The modal dialog's contents.
+        """
+        super(NetgearR6200APConfigurator, self)._alert_handler(alert)
+        text = alert.text
+        if 'WPS requires SSID broadcasting in order to work' in text:
+            alert.accept()
+
+
     def get_supported_modes(self):
-        return [{'band': self.band_5ghz, 'modes': [self.mode_g, self.mode_n]},
-                {'band': self.band_2ghz, 'modes': [self.mode_g, self.mode_n]}]
+        return [{'band': self.band_2ghz, 'modes': [self.mode_b, self.mode_g,
+                                                   self.mode_n]},
+                {'band': self.band_5ghz, 'modes': [self.mode_g, self.mode_a,
+                                                   self.mode_n]}]
 
 
     def get_supported_bands(self):
@@ -45,10 +60,35 @@ class NetgearR6200APConfigurator(netgear_WNDR_dual_band_configurator.
                                              xpath)
 
 
+    def _set_mode(self, mode, band=None):
+        router_mode = None
+        xpath = '//select[@name="opmode"]'
+        if self.current_band == self.band_2ghz:
+            if mode == self.mode_b:
+                router_mode = 'Up to 54 Mbps'
+            elif mode == self.mode_g:
+                router_mode = 'Up to 145 Mbps'
+            elif mode == self.mode_n:
+                router_mode = 'Up to 300 Mbps'
+        elif self.current_band == self.band_5ghz:
+            xpath = '//select[@name="opmode_an"]'
+            if mode == self.mode_g:
+                router_mode = 'Up to 173 Mbps'
+            elif mode == self.mode_a:
+                router_mode = 'Up to 400 Mbps'
+            elif mode == self.mode_n:
+                router_mode = 'Up to 867 Mbps'
+        if not router_mode:
+            raise RuntimeException('You selected a mode that is not assigned '
+                                   'to this router. Select either b, g or n '
+                                   'for 2.4Ghz or either g, a or n for 5Ghz.')
+        self.select_item_from_popup_by_xpath(router_mode, xpath)
+
+
     def set_security_wep(self, key_value, authentication):
         if self.current_band == self.band_5ghz:
-            logging.debug('Cannot set WEP \
-                          security for 5GHz band in Netgear R6200 router.')
+            logging.debug('Cannot set WEP security for 5GHz band in Netgear '
+                          'R6200 router.')
             return None
         super(NetgearR6200APConfigurator, self).set_security_wep(
         key_value, authentication)
