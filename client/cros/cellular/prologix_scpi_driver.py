d34edf26ab8171d0ad944293d2a0439fa1f1b1b4
Ben Chan <benchan@chromium.org>
2013-04-29 12:59:26 -0700

    cellular: Fix indentation issues in scpi interface code.
    BUG=chromium:236512
    TEST=`pylint --rcfile ../../../utils/pylintrc` reports no indentation issues.
    
    Change-Id: I3e73e06f8deea302c20d95b082613d3fec849602
    Reviewed-on: https://gerrit.chromium.org/gerrit/49507
    Reviewed-by: Byron Kubert <byronk@chromium.org>
    Commit-Queue: Ben Chan <benchan@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>

--- client/cros/cellular/prologix_scpi_driver.py
+++ client/cros/cellular/prologix_scpi_driver.py
@@ -9,178 +9,189 @@ import socket
 
 
 class PrologixScpiDriver:
-  """Wrapper for a Prologix TCP<->GPIB bridge.
-  http://prologix.biz/gpib-ethernet-controller.html
-  http://prologix.biz/index.php?dispatch=attachments.getfile&attachment_id=1
+    """Wrapper for a Prologix TCP<->GPIB bridge.
+    http://prologix.biz/gpib-ethernet-controller.html
+    http://prologix.biz/index.php?dispatch=attachments.getfile&attachment_id=1
 
-  Communication is over a plain TCP stream on port 1234.  Commands to
-  the bridge are in-band, prefixed with ++.
+    Communication is over a plain TCP stream on port 1234.  Commands to
+    the bridge are in-band, prefixed with ++.
 
-  Notable instance variables include:
+    Notable instance variables include:
 
-    self.auto: When 1, the bridge automatically addresses the target
-      in listen mode.  When 0, we must issue a ++read after every
-      query.  As of Aug '11, something between us and the Agilent 8960
-      is wrong such that running in auto=0 mode leaves us hanging if
-      we issue '*RST;*OPC?'
-  """
-
-  def __init__(self, hostname, port=1234, gpib_address=14,
-               read_timeout_seconds=30, connect_timeout_seconds=5):
-    """Constructs a wrapper for the Prologix TCP<->GPIB bridge :
-    Arguments:
-        hostname: hostname of prologix device
-        port: port number
-        gpib_address: initial GPIB device to connect to
-        read_timeout_seconds: the read time out for the socket to the prologix
-            box
-        connect_timeout_seconds: the read time out for the socket to the prologix
-            box
+      self.auto: When 1, the bridge automatically addresses the target
+        in listen mode.  When 0, we must issue a ++read after every
+        query.  As of Aug '11, something between us and the Agilent 8960
+        is wrong such that running in auto=0 mode leaves us hanging if
+        we issue '*RST;*OPC?'
     """
-    self.scpi_logger = logging.getLogger('prologix')
-    self.scpi_logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    s = 'IP:%s GPIB:%s: ' % (hostname, gpib_address)
-    formatter = logging.Formatter('%(asctime)s %(filename)s %(lineno)d ' + s +
-                                  ' %(name)s - %(message)s')
-    ch.setFormatter(formatter)
-    #replaces all handlers with this one
-    #Otherwise, on each creation of this object type, we add one more handler.
-    #This setup could be done outside the class, but then it runs at import time.
-    #and we need the hostname and GPIB address.
-    self.scpi_logger.handlers = [ch]
-    #don't pass this up to other loggers. True here will print duplicates to the log
-    self.scpi_logger.propagate = False
 
-    self.socket = connect_to_port(hostname, port, connect_timeout_seconds)
-    self.read_timeout_seconds = read_timeout_seconds
-    self.socket.setblocking(0)
-    self.SetAuto(1)
-    #we may need this later.
-    #self._AddCarrigeReturnsToResponses()
-    self.SetGpibAddress(gpib_address)
-    self.scpi_logger.debug('set read_timeout_seconds: %s ' %
-                            self.read_timeout_seconds)
+    def __init__(self, hostname, port=1234, gpib_address=14,
+                 read_timeout_seconds=30, connect_timeout_seconds=5):
+        """Constructs a wrapper for the Prologix TCP<->GPIB bridge :
+        Arguments:
+            hostname: hostname of prologix device
+            port: port number
+            gpib_address: initial GPIB device to connect to
+            read_timeout_seconds: the read time out for the socket to the
+                prologix box
+            connect_timeout_seconds: the read time out for the socket to the
+                prologix box
+        """
+        self.scpi_logger = logging.getLogger('prologix')
+        self.scpi_logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        s = 'IP:%s GPIB:%s: ' % (hostname, gpib_address)
+        formatter = logging.Formatter('%(asctime)s %(filename)s %(lineno)d ' +
+                                      s + ' %(name)s - %(message)s')
+        ch.setFormatter(formatter)
+        # Replaces all handlers with this one. Otherwise, on each creation of
+        # this object type, we add one more handler. This setup could be done
+        # outside the class, but then it runs at import time. And we need the
+        # hostname and GPIB address.
+        self.scpi_logger.handlers = [ch]
+        # Don't pass this up to other loggers. True here will print duplicates
+        # to the log.
+        self.scpi_logger.propagate = False
 
-  def __del__(self):
-    self.Close()
+        self.socket = connect_to_port(hostname, port, connect_timeout_seconds)
+        self.read_timeout_seconds = read_timeout_seconds
+        self.socket.setblocking(0)
+        self.SetAuto(1)
+        # We may need this later.
+        #self._AddCarrigeReturnsToResponses()
+        self.SetGpibAddress(gpib_address)
+        self.scpi_logger.debug('set read_timeout_seconds: %s ' %
+                               self.read_timeout_seconds)
 
-  # This function is commented for now. The PXT may need to the Prologix
-  # adapter to append a newline to each GPIB response.
-  # def _AddCarrigeReturnsToResponses(self):
-  #   """
-  #   Have the prologix box add a line feed to each response.
-  #   Some instruments may need this.
-  #   """
-  #   pass
-  #   self.Send('++eot_enable 1')
-  #   self.Send('++eot_char 10')
+    def __del__(self):
+        self.Close()
 
-  def SetAuto(self, auto):
-    """Controls Prologix read-after-write (aka 'auto') mode."""
-    self.auto = int(auto)       # Must be an int so we can send it as
-    # an arg to ++auto
-    self.Send('++auto %d' % self.auto)
+#   This function is commented for now. The PXT may need to the Prologix
+#   adapter to append a newline to each GPIB response.
+#   def _AddCarrigeReturnsToResponses(self):
+#     """
+#     Have the prologix box add a line feed to each response.
+#     Some instruments may need this.
+#     """
+#     pass
+#     self.Send('++eot_enable 1')
+#     self.Send('++eot_char 10')
 
-  def Close(self):
-    """Close the socket"""
-    try:
-      self.socket.close()
-    except AttributeError:  # maybe we close before we finish building.
-      pass
+    def SetAuto(self, auto):
+        """Controls Prologix read-after-write (aka 'auto') mode."""
+        # Must be an int so we can send it as an arg to ++auto.
+        self.auto = int(auto)
+        self.Send('++auto %d' % self.auto)
 
-  def SetGpibAddress(self, gpib_address):
-    max_tries = 10
-    while max_tries > 0:
-      max_tries -= 1
-      self.Send('++addr %s' % gpib_address)
-      read_back_value = self._DirectQuery('++addr')
-      try:
-        if (int(read_back_value) == int(gpib_address)):
-          break
-      except ValueError: # If we read a string, don't raise, just try again
-        pass
-      self.scpi_logger.error('Set gpib addr to: %s, read back: %s' %
-                             (gpib_address, read_back_value))
-      self.scpi_logger.error('Setting the GPIB address failed. Trying again..')
+    def Close(self):
+        """Closes the socket."""
+        try:
+            self.socket.close()
+        except AttributeError:  # Maybe we close before we finish building.
+            pass
 
-  def Send(self, command):
-    self.scpi_logger.info('] %s', command)
-    try:
-      self.socket.send(command + '\n')
-    except Exception as e:
-      self.scpi_logger.error('sending SCPI command %s failed. ' % command)
-      self.scpi_logger.exception(e)
-      raise SystemError('Sending SCPI command failed. Instrument stopped talking?')
+    def SetGpibAddress(self, gpib_address):
+        max_tries = 10
+        while max_tries > 0:
+            max_tries -= 1
+            self.Send('++addr %s' % gpib_address)
+            read_back_value = self._DirectQuery('++addr')
+            try:
+                if (int(read_back_value) == int(gpib_address)):
+                    break
+            except ValueError:
+                # If we read a string, don't raise, just try again.
+                pass
+            self.scpi_logger.error('Set gpib addr to: %s, read back: %s' %
+                                   (gpib_address, read_back_value))
+            self.scpi_logger.error('Setting the GPIB address failed. ' +
+                                   'Trying again...')
 
-  def Reset(self):
-    """Sends a standard SCPI reset and waits for it to complete."""
-    # There is some misinteraction between the devices such that if we
-    # send *RST and *OPC? and then manually query with ++read,
-    # occasionally that ++read doesn't come back.  We currently depend
-    # on self.Query to turn on Prologix auto mode to avoid this
-    self.Send('*RST')
-    self.Query('*OPC?')
+    def Send(self, command):
+        self.scpi_logger.info('] %s', command)
+        try:
+            self.socket.send(command + '\n')
+        except Exception as e:
+            self.scpi_logger.error('sending SCPI command %s failed. ' %
+                                   command)
+            self.scpi_logger.exception(e)
+            raise SystemError('Sending SCPI command failed. ' +
+                              'Instrument stopped talking?')
 
-  def Read(self):
-    """Read a response from the bridge."""
-    try:
-      ready = select.select([self.socket], [], [], self.read_timeout_seconds)
-    except Exception as e:
-      self.scpi_logger.exception(e)
-      s = 'Read from the instrument failed. Timeout:%s' % self.read_timeout_seconds
-      self.scpi_logger.error(s)
-      raise SystemError(s)
+    def Reset(self):
+        """Sends a standard SCPI reset and waits for it to complete."""
+        # There is some misinteraction between the devices such that if we
+        # send *RST and *OPC? and then manually query with ++read,
+        # occasionally that ++read doesn't come back.  We currently depend
+        # on self.Query to turn on Prologix auto mode to avoid this
+        self.Send('*RST')
+        self.Query('*OPC?')
 
-    if ready[0]:
-      response = self.socket.recv(4096)
-      response = response.rstrip()
-      self.scpi_logger.info('[ %s', response)
-      return response
-    else:
-      self.Close()
-      s = 'Connection to the prologix adapter worked.' \
-          'But there was not data to read from the instrument.' \
-          'Does that command return a result?' \
-          'Bad GPIB port number, or timeout too short?'
-    raise SystemError(s)
+    def Read(self):
+        """Read a response from the bridge."""
+        try:
+            ready = select.select([self.socket], [], [],
+                                  self.read_timeout_seconds)
+        except Exception as e:
+            self.scpi_logger.exception(e)
+            s = 'Read from the instrument failed. Timeout:%s' % \
+                self.read_timeout_seconds
+            self.scpi_logger.error(s)
+            raise SystemError(s)
 
-  def Query(self, command):
-    """Send a GPIB command and return the response."""
-    #self.SetAuto(1) #maybe useful?
-    self.Send(command)
-    if not self.auto:
-      self.Send('++read eoi')
-    output = self.Read()
-    #self.SetAuto(0) #maybe useful?
-    return output
+        if ready[0]:
+            response = self.socket.recv(4096)
+            response = response.rstrip()
+            self.scpi_logger.info('[ %s', response)
+            return response
+        else:
+            self.Close()
+            s = 'Connection to the prologix adapter worked.' \
+                'But there was not data to read from the instrument.' \
+                'Does that command return a result?' \
+                'Bad GPIB port number, or timeout too short?'
+        raise SystemError(s)
 
-  def _DirectQuery(self, command):
-    """Sends a query to the prologix (do not send ++read), return response."""
-    self.Send(command)
-    return self.Read()
+    def Query(self, command):
+        """Send a GPIB command and return the response."""
+        #self.SetAuto(1) #maybe useful?
+        self.Send(command)
+        if not self.auto:
+            self.Send('++read eoi')
+        output = self.Read()
+        #self.SetAuto(0) #maybe useful?
+        return output
+
+    def _DirectQuery(self, command):
+        """Sends a query to the prologix (do not send ++read).
+
+        Returns: response of the query.
+        """
+        self.Send(command)
+        return self.Read()
 
 
 def connect_to_port(hostname, port, connect_timeout_seconds):
-  # Right out of the python documentation,
-  #  http://docs.python.org/library/socket.html
-  for res in socket.getaddrinfo(
-          hostname, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
-    af, socktype, proto, canonname, sa = res
-    try:
-      s = socket.socket(af, socktype, proto)
-    except socket.error as msg:
-      raise SystemError('Failed to make a new socket object. ' + str(msg))
-    try:
-      s.settimeout(connect_timeout_seconds)
-      s.connect(sa)
-    except socket.error as msg:
-      try:
-        s.close()
-      except Exception:
-        pass  # try to close it, but it may not have been created.
-      temp_string_var = ' Could be bad IP address. Tried: %s : %s' %  \
-                                                        (hostname, port)
-      raise SystemError(str(msg) + temp_string_var)
-  return s
+    # Right out of the python documentation,
+    #  http://docs.python.org/library/socket.html
+    for res in socket.getaddrinfo(
+                hostname, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        af, socktype, proto, _, sa = res
+        try:
+            s = socket.socket(af, socktype, proto)
+        except socket.error as msg:
+            raise SystemError('Failed to make a new socket object. ' +
+                              str(msg))
+        try:
+            s.settimeout(connect_timeout_seconds)
+            s.connect(sa)
+        except socket.error as msg:
+            try:
+                s.close()
+            except Exception:
+                pass  # Try to close it, but it may not have been created.
+            temp_string_var = ' Could be bad IP address. Tried: %s : %s' % \
+                              (hostname, port)
+            raise SystemError(str(msg) + temp_string_var)
+    return s
--- client/cros/cellular/scpi.py
+++ client/cros/cellular/scpi.py
@@ -5,7 +5,7 @@
 
 
 class Error(Exception):
-  pass
+    pass
 
 import logging
 log = logging.getLogger('scpi_driver')
@@ -51,98 +51,103 @@ class _ErrorCheckerContext(object):
 
 
 class Scpi(object):
-  """Wrapper for SCPI.
+    """Wrapper for SCPI.
 
-  SCPI = "standard commands for programmable instruments",
-  a relative of GPIB.
+    SCPI = "standard commands for programmable instruments",
+    a relative of GPIB.
 
-  The SCPI driver must export:  Query, Send, Reset and Close
-  """
-
-  def __init__(self, driver, opc_on_stanza = False):
-    self.driver = driver
-    self.opc_on_stanza = opc_on_stanza
-    self.checker_context = _ErrorCheckerContext(self)
-
-  def Query(self, command):
-    """Send the SCPI command and return the response."""
-    response = self.driver.Query(command)
-    return response
-
-  def Send(self, command):
-    """Send the SCPI command."""
-    self.driver.Send(command)
-
-  def Reset(self):
-    """Tell the device to reset with *RST."""
-    # Some devices (like the prologix) require special handling for
-    # reset.
-    self.driver.Reset()
-
-  def Close(self):
-    """Close the device."""
-    self.driver.Close()
-
-  def RetrieveErrors(self):
-    """Retrieves all SYSTem:ERRor messages from the device."""
-    errors = []
-    while True:
-      error = self.Query('SYSTem:ERRor?')
-      if '+0,"No error"' in error:
-        # We've reached the end of the error stack
-        break
-      if '-420' in error and 'Query UNTERMINATED' in error:
-        # This is benign; the GPIB bridge asked for a response when
-        # the device didn't have one to give.
-
-        # TODO(rochberg): This is a layering violation; we should
-        # really only accept -420 if the underlying driver is in a
-        # mode that is known to cause this
-        continue
-      if '+292' in error and 'Data arrived on unknown SAPI' in error:
-        # This may be benign; It is known to occur when we do a switch
-        # from GPRS to WCDMA
-        continue
-
-      else:
-        errors.append(error)
-    self.Send('*CLS')           # Clear status
-    errors.reverse()
-    return errors
-
-  def _WaitAndFetchErrors(self, raise_on_error = True):
-    """Waits for command completion, returns errors."""
-    self.Query('*OPC?')      # Wait for operation complete
-    errors = self.RetrieveErrors()
-    if errors and raise_on_error:
-      raise Error('\n'.join(errors))
-    return errors
-
-  def SimpleVerify(self, command, arg):
-    """Sends "command arg", then "command?", expecting arg back.
-
-    Arguments:
-      command: SCPI command
-      arg: Argument.  We currently check for exact equality: you should
-        send strings quoted with " because that's what the 8960 returns.
-        We also fail if you send 1 and receive +1 back.
-
-    Raises:
-      Error:  Verification failed
+    The SCPI driver must export:  Query, Send, Reset and Close
     """
-    self.always_check = False
-    with self.checker_context:
-        self.Send('%s %s' % (command, arg))
-        result = self.Query('%s?' % (command,))
-        if result != arg:
-          raise Error('Error on %s: sent %s, got %s' % (command, arg, result))
 
-  def SendStanza(self, commands):
-    """Sends a list of commands, checks to see that they complete correctly."""
-    with self.checker_context:
-        for c in commands:
-          if self.opc_on_stanza:
-            self.Send(c)
-            self.Query('*OPC?')
-          else:
-            self.Send(c)
+    def __init__(self, driver, opc_on_stanza = False):
+        self.driver = driver
+        self.opc_on_stanza = opc_on_stanza
+        self.checker_context = _ErrorCheckerContext(self)
+
+    def Query(self, command):
+        """Send the SCPI command and return the response."""
+        response = self.driver.Query(command)
+        return response
+
+    def Send(self, command):
+        """Send the SCPI command."""
+        self.driver.Send(command)
+
+    def Reset(self):
+        """Tell the device to reset with *RST."""
+        # Some devices (like the prologix) require special handling for
+        # reset.
+        self.driver.Reset()
+
+    def Close(self):
+        """Close the device."""
+        self.driver.Close()
+
+    def RetrieveErrors(self):
+        """Retrieves all SYSTem:ERRor messages from the device."""
+        errors = []
+        while True:
+            error = self.Query('SYSTem:ERRor?')
+            if '+0,"No error"' in error:
+                # We've reached the end of the error stack
+                break
+
+            if '-420' in error and 'Query UNTERMINATED' in error:
+                # This is benign; the GPIB bridge asked for a response when
+                # the device didn't have one to give.
+
+                # TODO(rochberg): This is a layering violation; we should
+                # really only accept -420 if the underlying driver is in a
+                # mode that is known to cause this
+                continue
+
+            if '+292' in error and 'Data arrived on unknown SAPI' in error:
+                # This may be benign; It is known to occur when we do a switch
+                # from GPRS to WCDMA
+                continue
+
+            errors.append(error)
+
+        self.Send('*CLS')           # Clear status
+        errors.reverse()
+        return errors
+
+    def _WaitAndFetchErrors(self, raise_on_error = True):
+        """Waits for command completion, returns errors."""
+        self.Query('*OPC?')      # Wait for operation complete
+        errors = self.RetrieveErrors()
+        if errors and raise_on_error:
+            raise Error('\n'.join(errors))
+        return errors
+
+    def SimpleVerify(self, command, arg):
+        """Sends "command arg", then "command?", expecting arg back.
+
+        Arguments:
+          command: SCPI command
+          arg: Argument.  We currently check for exact equality: you should
+            send strings quoted with " because that's what the 8960 returns.
+            We also fail if you send 1 and receive +1 back.
+
+        Raises:
+          Error:  Verification failed
+        """
+        self.always_check = False
+        with self.checker_context:
+            self.Send('%s %s' % (command, arg))
+            result = self.Query('%s?' % (command,))
+            if result != arg:
+                raise Error('Error on %s: sent %s, got %s' % (
+                            command, arg, result))
+
+    def SendStanza(self, commands):
+        """
+        Sends a list of commands and verifies that they complete correctly.
+        """
+        with self.checker_context:
+            for c in commands:
+                if self.opc_on_stanza:
+                    self.Send(c)
+                    self.Query('*OPC?')
+                else:
+                    self.Send(c)
