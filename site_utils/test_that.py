d4a0430e484047965e6e2426d84a3ffd2539b2cb
Aviv Keshet <akeshet@chromium.org>
2013-04-30 15:48:30 -0700

    [autotest] add suite-running to test_that.py
    This CL adds basic suite-running capability to test_that. A single or
    multiple suites can be specified at the command line, and run against a
    local DUT (see TEST for usage).
    
    CQ-DEPEND=CL:Id57038b001f18a589e665b4d044cfa03ccebc954
    
    BUG=chromium:236471
    TEST=From anywhere in the chroot, assuming you have run build_packages
    for the board in question:
    `test_that -b <board> <DUT> suite:smoke` # see suite run
                                             # run, suite, run
    Also, new unit tests, which pass.
    
    Change-Id: Ibdc284ab59f50c84d5deaf39e6e8c5406188b81c
    Reviewed-on: https://gerrit.chromium.org/gerrit/49990
    Tested-by: Aviv Keshet <akeshet@chromium.org>
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Commit-Queue: Aviv Keshet <akeshet@chromium.org>

--- site_utils/test_that.py
+++ site_utils/test_that.py
@@ -6,10 +6,17 @@
 import argparse
 import logging
 import os
+import re
 import subprocess
 import sys
+import tempfile
 
 import common
+from autotest_lib.client.common_lib.cros import dev_server
+from autotest_lib.server.cros.dynamic_suite import suite
+from autotest_lib.server.cros.dynamic_suite import constants
+from autotest_lib.server import autoserv_utils
+
 try:
     from chromite.lib import cros_build_lib
 except ImportError:
@@ -19,11 +26,99 @@ except ImportError:
     print '  - (not yet supported) be run after running '
     print '    ../utils/build_externals.py'
 
-# first goal: reproduce the following use case:
-# run_remote_test.sh --remote=<ip address of dut> suite:smoke
+
+def schedule_local_suite(autotest_path, suite_name, afe, build=''):
+    """
+    Schedule a suite against a mock afe object, for a local suite run.
+    @param autotest_path: Absolute path to autotest (in sysroot).
+    @param suite_name: Name of suite to schedule.
+    @param afe: afe object to schedule against (typically a directAFE)
+    @param build: Build to schedule suite for.
+    """
+    fs_getter = suite.Suite.create_fs_getter(autotest_path)
+    devserver = dev_server.ImageServer('')
+    my_suite = suite.Suite.create_from_name(suite_name, build, devserver,
+                                            fs_getter, afe, ignore_deps=True)
+    if len(my_suite.tests) == 0:
+        raise ValueError('Suite named %s does not exist, or contains no '
+                         'tests.' % suite_name)
+    my_suite.schedule(lambda x: None) # Schedule tests, discard record calls.
 
 
-def ValidateArguments(arguments):
+def schedule_local_test(autotest_path, suite_name, afe, build=''):
+    #temporarily disabling pylint
+    #pylint: disable-msg=C0111
+    """
+    Schedule an individual test against a mock afe object, for a local run.
+
+    NOT YET IMPLEMENTED
+    """
+    pass
+
+
+def run_job(job, host, sysroot_autotest_path):
+    """
+    Shell out to autoserv to run an individual test job.
+
+    @param job: A Job object containing the control file contents and other
+                relevent metadata for this test.
+    @param host: Hostname of DUT to run test against.
+    @param sysroot_autotest_path: Absolute path of autotest directory.
+    """
+    with tempfile.NamedTemporaryFile() as temp_file:
+        temp_file.write(job.control_file)
+        temp_file.flush()
+
+        command = autoserv_utils.autoserv_run_job_command(
+                os.path.join(sysroot_autotest_path, 'server'),
+                machines=host, job=job, verbose=False,
+                extra_args=[temp_file.name])
+        subprocess.call(command)
+
+
+def setup_local_afe():
+    """
+    Setup a local afe database and return a direct_afe object to access it.
+
+    @returns: A autotest_lib.frontend.afe.direct_afe instance.
+    """
+    # This import statement is delayed until now rather than running at
+    # module load time, because it kicks off a local sqlite :memory: backed
+    # database, and we don't need that unless we are doing a local run.
+    from autotest_lib.frontend import setup_django_lite_environment
+    from autotest_lib.frontend.afe import direct_afe
+    return direct_afe.directAFE()
+
+
+def perform_local_run(afe, autotest_path, tests, remote, build=''):
+    """
+    @param afe: A direct_afe object used to interact with local afe database.
+    @param autotest_path: Absolute path of sysroot installed autotest.
+    @param tests: List of strings naming tests and suites to run. Suite strings
+                  should be formed like "suite:smoke".
+    @param remote: Remote hostname.
+    @param build: String specifying build for local run.
+    """
+    afe.create_label(constants.VERSION_PREFIX + build)
+    afe.create_host(remote)
+
+    # Schedule tests / suites in local afe
+    for test in tests:
+        suitematch = re.match(r'suite:(.*)', test)
+        if suitematch:
+            suitename = suitematch.group(1)
+            logging.info('Scheduling suite %s.', suitename)
+            schedule_local_suite(autotest_path, suitename, afe)
+        else:
+            logging.info('Would schedule test %s.', test)
+
+    for job in afe.get_jobs():
+        run_job(job, remote, autotest_path)
+
+    return 0
+
+
+def validate_arguments(arguments):
     """
     Validates parsed arguments.
 
@@ -37,8 +132,11 @@ def ValidateArguments(arguments):
         raise ValueError('Board autodetection not yet supported. '
                          '--board required.')
 
+    if arguments.remote == ':lab:':
+        raise ValueError('Running tests in test lab not yet supported.')
 
-def ParseArguments(argv):
+
+def parse_arguments(argv):
     """
     Parse command line arguments
 
@@ -57,11 +155,11 @@ def ParseArguments(argv):
     parser.add_argument('-b', '--board', metavar='BOARD',
                         action='store',
                         help='Board for which the test will run.')
-    parser.add_argument('-i', '--build', nargs=1, metavar='BUILD',
+    parser.add_argument('-i', '--build', metavar='BUILD',
                         help='Build to test. Device will be reimaged if '
                         'necessary. Omit flag to skip reimage and test '
                         'against already installed DUT image.')
-    parser.add_argument('--args', nargs=1, metavar='ARGS',
+    parser.add_argument('--args', metavar='ARGS',
                         help='Argument string to pass through to test.')
 
     return parser.parse_args(argv)
@@ -76,9 +174,9 @@ def main(argv):
         logging.error('Script must be invoked inside the chroot.')
         return 1
 
-    arguments = ParseArguments(argv)
+    arguments = parse_arguments(argv)
     try:
-        ValidateArguments(arguments)
+        validate_arguments(arguments)
     except ValueError as err:
         logging.error('Invalid arguments. %s', err.message)
         return 1
@@ -108,11 +206,14 @@ def main(argv):
                                       os.path.basename(realpath))
         return subprocess.call([script_command] + argv)
 
-    print 'This script does not do anything yet.'
-    print 'Reached end of execution for script %s with args %s.' % (realpath,
-                                                                    argv)
-    return 0
+    # Hard coded to True temporarily. This will eventually be parsed to false
+    # if we are doing a run in the test lab.
+    local_run = True
 
+    if local_run:
+        afe = setup_local_afe()
+        return perform_local_run(afe, sysroot_autotest_path, arguments.tests,
+                               arguments.remote)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
---
+++ site_utils/test_that_unittest.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+# pylint: disable-msg=C0111
+
+import os, unittest
+import mox
+import common
+from autotest_lib.site_utils import test_that
+
+class StartsWithList(mox.Comparator):
+    def __init__(self, start_of_list):
+        self._lhs = start_of_list
+
+    def equals(self, rhs):
+        if len(rhs)<len(self._lhs):
+            return False
+        for (x,y) in zip(self._lhs, rhs):
+            if x != y:
+                return False
+        return True
+
+class TestThatUnittests(unittest.TestCase):
+    def test_validate_arguments(self):
+        # Deferred until validate_arguments allows for lab runs.
+        pass
+
+    def test_parse_arguments(self):
+        args = test_that.parse_arguments(
+                ['-b', 'some_board', '-i', 'some_image', '--args', 'some_args',
+                 'some_remote', 'test1', 'test2'])
+        self.assertEqual('some_board', args.board)
+        self.assertEqual('some_image', args.build)
+        self.assertEqual('some_args', args.args)
+        self.assertEqual('some_remote', args.remote)
+        self.assertEqual(['test1', 'test2'], args.tests)
+
+    def test_schedule_local_suite(self):
+        # Deferred until schedule_local_suite knows about non-local builds.
+        pass
+
+    def test_schedule_local_test(self):
+        # Deferred until schedule_local_test knows about non-local builds.
+        pass
+
+    def test_run_job(self):
+        class Object():
+            pass
+
+        autotest_path = 'htap_tsetotua'
+        autoserv_command = os.path.join(autotest_path, 'server', 'autoserv')
+        remote = 'etomer'
+        self.mox = mox.Mox()
+
+        # Create some dummy job objects.
+        job1 = Object()
+        job2 = Object()
+        setattr(job1, 'control_type', 'cLiEnT')
+        setattr(job1, 'control_file', 'c1')
+        setattr(job2, 'control_type', 'Server')
+        setattr(job2, 'control_file', 'c2')
+
+        # Stub out subprocess.call, make it expect correct arguments.
+        self.mox.StubOutWithMock(test_that.subprocess, 'call')
+        test_that.subprocess.call(StartsWithList([autoserv_command, '-p', '-m',
+                                                  remote, '-c']))
+        test_that.subprocess.call(StartsWithList([autoserv_command, '-p', '-m',
+                                                  remote, '-s']))
+
+        # Test run_job.
+        self.mox.ReplayAll()
+        test_that.run_job(job1, remote, autotest_path)
+        test_that.run_job(job2, remote, autotest_path)
+        self.mox.UnsetStubs()
+        self.mox.VerifyAll()
+        self.mox.ResetAll()
+
+
+    def test_perform_local_run(self):
+        afe = test_that.setup_local_afe()
+        autotest_path = 'ottotest_path'
+        suite_name = 'sweet_name'
+        remote = 'remoat'
+        suite_control_files=['c1', 'c2', 'c3', 'c4']
+
+        def fake_suite_callback(*args):
+            for control_file in suite_control_files:
+                afe.create_job(control_file, hosts=[remote])
+
+        # Mock out scheduling of suite and running of jobs.
+        self.mox = mox.Mox()
+        self.mox.StubOutWithMock(test_that, 'schedule_local_suite')
+        test_that.schedule_local_suite(autotest_path, suite_name,
+                                     afe).WithSideEffects(fake_suite_callback)
+        self.mox.StubOutWithMock(test_that, 'run_job')
+
+        # Test perform_local_run. Enforce that run_job is called correctly.
+        for control_file in suite_control_files:
+            test_that.run_job(mox.ContainsAttributeValue('control_file',
+                                                        control_file),
+                             remote, autotest_path)
+        self.mox.ReplayAll()
+        test_that.perform_local_run(afe, autotest_path, ['suite:'+suite_name],
+                                  remote)
+        self.mox.UnsetStubs()
+        self.mox.VerifyAll()
+
+
+if __name__ == '__main__':
+    unittest.main()
