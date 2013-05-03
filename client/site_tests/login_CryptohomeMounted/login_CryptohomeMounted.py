a27c440dd47093388e1122beeb0afc94f4689111
Dennis Jeffrey <dennisjeffrey@chromium.org>
2013-05-03 13:24:31 -0700

    login_CryptohomeMounted now uses Telemetry-based login instead of PyAuto-based login.
    This change must land along with a corresponding change that removes
    login_CryptohomeMounted from the autotest-chrome ebuild, and adds it to
    the autotest-telemetry ebuild.
    
    CQ-DEPEND=CL:50061
    BUG=chromium:236453
    TEST=cbuildbot --remote --hwtest --chrome_rev=tot -g '50060 50061' lumpy-release
    
    Change-Id: Ice63460dc0532220bee70cb55863b27623bc929d
    Reviewed-on: https://gerrit.chromium.org/gerrit/50060
    Reviewed-by: Dennis Jeffrey <dennisjeffrey@chromium.org>
    Tested-by: Dennis Jeffrey <dennisjeffrey@chromium.org>
    Commit-Queue: Doug Anderson <dianders@chromium.org>

--- client/site_tests/login_CryptohomeMounted/login_CryptohomeMounted.py
+++ client/site_tests/login_CryptohomeMounted/login_CryptohomeMounted.py
@@ -5,35 +5,33 @@
 #pylint: disable-msg=C0111
 
 import os
-from autotest_lib.client.cros import constants
+
+from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros import cros_ui_test, cryptohome, login
+from autotest_lib.client.common_lib.cros import chrome
+from autotest_lib.client.cros import cryptohome, login
 
 TEST_USER = 'cryptohome_test@chromium.org'
 TEST_PASS = 'testme'
 
-class login_CryptohomeMounted(cros_ui_test.UITest):
+class login_CryptohomeMounted(test.test):
     version = 1
-
-    def initialize(self, creds='$default'):
-        try:
-            super(login_CryptohomeMounted, self).initialize(creds)
-        except Exception as err:
-            raise error.TestFailRetry(err)
 
 
     def run_once(self):
         try:
-            login.wait_for_cryptohome(self.username)
-            self.logout()
-            cryptohome.remove_vault(self.username)
+            with chrome.login() as _:
+                login.wait_for_cryptohome(chrome.LOGIN_USER)
+            cryptohome.remove_vault(chrome.LOGIN_USER)
+
             cryptohome.mount_vault(TEST_USER, TEST_PASS, create=True)
             test_file = os.path.join(cryptohome.user_path(TEST_USER), 'hello')
             open(test_file, 'w').close()
             cryptohome.unmount_vault(TEST_USER)
-            self.login()
-            login.wait_for_cryptohome(self.username)
-            self.assert_(not os.path.exists(test_file))
-        #TODO: Make this more fine-grained. See crbug.com/225542
+
+            with chrome.login() as _:
+                login.wait_for_cryptohome(chrome.LOGIN_USER)
+                self.assert_(not os.path.exists(test_file))
+        # TODO: Make this more fine-grained. See crbug.com/225542
         except Exception as err:
-            raise error.TestFailRetry(err)
+            raise error.TestFailRetry(repr(err))
