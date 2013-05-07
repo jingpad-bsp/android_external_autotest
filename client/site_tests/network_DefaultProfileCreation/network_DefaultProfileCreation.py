c4f34095f1e7bed8bd526eec8a13fa395f801d83
mukesh agrawal <quiche@chromium.org>
2013-05-07 11:11:47 -0700

    autotest (shill): add test for default profile default settings
    When creating a default profile, shill should populate some
    settings with default values. Because default profile creation
    is somewhat subtle, it's possible to break this without realizing
    it.
    
    Add an autotest so that we can catch such regressions automatically.
    
    BUG=chromium:238708
    TEST=manual
    CQ-DEPEND=CL:50481
    
    Manual test
    -----------
    - run this test on shill at revision b9c3feb: tests FAILS
    - run this test on shill at ToT: test PASSES
    
    Change-Id: Ia6902c6a54cafe45e012652d31d96043814ff626
    Reviewed-on: https://gerrit.chromium.org/gerrit/50332
    Commit-Queue: mukesh agrawal <quiche@chromium.org>
    Reviewed-by: mukesh agrawal <quiche@chromium.org>
    Tested-by: mukesh agrawal <quiche@chromium.org>

---
+++ client/site_tests/network_DefaultProfileCreation/network_DefaultProfileCreation.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib import utils
+
+# This hacks the path so that we can import shill_proxy.
+# pylint: disable=W0611
+from autotest_lib.client.cros import flimflam_test_path
+# pylint: enable=W0611
+from shill_proxy import ShillProxy
+
+class network_DefaultProfileCreation(test.test):
+    """The Default Profile Creation class.
+
+    Wipe the default profile, start shill, and check that a default
+    profile has been created.
+
+    Test that the default profile contains default values for properties
+    that should have them.
+
+    """
+    DEFAULT_PROFILE_PATH = '/var/cache/shill/default.profile'
+    EXPECTED_SETTINGS = [
+        # From DefaultProfile::LoadManagerProperties
+        'CheckPortalList=ethernet,wifi,cellular',
+        'IgnoredDNSSearchPaths=gateway.2wire.net',
+        'LinkMonitorTechnologies=wifi',
+        'PortalURL=http://www.gstatic.com/generate_204',
+        'PortalCheckInterval=30',
+        ]
+    version = 1
+
+
+    def run_once(self):
+        """Test main loop."""
+        utils.run('stop shill')
+        os.remove(self.DEFAULT_PROFILE_PATH)
+        utils.run('start shill')
+        ShillProxy.get_shill_proxy()
+
+        profile = open(self.DEFAULT_PROFILE_PATH).read()
+        for setting in self.EXPECTED_SETTINGS:
+            if setting not in profile:
+                raise error.TestFail('Missing setting ' + setting)
