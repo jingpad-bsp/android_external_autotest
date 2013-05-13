092438f0741c70465501b1b2866f8bd7a8382233
Arman Uguray <armansito@chromium.org>
2013-05-13 15:58:50 -0700

    cellular: Run network_3GStressEnable.pseudomodem against a CDMA modem.
    Modified control.pseudomodem to run the test once on 3GPP and once on
    CDMA.
    
    BUG=chromium:240424,chromium:240015
    TEST=Running network_3GStressEnable/control.pseudomodem should pass.
    
    Change-Id: Ifb1839ec3a3b453097feb96e2aadc1cdd3386b5b
    Reviewed-on: https://gerrit.chromium.org/gerrit/51064
    Commit-Queue: Arman Uguray <armansito@chromium.org>
    Reviewed-by: Arman Uguray <armansito@chromium.org>
    Tested-by: Arman Uguray <armansito@chromium.org>

--- client/site_tests/network_3GStressEnable/network_3GStressEnable.py
+++ client/site_tests/network_3GStressEnable/network_3GStressEnable.py
@@ -2,25 +2,29 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from autotest_lib.client.bin import test, utils
+from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import backchannel
+from autotest_lib.client.cros.cellular.pseudomodem import pseudomodem
 
-import logging, re, socket, string, time, urllib2
-import dbus, dbus.mainloop.glib, gobject
+import time
+import dbus, dbus.mainloop.glib
 
 from autotest_lib.client.cros import flimflam_test_path
-import flimflam, mm
-from autotest_lib.client.cros.cellular.pseudomodem import mm1, pseudomodem, sim
+import flimflam
 
 class network_3GStressEnable(test.test):
+    """
+    Stress-tests enabling and disabling a technology at short intervals.
+
+    """
     version = 1
 
     okerrors = [
         'org.chromium.flimflam.Error.InProgress'
     ]
 
-    def EnableDevice(self, enable):
+    def _enable_device(self, enable):
         try:
             if enable:
                 self.device.Enable()
@@ -32,13 +36,13 @@ class network_3GStressEnable(test.test):
             else:
                 raise error.TestFail(err)
 
-    def test(self, settle):
-        self.EnableDevice(True)
+    def _test(self, settle):
+        self._enable_device(True)
         time.sleep(settle)
-        self.EnableDevice(False)
+        self._enable_device(False)
         time.sleep(settle)
 
-    def run_once_internal(self, cycles, min, max):
+    def _run_once_internal(self, cycles, min, max):
         self.flim = flimflam.FlimFlam(dbus.SystemBus())
         self.device = self.flim.FindCellularDevice()
         if not self.device:
@@ -55,18 +59,17 @@ class network_3GStressEnable(test.test):
                 if err._dbus_error_name != 'org.chromium.flimflam.'\
                                              'Error.InvalidService':
                     raise err
-            self.EnableDevice(False)
+            self._enable_device(False)
         for t in xrange(max, min, -1):
             for n in xrange(cycles):
                 # deciseconds are an awesome unit.
                 print 'Cycle %d: %f seconds delay.' % (n, t / 10.0)
-                self.test(t / 10.0)
+                self._test(t / 10.0)
         print 'Done.'
 
-    def run_once(self, cycles=3, min=15, max=25, use_pseudomodem=False):
+    def run_once(self, cycles=3, min=15, max=25, use_pseudomodem=False,
+                 pseudomodem_family='3GPP'):
         with backchannel.Backchannel():
-            fake_sim = sim.SIM(sim.SIM.Carrier('att'),
-                mm1.MM_MODEM_ACCESS_TECHNOLOGY_GSM)
-            with pseudomodem.TestModemManagerContext(use_pseudomodem,
-                                                     sim=fake_sim):
-                self.run_once_internal(cycles, min, max)
+            with pseudomodem.TestModemManagerContext(
+                use_pseudomodem, family=pseudomodem_family):
+                self._run_once_internal(cycles, min, max)
