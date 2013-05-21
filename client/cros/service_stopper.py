5404fef84c6ac8bc6de4227136c0bc5ba390bd4d
Todd Broch <tbroch@chromium.org>
2013-05-21 07:37:33 -0700

    Revert "Fix bug in service_stopper."
    This reverts commit 8b72430b5cf2ba9918377de4149123a256b2fe89.
    
    Change-Id: I9647e3007da8f71c0bf9138c17315ed1632e68e4
    Reviewed-on: https://gerrit.chromium.org/gerrit/56024
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>

--- client/cros/service_stopper.py
+++ client/cros/service_stopper.py
@@ -43,7 +43,7 @@ class ServiceStopper(object):
         for service in self.services_to_stop:
             cmd = 'status %s' % service
             out = utils.system_output(cmd, ignore_status=True)
-            is_stopped = out.find('start/running') == -1
+            is_stopped = out.find('start/running') != 0
             if is_stopped:
                 continue
             try:
