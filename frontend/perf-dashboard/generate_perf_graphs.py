2b7f5e283fccd35f4e02116fea9f00b01d60cd89
Dennis Jeffrey <dennisjeffrey@chromium.org>
2013-05-21 15:46:12 -0700

    Upload chromeOS Telemetry perf test results for all platforms to perf dash.
    Previously, we only uploaded chromeOS Telemetry perf test results for
    the "lumpy" platform.  Now, we upload results for all platforms.
    
    By default, the perf dashboard considers platforms to be internal-only
    unless they are explicitly whitelisted by an authorized user through the
    dashboard UI.  Internal-only test results can only be viewed by people
    who are logged into the dashboard using an @google.com account.
    
    BUG=chromium:237598
    TEST=Verified that Telemetry perf results for all platforms can be uploaded
    to a local instance of the chrome perf dashboard.  Also verified that the
    results are only displayed on the dashboard when logged in to an @google.com
    account.
    
    Change-Id: Icb1de793540ed13267e78e247c668517f9135405
    Reviewed-on: https://gerrit.chromium.org/gerrit/56111
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Commit-Queue: Dennis Jeffrey <dennisjeffrey@chromium.org>
    Tested-by: Dennis Jeffrey <dennisjeffrey@chromium.org>

--- frontend/perf-dashboard/generate_perf_graphs.py
+++ frontend/perf-dashboard/generate_perf_graphs.py
@@ -250,12 +250,12 @@ def upload_to_chrome_dashboard(data_point_info, platform, test_name,
 
     """
 
-    # Start slow - only upload Telemetry results for one bot right now.
+    # Only upload Telemetry results right now (not ChromeOS-specific results).
     # TODO(dennisjeffrey): Remove the check below as soon as we're ready to
     # upload the full suite of perf results to the new dashboard.  The check is
     # only in place temporarily to allow a subset of results to be uploaded
     # until we're ready to upload everything.
-    if platform != 'lumpy' or not test_name.startswith('telemetry_Benchmarks'):
+    if not test_name.startswith('telemetry_Benchmarks'):
         return
 
     # Generate a warning and return if any expected values in |data_point_info|
