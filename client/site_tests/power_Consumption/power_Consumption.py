09e78027653cc05218fa5bb5c4b8c71bf34bd1e4
Mark Koudritsky <kamrik@chromium.org>
2013-06-11 16:50:27 -0400

    Port power_Consumption test to Telemetry
    Was pretty flaky before:
    https://wmatrix.googleplex.com/reasons?tests=power_Consumption
    
    BUG=chromium:214151
    TEST=./run_remote_tests.sh --remote=mylumpy  power_Consumption
    
    Change-Id: Ic3cdae1d0f836070a080a291636d62ba1ae3f709
    Reviewed-on: https://gerrit.chromium.org/gerrit/58231
    Commit-Queue: Mark Koudritsky <kamrik@chromium.org>
    Reviewed-by: Mark Koudritsky <kamrik@chromium.org>
    Tested-by: Mark Koudritsky <kamrik@chromium.org>

--- client/site_tests/power_Consumption/power_Consumption.py
+++ client/site_tests/power_Consumption/power_Consumption.py
@@ -6,18 +6,22 @@ import logging
 import os
 import time
 import urllib
-from autotest_lib.client.bin import utils
+
+import telemetry.core.util
+
+from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib.cros import chrome
 from autotest_lib.client.cros import backchannel
-from autotest_lib.client.cros import cros_ui, cros_ui_test
-from autotest_lib.client.cros import flimflam_test_path
+from autotest_lib.client.cros import cros_ui
+from autotest_lib.client.cros import flimflam_test_path  # Needed for flimflam
 from autotest_lib.client.cros import httpd
 from autotest_lib.client.cros import power_rapl, power_status, power_utils
 from autotest_lib.client.cros import service_stopper
 import flimflam  # Requires flimflam_test_path to be imported first.
 
 
-class power_Consumption(cros_ui_test.UITest):
+class power_Consumption(test.test):
     """Measure power consumption for different types of loads.
 
     This test runs a series of different tasks like media playback, flash
@@ -25,25 +29,13 @@ class power_Consumption(cros_ui_test.UITest):
     consumptions during each of those tasks.
     """
 
-    version = 1
+    version = 2
 
 
-    def start_authserver(self):
-        """ For some reason if we fire up the authserver, we get stuck at
-        the network selection dialog.
-        """
-        pass
-
-
-    def initialize(self, creds='$default'):
-        super(power_Consumption, self).initialize(creds=creds)
+    def initialize(self):
         self._backlight = None
         self._services = None
-
-        # Some things from pyauto module are not accessible from
-        # self.pyauto object
-        import pyauto
-        self._pyauto_module = pyauto
+        self._browser = None
 
         # Time to exclude from calculation after firing a task [seconds]
         self._stabilization_seconds = 5
@@ -56,8 +48,6 @@ class power_Consumption(cros_ui_test.UITest):
         batinfo = self._power_status.battery[0]
         self.energy_full_design = batinfo.energy_full_design
         logging.info("energy_full_design = %0.3f Wh", self.energy_full_design)
-
-        self._do_xset()
 
         # Local data and web server settings. Tarballs with traditional names
         # like *.tgz don't get copied to the image by ebuilds (see
@@ -74,6 +64,14 @@ class power_Consumption(cros_ui_test.UITest):
 
         self._test_server.run()
 
+        # Log in.
+        self._browser = chrome.logged_in_browser()
+        # Wait for login to finish and any extra windows to appear.
+        time.sleep(10)
+        self._do_xset()
+        # Most of the tests will be running in this tab.
+        self._tab = self._browser.tabs[0]
+        logging.info('initialize() finished')
 
     def _do_xset(self):
         XSET = 'LD_LIBRARY_PATH=/usr/local/lib xset'
@@ -94,7 +92,7 @@ class power_Consumption(cros_ui_test.UITest):
         """
 
         repo = 'http://commondatastorage.googleapis.com/chromeos-test-public/'
-        file_list = [repo + 'big_buck_bunny/big_buck_bunny_trailer_400p.mp4',]
+        file_list = [repo + 'big_buck_bunny/big_buck_bunny_trailer_400p.mp4', ]
         if not self.short:
             file_list += [
                 repo + 'big_buck_bunny/big_buck_bunny_trailer_400p.ogg',
@@ -102,32 +100,20 @@ class power_Consumption(cros_ui_test.UITest):
                 repo + 'big_buck_bunny/big_buck_bunny_trailer_400p.webm',
                 repo + 'big_buck_bunny/big_buck_bunny_trailer_1080p.ogg',
                 repo + 'big_buck_bunny/big_buck_bunny_trailer_1080p.webm',
-                repo + 'wikimedia/Greensleeves.ogg',]
+                repo + 'wikimedia/Greensleeves.ogg',
+                ]
 
         for url in file_list:
             logging.info('Downloading %s', url)
             utils.unmap_url('', url, self._media_dir)
 
 
-    def _toggle_fullscreen(self, expected = None):
-        """Toggle full screen mode.
-
-        Args:
-            expected: boolean, True = full screen, False = normal mode. Will
-                raise error.TestError if the actual result is different.
-        Returns:
-            True if the final state is full screen, False if normal mode.
-        Raises:
-            error.TestError if |expected| is not None and different from the
-            final state.
-        """
-
-        self.pyauto.ApplyAccelerator(self._pyauto_module.IDC_FULLSCREEN)
-        is_fullscreen = self.pyauto.GetBrowserInfo()['windows'][0]['fullscreen']
-        if expected is not None and expected != is_fullscreen:
-            raise error.TestError('_toggle_fullscreen() expected %s, got %s' %
-                (expected, is_fullscreen))
-        return is_fullscreen
+    def _toggle_fullscreen(self):
+        """Toggle full screen mode."""
+        # Note: full screen mode toggled with F11 is different from clicking the
+        # full screen icon on video player controls. This needs improvement.
+        # Bug: http://crbug.com/248939
+        cros_ui.xsystem('xdotool key F11')
 
 
     # Below are a series of generic sub-test runners. They run a given task
@@ -154,6 +140,7 @@ class power_Consumption(cros_ui_test.UITest):
         """Run command in a shell as a sub-test"""
         start_time = time.time() + self._stabilization_seconds
         for _ in xrange(repeat):
+            logging.info('Executing command: %s', cmd)
             exit_status = utils.system(cmd, ignore_status=True)
             if exit_status != 0:
                 logging.error('run_cmd: the following command terminated with'
@@ -162,20 +149,22 @@ class power_Consumption(cros_ui_test.UITest):
         return exit_status
 
 
-    def _run_until(self, name, predicate, retval=True):
+    def _run_until(self, name, predicate, timeout=60):
         """Probe the |predicate| function  and wait until it returns true.
         Record the waiting time as a sub-test
         """
         start_time = time.time() + self._stabilization_seconds
-        self.pyauto.WaitUntil(predicate, expect_retval=retval)
+        telemetry.core.util.WaitFor(predicate, timeout)
         self._plog.checkpoint(name, start_time)
 
 
     def _run_url(self, name, url, duration):
         """Navigate to URL, sleep for some time and record it as a sub-test."""
-        self.pyauto.NavigateToURL(url)
+        logging.info('Navigating to %s', url)
+        self._tab.Activate()
+        self._tab.Navigate(url)
         self._run_sleep(name, duration)
-        tab_title = self.pyauto.GetActiveTabTitle()
+        tab_title = self._tab.EvaluateJavaScript('document.title')
         logging.info('Sub-test name: %s Tab title: %s.', name, tab_title)
 
 
@@ -190,14 +179,18 @@ class power_Consumption(cros_ui_test.UITest):
             url: url to open in background tab.
             duration: number of seconds to sleep while taking measurements.
         """
-        self.pyauto.NavigateToURL(url)
+        bg_tab = self._tab
+        bg_tab.Navigate(url)
         # Let it load and settle
         time.sleep(self._stabilization_seconds / 2.)
-        tab_title = self.pyauto.GetActiveTabTitle()
+        tab_title = bg_tab.EvaluateJavaScript('document.title')
         logging.info('App name: %s Tab title: %s.', name, tab_title)
-        self.pyauto.AppendTab('about:blank')
+        # Open a new empty tab to cover the one with test payload.
+        fg_tab = self._browser.tabs.New()
+        fg_tab.Activate()
         self._run_sleep(name, duration)
-        self.pyauto.CloseTab(tab_index=1, windex=0)
+        fg_tab.Close()
+        bg_tab.Activate()
 
 
     def _run_group_download(self):
@@ -214,15 +207,17 @@ class power_Consumption(cros_ui_test.UITest):
         """Runs a series of web pages as sub-tests."""
         data_url = self._url_base + self._static_sub_dir + '/'
 
-        # URLs to be only tested in foreground tab
-        urls = [('AboutBlank', 'about:blank')]
+        # URLs to be only tested in foreground tab.
+        # Can't use about:blank here - crbug.com/248945
+        # but chrome://version is just as good for our needs.
+        urls = [('ChromeVer', 'chrome://version/')]
         # URLs to be tested in both, background and foreground modes.
         bg_urls = []
 
         more_urls = [('BallsDHTML',
                       data_url + 'balls/DHTMLBalls/dhtml.htm'),
                      ('BallsFlex',
-                      data_url + 'balls/FlexBalls/flexballs.html'),]
+                      data_url + 'balls/FlexBalls/flexballs.html'), ]
 
         if self.short:
             urls += more_urls
@@ -232,7 +227,7 @@ class power_Consumption(cros_ui_test.UITest):
                          'http://www.parapluesch.de/whiskystore/test.htm'),
                          ('PosterCircle',
                           'http://www.webkit.org'
-                          '/blog-files/3d-transforms/poster-circle.html'),]
+                          '/blog-files/3d-transforms/poster-circle.html'), ]
 
         for name, url in urls + bg_urls:
             self._run_url(name, url, duration=self._duration_secs)
@@ -248,23 +243,24 @@ class power_Consumption(cros_ui_test.UITest):
         """
 
         url = 'http://v8.googlecode.com/svn/data/benchmarks/v7/run.html'
-
-        js = """s = document.getElementById('status').textContent.substr(0,5);
-                window.domAutomationController.send(s);"""
+        js = "document.getElementById('status').textContent"
+        tab = self._tab
 
         def v8_func():
-            self.pyauto.NavigateToURL(url)
-            self.pyauto.WaitUntil(lambda: self.pyauto.ExecuteJavascript(js),
-                              expect_retval='Score')
+            """To be passed as the callable to self._run_func()"""
+            tab.Navigate(url)
+            # V8 test will usually take 17-25 seconds. Need some sleep here
+            # to let the V8 page load and create the 'status' div.
+            is_done = lambda: tab.EvaluateJavaScript(js).startswith('Score')
+            time.sleep(self._stabilization_seconds)
+            telemetry.core.util.WaitFor(is_done, 60)
 
         self._run_func('V8', v8_func, repeat=self._repeats)
 
-        # Write v8 score to log
-        js_get_score = """s = document.getElementById('status').textContent;
-                window.domAutomationController.send(s);"""
-        score = self.pyauto.ExecuteJavascript(js_get_score)
+        # Write v8 score from the last run to log
+        score = tab.EvaluateJavaScript(js)
         score = score.strip().split()[1]
-        logging.info('V8 Score: %s', score )
+        logging.info('V8 Score: %s', score)
 
 
     def _run_group_video(self):
@@ -272,17 +268,17 @@ class power_Consumption(cros_ui_test.UITest):
 
         # Note: for perf keyvals, key names are defined as VARCHAR(30) in the
         # results DB. Chars above 30 are truncated when saved to DB.
-        urls = [('vid400p_h264', 'big_buck_bunny_trailer_400p.mp4'),]
+        urls = [('vid400p_h264', 'big_buck_bunny_trailer_400p.mp4'), ]
         fullscreen_urls = []
         bg_urls = []
 
         if not self.short:
             urls += [
                 ('vid400p_ogg', 'big_buck_bunny_trailer_400p.ogg'),
-                ('vid1080_h264','big_buck_bunny_trailer_1080p.mp4'),
+                ('vid1080_h264', 'big_buck_bunny_trailer_1080p.mp4'),
                 ('vid400p_vp8', 'big_buck_bunny_trailer_400p.webm'),
-                ('vid1080_ogg','big_buck_bunny_trailer_1080p.ogg'),
-                ('vid1080_vp8','big_buck_bunny_trailer_1080p.webm'),
+                ('vid1080_ogg', 'big_buck_bunny_trailer_1080p.ogg'),
+                ('vid1080_vp8', 'big_buck_bunny_trailer_1080p.webm'),
                 ('audio', 'Greensleeves.ogg'),
                 ]
 
@@ -309,33 +305,37 @@ class power_Consumption(cros_ui_test.UITest):
         js_loop_enable = """ve = document.getElementsByTagName('video')[0];
                          ve.loop = true;
                          ve.play();
-                         window.domAutomationController.send('');
                          """
 
         for name, url in urls:
-            self.pyauto.NavigateToURL(full_url(url))
-            self.pyauto.ExecuteJavascript(js_loop_enable)
+            logging.info('Playing video %s', url)
+            self._tab.Navigate(full_url(url))
+            self._tab.ExecuteJavaScript(js_loop_enable)
             self._run_sleep(name, self._duration_secs)
 
         for name, url in fullscreen_urls:
-            self._toggle_fullscreen(expected=True)
-            self.pyauto.NavigateToURL(full_url(url))
-            self.pyauto.ExecuteJavascript(js_loop_enable)
+            self._toggle_fullscreen()
+            self._tab.Navigate(full_url(url))
+            self._tab.ExecuteJavaScript(js_loop_enable)
             self._run_sleep(name, self._duration_secs)
-            self._toggle_fullscreen(expected=False)
+            self._toggle_fullscreen()
 
         for name, url in bg_urls:
-            self.pyauto.NavigateToURL(full_url(url))
-            self.pyauto.ExecuteJavascript(js_loop_enable)
-            self.pyauto.AppendTab('about:blank')
+            logging.info('Playing video in background tab %s', url)
+            self._tab.Navigate(full_url(url))
+            self._tab.ExecuteJavaScript(js_loop_enable)
+            fg_tab = self._browser.tabs.New()
             self._run_sleep(name, self._duration_secs)
-            self.pyauto.CloseTab(tab_index=1, windex=0)
+            fg_tab.Close()
+            self._tab.Activate()
 
 
     def _run_group_sound(self):
         """Run non-UI sound test using 'speaker-test'."""
-
-        cmd = 'speaker-test -l %s -t sine -c 2' % (self._repeats * 6)
+        # For some reason speaker-test won't work on CrOS without a reasonable
+        # buffer size specified with -b.
+        # http://crbug.com/248955
+        cmd = 'speaker-test -l %s -t sine -c 2 -b 16384' % (self._repeats * 6)
         self._run_cmd('speaker_test', cmd)
 
 
@@ -347,7 +347,7 @@ class power_Consumption(cros_ui_test.UITest):
         # one rep of dd takes about 15 seconds
         root_dev = utils.system_output('rootdev -s').strip()
         cmd = 'dd if=%s of=/dev/null' % root_dev
-        self._run_cmd('dd', cmd, repeat=2*self._repeats)
+        self._run_cmd('dd', cmd, repeat=2 * self._repeats)
 
 
     def _run_group_backchannel(self):
@@ -399,7 +399,7 @@ class power_Consumption(cros_ui_test.UITest):
         """ Displays a message in the browser."""
         url = self._url_base + 'echo.html?'
         url += urllib.quote(msg)
-        self.pyauto.NavigateToURL(url)
+        self._tab.Navigate(url)
 
 
     def _run_test_groups(self, groups):
@@ -414,6 +414,7 @@ class power_Consumption(cros_ui_test.UITest):
             logging.info('Running group %s', group)
             # The _web_echo here is important for some tests (esp. non UI)
             # it gets the previous web page replaced with an almost empty one.
+            self._tab.Activate()
             self._web_echo('Running test %s' % group)
             test_func = getattr(self, '_run_group_%s' % group)
             test_func()
@@ -441,9 +442,7 @@ class power_Consumption(cros_ui_test.UITest):
                 test_groups = DEFAULT_SHORT_TESTS
             else:
                 test_groups = DEFAULT_TESTS
-
-        # Let the login complete
-        time.sleep(5)
+        logging.info('Test groups to run: %s', ', '.join(test_groups))
 
         self._services = service_stopper.ServiceStopper(
             service_stopper.ServiceStopper.POWER_DRAW_SERVICES)
@@ -459,10 +458,11 @@ class power_Consumption(cros_ui_test.UITest):
         self._plog = power_status.PowerLogger(measurements)
         self._plog.start()
 
-        # Check that we have a functioning browser and network
-        self.pyauto.NavigateToURL('http://www.google.com/')
-        if self.pyauto.GetActiveTabTitle() != 'Google':
-            raise error.TestError('Could not load www.google.com')
+        # Verify that we have a functioning browser and local web server.
+        self._tab.Activate()
+        self._web_echo("Sanity_test")
+        if self._tab.EvaluateJavaScript('document.title') != 'Test page':
+            raise error.TestError('Could not load local test page')
 
         # Video test must have the data from download test
         if ('video' in test_groups):
@@ -477,11 +477,11 @@ class power_Consumption(cros_ui_test.UITest):
         # Wrap up
         keyvals = self._plog.calc()
 
-        # Calculate expected battery life time with AboutBlank power draw
-        idle_name = 'AboutBlank_system_pwr'
+        # Calculate expected battery life time with ChromeVer power draw
+        idle_name = 'ChromeVer_system_pwr'
         if idle_name in keyvals:
             hours_life = self.energy_full_design / keyvals[idle_name]
-            keyvals['hours_battery_AboutBlank'] = hours_life
+            keyvals['hours_battery_ChromeVer'] = hours_life
 
         # Calculate a weighted power draw and battery life time. The weights
         # are intended to represent "typical" usage. Some video, some Flash ...
@@ -489,7 +489,8 @@ class power_Consumption(cros_ui_test.UITest):
         # see http://www.chromium.org/chromium-os/testing/power-testing
         weights = {'vid400p_h264_system_pwr':0.1,
                    'BallsFlex_system_pwr':0.1,
-                   'BallsDHTML_system_pwr':0.2,}
+                   'BallsDHTML_system_pwr':0.2,
+                   }
         weights[idle_name] = 1 - sum(weights.values())
 
         if set(weights).issubset(set(keyvals)):
@@ -508,6 +509,8 @@ class power_Consumption(cros_ui_test.UITest):
         except AttributeError:
             logging.debug('test_server could not be stopped in cleanup')
 
+        if self._browser:
+            self._browser.Close()
         if self._backlight:
             self._backlight.restore()
         if self._services:
