96bd6c7bd923816dbad81a3bec8b1f488d14b3fa
Luigi Semenzato <semenzato@chromium.org>
2013-06-17 14:53:05 -0700

    Verify that the TPM driver contains the "gentle shutdown" patch.
    The "gentle shutdown" patch to the TPM driver fixes (or reduces)
    a serious problem that causes a certain TPM model to go into
    self-defense mode for a long time.  Originally this test made
    sure (among other things) that the driver was installed.  Now
    it also checks that the driver running in the latest boot
    session contains the "gentle shutdown" patch.
    
    BUG=chromium:234976
    TEST=tested for both success and failure
    
    Change-Id: I5a47c3d0a993b83b3f9970753acb586d021d5d29
    Reviewed-on: https://chromium-review.googlesource.com/58920
    Reviewed-by: Darren Krahn <dkrahn@chromium.org>
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>

--- client/site_tests/kernel_TPMPing/kernel_TPMPing.py
+++ client/site_tests/kernel_TPMPing/kernel_TPMPing.py
@@ -1,17 +1,29 @@
-# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2010,2013 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os
+import logging
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 
 class kernel_TPMPing(test.test):
-  version = 1
+  """See control file for doc"""
+  version = 2
 
   def run_once(self):
-    version = utils.system_output("tpm_version")
-    if version.find("Version Info") == -1:
-      raise error.TestError("Invalid output of tpm_version:\n%s\n" % version)
+    tpm_version = utils.system_output("tpm_version")
+    if tpm_version.find("Version Info") == -1:
+      raise error.TestFail("Invalid tpm_version output:\n%s\n" % tpm_version)
     else:
-      logging.info(version)
+      logging.info(tpm_version)
+
+    # If the "gentle shutdown" string is missing from the log, we
+    # forgot to carry over an important patch.
+    tpm_device_re = "tpm_tis: 1.2 TPM (device"
+    tpm_gentle_re = "\\[gentle shutdown\\]"
+    log_file_glob = "$(echo /var/log/messages* | tac -s ' ')"
+    # We only care about the most recent instance of the TPM driver message.
+    if utils.system("grep '%s' %s | tail -1 | grep '%s'" %
+                    (tpm_device_re, log_file_glob, tpm_gentle_re),
+                    ignore_status=True) != 0:
+      raise error.TestFail("no 'gentle shutdown' TPM driver init message")
