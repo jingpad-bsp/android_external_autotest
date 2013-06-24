0f3ecf9ddb374708e8e157cdf96dc99d1e01e018
bmahadev <bmahadev@chromium.org>
2013-06-24 12:09:26 -0700

    Formating the error logs for APs that are not listed in scan
    BUG=None
    TEST=ChaosPSK
    
    Change-Id: I2b1ccddfef869e46304da3b661e8c490c7b5c586
    Reviewed-on: https://gerrit.chromium.org/gerrit/59809
    Commit-Queue: Bindu Mahadev <bmahadev@chromium.org>
    Tested-by: Bindu Mahadev <bmahadev@chromium.org>
    Reviewed-by: Bindu Mahadev <bmahadev@chromium.org>

--- server/cros/chaos_lib/chaos_base_test.py
+++ server/cros/chaos_lib/chaos_base_test.py
@@ -306,8 +306,12 @@ class WiFiChaosConnectionTest(object):
                    continue
         if len(scan_list):
             logging.error('These APs were not listed in scan:')
-            for ap in scan_list:
-                logging.error(ap)
+            for ap_info in configured_aps:
+                if ap_info['configurator'] in scan_list:
+                    logging.error('Brand:%s\n\tModel:%s\n\tSSID:%s\n'
+                                  '\tBSS:%s'.expandtabs(16),
+                                   ap_info['brand'], ap_info['model'],
+                                   ap_info['ssid'], ap_info['bss'])
                 ap.reset_command_list()
         return configured_aps
 
