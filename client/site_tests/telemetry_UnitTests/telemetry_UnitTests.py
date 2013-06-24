4c0327864e2da368da6ca4c108261840f46ab14a
Dennis Jeffrey <dennisjeffrey@chromium.org>
2013-06-24 16:49:34 -0700

    Add telemetry_UnitTests autotest to run the Telemetry unit tests.
    Also add 2 control files for the test: one to run the Telemetry unit
    tests as a regular logged-in user; and one to run the unit tests as
    guest (incognito).
    
    CQ-DEPEND=CL:60893
    BUG=chromium:252411
    TEST=./run_remote_tests.sh --remote=172.22.75.116 --use_emerged telemetry_UnitTests/control.user
         ./run_remote_tests.sh --remote=172.22.75.116 --use_emerged telemetry_UnitTests/control.guest
    
    Change-Id: I71b12e1095bdcf619c6c3035d8610a00ca500890
    Reviewed-on: https://gerrit.chromium.org/gerrit/60916
    Reviewed-by: Simran Basi <sbasi@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Dennis Jeffrey <dennisjeffrey@chromium.org>
    Tested-by: Dennis Jeffrey <dennisjeffrey@chromium.org>

---
+++ client/site_tests/telemetry_UnitTests/telemetry_UnitTests.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging, StringIO, sys
+
+from telemetry.unittest import gtest_testrunner, run_tests
+
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import error
+
+
+TELEMETRY_BASE_DIR = '/usr/local/telemetry/src/tools/telemetry'
+UNIT_TEST_SUBDIR = 'telemetry'
+
+
+class telemetry_UnitTests(test.test):
+    """This is a client side wrapper for the Telemetry unit tests."""
+    version = 1
+
+
+    def run_once(self, browser_type):
+        """
+        Runs the Telemetry unit tests.
+
+        @param browser_tye: The string type of browser to use, e.g., 'system'.
+
+        """
+        logging.info('Running the Telemetry unit tests with '
+                     'browser_type "%s".', browser_type)
+
+        # Capture the Telemetry output when running the unit tests.
+        capturer = StringIO.StringIO()
+        sys.stdout = capturer
+        runner = gtest_testrunner.GTestTestRunner(print_result_after_run=False)
+        run_tests.Main(['--browser=' + browser_type], UNIT_TEST_SUBDIR,
+                       TELEMETRY_BASE_DIR, runner)
+
+        # TODO(dennisjeffrey): The logging module no longer works after
+        # invoking run_tests.Main() above.  Using print statements below still
+        # allows the messages to be logged in the test debug log file.  See
+        # http://crbug.com/259041 for more details.  Once this is understood,
+        # we should change the print statements below to use logging if
+        # possible.
+        sys.stdout = sys.__stdout__  # Restore sys.stdout.
+        print capturer.getvalue()  # Print Telemetry output.
+        capturer.close()
+
+        if runner.result:
+            # The PrintSummary() below is captured in the test debug log file.
+            runner.result.PrintSummary()
+            if runner.result.num_errors:
+                raise error.TestFail(
+                        '%d unit tests failed.' % runner.result.num_errors)
+            else:
+                print 'All %d unit tests passed.' % runner.result.num_successes
+        else:
+            raise error.TestFail('No results found.')
