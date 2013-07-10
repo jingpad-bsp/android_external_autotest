daabd0feb926b44673bde3a4663693a9b1cdc59b
Rohit Makasana <rohitbm@chromium.org>
2013-07-11 00:39:07 +0530

    Migrate FlashSanity test to Telemetry
    BUG=chromium:237246
    TEST=Manual
    
    Change-Id: Ifb6640860b55fd46b29daffaf9034cbc4b6679ea
    Reviewed-on: https://gerrit.chromium.org/gerrit/61449
    Commit-Queue: Rohit Makasana <rohitbm@chromium.org>
    Reviewed-by: Rohit Makasana <rohitbm@chromium.org>
    Tested-by: Rohit Makasana <rohitbm@chromium.org>

--- client/site_tests/desktopui_FlashSanityCheck/desktopui_FlashSanityCheck.py
+++ client/site_tests/desktopui_FlashSanityCheck/desktopui_FlashSanityCheck.py
@@ -2,35 +2,54 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging, os, time
+
+from telemetry.core.chrome import cros_interface
+
+from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import constants, cros_logging
-from autotest_lib.client.cros import cros_ui_test, httpd
+from autotest_lib.client.cros import httpd
+from autotest_lib.client.common_lib.cros import chrome
 
 
-class desktopui_FlashSanityCheck(cros_ui_test.UITest):
-    version = 3
+FLASH_PROCESS_NAME = 'chrome/chrome --type=ppapi'
+
+
+class desktopui_FlashSanityCheck(test.test):
+    version = 4
+
 
     def initialize(self):
         self._test_url = 'http://localhost:8000/index.html'
-        self._testServer = httpd.HTTPListener(8000, docroot=self.srcdir)
+        self._testServer = httpd.HTTPListener(8000, docroot=self.bindir)
         self._testServer.run()
-        super(desktopui_FlashSanityCheck, self).initialize()
 
 
-    def run_once(self, time_to_wait=25):
+    def cleanup(self):
+        self._testServer.stop()
+
+
+    def run_flash_sanity_test(self, browser, time_to_wait_secs):
+        """Run the Flash sanity test.
+
+        @param browser: The Browser object to run the test with.
+        @param time_to_wait_secs: wait time for swf file to load.
+
+        """
+        tab = browser.tabs[0]
         self._log_reader = cros_logging.LogReader()
         self._log_reader.set_start_by_current()
 
         # Ensure that the swf got pulled
         latch = self._testServer.add_wait_url('/Trivial.swf')
-        self.pyauto.NavigateToURL(self._test_url)
-        latch.wait(time_to_wait)
+        tab.Navigate(self._test_url)
+        latch.wait(time_to_wait_secs)
 
-        child_processes = self.pyauto.GetBrowserInfo()['child_processes']
-        flash_processes = [x for x in child_processes if
-                           x['name'] == 'Shockwave Flash']
-        if not flash_processes:
-            raise error.TestFail('No flash process found.')
+        prc = [proc for proc in cros_interface.CrOSInterface().ListProcesses()
+                if FLASH_PROCESS_NAME in proc[1]]
+        if not prc:
+            raise error.TestFail('No Flash process found.')
 
         # Any better pattern matching?
         msg = ' Received crash notification for ' + constants.BROWSER
@@ -38,6 +57,6 @@ class desktopui_FlashSanityCheck(cros_ui_test.UITest):
             raise error.TestFail('Browser crashed during test.')
 
 
-    def cleanup(self):
-        self._testServer.stop()
-        super(desktopui_FlashSanityCheck, self).cleanup()
+    def run_once(self, time_to_wait_secs=25):
+        with chrome.login() as browser:
+            self.run_flash_sanity_test(browser, time_to_wait_secs)
