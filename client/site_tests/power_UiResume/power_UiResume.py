dca355d190ee9688b4d45a61cf0c49cb9d16bea2
Mark Koudritsky <kamrik@chromium.org>
2013-07-16 20:11:13 -0400

    Enable power_UiResume and port it to Telemetry.
    The power_UiResume test was disabled because it was killing lab
    machines. Now that the below CL has landed it should safe to enable it:
    https://gerrit.chromium.org/gerrit/#/c/61321/
    
    In addition ported it to use Telemetry login instead of PyAuoto.
    
    BUG=chromium:218175
    TEST=./run_remote_tests.sh --remote=mylumpy  power_UiResume
    
    Change-Id: I088b56f58415c76f2f239891603669b65c40de44
    Reviewed-on: https://gerrit.chromium.org/gerrit/62268
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Commit-Queue: Mark Koudritsky <kamrik@chromium.org>
    Tested-by: Mark Koudritsky <kamrik@chromium.org>

--- client/site_tests/power_UiResume/power_UiResume.py
+++ client/site_tests/power_UiResume/power_UiResume.py
@@ -2,24 +2,33 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import random, time
+import time
 
-from autotest_lib.client.cros import cros_ui_test, power_suspend, sys_power
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib.cros import chrome
+from autotest_lib.client.cros import power_suspend, sys_power
 
 
-class power_UiResume(cros_ui_test.UITest):
-    version = 1
+class power_UiResume(test.test):
+    """
+    Suspend a logged in system by sending a request to power manager via dbus.
 
-    def initialize(self, creds='$default'):
-        # It's important to log in with a real user. If logged in as
-        # guest, powerd will shut down instead of suspending.
-        super(power_UiResume, self).initialize(creds=creds)
+    This test waits quite a bit after logging in and before suspending.
+    Therefore it verifies suspend/resume functionality for an idle system. For
+    stress-testing suspend/resume in parallel with other things going on, see
+    power_SuspendStress.
+
+    """
+    version = 2
+
+    def initialize(self):
         self._suspender = power_suspend.Suspender(self.resultsdir,
                 method=sys_power.do_suspend, throw=True)
 
 
     def run_once(self):
         # Some idle time before initiating suspend-to-ram
-        time.sleep(random.randint(3, 7))
-        results = self._suspender.suspend(random.randint(0, 6))
-        self.write_perf_keyval(results)
+        with chrome.logged_in_browser():
+            time.sleep(10)
+            results = self._suspender.suspend(0)
+            self.write_perf_keyval(results)
