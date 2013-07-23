57067adf937e3a96ae161366bb6a0933cbe61a5a
Charlie Mooney <charliemooney@chromium.org>
2013-07-23 08:23:02 -0700

    Add Pit's keyboard string to TouchMTB test
    The keyboard device module for the Touch FW test didn't check for the
    string that's used for the keyboard on Pit.  As a result it would fail
    to run the test on this device.  This change adds that string.
    
    BUG=none
    TEST=manually tested on evt2 pit
    
    Change-Id: I38585223b57fb0bc94f3f348959eb01e7791f03f
    Signed-off-by: Charlie Mooney <charliemooney@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/63039
    Reviewed-by: Joseph Shyh-In Hwang <josephsih@chromium.org>

--- client/site_tests/firmware_TouchMTB/keyboard_device.py
+++ client/site_tests/firmware_TouchMTB/keyboard_device.py
@@ -19,7 +19,7 @@ from linux_input import EV_KEY
 
 class KeyboardDevice:
     """A class about keyboard device properties."""
-    TYPE = '(keyboard|chromeos-ec-i2c)'
+    TYPE = '(keyboard|chromeos-ec-i2c|cros-ec-spi)'
 
     def __init__(self, device_node=None):
         self._device_info_file = '/proc/bus/input/devices'
