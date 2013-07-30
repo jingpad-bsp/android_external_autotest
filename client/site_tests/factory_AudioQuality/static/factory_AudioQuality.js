172ab7202d2d82ee9ce8c355cefe136350210b0a
henryhsu <henryhsu@chromium.org>
2013-07-30 12:35:01 +0800

    Modify communication protocol for AudioQuality test case
    We put audio fixture parameter on shopfloor server.
    When DUT tests, it should download the latest audio fixture parameters
    from shopfloor server.
    And control machine will ask DUT the version of parameter.
    If version is mismatch, DUT will send the parameter to control machine.
    Upload AUXLOG after test complete
    
    BUG=chrome-os-partner:21127
    TEST=manually test on vendor site
    
    Change-Id: I2f63b411831060a7198763315d9fe0cc02ff3ab7
    Reviewed-on: https://gerrit.chromium.org/gerrit/63703
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>
    Commit-Queue: Heng-ruey Hsu <henryhsu@chromium.org>
    Tested-by: Heng-ruey Hsu <henryhsu@chromium.org>

--- client/site_tests/factory_AudioQuality/static/factory_AudioQuality.js
+++ client/site_tests/factory_AudioQuality/static/factory_AudioQuality.js
@@ -1,18 +1,18 @@
-/*
-window.onkeydown = function(event) {
-  if (event.keyCode == 83) {
-    test.sendTestEvent("init_audio_server",{});
-  }
-}
-*/
-
-window.onload = function(event) {
-  test.sendTestEvent("init_audio_server",{});
-}
-
 var active = 'loop_0';
 var display_fa_utility = false;
-var count = 3, count_timer = null, count_msg = '';
+var start_run = false;
+
+window.onkeydown = function(event) {
+  if (event.keyCode == 32 && start_run == false) {
+    test.sendTestEvent("start_run",{});
+    document.getElementById('msg-utility').style.display = 'none';
+    start_run = true;
+  }
+}
+
+//window.onload = function(event) {
+//  test.sendTestEvent("start_run",{});
+//}
 
 function setMessage(msg) {
   document.getElementById("message").innerHTML = msg;
@@ -21,7 +21,7 @@ function setMessage(msg) {
 function testCommand(cmd) {
   if (active.length != 0)
     document.getElementById(active).checked = false;
-  test.sendTestEvent("test_command", {"cmd": cmd});
+  test.sendTestEvent("mock_command", {"cmd": cmd});
   active = cmd;
 }
 
@@ -36,26 +36,9 @@ function toggleFAUtility() {
   if (display_fa_utility) {
     document.getElementById('fa-utility').style.display = 'none';
     display_fa_utility = false;
-    test.sendTestEvent("init_audio_server",{});
   } else {
     document.getElementById('fa-utility').style.display = 'block';
     display_fa_utility = true;
     setMessage('');
-    clearInterval(count_timer);
   }
-}
-
-function countdown() {
-  setMessage(count_msg + ' ' + count);
-  count = count - 1;
-  if (count <= 0) {
-     clearInterval(count_timer);
-     test.sendTestEvent("init_audio_server",{});
-  }
-}
-
-function start_countdown(msg, num) {
-  count = num;
-  count_msg = msg;
-  count_timer = setInterval(countdown, 1000);
 }
