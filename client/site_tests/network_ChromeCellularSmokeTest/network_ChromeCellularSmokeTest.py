71e2d7b9205debb98407888ae9136534b78fb24d
Arman Uguray <armansito@chromium.org>
2013-08-08 19:30:48 -0700

    autotest: network_ui: Added test_utils.py
    Added common code to test_utils.py
    
    BUG=chromium:264107
    TEST=network_ChromeCellularSmokeTest
    
    Change-Id: Ifdda4f59c472d6589d36a9100669c8451a6f953a
    Reviewed-on: https://gerrit.chromium.org/gerrit/65274
    Commit-Queue: Arman Uguray <armansito@chromium.org>
    Reviewed-by: Arman Uguray <armansito@chromium.org>
    Tested-by: Arman Uguray <armansito@chromium.org>

--- client/site_tests/network_ChromeCellularSmokeTest/network_ChromeCellularSmokeTest.py
+++ client/site_tests/network_ChromeCellularSmokeTest/network_ChromeCellularSmokeTest.py
@@ -7,12 +7,11 @@ import logging
 
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros.cellular.pseudomodem import mm1
-from autotest_lib.client.cros.cellular.pseudomodem import pseudomodem
 from autotest_lib.client.cros.cellular.chrome_testing \
         import chrome_networking_test_context as cntc
-
-from telemetry.core import util
+from autotest_lib.client.cros.cellular.chrome_testing import test_utils
+from autotest_lib.client.cros.cellular.pseudomodem import mm1
+from autotest_lib.client.cros.cellular.pseudomodem import pseudomodem
 
 class network_ChromeCellularSmokeTest(test.test):
     """
@@ -24,8 +23,6 @@ class network_ChromeCellularSmokeTest(test.test):
     """
     version = 1
 
-    LONG_TIMEOUT = 120
-    SHORT_TIMEOUT = 10
     CONNECT_COUNT = 5
 
     def _setup_modem_proxy(self):
@@ -38,9 +35,11 @@ class network_ChromeCellularSmokeTest(test.test):
                                  len(devices))
         self._modem = self._bus.get_object(mm1.I_MODEM_MANAGER, devices[0])
 
+
     def _get_modem_state(self):
         iprops = dbus.Interface(self._modem, mm1.I_PROPERTIES)
         return iprops.Get(mm1.I_MODEM, mm1.MM_MODEM_PROPERTY_NAME_STATE)
+
 
     def _get_cellular_network(self):
         networks = self._chrome_testing.find_cellular_networks()
@@ -48,8 +47,11 @@ class network_ChromeCellularSmokeTest(test.test):
             raise error.TestFail(
                     'Expected 1 cellular network, found ' + str(len(networks)))
         network = networks[0]
-        self._ensure_network_is_valid(network)
+        test_utils.simple_network_sanity_check(
+                network, pseudomodem.DEFAULT_TEST_NETWORK_PREFIX,
+                self._chrome_testing.CHROME_NETWORK_TYPE_CELLULAR)
         return network
+
 
     def _assert_modem_state(self, expected_state):
         modem_state = self._get_modem_state()
@@ -59,67 +61,45 @@ class network_ChromeCellularSmokeTest(test.test):
                     mm1.ModemStateToString(expected_state) + '", found: ' +
                     mm1.ModemStateToString(modem_state))
 
-    def _get_network_by_id(self, network_id):
-        call_status = self._chrome_testing.call_test_function(
-                self.SHORT_TIMEOUT, 'getNetworkInfo', '"' + network_id + '"')
-        if call_status['status'] != self._chrome_testing.STATUS_SUCCESS:
-            raise error.TestFail(
-                    'Failed to get network with id: ' + network_id)
-        network = call_status['result']
-        self._ensure_network_is_valid(network)
-        return network
-
-    def _ensure_network_is_valid(self, network):
-        if network['Type'] != self._chrome_testing.CHROME_NETWORK_TYPE_CELLULAR:
-            raise error.TestFail(
-                    'Expected network of type "Cellular", found ' +
-                    network['Type'])
-        if not network["Name"].startswith(
-                pseudomodem.DEFAULT_TEST_NETWORK_PREFIX):
-            raise error.TestFail('Network name is incorrect: ' +
-                                 network["Name"])
 
     def _ensure_network_status(self, network_id, status, timeout):
-        def _compare_network_status():
-            network = self._get_network_by_id(network_id)
-            return network['ConnectionState'] == status
-        try:
-            util.WaitFor(_compare_network_status, timeout)
-        except util.TimeoutException:
-            raise error.TestFail(
-                    'Timed out waiting for network status: ' + status)
+        test_utils.check_ui_property(
+                self._chrome_testing, network_id, 'ConnectionState', status)
+
 
     def _disconnect_cellular_network(self):
         # Make sure that the network becomes disconnected.
         network_id = self._network['GUID']
         logging.info('Disconnecting from network: ' + network_id)
         call_status = self._chrome_testing.call_test_function(
-                self.LONG_TIMEOUT,
+                test_utils.LONG_TIMEOUT,
                 'disconnectFromNetwork',
                 '"' + network_id + '"')
         logging.info('Checking that the network is disconnected.')
         self._ensure_network_status(
-                network_id, 'NotConnected', self.LONG_TIMEOUT)
+                network_id, 'NotConnected', test_utils.LONG_TIMEOUT)
         logging.info('The network is disconnected. Checking that the modem is '
                      'in the REGISTERED state.')
         self._assert_modem_state(mm1.MM_MODEM_STATE_REGISTERED)
         logging.info('Modem is disconnected. Disconnect was successful.')
+
 
     def _connect_cellular_network(self):
         # Make sure that the network becomes connected.
         network_id = self._network['GUID']
         logging.info('Connecting to network: ' + network_id)
         call_status = self._chrome_testing.call_test_function(
-                self.LONG_TIMEOUT,
+                test_utils.LONG_TIMEOUT,
                 'connectToNetwork',
                 '"' + network_id + '"')
         logging.info('Checking that the network is connected.')
         self._ensure_network_status(
-                network_id, 'Connected', self.LONG_TIMEOUT)
+                network_id, 'Connected', test_utils.LONG_TIMEOUT)
         logging.info('The network is connected. Checking that the modem is in '
                      'the CONNECTED state.')
         self._assert_modem_state(mm1.MM_MODEM_STATE_CONNECTED)
         logging.info('Modem is connected. Connect was successful.')
+
 
     def _run_once_internal(self):
         # Set up a ModemManager proxy to use to verify the modem state.
@@ -136,6 +116,7 @@ class network_ChromeCellularSmokeTest(test.test):
         for _ in xrange(self.CONNECT_COUNT):
             self._connect_cellular_network()
             self._disconnect_cellular_network()
+
 
     def run_once(self, family):
         with pseudomodem.TestModemManagerContext(
