2bc089bbb0f19e07c25aa5d0202ad9edd4dcef57
Todd Broch <tbroch@chromium.org>
2013-08-20 12:55:39 -0700

    Allow eMMC devices to consume some small portion of storage.
    eMMC vendors typically allocate some small amount of storage for
    metadata and bad block repair which makes the available GBs less than
    the raw size.
    
    Change allows a modest amount (250MB) for the vendor below the minimum
    hwqual of 16GB.
    
    Additionally it details (does not implement) a future method using CSD
    and EXT_CSD fields that may eventually make it possible to calculate
    the raw bits although that does not appear to be the case for existing
    eMMC devices polled.
    
    BUG=chrome-os-partner:11308
    TEST=run test on lumpy & spring and it passes
    
    Change-Id: I7e6ba8d4d63fcd39d448c02ca73cda3001b68976
    Reviewed-on: https://gerrit.chromium.org/gerrit/66413
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

--- client/site_tests/hardware_DiskSize/hardware_DiskSize.py
+++ client/site_tests/hardware_DiskSize/hardware_DiskSize.py
@@ -9,20 +9,62 @@ import re
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 
+DEFAULT_MIN_GB = 16
+# Allowable amount of bits eMMC vendor can use in firmware to support bad block
+# replacement and metadata.
+EMMC_VENDOR_ALLOWED_GB = 0.25
+
 class hardware_DiskSize(test.test):
     version = 1
 
+    def _is_emmc(self):
+        path = os.path.join("/sys/class/block/", self._device,
+                            "device", "type")
+        if not os.path.exists(path):
+            return False
+        return utils.read_one_line(path) == 'MMC'
+
+
+    def _compute_min_gb(self):
+        """Computes minimum size allowed primary storage device.
+
+        TODO(tbroch): Add computation of raw bytes in eMMC using 'Chip Specific
+        Data' (CSD & EXT_CSD) defined by JEDEC JESD84-A44.pdf if possible.
+
+        CSD :: /sys/class/block/<device>/device/csd
+        EXT_CSD :: debugfs
+
+        Algorithm should look something like this:
+        CSD[C_SIZE] = 0xfff == eMMC > 2GB
+        EXT_CSD[SEC_COUNT] = # of 512byte sectors
+
+        Now for existing eMMC I've examined I do see the C_SIZE == 0xfff.
+        Unfortunately the SEC_COUNT appears to have excluded the sectors
+        reserved for metadata & repair.  Perhaps thats by design in which case
+        there is no mechanism to determine the actual raw sectors.
+
+        For now I use 0.25GB as an acceptable fudge.
+
+        Returns:
+            integer, in GB of minimum storage size.
+        """
+
+        min_gb = DEFAULT_MIN_GB
+        if self._is_emmc():
+            min_gb -= EMMC_VENDOR_ALLOWED_GB
+        return min_gb
+
+
     def run_once(self):
         devnode = utils.system_output('rootdev -s -d -i')
-        device = os.path.basename(devnode)
+        self._device = os.path.basename(devnode)
 
         for line in file('/proc/partitions'):
             try:
-                major, minor, blocks, name = re.split(r' +', line.strip())
+                _, _, blocks, name = re.split(r' +', line.strip())
             except ValueError:
                 continue
-            # TODO(waihong@): Check if this works on ARM.
-            if name == device:
+            if name == self._device:
                 blocks = int(blocks)
                 break
         else:
@@ -32,8 +74,8 @@ class hardware_DiskSize(test.test):
         # powers of 1000, instead of powers of 1024.
         gb = blocks * 1024.0 / 1000.0 / 1000.0 / 1000.0
         self.write_perf_keyval({"gb_main_disk_size": gb})
-        logging.info("DiskSize: %.3f GB" % gb)
-        min_gb = 16
+        min_gb = self._compute_min_gb()
+        logging.info("DiskSize: %.3f GB MinDiskSize: %.3f GB", gb, min_gb)
         if (gb < min_gb):
             raise error.TestError("DiskSize %.3f GB below minimum (%.3f GB)" \
                 % (gb, min_gb))
