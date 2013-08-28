eadd7cc6f21d02c3e8fe64fd793961d1812545a8
Todd Broch <tbroch@chromium.org>
2013-08-28 14:27:36 -0700

    Refactor kernel_SchedBandwith test.
    Test was failing due to absence of setting <cgroup>/cpu.cfs_quota_us
    since CrOS transitioned to using the cpu.shares method of cgroup
    throttling.
    
    Decided along the way to refactor the test to be done in python-only
    as the additional binary seemed unnecessary.
    
    BUG=chromium:235535
    TEST=run test on Daisy and it passes.
    
    Change-Id: I79f397d592f8e73f9ec8643ddb7babdea3d8d198
    Reviewed-on: https://chromium-review.googlesource.com/167359
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

--- client/site_tests/kernel_SchedBandwith/kernel_SchedBandwith.py
+++ client/site_tests/kernel_SchedBandwith/kernel_SchedBandwith.py
@@ -4,40 +4,97 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 import os
+import subprocess
+import time
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 
 class kernel_SchedBandwith(test.test):
-    """
-    Test kernel CFS_BANDWIDTH scheduler mechanism (/tmp/cgroup/...)
-    """
+    """Test kernel CFS_BANDWIDTH scheduler mechanism (/sys/fs/cgroup/...)"""
     version = 1
-    executable = "cfs-bandwidth-test"
-    # This is the largest absolute difference we consider acceptable
-    # between the number of periods in which there was the opportunity
-    # to throttle vs. the number of periods in which throttling occurred.
-    # Typical results on Alex are within 0..2.
-    MAX_DELTA = 3
-    # A 30 second (default) run should result in most of the 300 time
-    # slices being throttled.  Set a conservative lower bound based on
-    # having an unknown system load.  Alex commonly yields numbers in
-    # the range 311..315, which includes test overhead and signal latency.
-    MIN_THROTTLED = 250
+    # A 30 second (default) run should result in most of the time slices being
+    # throttled.  Set a conservative lower bound based on having an unknown
+    # system load.  Alex commonly yields numbers in the range 311..315, which
+    # includes test overhead and signal latency.
+    _MIN_SECS = 30
 
-    def setup(self):
-        os.chdir(self.srcdir)
-        utils.make(self.executable)
+    _CG_DIR = "/sys/fs/cgroup/cpu/chrome_renderers/background"
+
+    def _parse_cpu_stats(self):
+        """Parse and return CFS bandwidth statistics.
+
+        From kernel/Documentation/scheduler/shed-bwc.txt
+
+        cpu.stat:
+        - nr_periods: Number of enforcement intervals that have elapsed.
+        - nr_throttled: Number of times the group has been throttled/limited.
+        - throttled_time: The total time duration (in nanoseconds) for which entities
+          of the group have been throttled.
+
+        Returns: tuple with nr_periods, nr_throttled, throttled_time.
+        """
+        nr_periods = None
+        nr_throttled = None
+        throttled_time = None
+
+        fd = open(os.path.join(self._CG_DIR, "cpu.stat"))
+
+        for ln in fd.readlines():
+            logging.debug(ln)
+            (name, val) = ln.split()
+            logging.debug("name = %s val = %s", name, val)
+            if name == 'nr_periods':
+                nr_periods = int(val)
+            if name == 'nr_throttled':
+                nr_throttled = int(val)
+            if name == 'throttled_time':
+                throttled_time = int(val)
+
+        fd.close()
+        return nr_periods, nr_throttled, throttled_time
+
 
     def run_once(self):
-    # The default in the C code is for this to take 30 seconds + 2
-    # seconds + latency.
-        out = utils.run(os.path.join(self.srcdir, self.executable))
-        result = out.stdout.rstrip("\r\n").split(" ")
-        actual = int(result[1])
-        delta = abs(int(result[0]) - actual)
-        if delta > self.MAX_DELTA:
-            raise error.TestFail("Test variance %d too large" % delta)
-        if actual < self.MIN_THROTTLED:
+
+        stats = []
+        if not os.path.exists(self._CG_DIR):
+            raise error.TestError("Locating cgroup dir %s" % self._CG_DIR)
+        quota = utils.read_one_line(os.path.join(self._CG_DIR,
+                                                 "cpu.cfs_quota_us"))
+        period_us = int(utils.read_one_line(os.path.join(self._CG_DIR,
+                                                     "cpu.cfs_period_us")))
+
+        # make sure its disabled
+        utils.write_one_line(os.path.join(self._CG_DIR, "cpu.cfs_quota_us"), -1)
+
+        stats.append(self._parse_cpu_stats())
+        # start a cpu-hogging task and add to group
+        null_fd = open("/dev/null", "w")
+        self._task = subprocess.Popen(['seq', '0', '0', '0'], stdout=null_fd)
+        utils.write_one_line(os.path.join(self._CG_DIR, "tasks"), self._task.pid)
+        utils.write_one_line(os.path.join(self._CG_DIR, "cpu.cfs_quota_us"),
+                             int(period_us)/2)
+        time.sleep(self._MIN_SECS)
+
+        stats.append(self._parse_cpu_stats())
+
+        # return quota to initial value
+        utils.write_one_line(os.path.join(self._CG_DIR, "cpu.cfs_quota_us"),
+                             quota)
+
+        periods = stats[1][0] - stats[0][0]
+        actual = stats[1][1] - stats[0][1]
+        logging.info("periods tested:%d periods throttled:%d", periods, actual)
+ 
+        # make sure we throttled at least 90% of the slices
+        min_throttled = self._MIN_SECS * 1e6 / period_us * 0.9
+        if actual < min_throttled:
             raise error.TestFail("Unexpected throttle count of %d" % actual)
+
+    def cleanup(self):
+        super(kernel_SchedBandwith, self).cleanup()
+        if hasattr(self, '_task') and self._task:
+            self._task.kill()
