f229bf6aa95c9fbb4f061a492578709f3756defb
Dan Shi <dshi@chromium.org>
2013-11-13 11:57:08 -0800

    [autotest] Rewrite the script to delete old builds.
    BUG=chromium:318833
    TEST=test in a local devserver setup
    /usr/local/autotest/site_utils/admin/clean_staged_images.py --max-age 0 --max-paladin-age 8 ~/images -v
    confirm old builds in dev-channel folder are deleted
    
    Change-Id: I93356b5b939f54707164c9c09c912477861942bf
    Reviewed-on: https://chromium-review.googlesource.com/176617
    Reviewed-by: Dan Shi <dshi@chromium.org>
    Tested-by: Dan Shi <dshi@chromium.org>
    Commit-Queue: Dan Shi <dshi@chromium.org>

--- site_utils/admin/clean_staged_images.py
+++ site_utils/admin/clean_staged_images.py
@@ -6,72 +6,50 @@
 """Clean Staged Images.
 
 This script is responsible for removing older builds from the Chrome OS
-devserver as well as old cros-version: labels from the Autotest DB.
-
-There are two different options to remove builds, one is for "regular builds"
-which is builds that happen around 4 times a day, and the other option is for
-"Paladin builds" which are builds that happen as part of the Commit Queue and
-are far more frequent. Since Paladin builds are only used for the BVT HWTest
-Step we can be more agressive when it comes to pruning these builds.
+devserver. It walks through the files in the images folder, check each found
+staged.timestamp and do following.
+1. Check if the modified time of the timestamp file is older than a given cutoff
+   time, e.g., 24 hours before the current time.
+2. If that's True, delete the folder containing staged.timestamp.
+3. Check if the parent folder of the deleted foler is empty. If that's True,
+   delete the parent folder as well. Do so recursively, until it hits the top
+   folder, e.g., |~/images|.
 """
 
-from distutils import version
-import optparse
-import glob
 import logging
+import optparse
 import os
 import re
 import sys
 import shutil
 import time
 
-import common
-from autotest_lib.frontend.afe import rpc_client_lib
-from autotest_lib.cli.rpc import AFE_RPC_PATH
-from autotest_lib.client.common_lib import global_config
-from autotest_lib.server.cros.dynamic_suite.constants import VERSION_PREFIX
-
 # This filename must be kept in sync with devserver's downloader.py
 _TIMESTAMP_FILENAME = 'staged.timestamp'
-_HOURS_TO_SECONDS = 60*60
+_HOURS_TO_SECONDS = 60 * 60
 _EXEMPTED_DIRECTORIES = [ 'servo-images' ]
 
+def get_all_timestamp_dirs(root):
+    """Get all directories that has timestamp file.
 
-def validate_and_parse_build_milestone(build_name):
-    """Parse the build name and ensure it is a proper build name.
-
-    Example build name: R21-4555.2.3 or R21-2368.0.0-rc30
-
-    @param build_name The name of the build.
-    @returns the milestone of the build if it is valid.
-
-    >>> validate_and_parse_build_milestone('R21-4555.2.3')
-    'R21'
-    >>> validate_and_parse_build_milestone('R21-2368.0.0-rc30')
-    'R21'
+    @param root: The top folder to look for timestamp file.
+    @return: An iterator of directories that have timestamp file in it.
     """
-    pattern = '(R\d+)-(\d+\.\d+\.\d+)'
-    match = re.search(pattern, build_name)
-    if match:
-        return match.group(1)
-
-
-def sort_builds(build_list):
-    """Sort a list of builds of format R21-121.0.0 or R21-121.0.0-rc3.
-
-    @param build_list: The list of builds to sort.
-    @returns a list of builds sorted in reverse order.
-    """
-    return sorted(build_list, key=lambda s: version.LooseVersion(s),
-                  reverse=True)
+    for dir_path, dir_names, file_names in os.walk(root):
+        if os.path.basename(dir_path) in _EXEMPTED_DIRECTORIES:
+            logging.debug('Skipping %s', dir_path)
+            dir_names[:] = []
+        elif _TIMESTAMP_FILENAME in file_names:
+            dir_names[:] = []
+            yield dir_path
 
 
 def file_is_too_old(build_path, max_age_hours):
     """Test to see if the build at |build_path| is older than |max_age_hours|.
 
-    @param build_path The path to the build (ie. 'build_dir/R21-2035.0.0')
-    @param max_age_hours The maximum allowed age of a build in hours.
-    @return True if the build is older than |max_age|, False otherwise.
+    @param build_path: The path to the build (ie. 'build_dir/R21-2035.0.0')
+    @param max_age_hours: The maximum allowed age of a build in hours.
+    @return: True if the build is older than |max_age_hours|, False otherwise.
     """
     cutoff = time.time() - max_age_hours * _HOURS_TO_SECONDS
     timestamp_path = os.path.join(build_path, _TIMESTAMP_FILENAME)
@@ -79,69 +57,39 @@ def file_is_too_old(build_path, max_age_hours):
         age = os.stat(timestamp_path).st_mtime
         if age < cutoff:
             return True
-    else:
-        # If the timestamp doesn't exist, then make one so we can wipe the
-        # build on a later run.
-        with open(timestamp_path, 'a'):
-            os.utime(timestamp_path, None)
     return False
 
 
-def prune_builds(build_dir, max_age_hours):
-    """Remove any builds older than |max_age_hours| hours old.
+def try_delete_parent_dir(path, root):
+    """Try to delete parent directory if it's empty.
 
-    Prune a directory down to only having builds last used sooner than
-    |max_age_hours| seconds ago. This will prune down all milestones to only
-    having fresh builds.
+    Recursively attempt to delete parent directory of given path. Only stop if:
+    1. parent directory is the root directory used to stage images.
+    2. The base name of given path is a valid build path, e.g., R31-4532.0.0 or
+       4530.0.0 (for builds staged in *-channel/[platform]/).
+    3. The parent directory is not empty.
 
-    @param build_dir: The build dir to prune builds in.
-    @param max_age_hours: The max age in hours of builds to keep around.
+    @param path: Start path that attempt to delete whose parent directory.
+    @param root: root directory that devserver used to stage images, e.g.,
+                 |/usr/local/google/home/dshi/images|, must be an absolute path.
     """
-    logging.debug('Pruning %s down to builds older than %d hours ago.',
-                  build_dir, max_age_hours)
-    build_dict = {}
-    # Create a dict that is of the format:
-    # build_dict[milestone][build_name] = build_path
-    # e.g. {'R21' : {'R21-2056.0.0': 'build_dir/R21-2056.0.0',
-    #                'R21-2035.0.0': 'build_dir/R21-2035.0.0'}
-    for entry in glob.glob(build_dir + '/*'):
-        build_name = os.path.basename(entry)
-        milestone = validate_and_parse_build_milestone(build_name)
-        if not milestone:
-            logging.debug('Skipping %s', build_name)
-            continue
-        build_dict.setdefault(milestone, {})[os.path.basename(entry)] = entry
+    pattern = '(\d+\.\d+\.\d+)'
+    match = re.search(pattern, os.path.basename(path))
+    if match:
+        return
 
-    for milestone in build_dict:
-        for (entry, path) in build_dict[milestone].items():
-            if file_is_too_old(path, max_age_hours):
-                logging.debug('Deleting %s', path)
-                shutil.rmtree(path)
+    parent_dir = os.path.abspath(os.path.join(path, os.pardir))
+    if parent_dir == root:
+        return
+
+    try:
+        os.rmdir(parent_dir)
+        try_delete_parent_dir(parent_dir, root)
+    except OSError:
+        pass
 
 
-def delete_labels_of_unstaged_builds(build_dir):
-    """Delete labels of builds that are no longer staged on the dev server.
-
-    @param build_dir: The build dir to compare known labels against.
-    """
-    autotest_server = global_config.global_config.get_config_value('SERVER',
-                                                                   'hostname')
-    web_server = 'http://%s/%s' % (autotest_server, AFE_RPC_PATH)
-    rpc_interface = rpc_client_lib.get_proxy(web_server)
-    builder_name = os.path.basename(build_dir.rstrip('/')).split('/')[-1]
-    vers = '%s%s/' % (VERSION_PREFIX, builder_name)
-    all_labels = rpc_interface.get_labels(name__startswith=vers)
-    # Build a list of cros-version:build_name/CROS_VERSION.
-    # e.g. cros-version:x86-mario-release/R20-2268.41.0
-    build_labels_to_keep = [vers + os.path.basename(entry) for entry in
-                    glob.glob(build_dir + '/*')]
-    for label in all_labels:
-        if label['name'] not in build_labels_to_keep:
-            logging.debug('Removing label %s', label['name'])
-            rpc_interface.delete_label(label['id'])
-
-
-def prune_builds_and_labels(builds_dir, keep_duration, keep_paladin_duration):
+def prune_builds(builds_dir, keep_duration, keep_paladin_duration):
     """Prune the build dirs and also delete old labels.
 
     @param builds_dir: The builds dir where all builds are staged.
@@ -149,24 +97,18 @@ def prune_builds_and_labels(builds_dir, keep_duration, keep_paladin_duration):
     @param keep_duration: How old of regular builds to keep around.
     @param keep_paladin_duration: How old of Paladin builds to keep around.
     """
-    if not os.path.exists(builds_dir):
-        logging.error('Builds dir %s does not exist', builds_dir)
-        return
-
-    for build_dir in glob.glob(builds_dir + '/*'):
-        if os.path.basename(build_dir) in _EXEMPTED_DIRECTORIES:
-            logging.debug('Skipping %s', build_dir)
-            continue
-        logging.debug('Processing %s', build_dir)
-        if build_dir.endswith('-paladin'):
+    for timestamp_dir in get_all_timestamp_dirs(builds_dir):
+        logging.debug('Processing %s', timestamp_dir)
+        if '-paladin/' in timestamp_dir:
             keep = keep_paladin_duration
         else:
             keep = keep_duration
+        if file_is_too_old(timestamp_dir, keep):
+            logging.debug('Deleting %s', timestamp_dir)
+            shutil.rmtree(timestamp_dir)
+            # Resursively delete parent folders
+            try_delete_parent_dir(timestamp_dir, builds_dir)
 
-        prune_builds(build_dir, keep)
-        # TODO(scottz): Skip label pruning for now.
-        # Refer to bug crosbug.com/33247
-        # delete_labels_of_unstaged_builds(build_dir)
 
 def main():
     """Main routine."""
@@ -181,16 +123,20 @@ def main():
                       help='Run in verbose mode')
     options, args = parser.parse_args()
     if len(args) != 1:
-      parser.print_usage()
-      sys.exit(1)
+        parser.print_usage()
+        sys.exit(1)
+
+    builds_dir = os.path.abspath(args[0])
+    if not os.path.exists(builds_dir):
+        logging.error('Builds dir %s does not exist', builds_dir)
+        sys.exit(1)
 
     if options.verbose:
         logging.getLogger().setLevel(logging.DEBUG)
     else:
         logging.getLogger().setLevel(logging.INFO)
 
-    prune_builds_and_labels(args[0], options.max_age,
-                            options.max_paladin_age)
+    prune_builds(builds_dir, options.max_age, options.max_paladin_age)
 
 
 if __name__ == '__main__':
