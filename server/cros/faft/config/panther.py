0abd78573234bea1cd6a248058a96f2c735d4175
Mohammed Habibulla <moch@chromium.org>
2013-11-13 14:19:54 -0800

    Panther: Add faft config for Panther
    BUG=chrome-os-partner:23512
    TEST=test_that -b panther <IP addr> firmware_FAFTSetup
    
    Change-Id: I32ddf773435a30bc4423e55c9ccbd046999366af
    Reviewed-on: https://chromium-review.googlesource.com/176674
    Reviewed-by: Wai-Hong Tam <waihong@chromium.org>
    Commit-Queue: Mohammed Habibulla <moch@chromium.org>
    Tested-by: Mohammed Habibulla <moch@chromium.org>

---
+++ server/cros/faft/config/panther.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""FAFT configuration overrides for Panther."""
+
+
+class Values(object):
+    has_lid = False
+    has_keyboard = False
+    keyboard_dev = True
