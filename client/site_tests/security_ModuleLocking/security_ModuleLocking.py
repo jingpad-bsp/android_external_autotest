bb7b57abc241909b7b6bcffee0d8138a53e932fc
Kees Cook <keescook@chromium.org>
2013-11-25 16:58:49 -0800

    security_ModuleLocking: use compression for old API
    To trigger the old module loading syscall, use file compression
    so that kmod will do the decompress in memory and use the blob
    style syscall.
    
    BUG=chromium:321655
    TEST=snow build and passes again
    
    Change-Id: I46cd5757ec9f9f6f4886951cdbbb558a34fc52c5
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/177970
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- client/site_tests/security_ModuleLocking/security_ModuleLocking.py
+++ client/site_tests/security_ModuleLocking/security_ModuleLocking.py
@@ -7,20 +7,30 @@ from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 
 class security_ModuleLocking(test.test):
+    """
+    Handle examining the system for specific module loading capabilities.
+    """
     version = 1
 
     def _passed(self, msg):
-        logging.info('ok: %s' % (msg))
+        logging.info('ok: %s', msg)
 
     def _failed(self, msg):
-        logging.error('FAIL: %s' % (msg))
+        logging.error('FAIL: %s', msg)
         self._failures.append(msg)
 
     def _fatal(self, msg):
-        logging.error('FATAL: %s' % (msg))
+        logging.error('FATAL: %s', msg)
         raise error.TestError(msg)
 
     def check(self, boolean, msg, fatal=False):
+        """
+        Check boolean state and report condition to log.
+
+        @param boolean: condition to examine
+        @param msg: what the condition is testing
+        @param fatal: should the test full abort on the condition failing
+        """
         if boolean == True:
             self._passed(msg)
         else:
@@ -31,6 +41,11 @@ class security_ModuleLocking(test.test):
                 self._failed(msg)
 
     def module_loaded(self, module):
+        """
+        Detect if the given module is already loaded in the kernel.
+
+        @param module: name of module to check
+        """
         module = module.replace('-', '_')
         match = "%s " % (module)
         for line in open("/proc/modules"):
@@ -39,14 +54,39 @@ class security_ModuleLocking(test.test):
         return False
 
     def rmmod(self, module):
+        """
+        Unload a module if it is already loaded in the kernel.
+
+        @param module: name of module to unload
+        """
         if self.module_loaded(module):
             utils.system("rmmod %s" % (module))
 
     def modprobe(self, module):
+        """
+        If a module is not already loaded in the kernel, load it via modprobe.
+
+        @param module: name of module to load
+        """
         if not self.module_loaded(module):
             utils.system("modprobe %s" % (module))
 
+    def _module_path(self, module):
+        """
+        Locate a kernel module's full filesystem path.
+
+        @param module: name of module to locate
+        """
+        ko = utils.system_output("find /lib/modules -name '%s.ko'" % (module))
+        return ko.splitlines()[0]
+
     def module_loads_outside_rootfs(self, module):
+        """
+        Copies the given module into /tmp and tries to load it from there
+        using insmod directly.
+
+        @param module: name of module to test
+        """
         # Start from a clean slate.
         self.rmmod(module)
 
@@ -55,7 +95,7 @@ class security_ModuleLocking(test.test):
         self.rmmod(module)
 
         # Load module directly with insmod from root filesystem.
-        ko = utils.system_output("find /lib/modules -name '%s.ko'" % (module))
+        ko = self._module_path(module)
         utils.system("insmod %s" % (ko))
         self.rmmod(module)
 
@@ -73,21 +113,38 @@ class security_ModuleLocking(test.test):
         return False
 
     def module_loads_old_api(self, module):
+        """
+        Loads a module using the old blob-style kernel syscall. With
+        kmod, this requires compressing the module first to trigger
+        in-memory decompression and loading.
+
+        @param module: name of module to test
+        """
         # Start from a clean slate.
         self.rmmod(module)
 
-        # Use --force-modversion to trigger the old API.
-        rc = utils.system("modprobe --force-modversion %s" % (module),
-                          ignore_status=True)
+        # Compress module to trigger the old API.
+        tmp = "/tmp/%s.ko.gz" % (module)
+        ko = self._module_path(module)
+        utils.system("gzip -c %s > %s" % (ko, tmp))
+        rc = utils.system("insmod %s" % (tmp), ignore_status=True)
 
         # Clean up.
         self.rmmod(module)
+        utils.system("rm %s" % (tmp))
 
         if rc == 0:
             return True
         return False
 
     def run_once(self):
+        """
+        Check that the fd-based module loading syscall is enforcing the
+        module fd origin to the root filesystem, and that it can be
+        disabled and will allow the old syscall API as well.
+        TODO(keescook): add production test to make sure that on a verified
+        boot, "/proc/sys/kernel/chromiumos/module_locking" does not exist.
+        """
         # Empty failure list means test passes.
         self._failures = []
 
