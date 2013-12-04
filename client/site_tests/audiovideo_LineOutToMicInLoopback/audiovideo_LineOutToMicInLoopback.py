979a35d88fd4b790533824027172abf33996a85f
Owen Lin <owenlin@chromium.org>
2013-12-04 17:03:15 +0800

    audiovideo_LineOutToMicInLoopback - Set playback volume and capture gain.
    BUG=chromium:307379
    TEST=Run the test on butterfly
    
    Change-Id: Ic1d84c73a29861929f77afb9a8ad6adabc69982d
    Reviewed-on: https://chromium-review.googlesource.com/178928
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>
    Commit-Queue: Owen Lin <owenlin@chromium.org>
    Tested-by: Owen Lin <owenlin@chromium.org>

--- client/site_tests/audiovideo_LineOutToMicInLoopback/audiovideo_LineOutToMicInLoopback.py
+++ client/site_tests/audiovideo_LineOutToMicInLoopback/audiovideo_LineOutToMicInLoopback.py
@@ -7,12 +7,17 @@ import logging, os, tempfile
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros.audio import audio_helper
+from autotest_lib.client.cros.audio import alsa_utils
+from autotest_lib.client.cros.audio import cmd_utils
+from autotest_lib.client.cros.audio import cras_utils
+from autotest_lib.client.cros.audio import sox_utils
 
 
 _DEFAULT_NUM_CHANNELS = 2
 _DEFAULT_RECORD_DURATION = 1
 _DEFAULT_VOLUME_LEVEL = 100
 _DEFAULT_CAPTURE_GAIN = 2500
+_DEFAULT_RMS_THRESHOLD = 0.05
 
 
 class audiovideo_LineOutToMicInLoopback(test.test):
@@ -44,66 +49,79 @@ class audiovideo_LineOutToMicInLoopback(test.test):
 
     def run_once(self):
         """Entry point of this test."""
-        audio_helper.set_volume_levels(self._volume_level, self._capture_gain)
+
+        # Config the playback volume and recording gain.
+        cras_utils.set_system_volume(self._volume_level)
+        output_node, _ = cras_utils.get_selected_nodes()
+        cras_utils.set_node_volume(output_node, self._volume_level)
+        cras_utils.set_capture_gain(self._capture_gain)
+
+        # CRAS does not apply the volume and capture gain to ALSA until
+        # streams are added. Do that to ensure the values have been set.
+        cras_utils.playback('-')
+        cras_utils.capture('/dev/null', duration=0.1)
+
         if not audio_helper.check_loopback_dongle():
             raise error.TestError('Audio loopback dongle is in bad state.')
 
         self.loopback_test_hw()
         self.loopback_test_cras()
 
+
+    def _check_rms(self, noise_file, recorded_file):
+        with tempfile.NamedTemporaryFile() as noise_profile,\
+             tempfile.NamedTemporaryFile() as reduced_file:
+            sox_utils.noise_profile(noise_file, noise_profile.name, channels=1)
+            sox_utils.noise_reduce(
+                    recorded_file, reduced_file.name, noise_profile.name,
+                    channels=1)
+            stat = sox_utils.get_stat(reduced_file.name, channels=1)
+            logging.info('stat: %s', str(stat))
+            if stat.rms < _DEFAULT_RMS_THRESHOLD:
+                raise error.TestFail('RMS: %s' % stat.rms)
+
+
     def loopback_test_hw(self):
-        """Uses aplay and arecord to test audio on internal card"""
-        # TODO(hychao): update device parameter for internal card
-        rec_cmd = ('arecord -f dat -d %d -D plughw' % self._record_duration)
+        logging.info('loopback_test_hw')
+        noise_file = os.path.join(self.resultsdir, 'hw_noise.wav')
+        recorded_file = os.path.join(self.resultsdir, 'hw_recorded.wav')
 
         # Record a sample of "silence" to use as a noise profile.
-        with tempfile.NamedTemporaryFile(mode='w+t') as noise_file:
-            logging.info('Noise file: %s', noise_file.name)
-            audio_helper.record_sample(noise_file.name, rec_cmd)
+        alsa_utils.record(
+                noise_file, duration=self._record_duration, channels=1)
 
-            def play_wav(channel):
-                """Plays multitone wav using aplay
+        p1 = cmd_utils.popen(alsa_utils.playback_cmd(
+                self._wav_path, duration=self._record_duration))
+        p2 = cmd_utils.popen(alsa_utils.record_cmd(
+                recorded_file, duration=self._record_duration))
 
-                   @param channel: Unsed variable
-                """
-                cmd = ('aplay -D plughw -d %d %s' %
-                       (self._record_duration, self._wav_path))
-                utils.system(cmd)
+        cmd_utils.wait_and_check_returncode(p1, p2)
+        self._check_rms(noise_file, recorded_file)
 
-            def record_callback(filename):
-                audio_helper.record_sample(filename, rec_cmd);
+        # Keep the file if the above check fails
+        os.unlink(noise_file)
+        os.unlink(recorded_file)
 
-            audio_helper.loopback_test_channels(noise_file.name,
-                                                self.resultsdir,
-                                                play_wav,
-                                                num_channels=self._num_channels,
-                                                record_callback=record_callback)
 
     def loopback_test_cras(self):
         """Uses cras_test_client to test audio on CRAS."""
-        rec_cmd = ('cras_test_client --duration_seconds %d '
-                   '--capture_file' % self._record_duration)
+        logging.info('loopback_test_cras')
+
+        noise_file = os.path.join(self.resultsdir, 'cras_noise.wav')
+        recorded_file = os.path.join(self.resultsdir, 'cras_recorded.wav')
 
         # Record a sample of "silence" to use as a noise profile.
-        with tempfile.NamedTemporaryFile(mode='w+t') as noise_file:
-            logging.info('Noise file: %s', noise_file.name)
-            audio_helper.record_sample(noise_file.name, rec_cmd)
+        cras_utils.capture(
+                noise_file, duration=self._record_duration, channels=1)
 
-            def play_wav(channel):
-                """Plays multitone wav using cras_test_client
+        p1 = cmd_utils.popen(cras_utils.playback_cmd(
+                self._wav_path, duration=self._record_duration))
+        p2 = cmd_utils.popen(cras_utils.capture_cmd(
+                recorded_file, duration=self._record_duration, channels=1))
 
-                   @param channel: Unsed variable
-                """
-                cmd = ('cras_test_client --duration_seconds %d '
-                       '--num_channels 1 --playback_file %s' %
-                       (self._record_duration, self._wav_path))
-                utils.system(cmd)
+        cmd_utils.wait_and_check_returncode(p1, p2)
+        self._check_rms(noise_file, recorded_file)
 
-            def record_callback(filename):
-                audio_helper.record_sample(filename, rec_cmd);
-
-            audio_helper.loopback_test_channels(noise_file.name,
-                                                self.resultsdir,
-                                                play_wav,
-                                                num_channels=self._num_channels,
-                                                record_callback=record_callback)
+        # Keep the file if the above check fails
+        os.unlink(noise_file)
+        os.unlink(recorded_file)
