be6213cfc67e06a85a572a8a96f6ef79e7996841
Owen Lin <owenlin@chromium.org>
2013-12-04 18:14:21 +0800

    audiovideo_LineOutToMicInLoopback - Remove the initialize() function.
    BUG=None
    TEST=Run the test on butterfly
    
    Change-Id: Id3c71071df5289172b727726aef844345f17a0ea
    Reviewed-on: https://chromium-review.googlesource.com/178929
    Reviewed-by: Owen Lin <owenlin@chromium.org>
    Tested-by: Owen Lin <owenlin@chromium.org>
    Commit-Queue: Owen Lin <owenlin@chromium.org>

--- client/site_tests/audiovideo_LineOutToMicInLoopback/audiovideo_LineOutToMicInLoopback.py
+++ client/site_tests/audiovideo_LineOutToMicInLoopback/audiovideo_LineOutToMicInLoopback.py
@@ -13,48 +13,28 @@ from autotest_lib.client.cros.audio import cras_utils
 from autotest_lib.client.cros.audio import sox_utils
 
 
-_DEFAULT_NUM_CHANNELS = 2
-_DEFAULT_RECORD_DURATION = 1
-_DEFAULT_VOLUME_LEVEL = 100
-_DEFAULT_CAPTURE_GAIN = 2500
-_DEFAULT_RMS_THRESHOLD = 0.05
-
+TEST_DURATION = 1
+VOLUME_LEVEL = 100
+CAPTURE_GAIN = 2500
+RMS_THRESHOLD = 0.05
 
 class audiovideo_LineOutToMicInLoopback(test.test):
     """Verifies audio playback and capture function."""
     version = 1
     preserve_srcdir = True
 
-    def initialize(self,
-                   num_channels=_DEFAULT_NUM_CHANNELS,
-                   record_duration=_DEFAULT_RECORD_DURATION,
-                   volume_level=_DEFAULT_VOLUME_LEVEL,
-                   capture_gain=_DEFAULT_CAPTURE_GAIN):
-        """ Setup the deps for the test.
-
-            @param num_channels: The number of channels on the device to test.
-            @param record_duration: How long of a sample to record.
-            @param volume_level: The playback volume to set.
-            @param capture_gain: The capture gain to set.
-        """
-        self._num_channels = num_channels
-        self._record_duration = record_duration
-        self._volume_level = volume_level
-        self._capture_gain = capture_gain
-
-        # Multitone wav file lasts 10 seconds
-        self._wav_path = os.path.join(self.srcdir, '10SEC.wav')
-
-        super(audiovideo_LineOutToMicInLoopback, self).initialize()
 
     def run_once(self):
         """Entry point of this test."""
 
+        # Multitone wav file lasts 10 seconds
+        self._wav_path = os.path.join(self.srcdir, '10SEC.wav')
+
         # Config the playback volume and recording gain.
-        cras_utils.set_system_volume(self._volume_level)
+        cras_utils.set_system_volume(VOLUME_LEVEL)
         output_node, _ = cras_utils.get_selected_nodes()
-        cras_utils.set_node_volume(output_node, self._volume_level)
-        cras_utils.set_capture_gain(self._capture_gain)
+        cras_utils.set_node_volume(output_node, VOLUME_LEVEL)
+        cras_utils.set_capture_gain(CAPTURE_GAIN)
 
         # CRAS does not apply the volume and capture gain to ALSA until
         # streams are added. Do that to ensure the values have been set.
@@ -77,7 +57,7 @@ class audiovideo_LineOutToMicInLoopback(test.test):
                     channels=1)
             stat = sox_utils.get_stat(reduced_file.name, channels=1)
             logging.info('stat: %s', str(stat))
-            if stat.rms < _DEFAULT_RMS_THRESHOLD:
+            if stat.rms < RMS_THRESHOLD:
                 raise error.TestFail('RMS: %s' % stat.rms)
 
 
@@ -87,13 +67,12 @@ class audiovideo_LineOutToMicInLoopback(test.test):
         recorded_file = os.path.join(self.resultsdir, 'hw_recorded.wav')
 
         # Record a sample of "silence" to use as a noise profile.
-        alsa_utils.record(
-                noise_file, duration=self._record_duration, channels=1)
+        alsa_utils.record(noise_file, duration=TEST_DURATION, channels=1)
 
         p1 = cmd_utils.popen(alsa_utils.playback_cmd(
-                self._wav_path, duration=self._record_duration))
+                self._wav_path, duration=TEST_DURATION))
         p2 = cmd_utils.popen(alsa_utils.record_cmd(
-                recorded_file, duration=self._record_duration))
+                recorded_file, duration=TEST_DURATION))
 
         cmd_utils.wait_and_check_returncode(p1, p2)
         self._check_rms(noise_file, recorded_file)
@@ -111,17 +90,16 @@ class audiovideo_LineOutToMicInLoopback(test.test):
         recorded_file = os.path.join(self.resultsdir, 'cras_recorded.wav')
 
         # Record a sample of "silence" to use as a noise profile.
-        cras_utils.capture(
-                noise_file, duration=self._record_duration, channels=1)
+        cras_utils.capture(noise_file, duration=TEST_DURATION, channels=1)
 
         p1 = cmd_utils.popen(cras_utils.playback_cmd(
-                self._wav_path, duration=self._record_duration))
+                self._wav_path, duration=TEST_DURATION))
         p2 = cmd_utils.popen(cras_utils.capture_cmd(
-                recorded_file, duration=self._record_duration, channels=1))
+                recorded_file, duration=TEST_DURATION, channels=1))
 
         cmd_utils.wait_and_check_returncode(p1, p2)
         self._check_rms(noise_file, recorded_file)
 
-        # Keep the file if the above check fails
+        # Keep these files if the above check failed
         os.unlink(noise_file)
         os.unlink(recorded_file)
