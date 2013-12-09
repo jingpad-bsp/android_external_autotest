69739d4b49efb23748c9bf102ecfaadf6a9a5ecb
Doug Anderson <dianders@chromium.org>
2013-12-09 09:19:28 -0800

    platform_HWwatchdog: Handle watchdog timeout dynamically
    Rather than hardcoding that a watchdog reboot will happen in 30
    seconds, query the watchdog to see when the timeout will happen.
    
    BUG=chromium:326608
    TEST=platform_HWwatchdog
    TEST=Comment out "self.client.run('echo "z" > %s' % wd_dev);" and
    platform_HWwatchdog now fails.
    
    Change-Id: Ia8961439414273923fabb9d66a983a94bf68cccf
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/179288
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- server/site_tests/platform_HWwatchdog/platform_HWwatchdog.py
+++ server/site_tests/platform_HWwatchdog/platform_HWwatchdog.py
@@ -6,7 +6,7 @@
 # to make sure it works as expected. The most robust implementations is
 # based on real HW but it doesn't have to be.
 
-import logging
+import logging, re
 
 # http://docs.python.org/2/library/errno.html
 import errno
@@ -36,18 +36,20 @@ class platform_HWwatchdog(test.test):
             raise error.TestError('write to %s failed (%s)' %
                                   (wd_dev, errno.errorcode[e.errno]))
 
-        logging.info("KernelHWpath: tickled watchdog on %s",
-                     self.client.hostname)
+        logging.info("KernelHWpath: tickled watchdog on %s (%ds to reboot)",
+                     self.client.hostname, self._hw_interval)
 
         # machine should became unpingable after lockup
-        if not self.client.wait_down(timeout=30):
-            raise error.TestError('machine should be unpingable'
-                                  'within 30 seconds')
+        # ...give 5 seconds slack...
+        wait_down = self._hw_interval + 5
+        if not self.client.wait_down(timeout=wait_down):
+            raise error.TestError('machine should be unpingable '
+                                  'within %d seconds' % wait_down)
 
         # make sure the machine comes back,
         # DHCP can take up to 45 seconds in odd cases.
         if not self.client.wait_up(timeout=60):
-            raise error.TestError('machine did not reboot/ping within'
+            raise error.TestError('machine did not reboot/ping within '
                                   '60 seconds of HW reset')
 
     def _exists_on_client(self, wd_dev):
@@ -62,6 +64,12 @@ class platform_HWwatchdog(test.test):
     def _start_daemon(self):
         self.client.run('start daisydog', ignore_status=True)
 
+    def _query_hw_interval(self):
+        """Check how long the hardware interval is."""
+        output = self.client.run('daisydog -c').stdout
+        secs = re.findall(r'HW watchdog interval is (\d*) seconds', output)[0]
+        return int(secs)
+
     def run_once(self, host=None):
         self.client = host
         wd_dev = '/dev/watchdog'
@@ -72,6 +80,7 @@ class platform_HWwatchdog(test.test):
             return
 
         self._stop_daemon()
+        self._hw_interval = self._query_hw_interval()
         self._stop_watchdog(wd_dev)
         self._trig_watchdog(wd_dev)
         self._start_daemon()
