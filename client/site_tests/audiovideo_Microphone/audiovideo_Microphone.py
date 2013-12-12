1cbf279a7662cc8ece7e6bc457b36384a2169e94
Owen Lin <owenlin@chromium.org>
2013-12-12 17:53:59 +0800

    audiovideo_Microphone - Remove the check for "Capture Switch"
    CRAS will turn it on when needed, so there is nothing wrong with the value
    as off by default. We should add another test to verify CRAS does turn
    it on (crbug.com/328239).
    
    BUG=chromium:307384
    TEST=Run the test on mario
    
    Change-Id: I7216c956c28900e909446f57a8bf613183df4e67
    Reviewed-on: https://chromium-review.googlesource.com/179765
    Reviewed-by: Owen Lin <owenlin@chromium.org>
    Commit-Queue: Owen Lin <owenlin@chromium.org>
    Tested-by: Owen Lin <owenlin@chromium.org>

--- client/site_tests/audiovideo_Microphone/audiovideo_Microphone.py
+++ client/site_tests/audiovideo_Microphone/audiovideo_Microphone.py
@@ -2,13 +2,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging
 import os
 import tempfile
 
 from autotest_lib.client.bin import test
-from autotest_lib.client.bin import utils
-
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros.audio import alsa_utils
 
@@ -37,26 +34,8 @@ class audiovideo_Microphone(test.test):
 
 
     def run_once(self):
-        sndcard_id = alsa_utils.get_default_soundcard_id()
-
-        # Microphone should be on by default.
-        if utils.get_cpu_arch() != "arm":
-            cmd = 'amixer -D hw:%d cget name="Capture Switch"'
-            output = utils.system_output(cmd % sndcard_id)
-            if 'values=on,on' not in output:
-                logging.error(output)
-                raise error.TestFail('The microphone is not on by default.')
-        else:
-            # TODO(jiesun): find consistent way to find the ALSA mixer control
-            # names for both internal mic and external mic on ARM, which is
-            # independent of the audio codec hardware vendor.
-            logging.warning("Can not verify the microphone capture switch.")
-
         # Mono and stereo capturing should work fine @ 44.1KHz and 48KHz.
         self.verify_capture(1, 44100)
         self.verify_capture(1, 48000)
         self.verify_capture(2, 48000)
         self.verify_capture(2, 44100)
-
-        # TODO(zhurunz):
-        # Low latency capturing should work fine with low CPU usage.
