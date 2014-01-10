9e8960bfd7debc17cf3be98578416c0cbc6e692c
Christopher Wiley <wiley@chromium.org>
2014-01-10 15:55:48 -0800

    autotest: Add signal level logging to VerifyRouter test
    This allows us to check that router signal levels are at
    expected levels for all kinds of setups.
    
    BUG=None
    TEST=Test continues to pass.
    
    Change-Id: I835b97b980c53e527d7e1f8aa90f454f39d9b30c
    Reviewed-on: https://chromium-review.googlesource.com/182187
    Tested-by: Christopher Wiley <wiley@chromium.org>
    Reviewed-by: Kris Rambish <krisr@chromium.org>
    Commit-Queue: Christopher Wiley <wiley@chromium.org>

--- server/site_tests/network_WiFi_VerifyRouter/network_WiFi_VerifyRouter.py
+++ server/site_tests/network_WiFi_VerifyRouter/network_WiFi_VerifyRouter.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
+
 from autotest_lib.client.common_lib.cros.network import xmlrpc_datatypes
 from autotest_lib.server import site_linux_system
 from autotest_lib.server.cros.network import hostap_config
@@ -25,3 +27,5 @@ class network_WiFi_VerifyRouter(wifi_cell_test_base.WiFiCellTestBase):
             client_conf = xmlrpc_datatypes.AssociationParameters(
                     ssid=self.context.router.get_ssid(instance=instance))
             self.context.assert_connect_wifi(client_conf)
+            logging.info('Signal level for AP %d is %d', instance,
+                         self.context.client.wifi_signal_level)
