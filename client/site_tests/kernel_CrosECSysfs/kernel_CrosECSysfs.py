5a8b8505b428348270bd6cfd2b1eb008463eeb02
Bill Richardson <wfrichar@chromium.org>
2014-01-14 15:44:13 -0800

    Add kernel_CrosECSysfs to verify sysfs interface to EC
    This checks that the sysfs interface to the Chrome OS EC is working. The
    test just ensures that the latest attributes are present and providing
    semi-useful data. We don't try to confirm the actual data, since that can
    vary widely between systems, just that *something* is readable without errors.
    
    BUG=chromium:334014
    TEST=Uh, it's a test. Run it.
    
    Change-Id: Ic12ea8a393c6d5f95901ecfa4fa1041bce7c3932
    Signed-off-by: Bill Richardson <wfrichar@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/182654

---
+++ client/site_tests/kernel_CrosECSysfs/kernel_CrosECSysfs.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging, os
+from autotest_lib.client.bin import utils, test
+from autotest_lib.client.common_lib import error
+
+class kernel_CrosECSysfs(test.test):
+    '''Make sure the EC sysfs interface provides meaningful output'''
+    version = 1
+
+    cros_ec = '/dev/cros_ec'
+    sysfs_path = '/sys/devices/virtual/chromeos/cros_ec'
+
+    def _read_file(self, filename):
+        """
+        Return the contents of the given file or fail.
+
+        @param filename Full path to the file to be read
+        """
+        try:
+            content = utils.read_file(filename)
+        except Exception as err:
+            raise error.TestFail('sysfs file problem: %s' % err)
+        return content
+
+    def _read_sysfs(self, filename):
+        """
+        Read the contents of the given sysfs file or fail
+
+        @param filename Name of the file within the sysfs interface directory
+        """
+        fullpath = os.path.join(self.sysfs_path, filename)
+        return self._read_file(fullpath)
+
+    def _read_field(self, filename, field):
+        """
+        Return the given field from the sysfs file or fail
+
+        @param filename Name of the file within the sysfs interface directory
+        @param field Name of field to match in the file content
+        """
+        fullpath = os.path.join(self.sysfs_path, filename)
+        content = self._read_file(fullpath)
+        match = utils.get_field(content, 0, field)
+        if match is None:
+            raise error.TestFail("no '%s' field in %s" % (field, fullpath))
+        return match
+
+    def run_once(self):
+        """
+        Quick check for the existence of the basic sysfs files
+        """
+        # If /dev/cros_ec isn't present, then the MFD_CROS_EC_DEV driver isn't
+        # present, so there's no point to looking for the sysfs interface to it.
+        try:
+            dontcase = utils.read_file(self.cros_ec)
+        except Exception as err:
+            raise error.TestFail("driver not present? %s" % err)
+
+        flashsize = self._read_field('flashinfo', 'FlashSize')
+        logging.info("flashsize is %s", flashsize)
+
+        build = self._read_field('version', 'Build info:')
+        logging.info("build is %s", build)
+
+        reboot = self._read_sysfs('reboot')
+        if reboot.find("ro") < 0:
+            raise error.TestFail('reboot help is weird: %s' % reboot)
+        logging.info("reboot is %s", reboot)
