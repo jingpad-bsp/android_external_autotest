5c3baf688df7cf7802ca70f379af004e68d9e330
Vic (Chun-Ju) Yang <victoryang@chromium.org>
2014-01-24 16:57:14 +0800

    Add FAFT config for Rambi
    This is needed for FAFT to run on Rambi.
    
    BUG=chrome-os-partner:25211
    TEST=Run FAFT on Rambi
    
    Change-Id: I2f87a3ae6d16354c615e3949bda227cfcf110f79
    Signed-off-by: Vic (Chun-Ju) Yang <victoryang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/183627
    Reviewed-by: Yusuf Mohsinally <mohsinally@chromium.org>

---
+++ server/cros/faft/config/rambi.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""FAFT config setting overrides for Rambi."""
+
+
+class Values(object):
+    """FAFT config values for Rambi."""
+    firmware_screen = 7
+    dev_screen = 4
+    chrome_ec = True
+    long_rec_combo = True
+    ec_capability = ['battery', 'charging', 'keyboard', 'lid', 'x86',
+                     'usb', 'smart_usb_charge']
+    wp_voltage = 'pp1800'
