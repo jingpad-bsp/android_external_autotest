e535883f7b2ae0ec5b5d16885cfe8b346d5bf37a
Kees Cook <keescook@chromium.org>
2014-01-24 13:07:33 -0800

    security_ChromiumOSLSM: do not canonicalize mount
    Do not resolve symlinks when running the mount tests. Also generally
    rewrote this test to avoid lots of copy/paste code.
    
    BUG=chromium:337667
    TEST=passes again on rambi
    
    Change-Id: I00a42bfbfbbe664d871a585a89b02c58bc0d276b
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/183802
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- client/site_tests/security_ChromiumOSLSM/security_ChromiumOSLSM.py
+++ client/site_tests/security_ChromiumOSLSM/security_ChromiumOSLSM.py
@@ -18,29 +18,42 @@ class security_ChromiumOSLSM(test.test):
     """
     version = 1
 
+    def _mount(self, target):
+        cmd = "mount -c -n -t tmpfs -o nodev,noexec,nosuid test %s" % (target)
+        return utils.system(cmd, ignore_status=True)
+
+    def _umount(self, target):
+        utils.system('umount -n %s' % (target))
+
+    def _check_mount(self, target, expected, msg):
+        succeeded = (self._mount(target) == 0)
+        if succeeded:
+            self._umount(target)
+        if succeeded != expected:
+            logging.error(msg)
+            return 1
+        return 0
+
     def run_once(self):
         errors = 0
-        test_directory= '/tmp/chromium_lsm_test_directory'
-        os.mkdir(test_directory,0700)
-        os.mkdir(test_directory + '/mount_point',0700)
-        os.symlink('mount_point',test_directory + '/symlink')
-        result = utils.system("mount -n -t tmpfs -o nodev,noexec,nosuid test " \
-            + test_directory + "/symlink",ignore_status=True)
-        # Mounting should fail as we used a mount path with a symbolic link.
-        if result == 0:
-            utils.system('umount ' + test_directory + '/symlink')
-            logging.error('Failed symbolic link mount point test')
-            errors += 1
-        result = utils.system("mount -n -t tmpfs -o nodev,noexec,nosuid test " \
-            + test_directory + "/mount_point",ignore_status=True)
-        # Mounting should succeed (no symbolic link in mount path).
-        if result != 0:
-            logging.error('Failed regular mount')
-            errors += 1
-        else:
-            utils.system('umount ' + test_directory + '/mount_point')
+        test_dir = '/tmp/chromium_lsm_test_dir'
+        os.mkdir(test_dir, 0700)
 
-        utils.system('rm -rf ' + test_directory)
+        mnt_target = '%s/mount_point' % (test_dir)
+        os.mkdir(mnt_target, 0700)
+
+        sym_target = '%s/symlink' % (test_dir)
+        os.symlink('mount_point', sym_target)
+
+        # Mounting should succeed (no symbolic link in mount path).
+        errors += self._check_mount(mnt_target, True,
+                                    'Unable to mount on a directory')
+
+        # Mounting should fail as we used a mount path with a symbolic link.
+        errors += self._check_mount(sym_target, False,
+                                    'Unexpectedly mounted on a symlink')
+
+        utils.system('rm -rf ' + test_dir)
         # If self.error is not zero, there were errors.
         if errors > 0:
             raise error.TestFail('Failed %d tests' % errors)
