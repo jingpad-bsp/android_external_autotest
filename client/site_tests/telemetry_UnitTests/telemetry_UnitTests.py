bc3e8f10a0bdc5378d069a0fe757dbd42f0a798f
Achuith Bhandarkar <achuith@chromium.org>
2014-01-24 17:43:20 -0800

    telemetry_UnitTests cleanup.
    * Support for running a subset of unit tests and perf tests.
    * Only run BrowserTest from telemetry unit tests in guest mode.
    * Replace _TESTS with local var test_specs.
    * Move TestSpec to outer scope.
    
    BUG=chromium:337582
    TEST=trybot
    
    Change-Id: Ia0552bd36461e39ddb6281cfab988503551d19a9
    Reviewed-on: https://chromium-review.googlesource.com/184031
    Tested-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Dan Shi <dshi@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>

--- client/site_tests/telemetry_UnitTests/telemetry_UnitTests.py
+++ client/site_tests/telemetry_UnitTests/telemetry_UnitTests.py
@@ -15,31 +15,28 @@ TELEMETRY_SUBDIR = os.path.join(TELEMETRY_BASE_DIR, 'telemetry')
 PERF_BASE_DIR = os.path.join(TOOLS_BASE_DIR, 'perf')
 
 
+class TestSpec(object):
+    """Test specification class with directory paths, etc."""
+    def __init__(self, test_type, root, top_level_dir, tests):
+        self.test_type = test_type
+        self.root = root
+        self.top_level_dir = top_level_dir
+        self.tests = tests
+        self.num_errors = 0
+        self.error_details = ''
+
+    def error_string(self):
+        """Printable error string. Returns empty string if no errors."""
+        if self.num_errors:
+            return ('%d %s unit tests failed:\n%s' %
+                    (self.num_errors, self.test_type,
+                    self.error_details))
+        return ''
+
+
 class telemetry_UnitTests(test.test):
     """This is a client side wrapper for the Telemetry unit tests."""
     version = 1
-
-    class TestSpec(object):
-        """Test specification class with directory paths, etc."""
-        def __init__(self, test_type, root, top_level_dir):
-            self.test_type = test_type
-            self.root = root
-            self.top_level_dir = top_level_dir
-            self.num_errors = 0
-            self.error_details = ''
-
-        def error_string(self):
-            """Printable error string. Returns empty string if no errors."""
-            if self.num_errors:
-                return ('%d %s unit tests failed:\n%s' %
-                        (self.num_errors, self.test_type,
-                        self.error_details))
-            return ''
-
-    _TESTS = [
-        TestSpec('telemetry', TELEMETRY_SUBDIR, TELEMETRY_BASE_DIR),
-        TestSpec('perf', PERF_BASE_DIR, PERF_BASE_DIR),
-    ]
 
     def _run_tests(self, browser_type, test_spec):
         """run unit tests in given directory and browser type.
@@ -55,8 +52,9 @@ class telemetry_UnitTests(test.test):
         capturer = StringIO.StringIO()
         sys.stdout = capturer
         runner = gtest_testrunner.GTestTestRunner(print_result_after_run=False)
-        run_tests.Main(['--browser=' + browser_type],
-                       test_spec.root, test_spec.top_level_dir, runner)
+        for test in test_spec.tests:
+            run_tests.Main(['--browser=%s' % browser_type, test],
+                           test_spec.root, test_spec.top_level_dir, runner)
 
         if runner.result:
             # The PrintSummary() below is captured in the test debug log file.
@@ -79,11 +77,11 @@ class telemetry_UnitTests(test.test):
             else:
                 logging.info('All %d %s unit tests passed.',
                              len(runner.result.successes), test_spec.test_type)
-        else:
+        elif test_spec.tests:
             raise error.TestFail('No results found.')
 
 
-    def run_once(self, browser_type):
+    def run_once(self, browser_type, unit_tests=[''], perf_tests=['']):
         """
         Runs telemetry/perf unit tests.
 
@@ -91,7 +89,13 @@ class telemetry_UnitTests(test.test):
 
         """
         error_str = ''
-        for test_spec in self._TESTS:
+        test_specs = [
+            TestSpec('telemetry', TELEMETRY_SUBDIR, TELEMETRY_BASE_DIR,
+                     unit_tests),
+            TestSpec('perf', PERF_BASE_DIR, PERF_BASE_DIR, perf_tests),
+        ]
+
+        for test_spec in test_specs:
             self._run_tests(browser_type, test_spec)
             error_str += test_spec.error_string()
         if error_str:
