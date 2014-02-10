33d99110b9d482b09b4bf9b30f3bd48b6d9ad4cd
Katherine Threlkeld <kathrelkeld@chromium.org>
2014-02-10 11:58:55 -0800

    Telemetry based version of desktopui_SimpleLogin test.
    Replacing the pyauto version with a telemetry version.  This test is only
    called by USB stress tests which are not run in the lab and is a very simple
    change, so there is no need for a separate telemetry_ version.
    
    BUG=None
    TEST=ran it
    
    Change-Id: Ie605516bb115153d2bf2415eefb02a1676b8590f
    Reviewed-on: https://chromium-review.googlesource.com/185707
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Katherine Threlkeld <kathrelkeld@chromium.org>
    Tested-by: Katherine Threlkeld <kathrelkeld@chromium.org>

--- client/site_tests/desktopui_SimpleLogin/desktopui_SimpleLogin.py
+++ client/site_tests/desktopui_SimpleLogin/desktopui_SimpleLogin.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -6,26 +6,24 @@ import logging
 import os
 import time
 
-from autotest_lib.client.cros import cros_ui_test, httpd
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib.cros import chrome
 
 
-class desktopui_SimpleLogin(cros_ui_test.UITest):
+class desktopui_SimpleLogin(test.test):
+    """Login and wait until exit flag file is seen."""
     version = 1
 
 
-    def initialize(self):
-        super(desktopui_SimpleLogin, self).initialize(creds='$default')
-        self._testServer = httpd.HTTPListener(8000, docroot=self.bindir)
-        self._testServer.run()
-
-
     def run_once(self):
+        """Entrance point for test."""
         terminate_path = '/tmp/simple_login_exit'
         if os.path.isfile(terminate_path):
             os.remove(terminate_path)
 
-        while True:
-            time.sleep(1)
-            if os.path.isfile(terminate_path):
-                logging.info('Exit flag detected, exiting.')
-                return
+        with chrome.Chrome():
+            while True:
+                time.sleep(1)
+                if os.path.isfile(terminate_path):
+                    logging.info('Exit flag detected; exiting.')
+                    return
