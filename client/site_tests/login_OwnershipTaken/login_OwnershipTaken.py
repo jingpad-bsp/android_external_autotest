c25b8ee227b0194da2bab175bcb67e23b1b41254
Chris Masone <cmasone@chromium.org>
2014-02-24 15:23:17 -0800

    Replace OwnershipTaken with OwnershipTakenTelemetry
    The old test is still flaking sometimes, and the new test has a good
    track record in the regression suite.
    Toss out the old, and replace with the new -- keeping the new test EXPERIMENTAL
    for now.
    
    BUG=chromium:338528
    TEST=run this test
    
    Change-Id: I1c64543ca339f39c951803cc10b3f7a9c89eb0e0
    Reviewed-on: https://chromium-review.googlesource.com/187725
    Reviewed-by: Chris Masone <cmasone@chromium.org>
    Commit-Queue: Chris Masone <cmasone@chromium.org>
    Tested-by: Chris Masone <cmasone@chromium.org>

--- client/site_tests/login_OwnershipTaken/login_OwnershipTaken.py
+++ client/site_tests/login_OwnershipTaken/login_OwnershipTaken.py
@@ -1,37 +1,60 @@
-# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
-from autotest_lib.client.bin import utils
+import os, sys
+
+from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.common_lib.cros import session_manager
-from autotest_lib.client.cros import constants, cros_ui_test, login
+from autotest_lib.client.common_lib.cros import chrome, session_manager
+from autotest_lib.client.cros import constants, login, ownership
 
 
-class login_OwnershipTaken(cros_ui_test.UITest):
+class login_OwnershipTaken(test.test):
     """Sign in and ensure that ownership of the device is taken."""
     version = 1
+
 
     def setup(self):
         os.chdir(self.srcdir)
         utils.make('OUT_DIR=.')
 
 
-    def initialize(self, creds='$default'):
-        self.auto_login = False  # Will log in manually later.
-        super(login_OwnershipTaken, self).initialize(creds,
-                                                     is_creating_owner=True)
+    def initialize(self):
+        ownership.clear_ownership_files()
         if os.access(constants.OWNER_KEY_FILE, os.F_OK):
-            raise error.TestFail("Ownership already taken!")
+            raise error.TestError('Ownership already taken!')
+
+
+    def _validate_policy(self, retrieved_policy, username):
+        # Pull in protobuf definitions.
+        sys.path.append(self.srcdir)
+        from chrome_device_policy_pb2 import ChromeDeviceSettingsProto
+        from chrome_device_policy_pb2 import UserWhitelistProto
+        from device_management_backend_pb2 import PolicyData
+        from device_management_backend_pb2 import PolicyFetchResponse
+
+        response_proto = PolicyFetchResponse()
+        response_proto.ParseFromString(retrieved_policy)
+        ownership.assert_has_policy_data(response_proto)
+
+        poldata = PolicyData()
+        poldata.ParseFromString(response_proto.policy_data)
+        ownership.assert_has_device_settings(poldata)
+        ownership.assert_username(poldata, username)
+
+        polval = ChromeDeviceSettingsProto()
+        polval.ParseFromString(poldata.policy_value)
+        ownership.assert_new_users(polval, True)
+        ownership.assert_users_on_whitelist(polval, (username,))
 
 
     def run_once(self):
-        self.login(self.username, self.password)
-        login.wait_for_ownership()
+        with chrome.Chrome() as cr:
+            login.wait_for_ownership()
 
-        sm = session_manager.connect()
-        retrieved_policy = sm.RetrievePolicy(byte_arrays=True)
-        if retrieved_policy is None:
-            raise error.TestFail('Policy not found')
-        self.validate_basic_policy(retrieved_policy)
+            sm = session_manager.connect()
+            retrieved_policy = sm.RetrievePolicy(byte_arrays=True)
+            if retrieved_policy is None:
+                raise error.TestFail('Policy not found.')
+            self._validate_policy(retrieved_policy, cr.username)
