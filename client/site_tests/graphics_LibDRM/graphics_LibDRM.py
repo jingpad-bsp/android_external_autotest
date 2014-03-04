4e7605eb3ab7c71f4b18e098d20a416f250f8b80
Daniel Kurtz <djkurtz@chromium.org>
2014-03-03 17:44:58 -0800

    graphics_LibDRM: stop ui before running tests
    The libDRM tests are not supposed to work if another process is the DRM
    Master.  So, stop ui to kill the X server, which should ensure that the
    drm tests can do what they want.
    
    See:
    http://lists.freedesktop.org/archives/dri-devel/2014-March/054936.html
    
    Note: These tests generally use the legacy idiom "drmOpen(name, NULL)" to
    open a connection to the kernel (/dev/dri/card0).  This is really only
    supposed to work if nobody already has a connection open (i.e.,
    drmGetBusid() does return a bus ID).  However, there is a backdoor in
    drmOpen() that, after the drmGetBusid() check fails, uses procfs to read
    the device's "bus" name, and, if it matches the provided "name", calls
    drmOpenByBusid() using the busid from procfs.  This procfs method has two
    problems:
      (a) it doesn't work on exynos, since exynos returns "exynos" as the name
    from drmGetVersion(), but uses "exynos-drm" as the bus (platform) driver
    name.
      (b) the drm procfs has been removed in 3.12 by:
    commit cb6458f97b53d7f73043206c18014b3ca63ac345
    Author: Daniel Vetter <daniel.vetter@ffwll.ch>
    Date:   Thu Aug 8 15:41:34 2013 +0200
        drm: remove procfs code, take 2
    
    Note: also cleans up some checkpatch warnings about
      "Specify string format arguments as logging function parameters"
    
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    
    BUG=chromium:220327
    TEST=test_that <daisy> graphics_LibDRM
      => test passes (only modetest is attempted)
    TEST=test_that <link> graphics_LibDRM
      => all tests pass
    
    Change-Id: I7d5e46b15eb3b9fd23f4e727b027416761a1c96a
    Reviewed-on: https://chromium-review.googlesource.com/188661
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Ilja Friedel <ihf@chromium.org>
    Reviewed-by: Dominik Behr <dbehr@chromium.org>
    Tested-by: Dominik Behr <dbehr@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>

--- client/site_tests/graphics_LibDRM/graphics_LibDRM.py
+++ client/site_tests/graphics_LibDRM/graphics_LibDRM.py
@@ -5,9 +5,18 @@ import logging
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
+from autotest_lib.client.cros import service_stopper
 
 class graphics_LibDRM(test.test):
     version = 1
+    _services = None
+
+    def initialize(self):
+        self._services = service_stopper.ServiceStopper(['ui'])
+
+    def cleanup(self):
+        if self._services:
+           self._services.restore_services()
 
     def run_once(self):
         num_errors = 0
@@ -23,14 +32,17 @@ class graphics_LibDRM(test.test):
                        'x86_64': tests_intel }
         arch = utils.get_cpu_arch()
         if not arch in arch_tests:
-            raise error.TestFail('Architecture "%s" not supported.' % arch)
+            raise error.TestFail('Architecture "%s" not supported.', arch)
         tests = tests_common + arch_tests[arch]
+
+        # If UI is running, we must stop it and restore later.
+        self._services.stop_services()
 
         for test in tests:
             # Make sure the test exists on this system.  Not all tests may be
             # present on a given system.
             if utils.system('which %s' % test):
-                logging.error('Could not find test %s.' % test)
+                logging.error('Could not find test %s.', test)
                 keyvals[test] = 'NOT FOUND'
                 num_errors += 1
                 continue
@@ -38,7 +50,7 @@ class graphics_LibDRM(test.test):
             # Run the test and check for success based on return value.
             return_value = utils.system(test)
             if utils.system(test):
-                logging.error('%s returned %d' % (test, return_value))
+                logging.error('%s returned %d', test, return_value)
                 num_errors += 1
                 keyvals[test] = 'FAILED'
             else:
