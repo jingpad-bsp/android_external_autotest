45099fcbb65db5d7c22563bc3d8fcdbfcd3a7240
Puthikorn Voravootivat <puthik@chromium.org>
2014-03-12 15:28:54 -0700

    Fix platform_MemCheck to correctly check swap memory size
    The test was assume that swap is turned off which is not true now
    
    BUG=chromium:351921
    TEST=Tested in spring. Test passed
    
    Change-Id: If02b7b4f12970a29c9ad03829d276ae4c75fa226
    Reviewed-on: https://chromium-review.googlesource.com/189738
    Reviewed-by: Grant Grundler <grundler@chromium.org>
    Commit-Queue: Puthikorn Voravootivat <puthik@chromium.org>
    Tested-by: Puthikorn Voravootivat <puthik@chromium.org>

--- client/site_tests/platform_MemCheck/platform_MemCheck.py
+++ client/site_tests/platform_MemCheck/platform_MemCheck.py
@@ -7,7 +7,6 @@
 __author__ = 'kdlucas@chromium.org (Kelly Lucas)'
 
 import logging
-import os
 
 from autotest_lib.client.bin import utils, test
 from autotest_lib.client.common_lib import error
@@ -18,6 +17,7 @@ class platform_MemCheck(test.test):
     Verify memory usage looks correct.
     """
     version = 1
+    swap_disksize_file = '/sys/block/zram0/disksize'
 
     def run_once(self):
         errors = 0
@@ -33,13 +33,17 @@ class platform_MemCheck(test.test):
             memref = 700000
             vmemref = 210000
 
+        # size reported in /sys/block/zram0/disksize is in byte
+        swapref = int(utils.read_one_line(self.swap_disksize_file)) / 1024
+
         less_refs = ['MemTotal', 'MemFree', 'VmallocTotal']
-        equal_refs = ['SwapCached', 'SwapTotal']
+        equal_refs = ['SwapCached']
+        approx_refs = ['SwapTotal']
 
         ref = {'MemTotal': memref,
-               'MemFree': memref/2,
+               'MemFree': memref / 2,
                'SwapCached': 0,
-               'SwapTotal': 0,
+               'SwapTotal': swapref,
                'VmallocTotal': vmemref,
               }
 
@@ -47,13 +51,18 @@ class platform_MemCheck(test.test):
             value = utils.read_from_meminfo(k)
             if k in less_refs:
                 if value < ref[k]:
-                    logging.warn('%s is %d' % (k, value))
-                    logging.warn('%s should be at least %d' % (k, ref[k]))
+                    logging.warn('%s is %d', k, value)
+                    logging.warn('%s should be at least %d', k, ref[k])
                     errors += 1
             elif k in equal_refs:
                 if value != ref[k]:
-                    logging.warn('%s is %d' % (k, value))
-                    logging.warn('%s should be %d' % (k, ref[k]))
+                    logging.warn('%s is %d', k, value)
+                    logging.warn('%s should be %d', k, ref[k])
+                    errors += 1
+            elif k in approx_refs:
+                if value < ref[k] * 0.9 or ref[k] * 1.1 < value:
+                    logging.warn('%s is %d', k, value)
+                    logging.warn('%s should be within 10%% of %d', k, ref[k])
                     errors += 1
 
         # If self.error is not zero, there were errors.
