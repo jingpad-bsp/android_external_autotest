692ad0b5807dee5932d4eac15b82e4351b516d77
Todd Broch <tbroch@chromium.org>
2014-03-20 15:21:00 -0700

    platform_ChromeCgroups: convert from pyauto to telemetry.
    Note, test previously visited live websites.  I've transitioned it to
    visiting local URLs instead (about:blank & chrome:system).
    
    BUG=chromium:344996
    TEST=platform_ChromeCgroups test completes successfully
    
    Change-Id: I7a5e02c2a6cbd880ca864c1ba3f7f6c36be4851b
    Reviewed-on: https://chromium-review.googlesource.com/190940
    Tested-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

--- client/site_tests/platform_ChromeCgroups/platform_ChromeCgroups.py
+++ client/site_tests/platform_ChromeCgroups/platform_ChromeCgroups.py
@@ -4,14 +4,15 @@
 
 import logging, os
 
-from autotest_lib.client.cros import cros_ui_test
+from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib.cros import chrome
 
 CGROUP_DIR = '/sys/fs/cgroup/cpu/chrome_renderers'
 FG_CGROUP_DIR = os.path.join(CGROUP_DIR, 'foreground')
 BG_CGROUP_DIR = os.path.join(CGROUP_DIR, 'background')
 
-class platform_ChromeCgroups(cros_ui_test.UITest):
+class platform_ChromeCgroups(test.test):
     version = 1
 
     def _get_cgroup_tasks(self, cgroup_dir):
@@ -26,7 +27,7 @@ class platform_ChromeCgroups(cros_ui_test.UITest):
             raise error.TestError('failed to open %s' % task_path)
         tasks = set(line.rstrip() for line in task_file.readlines())
         task_file.close()
-        logging.info('tasks in cgroup %s: %s' % (cgroup_dir, ','.join(tasks)))
+        logging.info('tasks in cgroup %s: %s', cgroup_dir, ','.join(tasks))
         return tasks
 
     def run_once(self):
@@ -34,21 +35,26 @@ class platform_ChromeCgroups(cros_ui_test.UITest):
         Check that the chrome_renderers cgroups are created and that tasks
         are placed in them.
         """
-        # Make sure the cgroup directories actually exist.
-        if not os.path.isdir(CGROUP_DIR):
-            raise error.TestFail('chrome_renderers cgroup does not exist')
-        if not os.path.isdir(FG_CGROUP_DIR):
-            raise error.TestFail('foreground cgroup does not exist')
-        if not os.path.isdir(BG_CGROUP_DIR):
-            raise error.TestFail('background cgroup does not exist')
+        with chrome.Chrome() as cr:
+            # Make sure the cgroup directories actually exist.
+            if not os.path.isdir(CGROUP_DIR):
+                raise error.TestFail('chrome_renderers cgroup does not exist')
+            if not os.path.isdir(FG_CGROUP_DIR):
+                raise error.TestFail('foreground cgroup does not exist')
+            if not os.path.isdir(BG_CGROUP_DIR):
+                raise error.TestFail('background cgroup does not exist')
 
-        # Open up two tabs in the same window. One should be in the foreground
-        # while the other is in the background.
-        self.pyauto.NavigateToURL('http://www.google.com')
-        self.pyauto.AppendTab('http://www.youtube.com')
+            # Open up two tabs in the same window. One should be in the foreground
+            # while the other is in the background.
+            tab1 = cr.browser.tabs[0]
+            tab1.Navigate('about:blank')
+            tab1.WaitForDocumentReadyStateToBeComplete()
+            tab2 = cr.browser.tabs.New()
+            tab2.Navigate('chrome:system')
+            tab2.WaitForDocumentReadyStateToBeComplete()
 
-        # Make sure the foreground and background cgroups are non-empty.
-        if not self._get_cgroup_tasks(FG_CGROUP_DIR):
-            raise error.TestFail('no tasks in foreground cgroup')
-        if not self._get_cgroup_tasks(BG_CGROUP_DIR):
-            raise error.TestFail('no tasks in background cgroup')
+            # Make sure the foreground and background cgroups are non-empty.
+            if not self._get_cgroup_tasks(FG_CGROUP_DIR):
+                raise error.TestFail('no tasks in foreground cgroup')
+            if not self._get_cgroup_tasks(BG_CGROUP_DIR):
+                raise error.TestFail('no tasks in background cgroup')
