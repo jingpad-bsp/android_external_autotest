cae02be48272116f2d18ba15872b2a799712cf7e
Puthikorn Voravootivat <puthik@chromium.org>
2014-03-20 17:10:41 -0700

    platform_MemCheck: Add memory timings check and improve size check
    Change 4 things:
    - Check that memory speed is at least DDR3-1333
    - Change the minimum total memory size that should be reported
      by kernel to scale with the physical size of the memory
    - Remove SwapCached = 0 check
    - Add keyval report for each result of the test
    
    BUG=chromium:351921
    TEST=Tesed in peach_pit. Test passed. Get expected value in log.
    
    Change-Id: I3b57a0f7f2f70dcb5a74d893aeba92299379c12d
    Reviewed-on: https://chromium-review.googlesource.com/191001
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Puthikorn Voravootivat <puthik@chromium.org>
    Tested-by: Puthikorn Voravootivat <puthik@chromium.org>

--- client/site_tests/platform_MemCheck/platform_MemCheck.py
+++ client/site_tests/platform_MemCheck/platform_MemCheck.py
@@ -6,7 +6,7 @@
 
 __author__ = 'kdlucas@chromium.org (Kelly Lucas)'
 
-import logging
+import logging, re
 
 from autotest_lib.client.bin import utils, test
 from autotest_lib.client.common_lib import error
@@ -21,6 +21,7 @@ class platform_MemCheck(test.test):
 
     def run_once(self):
         errors = 0
+        keyval = dict()
         # The total memory will shrink if the system bios grabs more of the
         # reserved memory. We derived the value below by giving a small
         # cushion to allow for more system BIOS usage of ram. The memref value
@@ -33,31 +34,41 @@ class platform_MemCheck(test.test):
             memref = 700000
             vmemref = 210000
 
+        speedref = 1333
+        os_reserve = 600000
+
         # size reported in /sys/block/zram0/disksize is in byte
         swapref = int(utils.read_one_line(self.swap_disksize_file)) / 1024
 
         less_refs = ['MemTotal', 'MemFree', 'VmallocTotal']
-        equal_refs = ['SwapCached']
         approx_refs = ['SwapTotal']
+
+        # read physical HW size from mosys and adjust memref if need
+        cmd = 'mosys memory spd print geometry -s size_mb'
+        phy_size_run = utils.run(cmd)
+        phy_size = 0
+        for line in phy_size_run.stdout.split():
+            phy_size += int(line)
+        logging.info('Physical memory size is %d MB', phy_size)
+        # memref is in KB but phy_size is in MB
+        phy_size *= 1024
+        keyval['PhysicalSize'] = phy_size
+        memref = max(memref, phy_size - os_reserve)
 
         ref = {'MemTotal': memref,
                'MemFree': memref / 2,
-               'SwapCached': 0,
                'SwapTotal': swapref,
                'VmallocTotal': vmemref,
               }
 
+
         for k in ref:
             value = utils.read_from_meminfo(k)
+            keyval[k] = value
             if k in less_refs:
                 if value < ref[k]:
                     logging.warn('%s is %d', k, value)
                     logging.warn('%s should be at least %d', k, ref[k])
-                    errors += 1
-            elif k in equal_refs:
-                if value != ref[k]:
-                    logging.warn('%s is %d', k, value)
-                    logging.warn('%s should be %d', k, ref[k])
                     errors += 1
             elif k in approx_refs:
                 if value < ref[k] * 0.9 or ref[k] * 1.1 < value:
@@ -65,6 +76,35 @@ class platform_MemCheck(test.test):
                     logging.warn('%s should be within 10%% of %d', k, ref[k])
                     errors += 1
 
+        # read spd timings
+        cmd = 'mosys memory spd print timings -s speeds'
+        # result example
+        # DDR3-800, DDR3-1066, DDR3-1333, DDR3-1600
+        pattern = 'DDR([3-9]|[1-9]\d+)-(?P<speed>\d+)'
+        timing_run = utils.run(cmd)
+
+        keyval['speedref'] = speedref
+        for dimm, line in enumerate(timing_run.stdout.split('\n')):
+            if not line:
+                continue
+            max_timing = line.split(', ')[-1]
+            keyval['timing_dimm_%d' % dimm] = max_timing
+            m = re.match(pattern, max_timing)
+            if not m:
+                logging.warn('Error parsing timings for dimm #%d (%s)',
+                             dimm, max_timing)
+                errors += 1
+                continue
+            logging.info('dimm #%d timings: %s', dimm, max_timing)
+            max_speed = int(m.group('speed'))
+            keyval['speed_dimm_%d' % dimm] = max_speed
+            if max_speed < speedref:
+                logging.warn('ram speed is %s', max_timing)
+                logging.warn('ram speed should be at least %d', speedref)
+                errors += 1
+
         # If self.error is not zero, there were errors.
         if errors > 0:
             raise error.TestFail('Found %d incorrect values' % errors)
+
+        self.write_perf_keyval(keyval)
