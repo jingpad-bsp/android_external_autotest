e974180f5b61ccf4826e544ce93e249446b9399a
Achuith Bhandarkar <achuith@chromium.org>
2014-03-25 11:50:07 -0700

    Make telemetry_LoginTest non-experimental
    BUG=chromium:355890
    TEST=trybot
    
    Change-Id: I877171d008efbb63c531ef738da6fc2349f69309
    Reviewed-on: https://chromium-review.googlesource.com/191457
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Tested-by: Achuith Bhandarkar <achuith@chromium.org>

--- client/site_tests/telemetry_LoginTest/telemetry_LoginTest.py
+++ client/site_tests/telemetry_LoginTest/telemetry_LoginTest.py
@@ -6,12 +6,13 @@ from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib.cros import chrome
 
+
 class telemetry_LoginTest(test.test):
     """This is a client side Telemetry Login Test."""
     version = 1
 
 
-    def get_login_status(self, cr):
+    def _get_login_status(self, cr):
         login_status = cr.login_status
         if not login_status:
             raise error.TestFail('Failed to get LoginStatus')
@@ -29,12 +30,14 @@ class telemetry_LoginTest(test.test):
 
 
     def run_once(self):
-        """
-        This test imports telemetry via chrome.py, logs in as a regular
-        user, and checks the login status via autotestPrivate.loginStatus
+        """Test chrome login status.
+
+        This test uses telemetry via chrome.py to log in as a regular user,
+        and then checks chrome login status via the private extension api
+        autotestPrivate.loginStatus.
         """
         with chrome.Chrome(logged_in=True, autotest_ext=True) as cr:
-            (is_regular_user, email) = self.get_login_status(cr)
+            (is_regular_user, email) = self._get_login_status(cr)
             if not is_regular_user:
                 raise error.TestFail('isRegularUser should be True')
             if email != cr.username:
