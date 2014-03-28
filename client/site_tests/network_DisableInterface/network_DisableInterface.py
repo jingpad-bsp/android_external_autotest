58e8f6b83e1537a13c0ca0df66faba5db9d0e013
Prathmesh Prabhu <pprabhu@chromium.org>
2014-03-28 15:56:33 -0700

    network_DisableInterface.pseudomodem: Wait for service to appear.
    network_DisableInterface.pseudomodem was flaky because shill's initialization
    sequence for the newly created pseudomodem device interfered with the test's
    intention of bringing down interfaces.
    We now wait for the service to appear, indicating that shill's initialization
    has completed, before running the actual test.
    
    BUG=chromium:357766
    TEST=test_that network_DisableInterface.pseudomodem
    
    Change-Id: I1cc1e213b49aa9178e6afd1180c233aa1dee0e12
    Reviewed-on: https://chromium-review.googlesource.com/192184
    Reviewed-by: Prathmesh Prabhu <pprabhu@chromium.org>
    Commit-Queue: Prathmesh Prabhu <pprabhu@chromium.org>
    Tested-by: Prathmesh Prabhu <pprabhu@chromium.org>

--- client/site_tests/network_DisableInterface/network_DisableInterface.py
+++ client/site_tests/network_DisableInterface/network_DisableInterface.py
@@ -1,13 +1,24 @@
 import logging, re, utils, os
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
+from autotest_lib.client.cros.cellular.pseudomodem import pseudomodem_context
+from autotest_lib.client.cros.networking import cellular_proxy
+
+PSEUDOMODEM_INTERFACE = 'pseudomodem0'
 
 class network_DisableInterface(test.test):
+    """ Verify a network interface can be disabled. """
     version = 1
 
 
     def run_once(self, iface_name='wlan0'):
+        """
+        Entry function to the test.
 
+        @param iface_name: Name of the interface to test.
+                           You can additionally pass in 'all', or 'wifi_only'.
+
+        """
         # use the right interface configuration utility
         self._ifconfig = 'ifconfig'
         if iface_name.startswith('hci'):
@@ -28,11 +39,33 @@ class network_DisableInterface(test.test):
         for nic in ifaces:
             if (nic != 'lo' and not nic.startswith('sit') and not
                 nic.startswith('p2p') and not nic.startswith('uap')):
-                self.test_one_nic(nic)
+                if nic == PSEUDOMODEM_INTERFACE:
+                    self.test_pseudomodem_nic()
+                else:
+                    self.test_one_nic(nic)
+
+
+    def test_pseudomodem_nic(self):
+        """ Handle pseudomodem specially, wait for service to appear. """
+        with pseudomodem_context.PseudoModemManagerContext(True,
+                                                           {'family': '3GPP'}):
+            # We must wait for shill to finish initializing the cellular service
+            # for the newly created pseudomodem object. Otherwise, shill's
+            # initialization sequence interferes with the intent of the test
+            # below.
+            proxy = cellular_proxy.CellularProxy.get_proxy()
+            proxy.wait_for_cellular_service_object()
+
+            self.test_one_nic(PSEUDOMODEM_INTERFACE)
 
 
     def test_one_nic(self, iface_name='wlan0'):
+        """
+        Bring down an interface, and verify that it's gone.
 
+        @param iface_name: Name of the interface to check.
+
+        """
         forced_up = False
 
         # bring up the interface if its not already up
@@ -58,6 +91,14 @@ class network_DisableInterface(test.test):
 
 
     def is_iface_up(self, name):
+        """
+        Check if the interface |name| is up.
+
+        @param name: name of the interface to check.
+        @returns: True if the interface |name| is up, False otherwise.
+        @raises: TestNAError if 'ifconfig' fails.
+
+        """
         try:
             out = utils.system_output('%s %s' % (self._ifconfig, name))
         except error.CmdError, e:
