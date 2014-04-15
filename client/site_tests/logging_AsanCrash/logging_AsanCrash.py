6ff8477eed58d5d09d73f685f9feab878be78a73
Achuith Bhandarkar <achuith@chromium.org>
2014-04-15 12:07:40 -0700

    Replace logging_AsanCrash with logging_AsanCrashTelemetry.
    BUG=chromium:362934
    TEST=None
    
    Change-Id: I4fc5cb74953cfb10ce24f00c81a1539079278e62
    Reviewed-on: https://chromium-review.googlesource.com/194974
    Reviewed-by: Dmitry Polukhin <dpolukhin@chromium.org>
    Tested-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>

--- client/site_tests/logging_AsanCrash/logging_AsanCrash.py
+++ client/site_tests/logging_AsanCrash/logging_AsanCrash.py
@@ -4,55 +4,72 @@
 
 import logging
 import os
-from autotest_lib.client.bin import utils
-from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros import cros_logging, cros_ui_test
 
-class logging_AsanCrash(cros_ui_test.UITest):
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib.cros import chrome
+from autotest_lib.client.cros import cros_logging
+
+
+class logging_AsanCrash(test.test):
+    """Verify Address Sanitizer does catch memory errors."""
+
     version = 1
-    auto_login = False
+
 
     def run_once(self):
-        import pyauto
-
         if not 'asan' in utils.read_file('/etc/session_manager_use_flags.txt'):
             raise error.TestFail('Current image not built with ASAN')
 
-        pid = self.pyauto.GetBrowserInfo()['browser_pid']
-        asan_log_name = '/var/log/chrome/asan_log.%d' % pid
-        logging.info('Browser PID under pyauto control is %d. '
-                     'So ASAN log is expected at %s.' % (pid, asan_log_name))
+        extension_path = os.path.join(os.path.dirname(__file__),
+                                      'asan_crash_ext')
 
-        logging.info('Initiate simulating memory bug to be caught by ASAN...')
-        self.pyauto.SimulateAsanMemoryBug()
+        with chrome.Chrome(extension_paths=[extension_path]) as cr:
+            pid = utils.get_oldest_pid_by_name('chrome')
+            asan_log_name = '/var/log/chrome/asan_log.%d' % pid
+            logging.info('Browser PID under telemetry control is %d. '
+                         'So ASAN log is expected at %s.', pid, asan_log_name)
 
-        utils.poll_for_condition(
-            lambda: os.path.isfile(asan_log_name),
-            timeout=10,
-            exception=error.TestFail(
-                    'Found no asan log file %s during 10s' % asan_log_name))
-        ui_log = cros_logging.LogReader(asan_log_name)
-        ui_log.read_all_logs()
+            logging.info('Initiate simulating memory bug to be caught by ASAN.')
+            extension = cr.get_extension(extension_path)
+            if not extension:
+                raise error.TestFail('Failed to find extension %s'
+                                     % extension_path)
 
-        # We must wait some time until memory bug is simulated (happens
-        # immediately after the return on the call) and caught by ASAN.
-        try:
-            utils.poll_for_condition(
-                lambda: ui_log.can_find('ERROR: AddressSanitizer'),
-                timeout=10,
-                exception=error.TestFail(
-                    'Found no asan log message about Address Sanitizer catch'))
+            # Catch the exception raised when the browser crashes.
+            cr.did_browser_crash(lambda: extension.ExecuteJavaScript(
+                    'chrome.autotestPrivate.simulateAsanMemoryBug();'))
 
             utils.poll_for_condition(
-                lambda: ui_log.can_find("'testarray'"),
-                timeout=10,
-                exception=error.TestFail(
-                    'ASAN caught bug but did not mentioned the cause in log'))
+                    lambda: os.path.isfile(asan_log_name),
+                    timeout=10,
+                    exception=error.TestFail(
+                            'Found no asan log file %s during 10s'
+                            % asan_log_name))
+            ui_log = cros_logging.LogReader(asan_log_name)
+            ui_log.read_all_logs()
 
-        except:
-            logging.debug('ASAN log content: ' + ui_log.get_logs())
-            raise
+            # We must wait some time until memory bug is simulated (happens
+            # immediately after the return on the call) and caught by ASAN.
+            try:
+                utils.poll_for_condition(
+                        lambda: ui_log.can_find('ERROR: AddressSanitizer'),
+                        timeout=10,
+                        exception=error.TestFail(
+                                'Found no asan log message about '
+                                'Address Sanitizer catch'))
 
-        # The cbuildbot logic will look for asan logs and process them.
-        # Remove the simulated log file to avoid that.
-        os.remove(asan_log_name)
+                utils.poll_for_condition(
+                        lambda: ui_log.can_find("'testarray'"),
+                        timeout=10,
+                        exception=error.TestFail(
+                                'ASAN caught bug but did not mention '
+                                'the cause in the log'))
+
+            except:
+                logging.debug('ASAN log content: ' + ui_log.get_logs())
+                raise
+
+            # The cbuildbot logic will look for asan logs and process them.
+            # Remove the simulated log file to avoid that.
+            os.remove(asan_log_name)
