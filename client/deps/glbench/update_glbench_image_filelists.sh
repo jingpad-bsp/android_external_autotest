3907fd1607fdc5ae5065adad80b35c7733fbbf7a
Ilja Friedel <ihf@chromium.org>
2014-04-15 14:29:43 -0700

    GLBench: add new fillrate test of variable FBO size.
    * Add fbofill test: We want to measure memory throughput (in this case
      bilinear copy) depending on texture size. This is interesting to study
      cache behavior.
    
    * Also changes slightly thermal throttling if we can't read temperatures
      (by sleeping for a full 30 seconds between tests to ensure cold state).
    
    * Finally add a command line option called "-hasty". This option will
      run each test at at most 512x512 resolution, run it only for a fraction
      of the loops we normally do, and not cool down the machine between
      tests. The hasty option will be used to verify MD5s in BVT and might
      also be useful for debugging test failures. Currently
        glbench -save -hasty
      runs in less than 30 seconds on daisy.
    
    * This change fixes an old standing bug with the yuv_shader images. The
      references for this and the new fbofill test are updated.
    
    * We will run "glbench -hasty" on the BVT suite. It will run a subset of
      tests in less than a minute total and verify MD5s of the generated
      images. We will not upload any performance results from BVT.
    
    Notice that we might need to collect more references from the lab as the
    yuv images differ depending on the GPU architecture. Hence BVT is only
    experimental.
    
    BUG=chromium:355202, chromium:361925
    TEST=Ran locally on daisy and link. Ran cbuildbot on alex.
    
    Change-Id: I54039cce967664d553c79d16ca2b1361953ab9fe
    Reviewed-on: https://chromium-review.googlesource.com/194991
    Commit-Queue: Ilja Friedel <ihf@chromium.org>
    Tested-by: Ilja Friedel <ihf@chromium.org>
    Reviewed-by: Ilja Friedel <ihf@chromium.org>

--- client/deps/glbench/update_glbench_image_filelists.sh
+++ client/deps/glbench/update_glbench_image_filelists.sh
@@ -1,8 +1,8 @@
 cd ../glbench-images/glbench_reference_images
-ls *.png > ../../glbench/glbench_reference_images.txt
-ls *.png > index.html
+ls *.png | sort > ../../glbench/glbench_reference_images.txt
+ls *.png | sort > index.html
 cd ..
 cd ../glbench-images/glbench_knownbad_images
-ls */*.png > ../../glbench/glbench_knownbad_images.txt
-ls */*.png > index.html
+ls */*.png | sort > ../../glbench/glbench_knownbad_images.txt
+ls */*.png | sort > index.html
 
