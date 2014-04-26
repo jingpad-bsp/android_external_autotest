99b7e65094726c8d7d4c7cc5b6e4d4165e4ce239
Ilja H. Friedel <ihf@chromium.org>
2014-04-25 21:05:01 -0700

    Piglit-stable: clean up towards closing the CQ.
    Add a cleaned up version of Piglit to autotest that is suitable for
    closing the CQ. The heart of this test is
      generate_scripts.py
    
    which using test results *main.txt from wmatrix/ generates simple
    shell scripts running piglit tests that pass everywhere. To not make
    this a too small set we destinguish 4 GPU families for now (exynos,
    pinetrail, tegra, other) which have similar feature set/driver support.
    
    For more detail in how to use the script check README.
    
    BUG=chromium:365888
    TEST=Followed steps in README. Ran on link.
    
    Change-Id: I8043a5d095f094c939acc1c1f5b2b995f935b404
    Reviewed-on: https://chromium-review.googlesource.com/197202
    Reviewed-by: Ilja Friedel <ihf@chromium.org>
    Commit-Queue: Ilja Friedel <ihf@chromium.org>
    Tested-by: Ilja Friedel <ihf@chromium.org>

---
+++ client/site_tests/graphics_PiglitBVT/graphics_PiglitBVT.py
@@ -0,0 +1,51 @@
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging, os
+
+from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
+
+class graphics_PiglitBVT(test.test):
+    """
+    Runs a slice of the passing Piglit test sets.
+    """
+    version = 1
+
+    test_scripts = 'test_scripts/'
+
+    def run_once(self, test_slice):
+        gpu_family = utils.get_gpu_family()
+        family = gpu_family
+        logging.info('Detected gpu family %s.', gpu_family)
+
+        # TODO(ihf): Delete this once we have a piglit that runs on ARM.
+        if gpu_family in ['arm', 'exynos', 'tegra']:
+            logging.info('Not running any tests, passing by default.')
+            return
+
+        # We don't want to introduce too many combinations, so fall back.
+        if not os.path.isdir(os.path.join(self.test_scripts, family)):
+            family = 'other'
+        logging.info('Using scripts for gpu family %s.', family)
+
+        # Mark scripts executable if they are not.
+        utils.system('chmod +x /usr/local/autotest/tests/graphics_PiglitBVT/' +
+                     self.test_scripts + '*/graphics_PiglitBVT_*.sh')
+
+        # Kick off test script.
+        cmd = ('source /usr/local/autotest/tests/graphics_PiglitBVT/' +
+               self.test_scripts +
+               '%s/graphics_PiglitBVT_%d.sh' % (family, test_slice))
+        logging.info('Executing cmd = %s', cmd)
+        # TODO(ihf): See if we can get the test output in real time to the logs.
+        # utils.run(cmd,
+        #           stdout_tee=utils.TEE_TO_LOGS,
+        #           stderr_tee=utils.TEE_TO_LOGS).stdout
+        tests_failed = utils.system(cmd, ignore_status=True)
+        if tests_failed:
+            reason = '%d tests failed on "%s" in slice %d' % (tests_failed,
+                                                              gpu_family,
+                                                              test_slice)
+            raise error.TestError(reason)
