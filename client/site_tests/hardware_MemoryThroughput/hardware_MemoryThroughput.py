f27e47a6bc3c5b2e8a3fba6ad553a32ecace4985
Sonny Rao <sonnyrao@chromium.org>
2014-05-02 14:16:39 -0700

    hardware_MemoryThroughput change to use lmbench
    This changes the hardware_MemoryThroughput test to use lmbench and
    removes the old code.  As far as I know nobody was paying attention to
    the old version of the test and the results from it made little sense
    to me, so I'm changing it to do something that makes more sense.
    
    BUG=chromium:354161
    TEST=`test_that <ip> hardware_MemoryThroughput`
    
    Change-Id: I03c6f1368ecf30f3b88c9cea8ef203bc08f1f72c
    Reviewed-on: https://chromium-review.googlesource.com/198765
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Sonny Rao <sonnyrao@chromium.org>

--- client/site_tests/hardware_MemoryThroughput/hardware_MemoryThroughput.py
+++ client/site_tests/hardware_MemoryThroughput/hardware_MemoryThroughput.py
@@ -2,45 +2,82 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import re
-import os
 import logging
 
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error, utils
+from autotest_lib.client.cros import service_stopper
 
 class hardware_MemoryThroughput(test.test):
+    """Autotest for measuring memory throughput.
+
+    This uses mem_bw with different parameters to measure memory and cache
+    throughput.
+
+    Private Attributes:
+      _results: dict containing keyvals with throughput measurements
+      _services: service_stopper.ServiceStopper object
+    """
     version = 1
-    preserve_srcdir = True
 
-    def setup(self):
-        os.chdir(self.srcdir)
-        utils.make('clean')
-        utils.make()
+    def _run_benchmarks(self, test, warmup, num_iterations, parallel, sizes):
+        """Run the benchmark.
 
-    def run_once(self, num_iteration = -1, test_list = ''):
-        exefile = os.path.join(self.srcdir, 'hardware_MemoryThroughput')
-        cmd = '%s %d %s' % (exefile, num_iteration, test_list)
-        self.results = utils.system_output(cmd, retain_output = True)
+        This runs the bw_mem benchmark from lmbench 3 and fills out results.
+        Args:
+          test: string containing either rd, rdwr, cp, bzero, or bcopy
+          warmup:  integer amount of time to spend warming up in microseconds.
+          num_iterations: integer number of times to run the benchmark on each
+            size.
+          parallel: integer number of instances to run in parallel
+          sizes: list of integer sizes in bytes to run
+        """
+        r = {}
 
-        # Resulting time in MicroSec / MegaBytes.
-        # Write out memory operation performance in MegaBytes / Second.
-        performance_pattern = re.compile(
-            r"Action = ([a-z0-9.]+), BlockSize = (\w+), " +
-            r"Method = (\w+), Time = ([0-9.]+)")
-        keyval_list = performance_pattern.findall(self.results)
-        keyvals = {}
-        for keyval in keyval_list:
-            key = ('mb_per_sec_memory_' +
-                   keyval[0] + '_' + keyval[1] + '_' + keyval[2])
-            keyvals[key] = 1000000.0 / float(keyval[3])
-        self.write_perf_keyval(keyvals)
+        for size in sizes:
+            cmd = 'bw_mem -P %d -W %d -N %d %d %s 2>&1' % (parallel, warmup,
+                                                           num_iterations,
+                                                           size, test)
+            logging.debug('cmd: %s', cmd)
+            out = utils.system_output(cmd)
+            logging.debug('Output: %s', out)
 
-        # Detect if an error has occured during the tests.
-        # Do this after writing out the test results so even an error occurred,
-        # we still get the performance evaluation.
-        error_pattern = re.compile(r"ERROR: \[(.+)\]")
-        errors = error_pattern.findall(self.results)
-        if len(errors) > 0:
-            logging.debug(self.results)
-            raise error.TestFail('malfunctioning memory detected');
+            lines = out.splitlines()
+            if len(lines) != 1:
+                raise error.TestFail('invalid amount of output from bw_mem')
+
+            s = lines[0].split()
+            if len(s) == 2:
+                bw = float(s[1])
+                if bw <= 0:
+                    raise error.TestFail('invalid throughput %f' % bw)
+                key = ('MB_per_second_' + test + '-' +
+                       str(parallel) + '-thread-' +
+                       str(size / 1024) + 'KB')
+                self._results[key] = bw
+            else:
+                raise error.TestFail('invalid output line %s' % lines[0])
+
+
+    def initialize(self):
+        super(hardware_MemoryThroughput, self).initialize()
+        self._results = {}
+        self._services = None
+
+
+    def run_once(self, test='bcopy', warmup=100, num_iterations=20,
+                 parallel=1, sizes= [ 4096, 192 * 1024, 32 * 1024 * 1024 ]):
+        stop = [ 'ui' ]
+        stop.extend(service_stopper.ServiceStopper.POWER_DRAW_SERVICES)
+        self._services = service_stopper.ServiceStopper(stop)
+        self._services.stop_services()
+
+        self._run_benchmarks(test, warmup, num_iterations, parallel,
+                             sizes)
+        self.write_perf_keyval(self._results)
+
+
+    def cleanup(self):
+        if self._services:
+            self._services.restore_services()
+        super(hardware_MemoryThroughput, self).cleanup()
