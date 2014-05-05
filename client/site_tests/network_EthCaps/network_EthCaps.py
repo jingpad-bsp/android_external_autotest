5fbb3602035932d7072859322264290850983c50
Grant Grundler <grundler@chromium.org>
2014-05-05 09:06:37 -0700

    autotest: teach network_EthCaps about USB devices
    USB devices we use aren't wired for Wake-on-LAN.
    
    Also give threshold_secs a default value. Fixes this error:
      Unhandled TypeError: unsupported operand type(s) for *: NoneType and int
    
    BUG=chrome-os-partner:28270
    TEST=test_that 192.168.1.138 network_EthCaps
        now passes on both leon (x86-haswell) and peach_pi (Exynos 5422)
    
    Change-Id: Ie60d47218f0026e988e7531468e42a5ce987d5c3
    Signed-off-by: Grant Grundler <grundler@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/198186
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

--- client/site_tests/network_EthCaps/network_EthCaps.py
+++ client/site_tests/network_EthCaps/network_EthCaps.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2011-2015 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -8,18 +8,47 @@ from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import rtc, sys_power
 
-# TODO(tbroch) General:
-# - What other Ethernet Capabilities should we check
-
 # TODO(tbroch) WOL:
 # - Should we test any of the other modes?  I chose magic as it meant that only
 #   the target device should be awaken.
+
 class network_EthCaps(test.test):
+    """Base class of EthCaps test.
+
+    Verify Capabilities advertised by an ethernet device work.
+    We can't verify much in reality though. But we can verify
+    WOL for built-in devices which is expected to work.
+
+    @param test.test: test instance
+    """
     version = 1
 
     # If WOL setting changed during test then restore to original during cleanup
     _restore_wol = False
 
+
+    def _is_usb(self):
+        """Determine if device is USB (or not)
+
+        Add-on USB devices won't report the same 'Supports Wake-on' value
+        as built-in (ie PCI) ethernet devices.
+        """
+        if not self._bus_info:
+            cmd = "ethtool -i %s | awk '/bus-info/ {print $2}'" % self._ethname
+            self._bus_info = utils.system_output(cmd)
+            logging.debug("bus_info is %s", self._bus_info)
+            if not self._bus_info:
+                logging.error("ethtool -i %s has no bus-info", self._ethname)
+
+        # Two bus_info formats are reported by different device drivers:
+        # 1) "usb-0000:00:1d.0-1.2"
+        #    "0000:00:1d.0" is the "platform" info of the USB host controller
+        #    But it's obvious it's USB since that's the prefix. :)
+        if self._bus_info.startswith('usb-'):
+            return True
+
+        # 2) "2-1.2" where "2-" is USB host controller instance
+        return os.path.exists("/sys/bus/usb/devices/%s" % self._bus_info)
 
     def _parse_ethtool_caps(self):
         """Retrieve ethernet capabilities.
@@ -62,8 +91,6 @@ class network_EthCaps(test.test):
                                       '1000baseT/Half', '10baseT/Full',
                                       '100baseT/Full', '1000baseT/Full'],
             'Supports auto-negotiation': ['Yes'],
-            # TODO(tbroch): Will this order, 'pumbg' remain across other h/w +
-            # drivers
             # TODO(tbroch): Other WOL caps: 'a': arp and 's': magicsecure are
             # they important?  Are any of these undesirable/security holes?
             'Supports Wake-on': ['pumbg']
@@ -79,6 +106,16 @@ class network_EthCaps(test.test):
 
             for value in default_eth_caps[keyname]:
                 if value not in self._caps[keyname]:
+                    # WOL not required for USB Ethernet plug-in devices
+                    # But all USB Ethernet devices to date report "pg".
+                    # Enforce that.
+                    # RTL8153 can report 'pumbag'.
+                    # AX88178 can report 'pumbg'.
+                    if self._is_usb() and keyname == 'Supports Wake-on':
+                        if (self._caps[keyname][0].find('p') >= 0) and \
+                            (self._caps[keyname][0].find('g') >= 0):
+                            continue
+
                     logging.error("\'%s\' not a supported mode in \'%s\' of %s",
                                   value, keyname, self._ethname)
                     errors += 1
@@ -107,42 +144,33 @@ class network_EthCaps(test.test):
             self._restore_wol = True
 
         # Set RTC as backup to WOL
-        before_suspend_secs = rtc.get_seconds()
-        alarm_secs =  before_suspend_secs + self._threshold_secs * 2
+        before_secs = rtc.get_seconds()
+        alarm_secs =  before_secs + self._suspend_secs + self._threshold_secs
         rtc.set_wake_alarm(alarm_secs)
 
-        sys_power.do_suspend()
+        sys_power.do_suspend(self._suspend_secs)
 
-        after_suspend_secs = rtc.get_seconds()
+        after_secs = rtc.get_seconds()
         # flush RTC as it may not work subsequently if wake was not RTC
         rtc.set_wake_alarm(0)
 
-        suspend_secs = after_suspend_secs - before_suspend_secs
-        if suspend_secs > self._threshold_secs:
+        suspended_secs = after_secs - before_secs
+        if suspended_secs >= (self._suspend_secs + self._threshold_secs):
             raise error.TestError("Device woke due to RTC not WOL")
 
 
     def _verify_wol_magic(self):
         """If possible identify wake source was caused by WOL.
 
-        The bits identifying this may be cleared by the time kernel/userspace
-        gets a change to query.  However if the firmware has a log it may expose
-        the wake source.  This method attempts to interrogate the wake source
-        details if they are present on the system.
+        The bits identifying the wake source may be cleared by the time
+        userspace gets a chance to query the kernel.  However, firmware
+        might have a log and expose the wake source.  Attempt to interrogate
+        the wake source details if they are present on the system.
 
         Returns:
           True if verified or unable to verify due to system limitations
           False otherwise
         """
-        cmd = "mosys smbios info bios"
-        bios_info = utils.system_output(cmd).replace(' ', '').split('|')
-        logging.debug("bios_info = %s", bios_info)
-        if 'coreboot' not in bios_info:
-            logging.warning("Unable to verify wake in s/w due to firmware type")
-            if 'INSYDE' not in bios_info:
-                raise error.TestError("Unrecognized firmware found")
-            return True
-
         fw_log = "/sys/firmware/log"
         if not os.path.isfile(fw_log):
             logging.warning("Unable to verify wake in s/w due to missing log %s",
@@ -177,7 +205,7 @@ class network_EthCaps(test.test):
                                 (self._ethname, self._caps['Wake-on'][0]))
 
 
-    def run_once(self, ethname=None, threshold_secs=None):
+    def run_once(self, ethname=None, suspend_secs=5, threshold_secs=10):
         """Run the test.
 
         Args:
@@ -190,9 +218,22 @@ class network_EthCaps(test.test):
 
         self._ethname = ethname
         self._threshold_secs = threshold_secs
+        self._suspend_secs = suspend_secs
+        self._bus_info = None
 
         self._parse_ethtool_caps()
         self._check_eth_caps()
+
+        # ChromeOS does not require WOL support for any USB Ethernet Adapters.
+        # In fact, WoL only known to work for PCIe Ethernet devices.
+        # We know _some_ platforms power off all USB ports when suspended.
+        # USB adapters with "pg" capabilities _might_ WoL on _some_ platforms.
+        # White list/black listing of platforms will be required to test
+        # WoL against USB dongles in the future.
+        if self._is_usb():
+            logging.debug("Skipping WOL test on USB Ethernet device.")
+            return
+
         self._test_wol_magic_packet()
         # TODO(tbroch) There is evidence in the filesystem of the wake source
         # for coreboot but its still being flushed out.  For now only produce a
