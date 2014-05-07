d592acd378d20315d303a66c64828716896a5c3b
Kalin Stoyanov <kalin@chromium.org>
2014-05-06 18:58:55 -0700

    PowerStatusStress: Start and finish as powered on
    BUG=chromium:358927
    TEST=Tested on stressrack devices
    
    Change-Id: Ifa1165db3c83efd19c88b77d7f527b3bd3542fa1
    Reviewed-on: https://chromium-review.googlesource.com/198605
    Reviewed-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Commit-Queue: Kalin Stoyanov <kalin@chromium.org>
    Tested-by: Kalin Stoyanov <kalin@chromium.org>

--- server/site_tests/platform_PowerStatusStress/platform_PowerStatusStress.py
+++ server/site_tests/platform_PowerStatusStress/platform_PowerStatusStress.py
@@ -2,12 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import re, time
+import logging, re, time
 from autotest_lib.server import test
 from autotest_lib.client.common_lib import error
 
 _CHARGING = 'CHARGING'
 _DISCHARGING = 'DISCHARGING'
+_WAIT_SECS_AFTER_SWITCH = 1
 
 class platform_PowerStatusStress(test.test):
     version = 1
@@ -15,57 +16,58 @@ class platform_PowerStatusStress(test.test):
     def suspend_resume(self):
         pass
 
-    def run_once(self, host, loop_count):
 
-        #Start as powered on
-        if host.has_power():
-            host.power_on()
+    def cleanup(self):
+        # Finish as powered on
+        self.host.power_on()
+
+
+    def switch_power_and_verify(self, powered_on, expected):
+        if powered_on:
+            self.host.power_on()
+        else:
+            self.host.power_off()
+        time.sleep(_WAIT_SECS_AFTER_SWITCH)
+
+        # Get power_supply_info output
+        psi_output = self.host.run('power_supply_info').stdout.strip()
+        psi_output = psi_output.replace('\n', '')
+
+        exp_psi_online, exp_psi_enum_type, exp_psi_bat_state = expected
+
+        is_psi_online = re.match(r'.+online:\s+%s.+' % exp_psi_online,
+                                 psi_output) is not None
+        is_psi_enum_type = re.match(r'.+enum type:\s+%s.+' % exp_psi_enum_type,
+                                    psi_output) is not None
+        is_psi_bat_state = re.match(r'.+state:\s+%s.+' % exp_psi_bat_state,
+                                    psi_output) is not None
+
+        if not all([is_psi_online, is_psi_enum_type, is_psi_bat_state]):
+            raise error.TestFail('Bad %s state: %s' %
+                (_CHARGING if powered_on else _DISCHARGING,
+                 psi_output))
+
+
+    def run_once(self, host, loop_count):
+        self.host = host
+
+        # Start as powered on
+        if self.host.has_power():
+            self.host.power_on()
         else:
             raise error.TestFail('No RPM is setup to device')
 
         pdu_connected = True
+        for i in xrange(loop_count):
+            logging.info('Iteration %d' % (i + 1))
 
-        for i in xrange(loop_count * 2):
-            time.sleep(1)
-            iteration = i/2 + 1
+            # Charging state
+            expected = ('yes', 'AC', '(Charging|Fully charged)')
+            self.switch_power_and_verify(True, expected)
 
-            # Get power_supply_info output
-            psi_output = host.run('power_supply_info').stdout.strip()
-            psi_output = psi_output.replace('\n', '')
+            # Discharging state
+            expected = ('no', 'Disconnected', 'Discharging')
+            self.switch_power_and_verify(False, expected)
 
-            if pdu_connected:
-                expected_psi_online = 'yes'
-                expected_psi_enum_type = 'AC'
-                expected_psi_bat_state = '(Charging|Fully charged)'
-            else:
-                expected_psi_online = 'no'
-                expected_psi_enum_type = 'Disconnected'
-                expected_psi_bat_state = 'Discharging'
-
-            is_psi_online = re.match(r'.+online:\s+%s.+' % expected_psi_online,
-                                     psi_output) is not None
-            is_psi_enum_type = re.match(r'.+enum type:\s+%s.+' %
-                expected_psi_enum_type, psi_output) is not None
-            is_psi_bat_state = re.match(r'.+state:\s+%s.+' %
-                expected_psi_bat_state, psi_output) is not None
-
-            if not all([is_psi_online,
-                       is_psi_enum_type,
-                       is_psi_bat_state]):
-                host.power_on()
-                raise error.TestFail('Bad %s state at iteration %d: %s' %
-                    (_CHARGING if pdu_connected else _DISCHARGING,
-                     iteration, psi_output))
-
-            if pdu_connected:
-                host.power_off()
-                pdu_connected = False
-            else:
-                host.power_on()
-                pdu_connected = True
-
-            #TODO(kalin@): Add suspend/resume
+            # TODO(kalin@): Add suspend/resume
             self.suspend_resume()
-
-        #Finish as powered on
-        host.power_on()
