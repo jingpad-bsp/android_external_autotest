169613678a67b5603c50699de8c02c896c02a945
Puthikorn Voravootivat <puthik@chromium.org>
2014-05-07 15:57:27 -0700

    fio_util: Add code generate chart from log file
    Parse fio log file and put it in javascript to generate
    graph using Google Charts API. developers.google.com/chart
    
    BUG=chrome-os-partner:27472
    TEST=Tested in falco. manually check graph
    
    Change-Id: I17ec8e15e8d33995b0bf1a46e68624e77b9af79f
    Reviewed-on: https://chromium-review.googlesource.com/198762
    Reviewed-by: Puthikorn Voravootivat <puthik@chromium.org>
    Tested-by: Puthikorn Voravootivat <puthik@chromium.org>
    Commit-Queue: Puthikorn Voravootivat <puthik@chromium.org>

--- client/common_lib/fio_util.py
+++ client/common_lib/fio_util.py
@@ -14,7 +14,7 @@ Decoding class can be invoked independently.
 
 """
 
-import logging, utils
+import json, logging, re, utils
 from UserDict import UserDict
 
 class fio_parser_exception(Exception):
@@ -261,6 +261,230 @@ class fio_job_output(UserDict):
             parser(self._job_name, field, data[idx])
 
 
+class fio_graph_generator():
+    """
+    Generate graph from fio log that created when specified these options.
+    - write_bw_log
+    - write_iops_log
+    - write_lat_log
+
+    The following limitations apply
+    - Log file name must be in format jobname_testpass
+    - Graph is generate using Google graph api -> Internet require to view.
+    """
+
+    html_head = """
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+"""
+
+    html_tail = """
+        var chart_div = document.getElementById('chart_div');
+        var chart = new google.visualization.ScatterChart(chart_div);
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="chart_div" style="width: 100%; height: 100%;"></div>
+  </body>
+</html>
+"""
+
+    h_title = { True: 'Percentile', False: 'Time (s)' }
+    v_title = { 'bw'  : 'Bandwidth (KB/s)',
+                'iops': 'IOPs',
+                'lat' : 'Total latency (us)',
+                'clat': 'Completion latency (us)',
+                'slat': 'Submission latency (us)' }
+    graph_title = { 'bw'  : 'bandwidth',
+                    'iops': 'IOPs',
+                    'lat' : 'total latency',
+                    'clat': 'completion latency',
+                    'slat': 'submission latency' }
+
+    test_name = ''
+    test_type = ''
+    pass_list = ''
+
+    @classmethod
+    def _parse_log_file(self, file_name, pass_index, pass_count, percentile):
+        """
+        Generate row for google.visualization.DataTable from one log file.
+        Log file is the one that generated using write_{bw,lat,iops}_log
+        option in the FIO job file.
+
+        The fio log file format is  timestamp, value, direction, blocksize
+        The output format for each row is { c: list of { v: value} }
+
+        @param file_name:  log file name to read data from
+        @param pass_index: index of current run pass
+        @param pass_count: number of all test run passes
+        @param percentile: flag to use percentile as key instead of timestamp
+
+        @return: list of data rows in google.visualization.DataTable format
+        """
+        # Read data from log
+        with open(file_name, 'r') as f:
+            data = []
+
+            for line in f.readlines():
+                if not line:
+                    break
+                t, v, _, _ = [int(x) for x in line.split(', ')]
+                data.append([t / 1000.0, v])
+
+        # Sort & calculate percentile
+        if percentile:
+            data.sort(key=lambda x:x[1])
+            l = len(data)
+            for i in range(l):
+                data[i][0] = 100 * (i + 0.5) / l
+
+        # Generate the data row
+        all_row = []
+        row = [None] * (pass_count + 1)
+        for d in data:
+            row[0] = {'v' : '%.3f' % d[0]}
+            row[pass_index + 1] = {'v': d[1] }
+            all_row.append({'c': row[:]})
+
+        return all_row
+
+    @classmethod
+    def _gen_data_col(self, pass_list, percentile):
+        """
+        Generate col for google.visualization.DataTable
+
+        The output format is list of dict of label and type. In this case,
+        type is always number.
+
+        @param pass_list:  list of test run passes
+        @param percentile: flag to use percentile as key instead of timestamp
+
+        @return: list of column in google.visualization.DataTable format
+        """
+        if percentile:
+            col_name_list = ['percentile'] + pass_list
+        else:
+            col_name_list = ['time'] + pass_list
+
+        return [{'label': name, 'type': 'number'} for name in col_name_list]
+
+    @classmethod
+    def _gen_data_row(self, test_name, test_type, pass_list, percentile):
+        """
+        Generate row for google.visualization.DataTable by generate all log
+        file name and call _parse_log_file for each file
+
+        @param test_name: name of current workload. i.e. randwrite
+        @param test_type: type of value collected for current test. i.e. IOPs
+        @param pass_list: list of run passes for current test
+        @param percentile: flag to use percentile as key instead of timestamp
+
+        @return: list of data rows in google.visualization.DataTable format
+        """
+        all_row = []
+        pass_count = len(pass_list)
+        for pass_index, pass_str in enumerate(pass_list):
+            log_file_name = str('%s_%s_%s.log' %
+                                (test_name, pass_str, test_type))
+            all_row.extend(self._parse_log_file(log_file_name, pass_index,
+                                                pass_count, percentile))
+        return all_row
+
+    @classmethod
+    def _write_data(self, f, test_name, test_type, pass_list, percentile):
+        """
+        Write google.visualization.DataTable object to output file.
+        https://developers.google.com/chart/interactive/docs/reference
+
+        @param test_name: name of current workload. i.e. randwrite
+        @param test_type: type of value collected for current test. i.e. IOPs
+        @param pass_list: list of run passes for current test
+        @param percentile: flag to use percentile as key instead of timestamp
+        """
+        col = self._gen_data_col(pass_list, percentile)
+        row = self._gen_data_row(test_name, test_type, pass_list, percentile)
+        data_dict = { 'cols' : col, 'rows' : row}
+
+        f.write('var data = new google.visualization.DataTable(')
+        json.dump(data_dict, f)
+        f.write(');\n')
+
+    @classmethod
+    def _write_option(self, f, test_name, test_type, percentile):
+        """
+        Write option to render scatter graph to output file.
+        https://google-developers.appspot.com/chart/interactive/docs/gallery/scatterchart
+
+        @param test_name: name of current workload. i.e. randwrite
+        @param test_type: type of value collected for current test. i.e. IOPs
+        @param percentile: flag to use percentile as key instead of timestamp
+        """
+        option = {'pointSize': 1 }
+        if percentile:
+            option['title'] = ('Percentile graph of %s for %s workload' %
+                               (self.graph_title[test_type], test_name))
+        else:
+            option['title'] = ('Graph of %s for %s workload over time' %
+                               (self.graph_title[test_type], test_name))
+
+        option['hAxis'] = { 'title': self.h_title[percentile]}
+        option['vAxis'] = { 'title': self.v_title[test_type]}
+
+        f.write('var options = ')
+        json.dump(option, f)
+        f.write(';\n')
+
+    @classmethod
+    def _write_graph(self, test_name, test_type, pass_list, percentile=False):
+        """
+        Generate graph for test name / test type
+
+        @param test_name: name of current workload. i.e. randwrite
+        @param test_type: type of value collected for current test. i.e. IOPs
+        @param pass_list: list of run passes for current test
+        @param percentile: flag to use percentile as key instead of timestamp
+        """
+        logging.info('fio_graph_generator._write_graph %s %s %s',
+                     test_name, test_type, str(pass_list))
+
+
+        if percentile:
+            out_file_name = '%s_%s_percentile.html' % (test_name, test_type)
+        else:
+            out_file_name = '%s_%s.html' % (test_name, test_type)
+
+        with open(out_file_name, 'w') as f:
+            f.write(self.html_head)
+            self._write_data(f, test_name, test_type, pass_list, percentile)
+            self._write_option(f, test_name, test_type, percentile)
+            f.write(self.html_tail)
+
+    def __init__(self, test_name, test_type, pass_list):
+        """
+        @param test_name: name of current workload. i.e. randwrite
+        @param test_type: type of value collected for current test. i.e. IOPs
+        @param pass_list: list of run passes for current test
+        """
+        self.test_name = test_name
+        self.test_type = test_type
+        self.pass_list = pass_list
+
+    def run(self):
+        """
+        Run the graph generator.
+        """
+        self._write_graph(self.test_name, self.test_type, self.pass_list, False)
+        self._write_graph(self.test_name, self.test_type, self.pass_list, True)
+
+
 def fio_parser(lines):
     """Parse the terse fio output
 
@@ -294,6 +518,61 @@ def fio_parser(lines):
     return results
 
 
+def fio_generate_graph():
+    """
+    Scan for fio log file in output directory and send data to generate each
+    graph to fio_graph_generator class.
+    """
+    log_types = ['bw', 'iops', 'lat', 'clat', 'slat']
+
+    # move fio log to result dir
+    for log_type in log_types:
+        logging.info('log_type %s', log_type)
+        logs = utils.system_output('ls *_%s.log' % log_type, ignore_status=True)
+        if not logs:
+            continue
+
+        # log file name should be in logname_pass_type.log
+        # Example randread_p1_iops.log
+        log_dict = dict()
+
+        pattern = r"""(?P<jobname>.*)_                    # jobname
+                      ((?P<runpass>p\d+)_)                # pass
+                      (?P<type>bw|iops|lat|clat|slat).log # type
+                   """
+        matcher = re.compile(pattern, re.X)
+
+        pass_list = []
+        current_job = ''
+
+        for log in logs.split():
+            match = matcher.match(log)
+            if not match:
+                logging.warn('Unknown log file %s', log)
+                continue
+
+            jobname = match.group('jobname')
+            runpass = match.group('runpass')
+
+            # All files for particular job name are group together for create
+            # graph that can compare performance between result from each pass.
+            if jobname != current_job:
+                if pass_list:
+                    fio_graph_generator(current_job, log_type, pass_list).run()
+                current_job = jobname
+                pass_list = []
+
+            pass_list.append(runpass)
+
+        if pass_list:
+            fio_graph_generator(current_job, log_type, pass_list).run()
+
+
+        cmd = 'mv *_%s.log results' % log_type
+        utils.run(cmd, ignore_status=True)
+        utils.run('mv *.html results', ignore_status=True)
+
+
 def fio_runner(job, env_vars):
     """
     Runs fio.
@@ -325,5 +604,8 @@ def fio_runner(job, env_vars):
     fio = utils.run(fio_cmd_line)
 
     logging.debug(fio.stdout)
+
+    fio_generate_graph()
+
     return fio_parser(fio.stdout)
 
