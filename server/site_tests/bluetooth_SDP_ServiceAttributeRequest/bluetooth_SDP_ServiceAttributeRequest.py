a6ed266c0db084d2fe69b581fbc1da178da4af85
Artem Rakhov <arakhov@chromium.org>
2014-05-12 16:37:43 -0700

    Bluetooth SDP autotest: test BrowseGroupList attribute
    This CL does some refactoring for existing SDP Service Attribute Request test,
    and also adds one more test for this type of request. It is TP/SERVER/SA/BV-08-C
    which tests the behavior of DUT when requesting for BrowseGroupList attribute.
    
    BUG=chromium:329044
    TEST=test_that --board ${BOARD} ${HOSTNAME} bluetooth_SDP_ServiceAttributeRequest
    
    Change-Id: Ifbbe34990467d134523b8be13cb3c59ea4eaafce
    Reviewed-on: https://chromium-review.googlesource.com/199956
    Tested-by: Artem Rakhov <arakhov@chromium.org>
    Reviewed-by: Dan Shi <dshi@chromium.org>
    Reviewed-by: Arman Uguray <armansito@chromium.org>
    Commit-Queue: Artem Rakhov <arakhov@chromium.org>

--- server/site_tests/bluetooth_SDP_ServiceAttributeRequest/bluetooth_SDP_ServiceAttributeRequest.py
+++ server/site_tests/bluetooth_SDP_ServiceAttributeRequest/bluetooth_SDP_ServiceAttributeRequest.py
@@ -12,35 +12,81 @@ class bluetooth_SDP_ServiceAttributeRequest(bluetooth_test.BluetoothTest):
     """
     version = 1
 
-    SDP_SERVER_CLASS_ID           = 0x1000
-    SERVICE_RECORD_HANDLE_ATTR_ID = 0x0000
+    MAX_REC_CNT                      = 3
+    MAX_ATTR_BYTE_CNT                = 300
+
+    SDP_SERVER_CLASS_ID              = 0x1000
+    SERVICE_RECORD_HANDLE_ATTR_ID    = 0x0000
+
+    GAP_CLASS_ID                     = 0x1800
+    BROWSE_GROUP_LIST_ATTR_ID        = 0x0005
+    PUBLIC_BROWSE_ROOT               = 0x1002
+
+
+    def get_single_handle(self, class_id):
+        """Send a Service Search Request to get a handle for specific class ID.
+
+        @return -1 if request failed, record handle as int otherwise
+
+        """
+        res = self.tester.service_search_request([class_id], self.MAX_REC_CNT)
+        if not (isinstance(res, list) and len(res) > 0):
+            return -1
+        return res[0]
+
+
+    def test_record_handle_attribute(self):
+        """Implementation of test TP/SERVER/SA/BV-01-C from SDP Specification.
+
+        @return True if test passes, False if test fails
+
+        """
+        # Send Service Search Request to find out record handle for
+        # SDP Server service.
+        record_handle = self.get_single_handle(self.SDP_SERVER_CLASS_ID)
+        if record_handle == -1:
+            return False
+
+        # Send Service Attribute Request for Service Record Handle Attribute.
+        res = self.tester.service_attribute_request(
+                  record_handle,
+                  self.MAX_ATTR_BYTE_CNT,
+                  [self.SERVICE_RECORD_HANDLE_ATTR_ID])
+
+        # Ensure that returned attribute is correct.
+        return res == [self.SERVICE_RECORD_HANDLE_ATTR_ID, record_handle]
+
+
+    def test_browse_group_attribute(self):
+        """Implementation of test TP/SERVER/SA/BV-08-C from SDP Specification.
+
+        @return True if test passes, False if test fails
+
+        """
+        record_handle = self.get_single_handle(self.GAP_CLASS_ID)
+        if record_handle == -1:
+            return False
+
+        res = self.tester.service_attribute_request(
+                  record_handle,
+                  self.MAX_ATTR_BYTE_CNT,
+                  [self.BROWSE_GROUP_LIST_ATTR_ID])
+
+        return res == [self.BROWSE_GROUP_LIST_ATTR_ID,
+                       [self.PUBLIC_BROWSE_ROOT]]
 
 
     def correct_request(self):
-        """Search the existing service on the DUT using the Tester.
+        """Run basic tests for Service Attribute Request.
 
-        @return True if found, False if not found
+        @return True if all tests finishes correctly, False otherwise
 
         """
         # Connect to the DUT via L2CAP using SDP socket.
         self.tester.connect(self.adapter['Address'])
 
-        # Send Service Search Request to find out record handle for
-        # SDP Server service.
-        lst = self.tester.service_search_request([self.SDP_SERVER_CLASS_ID],
-                                                 3, 128)
-        if not lst:
-            return False
-        record_handle = lst[0]
-
-        # Send Service Attribute Request for Service Record Handle Attribute.
-        res = self.tester.service_attribute_request(
-                  record_handle, 100, [self.SERVICE_RECORD_HANDLE_ATTR_ID])
-
-        # Ensure that returned attribute is correct.
-        return (len(res) == 2 and
-                res[0] == self.SERVICE_RECORD_HANDLE_ATTR_ID and
-                res[1] == record_handle)
+        return (self.test_record_handle_attribute() and
+                self.test_browse_group_attribute())
 
 
     def run_once(self):
