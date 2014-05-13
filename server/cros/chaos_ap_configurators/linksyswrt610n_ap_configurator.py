a17686bd552e9f4b270eacda2f6354f1d9662b78
Deepak Gopal <deepakg@chromium.org>
2014-05-13 10:40:16 +0530

    Adding host12 and host13 from row4-rack3.
    I had missed checking in the configurator file used for both these routers.
    
    BUG=chromium:364792
    TEST=unittest
    
    Change-Id: I25347f8b72c1231950273dbe9b2ba802fd9f42cb
    Reviewed-on: https://chromium-review.googlesource.com/199355
    Commit-Queue: Deepak Gopal <deepakg@chromium.org>
    Tested-by: Deepak Gopal <deepakg@chromium.org>
    Reviewed-by: Kris Rambish <krisr@chromium.org>

--- server/cros/chaos_ap_configurators/linksyswrt610n_ap_configurator.py
+++ server/cros/chaos_ap_configurators/linksyswrt610n_ap_configurator.py
@@ -1,28 +1,16 @@
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Copyright (c) 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import urlparse
 
 import ap_spec
-import linksyse_dual_band_configurator
+import linksyse2500_ap_configurator
 
 
-class Linksyse2500APConfigurator(linksyse_dual_band_configurator.
-                                 LinksyseDualBandAPConfigurator):
-    """Derived class to control Linksys E2500 router."""
-
-
-    def get_number_of_pages(self):
-        return 2
-
-
-    def get_supported_modes(self):
-        return [{'band': ap_spec.BAND_2GHZ,
-                 'modes': [ap_spec.MODE_B, ap_spec.MODE_N, ap_spec.MODE_B |
-                           ap_spec.MODE_G, ap_spec.MODE_G, ap_spec.MODE_M]},
-                {'band': ap_spec.BAND_5GHZ,
-                 'modes': [ap_spec.MODE_A, ap_spec.MODE_N, ap_spec.MODE_M]}]
+class LinksysWRT610NAPConfigurator(linksyse2500_ap_configurator.
+                                   Linksyse2500APConfigurator):
+    """Base class for objects to configure Linksys wrt610 and E3000 AP's
+       using webdriver."""
 
 
     def is_security_mode_supported(self, security_mode):
@@ -33,33 +21,10 @@ class Linksyse2500APConfigurator(linksyse_dual_band_configurator.
 
         @returns True is suppported; False otherwise.
         """
-        if self.current_band == ap_spec.BAND_5GHZ:
-            return security_mode in (ap_spec.SECURITY_TYPE_DISABLED,
-                                     ap_spec.SECURITY_TYPE_WPAPSK,
-                                     ap_spec.SECURITY_TYPE_WPA2PSK)
         return security_mode in (ap_spec.SECURITY_TYPE_DISABLED,
                                  ap_spec.SECURITY_TYPE_WPAPSK,
                                  ap_spec.SECURITY_TYPE_WPA2PSK,
                                  ap_spec.SECURITY_TYPE_WEP)
-
-
-    def navigate_to_page(self, page_number):
-        """Navigate to the passed in page.
-
-        @param page_number: the page number as an integer
-        """
-        if page_number == 1:
-            page_url = urlparse.urljoin(self.admin_interface_url,
-                                        'Wireless_Basic.asp')
-            self.get_url(page_url, page_title='Settings')
-        elif page_number == 2:
-            page_url = urlparse.urljoin(self.admin_interface_url,
-                                        'WL_WPATable.asp')
-            self.get_url(page_url, page_title='Security')
-        else:
-            raise RuntimeError('Invalid page number passed. Number of pages '
-                               '%d, page value sent was %d' %
-                               (self.get_number_of_pages(), page_number))
 
 
     def _set_mode(self, mode, band=None):
@@ -69,10 +34,9 @@ class Linksyse2500APConfigurator(linksyse_dual_band_configurator.
                         ap_spec.MODE_N: 'Wireless-N Only',
                         ap_spec.MODE_A: 'Wireless-A Only',
                         ap_spec.MODE_M: 'Mixed'}
-        xpath = '//select[@name="net_mode_24g"]'
-        if self.current_band == ap_spec.BAND_5GHZ or band == ap_spec.BAND_5GHZ:
-            self.current_band = ap_spec.BAND_5GHZ
-            xpath = '//select[@name="net_mode_5g"]'
+        xpath = '//select[@name="wl0_net_mode"]'
+        if self.current_band == ap_spec.BAND_5GHZ:
+            xpath = '//select[@name="wl1_net_mode"]'
         mode_name = ''
         if mode in mode_mapping.keys():
             mode_name = mode_mapping[mode]
@@ -84,9 +48,9 @@ class Linksyse2500APConfigurator(linksyse_dual_band_configurator.
 
 
     def _set_ssid(self, ssid):
-        xpath = '//input[@maxlength="32" and @name="ssid_24g"]'
+        xpath = '//input[@name="wl0_ssid"]'
         if self.current_band == ap_spec.BAND_5GHZ:
-            xpath = '//input[@maxlength="32" and @name="ssid_5g"]'
+            xpath = '//input[@name="wl1_ssid"]'
         self.set_content_of_text_field_by_xpath(ssid, xpath)
         self._ssid = ssid
 
@@ -110,25 +74,10 @@ class Linksyse2500APConfigurator(linksyse_dual_band_configurator.
                                              alert_handler=self._alert_handler)
 
 
-    def set_security_disabled(self):
-        self.add_item_to_command_list(self._set_security_disabled, (), 2, 900)
-
-
-    def set_security_wep(self, key_value, authentication):
-        self.add_item_to_command_list(self._set_security_wep,
-                                      (key_value, authentication), 2, 900)
-
-
-    def set_security_wpapsk(self, security, shared_key, update_interval=None):
-        self.add_item_to_command_list(self._set_security_wpapsk,
-                                      (security, shared_key, update_interval),
-                                       2, 900)
-
-
     def _set_visibility(self, visible=True):
-        button = 'closed_24g'
+        button = 'wl0_closed'
         if self.current_band == ap_spec.BAND_5GHZ:
-            button = 'closed_5g'
+            button = 'wl1_closed'
         int_value = 0 if visible else 1
         xpath = ('//input[@value="%d" and @name="%s"]' % (int_value, button))
-        self.click_button_by_xpath(xpath)
+        self.click_button_by_xpath(xpath, alert_handler=self._alert_handler)
