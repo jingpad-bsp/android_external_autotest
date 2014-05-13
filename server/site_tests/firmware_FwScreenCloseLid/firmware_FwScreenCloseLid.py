79e6739c6dc5c9b35c877c176b3fc121132ed385
Yusuf Mohsinally <mohsinally@google.com>
2014-05-12 23:25:58 -0700

    Converting firmware_FwScreenCloseLid to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against wolf device.
    
    Change-Id: I75ee7626546311ced3b886c70a95a2b703fb1a29
    Reviewed-on: https://chromium-review.googlesource.com/199467
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Reviewed-by: David Hendricks <dhendrix@chromium.org>

--- server/site_tests/firmware_FwScreenCloseLid/firmware_FwScreenCloseLid.py
+++ server/site_tests/firmware_FwScreenCloseLid/firmware_FwScreenCloseLid.py
@@ -6,10 +6,10 @@ import logging
 import time
 
 from autotest_lib.client.common_lib import error
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_FwScreenCloseLid(FAFTSequence):
+class firmware_FwScreenCloseLid(FirmwareTest):
     """
     Servo based lid close triggered shutdown test during firmware screens.
 
@@ -22,12 +22,10 @@ class firmware_FwScreenCloseLid(FAFTSequence):
 
     SHORT_SHUTDOWN_CONFIRMATION_PERIOD = 0.1
 
-
     def wait_second_screen_and_close_lid(self):
         """Wait and trigger TO_NORM or RECOVERY INSERT screen and close lid."""
         self.wait_fw_screen_and_trigger_recovery()
         self.wait_longer_fw_screen_and_close_lid()
-
 
     def wait_yuck_screen_and_close_lid(self):
         """Wait and trigger yuck screen and clod lid."""
@@ -35,7 +33,6 @@ class firmware_FwScreenCloseLid(FAFTSequence):
         self.servo.switch_usbkey('dut')
         time.sleep(self.faft_config.load_usb)
         self.wait_longer_fw_screen_and_close_lid()
-
 
     def initialize(self, host, cmdline_args):
         super(firmware_FwScreenCloseLid, self).initialize(host, cmdline_args)
@@ -48,15 +45,13 @@ class firmware_FwScreenCloseLid(FAFTSequence):
             # yuck screen later.
             self.corrupt_usb_kernel(usb_dev)
 
-
     def cleanup(self):
         if self.faft_config.has_lid:
             self.servo.switch_usbkey('host')
             usb_dev = self.servo.probe_host_usb_dev()
-            # Restore the kernel of USB stick which is corrupted on setup phase.
+            # Restore kernel of USB stick which is corrupted on setup phase.
             self.restore_usb_kernel(usb_dev)
         super(firmware_FwScreenCloseLid, self).cleanup()
-
 
     def run_once(self):
         if not self.faft_config.has_lid:
@@ -64,93 +59,84 @@ class firmware_FwScreenCloseLid(FAFTSequence):
             return
 
         if self.faft_config.chrome_ec and not self.check_ec_capability(['lid']):
-            raise error.TestNAError("TEST IT MANUALLY! Chrome EC can't control "
-                    "lid on the device %s" % self.faft_config.platform)
+            raise error.TestNAError("TEST IT MANUALLY! ChromeEC can't control "
+                                    "lid on the device %s" %
+                                    self.faft_config.platform)
 
-        self.register_faft_sequence((
-            {   # Step 1, expected dev mode and reboot.
-                # When the next DEVELOPER SCREEN shown, close lid
-                # to make DUT shutdown.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'firmware_action': (self.run_shutdown_process,
-                                    (self.wait_fw_screen_and_close_lid,
-                                     self.servo.lid_open,
-                                     self.wait_fw_screen_and_ctrl_d)),
-            },
-            {   # Step 2, reboot. When the developer screen shown, press
-                # enter key to trigger either TO_NORM screen (new) or
-                # RECOVERY INSERT screen (old). Then close lid to
-                # make DUT shutdown.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'firmware_action': (self.run_shutdown_process,
-                                    (self.wait_second_screen_and_close_lid,
-                                     self.servo.lid_open,
-                                     self.wait_fw_screen_and_ctrl_d,
-                                     self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)),
-            },
-            {   # Step 3, request recovery boot. When the RECOVERY INSERT
-                # screen shows, close lid to make DUT shutdown.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'userspace_action':
-                    (self.faft_client.system.request_recovery_boot),
-                'firmware_action': (self.run_shutdown_process,
-                                    (self.wait_longer_fw_screen_and_close_lid,
-                                     self.servo.lid_open,
-                                     self.wait_fw_screen_and_ctrl_d,
-                                     self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)),
-            },
-            {   # Step 4, request recovery boot again. When the recovery
-                # insert screen shows, insert a corrupted USB and trigger
-                # a YUCK SCREEN. Then close lid to make DUT shutdown.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'userspace_action': (
-                    self.faft_client.system.request_recovery_boot),
-                'firmware_action': (self.run_shutdown_process,
-                                    (self.wait_yuck_screen_and_close_lid,
-                                     self.servo.lid_open,
-                                     self.wait_fw_screen_and_ctrl_d,
-                                     self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)),
-            },
-            {   # Step 5, switch back to normal mode.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'userspace_action': self.enable_normal_mode_and_reboot,
-                'reboot_action': None,
-            },
-            {   # Step 6, expected normal mode and request recovery boot.
-                # Because an USB stick is inserted, a RECOVERY REMOVE screen
-                # shows. Close lid to make DUT shutdown.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '0',
-                    'mainfw_type': 'normal',
-                }),
-                'userspace_action':
-                    (self.faft_client.system.request_recovery_boot),
-                'firmware_action': (self.run_shutdown_process,
-                                    (self.wait_longer_fw_screen_and_close_lid,
-                                     self.servo.lid_open,
-                                     None,
-                                     self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)),
-            },
-            {   # Step 7, done.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '0',
-                    'mainfw_type': 'normal',
-                }),
-            },
-        ))
-        self.run_faft_sequence()
+        logging.info("Expected dev mode and reboot. "
+                     "When the next DEVELOPER SCREEN shown, close lid "
+                     "to make DUT shutdown.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'devsw_boot': '1',
+                          'mainfw_type': 'developer',
+                          }))
+        self.reboot_warm(wait_for_dut_up=False)
+        self.run_shutdown_process(self.wait_fw_screen_and_close_lid,
+                                  self.servo.lid_open,
+                                  self.wait_fw_screen_and_ctrl_d)
+
+        logging.info("Reboot. When the developer screen shown, press "
+                     "enter key to trigger either TO_NORM screen (new) or "
+                     "RECOVERY INSERT screen (old). Then close lid to "
+                     "make DUT shutdown.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'devsw_boot': '1',
+                          'mainfw_type': 'developer',
+                          }))
+        self.reboot_warm(wait_for_dut_up=False)
+        self.run_shutdown_process(self.wait_second_screen_and_close_lid,
+                                  self.servo.lid_open,
+                                  self.wait_fw_screen_and_ctrl_d,
+                                  self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)
+
+        logging.info("Request recovery boot. When the RECOVERY INSERT "
+                     "screen shows, close lid to make DUT shutdown.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'devsw_boot': '1',
+                          'mainfw_type': 'developer',
+                          }))
+        self.faft_client.system.request_recovery_boot()
+        self.reboot_warm(wait_for_dut_up=False)
+        self.run_shutdown_process(self.wait_longer_fw_screen_and_close_lid,
+                                  self.servo.lid_open,
+                                  self.wait_fw_screen_and_ctrl_d,
+                                  self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)
+
+        logging.info("Request recovery boot again. When the recovery "
+                     "insert screen shows, insert a corrupted USB and trigger "
+                     "a YUCK SCREEN. Then close lid to make DUT shutdown.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'devsw_boot': '1',
+                          'mainfw_type': 'developer',
+                          }))
+        self.faft_client.system.request_recovery_boot()
+        self.reboot_warm(wait_for_dut_up=False)
+        self.run_shutdown_process(self.wait_yuck_screen_and_close_lid,
+                                  self.servo.lid_open,
+                                  self.wait_fw_screen_and_ctrl_d,
+                                  self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)
+
+        logging.info("Switch back to normal mode.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'devsw_boot': '1',
+                          'mainfw_type': 'developer',
+                          }))
+        self.enable_normal_mode_and_reboot()
+
+        logging.info("Expected normal mode and request recovery boot. "
+                     "Because an USB stick is inserted, a RECOVERY REMOVE "
+                     "screen shows. Close lid to make DUT shutdown.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'devsw_boot': '0',
+                          'mainfw_type': 'normal',
+                          }))
+        self.faft_client.system.request_recovery_boot()
+        self.reboot_warm(wait_for_dut_up=False)
+        self.run_shutdown_process(self.wait_longer_fw_screen_and_close_lid,
+                                  self.servo.lid_open,
+                                  None,
+                                  self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)
+        self.check_state((self.checkers.crossystem_checker, {
+                          'devsw_boot': '0',
+                          'mainfw_type': 'normal',
+                          }))
