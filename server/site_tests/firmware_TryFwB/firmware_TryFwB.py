b3b23205345e1611024003ff2e402d9e0f08c7a7
Yusuf Mohsinally <mohsinally@google.com>
2014-05-13 11:40:08 -0700

    Converting firmware_TryFwB to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against wolf device.
    
    Change-Id: I6d5fe640f746ee81763e3e606cb1125a3110b658
    Reviewed-on: https://chromium-review.googlesource.com/199530
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_TryFwB/firmware_TryFwB.py
+++ server/site_tests/firmware_TryFwB/firmware_TryFwB.py
@@ -2,15 +2,15 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+import logging
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_TryFwB(FAFTSequence):
+class firmware_TryFwB(FirmwareTest):
     """
     Servo based RW firmware B boot test.
     """
     version = 1
-
 
     def initialize(self, host, cmdline_args, dev_mode=False, ec_wp=None):
         super(firmware_TryFwB, self).initialize(host, cmdline_args, ec_wp=ec_wp)
@@ -18,32 +18,28 @@ class firmware_TryFwB(FAFTSequence):
         self.setup_usbkey(usbkey=False)
         self.setup_tried_fwb(tried_fwb=False)
 
-
     def cleanup(self):
         self.setup_tried_fwb(tried_fwb=False)
         super(firmware_TryFwB, self).cleanup()
 
-
     def run_once(self):
-        self.register_faft_sequence((
-            {   # Step 1, set fwb_tries flag
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_act': 'A',
-                    'tried_fwb': '0',
-                }),
-                'userspace_action': self.faft_client.system.set_try_fw_b,
-            },
-            {   # Step 2, expected firmware B boot, reboot
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_act': 'B',
-                    'tried_fwb': '1',
-                }),
-            },
-            {   # Step 3, expected firmware A boot, done
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_act': 'A',
-                    'tried_fwb': '0',
-                }),
-            },
-        ))
-        self.run_faft_sequence()
+        logging.info("Set fwb_tries flag")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'mainfw_act': 'A',
+                          'tried_fwb': '0',
+                          }))
+        self.faft_client.system.set_try_fw_b()
+        self.reboot_warm()
+
+        logging.info("Expected firmware B boot, reboot")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'mainfw_act': 'B',
+                          'tried_fwb': '1',
+                          }))
+        self.reboot_warm()
+
+        logging.info("Expected firmware A boot, done")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'mainfw_act': 'A',
+                          'tried_fwb': '0',
+                          }))
