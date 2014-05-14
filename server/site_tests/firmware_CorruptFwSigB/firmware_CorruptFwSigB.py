6fd1ccc2410d73db8b02a48c00f2ca56220888ed
Yusuf Mohsinally <mohsinally@google.com>
2014-05-14 15:35:25 -0700

    Converting firmware_CorruptFwSigB to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against peppy
    
    Change-Id: Ie6c9f8fb707b471e9fb63111b9010d85ee8d0353
    Reviewed-on: https://chromium-review.googlesource.com/199845
    Reviewed-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_CorruptFwSigB/firmware_CorruptFwSigB.py
+++ server/site_tests/firmware_CorruptFwSigB/firmware_CorruptFwSigB.py
@@ -2,15 +2,16 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+import logging
+
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_CorruptFwSigB(FAFTSequence):
+class firmware_CorruptFwSigB(FirmwareTest):
     """
     Servo based firmware signature B corruption test.
     """
     version = 1
-
 
     def initialize(self, host, cmdline_args, dev_mode=False):
         super(firmware_CorruptFwSigB, self).initialize(host, cmdline_args)
@@ -18,41 +19,38 @@ class firmware_CorruptFwSigB(FAFTSequence):
         self.setup_dev_mode(dev_mode)
         self.setup_usbkey(usbkey=False)
 
-
     def cleanup(self):
         self.restore_firmware()
         super(firmware_CorruptFwSigB, self).cleanup()
 
-
     def run_once(self):
-        self.register_faft_sequence((
-            {   # Step 1, expected firmware A boot and corrupt firmware
-                # signature B.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_act': 'A',
-                    'tried_fwb': '0',
-                }),
-                'userspace_action': (self.faft_client.bios.corrupt_sig, 'b'),
-            },
-            {   # Step 2, expected firmware A boot and set try_fwb flag
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_act': 'A',
-                    'tried_fwb': '0',
-                }),
-                'userspace_action': self.faft_client.system.set_try_fw_b,
-            },
-            {   # Step 3, expected firmware A boot and restore firmware B
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_act': 'A',
-                    'tried_fwb': '1',
-                }),
-                'userspace_action': (self.faft_client.bios.restore_sig, 'b'),
-            },
-            {   # Step 4, final check and done
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_act': 'A',
-                    'tried_fwb': '0',
-                }),
-            },
-        ))
-        self.run_faft_sequence()
+        logging.info("Expected firmware A boot and corrupt "
+                     "firmware signature B.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'mainfw_act': 'A',
+                              'tried_fwb': '0',
+                              }))
+        self.faft_client.bios.corrupt_sig('b')
+        self.reboot_warm()
+
+        logging.info("Expected firmware A boot and set try_fwb flag.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'mainfw_act': 'A',
+                              'tried_fwb': '0',
+                              }))
+        self.faft_client.system.set_try_fw_b()
+        self.reboot_warm()
+
+        logging.info("Expected firmware A boot and restore firmware B.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'mainfw_act': 'A',
+                              'tried_fwb': '1',
+                              }))
+        self.faft_client.bios.restore_sig('b')
+        self.reboot_warm()
+
+        logging.info("Final check and done.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'mainfw_act': 'A',
+                              'tried_fwb': '0',
+                              }))
