ffb5077df39f107654a9e66d9748696d3d174408
mussa <mussa@google.com>
2014-05-13 17:23:34 -0700

    Adds basic test case to detect video glitches.
    This test case will select a 480p video, generate random time values within video
    duration and capture screenshots at those times. It will compare such images with
    known good images that it had downloaded when it(the test) began.
    
      * Removed video.html file because it was already included in another CL.
      * Changed 'TIME' attribute of the test from SHORT to LONG. Test runs for about 20mins.
    
    BUG=chromium:363815
    TEST=Ran this test locally.
    
    Change-Id: Iaad9f058727548a9d23dbefb431d7638593d3790
    Reviewed-on: https://chromium-review.googlesource.com/199646
    Reviewed-by: Kris Rambish <krisr@chromium.org>
    Reviewed-by: Rohit Makasana <rohitbm@chromium.org>
    Commit-Queue: Rohit Makasana <rohitbm@chromium.org>
    Tested-by: Rohit Makasana <rohitbm@chromium.org>

---
+++ client/site_tests/video_GlitchDetection/video_GlitchDetection.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import file_utils
+from autotest_lib.client.common_lib.cros import chrome
+from autotest_lib.client.cros.video import media_test_factory
+
+
+class video_GlitchDetection(test.test):
+    """
+    Seeks video to random time instances and checks if the images shown at
+    such respective times are expected.
+
+    """
+
+    version = 2
+
+    def run_video_glitch_detection_test(self, browser, channel,
+                                        video_format, video_def):
+        """
+        Takes video screenshots and compares them against known golden images.
+
+        Main test steps:
+        1. Configure test, set up environment, create needed objects.
+        2. Generate sequence of time instance to capture images.
+        (This sequence will be generated based on configuration above.)
+        3. Download golden images for time instances above from cloud storage.
+        4. Load the video based on received configuration (video_format, res)
+        5. Capture screenshots for each of generated time instances above.
+        6. Verify that captured screenshots are the same as expected golden
+        screenshots downloaded earlier.
+        The criteria to determine if two images are the same is read from the
+        configuration above.
+
+        @param browser: Object to interact with Chrome browser
+        @param channel: The channel we are running on: dev, beta
+                        This is used to choose how many screenshots we will take
+        @param video_format: Format of the video to test
+        @param video_def: Resolution of the video to test
+
+        """
+
+        factory = media_test_factory.MediaTestFactory(browser.tabs[0],
+                                                      browser.http_server,
+                                                      self.bindir, channel,
+                                                      video_format,
+                                                      video_def)
+
+        golden_image_downloader = factory.make_golden_image_downloader()
+        player = factory.make_media_player()
+        sequence_generator = factory.make_capture_sequence_generator()
+        screenshot_collector = factory.make_video_screenshot_collector()
+        verifier = factory.make_verifier()
+
+        test_dir = factory.local_golden_images_dir
+
+        file_utils.make_leaf_dir(test_dir)
+
+        file_utils.ensure_all_dirs_exist(test_dir)
+
+        timestamps = sequence_generator.generate()
+
+        golden_images = golden_image_downloader.download_images(timestamps)
+
+        file_utils.ensure_all_files_exist(golden_images)
+
+        player.load_video()
+
+        results = screenshot_collector.collect_multiple_screenshots(
+            timestamps)
+
+        file_utils.ensure_all_files_exist(results)
+
+        verifier.verify(golden_images, results)
+
+        file_utils.rm_dirs_if_exist(test_dir)
+
+
+    def run_once(self, channel, video_format, video_def):
+        with chrome.Chrome() as cr:
+            cr.browser.SetHTTPServerDirectories(self.bindir)
+            self.run_video_glitch_detection_test(cr.browser, channel,
+                                                 video_format, video_def)
