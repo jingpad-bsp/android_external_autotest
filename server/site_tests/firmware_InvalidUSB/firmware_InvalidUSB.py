a7da6ce6053769e1e25d3f0c25c861cded6964f3
Yusuf Mohsinally <mohsinally@google.com>
2014-05-14 14:06:55 -0700

    Converting firmware_InvalidUSB to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against peppy
    
    Change-Id: I60d739605d8e8eb70cd2169aa36926311713378a
    Reviewed-on: https://chromium-review.googlesource.com/199890
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_InvalidUSB/firmware_InvalidUSB.py
+++ server/site_tests/firmware_InvalidUSB/firmware_InvalidUSB.py
@@ -7,11 +7,11 @@ import time
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.server.cros import vboot_constants as vboot
-from autotest_lib.server.cros.faft.faft_classes import ConnectionError
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+from autotest_lib.server.cros.faft.firmware_test import ConnectionError
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_InvalidUSB(FAFTSequence):
+class firmware_InvalidUSB(FirmwareTest):
     """
     Servo based booting an invalid USB image test.
 
@@ -22,16 +22,35 @@ class firmware_InvalidUSB(FAFTSequence):
     """
     version = 1
 
-
     def restore_usb(self):
         """Restore the USB image. USB plugs/unplugs happen in this method."""
         self.servo.switch_usbkey('host')
         usb_dev = self.servo.probe_host_usb_dev()
         self.restore_usb_kernel(usb_dev)
 
+    def initialize(self, host, cmdline_args):
+        super(firmware_InvalidUSB, self).initialize(host, cmdline_args)
+        self.servo.switch_usbkey('host')
+        usb_dev = self.servo.probe_host_usb_dev()
+        self.assert_test_image_in_usb_disk(usb_dev)
+        self.corrupt_usb_kernel(usb_dev)
+        self.setup_dev_mode(dev_mode=False)
+        self.servo.switch_usbkey('dut')
 
-    def insert_corrupted_usb_and_restore(self):
-        """Insert the corrupted USB on firmware screen. Then restore it."""
+    def cleanup(self):
+        self.restore_usb()
+        super(firmware_InvalidUSB, self).cleanup()
+
+    def run_once(self):
+        logging.info("Turn on the recovery boot. Remove and insert the"
+                     "corrupted USB stick, a boot failure is expected."
+                     "Restore the USB image and boot it again.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'devsw_boot': '0',
+                          'mainfw_type': 'normal',
+                          }))
+        self.faft_client.system.request_recovery_boot()
+        self.reboot_warm(wait_for_dut_up=False)
         self.wait_fw_screen_and_plug_usb()
         logging.info('Wait to ensure the USB image is unable to boot...')
         try:
@@ -44,48 +63,17 @@ class firmware_InvalidUSB(FAFTSequence):
         self.restore_usb()
         time.sleep(self.faft_config.sync)
         self.servo.switch_usbkey('dut')
+        self.wait_for_kernel_up(install_deps=True)
 
+        logging.info("Expected to boot the restored USB image and reboot.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'mainfw_type': 'recovery',
+                          'recovery_reason': vboot.RECOVERY_REASON['US_TEST'],
+                          }))
+        self.reboot_warm()
 
-    def initialize(self, host, cmdline_args):
-        super(firmware_InvalidUSB, self).initialize(host, cmdline_args)
-        self.servo.switch_usbkey('host')
-        usb_dev = self.servo.probe_host_usb_dev()
-        self.assert_test_image_in_usb_disk(usb_dev)
-        self.corrupt_usb_kernel(usb_dev)
-        self.setup_dev_mode(dev_mode=False)
-        self.servo.switch_usbkey('dut')
-
-
-    def cleanup(self):
-        self.restore_usb()
-        super(firmware_InvalidUSB, self).cleanup()
-
-
-    def run_once(self):
-        self.register_faft_sequence((
-            {   # Step 1, turn on the recovery boot. Remove and insert the
-                # corrupted USB stick, a boot failure is expected.
-                # Restore the USB image and boot it again.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '0',
-                    'mainfw_type': 'normal',
-                }),
-                'userspace_action':
-                    (self.faft_client.system.request_recovery_boot),
-                'firmware_action': self.insert_corrupted_usb_and_restore,
-                'install_deps_after_boot': True,
-            },
-            {   # Step 2, expected to boot the restored USB image and reboot.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_type': 'recovery',
-                    'recovery_reason' : vboot.RECOVERY_REASON['US_TEST'],
-                }),
-            },
-            {   # Step 3, expected to normal boot and done.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '0',
-                    'mainfw_type': 'normal',
-                }),
-            },
-        ))
-        self.run_faft_sequence()
+        logging.info("Expected to normal boot and done.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'devsw_boot': '0',
+                          'mainfw_type': 'normal',
+                          }))
