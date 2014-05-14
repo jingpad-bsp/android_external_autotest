67d74e27cda90ad0ded6951025e14d441d94e61c
Yusuf Mohsinally <mohsinally@google.com>
2014-05-14 15:14:49 -0700

    Converting firmware_CorruptKernelB to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against
    
    Change-Id: I36c9d2dd24babf75373f77615e5d564a67e12583
    Reviewed-on: https://chromium-review.googlesource.com/199903
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_CorruptKernelB/firmware_CorruptKernelB.py
+++ server/site_tests/firmware_CorruptKernelB/firmware_CorruptKernelB.py
@@ -2,10 +2,12 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+import logging
+
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_CorruptKernelB(FAFTSequence):
+class firmware_CorruptKernelB(FirmwareTest):
     """
     Servo based kernel B corruption test.
 
@@ -15,7 +17,6 @@ class firmware_CorruptKernelB(FAFTSequence):
     """
     version = 1
 
-
     def initialize(self, host, cmdline_args, dev_mode=False):
         super(firmware_CorruptKernelB, self).initialize(host, cmdline_args)
         self.backup_kernel()
@@ -24,35 +25,31 @@ class firmware_CorruptKernelB(FAFTSequence):
         self.setup_usbkey(usbkey=False)
         self.setup_kernel('a')
 
-
     def cleanup(self):
         self.restore_cgpt_attributes()
         self.restore_kernel()
         super(firmware_CorruptKernelB, self).cleanup()
 
-
     def run_once(self):
-        self.register_faft_sequence((
-            {   # Step 1, prioritize kernel B
-                'state_checker': (self.checkers.root_part_checker, 'a'),
-                'userspace_action': (self.reset_and_prioritize_kernel, 'b'),
-                'reboot_action': self.warm_reboot,
-            },
-            {   # Step 2, expected kernel B boot and corrupt kernel B
-                'state_checker': (self.checkers.root_part_checker, 'b'),
-                'userspace_action': (self.faft_client.kernel.corrupt_sig, 'b'),
-            },
-            {   # Step 3, expected kernel A boot and restore kernel B
-                'state_checker': (self.checkers.root_part_checker, 'a'),
-                'userspace_action': (self.faft_client.kernel.restore_sig, 'b'),
-            },
-            {   # Step 4, expected kernel B boot and prioritize kerenl A
-                'state_checker': (self.checkers.root_part_checker, 'b'),
-                'userspace_action': (self.reset_and_prioritize_kernel, 'a'),
-                'reboot_action': self.warm_reboot,
-            },
-            {   # Step 5, expected kernel A boot
-                'state_checker': (self.checkers.root_part_checker, 'a'),
-            },
-        ))
-        self.run_faft_sequence()
+        logging.info("Prioritize kernel B.")
+        self.check_state((self.checkers.root_part_checker, 'a'))
+        self.reset_and_prioritize_kernel('b')
+        self.reboot_warm()
+
+        logging.info("Expected kernel B boot and corrupt kernel B.")
+        self.check_state((self.checkers.root_part_checker, 'b'))
+        self.faft_client.kernel.corrupt_sig('b')
+        self.reboot_warm()
+
+        logging.info("Expected kernel A boot and restore kernel B.")
+        self.check_state((self.checkers.root_part_checker, 'a'))
+        self.faft_client.kernel.restore_sig('b')
+        self.reboot_warm()
+
+        logging.info("Expected kernel B boot and prioritize kerenl A.")
+        self.check_state((self.checkers.root_part_checker, 'b'))
+        self.reset_and_prioritize_kernel('a')
+        self.reboot_warm()
+
+        logging.info("Expected kernel A boot.")
+        self.check_state((self.checkers.root_part_checker, 'a'))
