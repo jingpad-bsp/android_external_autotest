cd8d656506a12f1545d58e11f9bb7cf6a755c5d0
Kris Rambish <krisr@chromium.org>
2014-05-15 19:34:47 -0700

    Update parser output to match spreadsheet.
    Cleans up some of the comments and makes the core status.log
    parser a bit more readable.
    
    BUG=None
    TEST=Ran it.
    
    Change-Id: I121c6702a5d79c0d4dfd8b459b00190572b28416
    Reviewed-on: https://chromium-review.googlesource.com/200190
    Tested-by: Kris Rambish <krisr@chromium.org>
    Reviewed-by: Bindu Mahadev <bmahadev@chromium.org>
    Commit-Queue: Kris Rambish <krisr@chromium.org>

--- server/cros/chaos_lib/chaos_parser.py
+++ server/cros/chaos_lib/chaos_parser.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 
 import argparse
+import copy
 import csv
 import logging
 import os
@@ -20,20 +21,31 @@ class ChaosParser(object):
     def __init__(self, results_dir):
         """ Constructs a parser interface.
 
-        @param results_dir: The results directory from a chaos test.
+        @param results_dir: complete path to restuls directory for a chaos test.
 
         """
         self.test_results_dir = results_dir
-        self.tests = list()
-        self.status_log = list()
-        self.keyval = None
 
 
-    def create_csv(self, filename, data):
+    def convert_set_to_string(self, set_list):
+        """Converts a set to a single string.
+
+        @param set_list: a set to convert
+
+        @returns a string, which is all items separated by the word 'and'
+
+        """
+        return_string = str()
+        for i in set_list:
+            return_string += str('%s and ' % i)
+        return return_string[:-5]
+
+
+    def create_csv(self, filename, data_list):
         """Creates a file in .csv format.
 
-        @param filename: Name for the csv file
-        @param data: All the information to write to the file
+        @param filename: name for the csv file
+        @param data_list: a list of all the info to write to a file
 
         """
         if not os.path.exists(RESULTS_DIR):
@@ -42,9 +54,8 @@ class ChaosParser(object):
             path = os.path.join(RESULTS_DIR, filename + '.csv')
             with open(path, 'wb') as f:
                 writer = csv.writer(f)
-                writer.writerows(data)
-                logging.info('Created CSV files for chaos failures in %s',
-                              RESULTS_DIR)
+                writer.writerow(data_list)
+                logging.info('Created CSV file %s', path)
         except IOError as e:
             logging.error('File operation failed with %s: %s', e.errno,
                            e.strerror)
@@ -56,7 +67,7 @@ class ChaosParser(object):
 
         @param line: Test ERROR string from chaos status.log
 
-        @return Returns the router name, brand.
+        @returns the router name or brand.
 
         """
         router_info = re.search('Router name: ([\w\s]+)', line)
@@ -64,45 +75,50 @@ class ChaosParser(object):
 
 
     def get_ap_mode_chan_freq(self, ssid):
-        """Gets the AP frequency from ssid using channel.
+        """Gets the AP band from ssid using channel.
 
-        @param ssid: A valid chaos test SSID
+        @param ssid: A valid chaos test SSID as a string
 
-        @return Returns the AP frequency.
+        @returns the AP band, mode, and channel.
 
         """
         channel_security_info = ssid.split('_')
         channel_info = channel_security_info[-2]
         mode = channel_security_info[-3]
         channel = int(re.split('(\d+)', channel_info)[1])
+        # TODO Choose if we want to keep band, we never put it in the
+        # spreadsheet and is currently unused.
         if channel in range(1, 15):
-            frequency = '2.4GHz'
+            band = '2.4GHz'
         else:
-            frequency = '5GHz'
+            band = '5GHz'
         return {'mode': mode.upper(), 'channel': channel,
-                'frequency': frequency}
+                'band': band}
 
 
-    def get_ap_security(self, ssid):
-        """Gets the AP security from ssid.
+    def generate_percentage_string(self, passed_tests, total_tests):
+        """Creates a pass percentage string in the formation x/y (zz%)
 
-        @param ssid: A valid chaos test SSID
+        @param passed_tests: int of passed tests
+        @param total_tests: int of total tests
 
-        @return Returns the AP's security.
+        @returns a formatted string as described above.
 
         """
-        channel_security_info = ssid.split('_')
-        security = channel_security_info[-1].upper()
-        return security
+        percent = float(passed_tests)/float(total_tests) * 100
+        percent_string = str(int(round(percent))) + '%'
+        return str('%d/%d (%s)' % (passed_tests, total_tests, percent_string))
 
 
-    def parse_keyval(self):
+    def parse_keyval(self, filepath):
         """Parses the 'keyvalue' file to get device details.
 
-        @return Returns a dict with device name and OS version.
+        @param filepath: the complete path to the keyval file
+
+        @returns a board with device name and OS version.
 
         """
-        f = open(self.keyval, 'r')
+        f = open(filepath, 'r')
         for line in f:
             line = line.split('=')
             if 'RELEASE_BOARD' in line[0]:
@@ -111,141 +127,109 @@ class ChaosParser(object):
                 lsb_dict['version'] = line[1].rstrip()
             else:
                 continue
+        f.close()
         return lsb_dict
 
 
-    def insert_into_tuple(self, item, value):
-        """Insert the value(pass percentage) information to the AP fail tuple.
-
-        @param item: The old tuple consisting of (board, version, mode, channel,
-                     security, AP)
-        @param value: The value to be inserted into the tuple
-
-        @return Returns a new tuple with percentage (board, version, mode,
-                channel, security, percentage, AP).
-
-        """
-        item = list(item)
-        item.insert(-1, value)
-        return tuple(item)
-
-
-    def get_pass_percentage(self, total_tests, config_fail, connect_fail):
-        """Calculate the actual pass percentages.
-
-        @total_tests: Total number of tests run
-        @config_fail: Total number of configuration failures
-        @connect_fail: Total number of connection failures
-
-        @return: Returns a dict with pass percentages for configuration and
-                 connection failures.
-
-        """
-        config_pass = total_tests - config_fail
-        connect_pass = config_pass - connect_fail
-        config_pass_percent = float(config_pass)/float(total_tests) * 100
-        config_pass_percent = str(int(round(config_pass_percent))) + '%'
-        connect_pass_percent = float(connect_pass)/float(config_pass) * 100
-        connect_pass_percent = str(int(round(connect_pass_percent))) + '%'
-        return {'config_pass_percent': config_pass_percent,
-                'connect_pass_percent':connect_pass_percent}
-
-
-    def parse_status_log(self):
+    def parse_status_log(self, board, os_version, security, status_log_path):
         """Parses the entire status.log file from chaos test for test failures.
            and creates two CSV files for connect fail and configuration fail
            respectively.
 
-        """
-        for test_type, status_log in zip(self.tests, self.status_log):
-            connect_fail_list = list()
-            config_fail_list = list()
-            f = open(status_log, 'r')
-            total = 0
-            for line in f:
-                line = line.strip()
-                if line.startswith('START\tnetwork_WiFi'):
-                   # TODO: @bmahadev, Add exception for PDU failure and do not
-                   # include that in the total tests.
-                   total += 1
-                elif 'END ERROR' in line or 'END FAIL' in line:
-                    if failure_type == CONNECT_FAIL:
-                        lsb_dict = self.parse_keyval()
-                        connect_fail_list.append((lsb_dict['board'],
-                                               lsb_dict['version'],
-                                               mode_chan_freq_dict['mode'],
-                                               mode_chan_freq_dict['channel'],
-                                               mode_chan_freq_dict['frequency'],
-                                               security, name))
-                    else:
-                        config_fail_list.append((security, hostname))
-                    failure_type = None
-                elif line.startswith('ERROR') or line.startswith('FAIL'):
-                    if 'Router name' in line:
-                        name = self.get_ap_name(line)
-                    title_info = line.split()
-                    ssid_or_host_info = title_info[1]
-                    # Get the hostname for the AP that failed configuration.
-                    if 'chromeos' in ssid_or_host_info:
-                        failure_type = CONFIG_FAIL
-                        host_info = title_info[1].split('.')
-                        hostname = host_info[1].split('_')[0]
-                    else:
-                        # Get the router name, frequency for the AP that failed
-                        # connect.
-                        ssid_info = title_info[1].split('.')
-                        ssid = ssid_info[1]
-                        if 'ch' not in ssid:
-                            ssid = ssid_info[2]
-                        security = self.get_ap_security(ssid)
-                        mode_chan_freq_dict = self.get_ap_mode_chan_freq(ssid)
-                        # Security mismatches and Ping failures are not connect
-                        # failures.
-                        if 'Ping command' in line or 'correct security' in line:
-                            failure_type = CONFIG_FAIL
-                            hostname = ssid
-                        else:
-                            failure_type = CONNECT_FAIL
-                elif line.startswith('Debug info'):
-                    name = self.get_ap_name(line)
-                else:
-                    continue
-            percent_dict = self.get_pass_percentage(total,
-                                                    len(config_fail_list),
-                                                    len(connect_fail_list))
-            if not config_fail_list:
-                config_fail_list.append((security, 'NONE'))
-            if not connect_fail_list:
-                connect_fail_list.append((lsb_dict['board'],
-                                          lsb_dict['version'],
-                                          mode_chan_freq_dict['mode'],
-                                          mode_chan_freq_dict['channel'],
-                                          mode_chan_freq_dict['frequency'],
-                                          security, 'NONE'))
-            connect_fail_csv_data = list()
-            config_fail_csv_data = list()
+        @param board: the board the test was run against as a string
+        @param os_version: the version of ChromeOS as a string
+        @param security: the security used during the test as a string
+        @param status_log_path: complete path to the status.log file
 
-            for item1 in connect_fail_list:
-                item1 = self.insert_into_tuple(item1,
-                                           percent_dict['connect_pass_percent'])
-                connect_fail_csv_data.append(item1)
-            for item2 in config_fail_list:
-                item2 = self.insert_into_tuple(item2,
-                                           percent_dict['config_pass_percent'])
-                config_fail_csv_data.append(item2)
-            self.create_csv('chaos_WiFi_connect_fail' + '.' + test_type, sorted(
-                             connect_fail_csv_data))
-            self.create_csv('chaos_WiFi_config_fail' + '.' + test_type, sorted(
-                             config_fail_csv_data))
+        """
+        # Items that can have multiple values
+        modes = list()
+        channels = list()
+        ap_names = list()
+        hostnames = list()
+        config_failure = connect_failure = False
+        f = open(status_log_path, 'r')
+        total = 0
+        for line in f:
+            line = line.strip()
+            if line.startswith('START\tnetwork_WiFi'):
+               # TODO: @bmahadev, Add exception for PDU failure and do not
+               # include that in the total tests.
+               total += 1
+            elif 'END ERROR' in line or 'END FAIL' in line:
+                if failure_type == CONNECT_FAIL:
+                    connect_failure = True
+                else:
+                    config_failure = True
+                failure_type = None
+            elif line.startswith('ERROR') or line.startswith('FAIL'):
+                if 'Router name' in line:
+                    # TODO: Should not appearing in the scan be a connect
+                    # failure?
+                    ap_names.append(self.get_ap_name(line))
+                title_info = line.split()
+                # Get the hostname for the AP that failed configuration.
+                if 'chromeos' in title_info[1]:
+                    failure_type = CONFIG_FAIL
+                    hostname = title_info[1].split('.')[1].split('_')[0]
+                else:
+                    # Get the router name, band for the AP that failed
+                    # connect.
+                    ssid_info = title_info[1].split('.')
+                    ssid = ssid_info[1]
+                    if 'ch' not in ssid:
+                        ssid = ssid_info[2]
+
+                    network_dict = self.get_ap_mode_chan_freq(ssid)
+                    modes.append(network_dict['mode'])
+                    channels.append(network_dict['channel'])
+
+                    # Security mismatches and Ping failures are not connect
+                    # failures.
+                    if ('Ping command' in line or
+                        'correct security' in line):
+                        failure_type = CONFIG_FAIL
+                        hostnames.append(ssid)
+                    else:
+                        failure_type = CONNECT_FAIL
+            elif line.startswith('Debug info'):
+                ap_names.append(self.get_ap_name(line))
+            else:
+                continue
+
+        config_pass = total - len(hostnames)
+        config_pass_string = self.generate_percentage_string(len(hostnames),
+                                                             total)
+        connect_pass_string = self.generate_percentage_string(len(ap_names),
+                                                              config_pass)
+
+        # Two blank entries for firmware and kernel versions
+        base_csv_list = [board, os_version, "", "",
+                         self.convert_set_to_string(set(modes)),
+                         self.convert_set_to_string(set(channels)),
+                         security]
+
+        connect_csv_list = copy.deepcopy(base_csv_list)
+        connect_csv_list.append(connect_pass_string)
+        connect_csv_list.extend(ap_names)
+
+        config_csv_list = copy.deepcopy(base_csv_list)
+        config_csv_list.append(config_pass_string)
+        config_csv_list.extend(hostnames)
+
+        self.create_csv('chaos_WiFi_connect_fail.' + security,
+                        connect_csv_list)
+        self.create_csv('chaos_WiFi_config_fail.' + security,
+                        config_csv_list)
 
 
     def traverse_results_dir(self, path):
         """Walks through the results directory and get the pathnames for the
            status.log and the keyval files.
 
-        @param path: Path for a specific test result directory.
+        @param path: complete path to a specific test result directory.
 
-        @return Returns a dict with absolute pathnames for the 'status.log' and
+        @returns a dict with absolute pathnames for the 'status.log' and
                 'keyfile' files.
 
         """
@@ -269,24 +253,31 @@ class ChaosParser(object):
 
 
     def parse_results_dir(self):
-        """For each results directory created by test_that, parse it and
-           create summary files.
+        """Parses each result directory.
+
+        For each results directory created by test_that, parse it and
+        create summary files.
 
         """
+        if os.path.exists(RESULTS_DIR):
+            shutil.rmtree(RESULTS_DIR)
+        test_processed = False
         for results_dir in os.listdir(self.test_results_dir):
             if 'results' in results_dir:
                 path = os.path.join(self.test_results_dir, results_dir)
-                test_info = results_dir.split('.')
-                self.tests.append(test_info[1])
+                test = results_dir.split('.')[1]
                 status_key_dict = self.traverse_results_dir(path)
-                self.status_log.append(status_key_dict['status_file'])
-                self.keyval = status_key_dict['keyval_file']
-        if not self.tests:
+                status_log_path = status_key_dict['status_file']
+                lsb_info = self.parse_keyval(status_key_dict['keyval_file'])
+                if test is not None:
+                    self.parse_status_log(lsb_info['board'],
+                                          lsb_info['version'],
+                                          test,
+                                          status_log_path)
+                    test_processed = True
+        if not test_processed:
             raise RuntimeError('chaos_parse: Did not find any results directory'
                                'to process')
-        if os.path.exists(RESULTS_DIR):
-            shutil.rmtree(RESULTS_DIR)
-        self.parse_status_log()
 
 
 def main():
