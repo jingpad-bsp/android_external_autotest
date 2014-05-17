114879bb8c93b604c9c9c200f90f7baa152b6d35
Yusuf Mohsinally <mohsinally@google.com>
2014-05-16 18:18:57 -0700

    Converting firmware_DevBootUSB to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against peppy device.
    
    Change-Id: I681181790eeea940ad9cdbe8caac5afd2615cf46
    Reviewed-on: https://chromium-review.googlesource.com/200346
    Reviewed-by: Wai-Hong Tam <waihong@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_DevBootUSB/firmware_DevBootUSB.py
+++ server/site_tests/firmware_DevBootUSB/firmware_DevBootUSB.py
@@ -2,13 +2,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, time
+import logging
+import time
 
 from autotest_lib.client.common_lib import error
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_DevBootUSB(FAFTSequence):
+class firmware_DevBootUSB(FirmwareTest):
     """
     Servo based Ctrl-U developer USB boot test.
 
@@ -20,7 +21,6 @@ class firmware_DevBootUSB(FAFTSequence):
     """
     version = 1
 
-
     def initialize(self, host, cmdline_args, ec_wp=None):
         super(firmware_DevBootUSB, self).initialize(host, cmdline_args,
                                                     ec_wp=ec_wp)
@@ -31,11 +31,9 @@ class firmware_DevBootUSB(FAFTSequence):
         logging.info('Original dev_boot_usb value: %s',
                      str(self.original_dev_boot_usb))
 
-
     def cleanup(self):
         self.ensure_internal_device_boot()
         super(firmware_DevBootUSB, self).cleanup()
-
 
     def try_ctrl_u_and_ctrl_d(self):
         """Try to press Ctrl-U first and then press Ctrl-D"""
@@ -45,7 +43,6 @@ class firmware_DevBootUSB(FAFTSequence):
         time.sleep(self.faft_config.beep)
         self.press_ctrl_d()
 
-
     def ensure_internal_device_boot(self):
         """Ensure internal device boot; if not, reboot into it.
 
@@ -54,46 +51,43 @@ class firmware_DevBootUSB(FAFTSequence):
         """
         if self.faft_client.system.is_removable_device_boot():
             logging.info('Reboot into internal disk...')
-            self.run_faft_step({
-                'userspace_action': (self.faft_client.system.set_dev_boot_usb,
-                                     self.original_dev_boot_usb),
-                'firmware_action': self.wait_fw_screen_and_ctrl_d,
-            })
-
+            self.faft_client.system.set_dev_boot_usb(self.original_dev_boot_usb)
+            self.reboot_warm(wait_for_dut_up=False)
+            self.wait_fw_screen_and_ctrl_d()
+            self.wait_for_client()
 
     def run_once(self):
         if (self.faft_config.has_keyboard and
                 not self.check_ec_capability(['keyboard'])):
             raise error.TestNAError("TEST IT MANUALLY! This test can't be "
-                    "automated on non-Chrome-EC devices.")
+                                    "automated on non-Chrome-EC devices.")
 
-        self.register_faft_sequence((
-            {   # Step 1, expected developer mode, set dev_boot_usb to 0
-                'state_checker': (self.checkers.dev_boot_usb_checker, False),
-                'userspace_action': (self.faft_client.system.set_dev_boot_usb,
-                                     0),
-                # Ctrl-U doesn't take effect as dev_boot_usb=0.
-                # Falls back to Ctrl-D internal disk boot.
-                'firmware_action': self.try_ctrl_u_and_ctrl_d,
-                'install_deps_after_boot': True,
-            },
-            {   # Step 2, expected internal disk boot, set dev_boot_usb to 1
-                'state_checker': (self.checkers.dev_boot_usb_checker, False,
-                        "Not internal disk boot, dev_boot_usb misbehaved"),
-                'userspace_action': (self.faft_client.system.set_dev_boot_usb,
-                                     1),
-                'firmware_action': self.wait_fw_screen_and_ctrl_u,
-                'install_deps_after_boot': True,
-            },
-            {   # Step 3, expected USB boot, set dev_boot_usb to the original
-                'state_checker': (self.checkers.dev_boot_usb_checker, True,
-                        "Not USB boot, Ctrl-U not work"),
-                'userspace_action': (self.faft_client.system.set_dev_boot_usb,
-                                     self.original_dev_boot_usb),
-                'firmware_action': self.wait_fw_screen_and_ctrl_d,
-            },
-            {   # Step 4, done
-                'state_checker': (self.checkers.dev_boot_usb_checker, False),
-            }
-        ))
-        self.run_faft_sequence()
+        logging.info("Expected developer mode, set dev_boot_usb to 0.")
+        self.check_state((self.checkers.dev_boot_usb_checker, False))
+        self.faft_client.system.set_dev_boot_usb(0)
+        # Ctrl-U doesn't take effect as dev_boot_usb=0.
+        # Falls back to Ctrl-D internal disk boot.
+        self.reboot_warm(wait_for_dut_up=False)
+        self.try_ctrl_u_and_ctrl_d()
+        self.wait_for_client(install_deps=True)
+
+        logging.info("Expected internal disk boot, set dev_boot_usb to 1.")
+        self.check_state((self.checkers.dev_boot_usb_checker,
+                          False,
+                          "Not internal disk boot, dev_boot_usb misbehaved"))
+        self.faft_client.system.set_dev_boot_usb(1)
+        self.reboot_warm(wait_for_dut_up=False)
+        self.wait_fw_screen_and_ctrl_u()
+        self.wait_for_client(install_deps=True)
+
+        logging.info("Expected USB boot, set dev_boot_usb to the original.")
+        self.check_state((self.checkers.dev_boot_usb_checker,
+                          True,
+                          "Not USB boot, Ctrl-U not work"))
+        self.faft_client.system.set_dev_boot_usb(self.original_dev_boot_usb)
+        self.reboot_warm(wait_for_dut_up=False)
+        self.wait_fw_screen_and_ctrl_d()
+        self.wait_for_client()
+
+        logging.info("Check and done.")
+        self.check_state((self.checkers.dev_boot_usb_checker, False))
