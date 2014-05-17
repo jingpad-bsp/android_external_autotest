56f1f0d988e66df08980f7d3e28de3741672286b
Alex Miller <milleral@chromium.org>
2014-05-16 21:21:00 -0700

    [autotest] contrib: Script to find hosts in both cautotest and CQ.
    This shouldn't happen.
    
    TEST=Ran it
    BUG=chromium:374481
    
    Change-Id: I77cc54481ee04b782f0a8702cdb94621c0def10f
    Reviewed-on: https://chromium-review.googlesource.com/200337
    Reviewed-by: Alex Miller <milleral@chromium.org>
    Commit-Queue: Alex Miller <milleral@chromium.org>
    Tested-by: Alex Miller <milleral@chromium.org>

---
+++ contrib/shared_hosts.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+"""
+Finds hosts that are shared between both cautotest and cautotest-cq.
+"""
+
+import common
+from autotest_lib.server import frontend
+
+cautotest = frontend.AFE(server='cautotest')
+cautotest_cq = frontend.AFE(server='cautotest-cq')
+
+cautotest_hosts = [x['hostname'] for x in cautotest.run('get_hosts')]
+cautotest_cq_hosts = [x['hostname'] for x in cautotest_cq.run('get_hosts')]
+
+for host in cautotest_hosts:
+    if host in cautotest_cq_hosts:
+        print host
