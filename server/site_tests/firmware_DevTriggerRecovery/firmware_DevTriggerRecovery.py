a0c9893e50e56396148081d215e56a1ab9e6e934
Yusuf Mohsinally <mohsinally@google.com>
2014-05-16 19:33:29 -0700

    Converting firmware_DevTriggerRecovery to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    2. Using error.TestNAError for case when this test is not necessary.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against peppy device.
    
    Change-Id: Ibfc68814a91fcabe76149c4d4ed9666d11903892
    Reviewed-on: https://chromium-review.googlesource.com/200328
    Reviewed-by: Wai-Hong Tam <waihong@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_DevTriggerRecovery/firmware_DevTriggerRecovery.py
+++ server/site_tests/firmware_DevTriggerRecovery/firmware_DevTriggerRecovery.py
@@ -4,11 +4,12 @@
 
 import logging
 
+from autotest_lib.client.common_lib import error
 from autotest_lib.server.cros import vboot_constants as vboot
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_DevTriggerRecovery(FAFTSequence):
+class firmware_DevTriggerRecovery(FirmwareTest):
     """
     Servo based recovery boot test triggered by pressing enter at dev screen.
 
@@ -22,7 +23,6 @@ class firmware_DevTriggerRecovery(FAFTSequence):
     # True if Alex/ZGB which needs a transition state to enter dev mode.
     need_dev_transition = False
 
-
     # The devsw off->on transition states are different based on platforms.
     # For Alex/ZGB, it is dev switch on but normal firmware boot.
     # For other platforms, it is dev switch on and developer firmware boot.
@@ -33,14 +33,13 @@ class firmware_DevTriggerRecovery(FAFTSequence):
                     'devsw_boot': '1',
                     'mainfw_act': 'A',
                     'mainfw_type': 'normal',
-                })
+                    })
         else:
             return self.checkers.crossystem_checker({
                     'devsw_boot': '1',
                     'mainfw_act': 'A',
                     'mainfw_type': 'developer',
-                })
-
+                    })
 
     # The devsw on->off transition states are different based on platforms.
     # For Alex/ZGB, it is firmware B normal boot. Firmware A is still developer.
@@ -51,71 +50,66 @@ class firmware_DevTriggerRecovery(FAFTSequence):
                     'devsw_boot': '0',
                     'mainfw_act': 'B',
                     'mainfw_type': 'normal',
-                })
+                    })
         else:
             return self.checkers.crossystem_checker({
                     'devsw_boot': '0',
                     'mainfw_act': 'A',
                     'mainfw_type': 'normal',
-                })
-
+                    })
 
     def initialize(self, host, cmdline_args):
         super(firmware_DevTriggerRecovery, self).initialize(host, cmdline_args)
         self.setup_dev_mode(dev_mode=False)
         self.setup_usbkey(usbkey=True, host=False)
 
-
     def run_once(self):
         if self.faft_config.keyboard_dev:
-            logging.info('This test is no longer valid in keyboard controlled '
-                         'dev mode firmware.')
-            return
+            raise error.TestNAError('This test is no longer valid in keyboard'
+                                    'controlled dev mode firmware.')
 
-        self.register_faft_sequence((
-            {   # Step 1, enable dev mode
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '0',
-                    'mainfw_act': 'A',
-                    'mainfw_type': 'normal',
-                }),
-                'userspace_action': self.servo.enable_development_mode,
-                'firmware_action': self.wait_fw_screen_and_ctrl_d,
-            },
-            {   # Step 2, expected values based on platforms (see above),
-                # run "chromeos-firmwareupdate --mode todev && reboot",
-                # and trigger recovery boot at dev screen
-                'state_checker': self.check_devsw_on_transition,
-                'userspace_action': (self.faft_client.system.run_shell_command,
-                    'chromeos-firmwareupdate --mode todev && reboot'),
-                # Ignore the default reboot_action here because the
-                # userspace_action (firmware updater) will reboot the system.
-                'reboot_action': None,
-                'firmware_action': (self.wait_fw_screen_and_trigger_recovery,
-                                    self.need_dev_transition),
-                'install_deps_after_boot': True,
-            },
-            {   # Step 3, expected recovery boot and disable dev switch
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'recovery',
-                    'recovery_reason' : vboot.RECOVERY_REASON['RW_DEV_SCREEN'],
-                }),
-                'userspace_action': self.servo.disable_development_mode,
-            },
-            {   # Step 4, expected values based on platforms (see above),
-                # and run "chromeos-firmwareupdate --mode tonormal && reboot"
-                'state_checker': self.check_devsw_off_transition,
-                'userspace_action': (self.faft_client.system.run_shell_command,
-                    'chromeos-firmwareupdate --mode tonormal && reboot'),
-                'reboot_action': None,
-            },
-            {   # Step 5, expected normal mode boot, done
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '0',
-                    'mainfw_act': 'A',
-                    'mainfw_type': 'normal',
-                }),
-            },
-        ))
-        self.run_faft_sequence()
+        logging.info("Enable dev mode.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '0',
+                              'mainfw_act': 'A',
+                              'mainfw_type': 'normal',
+                              }))
+        self.servo.enable_development_mode()
+        self.reboot_warm(wait_for_dut_up=False)
+        self.wait_fw_screen_and_ctrl_d()
+        self.wait_for_client()
+
+        logging.info("Expected values based on platforms (see above), "
+                     "run 'chromeos-firmwareupdate --mode todev && reboot', "
+                     "and trigger recovery boot at dev screen. ")
+        self.check_state(self.check_devsw_on_transition)
+        self.faft_client.system.run_shell_command(
+                 'chromeos-firmwareupdate --mode todev && reboot')
+        # Ignore the default reboot_action here because the
+        # userspace_action (firmware updater) will reboot the system.
+        self.wait_fw_screen_and_trigger_recovery(self.need_dev_transition)
+        self.wait_for_client(install_deps=True)
+
+        logging.info("Expected recovery boot and disable dev switch.")
+        self.check_state((self.checkers.crossystem_checker, {
+                     'devsw_boot': '1',
+                     'mainfw_type': 'recovery',
+                     'recovery_reason' : vboot.RECOVERY_REASON['RW_DEV_SCREEN'],
+                     }))
+        self.servo.disable_development_mode()
+        self.reboot_warm()
+
+        logging.info("Expected values based on platforms (see above), "
+                     "and run 'chromeos-firmwareupdate --mode tonormal && "
+                     "reboot'")
+        self.check_state(self.check_devsw_off_transition)
+        self.faft_client.system.run_shell_command(
+                            'chromeos-firmwareupdate --mode tonormal && reboot')
+        self.wait_for_client()
+
+        logging.info("Expected normal mode boot, done.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '0',
+                              'mainfw_act': 'A',
+                              'mainfw_type': 'normal',
+                              }))
