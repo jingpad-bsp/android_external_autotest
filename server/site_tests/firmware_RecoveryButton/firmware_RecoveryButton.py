d856499b291aae632bbfb2695cdc79528fc3d6b5
Yusuf Mohsinally <mohsinally@google.com>
2014-05-18 18:52:03 -0700

    Converting firmware_RecoveryButton to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against peppy device.
    
    Change-Id: I7d4eba40108b018b5e4275213b5b5e8721180c4e
    Reviewed-on: https://chromium-review.googlesource.com/200405
    Reviewed-by: Wai-Hong Tam <waihong@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_RecoveryButton/firmware_RecoveryButton.py
+++ server/site_tests/firmware_RecoveryButton/firmware_RecoveryButton.py
@@ -2,11 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
+
 from autotest_lib.server.cros import vboot_constants as vboot
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_RecoveryButton(FAFTSequence):
+class firmware_RecoveryButton(FirmwareTest):
     """
     Servo based recovery button test.
 
@@ -17,7 +19,6 @@ class firmware_RecoveryButton(FAFTSequence):
     """
     version = 1
 
-
     def ensure_normal_boot(self):
         """Ensure normal mode boot this time.
 
@@ -26,10 +27,8 @@ class firmware_RecoveryButton(FAFTSequence):
         """
         if not self.checkers.crossystem_checker(
                 {'mainfw_type': ('normal', 'developer')}):
-            self.run_faft_step({
-                'userspace_action': self.servo.disable_recovery_mode,
-            })
-
+            self.servo.disable_recovery_mode()
+            self.reboot_warm()
 
     def initialize(self, host, cmdline_args, dev_mode=False, ec_wp=None):
         super(firmware_RecoveryButton, self).initialize(host, cmdline_args,
@@ -37,46 +36,36 @@ class firmware_RecoveryButton(FAFTSequence):
         self.setup_dev_mode(dev_mode)
         self.setup_usbkey(usbkey=True, host=False)
 
-
     def cleanup(self):
         self.ensure_normal_boot()
         super(firmware_RecoveryButton, self).cleanup()
 
-
     def run_once(self, dev_mode=False):
-        # The old models need users to remove and insert USB stick during boot.
-        remove_usb = (self.faft_client.system.get_platform_name() in
-                      ('Mario', 'Alex', 'ZGB', 'Aebl', 'Kaen'))
-        self.register_faft_sequence((
-            {   # Step 1, switch to recovery mode and reboot
-                'state_checker': (self.checkers.crossystem_checker, {
+        logging.info("Switch to recovery mode and reboot.")
+        self.check_state((self.checkers.crossystem_checker, {
                     'mainfw_type': 'developer' if dev_mode else 'normal',
-                }),
-                'userspace_action': self.enable_rec_mode_and_reboot,
-                'reboot_action': None,
-                # In the keyboard controlled recovery mode design, it doesn't
-                # require users to remove and insert the USB.
-                #
-                # In the old design, it checks:
-                #   if dev_mode ON, directly boot to USB stick if presented;
-                #   if dev_mode OFF,
-                #     the old models need users to remove and insert the USB;
-                #     the new models directly boot to the USB.
-                'firmware_action': None if self.faft_config.keyboard_dev or
-                                   dev_mode or not remove_usb else
-                                   self.wait_fw_screen_and_plug_usb,
-                'install_deps_after_boot': True,
-            },
-            {   # Step 2, expected recovery boot and reboot
-                'state_checker': (self.checkers.crossystem_checker, {
+                    }))
+        self.enable_rec_mode_and_reboot()
+        # In the keyboard controlled recovery mode design, it doesn't
+        # require users to remove and insert the USB.
+        #
+        # In the old design, it checks:
+        #   if dev_mode ON, directly boot to USB stick if presented;
+        #   if dev_mode OFF,
+        #     the old models need users to remove and insert the USB;
+        #     the new models directly boot to the USB.
+        if not (self.faft_config.keyboard_dev or dev_mode):
+            self.wait_fw_screen_and_plug_usb()
+        self.wait_for_client(install_deps=True)
+
+        logging.info("Expected recovery boot and reboot.")
+        self.check_state((self.checkers.crossystem_checker, {
                     'mainfw_type': 'recovery',
-                    'recovery_reason' : vboot.RECOVERY_REASON['RO_MANUAL'],
-                }),
-            },
-            {   # Step 3, expected normal boot
-                'state_checker': (self.checkers.crossystem_checker, {
+                    'recovery_reason': vboot.RECOVERY_REASON['RO_MANUAL'],
+                    }))
+        self.reboot_warm()
+
+        logging.info("Expected normal boot.")
+        self.check_state((self.checkers.crossystem_checker, {
                     'mainfw_type': 'developer' if dev_mode else 'normal',
-                }),
-            },
-        ))
-        self.run_faft_sequence()
+                    }))
