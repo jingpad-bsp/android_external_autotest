74d98c72d49568d84c03b6aa60d910733b7ce46a
Yusuf Mohsinally <mohsinally@google.com>
2014-05-18 19:05:19 -0700

    Converting firmware_DevMode to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against peppy device.
    
    Change-Id: I2bd30240a4bfc30fb5dce72de560261487b58b20
    Reviewed-on: https://chromium-review.googlesource.com/200396
    Reviewed-by: Wai-Hong Tam <waihong@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_DevMode/firmware_DevMode.py
+++ server/site_tests/firmware_DevMode/firmware_DevMode.py
@@ -2,15 +2,16 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+import logging
+
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_DevMode(FAFTSequence):
+class firmware_DevMode(FirmwareTest):
     """
     Servo based developer firmware boot test.
     """
     version = 1
-
 
     def initialize(self, host, cmdline_args, ec_wp=None):
         super(firmware_DevMode, self).initialize(host, cmdline_args,
@@ -18,36 +19,30 @@ class firmware_DevMode(FAFTSequence):
         self.setup_dev_mode(dev_mode=False)
         self.setup_usbkey(usbkey=False)
 
-
     def cleanup(self):
         self.setup_dev_mode(dev_mode=False)
         super(firmware_DevMode, self).cleanup()
 
-
     def run_once(self):
-        self.register_faft_sequence((
-            {   # Step 1, enable dev mode
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '0',
-                    'mainfw_type': 'normal',
-                }),
-                'userspace_action': self.enable_dev_mode_and_reboot,
-                'reboot_action': None,
-                'firmware_action': self.wait_dev_screen_and_ctrl_d,
-            },
-            {   # Step 2, expected developer mode boot and enable normal mode
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'userspace_action': self.enable_normal_mode_and_reboot,
-                'reboot_action': None,
-            },
-            {   # Step 3, expected normal mode boot, done
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '0',
-                    'mainfw_type': 'normal',
-                }),
-            }
-        ))
-        self.run_faft_sequence()
+        logging.info("Enable dev mode.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '0',
+                              'mainfw_type': 'normal',
+                              }))
+        self.enable_dev_mode_and_reboot()
+        self.wait_dev_screen_and_ctrl_d()
+        self.wait_for_client()
+
+        logging.info("Expected developer mode boot and enable normal mode.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '1',
+                              'mainfw_type': 'developer',
+                              }))
+        self.enable_normal_mode_and_reboot()
+        self.wait_for_client()
+
+        logging.info("Expected normal mode boot, done.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '0',
+                              'mainfw_type': 'normal',
+                              }))
