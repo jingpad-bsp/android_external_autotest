8411520d741c8c9a33de1a8518de558c8c513ae7
Yusuf Mohsinally <mohsinally@google.com>
2014-05-21 10:44:20 -0700

    Converting firmware_CgptStress to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against peppy device.
    
    Change-Id: I4be6abd69034885e0e2188ac8639cc77af74769b
    Reviewed-on: https://chromium-review.googlesource.com/200763
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_CgptStress/firmware_CgptStress.py
+++ server/site_tests/firmware_CgptStress/firmware_CgptStress.py
@@ -5,16 +5,15 @@
 import logging
 
 from autotest_lib.client.common_lib import utils
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_CgptStress(FAFTSequence):
+class firmware_CgptStress(FirmwareTest):
     """
     Servo based, iterative cgpt test. One iteration of test modifies CGPT to
     switch to boot kernel B and then switch back to kernel A again.
     """
     version = 1
-
 
     def initialize(self, host, cmdline_args, dev_mode):
         # Parse arguments from command line
@@ -26,29 +25,23 @@ class firmware_CgptStress(FAFTSequence):
         self.setup_usbkey(usbkey=False)
         self.setup_kernel('a')
 
-
     def cleanup(self):
         self.restore_cgpt_attributes()
         super(firmware_CgptStress, self).cleanup()
 
-
     def run_once(self):
-        self.register_faft_sequence((
-            {   # Step 1, expected kernel A boot and prioritize kernel B
-                'state_checker': (self.checkers.root_part_checker, 'a'),
-                'userspace_action': (self.reset_and_prioritize_kernel, 'b'),
-                'reboot_action': self.warm_reboot,
-            },
-            {   # Step 2, expected kernel B boot and prioritize kernel A
-                'state_checker': (self.checkers.root_part_checker, 'b'),
-                'userspace_action': (self.reset_and_prioritize_kernel, 'a'),
-                'reboot_action': self.warm_reboot,
-            },
-            {   # Step 3, expected kernel A boot, done
-                'state_checker': (self.checkers.root_part_checker, 'a'),
-            },
-        ))
         for i in xrange(self.faft_iterations):
             logging.info('======== Running FAFT ITERATION %d/%s ========',
-                         i+1, self.faft_iterations)
-            self.run_faft_sequence()
+                         i + 1, self.faft_iterations)
+            logging.info("Expected kernel A boot and prioritize kernel B.")
+            self.check_state((self.checkers.root_part_checker, 'a'))
+            self.reset_and_prioritize_kernel('b')
+            self.reboot_warm()
+
+            logging.info("Expected kernel B boot and prioritize kernel A.")
+            self.check_state((self.checkers.root_part_checker, 'b'))
+            self.reset_and_prioritize_kernel('a')
+            self.reboot_warm()
+
+            logging.info("Expected kernel A boot, done.")
+            self.check_state((self.checkers.root_part_checker, 'a'))
