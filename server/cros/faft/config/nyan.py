a8a2a555f8cb17c261aebf8a5e3135b19128a7e2
David Hendricks <dhendrix@chromium.org>
2014-05-23 13:52:06 -0700

    nyan: Fix syntax of ec_capability
    There shouldn't be perens there, and whatever it's doing is causing
    autotest to not process the following line.
    
    BUG=chrome-os-partner:24835
    BRANCH=nyan
    TEST=ec_boot_to_console on Nyan gets properly overridden to 0.3
    
    Change-Id: I2b950a51b57cc69d29360989a73bc4a9d293954a
    Reviewed-on: https://chromium-review.googlesource.com/201341
    Reviewed-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Commit-Queue: David Hendricks <dhendrix@chromium.org>
    Tested-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-by: shikha shrivastava <shikhas@nvidia.com>

--- server/cros/faft/config/nyan.py
+++ server/cros/faft/config/nyan.py
@@ -9,5 +9,5 @@ class Values(object):
     """FAFT config values for Nyan."""
     software_sync_update = 6
     chrome_ec = True
-    ec_capability = (['battery', 'charging', 'keyboard', 'arm', 'lid'])
+    ec_capability = ['battery', 'charging', 'keyboard', 'arm', 'lid']
     ec_boot_to_console = 0.3
