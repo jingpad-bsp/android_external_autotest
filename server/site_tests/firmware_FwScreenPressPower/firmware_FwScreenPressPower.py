089075852257fe42bc4f6dc7d890d0e46787fcbc
Yusuf Mohsinally <mohsinally@google.com>
2014-05-22 18:16:59 -0700

    Converting firmware_FwScreenPressPower to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    
    1. Converted the test to use the new flat function call style.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against peppy device.
    
    Change-Id: I4f70dd978d1d29c5b1abc5c564edb73486ee0267
    Reviewed-on: https://chromium-review.googlesource.com/201200
    Reviewed-by: Wai-Hong Tam <waihong@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_FwScreenPressPower/firmware_FwScreenPressPower.py
+++ server/site_tests/firmware_FwScreenPressPower/firmware_FwScreenPressPower.py
@@ -2,12 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 import time
 
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_FwScreenPressPower(FAFTSequence):
+class firmware_FwScreenPressPower(FirmwareTest):
     """
     Servo based power button triggered shutdown test during firmware screens.
 
@@ -20,12 +21,10 @@ class firmware_FwScreenPressPower(FAFTSequence):
 
     SHORT_SHUTDOWN_CONFIRMATION_PERIOD = 0.1
 
-
     def wait_second_screen_and_press_power(self):
         """Wait and trigger a second screen and press power button."""
         self.wait_fw_screen_and_trigger_recovery()
         self.wait_longer_fw_screen_and_press_power()
-
 
     def wait_yuck_screen_and_press_power(self):
         """Insert corrupted USB for yuck screen and press power button."""
@@ -33,7 +32,6 @@ class firmware_FwScreenPressPower(FAFTSequence):
         self.servo.switch_usbkey('dut')
         time.sleep(self.faft_config.load_usb)
         self.wait_longer_fw_screen_and_press_power()
-
 
     def initialize(self, host, cmdline_args):
         super(firmware_FwScreenPressPower, self).initialize(host, cmdline_args)
@@ -45,7 +43,6 @@ class firmware_FwScreenPressPower(FAFTSequence):
         # yuck screen later.
         self.corrupt_usb_kernel(usb_dev)
 
-
     def cleanup(self):
         self.servo.switch_usbkey('host')
         usb_dev = self.servo.probe_host_usb_dev()
@@ -53,93 +50,90 @@ class firmware_FwScreenPressPower(FAFTSequence):
         self.restore_usb_kernel(usb_dev)
         super(firmware_FwScreenPressPower, self).cleanup()
 
-
-
     def run_once(self):
-        self.register_faft_sequence((
-            {   # Step 1, expected dev mode and reboot.
-                # When the next DEVELOPER SCREEN shown, press power button
-                # to make DUT shutdown.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'firmware_action': (self.run_shutdown_process,
-                                    (self.wait_fw_screen_and_press_power,
-                                     None,
-                                     self.wait_fw_screen_and_ctrl_d)),
-            },
-            {   # Step 2, reboot. When the developer screen shown, press
-                # enter key to trigger either TO_NORM screen (new) or
-                # RECOVERY INSERT screen (old). Then press power button to
-                # make DUT shutdown.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'firmware_action': (self.run_shutdown_process,
-                                    (self.wait_second_screen_and_press_power,
-                                     None,
-                                     self.wait_fw_screen_and_ctrl_d,
-                                     self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)),
-            },
-            {   # Step 3, request recovery boot. When the RECOVERY INSERT
-                # screen shows, press power button to make DUT shutdown.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'userspace_action':
-                    (self.faft_client.system.request_recovery_boot),
-                'firmware_action': (self.run_shutdown_process,
-                                    (self.wait_longer_fw_screen_and_press_power,
-                                     None,
-                                     self.wait_fw_screen_and_ctrl_d,
-                                     self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)),
-            },
-            {   # Step 4, request recovery boot again. When the recovery
-                # insert screen shows, insert a corrupted USB and trigger
-                # a YUCK SCREEN. Then press power button to make DUT shutdown.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'userspace_action':
-                    (self.faft_client.system.request_recovery_boot),
-                'firmware_action': (self.run_shutdown_process,
-                                    (self.wait_yuck_screen_and_press_power,
-                                     None,
-                                     self.wait_fw_screen_and_ctrl_d,
-                                     self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)),
-            },
-            {   # Step 5, switch back to normal mode.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '1',
-                    'mainfw_type': 'developer',
-                }),
-                'userspace_action': self.enable_normal_mode_and_reboot,
-                'reboot_action': None,
-            },
-            {   # Step 6, expected normal mode and request recovery boot.
-                # Because an USB stick is inserted, a RECOVERY REMOVE screen
-                # shows. Press power button to make DUT shutdown.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '0',
-                    'mainfw_type': 'normal',
-                }),
-                'userspace_action':
-                    (self.faft_client.system.request_recovery_boot),
-                'firmware_action': (self.run_shutdown_process,
-                                    (self.wait_longer_fw_screen_and_press_power,
-                                     None,
-                                     None,
-                                     self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)),
-            },
-            {   # Step 7, done.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'devsw_boot': '0',
-                    'mainfw_type': 'normal',
-                }),
-            },
-        ))
-        self.run_faft_sequence()
+        logging.info("Expected dev mode and reboot. "
+                     "When the next DEVELOPER SCREEN shown, press power button "
+                     "to make DUT shutdown.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '1',
+                              'mainfw_type': 'developer',
+                              }))
+        self.reboot_warm(wait_for_dut_up=False)
+        self.run_shutdown_process(self.wait_fw_screen_and_press_power,
+                                  None,
+                                  self.wait_fw_screen_and_ctrl_d)
+        self.wait_for_client()
+
+        logging.info("Reboot. When the developer screen shown, press "
+                     "enter key to trigger either TO_NORM screen (new) or "
+                     "RECOVERY INSERT screen (old). Then press power button to "
+                     "make DUT shutdown.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '1',
+                              'mainfw_type': 'developer',
+                              }))
+        self.reboot_warm(wait_for_dut_up=False)
+        self.run_shutdown_process(self.wait_second_screen_and_press_power,
+                                  None,
+                                  self.wait_fw_screen_and_ctrl_d,
+                                  self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)
+        self.wait_for_client()
+
+        logging.info("Request recovery boot. When the RECOVERY INSERT "
+                     "screen shows, press power button to make DUT shutdown.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '1',
+                              'mainfw_type': 'developer',
+                              }))
+        self.faft_client.system.request_recovery_boot()
+        self.reboot_warm(wait_for_dut_up=False)
+        self.run_shutdown_process(self.wait_longer_fw_screen_and_press_power,
+                                  None,
+                                  self.wait_fw_screen_and_ctrl_d,
+                                  self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)
+        self.wait_for_client()
+
+        logging.info("Request recovery boot again. When the recovery "
+                     "insert screen shows, insert a corrupted USB and trigger "
+                     "a YUCK SCREEN. Then press power button to "
+                     "make DUT shutdown.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '1',
+                              'mainfw_type': 'developer',
+                              }))
+        self.faft_client.system.request_recovery_boot()
+        self.reboot_warm(wait_for_dut_up=False)
+        self.run_shutdown_process(self.wait_yuck_screen_and_press_power,
+                                  None,
+                                  self.wait_fw_screen_and_ctrl_d,
+                                  self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)
+        self.wait_for_client()
+
+        logging.info("Switch back to normal mode.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '1',
+                              'mainfw_type': 'developer',
+                              }))
+        self.enable_normal_mode_and_reboot()
+        self.wait_for_client()
+
+        logging.info("Expected normal mode and request recovery boot. "
+                     "Because an USB stick is inserted, a RECOVERY REMOVE "
+                     "screen shows. Press power button to make DUT shutdown.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '0',
+                              'mainfw_type': 'normal',
+                              }))
+        self.faft_client.system.request_recovery_boot()
+        self.reboot_warm(wait_for_dut_up=False)
+        self.run_shutdown_process(self.wait_longer_fw_screen_and_press_power,
+                                  None,
+                                  None,
+                                  self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)
+        self.wait_for_client()
+
+        logging.info("Check and done.")
+        self.check_state((self.checkers.crossystem_checker, {
+                              'devsw_boot': '0',
+                              'mainfw_type': 'normal',
+                              }))
