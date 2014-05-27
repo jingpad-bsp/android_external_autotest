c9df250a60a7456d483f23a124dd0122bd092bdd
Chris Masone <cmasone@chromium.org>
2014-05-27 13:54:49 -0700

    Switch test references from 'proxystate' to 'tlsdate' user.
    The proxystate user is deprecated. Switch to the tlsdate user. Also, delete
    several now-defunct users from the expectations dict in
    security_StatefulPermissions.
    
    BUG=chromium:376526
    TEST=platform_TLSDate, security_StatefulPermissions, security_SandboxedServices
    CQ-DEPEND=CL:201467
    
    Change-Id: Ibffe4404afb0c89815fe82d48d1a8bcf09cbc241
    Reviewed-on: https://chromium-review.googlesource.com/201466
    Commit-Queue: Chris Masone <cmasone@chromium.org>
    Tested-by: Chris Masone <cmasone@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- client/site_tests/platform_TLSDate/platform_TLSDate.py
+++ client/site_tests/platform_TLSDate/platform_TLSDate.py
@@ -2,12 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging
-import os
-import re
-import shutil
-import subprocess
-import tempfile
+import os, pwd, subprocess, tempfile
 
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
@@ -25,12 +20,13 @@ class TLSDate:
         self._subprog = '?'
         self._test_obj = test_obj
         self._output = None
-        self._tlsdate_uid = 227  # proxystate
+        self._tlsdate_uid = pwd.getpwnam('tlsdate').pw_uid
         os.mkdir(self._cachedir)
-        # Let the tlsdate user (proxystate) write.
+        # Let the tlsdate user (tlsdate) write.
         os.chown(self._testdir, self._tlsdate_uid, -1)
         # Allow support shell library to be sourced.
         os.chown(self._test_obj.srcdir + '/time.sh', self._tlsdate_uid, -1)
+
 
     def start(self, subprog):
         print 'running with %s' % self._test_obj.srcdir + '/' + subprog
@@ -47,17 +43,21 @@ class TLSDate:
         self._proc = subprocess.Popen(args, stdin=subprocess.PIPE,
                                       stderr=subprocess.PIPE)
 
+
     def route_up(self):
         self._proc.stdin.write('n')
         self._proc.stdin.flush()
 
+
     def kill(self):
         self._proc.terminate()
+
 
     def output(self):
         if not self._output:
             self._output = self._proc.communicate()[1].split('\n')
         return self._output
+
 
     def in_output(self, string):
         for x in self.output():
@@ -65,12 +65,15 @@ class TLSDate:
                 return True
         return False
 
+
     def subproc_output(self):
         with open(self._outfile) as f:
             return [x.rstrip() for x in f.readlines()]
 
+
     def ok(self):
         return 'ok' in self.subproc_output()
+
 
 class platform_TLSDate(test.test):
     version = 1
@@ -80,15 +83,18 @@ class platform_TLSDate(test.test):
             raise error.TestFail('Expected success, got:' +
                                  ';'.join(t.subproc_output()))
 
+
     def require_output(self, t, string):
         if not t.in_output(string):
             raise error.TestFail('Needed "%s" but got "%s"' % (string,
                                  ';'.join(t.output())))
 
+
     def require_not_output(self, t, string):
         if t.in_output(string):
             raise error.TestFail('Needed no "%s" but got "%s"' % (string,
                                  ';'.join(t.output())))
+
 
     def test_delay_subproc(self):
         """
@@ -101,6 +107,7 @@ class platform_TLSDate(test.test):
         self.require_output(t, 'time set from the network')
         self.require_ok(t)
 
+
     def test_hang_subproc(self):
         """
         Tests that a subprocess that delays for too long is considered hung and
@@ -111,6 +118,7 @@ class platform_TLSDate(test.test):
         self.require_output(t, 'attempt 1 backoff')
         self.require_output(t, 'tlsdate timed out')
         self.require_ok(t)
+
 
     def test_fail_routes(self):
         """
@@ -124,6 +132,7 @@ class platform_TLSDate(test.test):
         self.require_output(t, 'stdin')
         self.require_output(t, 'time set from the network')
         self.require_ok(t)
+
 
     def run_once(self):
         self.test_delay_subproc()
