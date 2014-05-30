fc7fe980e218fa603c0a05477189a2b3e1cf8531
Luigi Semenzato <semenzato@chromium.org>
2014-05-30 14:31:26 -0700

    kernel_TPMStress: stop/restart services that need the TPM
    When tcsd is stopped, chapsd and cryptohomed may time out and fail,
    so we stop and restart those too.
    
    BUG=chromium:197368
    TEST=run kernel_TPMStress autotest
    
    Change-Id: Id531492881cb5cfa88dc30c32ba1a347a89cb53c
    Reviewed-on: https://chromium-review.googlesource.com/202307
    Reviewed-by: Alex Miller <milleral@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>

--- client/site_tests/kernel_TPMStress/kernel_TPMStress.py
+++ client/site_tests/kernel_TPMStress/kernel_TPMStress.py
@@ -2,31 +2,32 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
+from autotest_lib.client.cros import service_stopper
 
 class kernel_TPMStress(test.test):
-  version = 1
+    "TPM communication stress test"
+    version = 1
 
-  def run_once(self):
+    def run_once(self):
 
-    # On a Mario, running the test with 1000 iterations takes 89 seconds, and
-    # with 2000 iterations 163 seconds, i.e. the incremental time for 1
-    # iteration is 74ms.
+        # On a Mario, running the test with 1000 iterations takes 89 seconds,
+        # and with 2000 iterations 163 seconds, i.e. the incremental time for 1
+        # iteration is 74ms.
 
-    n_iterations = 3000
-    iteration = 0
+        n_iterations = 3000
+        iteration = 0
 
-    try:
-      utils.system("stop tcsd")
-
-      for iteration in range(1, n_iterations + 1):
-        utils.system("tpmc getpf")
-
-    except:
-      raise error.TestError("TPM communication error at iteration %d of %d" %
-                            (iteration, n_iterations))
-
-    finally:
-      utils.system("start tcsd")
+        try:
+            stopper = service_stopper.ServiceStopper(['cryptohomed',
+                                                      'chapsd', 'tcsd'])
+            stopper.stop_services()
+            for iteration in range(1, n_iterations + 1):
+              utils.system("tpmc getpf")
+        except:
+            raise error.TestError(("TPM communication error at " +
+                                   "iteration %d of %d") %
+                                  (iteration, n_iterations))
+        finally:
+            stopper.restore_services()
