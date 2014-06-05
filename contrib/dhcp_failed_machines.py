3b102f1a13ac993f45c0b8a729e318f8a8e31d6e
Prashanth B <beeps@google.com>
2014-06-04 18:37:50 -0700

    [autotest] Modify dhcp diagnosis script to detect dhcpd restarts.
    TEST=Ran the script.
    BUG=None
    
    Change-Id: If3948b96992c9ce529824aea27ba33d23c82a0a1
    Reviewed-on: https://chromium-review.googlesource.com/202639
    Reviewed-by: Prashanth B <beeps@chromium.org>
    Commit-Queue: Prashanth B <beeps@chromium.org>
    Tested-by: Prashanth B <beeps@chromium.org>

--- contrib/dhcp_failed_machines.py
+++ contrib/dhcp_failed_machines.py
@@ -35,9 +35,12 @@ with open('/etc/dhcp/dhcpd.conf', 'r') as f:
 
 offers = {}
 offenders = set()
+restarts = []
 
 rgx = re.compile(
   r'(?P<command>[A-Z]+) (?:from|on|for) (?P<host>\d+.\d+.\d+.\d+)')
+server_restart_str = 'Internet Systems Consortium'
+
 
 def open_file(f):
   if f.endswith('.gz'):
@@ -47,6 +50,9 @@ def open_file(f):
 
 with open_file(sys.argv[1]) as f:
   for line in f:
+    if server_restart_str in line:
+        restarts.append(line)
+        continue
     m = rgx.search(line)
     if m:
       command = m.group('command')
@@ -57,6 +63,9 @@ with open_file(sys.argv[1]) as f:
           offenders.add(host)
       if command == 'DHCPREQUEST':
         offers[host] = 0
+
+if restarts:
+    print 'DHCP restarts:\n %s' % ''.join(restarts)
 
 def lookup(h):
   return lookups.get(h, h)
