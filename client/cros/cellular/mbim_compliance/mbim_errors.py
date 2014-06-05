261cec5d1200a054d1348aa79a8861bf077ae93d
Prathmesh Prabhu <pprabhu@chromium.org>
2014-06-05 11:21:56 -0700

    cellular: Map assertion ids to descriptions in MBIMComplianceAssertionError.
    This CL streamlines the way assertion errors are raised in the MBIM compliance
    suite by having the class accept an assertion id, and mapping it internally to
    the actual assertion error string.
    
    Additionally, a subclass is provided to raise assertion errors that don't fit in
    with the MBIM compliance doc list of assertions.
    
    BUG=chromium:376958
    TEST=python mbim_errors_unittest.py
    
    Change-Id: I68c2d1262aa4367c0d6f9d981c3a4bb74e91aaee
    Reviewed-on: https://chromium-review.googlesource.com/202718
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Prathmesh Prabhu <pprabhu@chromium.org>
    Tested-by: Prathmesh Prabhu <pprabhu@chromium.org>

--- client/cros/cellular/mbim_compliance/mbim_errors.py
+++ client/cros/cellular/mbim_compliance/mbim_errors.py
@@ -37,22 +37,88 @@ class MBIMComplianceSequenceError(MBIMComplianceError):
 
 class MBIMComplianceAssertionError(MBIMComplianceError):
     """ Errors raised by compliance suite assertions. """
-    pass
+
+    MBIM_ASSERTIONS = {
+            # This key should not be used directly.
+            # Raise |MBIMComplianceGenericAssertionError| instead.
+            'no_code': '',
+
+            'mbim1.0:3.2.1#1': 'Functions that implement both NCM 1.0 and MBIM '
+                               'shall provide two alternate settings for the '
+                               'Communication Interface.',
+            'mbim1.0:3.2.1#2': 'For alternate setting 0 of the Communication '
+                               'Interface of an NCM/MBIM function: interface, '
+                               'functional and endpoint descriptors shall be '
+                               'constructed according to the rules given in '
+                               '[USBNCM10].',
+            'mbim1.0:3.2.1#2': 'For alternate setting 1 of the Communication '
+                               'Interface of an NCM/MBIM function: interface, '
+                               'functional and endpoint descriptors shall be '
+                               'constructed according to the rules given in '
+                               '[MBIM 1.0] section 6.',
+            'mbim1.0:3.2.1#4': 'When alternate setting 0 of the Communiation'
+                               'Interface of an NCM/MBIM function is selected, '
+                               'the function shall operator according to the '
+                               'NCM rules givein in [USBNCM10].',
+            'mbim1.0:3.2.1#5': 'When alternate setting 1 of the Communiation'
+                               'Interface of an NCM/MBIM function is selected, '
+                               'the function shall operator according to the '
+                               'MBIM rules given in [MBIM1.0].',
+            'mbim1.0:3.2.2.1#1': 'If an Interface Association Descriptor is '
+                                 'used to form an NCM/MBIM function, its '
+                                 'interface class, subclass, and protocol '
+                                 'codes shall match those given in alternate '
+                                 'setting 0 of the Communication Interface. ',
+            # TODO(mcchou): Add other assertions as needed.
+    }
+
+    def __init__(self, assertion_id, error_string=None):
+        """
+        @param assertion_id: A str that must be a key in the MBIM_ASSERTIONS map
+                defined in this class.
+        @param error_string: An optional str to be appended to the error
+                description.
+
+        For example,
+            MBIMComplianceAssertionError('mbim1.0:3.2.1#1')
+            raises an error associated with assertion [MBIM 1.0]-3.2.1#1
+
+        """
+        if assertion_id not in self.MBIM_ASSERTIONS:
+            log_and_raise(MBIMComplianceFrameworkError,
+                          'Unknown assertion id "%s"' % assertion_id)
+
+        message = '[%s]: %s' % (assertion_id,
+                                self.MBIM_ASSERTIONS[assertion_id])
+        if error_string:
+            message += ': %s' % error_string
+
+        super(MBIMComplianceAssertionError, self).__init__(message)
 
 
-def log_and_raise(error_class, error_string=None):
+class MBIMComplianceGenericAssertionError(MBIMComplianceAssertionError):
+    """ Assertion errors that don't map directly to an MBIM assertion. """
+    def __init__(self, error_string):
+        """
+        @param error_string: A description of the error.
+        """
+        super(MBIMComplianceGenericAssertionError, self).__init__(
+                'no_code',
+                error_string)
+
+
+def log_and_raise(error_class, *args):
     """
     Log and raise an error.
 
     This function should be used to raise all errors.
 
     @param error_class: An Exception subclass to raise.
-    @param error_string: A string describing the error condition.
+    @param *args: Arguments to be passed to the error class constructor.
     @raises: |error_class|.
 
     """
-    error_string = error_string if error_string is not None else ''
-    error_object = error_class(error_string)
+    error_object = error_class(*args)
     logging.error(error_object)
     trace = traceback.format_stack()
     # Get rid of the current frame from trace
