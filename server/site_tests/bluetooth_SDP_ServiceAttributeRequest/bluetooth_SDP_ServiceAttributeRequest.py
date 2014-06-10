7b1048dec29a5eb2191f85e980721b73e0575311
Artem Rakhov <arakhov@chromium.org>
2014-06-09 21:14:17 -0700

    Bluetooth SDP autotest: tests for attributes of fake service
    This CL adds tests for those attributes, which are not present in any
    of existing services. In order to do this, DUT starts a fake service,
    which contains all this attributes with some fake value. After that
    tester sends requests to the DUT and ensures that responses are
    correct.
    
    BUG=chromium:329044
    TEST=test_that --board ${BOARD} ${HOSTNAME} bluetooth_SDP_ServiceAttributeRequest
    
    Change-Id: Ica66c77e9fe71d6c179b88a648bd8aa5b0315871
    Reviewed-on: https://chromium-review.googlesource.com/203190
    Tested-by: Artem Rakhov <arakhov@chromium.org>
    Reviewed-by: Scott James Remnant <keybuk@chromium.org>
    Reviewed-by: Fang Deng <fdeng@chromium.org>
    Reviewed-by: Arman Uguray <armansito@chromium.org>
    Commit-Queue: Artem Rakhov <arakhov@chromium.org>

--- server/site_tests/bluetooth_SDP_ServiceAttributeRequest/bluetooth_SDP_ServiceAttributeRequest.py
+++ server/site_tests/bluetooth_SDP_ServiceAttributeRequest/bluetooth_SDP_ServiceAttributeRequest.py
@@ -4,6 +4,8 @@
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.server.cros.bluetooth import bluetooth_test
+import uuid
+import xml.etree.ElementTree as ET
 
 class bluetooth_SDP_ServiceAttributeRequest(bluetooth_test.BluetoothTest):
     """
@@ -40,6 +42,27 @@ class bluetooth_SDP_ServiceAttributeRequest(bluetooth_test.BluetoothTest):
     AVRCP_TG_CLASS_ID                = 0x110c
     PROFILE_DESCRIPTOR_LIST_ATTR_ID  = 0x0009
     ADDITIONAL_PROTOCOLLIST_ATTR_ID  = 0x000D
+
+    FAKE_SERVICE_PATH                = '/autotest/fake_service'
+    FAKE_SERVICE_CLASS_ID            = 0xCDEF
+    FAKE_ATTRIBUTE_VALUE             = 42
+    LANGUAGE_BASE_ATTRIBUTE_ID       = 0x0006
+    FAKE_GENERAL_ATTRIBUTE_IDS       = [
+                                        0x0003, # TP/SERVER/SA/BV-04-C
+                                        0x0002, # TP/SERVER/SA/BV-06-C
+                                        0x0007, # TP/SERVER/SA/BV-07-C
+                                        0x0008, # TP/SERVER/SA/BV-10-C
+                                        # TP/SERVER/SA/BV-09-C:
+                                        LANGUAGE_BASE_ATTRIBUTE_ID
+                                       ]
+    FAKE_LANGUAGE_ATTRIBUTE_OFFSETS  = [
+                                        0x0000, # TP/SERVER/SA/BV-12-C
+                                        0x0001, # TP/SERVER/SA/BV-13-C
+                                        0x0002  # TP/SERVER/SA/BV-14-C
+                                       ]
+    NON_EXISTING_ATTRIBUTE_ID        = 0xFEDC
+    BLUETOOTH_BASE_UUID              = 0x0000000000001000800000805F9B34FB
+    SERVICE_CLASS_ID_ATTR_ID         = 0x0001
 
 
     def get_single_handle(self, class_id):
@@ -226,7 +249,7 @@ class bluetooth_SDP_ServiceAttributeRequest(bluetooth_test.BluetoothTest):
 
 
     def test_additional_protocol_descriptor_list_attribute(self):
-        """Implementation of test TP/SERVER/SA/BV-18-C from SDP Specification.
+        """Implementation of test TP/SERVER/SA/BV-21-C from SDP Specification.
 
         @return True if test passes, False if test fails
 
@@ -234,6 +257,53 @@ class bluetooth_SDP_ServiceAttributeRequest(bluetooth_test.BluetoothTest):
         protocol_list = self.get_attribute(self.AVRCP_TG_CLASS_ID,
                                            self.ADDITIONAL_PROTOCOLLIST_ATTR_ID)
         return isinstance(protocol_list, list) and protocol_list != []
+
+
+    def test_non_existing_attribute(self):
+        """Implementation of test TP/SERVER/SA/BV-20-C from SDP Specification.
+
+        @return True if test passes, False if test fails
+
+        """
+        record_handle = self.get_single_handle(self.FAKE_SERVICE_CLASS_ID)
+        if record_handle == -1:
+            return False
+
+        res = self.tester.service_attribute_request(
+                  record_handle, self.MAX_ATTR_BYTE_CNT,
+                  [self.NON_EXISTING_ATTRIBUTE_ID])
+
+        return res == []
+
+
+    def test_fake_attributes(self):
+        """Test values of attributes of the fake service record.
+
+        @return True if all tests pass, False otherwise
+
+        """
+        for attr_id in self.FAKE_GENERAL_ATTRIBUTE_IDS:
+            if not self.test_attribute(self.FAKE_SERVICE_CLASS_ID,
+                                       attr_id, self.FAKE_ATTRIBUTE_VALUE):
+                return False
+
+        for offset in self.FAKE_LANGUAGE_ATTRIBUTE_OFFSETS:
+            record_handle = self.get_single_handle(self.FAKE_SERVICE_CLASS_ID)
+            if record_handle == -1:
+                return False
+
+            lang_base = self.tester.service_attribute_request(
+                            record_handle, self.MAX_ATTR_BYTE_CNT,
+                            [self.LANGUAGE_BASE_ATTRIBUTE_ID])
+            attr_id = lang_base[1] + offset
+
+            response = self.tester.service_attribute_request(
+                           record_handle, self.MAX_ATTR_BYTE_CNT, [attr_id])
+
+            if response != [attr_id, self.FAKE_ATTRIBUTE_VALUE]:
+                return False
+
+        return True
 
 
     def correct_request(self):
@@ -255,7 +325,41 @@ class bluetooth_SDP_ServiceAttributeRequest(bluetooth_test.BluetoothTest):
                 self.test_version_list_attribute() and
                 self.test_service_database_state_attribute() and
                 self.test_profile_descriptor_list_attribute() and
-                self.test_additional_protocol_descriptor_list_attribute())
+                self.test_additional_protocol_descriptor_list_attribute() and
+                self.test_fake_attributes() and
+                self.test_non_existing_attribute())
+
+
+    def build_service_record(self):
+        """Build SDP record manually for the fake service.
+
+        @return resulting record as string
+
+        """
+        value = ET.Element('uint16', {'value': str(self.FAKE_ATTRIBUTE_VALUE)})
+
+        sdp_record = ET.Element('record')
+
+        service_id_attr = ET.Element(
+            'attribute', {'id': str(self.SERVICE_CLASS_ID_ATTR_ID)})
+        service_id_attr.append(
+            ET.Element('uuid', {'value': '0x%X' % self.FAKE_SERVICE_CLASS_ID}))
+        sdp_record.append(service_id_attr)
+
+        for attr_id in self.FAKE_GENERAL_ATTRIBUTE_IDS:
+            attr = ET.Element('attribute', {'id': str(attr_id)})
+            attr.append(value)
+            sdp_record.append(attr)
+
+        for offset in self.FAKE_LANGUAGE_ATTRIBUTE_OFFSETS:
+            attr_id = self.FAKE_ATTRIBUTE_VALUE + offset
+            attr = ET.Element('attribute', {'id': str(attr_id)})
+            attr.append(value)
+            sdp_record.append(attr)
+
+        sdp_record_str = ('<?xml version="1.0" encoding="UTF-8"?>' +
+                          ET.tostring(sdp_record))
+        return sdp_record_str
 
 
     def run_once(self):
@@ -265,6 +369,16 @@ class bluetooth_SDP_ServiceAttributeRequest(bluetooth_test.BluetoothTest):
             raise error.TestFail('DUT could not be reset to initial state')
 
         self.adapter = self.device.get_adapter_properties()
+
+        # Create a fake service record in order to test attributes,
+        # that are not present in any of existing services.
+        uuid128 = ((self.FAKE_SERVICE_CLASS_ID << 96) +
+                   self.BLUETOOTH_BASE_UUID)
+        uuid_str = str(uuid.UUID(int=uuid128))
+        sdp_record = self.build_service_record()
+        self.device.register_profile(self.FAKE_SERVICE_PATH,
+                                     uuid_str,
+                                     {"ServiceRecord": sdp_record})
 
         # Setup the tester as a generic computer.
         if not self.tester.setup('computer'):
