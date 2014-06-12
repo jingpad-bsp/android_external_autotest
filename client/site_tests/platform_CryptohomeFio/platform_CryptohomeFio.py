c61bbd9e0a5c319df3adc3ee2f446ba3c822c540
Gwendal Grignou <gwendal@chromium.org>
2014-06-12 13:39:59 -0700

    Test a given fio run on different location in the same job.
    This way, performance and results from fio are in the same result
    directory.
    
    BUG=None
    TEST=Ran platform_CryptohomeFio and platform_CryptohomeFio.stress.
    
    Change-Id: Ibf1623fd5cdeb7b800f6b6ad7f116b96528ac95a
    Reviewed-on: https://chromium-review.googlesource.com/203629
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>

--- client/site_tests/platform_CryptohomeFio/platform_CryptohomeFio.py
+++ client/site_tests/platform_CryptohomeFio/platform_CryptohomeFio.py
@@ -4,6 +4,7 @@
 
 import logging, os, shutil, tempfile
 from autotest_lib.client.bin import fio_util, test, utils
+from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import cryptohome
 
 TEST_USER = 'test@chromium.org'
@@ -14,42 +15,59 @@ class platform_CryptohomeFio(test.test):
 
     version = 2
 
-    def run_once(self, runtime, mount_cryptohome=True, tmpfs=False,
-                 script=None, sysctls=None):
-        if sysctls:
-            for sysctl in sysctls:
-                for key, val in sysctl.iteritems():
+    USE_CRYPTO = 'crypto'
+    USE_PLAIN = 'plain'
+    USE_TMPFS = 'tmpfs'
+    DISK_CONFIG_KEYS = [ USE_CRYPTO, USE_PLAIN, USE_TMPFS ]
+
+    def run_once(self, runtime, disk_configs,
+                 script=None, sysctls_list=None):
+        """
+        Create a 300MB file in tmpfs/encrypted/unencrypted location
+        and run fio tesst.
+
+        @param disk_configs: list of keys from DISK_CONFIG_KEYS.
+        @param script: fio script to run
+        @param sysctls_list: list of dictionary of sysctls to alter.
+        """
+        if not set(disk_configs).issubset(set(self.DISK_CONFIG_KEYS)):
+            raise error.TestFail('Unknown keys in disk config')
+        for config in disk_configs:
+            for sysctls in sysctls_list or [ {} ]:
+
+                graph_descr = ''
+                for key, val in sysctls.iteritems():
                     utils.sysctl(key, val)
-        # Mount a test cryptohome vault.
-        self.__mount_cryptohome = mount_cryptohome
-        if mount_cryptohome:
-            cryptohome.mount_vault(TEST_USER, TEST_PASSWORD, create=True)
-            tmpdir = cryptohome.user_path(TEST_USER)
-        else:
-            if tmpfs:
-                tmpdir = None
-            else:
-                tmpdir = self.tmpdir
-        self.__work_dir = tempfile.mkdtemp(dir=tmpdir)
+                    graph_descr += '-'.join([os.path.basename(key), str(val)])
+                # Mount a test cryptohome vault.
+                if config == self.USE_CRYPTO:
+                    cryptohome.mount_vault(TEST_USER, TEST_PASSWORD,
+                                           create=True)
+                    tmpdir = cryptohome.user_path(TEST_USER)
+                elif config == self.USE_TMPFS:
+                    tmpdir = None
+                else:
+                    tmpdir = self.tmpdir
+                self.__work_dir = tempfile.mkdtemp(dir=tmpdir)
 
-        results = {}
-        # TODO make these parameters to run_once & check target disk for space.
-        self.__filesize = '300m'
-        self.__runtime = str(runtime)
-        env_vars = ' '.join(
-            ['FILENAME=' + os.path.join(self.__work_dir, script),
-             'FILESIZE=' + self.__filesize,
-             'RUN_TIME=' + self.__runtime
-             ])
-        job_file = os.path.join(self.bindir, script)
-        results.update(fio_util.fio_runner(self, job_file, env_vars))
-        self.write_perf_keyval(results)
+                results = {}
+                # TODO make these parameters to run_once & check disk for space.
+                self.__filesize = '300m'
+                self.__runtime = str(runtime)
+                env_vars = ' '.join(
+                    ['FILENAME=' + os.path.join(self.__work_dir, script),
+                     'FILESIZE=' + self.__filesize,
+                     'RUN_TIME=' + self.__runtime
+                     ])
+                job_file = os.path.join(self.bindir, script)
+                results.update(fio_util.fio_runner(self, job_file, env_vars,
+                    name_prefix=graph_descr + config))
+                self.write_perf_keyval(results)
 
 
-    def cleanup(self):
-        logging.info('Finished with FS stress, cleaning up.')
-        if self.__mount_cryptohome:
-            cryptohome.unmount_vault(TEST_USER)
-            cryptohome.remove_vault(TEST_USER)
-        else:
-            shutil.rmtree(self.__work_dir)
+                logging.info('Finished with FS stress, cleaning up.')
+                if config == self.USE_CRYPTO:
+                    cryptohome.unmount_vault(TEST_USER)
+                    cryptohome.remove_vault(TEST_USER)
+                else:
+                    shutil.rmtree(self.__work_dir)
