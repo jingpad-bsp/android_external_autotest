7b48e54b06e0e3904251a1f656d5972e2a3db74e
Yusuf Mohsinally <mohsinally@google.com>
2014-06-27 12:22:13 -0700

    Converting firmware_UpdateECBin to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    Note that this test does not currently work, and will not work till some
    additional fixes are put into place. Doing this work to unblock removal of the
    old faft classes.
    
    1. Converted the test to use the new flat function call style.
    2. Fixed doc string in control file.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against squawks device.
    
    Change-Id: I7b182972e2b4c946632c3f704c967d33cb5004a0
    Reviewed-on: https://chromium-review.googlesource.com/205967
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_UpdateECBin/firmware_UpdateECBin.py
+++ server/site_tests/firmware_UpdateECBin/firmware_UpdateECBin.py
@@ -3,14 +3,16 @@
 # found in the LICENSE file.
 
 
-import logging, os, time
+import logging
+import os
+import time
 
 from autotest_lib.client.common_lib import error, utils
 from autotest_lib.server.cros import vboot_constants as vboot
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_UpdateECBin(FAFTSequence):
+class firmware_UpdateECBin(FirmwareTest):
     """
     This test verified the EC software sync. The EC binary is embedded in
     the BIOS image. On AP startup, AP verifies the EC by comparing the
@@ -32,8 +34,7 @@ class firmware_UpdateECBin(FAFTSequence):
     """
     version = 1
 
-
-    def initialize(self, host, cmdline_args):
+    def initialize(self, host, cmdline_args, dev_mode=False):
         # Parse arguments from command line
         dict_args = utils.args_to_dict(cmdline_args)
         if 'new_ec' not in dict_args or not os.path.isfile(dict_args['new_ec']):
@@ -58,18 +59,15 @@ class firmware_UpdateECBin(FAFTSequence):
         self.new_ec_path = os.path.join(temp_path, 'new_ec.bin')
         host.send_file(self.arg_new_ec, self.new_ec_path)
 
-
     def cleanup(self):
         self.restore_firmware()
         self.faft_client.updater.cleanup()
         super(firmware_UpdateECBin, self).cleanup()
 
-
     def do_ronormal_update(self):
         self.faft_client.bios.setup_EC_image(self.new_ec_path)
         self.new_ec_sha = self.faft_client.bios.get_EC_image_sha()
         self.faft_client.bios.update_EC_from_image('a', 0)
-
 
     def do_twostop_update(self):
         # We update the original BIOS image back. This BIOS image contains
@@ -77,7 +75,6 @@ class firmware_UpdateECBin(FAFTSequence):
         # TWOSTOP update.
         self.faft_client.bios.write_whole(self.old_bios_path)
         self.faft_client.bios.set_preamble_flags('a', 0)
-
 
     def ec_checker(self, use_new_ec):
         ro_normal_checker = self.checkers.ro_normal_checker('A', twostop=True)
@@ -88,11 +85,9 @@ class firmware_UpdateECBin(FAFTSequence):
             sha_checker = (sha_now != self.new_ec_sha)
         return (ro_normal_checker and sha_checker)
 
-
     def software_sync_and_ctrl_d(self):
         time.sleep(self.faft_config.software_sync)
         self.wait_dev_screen_and_ctrl_d()
-
 
     def run_once(self, dev_mode=False):
         if not self.check_ec_capability():
@@ -103,34 +98,31 @@ class firmware_UpdateECBin(FAFTSequence):
             logging.info('The firmware USE_RO_NORMAL flag is disabled.')
             return
 
-        self.register_faft_sequence((
-            {   # Step 1, expected EC RO boot, update EC and disable RO flag
-                'state_checker': (self.checkers.ro_normal_checker, 'A'),
-                'userspace_action': self.do_ronormal_update,
-                'reboot_action': self.sync_and_warm_reboot,
-                'firmware_action': self.software_sync_and_ctrl_d if dev_mode
-                                   else None
-            },
-            {   # Step 2, expected new EC and RW boot, restore the original BIOS
-                'state_checker': (self.ec_checker, True),
-                'userspace_action': self.do_twostop_update,
-                # We use warm reboot here to test the following EC behavior:
-                #   If EC is already into RW before powering on the AP, the AP
-                #   will need to get the EC into RO first. It does this by
-                #   telling the EC to wait for the AP to shut down, reboot
-                #   into RO, then power on the AP automatically.
-                'reboot_action': self.sync_and_warm_reboot,
-                'firmware_action': self.software_sync_and_ctrl_d if dev_mode
-                                   else None
-            },
-            {   # Step 3, expected different EC and RW boot, enable RO flag
-                'state_checker': (self.ec_checker, False),
-                'userspace_action': (self.faft_client.bios.set_preamble_flags,
-                                     ('a', flags)),
-                'reboot_action': self.sync_and_warm_reboot,
-            },
-            {   # Step 4, expected EC RO boot, done
-                'state_checker': (self.checkers.ro_normal_checker, 'A'),
-            },
-        ))
-        self.run_faft_sequence()
+        logging.info("Expected EC RO boot, update EC and disable RO flag.")
+        self.check_state((self.checkers.ro_normal_checker, 'A'))
+        self.do_ronormal_update()
+        self.reboot_warm(wait_for_dut_up=False)
+        if dev_mode:
+            self.software_sync_and_ctrl_d()
+        self.wait_for_kernel_up()
+
+        logging.info("Expected new EC and RW boot, restore the original BIOS.")
+        self.check_state((self.ec_checker, True))
+        self.do_twostop_update()
+        # We use warm reboot here to test the following EC behavior:
+        #   If EC is already into RW before powering on the AP, the AP
+        #   will need to get the EC into RO first. It does this by
+        #   telling the EC to wait for the AP to shut down, reboot
+        #   into RO, then power on the AP automatically.
+        self.reboot_warm(wait_for_dut_up=False)
+        if dev_mode:
+            self.software_sync_and_ctrl_d()
+        self.wait_for_kernel_up()
+
+        logging.info("Expected different EC and RW boot, enable RO flag.")
+        self.check_state((self.ec_checker, False))
+        self.faft_client.bios.set_preamble_flags(('a', flags))
+        self.reboot_warm(ctrl_d=dev_mode)
+
+        logging.info("Expected EC RO boot, done.")
+        self.check_state((self.checkers.ro_normal_checker, 'A'))
