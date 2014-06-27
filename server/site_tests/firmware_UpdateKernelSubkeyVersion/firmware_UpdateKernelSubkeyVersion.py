b51c9355ec4d153740adecb419449a945952c61f
Yusuf Mohsinally <mohsinally@google.com>
2014-06-27 14:55:35 -0700

    Converting firmware_UpdateKernelSubkeyVersion to use new FirmwareTest class.
    The new FirmwareTest class does not require the traditional dict of objects
    that FAFTSequence required.
    Note that this test does not currently work, and will not work till some
    additional fixes are put into place. Doing this work to unblock removal of the
    old faft classes.
    
    1. Converted the test to use the new flat function call style.
    2. Fixed doc string in control file.
    
    BUG=chromium:371185
    TEST=Manually ran this test at desk against squawks device.
    
    Change-Id: I03e16d4a60b544c7fda830e90d341cbf6a664447
    Reviewed-on: https://chromium-review.googlesource.com/206012
    Reviewed-by: Vic Yang <victoryang@chromium.org>
    Commit-Queue: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- server/site_tests/firmware_UpdateKernelSubkeyVersion/firmware_UpdateKernelSubkeyVersion.py
+++ server/site_tests/firmware_UpdateKernelSubkeyVersion/firmware_UpdateKernelSubkeyVersion.py
@@ -2,13 +2,15 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os
+import logging
+import os
+
 from autotest_lib.server import utils
-from autotest_lib.server.cros.faft.faft_classes import FAFTSequence
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 from autotest_lib.client.common_lib import error
 
 
-class firmware_UpdateKernelSubkeyVersion(FAFTSequence):
+class firmware_UpdateKernelSubkeyVersion(FirmwareTest):
     """
     This test requires firmware id matches fwid of shellball
     chromeos-firmwareupdate. On runtime, this test modifies shellball and runs
@@ -35,7 +37,6 @@ class firmware_UpdateKernelSubkeyVersion(FAFTSequence):
                          'make_keys.sh'),
             self._update_version))
 
-
     def check_kernel_subkey_version(self, expected_ver):
         actual_ver = self.faft_client.bios.get_kernel_subkey_version('a')
         if actual_ver != expected_ver:
@@ -47,12 +48,10 @@ class firmware_UpdateKernelSubkeyVersion(FAFTSequence):
                 'Update success, now subkey version is %s',
                 actual_ver)
 
-
     def run_bootok_and_recovery(self):
         self.faft_client.updater.run_bootok('test')
         self.check_kernel_subkey_version(self._update_version)
         self.faft_client.updater.run_recovery()
-
 
     def initialize(self, host, cmdline_args):
         dict_args = utils.args_to_dict(cmdline_args)
@@ -77,12 +76,11 @@ class firmware_UpdateKernelSubkeyVersion(FAFTSequence):
         logging.info('Origin version is %s', ver)
         self._update_version = ver + 1
         logging.info('Kernel subkey version will update to version %s',
-            self._update_version)
+                     self._update_version)
 
         self.resign_kernel_subkey_version(host)
         self.faft_client.updater.resign_firmware(1)
         self.faft_client.updater.repack_shellball('test')
-
 
     def cleanup(self):
         self.faft_client.updater.cleanup()
@@ -90,35 +88,28 @@ class firmware_UpdateKernelSubkeyVersion(FAFTSequence):
         self.invalidate_firmware_setup()
         super(firmware_UpdateKernelSubkeyVersion, self).cleanup()
 
-
     def run_once(self):
-        self.register_faft_sequence((
-            {   # Step1. Update firmware with new kernel subkey version.
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_act': 'A',
-                    'tried_fwb': '0',
-                    'fwid': self._fwid
-                }),
-                'userspace_action': (
-                    self.faft_client.updater.run_autoupdate,
-                    'test'
-                ),
-            },
-            {   # Step2. Check firmware data key version and Rollback
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_act': 'B',
-                    'tried_fwb': '1'
-                }),
-                'userspace_action': (self.run_bootok_and_recovery),
-            },
-            {   # Step3, Check Rollback version
-                'state_checker': (self.checkers.crossystem_checker, {
-                    'mainfw_act': 'A',
-                    'tried_fwb': '0',
-                    'fwid': self._fwid
-                }),
-                'userspace_action':(self.check_kernel_subkey_version,
-                                    self._update_version-1)
-            }
-        ))
-        self.run_faft_sequence()
+        logging.info("Update firmware with new kernel subkey version.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'mainfw_act': 'A',
+                          'tried_fwb': '0',
+                          'fwid': self._fwid
+                          }))
+        self.faft_client.updater.run_autoupdate('test')
+        self.reboot_warm()
+
+        logging.info("Check firmware data key version and Rollback.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'mainfw_act': 'B',
+                          'tried_fwb': '1'
+                          }))
+        self.run_bootok_and_recovery()
+        self.reboot_warm()
+
+        logging.info("Check Rollback version.")
+        self.check_state((self.checkers.crossystem_checker, {
+                          'mainfw_act': 'A',
+                          'tried_fwb': '0',
+                          'fwid': self._fwid
+                          }))
+        self.check_kernel_subkey_version(self._update_version - 1)
