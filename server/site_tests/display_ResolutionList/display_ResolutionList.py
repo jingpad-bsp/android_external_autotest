49745a5a2d0fdc23abc05368af169fc7ec90e821
Ting-Yuan Cheng <tingyuan@chromium.org>
2014-07-08 18:25:41 +0800

    Chameleon: Add a new ResolutionList test.
    The test tests all the resolutions available from the display options dialog in
    ChromeOS (chrome://settings-frame/display) for the connected external display
    (ie, the Chameleon board). Specifically, the test iterates the resolutions in
    random order and, for each resolution, checks if the chrome device emits correct
    video signal accordingly.
    
    BUG=chromium:391681
    TEST=manually run the test on peppy and see it passed.
    CQ-DEPEND=CL:206970, CL:197012
    
    Tested-by: Yuan <tingyuan@chromium.org>
    
    Change-Id: Iacdc7d2a0e51a8144093c0eae9e56d32f4745992
    Reviewed-on: https://chromium-review.googlesource.com/206962
    Reviewed-by: Hung-ying Tyan <tyanh@chromium.org>
    Commit-Queue: Ting-Yuan Cheng <tingyuan@chromium.org>
    Tested-by: Ting-Yuan Cheng <tingyuan@chromium.org>

---
+++ server/site_tests/display_ResolutionList/display_ResolutionList.py
@@ -0,0 +1,149 @@
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""This is a server side resolution display test using the Chameleon board."""
+
+import logging
+import os
+import time
+from random import shuffle
+from autotest_lib.client.common_lib import error
+from autotest_lib.server.cros.chameleon import chameleon_test
+from autotest_lib.server.cros.chameleon import edid
+
+
+class display_ResolutionList(chameleon_test.ChameleonTest):
+    """Server side external display test.
+
+    This test iterates the resolution list obtained from the display options
+    dialog and verifies that each of them works.
+    """
+
+    version = 1
+    # Allow a range of pixel value difference.
+    PIXEL_DIFF_VALUE_MARGIN = 5
+    # Time to wait the calibration image stable, like waiting the info
+    # window "DisplayTestExtension triggered full screen" disappeared.
+    CALIBRATION_IMAGE_SETUP_TIME = 10
+    DEFAULT_TESTCASE_EDID = ('HDMI', 1920, 1080)
+
+    def initialize(self, host):
+        super(display_ResolutionList, self).initialize(host)
+        self.backup_edid()
+
+
+    def cleanup(self):
+        super(display_ResolutionList, self).cleanup()
+        self.restore_edid()
+
+    # TODO: Allow reading testcase_edid from command line.
+    def run_once(self, host, test_mirrored=False, testcase_edid=None):
+        errors = []
+        # (width, height) is the natural resolution for display
+        if testcase_edid is None:
+            testcase_edid = self.DEFAULT_TESTCASE_EDID
+        (tag, width, height) = testcase_edid
+        if not self.is_edid_supported(tag, width, height):
+            logging.info('skip unsupported EDID: %s_%dx%d', tag, width, height)
+            testcase_edid = self.DEFAULT_TESTCASE_EDID
+
+        self.set_up_chameleon((tag, width, height))
+        try:
+            logging.info('Reconnect output...')
+            self.display_client.reconnect_output_and_wait()
+
+
+            display_info = self.display_client.get_display_info()
+            for display_index in xrange(len(display_info)):
+                current_display = display_info[display_index]
+                if current_display.is_internal or not current_display.is_enabled:
+                    logging.info('Display %d (%s): Internal display, '
+                            'skipped.' , display_index,
+                            current_display.display_id)
+                    continue
+
+                resolutions = self.display_client.get_available_resolutions(
+                        display_index)
+                logging.info('Display %d (%s): Total %d resolution modes.'
+                        '%s', display_index,current_display.display_id,
+                        len(resolutions),
+                        " (Primary)"
+                        if current_display.is_primary
+                        else "")
+
+                resolution_test_seq = [i for i in xrange(len(resolutions))]
+                shuffle(resolution_test_seq)
+                logging.info('Set mirrored: %s', test_mirrored)
+                self.display_client.set_mirrored(test_mirrored)
+
+                for test_resolution_index in resolution_test_seq:
+                    # (set_width, set_height) resolution by manual setting
+                    set_width, set_height = resolutions[
+                            test_resolution_index]
+
+                    logging.info('Set external display resolution: mode %d,'
+                                 ' width: %d, height: %d ... %s',
+                                 test_resolution_index, set_width, set_height,
+                                 "Successfully"
+                                 if self.display_client.set_resolution(
+                                         display_index, set_width, set_height)
+                                 else "Unsuccessfully")
+
+                    logging.info('Waiting the calibration image stable.')
+
+                    self.display_client.load_calibration_image ((
+                            set_width, set_height))
+                    self.display_client.hide_cursor()
+                    time.sleep(self.CALIBRATION_IMAGE_SETUP_TIME)
+
+                    logging.info('Checking the resolution.')
+                    chameleon_resolution = self.chameleon_port.get_resolution()
+                    dut_resolution = self.display_client.get_resolution()
+                    # Verify the actual resolution detected by chameleon and
+                    # dut are the same as what is expected.
+                    # Note: In mirrored mode, the device may be in hardware
+                    # mirror (as opposed to software mirror). If so, the
+                    # actual resolution could be different from the expected
+                    # one. So we skip the check in mirrored mode.
+                    if ((set_width, set_height) != chameleon_resolution or
+                            (set_width, set_height) != dut_resolution):
+                        error_message = (
+                                'Detected a different resolution: '
+                                'chameleon: %r; dut: %r; expected %r' %
+                                (chameleon_resolution, dut_resolution,
+                                 (set_width, set_height)))
+                        if test_mirrored:
+                            logging.warn(error_message)
+                        else:
+                            logging.error(error_message)
+                            errors.append(error_message)
+
+                    if chameleon_resolution == dut_resolution:
+                        error_message = self.check_screen_with_chameleon(
+                                '%s-%dx%d' % ((tag,) + dut_resolution),
+                                self.PIXEL_DIFF_VALUE_MARGIN, 0)
+                        if error_message:
+                            errors.append(error_message)
+
+        finally:
+            self.display_client.close_tab()
+
+        if errors:
+            raise error.TestFail('; '.join(errors))
+
+
+    def set_up_chameleon(self, resolution):
+        """Loads the EDID of the given resolution onto Chameleon.
+
+        @param resolution: A tuple (tag, width, height) representing the
+                resolution to test.
+        """
+        logging.info('Setting up %r on port %d (%s)...',
+                     resolution,
+                     self.chameleon_port.get_connector_id(),
+                     self.chameleon_port.get_connector_type())
+        filename = os.path.join(
+                self.bindir, 'test_data', 'edids', '%s_%dx%d' % resolution)
+        logging.info('Apply edid: %s', filename)
+        self.chameleon_port.apply_edid(edid.Edid.from_file(filename))
