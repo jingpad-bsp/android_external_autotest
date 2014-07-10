c57ec863b1f8a90eae3e1f4d980a3b877b4e7ca1
Dave Tu <dtu@chromium.org>
2014-07-09 18:06:29 -0700

    [telemetry] Update telemetry_UnitTests to use OutputFormatter.
    The Telemetry run_tests interface is changing again with https://codereview.chromium.org/382433002/. Update to use the new interface.
    
    BUG=chromium:346956
    TEST=Run this test.
    
    Change-Id: Ib553f124b78a47438baa4f1941387c7ffd177087
    Reviewed-on: https://chromium-review.googlesource.com/207360
    Reviewed-by: David Tu <dtu@chromium.org>
    Tested-by: David Tu <dtu@chromium.org>

--- client/site_tests/telemetry_UnitTests/telemetry_UnitTests.py
+++ client/site_tests/telemetry_UnitTests/telemetry_UnitTests.py
@@ -2,11 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, StringIO, sys
+import logging, os, sys
 
-from telemetry.core import browser_finder
-from telemetry.core import browser_options
-from telemetry.unittest import gtest_unittest_results
+from telemetry.unittest import gtest_output_formatter
 from telemetry.unittest import run_tests
 
 from autotest_lib.client.bin import test
@@ -18,6 +16,34 @@ TELEMETRY_SUBDIR = os.path.join(TELEMETRY_BASE_DIR, 'telemetry')
 PERF_BASE_DIR = os.path.join(TOOLS_BASE_DIR, 'perf')
 
 
+class LoggingOutputStream(object):
+    """A file-like object that buffers lines and sends them to logging.info."""
+    def __init__(self):
+        self._buffer = []
+
+    def write(self, s):
+        """Buffer a string write. Log it when we encounter a newline.
+
+        @param s: The string to write to this stream.
+        """
+        if '\n' in s:
+            segments = s.split('\n')
+            segments[0] = ''.join(self._buffer + [segments[0]])
+            log_level = logging.getLogger().getEffectiveLevel()
+            try:  # TODO(dtu): We need this because of crbug.com/394571
+                logging.getLogger().setLevel(logging.INFO)
+                for line in segments[:-1]:
+                    logging.info(line)
+            finally:
+                logging.getLogger().setLevel(log_level)
+            self._buffer = [segments[-1]]
+        else:
+            self._buffer.append(s)
+
+    def flush(self):
+        """Dummy implementation of file flush."""
+
+
 class TestSpec(object):
     """Test specification class with directory paths, etc."""
     def __init__(self, test_type, root, top_level_dir, tests):
@@ -26,14 +52,12 @@ class TestSpec(object):
         self.top_level_dir = top_level_dir
         self.tests = tests
         self.num_errors = 0
-        self.error_details = ''
 
     def error_string(self):
         """Printable error string. Returns empty string if no errors."""
         if self.num_errors:
-            return ('%d %s unit tests failed:\n%s' %
-                    (self.num_errors, self.test_type,
-                    self.error_details))
+            return ('%d %s unit tests failed.\n' %
+                    (self.num_errors, self.test_type))
         return ''
 
 
@@ -50,36 +74,14 @@ class telemetry_UnitTests(test.test):
         """
         logging.info('Running %s unit tests with browser_type "%s".',
                      test_spec.test_type, browser_type)
-        sys.path.append(test_spec.root)
-        args = browser_options.BrowserFinderOptions()
-        args.browser_type = browser_type
-        args.CreateParser().parse_args([])  # Hack to update BrowserOptions.
-        possible_browser = browser_finder.FindBrowser(args)
-        tests = run_tests.DiscoverTests(
-            [test_spec.root], test_spec.top_level_dir,
-            possible_browser, test_spec.tests)
-        runner = gtest_unittest_results.GTestTestRunner()
-        # Capture the Telemetry output when running the unit tests.
-        capturer = StringIO.StringIO()
-        try:
-            sys.stdout = capturer
-            result = runner.run(tests, 1, args)
-            test_output = capturer.getvalue()
-        finally:
-            sys.stdout = sys.__stdout__  # Restore sys.stdout.
-            capturer.close()
-        logging.info(test_output)  # Log the Telemetry output.
+        sys.path.append(test_spec.top_level_dir)
 
-        if result.wasSuccessful():
-            logging.info('All %d %s unit tests passed.',
-                         len(result.successes), test_spec.test_type)
-        else:
-            for (test_name, error_string) in result.failures_and_errors:
-                test_spec.error_details += '%s\n%s\n' % (test_name,
-                                                         error_string)
-            test_spec.num_errors = len(result.failures_and_errors)
-            logging.error(test_spec.error_string())
-
+        output_formatters = [
+            gtest_output_formatter.GTestOutputFormatter(LoggingOutputStream())]
+        run_tests.config = run_tests.Config(
+            test_spec.top_level_dir, [test_spec.root], output_formatters)
+        test_spec.num_errors = run_tests.RunTestsCommand.main(
+            ['--browser', browser_type] + test_spec.tests)
 
     def run_once(self, browser_type, unit_tests, perf_tests):
         """
