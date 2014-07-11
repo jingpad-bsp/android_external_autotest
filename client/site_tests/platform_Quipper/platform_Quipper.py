e17bfe47c3337abf4b9a3cfcd00dec5d924768dd
Simon Que <sque@chromium.org>
2014-07-10 17:44:01 -0700

    platform_Quipper: Test more advanced perf options
    Test a wide range of possible perf options.
    
    BUG=chromium:388366
    TEST=platform_Quipper test passes
    
    Change-Id: I425af3fcafa652d372f87d7827c104886c44e243
    Signed-off-by: Simon Que <sque@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/207455
    Reviewed-by: David Sharp <dhsharp@google.com>

--- client/site_tests/platform_Quipper/platform_Quipper.py
+++ client/site_tests/platform_Quipper/platform_Quipper.py
@@ -13,34 +13,80 @@ class platform_Quipper(test.test):
     """
     version = 1
 
+    def _get_quipper_command(self, duration, perf_options):
+        return ('quipper', str(duration), 'perf', 'record', '-a') + \
+               perf_options
+
+
+    def _get_perf_command(self, duration, perf_options):
+        return ('perf', 'record', '-a') + perf_options + \
+               ('--', 'sleep', str(duration))
+
 
     def run_once(self):
         """
         See test description.
         """
 
-        # Quipper command.
-        # TODO(sque): Test more event types, LBR, callgraph, etc.
         duration = 2
-        quipper_command = [ 'quipper', str(duration), 'perf', 'record', '-a',
-                            '-e', 'cycles' ]
-        quipper_command_string = ' '.join(quipper_command)
 
-        result = ""
-        try:
-            result = subprocess.check_output(quipper_command,
-                                             stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError:
-            raise error.TestFail('Error running command: \"' +
-                                 quipper_command_string)
+        # These are the various perf command options to add to
+        # |quipper_command_base|, for a wide range of commands to test.
+        quipper_command_options = (
+            # Basic cycle-based profile.
+            ('-e', 'cycles'),
+            # Set a custom sampling frequency.
+            ('-e', 'cycles', '-F', '3011'),
+            # Set a custom sampling period.
+            ('-e', 'cycles', '-c', '2000003'),
+            # Test various events.
+            ('-e', 'cycles,instructions,branch-misses,cache-misses'),
+            # Test callgraph.
+            ('-e', 'cycles', '-g'),
+            # Test callgraph and raw data.
+            ('-e', 'cycles', '-g', '-R'),
+            # Test LBR.
+            ('-e', 'cycles', '-b'),
+            # Test LBR, callgraph, and raw data.
+            ('-e', 'cycles', '-b', '-g', '-R'),
+        )
 
-        # Write keyvals.
         keyvals = {}
-        keyvals['command'] = quipper_command_string;
-        keyvals['result_length'] = len(result)
-        self.write_perf_keyval(keyvals)
+        # Run quipper with each of the options.
+        for options in quipper_command_options:
+            result = ""
 
-        # Verify the output size.
-        if len(result) == 0:
-            raise error.TestFail('Got no result data from quipper.')
+            # Try running the associated perf command first.
+            perf_command = self._get_perf_command(duration, options)
+
+            # Generate a full quipper command by joining the base command
+            # and various perf options.
+            quipper_command = self._get_quipper_command(duration, options)
+            quipper_command_string = ' '.join(quipper_command)
+
+            try:
+                result = subprocess.check_output(perf_command)
+            except subprocess.CalledProcessError:
+                # If the perf command fails, don't test quipper. But record that
+                # it was skipped.
+                keyvals['command'] = '(' + quipper_command_string + ')'
+                keyvals['result_length'] = '(skipped)'
+                self.write_perf_keyval(keyvals)
+                continue
+
+            try:
+                result = subprocess.check_output(quipper_command,
+                                                 stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError:
+                raise error.TestFail('Error running command: ' +
+                                     quipper_command_string)
+
+            # Write keyvals.
+            keyvals['command'] = quipper_command_string;
+            keyvals['result_length'] = len(result)
+            self.write_perf_keyval(keyvals)
+
+            # Verify the output size.
+            if len(result) == 0:
+                raise error.TestFail('Got no result data from quipper.')
 
