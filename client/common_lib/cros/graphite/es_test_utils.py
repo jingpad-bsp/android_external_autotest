e95a0eb4a4f9df2c7f569d35caed9532c9c7b974
Michael Liang <michaelliang@chromium.org>
2014-07-18 09:44:20 -0700

    [autotest] Support multiple equality/range constraints in es query.
    Create create_range_eq_query_multiple which can take in
    multiple range and equality constraints and apply them in a query
    to es. Cleaned up some TODOs that no longer applied.
    
    BUG=chromium:394451
    TEST=python stats_es_functionaltest.py --all --es_host=prod
    
    Change-Id: If765fbb7c84866194b6009c32cd4131dde585f05
    Reviewed-on: https://chromium-review.googlesource.com/208893
    Reviewed-by: Michael Liang <michaelliang@chromium.org>
    Commit-Queue: Michael Liang <michaelliang@chromium.org>
    Tested-by: Michael Liang <michaelliang@chromium.org>
    Reviewed-by: Dan Shi <dshi@chromium.org>

--- client/common_lib/cros/graphite/es_test_utils.py
+++ client/common_lib/cros/graphite/es_test_utils.py
@@ -5,6 +5,7 @@
 """Helper functions for testing stats module and elasticsearch
 """
 
+import datetime
 import logging
 import time
 
@@ -41,13 +42,30 @@ TARGET_TO_METHOD = {
     'counter': '_send',
 }
 
+# Default maximum number of entries to return from ES query
+DEFAULT_NUM_ENTRIES = 100
 
 class EsTestUtilException(Exception):
     """Exception raised when functions here fail. """
     pass
 
 
-# TODO: Point to test es instance, instead of prod.
+def _to_epoch_time(value):
+    """Converts value to epoch time if it is a datetime object.
+
+    This function should only be called in create_range_eq_query_multiple.
+
+    @param value: anything
+    @param returns: epoch time if value is datetime.datetime,
+                    otherwise returns the value.
+    """
+    if isinstance(value, datetime.datetime):
+        # This looks kinda hacky, timetuple() only goes to the
+        # level of seconds. So I need to add on the decimal part.
+        return time.mktime(value.timetuple()) + 0.000001*value.microsecond
+    return value
+
+
 def sequential_random_insert_ints(keys, num_entries, target_type, index,
                                   host, port, between_insert_secs=0,
                                   print_interval=10):
@@ -59,7 +77,9 @@ def sequential_random_insert_ints(keys, num_entries, target_type, index,
     @param target_type: This must be in
             ['timer', 'gauge', 'raw', 'average', 'counter']
     @param between_insert_secs: Time to sleep after each insert.
+                                defaults to no sleep time.
     @param print_interval: how often to print
+                           defaults to every 10 entries.
     @param index: Index of es db to insert to
     @param host: host of es db
     @param port: port of es db
@@ -67,7 +87,7 @@ def sequential_random_insert_ints(keys, num_entries, target_type, index,
     # We are going to start the value at 0 and increment it by one per val.
     for i in range(num_entries):
         if print_interval == 0 or i % print_interval == 0:
-            print ('    Inserting entry #%s with keys %s into index "%s."'
+            print('    Inserting entry #%s with keys %s into index "%s."'
                    % (i, str(keys), index))
         metadata = {}
         for value, key in enumerate(keys):
@@ -100,7 +120,9 @@ def clear_index(index, host, port, timeout, sleep_time=0.5, clear_timeout=5):
     @param port: elasticsearch port
     @param timeout: how long to wait while connecting to es.
     @param sleep_time: time between tries of clear_index
+                       defaults to 0.5 seconds
     @param clear_timeout: how long to wait for index to be cleared.
+                       defualts to 5 seconds
       Will quit and throw error if not cleared. (Number of seconds)
     """
     es = elasticsearch.Elasticsearch(host=host,
@@ -119,23 +141,140 @@ def clear_index(index, host, port, timeout, sleep_time=0.5, clear_timeout=5):
     print 'successfully deleted index %s' % (index)
 
 
+def create_range_eq_query_multiple(fields_returned,
+                                   equality_constraints,
+                                   range_constraints,
+                                   size,
+                                   sort_specs):
+    """Creates a dict. representing multple range and/or equality queries.
+
+    Example input:
+        create_range_eq_query_multiple(
+                fields_returned = ['time_recorded', 'hostname',
+                                   'status', 'dbg_str'],
+                equality_constraints = [
+                    ('_type', 'host_history'),
+                    ('hostname', '172.22.169.106'),
+                ],
+                range_constraints = [
+                    ('time_recorded', 1405628341.904379, 1405700341.904379)
+                ],
+                size=20,
+                sort_specs=[
+                    'hostname',
+                    {'time_recorded': 'asc'},
+                ]
+        )
+
+    Output:
+    {
+        'fields': ['time_recorded', 'hostname', 'status', 'dbg_str'],
+        'query': {
+            'bool': {
+                'minimum_should_match': 3,
+                'should': [
+                    {
+                        'term':  {
+                            '_type': 'host_history'
+                        }
+                    },
+
+                    {
+                        'term': {
+                            'hostname': '172.22.169.106'
+                        }
+                    },
+
+                    {
+                        'range': {
+                            'time_recorded': {
+                                'gte': 1405628341.904379,
+                                'lte': 1405700341.904379
+                            }
+                        }
+                    }
+                ]
+            },
+        },
+        'size': 20
+        'sort': [
+            'hostname',
+            { 'time_recorded': 'asc'},
+        ]
+    }
+
+    @param fields_returned: list of fields that we should return when
+                            the query is executed
+    @param equality_constraints: list of tuples of (field, value) pairs
+        representing what each field should equal to in the query.
+        e.g. [ ('field1', 1), ('field2', 'value') ]
+    @param range_constraints: list of tuples of (field, low, high) pairs
+        representing what each field should be between (inclusive).
+        e.g. [ ('field1', 2, 10), ('field2', -1, 20) ]
+        If you want one side to be unbounded, you can use None.
+        e.g. [ ('field1', 2, None) ] means value of field1 >= 2.
+    @param size: max number of entries to return.
+    @param sort_specs: A list of fields to sort on, tiebreakers will be
+        broken by the next field(s).
+    @param returns: dictionary object that represents query to es.
+                    This will return None if there are no equality constraints
+                    and no range constraints.
+    """
+    if not equality_constraints and not range_constraints:
+        raise EsTestUtilException('No range or equality constraints specified...')
+
+    # Creates list of range dictionaries to put in the 'should' list.
+    if range_constraints:
+        range_list = []
+        for key, low, high in range_constraints:
+            if low == None and high == None:
+                continue
+            temp_dict = {}
+            if low != None:
+                temp_dict['gte'] = _to_epoch_time(low)
+            if high != None:
+                temp_dict['lte'] = _to_epoch_time(high)
+            range_list.append( {'range': {key: temp_dict}})
+
+    # Creates the list of term dictionaries to put in the 'should' list.
+    eq_list = [{'term': {k: v}} for k, v in equality_constraints if k]
+    num_constraints = len(equality_constraints) + len(range_constraints)
+    return {
+        'fields': fields_returned,
+        'query': {
+            'bool': {
+                'should': eq_list + range_list,
+                'minimum_should_match': num_constraints,
+            }
+        },
+        'size': size,
+        'sort': sort_specs if sort_specs else [],
+    }
+
+
 def create_range_eq_query(fields_returned,
                           equals_key=None,
                           equals_val=None,
                           range_key=None,
                           range_low=None,
-                          range_high=None):
+                          range_high=None,
+                          size=DEFAULT_NUM_ENTRIES,
+                          sort_specs=None):
     """Creates a dict. representing range and/or equality queries.
 
     @param fields_returned: list of fields that we should return when
                             the query is executed
-    @param equals_key: Key that we filter based on equality.
-    @param equals_val: value we want equals_key to be equal to.
-    @param range_key: Key that we filter based on range
-    @param range_low: lower bound on the range_key (inclusive)
-    @param range_high: upper bound on the range key (inclusive)
+    @param equals_key: Key that we filter based on equality. default=None
+    @param equals_val: value we want equals_key to be equal to. default=None
+    @param range_key: Key that we filter based on range. default=None
+    @param range_low: lower bound on the range_key (inclusive). default=None
+    @param range_high: upper bound on the range key (inclusive). default=None
+    @param size: max number of entries to return. default=100
+    @param sort_specs: A list of fields to sort on, tiebreakers will be
+        broken by the next field(s). default=None
 
     @returns: dictionary representing query.
+              Returns none if there is no equals_key or range_key provided.
 
     Example usage:
     range_eq_query = create_range_eq_query(
@@ -146,81 +285,44 @@ def create_range_eq_query(fields_returned,
     Output:
     {
         'fields': ['job_id', 'host_id', 'job_start'],
-        'query': {
-            'filtered': {
-                'query': {
-                    'match': {
-                        'host_id': 10,
-                    }
-                }
-                'filter': {
-                    'range': {
-                        'job_id': {
-                            'gte': 0,
-                            'lte': 99999,
-                        }
-                    }
-                }
-            }
-        }
-    }
+            'query': {
+                'bool': {
+                    'minimum_should_match': 2,
+                    'should': [
+                        {
+                            'term':  {
+                                'host_id': 10,
+                            }
+                        },
 
-    TODO(michaelliang): equals_key: make this support more than 1 equality key
-    TODO(michaelliang): range_key: make this support more than 1 range key
+                        {   
+                            'range': {
+                                'job_id': {
+                                    'gte': 0,
+                                    'lte': 99999,
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+        'size': 20
+        'sort': [ ]
+    }
     """
     if not equals_key and not range_key:
-        logging.warn('No range_key or equals_key specified...')
-        return
-    filtered_terms = {}
-    if equals_key:
-        filtered_terms['match'] = {equals_key: equals_val}
+        raise EsTestUtilException('No range_key or equals_key specified.')
+    equality_constraints = [(equals_key, equals_val)] if equals_key else []
+    range_constraints = []
     if range_key:
-        filtered_terms['range'] = {}
-        filtered_terms['range'][range_key] = {}
-        if range_low != None:
-            filtered_terms['range'][range_key]['gte'] = range_low
-        if range_high != None:
-            filtered_terms['range'][range_key]['lte'] = range_high
-    return _create_query(fields_returned, filtered_terms)
-
-
-def _create_query(fields_returned, filtered_terms):
-    """Helper method for create_range_query
-
-    @param fields_returned: list of fields to return
-    @param filtered_terms: dict where keys can be 'match' and/or 'range'
-        and value is dict contains information about what to match or
-        within what range. For example:
-        {
-            'match': {
-                'host_id': 10,
-            }
-            'range': {
-                'job_id': {
-                    'gte': 0,
-                    'lte': 99999,
-                }
-            }
-        }
-    @returns: query string
-    """
-    range_dict = {}
-    filtered_dict = {}
-    if 'range' in filtered_terms:
-        range_dict['range'] = filtered_terms['range']
-        filtered_dict['filter'] = range_dict
-    match_dict = {}
-    if 'match' in filtered_terms:
-        match_dict['match'] = filtered_terms['match']
-        filtered_dict['query'] = match_dict
-
-    query_base = {
-        'fields': fields_returned,
-        'query' : {
-            'filtered' : filtered_dict,
-        }
-    }
-    return query_base
+        range_constraints = [(range_key, range_low, range_high)]
+    return create_range_eq_query_multiple(
+        fields_returned,
+        equality_constraints=equality_constraints,
+        range_constraints=range_constraints,
+        size=size,
+        sort_specs=sort_specs,
+    )
 
 
 def execute_query(query, index, host, port, timeout=3):
@@ -231,6 +333,7 @@ def execute_query(query, index, host, port, timeout=3):
     @param host: host running es
     @param port: port running es
     @param timeout: seconds to wait before es retries if conn. fails.
+                    default is 3 seconds.
     @returns: dictionary of the results, or None if index does not exist.
 
     Example output:
@@ -286,7 +389,6 @@ def execute_query(query, index, host, port, timeout=3):
     }
 
     """
-
     es = elasticsearch.Elasticsearch(host=host, port=port, timeout=timeout)
     if not es.indices.exists(index=index):
         return None
