3e31a77736ae7eeb0c43d6cb9c3d73e5850871c7
Alex Deymo <deymo@chromium.org>
2014-08-08 18:23:08 -0700

    graphics_*: Set label Cr-OS-Kernel-Graphics when autofiling.
    When a new bug is filled as untriaged, add the right Cr- flag so
    the owners can watch for new failures. This patch includes missing
    control files on the previous CL.
    
    BUG=chromium:402142
    TEST=None
    
    Change-Id: I225a99279a59bbf6a25c474d309a75b59290c9e2
    Reviewed-on: https://chromium-review.googlesource.com/211761
    Reviewed-by: Ilja Friedel <ihf@chromium.org>
    Commit-Queue: Alex Deymo <deymo@chromium.org>
    Tested-by: Alex Deymo <deymo@chromium.org>

--- client/site_tests/graphics_PiglitBVT/generate_scripts.py
+++ client/site_tests/graphics_PiglitBVT/generate_scripts.py
@@ -68,6 +68,10 @@ TEST_CLASS = 'graphics'\n\
 TEST_TYPE = 'client'\n\
 JOB_RETRIES = 1\n\
 \n\
+BUG_TEMPLATE = {\n\
+    'labels': ['Cr-OS-Kernel-Graphics'],\n\
+}\n\
+\n\
 DOC = \"\"\"\n\
 Piglit is a collection of automated tests for OpenGL implementations.\n\
 \n\
