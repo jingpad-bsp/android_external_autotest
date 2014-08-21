231228506cb91e0442ce79ad54e6ab34f89d17ab
Alex Miller <milleral@chromium.org>
2014-08-21 14:38:53 -0700

    [autotest] Only warn about non-locked shared hosts.
    BUG=None
    TEST=Ran and saw no output
    
    Change-Id: I077ddf323434a3942a86baaabc6e7ac1c5aaf420
    Reviewed-on: https://chromium-review.googlesource.com/213677
    Reviewed-by: Alex Miller <milleral@chromium.org>
    Commit-Queue: Alex Miller <milleral@chromium.org>
    Tested-by: Alex Miller <milleral@chromium.org>

--- contrib/shared_hosts.py
+++ contrib/shared_hosts.py
@@ -10,8 +10,10 @@ from autotest_lib.server import frontend
 cautotest = frontend.AFE(server='cautotest')
 cautotest_cq = frontend.AFE(server='cautotest-cq')
 
-cautotest_hosts = [x['hostname'] for x in cautotest.run('get_hosts')]
-cautotest_cq_hosts = [x['hostname'] for x in cautotest_cq.run('get_hosts')]
+cautotest_hosts = [x['hostname'] for x in cautotest.run('get_hosts')
+                   if not x['locked']]
+cautotest_cq_hosts = [x['hostname'] for x in cautotest_cq.run('get_hosts')
+                      if not x['locked']]
 
 for host in cautotest_hosts:
     if host in cautotest_cq_hosts:
