bfa3d703298c5eb92eb1d418a6abf21d19cfec7d
Zach Reizner <zachr@chromium.org>
2014-09-15 16:32:33 -0700

    Freonize hardware_Resolution
    BUG=chromium:413091
    TEST=run hardware_Resolution autotest
    CQ-DEPEND=CL:217942
    
    Change-Id: I0bcace6a4fdaeb1e88f326cf51df8d1cc2c84e85
    Reviewed-on: https://chromium-review.googlesource.com/218326
    Reviewed-by: Ilja Friedel <ihf@chromium.org>
    Commit-Queue: Zach Reizner <zachr@chromium.org>
    Tested-by: Zach Reizner <zachr@chromium.org>

--- client/site_tests/hardware_Resolution/hardware_Resolution.py
+++ client/site_tests/hardware_Resolution/hardware_Resolution.py
@@ -6,11 +6,26 @@
 
 __author__ = 'kdlucas@chromium.org (Kelly Lucas)'
 
-import os, re
+import os, re, logging
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 
+_SUPPORTED_LVDS_RESOLUTIONS = ['1280x800', '1366x768']
+
+_MODETEST_COMMAND = 'modetest -c'
+_MODETEST_CONNECTED = 'connected'
+_MODETEST_CONNECTOR_LVDS = 'LVDS'
+# The list of connectors in this regex pattern comes from an array called
+# connector_type_names in the libdrm file caled modetest.c .
+_MODETEST_CONNECTOR_PATTERN = (r'\d+\s+\d+\s+(connected|disconnected)\s+'
+                               r'(unknown|VGA|DVI-I|DVI-D|DVI-A|composite|'
+                               r's-video|LVDS|component|9-pin DIN|HDMI-A|'
+                               r'HDMI-B|TV|eDP)\s+\d+x\d+\s+\d+\s+\d+')
+_MODETEST_MODE_PATTERN = (r'\s+.+\d+\s+(\d+)\s+\d+\s+\d+\s+\d+\s+(\d+)\s+\d+\s+'
+                          r'\d+\s+\d+\s+flags:')
+
+_LVDS_UNSUPPORTED_MESSAGE = '%s is not a supported LVDS resolution'
 
 class hardware_Resolution(test.test):
     """
@@ -38,7 +53,6 @@ class hardware_Resolution(test.test):
 
         return output.split('\n')
 
-
     def is_lvds_res(self, res, xrandr_output):
         """
         Returns True if the supplied resolution is associated with
@@ -50,7 +64,6 @@ class hardware_Resolution(test.test):
                 return True;
 
         return False
-
 
     def get_current_res(self, xrandr_output):
         """
@@ -68,15 +81,43 @@ class hardware_Resolution(test.test):
 
         return None
 
-
-    def run_once(self):
+    def run_x(self):
         xrandr_output = self.get_xrandr_output()
 
         res = self.get_current_res(xrandr_output)
         if not res or not re.match(r'\d+x\d+$', res):
             raise error.TestFail('%s is not a valid resolution' % res)
 
-        supported_lvds_resolutions = ['1280x800', '1366x768']
         if self.is_lvds_res(res, xrandr_output) and \
-           res not in supported_lvds_resolutions:
-            raise error.TestFail('%s is not a supported LVDS resolution' % res)
+           res not in _SUPPORTED_LVDS_RESOLUTIONS:
+            raise error.TestFail(_LVDS_UNSUPPORTED_MESSAGE % res)
+
+    def run_freon(self):
+        modetest_output = utils.system_output(_MODETEST_COMMAND)
+        logging.info('modetest output: \n{0}'.format(modetest_output))
+
+        # True if the information being read is about a connected LVDS
+        # connector, False otherwise
+        connected_lvds = False
+
+        for line in modetest_output.splitlines():
+            connector_match = re.match(_MODETEST_CONNECTOR_PATTERN, line)
+            if connector_match is not None:
+                connected_lvds = False
+                if connector_match.group(1) == _MODETEST_CONNECTED:
+                    if connector_match.group(2) == _MODETEST_CONNECTOR_LVDS:
+                        connected_lvds = True
+
+            if connected_lvds:
+                mode_match = re.match(_MODETEST_MODE_PATTERN, line)
+                if mode_match is not None:
+                    res = '{0}x{1}'.format(int(mode_match.group(1)),
+                                           int(mode_match.group(2)))
+                    if res not in _SUPPORTED_LVDS_RESOLUTIONS:
+                        raise error.TestFail(_LVDS_UNSUPPORTED_MESSAGE % res)
+
+    def run_once(self):
+        if utils.is_freon():
+            self.run_freon()
+        else:
+            self.run_x()
