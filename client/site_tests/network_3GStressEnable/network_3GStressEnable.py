7803e4f0c008db708b6e46b045e9c5cb9d5ea871
Thieu Le <thieule@chromium.org>
2014-09-18 13:02:33 -0700

    cellular: Migrate network_3GStressEnable to new test_environment
    BUG=chromium:415293
    TEST=network_3GStressEnable
    
    Change-Id: Id29ffdc3370020e23db399387852b72e04bf14c6
    Reviewed-on: https://chromium-review.googlesource.com/218905
    Reviewed-by: Prathmesh Prabhu <pprabhu@chromium.org>
    Commit-Queue: Thieu Le <thieule@chromium.org>
    Tested-by: Thieu Le <thieule@chromium.org>

--- client/site_tests/network_3GStressEnable/network_3GStressEnable.py
+++ client/site_tests/network_3GStressEnable/network_3GStressEnable.py
@@ -2,19 +2,15 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import dbus
+import logging
+import time
+
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros import backchannel
-from autotest_lib.client.cros import network
-from autotest_lib.client.cros.cellular.pseudomodem import pseudomodem_context
+from autotest_lib.client.cros.networking import shill_context
+from autotest_lib.client.cros.networking import shill_proxy
 
-import time
-import dbus
-
-from autotest_lib.client.cros import flimflam_test_path
-import flimflam
-
-DEVICE_TIMEOUT=45
 
 class network_3GStressEnable(test.test):
     """
@@ -24,57 +20,36 @@ class network_3GStressEnable(test.test):
     version = 1
 
     okerrors = [
-        'org.chromium.flimflam.Error.InProgress'
+        shill_proxy.ShillProxy.ERROR_IN_PROGRESS
     ]
 
     def _enable_device(self, enable):
         try:
+            timeout = shill_proxy.ShillProxy.DEVICE_ENABLE_DISABLE_TIMEOUT
             if enable:
-                self.device.Enable(timeout=DEVICE_TIMEOUT)
+                self.device.Enable(timeout=timeout)
             else:
-                self.device.Disable(timeout=DEVICE_TIMEOUT)
+                self.device.Disable(timeout=timeout)
         except dbus.exceptions.DBusException, err:
-            if err._dbus_error_name in network_3GStressEnable.okerrors:
+            if err.get_dbus_name() in network_3GStressEnable.okerrors:
                 return
-            else:
-                raise error.TestFail(err)
+            raise error.TestFail(err)
+
 
     def _test(self, settle):
-        self._enable_device(True)
-        time.sleep(settle)
         self._enable_device(False)
         time.sleep(settle)
+        self._enable_device(True)
+        time.sleep(settle)
 
-    def _run_once_internal(self, cycles, min, max):
-        self.flim = flimflam.FlimFlam(dbus.SystemBus())
-        network.ResetAllModems(self.flim)
-        self.device = self.flim.FindCellularDevice()
-        if not self.device:
-            raise error.TestFail('Failed to find a cellular device.')
-        service = self.flim.FindCellularService()
-        if service:
-            # If cellular's already up, take it down to start.
-            try:
-                service.SetProperty('AutoConnect', False)
-            except dbus.exceptions.DBusException, err:
-                # If the device has never connected to the cellular service
-                # before, flimflam will raise InvalidService when attempting
-                # to change the AutoConnect property.
-                if err._dbus_error_name != 'org.chromium.flimflam.'\
-                                             'Error.InvalidService':
-                    raise err
-            self._enable_device(False)
-        for t in xrange(max, min, -1):
-            for n in xrange(cycles):
-                # deciseconds are an awesome unit.
-                print 'Cycle %d: %f seconds delay.' % (n, t / 10.0)
-                self._test(t / 10.0)
-        print 'Done.'
 
-    def run_once(self, cycles=3, min=15, max=25, use_pseudomodem=False,
-                 pseudomodem_family='3GPP'):
-        with backchannel.Backchannel():
-            with pseudomodem_context.PseudoModemManagerContext(
-                    use_pseudomodem,
-                    {'family' : pseudomodem_family}):
-                self._run_once_internal(cycles, min, max)
+    def run_once(self, test_env, cycles=3, min=15, max=25):
+        with test_env, shill_context.ServiceAutoConnectContext(
+                test_env.shill.find_cellular_service_object, False):
+            self.device = test_env.shill.find_cellular_device_object()
+            for t in xrange(max, min, -1):
+                for n in xrange(cycles):
+                    # deciseconds are an awesome unit.
+                    logging.info('Cycle %d: %f seconds delay.', n, t / 10.0)
+                    self._test(t / 10.0)
+            logging.info('Done.')
