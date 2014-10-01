4bb434dd2c7a2a2853864c1a2e31284fb46a46f5
Christopher Wiley <wiley@chromium.org>
2014-10-01 11:52:25 -0700

    autotest: Refactor avahi chroot related logic into delegate
    This helps us reused this across tests which may also need avahi
    (e.g. peerd_DiscoverServices).
    
    BUG=chromium:418738
    TEST=This test continues to pass.
    
    Change-Id: If4844c9dcacbb8f2d2a74887f3c217f34a0db81b
    Reviewed-on: https://chromium-review.googlesource.com/220873
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Commit-Queue: Christopher Wiley <wiley@chromium.org>
    Tested-by: Christopher Wiley <wiley@chromium.org>

--- client/site_tests/peerd_AdvertiseServices/peerd_AdvertiseServices.py
+++ client/site_tests/peerd_AdvertiseServices/peerd_AdvertiseServices.py
@@ -4,16 +4,10 @@
 
 import dpkt
 import logging
-import time
 
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.common_lib.cros.network import netblock
-from autotest_lib.client.cros import avahi_utils
-from autotest_lib.client.cros import network_chroot
-from autotest_lib.client.cros import service_stopper
-from autotest_lib.client.cros import tcpdump
-from autotest_lib.client.cros import virtual_ethernet_pair
+from autotest_lib.client.cros import chrooted_avahi
 from autotest_lib.client.cros.netprotos import interface_host
 from autotest_lib.client.cros.netprotos import zeroconf
 from autotest_lib.client.cros.tendo import peerd_helper
@@ -23,139 +17,46 @@ class peerd_AdvertiseServices(test.test):
     """Test that peerd can correctly advertise services over mDNS."""
     version = 1
 
-    SERVICES_TO_STOP = ['p2p', 'peerd', 'avahi']
-    # This side has to be called something special to avoid shill touching it.
-    MONITOR_IF_IP = netblock.Netblock('10.9.8.1/24')
-    MONITOR_IF_NAME = 'pseudoethernet1'
-    # We'll drop the Avahi side into our network namespace.
-    AVAHI_IF_IP = netblock.Netblock('10.9.8.2/24')
-    AVAHI_IF_NAME = 'pseudoethernet0'
-    TCPDUMP_FILE_PATH = '/tmp/peerd_dump.pcap'
-    AVAHI_CONFIG_FILE = 'etc/avahi/avahi-daemon.conf'
-    AVAHI_CONFIGS = {
-        AVAHI_CONFIG_FILE :
-            '[server]\n'
-                'host-name-from-machine-id=yes\n'
-                'browse-domains=\n'
-                'use-ipv4=yes\n'
-                'use-ipv6=no\n'
-                'ratelimit-interval-usec=1000000\n'
-                'ratelimit-burst=1000\n'
-            '[wide-area]\n'
-                'enable-wide-area=no\n'
-            '[publish]\n'
-                'publish-hinfo=no\n'
-                'publish-workstation=no\n'
-                'publish-aaaa-on-ipv4=no\n'
-                'publish-a-on-ipv6=no\n'
-            '[rlimits]\n'
-                'rlimit-core=0\n'
-                'rlimit-data=4194304\n'
-                'rlimit-fsize=1024\n'
-                'rlimit-nofile=768\n'
-                'rlimit-stack=4194304\n'
-                'rlimit-nproc=10\n',
-
-        'etc/passwd' :
-            'root:x:0:0:root:/root:/bin/bash\n'
-            'avahi:*:238:238::/dev/null:/bin/false\n',
-
-        'etc/group' :
-            'avahi:x:238:\n',
-    }
-    AVAHI_LOG_FILE = '/var/log/avahi.log'
-    AVAHI_PID_FILE = 'var/run/avahi-daemon/pid'
-    AVAHI_UP_TIMEOUT_SECONDS = 10
     FAKE_HOST_HOSTNAME = 'test-host'
-    POLLING_PERIOD_SECONDS = 0.2
     TEST_TIMEOUT_SECONDS = 30
     PEERD_SERVICE_ID = 'test-service-0'
     PEERD_SERVICE_INFO = {'some_data': 'a value',
                           'other_data': 'another value'}
-    QUERY_INTERVAL_SECONDS = 3.0
 
 
     def initialize(self):
-        self._services = None
-        self._vif = None
-        self._tcpdump = None
-        self._chroot = None
+        # Make sure these are initiallized to None in case we throw
+        # during self.initialize().
+        self._chrooted_avahi = None
         self._peerd = None
-        self._avahi_proxy = None
         self._host = None
         self._zc_listener = None
-        # Prevent weird interactions between services which talk to Avahi.
-        # TODO(wiley) Does Chrome need to die here as well?
-        self._services = service_stopper.ServiceStopper(
-                self.SERVICES_TO_STOP)
-        self._services.stop_services()
-        # We don't want Avahi talking to the real world, so give it a nice
-        # fake interface to use.  We'll watch the other half of the pair.
-        self._vif = virtual_ethernet_pair.VirtualEthernetPair(
-                interface_name=self.MONITOR_IF_NAME,
-                peer_interface_name=self.AVAHI_IF_NAME,
-                interface_ip=self.MONITOR_IF_IP.netblock,
-                peer_interface_ip=self.AVAHI_IF_IP.netblock,
-                # Moving one end into the chroot causes errors.
-                ignore_shutdown_errors=True)
-        self._vif.setup()
-        if not self._vif.is_healthy:
-            raise error.TestError('Failed to setup virtual ethernet pair.')
-        # By default, take a packet capture of everything Avahi sends out.
-        self._tcpdump = tcpdump.Tcpdump(self.MONITOR_IF_NAME,
-                                        self.TCPDUMP_FILE_PATH)
-        # We're going to run Avahi in a network namespace to avoid interactions
-        # with the outside world.
-        self._chroot = network_chroot.NetworkChroot(self.AVAHI_IF_NAME,
-                                                    self.AVAHI_IF_IP.addr,
-                                                    self.AVAHI_IF_IP.prefix_len)
-        self._chroot.add_config_templates(self.AVAHI_CONFIGS)
-        self._chroot.add_root_directories(['etc/avahi', 'etc/avahi/services'])
-        self._chroot.add_copied_config_files(['etc/resolv.conf',
-                                              'etc/avahi/hosts'])
-        self._chroot.add_startup_command(
-                '/usr/sbin/avahi-daemon --file=/%s &> %s' %
-                (self.AVAHI_CONFIG_FILE, self.AVAHI_LOG_FILE))
-        self._chroot.bridge_dbus_namespaces()
-        self._chroot.startup()
+        self._chrooted_avahi = chrooted_avahi.ChrootedAvahi()
+        self._chrooted_avahi.start()
         # Start up a cleaned up peerd with really verbose logging.
         self._peerd = peerd_helper.make_helper(start_instance=True,
                                                verbosity_level=3)
-        # Wait for Avahi to come up, claim its DBus name, settle on a hostname.
-        start_time = time.time()
-        while time.time() - start_time < self.AVAHI_UP_TIMEOUT_SECONDS:
-            if avahi_utils.avahi_ping():
-                break
-            time.sleep(self.POLLING_PERIOD_SECONDS)
-        else:
-            raise error.TestFail('Avahi did not come up in time.')
-        self._avahi_hostname = avahi_utils.avahi_get_hostname()
-        self._dns_domain = avahi_utils.avahi_get_domain_name()
-        if not self._avahi_hostname or not self._dns_domain:
-            raise error.TestFail('Failed to get hostname/domain from avahi.')
         # Listen on our half of the interface pair for mDNS advertisements.
-        self._host = interface_host.InterfaceHost(self.MONITOR_IF_NAME)
+        self._host = interface_host.InterfaceHost(
+                self._chrooted_avahi.unchrooted_interface_name)
         self._zc_listener = zeroconf.ZeroconfDaemon(self._host,
                                                     self.FAKE_HOST_HOSTNAME)
+        # The queries for hostname/dns_domain are IPCs and therefor relatively
+        # expensive.  Do them just once.
+        hostname = self._chrooted_avahi.hostname
+        dns_domain = self._chrooted_avahi.dns_domain
+        if not hostname or not dns_domain:
+            raise error.TestFail('Failed to get hostname/domain from avahi.')
+        self._dns_domain = dns_domain
+        self._hostname = '%s.%s' % (hostname, dns_domain)
 
 
     def cleanup(self):
-        if self._host:
-            self._host.close()
-        if self._peerd:
-            self._peerd.close()
-        if self._chroot:
-            # TODO(wiley) This is sloppy.  Add a helper to move the logs over.
-            for line in self._chroot.get_log_contents().splitlines():
-                logging.debug(line)
-            self._chroot.kill_pid_file(self.AVAHI_PID_FILE)
-            self._chroot.shutdown()
-        if self._tcpdump:
-            self._tcpdump.stop()
-        if self._vif:
-            self._vif.teardown()
-        if self._services:
-            self._services.restore_services()
+        for obj in (self._chrooted_avahi,
+                    self._host,
+                    self._peerd):
+            if obj is not None:
+                obj.close()
 
 
     def _ask_for_record(self, record_name, record_type):
@@ -191,17 +92,15 @@ class peerd_AdvertiseServices(test.test):
         @return True if we have all expected records, False otherwise.
 
         """
-        hostname = '%s.%s' % (self._avahi_hostname, self._dns_domain)
-        logging.debug('Looking for records for %s.', hostname)
-        logging.error('WILEY: Have records: %r', self._zc_listener._peer_records)
+        logging.debug('Looking for records for %s.', self._hostname)
         # First, check that Avahi is doing the simple things and publishing
         # an A record.
-        records_A = self._ask_for_record(hostname, dpkt.dns.DNS_A)
+        records_A = self._ask_for_record(self._hostname, dpkt.dns.DNS_A)
         if not records_A:
             return False
-        if records_A[0].data != self.AVAHI_IF_IP.addr:
+        if records_A[0].data != self._chrooted_avahi.avahi_interface_addr:
             raise error.TestFail('Did not see expected A record with value %s',
-                                 self.AVAHI_IF_IP.addr)
+                                 self._chrooted_avahi.avahi_interface_addr)
         # If we can see Avahi publishing that it's there, check that it has
         # a PTR to the unique name of the interesting service.
         PTR_name = '_%s._tcp.%s' % (self.PEERD_SERVICE_ID, self._dns_domain)
@@ -215,7 +114,7 @@ class peerd_AdvertiseServices(test.test):
         # Check that SRV exists, and contains the expected hostname.
         if not records_SRV:
             return False
-        if records_SRV[0].data[0] != hostname:
+        if records_SRV[0].data[0] != self._hostname:
             raise error.TestFail('Unexpect SRV record: %r' % records_SRV[0])
         # TXT should exist.
         records_TXT = self._ask_for_record(TXT_name, dpkt.dns.DNS_TXT)
