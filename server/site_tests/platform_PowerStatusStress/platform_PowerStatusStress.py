a69b9ae88004487e219aa30486f72449d317bcc4
Kalin Stoyanov <kalin@chromium.org>
2014-10-02 15:34:11 -0700

    PowerStatusStress: Change verification and timing
    'Discharge' state is allowed at high % battery state when charging.
    Alow more(5 sec) wait after power is switched.
    
    BUG=419846
    TEST=Tested with boards on test rack
    
    Change-Id: I21d40f5c6673843d2c8de016e5f0d291fd967ab0
    Reviewed-on: https://chromium-review.googlesource.com/221312
    Reviewed-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: Kalin Stoyanov <kalin@chromium.org>
    Commit-Queue: Kalin Stoyanov <kalin@chromium.org>

--- server/site_tests/platform_PowerStatusStress/platform_PowerStatusStress.py
+++ server/site_tests/platform_PowerStatusStress/platform_PowerStatusStress.py
@@ -8,12 +8,19 @@ from autotest_lib.client.common_lib import error
 
 _CHARGING = 'CHARGING'
 _DISCHARGING = 'DISCHARGING'
-_WAIT_SECS_AFTER_SWITCH = 2
+_WAIT_SECS_AFTER_SWITCH = 5
+_RESUME_TIMEOUT = 10
+
 
 class platform_PowerStatusStress(test.test):
     version = 1
 
     def do_suspend_resume(self, suspend_time):
+        """ Suspends the DUT through powerd_dbus_suspend
+
+        @param suspend_time: time in sec to suspend device for
+
+        """
         logging.info('Suspending for %s sec' % suspend_time)
         self.host.run('echo 0 > /sys/class/rtc/rtc0/wakealarm')
         self.host.run('echo +%d > /sys/class/rtc/rtc0/wakealarm' %
@@ -22,11 +29,32 @@ class platform_PowerStatusStress(test.test):
 
 
     def cleanup(self):
-        # Finish as powered on
+        """ Finish as powered on """
         self.host.power_on()
 
 
+    def wait_to_resume(self, resume_timeout):
+        """Wait for DUT to resume.
+
+        @param resume_timeout: Time in seconds to wait for resuming
+
+        @exception TestFail  if fail to resume in time
+        """
+        if not self.host.wait_up(timeout=resume_timeout):
+            raise error.TestFail('Failed to RESUME within timeout!')
+        logging.info('DUT resumed!')
+
+
     def switch_power_and_verify(self, powered_on, expected):
+        """ Main action on switching the power state, and verifying status
+
+        @param powered_on: a boolean ON if True, OFF else
+        @param expected: touple of cmd and values to verify
+
+        @exception TestFail  if line_power or battery state do not match
+        """
+        bat_state = _CHARGING if powered_on else _DISCHARGING,
+        logging.info('Switching status to %s ' % bat_state)
         if powered_on:
             self.host.power_on()
         else:
@@ -47,9 +75,7 @@ class platform_PowerStatusStress(test.test):
                                     psi_output) is not None
 
         if not all([is_psi_online, is_psi_enum_type, is_psi_bat_state]):
-            raise error.TestFail('Bad %s state: %s' %
-                (_CHARGING if powered_on else _DISCHARGING,
-                 psi_output))
+            raise error.TestFail('Bad %s state!' % bat_state)
 
 
     def run_once(self, host, loop_count, suspend_time):
@@ -66,12 +92,12 @@ class platform_PowerStatusStress(test.test):
             logging.info('Iteration %d' % (i + 1))
 
             # Suspend/resume
-            if suspend_time != -1:
+            if suspend_time > 0:
                 self.do_suspend_resume(suspend_time)
-                time.sleep(1)
+                self.wait_to_resume(_RESUME_TIMEOUT)
 
-            # Charging state
-            expected = ('yes', 'AC', '(Charging|Fully charged)')
+            # Charging state - it could be any of the three below
+            expected = ('yes', 'AC', '(Charging|Fully charged|Discharging)')
             self.switch_power_and_verify(True, expected)
 
             # Discharging state
