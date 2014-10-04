92b648c5cb509c879f52e79193325bf6e1210e2b
Rohit Makasana <rohitbm@chromium.org>
2014-10-03 18:04:56 -0700

    Enable missing telemetry benchmark tests on ChromeOS
    This CL also enables running telemetry benchmarks locally and per build.
    
    TEST=None
    BUG=None
    
    Change-Id: Ifb3ec938bdf6080b16c97bb09c7f26006c7e92ce
    Reviewed-on: https://chromium-review.googlesource.com/221506
    Reviewed-by: Ilja Friedel <ihf@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@google.com>
    Reviewed-by: Rohit Makasana <rohitbm@chromium.org>
    Commit-Queue: Rohit Makasana <rohitbm@chromium.org>
    Tested-by: Rohit Makasana <rohitbm@chromium.org>

--- server/site_tests/telemetry_Benchmarks/telemetry_Benchmarks.py
+++ server/site_tests/telemetry_Benchmarks/telemetry_Benchmarks.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 from autotest_lib.server import test
 from autotest_lib.server.cros import telemetry_runner
 
@@ -10,11 +11,12 @@ class telemetry_Benchmarks(test.test):
     version = 1
 
 
-    def run_once(self, host=None, benchmark=None):
+    def run_once(self, host=None, benchmark=None, args={}):
         """Run a telemetry benchmark.
 
-        @param host: host we are running telemetry on.
-        @param benchmark: benchmark we want to run.
+        @param host: hostname(ip address) to run the telemetry benchmark on.
+        @param benchmark: telemetry benchmark test to run.
         """
-        telemetry = telemetry_runner.TelemetryRunner(host)
+        local = args.get("local") == "True"
+        telemetry = telemetry_runner.TelemetryRunner(host, local)
         telemetry.run_telemetry_benchmark(benchmark, perf_value_writer=self)
