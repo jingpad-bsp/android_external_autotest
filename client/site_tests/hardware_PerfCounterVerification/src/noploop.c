9123ae6ad0d521f042f174f91576ae16b6ce87e4
David Sharp <dhsharp@chromium.org>
2014-10-06 17:50:21 -0700

    hardware_PerfCounterVerification: Find the branch instruction in noploop
    Since the toolchain is not available on the test machine, we must find the
    address of theloop  branch instruction and the target address at build
    time.
    
    Label the loop with inline assembly. In a python script, use 'nm' to get
    the address range of the loop, and use 'objdump -d' to disassemble it.
    Parse the disasembly and write the branch source/target.
    
    Currently can find branches in x86 and arm disassembly
    
    TEST=used in an upcoming change
    BUG=None
    
    Change-Id: I97097524118753424fee13fde2bdef5a645a15ce
    Reviewed-on: https://chromium-review.googlesource.com/226710
    Reviewed-by: David Sharp <dhsharp@chromium.org>
    Commit-Queue: David Sharp <dhsharp@chromium.org>
    Tested-by: David Sharp <dhsharp@chromium.org>

--- client/site_tests/hardware_PerfCounterVerification/src/noploop.c
+++ client/site_tests/hardware_PerfCounterVerification/src/noploop.c
@@ -9,7 +9,12 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  while (--loops) { /* nop */ }
+  asm (".local the_loop_start\nthe_loop_start:\n\t");
+  while (--loops) {
+    /* nop */
+    asm (".local the_loop_body\nthe_loop_body:\n\t");
+  }
+  asm (".local the_loop_end\nthe_loop_end:\n\t");
 
   return 0;
 }
