d9e0e70b7f27b58df8818e2d98064d7246276850
Thieu Le <thieule@chromium.org>
2014-10-16 18:06:31 -0700

    cellular: Migrate cellular_OutOfCreditsSubscriptionState to test_env
    BUG=chromium:415293
    TEST=cellular_OutOfCreditsSubscriptionState
    
    Change-Id: I25eaad6284e05ee72055a97aa9e7edae791d554b
    Reviewed-on: https://chromium-review.googlesource.com/223852
    Reviewed-by: Prathmesh Prabhu <pprabhu@chromium.org>
    Tested-by: Thieu Le <thieule@chromium.org>
    Commit-Queue: Thieu Le <thieule@chromium.org>

--- client/site_tests/cellular_OutOfCreditsSubscriptionState/cellular_OutOfCreditsSubscriptionState.py
+++ client/site_tests/cellular_OutOfCreditsSubscriptionState/cellular_OutOfCreditsSubscriptionState.py
@@ -9,9 +9,10 @@ from autotest_lib.client.bin import utils
 from autotest_lib.client.common_lib import error
 
 from autotest_lib.client.cros.cellular import mm1_constants
-from autotest_lib.client.cros.cellular.pseudomodem import pseudomodem_context
+from autotest_lib.client.cros.cellular import test_environment
 from autotest_lib.client.cros.networking import cellular_proxy
 from autotest_lib.client.cros.networking import pm_proxy
+from autotest_lib.client.cros.networking import shill_context
 
 SHORT_TIMEOUT = 10
 
@@ -27,17 +28,17 @@ class cellular_OutOfCreditsSubscriptionState(test.test):
     def _initialize_modem(self, subscription_state):
         # Simulate an Altair 3100 modem since that modem supports subscription
         # state information.
-        self.shill.disable_modem_for_test_setup()
+        self.test_env.shill.disable_modem_for_test_setup()
         # TODO(thieule): Set the modem model using the pseudomodem testing
         # interface (crbug.com/343258).
-        self.pseudomm.get_modem().iface_properties.Set(
+        self.modem.iface_properties.Set(
                 mm1_constants.I_MODEM,
                 mm1_constants.MM_MODEM_PROPERTY_NAME_PLUGIN,
                 'Altair LTE')
         self.pseudomm.iface_testing.SetSubscriptionState(
                 mm1_constants.MM_MODEM_3GPP_SUBSCRIPTION_STATE_UNKNOWN,
                 subscription_state)
-        self.shill.manager.EnableTechnology(
+        self.test_env.shill.manager.EnableTechnology(
                 cellular_proxy.CellularProxy.TECHNOLOGY_CELLULAR)
         # Wait for a registered state.
         self.modem.wait_for_states([mm1_constants.MM_MODEM_STATE_REGISTERED,
@@ -55,7 +56,8 @@ class cellular_OutOfCreditsSubscriptionState(test.test):
         self._initialize_modem(
                 mm1_constants.MM_MODEM_3GPP_SUBSCRIPTION_STATE_PROVISIONED)
         logging.info('Verify out-of-credits is not set in cellular service')
-        cellular_service = self.shill.wait_for_cellular_service_object()
+        cellular_service = \
+                self.test_env.shill.wait_for_cellular_service_object()
         if self._is_out_of_credits(cellular_service):
             error_msg = 'Service marked as out-of-credits when it ' \
                         'should not be.'
@@ -68,7 +70,8 @@ class cellular_OutOfCreditsSubscriptionState(test.test):
         self._initialize_modem(
                 mm1_constants.MM_MODEM_3GPP_SUBSCRIPTION_STATE_OUT_OF_DATA)
         logging.info('Verify out-of-credits is set in cellular service')
-        cellular_service = self.shill.wait_for_cellular_service_object()
+        cellular_service = \
+                self.test_env.shill.wait_for_cellular_service_object()
         if not self._is_out_of_credits(cellular_service):
             error_msg = 'Service not marked out-of-credits when it ' \
                         'should be.'
@@ -80,7 +83,8 @@ class cellular_OutOfCreditsSubscriptionState(test.test):
         logging.info('Initialize modem with provisioned state')
         self._initialize_modem(
                 mm1_constants.MM_MODEM_3GPP_SUBSCRIPTION_STATE_PROVISIONED)
-        cellular_service = self.shill.wait_for_cellular_service_object()
+        cellular_service = \
+                self.test_env.shill.wait_for_cellular_service_object()
         logging.info('Mark modem as out-of-credits')
         self.pseudomm.iface_testing.SetSubscriptionState(
                 mm1_constants.MM_MODEM_3GPP_SUBSCRIPTION_STATE_UNKNOWN,
@@ -99,10 +103,10 @@ class cellular_OutOfCreditsSubscriptionState(test.test):
 
     def run_once(self):
         """Calls by autotest to run this test."""
-        with pseudomodem_context.PseudoModemManagerContext(
-                True, {'family': '3GPP'}):
-            self.shill = cellular_proxy.CellularProxy.get_proxy()
-            self.shill.set_logging_for_cellular_test()
+        self.test_env = test_environment.CellularPseudoMMTestEnvironment(
+                pseudomm_args=({'family': '3GPP'},))
+        with self.test_env, shill_context.ServiceAutoConnectContext(
+                self.test_env.shill.find_cellular_service_object, False):
             self.pseudomm = pm_proxy.PseudoMMProxy.get_proxy()
             self.modem = self.pseudomm.get_modem()
 
