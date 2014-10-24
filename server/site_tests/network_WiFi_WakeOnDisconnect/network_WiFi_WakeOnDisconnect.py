f492f96927d1d8b843094f1bcef2fda805148940
Eric Caruso <ejcaruso@chromium.org>
2014-10-24 12:22:09 -0700

    autotest: Add autotest for wake on disconnect
    Wake-on-disconnect is a feature where we can suspend while leaving
    the NIC in such a state that it will wake us up when we disconnect
    from whatever WAP we were connected to. As part of Lucid Sleep, we
    should make sure we can program the NIC correctly to do this.
    
    BUG=chromium:426222,chromium:410450
    TEST=test_that with panther AP
    
    Signed-off-by: Eric Caruso <ejcaruso@chromium.org>
    Change-Id: Id84144e5f81d1b5184358dd78a3b7069058e8a0b
    Reviewed-on: https://chromium-review.googlesource.com/229942
    Reviewed-by: Christopher Wiley <wiley@chromium.org>

---
+++ server/site_tests/network_WiFi_WakeOnDisconnect/network_WiFi_WakeOnDisconnect.py
@@ -0,0 +1,55 @@
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import time
+
+from autotest_lib.client.common_lib.cros.network import xmlrpc_datatypes
+from autotest_lib.client.common_lib import error
+from autotest_lib.server.cros.network import hostap_config
+from autotest_lib.server.cros.network import wifi_cell_test_base
+from autotest_lib.server.cros.network import wifi_client
+
+SUSPEND_WAIT_TIME=10
+RESUME_WAIT_TIME=10
+
+
+class network_WiFi_WakeOnDisconnect(wifi_cell_test_base.WiFiCellTestBase):
+    """Test that WiFi disconnect wakes up the system."""
+
+    version = 1
+
+    def run_once(self):
+        """Body of the test."""
+        self.context.configure(hostap_config.HostapConfig(channel=1))
+        assoc_params = xmlrpc_datatypes.AssociationParameters(
+                ssid=self.context.router.get_ssid())
+        self.context.assert_connect_wifi(assoc_params)
+
+        client = self.context.client
+        router = self.context.router
+
+        # ask shill to set up wake-on-ssid
+        with client.wake_on_wifi_features(wifi_client.WAKE_ON_WIFI_SSID):
+            logging.info('Set up WoWLAN')
+
+            client.do_suspend_bg(SUSPEND_WAIT_TIME + RESUME_WAIT_TIME + 10)
+            time.sleep(SUSPEND_WAIT_TIME)
+
+            # kick over the router
+            router.deconfig_aps(silent=True)
+
+            # The DUT should wake up soon, but we'll give it a bit of a
+            # grace period.
+            if not client.host.wait_up(timeout=RESUME_WAIT_TIME):
+                raise error.TestFail('Client failed to wake up.')
+
+            logging.info('Client woke up successfully.')
+
+
+    def cleanup(self):
+        # make sure the DUT is up on the way out
+        self.context.client.host.servo.ctrl_key()
+        # make sure we clean up everything
+        super(network_WiFi_WakeOnDisconnect, self).cleanup()
