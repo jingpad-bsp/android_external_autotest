aa6b2e842f6038171c54acba95e0b6922dad86c4
Jorge Lucangeli Obes <jorgelo@chromium.org>
2014-10-24 17:13:17 -0700

    security_RestartJob: clean up error messages, style nits.
    BUG=None
    TEST=Still passes.
    
    Change-Id: Ia0cf63b7a370ac6980070fcaaa5054b5b357eaf9
    Reviewed-on: https://chromium-review.googlesource.com/225683
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Tested-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Chris Masone <cmasone@chromium.org>

--- client/site_tests/security_RestartJob/security_RestartJob.py
+++ client/site_tests/security_RestartJob/security_RestartJob.py
@@ -10,17 +10,19 @@ from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import constants, cros_ui, login
 
+
 class security_RestartJob(test.test):
     version = 1
     _FLAGFILE = '/tmp/security_RestartJob_regression'
+
 
     def _ps(self, proc=constants.BROWSER):
         pscmd = 'ps -C %s -o pid --no-header | head -1' % proc
         return utils.system_output(pscmd)
 
+
     def run_once(self):
-        """
-        Verifies that RestartJob cannot be abused to exec
+        """Verifies that RestartJob cannot be abused to execute
         arbitrary processes.
         """
         login.wait_for_browser()
@@ -28,29 +30,30 @@ class security_RestartJob(test.test):
         proxy = bus.get_object('org.chromium.SessionManager',
                                '/org/chromium/SessionManager')
         sessionmanager = dbus.Interface(proxy,
-                           'org.chromium.SessionManagerInterface')
+                                        'org.chromium.SessionManagerInterface')
 
         # We can't just start our own sacrificial process to let
-        # Session Manager kill, because it knows which processes it's
-        # managing... So we have to locate the pid of chrome.
+        # 'session_manager' kill, because it knows which processes it's
+        # managing, so we have to locate the pid of Chrome.
         pid = int(self._ps())
 
-        # Craft a malicious replacement for the target process
+        # Craft a malicious replacement for the target process.
         cmd = 'touch %s' % self._FLAGFILE
 
-        # Try to get our malicious replacement to run
-        logging.info('Calling RestartJob(%s,\'%s\')' % (pid,cmd))
+        # Try to get our malicious replacement to run.
+        logging.info('Calling RestartJob(%s, \'%s\')', pid, cmd)
         testfail = False
         try:
             if sessionmanager.RestartJob(pid, cmd):
-                raise error.TestFail('RestartJob regression, see bug 10877')
-        except dbus.DBusException, e:
+                raise error.TestFail(
+                        'RestartJob regression, see crbug.com/193322')
+        except dbus.DBusException:
             pass
+
         testfail = os.path.exists(self._FLAGFILE)
 
-        # Clean up, before we throw our TestFail, since this test
-        # killed chrome and mangled its argv...
+        # Clean up before we throw TestFail, since this test killed Chrome.
         cros_ui.nuke()
 
         if testfail:
-            raise error.TestFail('RestartJob regression, see cros bug 7018')
+            raise error.TestFail('RestartJob regression, see crbug.com/189233')
