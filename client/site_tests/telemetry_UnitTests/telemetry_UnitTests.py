2a6c1d539cc0baf87ae162f04b62cf32d3b4b710
Achuith Bhandarkar <achuith@chromium.org>
2014-11-12 17:57:19 -0800

    Fix telemetry unittest_util build break.
    BUG=chromium:432228
    TEST=build
    
    Change-Id: I500363e6133ef51b81b6ecdffd48ca89c825c69f
    Reviewed-on: https://chromium-review.googlesource.com/229442
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Tested-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>

--- client/site_tests/telemetry_UnitTests/telemetry_UnitTests.py
+++ client/site_tests/telemetry_UnitTests/telemetry_UnitTests.py
@@ -5,7 +5,11 @@
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 
-from telemetry.unittest import run_chromeos_tests
+# TODO(achuith): Clean this up crbug.com/432228.
+try:
+    from telemetry.unittest import run_chromeos_tests
+except ImportError:
+    from telemetry.unittest_util import run_chromeos_tests
 
 
 class telemetry_UnitTests(test.test):
