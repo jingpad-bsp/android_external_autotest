56d76425460475ef20569fbc9d643366ce6707f6
Alex Vakulenko <avakulenko@chromium.org>
2014-11-13 09:46:32 -0800

    autotest: Add sanity test for privetd's web server
    Added a test to verify that the web server running on the device responds
    to simple HTTP requests at http://<ip>:8080/privet/ping URL and returns
    the expected payload. This verifies the web server operation as well as
    the firewall to make sure the TCP port is open and accessible from outside.
    
    BUG=chromium:432650
    TEST=test_that -b x86-alex --fast 100.96.48.222 privetd_WebServerSanity
    CQ-DEPEND=CL:229592
    
    Change-Id: Ie1e91f4e75f1159d3360ea9137b2e942cd44ba0e
    Reviewed-on: https://chromium-review.googlesource.com/229601
    Tested-by: Alex Vakulenko <avakulenko@chromium.org>
    Reviewed-by: Christopher Wiley <wiley@chromium.org>
    Commit-Queue: Alex Vakulenko <avakulenko@chromium.org>

---
+++ server/site_tests/privetd_WebServerSanity/privetd_WebServerSanity.py
@@ -0,0 +1,55 @@
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import time
+import urllib2
+
+from autotest_lib.server import test
+from autotest_lib.client.common_lib import error
+
+class privetd_WebServerSanity(test.test):
+    """Test that we can connect to privetd's web server and get a response
+    from a simple GET request."""
+    version = 1
+    HTTP_PORT = 8080
+
+    def fetch_url_with_retries(self, url, retry_count, delay):
+        """Sends a GET request to a web server at the given |url|.
+        If the request fails due to error 111:Connection refused, try it again
+        after |delay| seconds and repeat this to a max |retry_count|.
+        This is needed to make sure peerd has a chance to start up and start
+        responding to HTTP requests.
+
+        @param url: URL to send the request to.
+        @param retry_count: max request retry count.
+        @param delay: delay (in seconds) between retries.
+        @return The string content of the page requested at |url|.
+        """
+        while retry_count > 0:
+            try:
+                logging.info('Connecting to host: %s', url)
+                return urllib2.urlopen(url, timeout=5).read()
+            except urllib2.URLError, err:
+                retry_count -= 1
+                if (not str(err.reason).endswith('Connection refused') or
+                        retry_count < 1):
+                    raise
+                logging.warn('Failed to connect to host. Retrying...')
+                time.sleep(delay)
+
+    def warmup(self, host):
+        host.run('stop privetd', ignore_status=True)
+        host.run('start privetd PRIVETD_LOG_LEVEL=3 PRIVETD_PORT=%s '
+                 'PRIVETD_ENABLE_PING=true' % self.HTTP_PORT)
+
+    def cleanup(self, host):
+        host.run('stop privetd')
+        host.run('start privetd')
+
+    def run_once(self, host):
+        url = 'http://%s:%s/privet/ping' % (host.hostname, self.HTTP_PORT)
+        content = self.fetch_url_with_retries(url, 5, 0.1)
+        if content != 'Hello, world!':
+            raise error.TestFail('Unexpected response from web server.')
