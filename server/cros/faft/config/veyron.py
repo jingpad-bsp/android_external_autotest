0313339e1765ad964544232d64c1657c34cbce16
David Hendricks <dhendrix@chromium.org>
2014-11-17 17:28:57 -0800

    FAFT: add initial veyron_* board support
    This adds veyron and the current derivatives (pinky and jerry).
    
    BUG=chrome-os-partner:33407
    BRANCH=none
    TEST=tests run on pinky (successfully or not is another matter...)
    
    Change-Id: I3b7a64891a85842681a6ce8e8da917fd14072dc8
    Reviewed-on: https://chromium-review.googlesource.com/230330
    Reviewed-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Tested-by: David Hendricks <dhendrix@chromium.org>
    Commit-Queue: David Hendricks <dhendrix@chromium.org>

---
+++ server/cros/faft/config/veyron.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""FAFT config setting overrides for Veyron."""
+
+
+class Values(object):
+    """FAFT config values for Veyron."""
+    software_sync_update = 6
+    chrome_ec = True
+    ec_capability = ['battery', 'charging', 'keyboard', 'arm', 'lid']
+    ec_boot_to_console = 1.1
+    wp_voltage = 'off'
