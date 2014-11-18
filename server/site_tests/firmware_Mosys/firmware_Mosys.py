5f52930fc41c9d692a2c988bffb03fbdb3c3f0e9
Danny Chan <dchan@google.com>
2014-11-17 16:44:02 -0800

    Cleanup test execution and help command.
    - execute all command and report at the end of the run.
    - check for available command moved to init.
    
    BUG=None
    TEST=None
    
    Change-Id: Ia364d6b0f740b7d60ea170f8fe19fc99dd75de55
    Reviewed-on: https://chromium-review.googlesource.com/230285
    Tested-by: danny chan <dchan@chromium.org>
    Reviewed-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Commit-Queue: danny chan <dchan@chromium.org>

--- server/site_tests/firmware_Mosys/firmware_Mosys.py
+++ server/site_tests/firmware_Mosys/firmware_Mosys.py
@@ -30,6 +30,20 @@ class firmware_Mosys(FirmwareTest):
         dict_args = utils.args_to_dict(cmdline_args)
         super(firmware_Mosys, self).initialize(host, cmdline_args)
         self.setup_dev_mode(dev_mode)
+        # a list contain failed execution.
+        self.failed_command = []
+        # Get a list of available mosys commands.
+        lines = self.run_cmd('mosys help')
+        self.command_list = []
+        cmdlist_start = False
+        for line in lines:
+            if cmdlist_start:
+                cmdlst = re.split('\s+', line)
+                if len(cmdlst) > 2:
+                    self.command_list.append(cmdlst[1])
+            elif 'Commands:' in line:
+                cmdlist_start = True
+        logging.info('Availabe commands: %s', ' '.join(self.command_list))
 
     def run_cmd(self, command):
         """
@@ -44,14 +58,13 @@ class firmware_Mosys(FirmwareTest):
         logging.info('Output %s', output)
         return output
 
-    def check_ec_version(self, exp_ec_version):
+    def check_ec_version(self, command, exp_ec_version):
         """
         Compare output of 'ectool version' for the current firmware
         copy to exp_ec_version.
 
+        @param command: command string
         @param exp_ec_version: The exepected EC version string.
-        @returns True if EC version string match expected.
-        @raises error.TestError if failed to locate pattern.
 
         """
         lines = self.run_cmd('ectool version')
@@ -65,27 +78,27 @@ class firmware_Mosys(FirmwareTest):
             fwcopy_matched = fwcopy_pattern.match(line)
             if fwcopy_matched:
                 fwcopy = fwcopy_matched.group(1)
-        try:
+        if fwcopy in version:
             actual_version = version[fwcopy]
-        except:
-            raise error.TestError('Failed to locate version from ectool:\n%s' %
-                                  '\n'.join(lines))
+        else:
+            self._tag_failure(command)
+            logging.error('Failed to locate version from ectool')
         logging.info('Expected ec version %s actual_version %s',
                      exp_ec_version, actual_version)
-        return exp_ec_version == actual_version
+        if exp_ec_version != actual_version:
+          self._tag_failure(command)
 
-    def check_lsb_info(self, fieldname, exp_value):
+    def check_lsb_info(self, command, fieldname, exp_value):
         """
         Comapre output of fieldname in /etc/lsb-release to exp_value.
 
+        @param command: command string
         @param fieldname: field name in lsd-release file.
         @param exp_value: expected value for fieldname
-        @returns True if exp_value is the same as value in lsb-release file.
-        @raises error.TestError if failed to locate pattern.
 
         """
-        command = 'cat /etc/lsb-release'
-        lines = self.run_cmd(command)
+        lsb_info = 'cat /etc/lsb-release'
+        lines = self.run_cmd(lsb_info)
         pattern = re.compile(fieldname + '=(.*)$')
         for line in lines:
             matched = pattern.match(line)
@@ -94,73 +107,71 @@ class firmware_Mosys(FirmwareTest):
                 logging.info('Expected %s %s actual %s',
                              fieldname, exp_value, actual)
                 # Some board will have prefix.  Example nyan_big for big.
-                return exp_value.lower() in actual.lower()
-        raise error.TestError('Failed to locate field %s from %s\n%s' %
-                              fieldname, command, '\n'.join(lines))
+                if exp_value.lower() in actual.lower():
+                  return
+        self._tag_failure(command)
+
+    def _tag_failure(self, cmd):
+        self.failed_command.append(cmd)
+        logging.error('Execute %s failed', cmd)
 
     def run_once(self, dev_mode=False):
-        # Get a list of available mosys commands.
-        command = 'mosys help'
-        lines = self.run_cmd(command)[0]
-        command_list = []
-        cmdlist_start = False
-        for line in lines:
-            if cmdlist_start:
-                cmdlst = re.split('\s+', line)
-                if len(cmdlst) > 2:
-                    command_list.append(cmdlst[1])
-            elif 'Commands:' in line:
-                cmdlist_start = True
-
-        # Test case: Mosys Commands
         # a. mosys -k smbios info bios
-        if 'smbios' in command_list:
-            command = 'mosys -k smbios info bios'
+        command = 'mosys -k smbios info bios'
+        if 'smbios' in self.command_list:
             output = self.run_cmd(command)[0]
             p = re.compile('vendor="coreboot" version="(.*)"'
                            ' release_date="[/0-9]+" size="[0-9]+ KB"')
             v = p.match(output)
             if not v:
-              raise error.TestFail('execute %s failed' % command)
+              self._tag_failure(command)
             version = v.group(1)
-            self.check_state((self.checkers.crossystem_checker,
-                             {'fwid': version}))
+            if not self.checkers.crossystem_checker({'fwid': version}):
+              self._tag_failure(command)
+        else:
+            logging.warning('Skip "%s", command not available.', command)
 
         # b. mosys -k ec info
+        command = 'mosys -k ec info'
         if self.faft_config.chrome_ec:
-          command = 'mosys -k ec info'
           output = self.run_cmd(command)[0]
           p = re.compile('vendor="[a-z]+" name="[ -~]+" fw_version="(.*)"')
           v = p.match(output)
           if not v:
-            raise error.TestFail('execute %s failed' % command)
+            self._tag_failure(command)
           version = v.group(1)
-          self.check_state((self.check_ec_version, version))
+          self.check_ec_version(command, version)
+        else:
+          logging.info('Skip "%s", command not available.', command)
 
         # c. mosys platform name
-        output = self.run_cmd('mosys platform name')[0]
-        self.check_state((self.check_lsb_info, ('CHROMEOS_RELEASE_BOARD',
-                                                output)))
+        command = 'mosys platform name'
+        output = self.run_cmd(command)[0]
+        self.check_lsb_info(command, 'CHROMEOS_RELEASE_BOARD', output)
 
         # d. mosys eeprom map
         command = "mosys eeprom map|egrep 'RW_SHARED|RW_SECTION_[AB]'"
         lines = self.run_cmd(command)
         if len(lines) != 3:
-            raise error.TestFail('Expect RW_SHARED|RW_SECTION_[AB] got "%s"' % lines)
+          logging.error('Expect RW_SHARED|RW_SECTION_[AB] got "%s"', lines)
+          self._tag_failure(command)
         emap = {'RW_SECTION_A': 0, 'RW_SECTION_B': 0, 'RW_SHARED': 0}
         for line in lines:
             row = line.split(' | ')
             if row[1] in emap:
                 emap[row[1]] += 1
             if row[2] == '0x00000000':
-                raise error.TestFail('Expect non zero but got %s instead(%s)' %
-                                     (row[2], line))
+                logging.error('Expect non zero but got %s instead(%s)',
+                              (row[2], line))
+                self._tag_failure(command)
             if row[3] == '0x00000000':
-                raise error.TestFail('Expect non zero but got %s instead(%s)' %
-                                     (row[3], line))
+                logging.error('Expect non zero but got %s instead(%s)',
+                              (row[3], line))
+                self._tag_failure(command)
         # Check that there are one A and one B.
         if emap['RW_SECTION_A'] != 1 or emap['RW_SECTION_B'] != 1:
-            raise error.TestFail('Missing RW_SECTION A or B, %s' % lines)
+            logging.error('Missing RW_SECTION A or B, %s', lines)
+            self._tag_failure(command)
 
         # e. mosys platform vendor
         # Output will be GOOGLE until launch, see crosbug/p/29755
@@ -168,5 +179,13 @@ class firmware_Mosys(FirmwareTest):
         output = self.run_cmd(command)[0]
         p = re.compile('^[-\w\s]+$')
         if not p.match(output):
-            raise error.TestFail('output is not a string Expect GOOGLE'
-                                 ' or name of maker.')
+            logging.error('output is not a string Expect GOOGLE'
+                          'or name of maker.')
+            self._tag_failure(command)
+
+        # empty failed_command indicate all passed.
+        if self.failed_command:
+          raise error.TestFail('%d commands failed, detail above.  '
+                               'Failed commands are "%s"' %
+                               (len(self.failed_command),
+                               ','.join(self.failed_command)))
