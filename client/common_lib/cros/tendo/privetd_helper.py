5240742bbdeeb9773968d2922a6b9e06abad1604
Christopher Wiley <wiley@chromium.org>
2014-12-05 12:12:33 -0800

    autotest: Add privetd_helper
    We can use this as a central place to write operations against privetd
    
    BUG=chromium:437640
    TEST=privetd_WebServerSanity continues to pass.
    
    Change-Id: Ica7482b53df917e71d4d27484919e17d3c04b482
    Reviewed-on: https://chromium-review.googlesource.com/233590
    Tested-by: Christopher Wiley <wiley@chromium.org>
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Commit-Queue: Christopher Wiley <wiley@chromium.org>

---
+++ client/common_lib/cros/tendo/privetd_helper.py
@@ -0,0 +1,100 @@
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import time
+import urllib2
+
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib import utils
+
+
+class PrivetdHelper(object):
+    """Delegate class containing logic useful with privetd."""
+    DEFAULT_HTTP_PORT = 8080
+    DEFAULT_HTTPS_PORT = 8081
+
+
+    def __init__(self, host=None):
+        self._host = None
+        self._run = utils.run
+        if host is not None:
+            self._host = host
+            self._run = host.run
+        self._http_port = self.DEFAULT_HTTP_PORT
+        self._https_port = self.DEFAULT_HTTPS_PORT
+
+
+    def _fetch_url_with_retries(self, url, retry_count, delay):
+        """Sends a GET request to a web server at the given |url|.
+        If the request fails due to error 111:Connection refused, try it again
+        after |delay| seconds and repeat this to a max |retry_count|.
+        This is needed to make sure peerd has a chance to start up and start
+        responding to HTTP requests.
+
+        @param url: URL to send the request to.
+        @param retry_count: max request retry count.
+        @param delay: delay (in seconds) between retries.
+        @return The string content of the page requested at |url|.
+
+        """
+        logging.debug('Requesting %s', url)
+        while retry_count > 0:
+            try:
+                logging.info('Connecting to host: %s', url)
+                return urllib2.urlopen(url, timeout=5).read()
+            except urllib2.URLError, err:
+                retry_count -= 1
+                if (not str(err.reason).endswith('Connection refused') or
+                        retry_count < 1):
+                    raise
+                logging.warn('Failed to connect to host. Retrying...')
+                time.sleep(delay)
+
+
+    def restart_privetd(self, log_verbosity=0, enable_ping=False,
+                        http_port=DEFAULT_HTTP_PORT,
+                        https_port=DEFAULT_HTTPS_PORT):
+        """Restart privetd in various configurations.
+
+        @param log_verbosity: integer verbosity level of log messages.
+        @param enable_ping: bool True if we should enable the ping URL
+                on the privetd web server.
+        @param http_port: integer port number for the privetd HTTP server.
+        @param https_port: integer port number for the privetd HTTPS server.
+
+        """
+        self._http_port = http_port
+        self._https_port = https_port
+        flag_list = []
+        flag_list.append('PRIVETD_LOG_LEVEL=%d' % log_verbosity)
+        flag_list.append('PRIVETD_HTTP_PORT=%d' % self._http_port)
+        flag_list.append('PRIVETD_HTTPS_PORT=%d' % self._https_port)
+        if enable_ping:
+            flag_list.append('PRIVETD_ENABLE_PING=true')
+        self._run('stop privetd', ignore_status=True)
+        self._run('start privetd %s' % ' '.join(flag_list))
+
+
+    def ping_server(self, use_https=False):
+        """Ping the privetd webserver.
+
+        Reuses port numbers from the last restart request.  The server
+        must have been restarted with enable_ping=True for this to work.
+
+        @param use_https: True if this request should be sent over HTTPS.
+
+        """
+        protocol = 'http'
+        port = self._http_port
+        if use_https:
+            protocol = 'https'
+            port = self._https_port
+        hostname = '127.0.0.1'
+        if self._host is not None:
+            hostname = self._host.hostname
+        url = '%s://%s:%s/privet/ping' % (protocol, hostname, port)
+        content = self._fetch_url_with_retries(url, 5, 0.1)
+        if content != 'Hello, world!':
+            raise error.TestFail('Unexpected response from web server.')
--- server/site_tests/privetd_WebServerSanity/privetd_WebServerSanity.py
+++ server/site_tests/privetd_WebServerSanity/privetd_WebServerSanity.py
@@ -2,66 +2,23 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging
-import time
-import urllib2
-
+from autotest_lib.client.common_lib.cros.tendo import privetd_helper
 from autotest_lib.server import test
-from autotest_lib.client.common_lib import error
 
 class privetd_WebServerSanity(test.test):
     """Test that we can connect to privetd's web server and get a response
     from a simple GET request."""
     version = 1
-    HTTP_PORT = 8080
-    HTTPS_PORT = 8081
-
-    def fetch_url_with_retries(self, url, retry_count, delay):
-        """Sends a GET request to a web server at the given |url|.
-        If the request fails due to error 111:Connection refused, try it again
-        after |delay| seconds and repeat this to a max |retry_count|.
-        This is needed to make sure peerd has a chance to start up and start
-        responding to HTTP requests.
-
-        @param url: URL to send the request to.
-        @param retry_count: max request retry count.
-        @param delay: delay (in seconds) between retries.
-        @return The string content of the page requested at |url|.
-        """
-        while retry_count > 0:
-            try:
-                logging.info('Connecting to host: %s', url)
-                return urllib2.urlopen(url, timeout=5).read()
-            except urllib2.URLError, err:
-                retry_count -= 1
-                if (not str(err.reason).endswith('Connection refused') or
-                        retry_count < 1):
-                    raise
-                logging.warn('Failed to connect to host. Retrying...')
-                time.sleep(delay)
-
-    def ping_server(self, proto, hostname, port):
-        """Tests an instance of a web server on particular port.
-
-        @param proto: protocol to use (http/https).
-        @param hostname: host name/IP address.
-        @param port: TCP port to use.
-        """
-        url = '%s://%s:%s/privet/ping' % (proto, hostname, port)
-        content = self.fetch_url_with_retries(url, 5, 0.1)
-        if content != 'Hello, world!':
-            raise error.TestFail('Unexpected response from web server.')
 
     def warmup(self, host):
-        host.run('stop privetd', ignore_status=True)
-        host.run('start privetd PRIVETD_LOG_LEVEL=3 '
-                 'PRIVETD_HTTP_PORT=%s PRIVETD_HTTPS_PORT=%s '
-                 'PRIVETD_ENABLE_PING=true' % (self.HTTP_PORT, self.HTTPS_PORT))
+        self.helper = privetd_helper.PrivetdHelper(host=host)
+        self.helper.restart_privetd(log_verbosity=3, enable_ping=True)
+
 
     def cleanup(self, host):
-        host.run('stop privetd')
-        host.run('start privetd')
+        self.helper.restart_privetd()
+
 
     def run_once(self, host):
-        self.ping_server("http", host.hostname, self.HTTP_PORT)
-        self.ping_server("https", host.hostname, self.HTTPS_PORT)
+        self.helper.ping_server()
+        self.helper.ping_server(use_https=True)
