6aafd21388d8c37e3660156aaddbeb22135c0875
Fang Deng <fdeng@chromium.org>
2014-12-09 23:27:20 +0000

    Revert "[autotest] higher_is_better in tko_iteration_perf_value allows null"
    This reverts commit 8386e7db1a9a60a4c1b7805b99f2662850a74a9c.
    
    There was a workaround here https://chromium-review.googlesource.com/#/c/234250/
    
    Hold back the db alteration for now.
    
    Change-Id: If3ed332ffc82e1e0c5a565fa26910a5139fa14cf
    Reviewed-on: https://chromium-review.googlesource.com/234260
    Reviewed-by: Dan Shi <dshi@chromium.org>
    Commit-Queue: Fang Deng <fdeng@chromium.org>
    Trybot-Ready: Fang Deng <fdeng@chromium.org>
    Tested-by: Fang Deng <fdeng@chromium.org>

--- frontend/migrations/097_tko_iteration_perf_value_higher_is_better_allows_null.py
+++
@@ -1,9 +0,0 @@
-UP_SQL = """
-ALTER TABLE tko_iteration_perf_value
-MODIFY higher_is_better BOOLEAN DEFAULT NULL;
-"""
-
-DOWN_SQL = """
-ALTER TABLE tko_iteration_perf_value
-MODIFY higher_is_better BOOLEAN NOT NULL DEFAULT TRUE;
-"""
