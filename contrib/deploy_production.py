3f2b660fd1505b10172247220d1bc95b3d03d3dd
Don Garrett <dgarrett@google.com>
2014-12-16 18:19:16 -0800

    deploy_production: Add support for listing specific servers.
    Add the ability to list exactly what servers to deploy too, instead of
    just using the update.
    
    BUG=chromium:443027
    TEST=Unitests, and --dryrun testing.
    
    Change-Id: I1d6b802fc21036cb474fcb2f1a6ca7b17605a988
    Reviewed-on: https://chromium-review.googlesource.com/236135
    Trybot-Ready: Don Garrett <dgarrett@chromium.org>
    Tested-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Prashanth B <beeps@chromium.org>
    Commit-Queue: Don Garrett <dgarrett@chromium.org>

--- contrib/deploy_production.py
+++ contrib/deploy_production.py
@@ -37,21 +37,50 @@ def parse_arguments(args):
     @returns An argparse.Namespace populated with argument values.
     """
     parser = argparse.ArgumentParser(
-            description='Command to update an entire autotest installation.')
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description='Command to update an entire autotest installation.',
+            epilog=('Update all servers:\n'
+                    '  deploy_production.py\n'
+                    '\n'
+                    'Update one server:\n'
+                    '  deploy_production.py <server>\n'
+                    '\n'
+                    'Send arguments to remote deploy_production_local.py:\n'
+                    '  deploy_production.py -- --dryrun\n'
+                    '\n'
+                    'See what arguments would be run on specified servers:\n'
+                    '  deploy_production.py --dryrun <server_a> <server_b> --'
+                    ' --skip-update\n'))
+
     parser.add_argument('--continue', action='store_true', dest='cont',
                         help='Continue to the next server on failure.')
     parser.add_argument('--dryrun', action='store_true',
                         help='Don\'t actually run remote commands.')
     parser.add_argument('args', nargs=argparse.REMAINDER,
-                        help=('deploy_production_local.py arguments'
-                              ' (use -- to separate).'))
+                        help=('<server>, <server> ... -- <remote_arg>, <remote_arg> ...'))
 
     results = parser.parse_args(args)
 
-    # If -- was used to pass arguments to the remote processes, we don't need to
-    # pass the -- along.
-    if results.args and results.args[0] == '--':
-        results.args.pop(0)
+    # We take the args list and further split it down. Everything before --
+    # is a server name, and everything after it is an argument to pass along
+    # to deploy_production_local.py.
+    #
+    # This:
+    #   server_a, server_b -- --dryrun --skip-report
+    #
+    # Becomes:
+    #   args.servers['server_a', 'server_b']
+    #   args.args['--dryrun', '--skip-report']
+    try:
+        local_args_index = results.args.index('--') + 1
+    except ValueError:
+        # If -- isn't present, they are all servers.
+        results.servers = results.args
+        results.args = []
+    else:
+        # Split arguments.
+        results.servers = results.args[:local_args_index-1]
+        results.args = results.args[local_args_index:]
 
     return results
 
@@ -65,18 +94,19 @@ def main(args):
     """
     options = parse_arguments(args)
 
-    print('Discover servers...')
-    servers = discover_servers()
-    print()
+    if not options.servers:
+        print('Discover servers...')
+        options.servers = discover_servers()
+        print()
 
     # Display what we plan to update.
     print('Will update (in this order):')
-    for server in servers:
+    for server in options.servers:
         print('  ', server)
     print()
 
     # Do the updating.
-    for server in servers:
+    for server in options.servers:
         cmd = ('/usr/local/autotest/contrib/deploy_production_local.py ' +
                ' '.join(options.args))
         print('%s: %s' % (server, cmd))
---
+++ contrib/deploy_production_unittest.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unittests for deploy_production_local.py."""
+
+from __future__ import print_function
+
+import unittest
+
+import deploy_production as deploy_production
+
+
+class TestDeployProduction(unittest.TestCase):
+    """Test deploy_production_local with commands mocked out."""
+
+    def test_parse_arguments(self):
+        """Test deploy_production_local.parse_arguments."""
+        # No arguments.
+        results = deploy_production.parse_arguments([])
+        self.assertEqual(
+                {'servers': [], 'args': [],
+                 'cont': False, 'dryrun': False},
+                vars(results))
+
+        # Dryrun, continue
+        results = deploy_production.parse_arguments(['--dryrun', '--continue'])
+        self.assertDictContainsSubset(
+                {'servers': [], 'args': [],
+                 'cont': True, 'dryrun': True},
+                vars(results))
+
+        # List some servers
+        results = deploy_production.parse_arguments(['foo', 'bar'])
+        self.assertDictContainsSubset(
+                {'servers': ['foo', 'bar'], 'args': [],
+                 'cont': False, 'dryrun': False},
+                vars(results))
+
+        # List some local args
+        results = deploy_production.parse_arguments(['--', 'foo', 'bar'])
+        self.assertDictContainsSubset(
+                {'servers': [], 'args': ['foo', 'bar'],
+                 'cont': False, 'dryrun': False},
+                vars(results))
+
+        # List everything.
+        results = deploy_production.parse_arguments(
+                ['--continue', '--dryrun', 'foo', 'bar', '--',
+                 '--actions-only', '--dryrun'])
+        self.assertDictContainsSubset(
+                {'servers': ['foo', 'bar'],
+                 'args': ['--actions-only', '--dryrun'],
+                 'cont': True, 'dryrun': True},
+                vars(results))
+
+
+if __name__ == '__main__':
+    unittest.main()
