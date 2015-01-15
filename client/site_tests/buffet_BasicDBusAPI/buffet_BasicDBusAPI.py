f38a76eee23d2b0c0634e231c5259f8db1c68b88
Christopher Wiley <wiley@chromium.org>
2015-01-14 16:54:19 -0800

    autotest: Fix buffet_BasicDBusAPI
    Our DBus API has apparently changed quite a bit since the last time that
    this test was run.  For now, remove all the calls that don't make sense
    any more, and just call a very basic DBus API.
    
    BUG=None
    TEST=This test now passes.
    
    Change-Id: I7f13ad491e14d04a2593101a5da2bc289d455744
    Reviewed-on: https://chromium-review.googlesource.com/240808
    Tested-by: Christopher Wiley <wiley@chromium.org>
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Commit-Queue: Alex Vakulenko <avakulenko@chromium.org>

--- client/site_tests/buffet_BasicDBusAPI/buffet_BasicDBusAPI.py
+++ client/site_tests/buffet_BasicDBusAPI/buffet_BasicDBusAPI.py
@@ -3,17 +3,12 @@
 # found in the LICENSE file.
 
 import dbus
-import json
-import logging
 
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 
 
 BUFFET_SERVICE_NAME = 'org.chromium.Buffet'
-
-BUFFET_ROOT_INTERFACE = 'org.chromium.Buffet'
-BUFFET_ROOT_OBJECT_PATH = '/org/chromium/Buffet'
 
 BUFFET_MANAGER_INTERFACE = 'org.chromium.Buffet.Manager'
 BUFFET_MANAGER_OBJECT_PATH = '/org/chromium/Buffet/Manager'
@@ -28,36 +23,12 @@ class buffet_BasicDBusAPI(test.test):
     def run_once(self):
         """Test entry point."""
         bus = dbus.SystemBus()
-        buffet_proxy = bus.get_object(
-                BUFFET_SERVICE_NAME, BUFFET_ROOT_OBJECT_PATH)
-        # The test method has no response.
-        buffet_proxy.TestMethod(dbus_interface=BUFFET_ROOT_INTERFACE)
         manager_proxy = bus.get_object(
                 BUFFET_SERVICE_NAME, BUFFET_MANAGER_OBJECT_PATH)
-        ticket_id = manager_proxy.RegisterDevice(
-                'client_id', 'client_secret', 'api_key',
-                dbus_interface=BUFFET_MANAGER_INTERFACE)
-        if not ticket_id:
-            raise error.TestFail('Manager.RegisterDevice should '
-                                 'return a ticket id.')
-
-        logging.info('Returned ticket id is %s.', ticket_id)
-
-        logging.info('Getting state via GetAll()')
-        properties = manager_proxy.GetAll(BUFFET_MANAGER_INTERFACE,
-                                          dbus_interface=dbus.PROPERTIES_IFACE)
-        if 'State' not in properties:
-            raise error.TestFail('Manager should have a State property.')
-
-        logging.info('Getting state via Get()')
-        state_property = manager_proxy.Get(BUFFET_MANAGER_INTERFACE, 'State',
-                                           dbus_interface=dbus.PROPERTIES_IFACE)
-        if state_property != properties['State']:
-            raise error.TestFail('State property from GetAll does not match '
-                                 'Get: (%s vs %s).' % (properties['State'],
-                                                       state_property))
-
-        logging.info('Updating state.')
-        manager_proxy.UpdateState(
-                json.dumps({TEST_STATE_KEY: TEST_STATE_VALUE}),
-                dbus_interface=BUFFET_MANAGER_INTERFACE)
+        test_message = 'Hello world!'
+        echoed_message = manager_proxy.TestMethod(
+                test_message, dbus_interface=BUFFET_MANAGER_INTERFACE)
+        if test_message != echoed_message:
+            raise error.TestFail('Expected Manager.TestMethod to return %s '
+                                 'but got %s instead.' % (test_message,
+                                                          echoed_message))
