0c0ab5ca13a1a37d458716e18b35020e6305759d
Frank Henigman <fjhenigman@chromium.org>
2015-01-21 10:37:42 -0500

    graphics_SyncControlTest: bring up to date
    The commands to start/stop the X server and the way the test result
    was being checked were out of date.  The latter was causing failure
    to be reported regardless of the actual test result.  Both are changed
    to match what other tests currently do, e.g.  graphics_Sanity and
    graphics_GLMark.
    Also skip this test on freon because the extension tested here
    (eglGetSyncValuesCHROMIUM) will not be used under freon.
    
    BUG=chromium:425225
    TEST=test_that {lumpy,link_freon} graphics_SyncControlTest
    
    Change-Id: I5af576f15c59b53870ecd55f084be5b1cb31964c
    Reviewed-on: https://chromium-review.googlesource.com/242240
    Reviewed-by: Ilja Friedel <ihf@chromium.org>
    Commit-Queue: Frank Henigman <fjhenigman@chromium.org>
    Tested-by: Frank Henigman <fjhenigman@chromium.org>

--- client/site_tests/graphics_SyncControlTest/graphics_SyncControlTest.py
+++ client/site_tests/graphics_SyncControlTest/graphics_SyncControlTest.py
@@ -18,27 +18,33 @@ class graphics_SyncControlTest(test.test):
         self.job.setup_dep(['glbench'])
 
     def run_once(self):
+        if utils.is_freon():
+            return
         dep = 'glbench'
         dep_dir = os.path.join(self.autodir, 'deps', dep)
         self.job.install_pkg(dep, 'dep', dep_dir)
 
+        # TODO: I bet this list of boards is incomplete.
+        #       Is there a better way to do this?
         hz = {'daisy' : 59.9}
 
         cmd = os.path.join(self.autodir, 'deps/glbench/synccontroltest')
         board = utils.get_current_board()
         if board in hz:
             cmd = cmd + " --vsync {0:.2f}".format(hz[board])
-        cmd = 'X :1 & sleep 1; DISPLAY=:1 %s; kill $!' % cmd
+        cmd = 'X :1 vt1 & sleep 1; chvt 1 && DISPLAY=:1 ' + cmd
 
         self._services = service_stopper.ServiceStopper(['ui'])
         self._services.stop_services()
 
-        # synccontroltest exits with a non zero status if a deviation above
-        # 200uS us is detected.
-        ret = utils.run(cmd, stderr_is_expected = False)
-        if ret != 0:
-            raise error.TestFail(
-                "Failed: graphics_SyncControlTest with {0}".format(ret))
+        try:
+            # synccontroltest exits with a non zero status if a deviation above
+            # 200uS us is detected.  utils.run will then raise an exception.
+            utils.run(cmd, stderr_is_expected = False)
+        finally:
+            # Just sending SIGTERM to X is not enough; we must wait for it to
+            # really die before we start a new X server (ie start ui).
+            utils.ensure_processes_are_dead_by_name('^X$')
 
     def cleanup(self):
         if hasattr(self, '_services') and self._services:
