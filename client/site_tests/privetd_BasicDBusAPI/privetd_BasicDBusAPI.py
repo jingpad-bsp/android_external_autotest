f20e050472deb2b21205676b7e48c2606a878b23
Christopher Wiley <wiley@chromium.org>
2015-01-26 16:28:55 -0800

    privetd: Add privetd helper
    This delegate object takes care of bringing up and tearing down
    instances of privetd.
    
    BUG=brillo:25
    TEST=rewritten privetd_BasicDBusAPI test still passes
    CQ-DEPEND=CL:243426, CL:243451
    
    Change-Id: I3cac737ac9c5e3a67bcee816bdb7d5eb7c73b07d
    Reviewed-on: https://chromium-review.googlesource.com/243427
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Tested-by: Christopher Wiley <wiley@chromium.org>
    Commit-Queue: Christopher Wiley <wiley@chromium.org>

--- client/site_tests/privetd_BasicDBusAPI/privetd_BasicDBusAPI.py
+++ client/site_tests/privetd_BasicDBusAPI/privetd_BasicDBusAPI.py
@@ -2,30 +2,44 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import dbus
-
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
-
-
-PRIVETD_SERVICE_NAME = 'org.chromium.privetd'
-
-PRIVETD_MANAGER_INTERFACE = 'org.chromium.privetd.Manager'
-PRIVETD_MANAGER_OBJECT_PATH = '/org/chromium/privetd/Manager'
+from autotest_lib.client.cros.tendo import privetd_helper
 
 class privetd_BasicDBusAPI(test.test):
     """Check that basic privetd daemon DBus APIs are functional."""
     version = 1
 
+    def initialize(self):
+        """Set up the objects we're going to use in the test."""
+        # We define all the objects we're going to clean up as None, because
+        # an exception part of the way through initializing will cause some
+        # of those objects to be undefined names.  This causes cleanup to
+        # fail with odd messages about "object self has no such field XXX".
+        self.privetd = None
+        self.privetd = privetd_helper.make_helper(
+                wifi_bootstrap_mode=privetd_helper.BOOTSTRAP_CONFIG_DISABLED,
+                gcd_bootstrap_mode=privetd_helper.BOOTSTRAP_CONFIG_DISABLED,
+                verbosity_level=3)
+
     def run_once(self):
         """Test entry point."""
-        bus = dbus.SystemBus()
-        manager_proxy = bus.get_object(
-                PRIVETD_SERVICE_NAME, PRIVETD_MANAGER_OBJECT_PATH)
-        test_message = 'Hello world!'
-        echoed_message = manager_proxy.Ping(
-                dbus_interface=PRIVETD_MANAGER_INTERFACE)
-        if test_message != echoed_message:
+        expected_response = 'Hello world!'
+        actual_response = self.privetd.manager.Ping()
+        if expected_response != actual_response:
             raise error.TestFail('Expected Manager.Ping to return %s '
-                                 'but got %s instead.' % (test_message,
-                                                          echoed_message))
+                                 'but got %s instead.' % (expected_response,
+                                                          actual_response))
+
+        # Initially, bootstrapping should be disabled entirely.
+        actual_state = self.privetd.wifi_bootstrap_status
+        if actual_state != privetd_helper.WIFI_BOOTSTRAP_STATE_DISABLED:
+            raise error.TestFail('Expected WiFi bootstrapping to be disabled, '
+                                 'but it was %r' % actual_state)
+
+
+    def clean(self):
+        """Clean up processes altered during the test."""
+        if self.privetd is not None:
+            self.privetd.close()
+
