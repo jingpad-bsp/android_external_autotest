669393837af4367f7f655d0b5bbf70915a6e1152
Ricky Zhou <rickyz@chromium.org>
2015-02-10 01:57:14 -0800

    Test that chrome thinks we are adequately sandboxed.
    This moves the decision making about whether the sandboxing is
    sufficient to chromium, which should be more future-proof.
    
    BUG=chromium:312380
    TEST=Tested on daisy with and without https://codereview.chromium.org/916153003/.
    
    Change-Id: If7034dd158d63564d5179c4e96be51812baf9058
    Reviewed-on: https://chromium-review.googlesource.com/248931
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Tested-by: Ricky Zhou <rickyz@chromium.org>
    Commit-Queue: Ricky Zhou <rickyz@chromium.org>
    Reviewed-by: Matthew Dempsky <mdempsky@chromium.org>

--- client/site_tests/security_SandboxStatus/security_SandboxStatus.py
+++ client/site_tests/security_SandboxStatus/security_SandboxStatus.py
@@ -9,11 +9,6 @@ from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib.cros import chrome
 from telemetry.core import exceptions
 
-SANDBOXES = [u'SUID Sandbox',
-             u'\xa0\xa0PID name ?spaces',
-             u'\xa0\xa0Network namespaces',
-             u'Seccomp-BPF sandbox']
-
 class security_SandboxStatus(test.test):
     """Verify sandbox status."""
     version = 1
@@ -27,42 +22,18 @@ class security_SandboxStatus(test.test):
         except exceptions.EvaluateException:
             return None
 
-    def _TableEntry(self, row, column):
-        '''Fetches table cell text content corresponding to row, column.'''
-
-        table_js = ("document.getElementsByTagName('table')[0]."
-                    "rows[%d].cells[%d].textContent" % (row, column))
-        return utils.poll_for_condition(
-                lambda: self._EvaluateJavaScript(table_js),
+    def _CheckAdequatelySandboxed(self):
+        '''Check that chrome:///sandbox shows "You are adequately sandboxed."'''
+        sandbox_good_js = "document.getElementsByTagName('p')[0].textContent"
+        sandbox_good = utils.poll_for_condition(
+                lambda: self._EvaluateJavaScript(sandbox_good_js),
                 exception=error.TestError(
                        'Failed to evaluate in chrome://sandbox "%s"'
-                        % table_js),
+                        % sandbox_good_js),
                 timeout=30)
-
-
-    def _CheckRowName(self, row, expected_name):
-        '''Ensures the name of the row is as we expect.'''
-
-        actual_name = self._TableEntry(row, 0)
-        if not re.match(expected_name, actual_name):
-            raise error.TestFail('Expected row %d to be "%s", found "%s"'
-                                 % (row, expected_name, actual_name))
-
-
-    def _CheckRowNames(self, expected_names):
-        for row in range(len(expected_names)):
-            self._CheckRowName(row, expected_names[row])
-
-
-    def _CheckRowValues(self, num_rows):
-        '''Ensures all sandboxes are on.'''
-
-        for row in range(num_rows):
-            value = self._TableEntry(row, 1)
-            if value != "Yes":
-                name = self._TableEntry(row, 0)
-                raise error.TestFail('"%s" enabled = "%s"' % (name, value))
-
+        if not re.match('You are adequately sandboxed.', sandbox_good):
+            raise error.TestFail('Could not find "You\'re adequately '
+                                 'sandboxed." in chrome://sandbox')
 
     def _CheckGPUCell(self, cell, content, error_msg):
         '''Checks the content of the cells in the GPU sandbox row.'''
@@ -80,13 +51,12 @@ class security_SandboxStatus(test.test):
         if res.find(content) == -1:
             raise error.TestFail(error_msg)
 
-
     def run_once(self):
+        '''Open various sandbox-related pages and test that we are sandboxed.'''
         with chrome.Chrome() as cr:
             self._tab = cr.browser.tabs[0]
             self._tab.Navigate('chrome://sandbox')
-            self._CheckRowNames(SANDBOXES)
-            self._CheckRowValues(len(SANDBOXES))
+            self._CheckAdequatelySandboxed()
 
             self._tab.Navigate('chrome://gpu')
             self._CheckGPUCell(0, 'Sandboxed',
