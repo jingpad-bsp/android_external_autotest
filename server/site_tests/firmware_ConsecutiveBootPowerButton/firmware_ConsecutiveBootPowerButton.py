04c6ff8a09acc514b12387ab234b3da69fe2dd45
Danny Chan <dchan@google.com>
2015-02-10 13:50:23 -0800

    Use shutdown to bring down the system.
    - Reboot sequence will turn off device via "/sbin/shutdown -P now" instead of
      doing power button long press.
    
    BUG=chrome-os-partner:36112
    TEST=Manually verify running on swanky and jerry.
    
    Change-Id: I9457c4566de6d84738a2d87904eb2a2572adad5e
    Reviewed-on: https://chromium-review.googlesource.com/248340
    Reviewed-by: danny chan <dchan@chromium.org>
    Tested-by: danny chan <dchan@chromium.org>
    Commit-Queue: danny chan <dchan@chromium.org>

--- server/site_tests/firmware_ConsecutiveBootPowerButton/firmware_ConsecutiveBootPowerButton.py
+++ server/site_tests/firmware_ConsecutiveBootPowerButton/firmware_ConsecutiveBootPowerButton.py
@@ -1,18 +1,16 @@
-# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 import logging
-import time
 
 from autotest_lib.client.common_lib import utils
 from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
 
 
-class firmware_ConsecutiveBoot(FirmwareTest):
+class firmware_ConsecutiveBootPowerButton(FirmwareTest):
     """
-    Servo based consecutive boot test via power button to turn on DUT and 
-    /sbin/shutdown command to turn off DUT.
+    Servo based consecutive boot test via power button for both on and off.
 
     This test is intended to be run with many iterations to ensure that the DUT
     does boot into Chrome OS and then does power off later.
@@ -26,30 +24,11 @@ class firmware_ConsecutiveBoot(FirmwareTest):
         # Parse arguments from command line
         dict_args = utils.args_to_dict(cmdline_args)
         self.faft_iterations = int(dict_args.get('faft_iterations', 1))
-        super(firmware_ConsecutiveBoot, self).initialize(host, cmdline_args)
+        super(firmware_ConsecutiveBootPowerButton,
+              self).initialize(host, cmdline_args)
         self.setup_dev_mode(dev_mode)
         self.setup_usbkey(usbkey=False)
 
-    def shutdown_power_on(self):
-        """
-        Use /sbin/shutdown to turn off device follow by power button press to
-        turn on device.  Do not want to call full_power_off_and_on since we
-        are testing firmware and mainly want to focus on power on sequence.
-        """
-        boot_id = self.get_bootid()
-        # Call shutdown instead of long press the power key since we are
-        # testing the firmware and not the OS.
-        logging.info("Sending /sbin/shutdown -P now")
-        self.faft_client.system.run_shell_command('/sbin/shutdown -P now')
-        logging.info('Wait for client to go offline')
-        self.wait_for_client_offline(timeout=100, orig_boot_id=boot_id)
-        # The system should be ready in a few seconds, sleep 5 to be sure.
-        logging.info('sleep 5')
-        time.sleep(5)
-        logging.info("Tap power key to boot.")
-        self.servo.power_short_press()
-        logging.info('wait_for_client online')
-        self.wait_for_client()
 
     def run_once(self, dev_mode=False):
         for i in xrange(self.faft_iterations):
@@ -59,7 +38,8 @@ class firmware_ConsecutiveBoot(FirmwareTest):
             self.check_state((self.checkers.crossystem_checker, {
                         'mainfw_type': 'developer' if dev_mode else 'normal',
                         }))
-            self.shutdown_power_on()
+            self.full_power_off_and_on()
+            self.wait_for_client()
 
             logging.info("Expected boot fine.")
             self.check_state((self.checkers.crossystem_checker, {
