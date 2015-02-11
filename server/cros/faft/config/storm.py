19388f1cf77f1b71945d33c5fe2f48b241ee89c5
David Hendricks <dhendrix@chromium.org>
2015-02-10 17:58:29 -0800

    storm: Add initial FAFT config
    BUG=chrome-os-partner:34513
    BRANCH=none
    TEST=none
    
    Change-Id: I4d6c2005ef990bf67869cf31d093e7b3943ef56b
    Reviewed-on: https://chromium-review.googlesource.com/248402
    Reviewed-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Commit-Queue: David Hendricks <dhendrix@chromium.org>
    Tested-by: David Hendricks <dhendrix@chromium.org>

---
+++ server/cros/faft/config/storm.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""FAFT configuration overrides for Storm."""
+
+
+class Values(object):
+    """FAFT config values for Storm."""
+    has_lid = False
+    has_keyboard = False
+    keyboard_dev = False
+    rec_button_dev_switch = True
