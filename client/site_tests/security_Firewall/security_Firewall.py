3790d82e5d2db3723250de076494cd63cfef5db1
Jorge Lucangeli Obes <jorgelo@chromium.org>
2015-02-17 17:05:49 -0800

    security_Firwall: test IPv6 rules as well.
    BUG=None
    TEST=Passes on expresso.
    
    Change-Id: I965b3e4655d0446d8f46884e21c391fbaa7d4c6a
    Reviewed-on: https://chromium-review.googlesource.com/250625
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Trybot-Ready: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Tested-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- client/site_tests/security_Firewall/security_Firewall.py
+++ client/site_tests/security_Firewall/security_Firewall.py
@@ -13,80 +13,83 @@ from autotest_lib.client.cros.networking import apmanager_helper
 
 
 class security_Firewall(test.test):
-    """Tests that rules in iptables match our expectations exactly."""
+    """Tests that rules in iptables/ip6tables match our expectations exactly."""
     version = 1
 
 
-    def get_firewall_settings(self):
-        ipt_rules = utils.system_output("iptables -S")
-        return set([line.strip() for line in ipt_rules.splitlines()])
+    @staticmethod
+    def get_firewall_settings(executable):
+        rules = utils.system_output("%s -S" % executable)
+        return set([line.strip() for line in rules.splitlines()])
 
 
     def load_baseline(self, baseline_filename):
-        """The baseline file lists the iptables rules that we expect.
+        """The baseline file lists the rules that we expect.
 
         @param baseline_filename: string name of file containing relevant rules.
-
         """
         baseline_path = os.path.join(self.bindir, baseline_filename)
         with open(baseline_path) as f:
             return set([line.strip() for line in f.readlines()])
 
 
-    def dump_iptables_rules(self, ipt_rules):
+    def dump_rules(self, rules, executable):
         """Store actual rules in results/ for future use.
 
-        Leaves a list of iptables rules in the results dir
+        Leaves a list of iptables/ip6tables rules in the results dir
         so that we can update the baseline file if necessary.
 
-        @param ipt_rules: list of string containing rules we found on the board.
-
+        @param rules: list of string containing rules we found on the board.
+        @param executable: 'iptables' for IPv4 or 'ip6tables' for IPv6.
         """
-        outf = open(os.path.join(self.resultsdir, "iptables_rules"), 'w')
-        for rule in ipt_rules:
+        outf = open(os.path.join(self.resultsdir, "%s_rules" % executable), 'w')
+        for rule in rules:
             outf.write(rule + "\n")
 
         outf.close()
 
 
-    def log_error_rules(self, rules, message):
+    @staticmethod
+    def log_error_rules(rules, message):
         """Log a set of rules and the problem with those rules.
 
         @param rules: list of string containing rules we have issues with.
         @param message: string detailing what our problem with the rules is.
-
         """
         rules_str = ", ".join(["'%s'" % rule for rule in rules])
         logging.error("%s: %s", message, rules_str)
 
 
     def run_once(self):
-        """Matches found and expected iptables rules.
+        """Matches found and expected iptables/ip6tables rules.
         Fails both when rules are missing and when extra rules are found.
         """
 
-        baseline = self.load_baseline('baseline')
-        # TODO(wiley) Remove when we get per-board baselines (crbug.com/406013)
-        if webservd_helper.webservd_is_installed():
-            baseline.update(self.load_baseline('baseline.webservd'))
-        if apmanager_helper.apmanager_is_installed():
-            baseline.update(self.load_baseline('baseline.apmanager'))
-        current = self.get_firewall_settings()
-
-        # Save to results dir
-        self.dump_iptables_rules(current)
-
-        missing_rules = baseline - current
-        extra_rules = current - baseline
-
         failed = False
-        if len(missing_rules) > 0:
-            failed = True
-            self.log_error_rules(missing_rules, "Missing iptables rules")
+        for executable in ["iptables", "ip6tables"]:
+            baseline = self.load_baseline("baseline.%s" % executable)
+            # TODO(wiley) Remove when we get per-board baselines (crbug.com/406013)
+            if webservd_helper.webservd_is_installed():
+                baseline.update(self.load_baseline("baseline.webservd"))
+            if apmanager_helper.apmanager_is_installed():
+                baseline.update(self.load_baseline("baseline.apmanager"))
+            current = self.get_firewall_settings(executable)
 
-        if len(extra_rules) > 0:
-            failed = True
-            self.log_error_rules(extra_rules, "Extra iptables rules")
+            # Save to results dir
+            self.dump_rules(current, executable)
+
+            missing_rules = baseline - current
+            extra_rules = current - baseline
+
+            failed = False
+            if len(missing_rules) > 0:
+                failed = True
+                self.log_error_rules(missing_rules,
+                                     "Missing %s rules" % executable)
+
+            if len(extra_rules) > 0:
+                failed = True
+                self.log_error_rules(extra_rules, "Extra %s rules" % executable)
 
         if failed:
-            raise error.TestFail("Mismatched iptables rules")
+            raise error.TestFail("Mismatched firewall rules")
