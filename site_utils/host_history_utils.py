jingpad-bsp/android_external_autotest

commit b72f4fbcf1583da27f09f4abb9d8162530bf4559

graphite: Reorganize the elastic search code so we can put it in chromite.
This change reorganizes the elastic search integration code so that it's
separate from the code that, for instance, reads config information from the
autotest global config. That way, it can be moved from chromite without
breaking any dependencies.

BUG=chromium:446291
TEST=Ran stats_es_functionaltest.py. Ran unit tests. Ran a butterfly-paladin
tryjob with --hwtest.

Change-Id: I0dbf135c4f1732d633e5fc9d5edb9e1f4f7199d5
Reviewed-on: https://chromium-review.googlesource.com/242701
Reviewed-by: Dan Shi <dshi@chromium.org>
Tested-by: Gabe Black <gabeblack@chromium.org>
Commit-Queue: Gabe Black <gabeblack@chromium.org>

site_utils/host_history_utils.py
@@ -10,7 +10,7 @@ from itertools import groupby
 
 import common
 from autotest_lib.client.common_lib import time_utils
-from autotest_lib.client.common_lib.cros.graphite import es_utils
+from autotest_lib.client.common_lib.cros.graphite import autotest_es
 from autotest_lib.frontend import setup_django_environment
 from autotest_lib.frontend.afe import models
 from autotest_lib.site_utils import host_label_utils
@@ -71,8 +71,8 @@ def lock_history_to_intervals(initial_lock_val, t_start, t_end, lock_history):
     locked_intervals = []
     t_prev = t_start
     state_prev = initial_lock_val
-    for entry in lock_history['hits']['hits']:
-        t_curr = entry['fields']['time_recorded'][0]
+    for entry in lock_history.hits:
+        t_curr = entry['time_recorded'][0]
 
         #If it is locked, then we put into locked_intervals
         if state_prev:
@@ -80,7 +80,7 @@ def lock_history_to_intervals(initial_lock_val, t_start, t_end, lock_history):
 
         # update vars
         t_prev = t_curr
-        state_prev = entry['fields']['locked'][0]
+        state_prev = entry['locked'][0]
     if state_prev:
         locked_intervals.append((t_prev, t_end))
     return locked_intervals
@@ -96,19 +96,15 @@ def find_most_recent_entry_before(t, type_str, hostname, fields):
     @returns: time, field_value of the latest entry.
     """
     t_epoch = time_utils.to_epoch_time(t)
-    query = es_utils.create_range_eq_query_multiple(
+    result = autotest_es.query(
             fields_returned=fields,
             equality_constraints=[('_type', type_str),
                                   ('hostname', hostname)],
             range_constraints=[('time_recorded', None, t_epoch)],
             size=1,
             sort_specs=[{'time_recorded': 'desc'}])
-    result = es_utils.execute_query(query)
-    if result['hits']['total'] > 0:
-        # If fields are not specified, the query returns all data for the
-        # record under key "_source"
-        key = 'fields' if fields else '_source'
-        return es_utils.convert_hit(result['hits']['hits'][0][key])
+    if result.total > 0:
+        return result.hits[0]
     return {}
 
 
@@ -137,27 +133,21 @@ def get_host_history_intervals(t_start, t_end, hostname, intervals):
     # I use [0] and [None] because lock_history_recent's type is list.
     t_lock = lock_history_recent.get('time_recorded', None)
     t_lock_val = lock_history_recent.get('locked', None)
-    host_history_recent = find_most_recent_entry_before(
+    t_metadata = find_most_recent_entry_before(
             t=t_start, type_str=_HOST_HISTORY_TYPE, hostname=hostname,
             fields=None)
-    t_host = host_history_recent.get('time_recorded', None)
-    t_host_stat = host_history_recent.get('status', None)
-    t_metadata = es_utils.get_metadata(host_history_recent,
-                                       ['time_recorded', 'status'])
-
-    status_first = t_host_stat if t_host else 'Ready'
+    t_host = t_metadata.pop('time_recorded', None)
+    t_host_stat = t_metadata.pop('status', None)
     t = min([t for t in [t_lock, t_host, t_start] if t])
 
     t_epoch = time_utils.to_epoch_time(t)
     t_end_epoch = time_utils.to_epoch_time(t_end)
-    query_lock_history = es_utils.create_range_eq_query_multiple(
+    lock_history_entries = autotest_es.query(
             fields_returned=['locked', 'time_recorded'],
             equality_constraints=[('_type', _LOCK_HISTORY_TYPE),
                                   ('hostname', hostname)],
             range_constraints=[('time_recorded', t_epoch, t_end_epoch)],
             sort_specs=[{'time_recorded': 'asc'}])
-
-    lock_history_entries = es_utils.execute_query(query_lock_history)
 
     locked_intervals = lock_history_to_intervals(t_lock_val, t, t_end,
                                                  lock_history_entries)
@@ -168,10 +158,9 @@ def get_host_history_intervals(t_start, t_end, hostname, intervals):
     intervals_of_statuses = collections.OrderedDict()
 
     for entry in intervals:
-        t_curr = entry['_source']['time_recorded']
-        status_curr = entry['_source']['status']
-        metadata = es_utils.get_metadata(entry['_source'],
-                                         ['time_recorded', 'status'])
+        metadata = entry.copy()
+        t_curr = metadata.pop('time_recorded')
+        status_curr = metadata.pop('status')
         intervals_of_statuses.update(calculate_status_times(
                 t_prev, t_curr, status_prev, metadata_prev, locked_intervals))
         # Update vars
@@ -277,15 +266,14 @@ def get_intervals_for_host(t_start, t_end, hostname):
     """
     t_start_epoch = time_utils.to_epoch_time(t_start)
     t_end_epoch = time_utils.to_epoch_time(t_end)
-    query_host_history = es_utils.create_range_eq_query_multiple(
+    host_history_entries = autotest_es.query(
                 fields_returned=None,
                 equality_constraints=[('_type', _HOST_HISTORY_TYPE),
                                       ('hostname', hostname)],
                 range_constraints=[('time_recorded', t_start_epoch,
                                     t_end_epoch)],
                 sort_specs=[{'time_recorded': 'asc'}])
-    host_history_entries = es_utils.execute_query(query_host_history)
-    return host_history_entries['hits']['hits']
+    return result.hits
 
 
 def get_intervals_for_hosts(t_start, t_end, hosts=None, board=None, pool=None):
@@ -324,14 +312,13 @@ def get_intervals_for_hosts(t_start, t_end, hosts=None, board=None, pool=None):
             equality_constraints.append(('labels', 'pool:'+pool))
         t_start_epoch = time_utils.to_epoch_time(t_start)
         t_end_epoch = time_utils.to_epoch_time(t_end)
-        query_labels =  es_utils.create_range_eq_query_multiple(
+        results =  autotest_es.query(
                 equality_constraints=equality_constraints,
                 range_constraints=[('time_recorded', t_start_epoch,
                                     t_end_epoch)],
                 sort_specs=[{'hostname': 'asc'}])
-        results = es_utils.execute_query(query_labels)
         results_group_by_host = {}
-        for hostname,intervals_for_host in groupby(results['hits']['hits'],
+        for hostname,intervals_for_host in groupby(results.hits,
                                                    lambda h: h['hostname']):
             results_group_by_host[hostname] = intervals_for_host
         for host in hosts:

site_utils/host_label_utils.py
@@ -21,7 +21,7 @@ import time
 
 import common
 from autotest_lib.client.common_lib import time_utils
-from autotest_lib.client.common_lib.cros.graphite import es_utils
+from autotest_lib.client.common_lib.cros.graphite import autotest_es
 from autotest_lib.frontend import setup_django_environment
 from autotest_lib.frontend.afe import models
 
@@ -62,19 +62,17 @@ def get_host_labels(days_back=0, hostname=None, labels=None):
     # Search for the latest logged labels before the given days_back.
     # Default is 0, which means the last time host labels were logged.
     t_end = time.time() - days_back*24*3600
-    query_time_index = es_utils.create_range_eq_query_multiple(
+    results = autotest_es.query(
             fields_returned=['time_index'],
             equality_constraints=[('_type', _HOST_LABEL_TIME_INDEX_TYPE),],
             range_constraints=[('time_index', None, t_end)],
             size=1,
             sort_specs=[{'time_index': 'desc'}])
-    results = es_utils.execute_query(query_time_index)
-    count = int(results['hits']['total'])
     t_end_str = time_utils.epoch_time_to_date_string(t_end)
-    if count == 0:
+    if results.total == 0:
         logging.error('No label information was logged before %s.', t_end_str)
         return
-    time_index = results['hits']['hits'][0]['fields']['time_index'][0]
+    time_index = results['time_index'][0]
     logging.info('Host labels were recorded at %s',
                  time_utils.epoch_time_to_date_string(time_index))
 
@@ -86,14 +84,12 @@ def get_host_labels(days_back=0, hostname=None, labels=None):
     if labels:
         for label in labels:
             equality_constraints.append(('labels', label))
-    query_labels =  es_utils.create_range_eq_query_multiple(
+    results = autotest_es.query(
             fields_returned=['hostname', 'labels'],
             equality_constraints=equality_constraints)
-    results = es_utils.execute_query(query_labels)
 
     host_labels = {}
-    for hit in results['hits']['hits']:
-        hit = es_utils.convert_hit(hit['fields'])
+    for hit in results.hits:
         if 'labels' in hit:
             host_labels[hit['hostname']] = hit['labels']
 
@@ -111,11 +107,11 @@ def collect_info():
         info = {'hostname': host.hostname,
                 'labels': [label.name for label in host.labels.all()],
                 'time_index': time_index}
-        es_utils.ESMetadata().post(type_str=_HOST_LABEL_TYPE, metadata=info,
+        autotest_es.post(type_str=_HOST_LABEL_TYPE, metadata=info,
                                    log_time_recorded=False)
 
     # After all host label information is logged, save the time stamp.
-    es_utils.ESMetadata().post(type_str=_HOST_LABEL_TIME_INDEX_TYPE,
+    autotest_es.post(type_str=_HOST_LABEL_TIME_INDEX_TYPE,
                                metadata={'time_index': time_index},
                                log_time_recorded=False)
     logging.info('Finished collecting host labels for %d hosts.', len(hosts))

client/common_lib/cros/graphite/autotest_es.py
@@ -1,0 +1,130 @@
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This file defines helper functions for putting entries into elasticsearch.
+
+"""Utils for sending metadata to elasticsearch
+
+Elasticsearch is a key-value store NOSQL database.
+Source is here: https://github.com/elasticsearch/elasticsearch
+We will be using es to store our metadata.
+
+For example, if we wanted to store the following metadata:
+
+metadata = {
+    'host_id': 1
+    'job_id': 20
+    'time_start': 100000
+    'time_recorded': 100006
+}
+
+The following call will send metadata to the default es server.
+    autotest_es.post(index, metadata)
+We can also specify which port and host to use.
+
+Using for testing: Sometimes, when we choose a single index
+to put entries into, we want to clear that index of all
+entries before running our tests. Use clear_index function.
+(see es_utils_functionaltest.py for an example)
+
+This file also contains methods for sending queries to es. Currently,
+the query (json dict) we send to es is quite complicated (but flexible).
+
+For example, the below query returns job_id, host_id, and job_start
+for all job_ids in [0, 99999] and host_id matching 10.
+
+range_eq_query = {
+    'fields': ['job_id', 'host_id', 'job_start'],
+    'query': {
+        'filtered': {
+            'query': {
+                'match': {
+                    'host_id': 10,
+                }
+            }
+            'filter': {
+                'range': {
+                    'job_id': {
+                        'gte': 0,
+                        'lte': 99999,
+                    }
+                }
+            }
+        }
+    }
+}
+
+To send a query once it is created, call execute_query() to send it to the
+intended elasticsearch server. The query() function can be used to construct a
+query with certain parameters and execute it all in one call.
+
+"""
+
+import es_utils
+
+import common
+from autotest_lib.client.common_lib import global_config
+
+
+# Server and ports for elasticsearch (for metadata use only)
+METADATA_ES_SERVER = global_config.global_config.get_config_value(
+        'CROS', 'ES_HOST', default='localhost')
+ES_PORT = global_config.global_config.get_config_value(
+        'CROS', 'ES_PORT', type=int, default=9200)
+ES_UDP_PORT = global_config.global_config.get_config_value(
+        'CROS', 'ES_UDP_PORT', type=int, default=9700)
+# Whether to use http. udp is very little overhead (around 3 ms) compared to
+# using http (tcp) takes ~ 500 ms for the first connection and 50-100ms for
+# subsequent connections.
+ES_USE_HTTP = global_config.global_config.get_config_value(
+        'CROS', 'ES_USE_HTTP', type=bool, default=False)
+
+# If CLIENT/metadata_index is not set, INDEX_METADATA falls back to
+# autotest instance name (SERVER/hostname).
+INDEX_METADATA = global_config.global_config.get_config_value(
+        'CLIENT', 'metadata_index', type=str, default=None)
+if not INDEX_METADATA:
+    INDEX_METADATA = global_config.global_config.get_config_value(
+            'SERVER', 'hostname', type=str, default='localhost')
+
+# 3 Seconds before connection to esdb timeout.
+DEFAULT_TIMEOUT = 3
+
+
+def post(use_http=ES_USE_HTTP, host=METADATA_ES_SERVER, port=ES_PORT,
+         timeout=DEFAULT_TIMEOUT, index=INDEX_METADATA, udp_port=ES_UDP_PORT,
+         *args, **kwargs):
+    """This function takes a series of arguments which are passed to the
+    es_utils.ESMetadata constructor, and any other arguments are passed to
+    its post() function. For an explanation of each, see those functions in
+    es_utils.
+    """
+    esmd = es_utils.ESMetadata(use_http=use_http, host=host, port=port,
+                               timeout=timeout, index=index, udp_port=udp_port)
+    return esmd.post(*args, **kwargs)
+
+
+def execute_query(host=METADATA_ES_SERVER, port=ES_PORT,
+                  timeout=DEFAULT_TIMEOUT, index=INDEX_METADATA,
+                  *args, **kwargs):
+    """This function takes a series of arguments which are passed to the
+    es_utils.ESMetadata constructor, and any other arguments are passed to
+    its execute_query() function. For an explanation of each, see those
+    functions in es_utils.
+    """
+    esmd = es_utils.ESMetadata(use_http=True, host=host, port=port,
+                               timeout=timeout, index=index, udp_port=0)
+    return esmd.execute_query(*args, **kwargs)
+
+
+def query(host=METADATA_ES_SERVER, port=ES_PORT, timeout=DEFAULT_TIMEOUT,
+          index=INDEX_METADATA, *args, **kwargs):
+    """This function takes a series of arguments which are passed to the
+    es_utils.ESMetadata constructor, and any other arguments are passed to
+    its query() function. For an explanation of each, see those functions in
+    es_utils.
+    """
+    esmd = es_utils.ESMetadata(use_http=True, host=host, port=port,
+                               timeout=timeout, index=index, udp_port=0)
+    return esmd.query(*args, **kwargs)

client/common_lib/cros/graphite/es_test_utils.py
@@ -46,7 +46,8 @@ class EsTestUtilException(Exception):
 
 
 def sequential_random_insert_ints(keys, num_entries, target_type, index,
-                                  host, port, between_insert_secs=0,
+                                  host, port, use_http, udp_port,
+                                  between_insert_secs=0,
                                   print_interval=10):
     """Inserts a bunch of random entries into the es database.
     Keys are given, values are randomly generated.
@@ -77,7 +78,9 @@ def sequential_random_insert_ints(keys, num_entries, target_type, index,
         value = 10
         stats_target = TARGET_TO_STATS_CLASS[target_type](subname,
                 metadata=metadata,
-                es=es_utils.ESMetadata(host=host, port=port, index=index))
+                es=es_utils.ESMetadata(use_http=use_http, host=host,
+                                       port=port, index=index,
+                                       udp_port=udp_port))
 
         if target_type == 'timer':
             stats_target.start()
@@ -116,76 +119,3 @@ def clear_index(index, host, port, timeout, sleep_time=0.5, clear_timeout=5):
                 raise EsTestUtilException('clear_index failed.')
 
     print 'successfully deleted index %s' % (index)
-
-
-def create_range_eq_query(fields_returned,
-                          equals_key=None,
-                          equals_val=None,
-                          range_key=None,
-                          range_low=None,
-                          range_high=None,
-                          size=DEFAULT_NUM_ENTRIES,
-                          sort_specs=None):
-    """Creates a dict. representing range and/or equality queries.
-
-    @param fields_returned: list of fields that we should return when
-                            the query is executed
-    @param equals_key: Key that we filter based on equality. default=None
-    @param equals_val: value we want equals_key to be equal to. default=None
-    @param range_key: Key that we filter based on range. default=None
-    @param range_low: lower bound on the range_key (inclusive). default=None
-    @param range_high: upper bound on the range key (inclusive). default=None
-    @param size: max number of entries to return. default=100
-    @param sort_specs: A list of fields to sort on, tiebreakers will be
-        broken by the next field(s). default=None
-
-    @returns: dictionary representing query.
-              Returns none if there is no equals_key or range_key provided.
-
-    Example usage:
-    range_eq_query = create_range_eq_query(
-        ['job_id', 'host_id', 'job_start'],
-        equals_key='host_id', equals_val=10,
-        range_key='job_id', range_low=0, range_high=99999)
-
-    Output:
-    {
-        'fields': ['job_id', 'host_id', 'job_start'],
-            'query': {
-                'bool': {
-                    'minimum_should_match': 2,
-                    'should': [
-                        {
-                            'term':  {
-                                'host_id': 10,
-                            }
-                        },
-
-                        {   
-                            'range': {
-                                'job_id': {
-                                    'gte': 0,
-                                    'lte': 99999,
-                                }
-                            }
-                        }
-                    ]
-                }
-            },
-        'size': 20
-        'sort': [ ]
-    }
-    """
-    if not equals_key and not range_key:
-        raise EsTestUtilException('No range_key or equals_key specified.')
-    equality_constraints = [(equals_key, equals_val)] if equals_key else []
-    range_constraints = []
-    if range_key:
-        range_constraints = [(range_key, range_low, range_high)]
-    return es_utils.create_range_eq_query_multiple(
-        equality_constraints=equality_constraints,
-        fields_returned=fields_returned,
-        range_constraints=range_constraints,
-        size=size,
-        sort_specs=sort_specs,
-    )

client/common_lib/cros/graphite/stats.py
@@ -8,6 +8,7 @@ import logging
 
 import common
 from autotest_lib.client.common_lib import global_config
+from autotest_lib.client.common_lib.cros.graphite import autotest_es
 from autotest_lib.client.common_lib.cros.graphite import es_utils
 
 try:
@@ -95,12 +96,17 @@ def _prepend_init(original):
     """Decorator to override __init__."""
 
     class _Derived(original):
-        def __init__(self, name, connection=None, bare=False, metadata=None,
-                     es=None):
+        default_es = es_utils.ESMetadata(use_http=autotest_es.ES_USE_HTTP,
+                                         host=autotest_es.METADATA_ES_SERVER,
+                                         port=autotest_es.ES_PORT,
+                                         index=autotest_es.INDEX_METADATA,
+                                         udp_port=autotest_es.ES_UDP_PORT)
+        def __init__(self, name, es=default_es, connection=None,
+                     bare=False, metadata=None):
             conn = connection or _conn
             super(_Derived, self).__init__(_prepend_server(name, bare), conn)
             self.metadata = metadata
-            self.es = es if es else es_utils.ESMetadata()
+            self.es = es
 
     return _Derived
 

client/common_lib/cros/graphite/stats_es_functionaltest.py
@@ -18,7 +18,7 @@ import optparse
 import time
 
 import common
-from autotest_lib.client.common_lib.cros.graphite import es_utils
+from autotest_lib.client.common_lib.cros.graphite import autotest_es
 from autotest_lib.client.common_lib.cros.graphite import es_test_utils
 
 
@@ -33,7 +33,7 @@ class StatsFunctionalTest(object):
         self.port = es_port
         self.index = index
         self.wait_time = 6 # Bulk flush is 5 seconds
-        if es_utils.ES_USE_HTTP:
+        if autotest_es.ES_USE_HTTP:
             # No flush time for http requests.
             self.wait_time = 2
 
@@ -69,6 +69,8 @@ class StatsFunctionalTest(object):
                 index=self.index,
                 host=self.host,
                 port=self.port,
+                use_http = autotest_es.ES_USE_HTTP,
+                udp_port = autotest_es.ES_UDP_PORT,
                 num_entries=num_entries,
                 print_interval=num_entries/5)
         # Wait a bit for es to be populated with the metadata entry.
@@ -76,20 +78,16 @@ class StatsFunctionalTest(object):
         # is configured to be 5 seconds.
         print 'waiting %s seconds...' % (self.wait_time)
         time.sleep(self.wait_time)
-        range_query = es_test_utils.create_range_eq_query(fields_returned=keys,
-                                                          range_key='host_id',
-                                                          range_low=0)
-        result = es_utils.execute_query(range_query,
-                                        self.index,
-                                        self.host,
-                                        self.port)
+        result = autotest_es.query(host=self.host, port=self.port,
+                                   index=self.index, fields_returned=keys,
+                                   range_constraints=[('host_id', 0, None)])
         if not result:
             print ('%s test error: Index %s not found.'
                    %(test_type, self.index))
             return
 
         # TODO(michaelliang): Check hits and total are valid keys at each layer.
-        num_entries_found = result['hits']['total']
+        num_entries_found = result.total
         print('  Inserted %s entries, found %s entries.'
               %(num_entries, num_entries_found))
         if num_entries_found != num_entries:
@@ -125,11 +123,11 @@ def main():
         print ('No tests specified.'
                'For help: python stats_es_functionaltest.py -h')
     if options.es_host == 'prod':
-        es_host = es_utils.METADATA_ES_SERVER
-        es_port = es_utils.ES_PORT
+        es_host = autotest_es.METADATA_ES_SERVER
+        es_port = autotest_es.ES_PORT
     elif options.es_host == 'test':
         es_host = 'http://localhost'
-        es_port = es_utils.ES_PORT
+        es_port = autotest_es.ES_PORT
     else:
         es_host = options.es_host
         es_port = options.es_port

frontend/afe/models.py
@@ -21,7 +21,7 @@ from autotest_lib.client.common_lib import global_config
 from autotest_lib.client.common_lib import host_queue_entry_states
 from autotest_lib.client.common_lib import control_data, priorities, decorators
 from autotest_lib.client.common_lib import site_utils
-from autotest_lib.client.common_lib.cros.graphite import es_utils
+from autotest_lib.client.common_lib.cros.graphite import autotest_es
 
 # job options and user preferences
 DEFAULT_REBOOT_BEFORE = model_attributes.RebootBefore.IF_DIRTY
@@ -570,7 +570,7 @@ class Host(model_logic.ModelWithInvalid, rdb_model_extensions.AbstractHostModel,
         }
         if other_metadata:
             metadata = dict(metadata.items() + other_metadata.items())
-        es_utils.ESMetadata().post(type_str=type_str, metadata=metadata)
+        autotest_es.post(type_str=type_str, metadata=metadata)
 
 
     def save(self, *args, **kwargs):

commit f242c20641311794fbddd022d8dda076eb3e8ea6

[autotest] Fix couple issues in host history/label collection.
Issue was introduced in CL 242701. There is no unittest to catch such issue
because the data needs to be retrieved from a live ES instance. Some manual
test could have helped though.

BUG=None
TEST=edit local shadow config to target to production metadb
CROS/ES_HOST and CLIENT/metadata_index

run some commands:
./collect_host_stats.py  --span 24
./host_history.py  -l 24 --hosts chromeos1-row2-rack4-host2 -v

Change-Id: If5c6409c0b4479c7e6cfd8575099c2e704d37c28
Reviewed-on: https://chromium-review.googlesource.com/251570
Commit-Queue: Dan Shi <dshi@chromium.org>
Trybot-Ready: Dan Shi <dshi@chromium.org>
Tested-by: Dan Shi <dshi@chromium.org>
Reviewed-by: Gabe Black <gabeblack@chromium.org>

site_utils/host_history_utils.py
@@ -72,7 +72,7 @@ def lock_history_to_intervals(initial_lock_val, t_start, t_end, lock_history):
     t_prev = t_start
     state_prev = initial_lock_val
     for entry in lock_history.hits:
-        t_curr = entry['time_recorded'][0]
+        t_curr = entry['time_recorded']
 
         #If it is locked, then we put into locked_intervals
         if state_prev:
@@ -80,7 +80,7 @@ def lock_history_to_intervals(initial_lock_val, t_start, t_end, lock_history):
 
         # update vars
         t_prev = t_curr
-        state_prev = entry['locked'][0]
+        state_prev = entry['locked']
     if state_prev:
         locked_intervals.append((t_prev, t_end))
     return locked_intervals
@@ -138,6 +138,7 @@ def get_host_history_intervals(t_start, t_end, hostname, intervals):
             fields=None)
     t_host = t_metadata.pop('time_recorded', None)
     t_host_stat = t_metadata.pop('status', None)
+    status_first = t_host_stat if t_host else 'Ready'
     t = min([t for t in [t_lock, t_host, t_start] if t])
 
     t_epoch = time_utils.to_epoch_time(t)
@@ -273,7 +274,7 @@ def get_intervals_for_host(t_start, t_end, hostname):
                 range_constraints=[('time_recorded', t_start_epoch,
                                     t_end_epoch)],
                 sort_specs=[{'time_recorded': 'asc'}])
-    return result.hits
+    return host_history_entries.hits
 
 
 def get_intervals_for_hosts(t_start, t_end, hosts=None, board=None, pool=None):

site_utils/host_label_utils.py
@@ -72,7 +72,7 @@ def get_host_labels(days_back=0, hostname=None, labels=None):
     if results.total == 0:
         logging.error('No label information was logged before %s.', t_end_str)
         return
-    time_index = results['time_index'][0]
+    time_index = results.hits[0]['time_index']
     logging.info('Host labels were recorded at %s',
                  time_utils.epoch_time_to_date_string(time_index))
 