48841e1228a3246199580e8e7bd16d1b40b2fea7
Christopher Wiley <wiley@chromium.org>
2015-03-14 17:25:59 -0700

    autotest: Expand functionality of ProcessWatcher
    Change ProcessWatcher to kill processes based on the name and arguments
    to a process.  This allows us to run multiple copies of the binary with
    slightly different arguments in parallel.
    
    In some cases, failing to close output streams causes the
    immediate death of the monitored process depending on what autotest
    does next.  Fix this while we're here.
    
    Sometimes we would like to start a process as a particular user,
    (e.g. DBus system configs limit the set of users that can claim a
    well known name).  Add MinijailConfig, which allows consumers to
    start processes in a minijail.
    
    BUG=brillo:535
    TEST=n_faced_peerd starts up correctly inside a ProcessWatcher.
    
    Change-Id: I30ae402425f3cb8174b51daba4e208dcdc6c0e79
    Reviewed-on: https://chromium-review.googlesource.com/260588
    Tested-by: Christopher Wiley <wiley@chromium.org>
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Commit-Queue: Christopher Wiley <wiley@chromium.org>

--- client/common_lib/cros/process_watcher.py
+++ client/common_lib/cros/process_watcher.py
@@ -2,32 +2,49 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import collections
 import time
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib import utils
 
+# Use this with ProcessWatcher to start your process in a minijail.  This
+# is useful for instance if you would like to drop autotest's default root
+# priviledges.  Both fields must be set to valid users/groups.
+MinijailConfig = collections.namedtuple('MinijailConfig', ['user', 'group'])
+
 
 class ProcessWatcher(object):
     """Start a process, and terminate it later."""
 
-    def __init__(self, command, args=[], host=None):
+    def __init__(self, command, args=[], minijail_config=None, host=None):
         """Construst a ProcessWatcher without starting the process.
 
-        @param command: string command to use to start and stop the process.
+        @param command: string command to use to start the process.
         @param args: list of strings to pass to the command.
+        @param minijail_config: MinijailConfig tuple defined above.
         @param host: host object if the server should be started on a remote
                 host.
 
         """
-        self._command = command
-        self._args = args
+        self._command = '%s %s' % (command, ' '.join(args))
+        if '"' in self._command:
+            raise error.TestError('Please implement shell escaping in '
+                                  'ProcessWatcher.')
+        self._minijail_config = minijail_config
         self._run = utils.run if host is None else host.run
 
 
     def start(self):
         """Start a (potentially remote) instance of the process."""
-        self._run('%s %s &' % (self._command, ' '.join(self._args)))
+        command = self._command
+        prefix = ''
+        if self._minijail_config is not None:
+            prefix = 'minijail0 -i -g %s -u %s ' % (self._minijail_config.group,
+                                                    self._minijail_config.user)
+        # Redirect output streams to avoid odd interactions between autotest's
+        # shell environment and the command's runtime environment.
+        self._run('%s%s >/dev/null 2>&1 &' % (prefix, self._command))
 
 
     def close(self, timeout_seconds=40):
@@ -36,11 +53,11 @@ class ProcessWatcher(object):
         @param timeout_seconds: int number of seconds to wait for shutdown.
 
         """
-        self._run('pkill -f --signal TERM %s' % self._command,
+        self._run('pkill -f --signal TERM "%s"' % self._command,
                   ignore_status=True)
         start_time = time.time()
         while time.time() - start_time < timeout_seconds:
-            result = self._run('pgrep -l %s' % self._command,
+            result = self._run('pgrep -f -l "%s"' % self._command,
                                ignore_status=True)
             if result.exit_status != 0:
                 return
