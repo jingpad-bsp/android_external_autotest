24a9562d9341b8d41cda4ebf04fd66638d554305
Alex Deymo <deymo@chromium.org>
2015-03-18 21:19:08 -0700

    build_RootFilesystemSize: Don't fail when using squashfs.
    build_RootFilesystemSize relies on a test_image script to store the size
    of the rootfs in production images in a file in /root in test images.
    This approach doesn't work well with squashfs since the image is mounted
    as read-write when this file is created, so the size is unknown.
    
    This patch makes the test pass, reporting only the rootfs size on test
    images when squashfs is used. This autotest test will be replaced by an
    ImageTestStage test instead.
    
    BUG=brillo:616
    TEST=test_that build_RootFilesystemSize
    
    Change-Id: I360fa09d9f20b8f1d601de6234257d7b5b3b3046
    Reviewed-on: https://chromium-review.googlesource.com/261025
    Reviewed-by: Bertrand Simonnet <bsimonnet@chromium.org>
    Commit-Queue: Alex Deymo <deymo@chromium.org>
    Trybot-Ready: Alex Deymo <deymo@chromium.org>
    Tested-by: Alex Deymo <deymo@chromium.org>

--- client/site_tests/build_RootFilesystemSize/build_RootFilesystemSize.py
+++ client/site_tests/build_RootFilesystemSize/build_RootFilesystemSize.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 
 import commands
+import os
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 
@@ -11,34 +12,48 @@ class build_RootFilesystemSize(test.test):
     version = 1
 
 
+    PROD_IMAGE_ROOTFS_BYTES = '/root/bytes-rootfs-prod'
+
+
     def run_once(self):
         """Run the free space on rootfs test."""
 
         # Report the production size.
-        f = open('/root/bytes-rootfs-prod', 'r')
-        self.output_perf_value('bytes_rootfs_prod', value=float(f.read()),
-                               units='bytes', higher_is_better=False)
-        f.close()
+        if os.path.exists(self.PROD_IMAGE_ROOTFS_BYTES):
+            rootfs_bytes_str = ''
+            with open(self.PROD_IMAGE_ROOTFS_BYTES, 'r') as f:
+                rootfs_bytes_str = f.read()
+            if rootfs_bytes_str:
+                self.output_perf_value('bytes_rootfs_prod',
+                                       value=float(rootfs_bytes_str),
+                                       units='bytes',
+                                       higher_is_better=False)
 
         # Report the current (test) size.
         (status, output) = commands.getstatusoutput(
-            'df -B1 / | tail -1')
+            'df -B1 --print-type / | tail -1')
         if status != 0:
             raise error.TestFail('Could not get size of rootfs')
 
         # Expected output format:
-        #                Total      Used Available
-        # rootfs    1056858112 768479232 288378880 73% /
+        # Filesystem     Type     1B-blocks      Used Available Use% Mounted on
+        # /dev/root      ext2    1056858112 768479232 288378880  73% /
         output_columns = output.split()
-        used = output_columns[2]
-        free = output_columns[3]
+        fs_type = output_columns[1]
+        used = int(output_columns[3])
+        free = int(output_columns[4])
 
         self.output_perf_value('bytes_rootfs_test', value=float(used),
                                units='bytes', higher_is_better=False)
 
-        # Fail if we are running out of free space on rootfs.
-        required_free_space = 20 * 1024 * 1024
+        # Ignore squashfs as the free space will be reported as 0.
+        if fs_type == 'squashfs':
+            return
 
-        if int(free) < required_free_space:
+        # Fail if we are running out of free space on rootfs (20 MiB or
+        # 2% free space).
+        required_free_space = min(20 * 1024 * 1024, used * 0.02)
+
+        if free < required_free_space:
             raise error.TestFail('%s bytes free is less than the %s required.' %
                                  (free, required_free_space))
