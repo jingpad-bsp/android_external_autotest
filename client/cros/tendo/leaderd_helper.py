81ad510f2fed5fda38148886875b3d92a9565ab4
Christopher Wiley <wiley@chromium.org>
2015-03-27 13:38:01 -0700

    autotest: Add additional helper methods to LeaderdHelper
    These are handy when writing tests against leaderd.
    
    BUG=brillo:536
    TEST=The test depending on these passes.
    
    Change-Id: If696db8b9bd212c1b15f710b5ff59cef089a8429
    Reviewed-on: https://chromium-review.googlesource.com/262941
    Tested-by: Christopher Wiley <wiley@chromium.org>
    Reviewed-by: David Zeuthen <zeuthen@chromium.org>
    Commit-Queue: Christopher Wiley <wiley@chromium.org>

--- client/cros/tendo/leaderd_helper.py
+++ client/cros/tendo/leaderd_helper.py
@@ -22,6 +22,19 @@ DBUS_INTERFACE_GROUP = 'org.chromium.leaderd.Group'
 DBUS_PATH_MANAGER = '/org/chromium/leaderd/Manager'
 
 GROUP_PROPERTY_MEMBERS = 'MemberUUIDs'
+GROUP_PROPERTY_LEADER_UUID = 'LeaderUUID'
+
+def _get_bus(bus):
+    """Get a dbus.Bus object to use.
+
+    @param bus: dbus.Bus object to use or None.
+
+    """
+    if bus is None:
+        # This is a global singleton DBus connection that stays
+        # open until explicitly closed or the process exits.
+        bus = dbus.SystemBus()
+    return bus
 
 
 def get_nth_service_name(n):
@@ -31,6 +44,8 @@ def get_nth_service_name(n):
     @return string: DBus service name for Nth instance.
 
     """
+    if n is None:
+        return SERVICE_NAME
     return '%s.TestInstance%d' % (TEST_SERVICE_NAME_PREFIX, n)
 
 
@@ -44,8 +59,7 @@ def confirm_leaderd_started(service_name=SERVICE_NAME,
     @param timeout_seconds: number of seconds to wait for leaderd to respond.
 
     """
-    if bus is None:
-        bus = dbus.SystemBus()
+    bus = _get_bus(bus)
     start_time = time.time()
     while time.time() - start_time < timeout_seconds:
         try:
@@ -112,13 +126,8 @@ class LeaderdHelper(object):
 
         """
         logging.info('Joining leaderd group %s.', group_id)
-        service_name = SERVICE_NAME
-        if instance_number is not None:
-            service_name = get_nth_service_name(instance_number)
-        if bus is None:
-            # This is a global singleton DBus connection that stays
-            # open until explicitly closed or the process exits.
-            bus = dbus.SystemBus()
+        service_name = get_nth_service_name(instance_number)
+        bus = _get_bus(bus)
         manager = dbus.Interface(
                 bus.get_object(service_name, DBUS_PATH_MANAGER),
                 DBUS_INTERFACE_MANAGER)
@@ -127,6 +136,25 @@ class LeaderdHelper(object):
         group_path = manager.JoinGroup(dbus.String(str(group_id)),
                                        dbus.Dictionary(dict(), 'sv'))
         return dbus_util.dbus2primitive(group_path)
+
+
+    def set_score(self, group_path, score, instance_number=None, bus=None):
+        """Set the score of a particular group.
+
+        @param group_path: string DBus path of group to set score on.
+        @param score: integer score.
+        @param instance_number: instance number of leaderd to join to the group.
+                If not provided, joins the global leaderd instance to the group.
+        @param bus: dbus.Bus instance if something other than the system bus
+                should be used to communicate with this instance.
+
+        """
+        bus = _get_bus(bus)
+        service_name = get_nth_service_name(instance_number)
+        group = dbus.Interface(
+                bus.get_object(service_name, group_path),
+                DBUS_INTERFACE_GROUP)
+        group.SetScore(dbus.Int32(score))
 
 
     def get_members(self, group_path, instance_number=None, bus=None):
@@ -141,15 +169,92 @@ class LeaderdHelper(object):
                 seen by this instance of leaderd.
 
         """
-        service_name = SERVICE_NAME
-        if instance_number is not None:
-            service_name = get_nth_service_name(instance_number)
-        if bus is None:
-            # This is a global singleton DBus connection that stays
-            # open until explicitly closed or the process exits.
-            bus = dbus.SystemBus()
+        service_name = get_nth_service_name(instance_number)
+        bus = _get_bus(bus)
         group = dbus.Interface(
                 bus.get_object(service_name, group_path),
                 dbus.PROPERTIES_IFACE)
         members = group.Get(DBUS_INTERFACE_GROUP, GROUP_PROPERTY_MEMBERS)
         return dbus_util.dbus2primitive(members)
+
+
+    def get_leader(self, group_path, instance_number=None, bus=None):
+        """Get the leader according to a particular group.
+
+        @param group_path: string DBus path of group to set score on.
+        @param instance_number: instance number of leaderd to join to the group.
+                If not provided, joins the global leaderd instance to the group.
+        @param bus: dbus.Bus instance if something other than the system bus
+                should be used to communicate with this instance.
+        @return string leader id found on group.
+
+        """
+        service_name = get_nth_service_name(instance_number)
+        bus = _get_bus(bus)
+        group = dbus.Interface(
+                bus.get_object(service_name, group_path),
+                dbus.PROPERTIES_IFACE)
+        leader = group.Get(DBUS_INTERFACE_GROUP, GROUP_PROPERTY_LEADER_UUID)
+        return dbus_util.dbus2primitive(leader)
+
+
+    def confirm_instances_see_each_other(self, group_paths, expected_peer_ids,
+                                         timeout_seconds=5, bus=None):
+        """Confirm that some previously created instances agree they're all
+        members of a group.
+
+        @param group_paths: dictionary mapping from instance numbers to string
+                DBus group paths of the group on those instances.
+        @param expected_peer_ids: list of string peer ids that should be listed
+                in each group's membership list.
+        @param timeout_seconds: number of seconds to wait for convergence.
+        @param bus: dbus.Bus instance if something other than the system bus
+                should be used to communicate with this instance.
+        @return True iff all groups list |expected_peer_ids| as group members.
+
+        """
+        start_time = time.time()
+        while time.time() - start_time < timeout_seconds:
+            for instance_number, group_path in group_paths.iteritems():
+                actual_peer_ids = sorted(self.get_members(
+                        group_path, instance_number=instance_number, bus=bus))
+                if actual_peer_ids != expected_peer_ids:
+                    logging.debug('Expected to find group members %r, but got '
+                                  '%r on instance %d.',
+                                  expected_peer_ids,
+                                  actual_peer_ids,
+                                  instance_number)
+                    time.sleep(1)
+                    continue  # Found an instance with a bad membership list.
+            return True  # All instances agree on the correct membership.
+        return False  # Timed out.
+
+
+    def confirm_instances_follow_leader(self, group_paths, expected_leader_id,
+                                        timeout_seconds=30, bus=None):
+        """Confirm that some previously created instances agree on a leader.
+
+        @param group_paths: dictionary mapping from instance numbers to string
+                DBus group paths of the group on those instances.
+        @param expected_leader_id: string identifier of leader (a UUID).
+        @param timeout_seconds: number of seconds to wait for convergence.
+        @param bus: dbus.Bus instance if something other than the system bus
+                should be used to communicate with this instance.
+        @return True iff all groups give |expected_leader_id| as their leader.
+
+        """
+        start_time = time.time()
+        while time.time() - start_time < timeout_seconds:
+            for instance_number, group_path in group_paths.iteritems():
+                actual_leader_id = self.get_leader(
+                        group_path, instance_number=instance_number, bus=bus)
+                if actual_leader_id != expected_leader_id:
+                    logging.debug('Expected to find leader id %s, but got %s '
+                                  'on instance %d.',
+                                  expected_leader_id,
+                                  actual_leader_id,
+                                  instance_number)
+                    time.sleep(1)
+                    continue  # Found a peer that disagrees about the leader.
+            return True  # Everyone agrees on the expected leader.
+        return False  # Timed out.
