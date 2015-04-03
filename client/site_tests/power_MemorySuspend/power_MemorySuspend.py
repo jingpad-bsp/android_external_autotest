47bf9cb2af77fa073bcce90509523b8ba5b030c6
Puthikorn Voravootivat <puthik@chromium.org>
2015-04-03 13:55:57 -0700

    power_MemorySuspend: allow 1% early wakeup
    The goal of this test is to make sure that the memory content is unaltered
    after suspend / resume. However this test failed a lot because of some
    other component wake the machine up early.
    
    This patch makes this test to not consider failing if we see less than 1%
    early wakeup while running this test.
    
    BUG=chromium:471911, chrome-os-partner:37417
    TEST=manual. Test did not fail when early suspend is less than treshold.
    
    Change-Id: I8b51e4ae5d605134a47fb79dd67ee3ae6a2316c7
    Signed-off-by: Puthikorn Voravootivat <puthik@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/264010
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>

--- client/site_tests/power_MemorySuspend/power_MemorySuspend.py
+++ client/site_tests/power_MemorySuspend/power_MemorySuspend.py
@@ -2,7 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 from autotest_lib.client.bin import test, utils
+from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import sys_power
 
 class power_MemorySuspend(test.test):
@@ -14,10 +16,20 @@ class power_MemorySuspend(test.test):
         utils.system('stop ui', ignore_status=True)
 
 
-    def run_once(self, num_suspends=1):
-        for _ in range(num_suspends):
-            sys_power.memory_suspend(10)
+    def run_once(self, num_suspends=1, max_spurious_wakeup_ratio=0.01):
+        spurious_wakeup_count = 0
+        max_spurious_wakeup = num_suspends * max_spurious_wakeup_ratio
 
+        for _ in range(num_suspends):
+            try:
+                sys_power.memory_suspend(10)
+            except sys_power.SpuriousWakeupError:
+                spurious_wakeup_count += 1
+                if spurious_wakeup_count > max_spurious_wakeup:
+                    raise error.TestFail('Too many SpuriousWakeupError.')
+
+        if spurious_wakeup_count > 0:
+            logging.info("Have %d SpuriousWakeupError", spurious_wakeup_count)
 
     def cleanup(self):
         utils.system('start ui')
