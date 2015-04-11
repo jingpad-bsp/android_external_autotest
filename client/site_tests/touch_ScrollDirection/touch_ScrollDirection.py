c7520cf37050702ce9f6c50b8d6a383a44193584
Dinesh Kumar Sunkara <dsunkara@google.com>
2015-04-10 17:16:56 -0700

    [autotest] Updated script's board validation based on gesture files
    Refactored the code to remove hardcoded board values and added the
    logic to check the board's validity (for the script to run) based on
    gesture files that we provide for this script.
    
    BUG=None
    TEST=Ran on all affected boards and new boards
    
    Change-Id: I0a932e8e262c633e4644f59673909624b1bd8bd0
    Reviewed-on: https://chromium-review.googlesource.com/265258
    Tested-by: Dinesh Kumar Sunkara <dsunkara@chromium.org>
    Reviewed-by: Dinesh Kumar Sunkara <dsunkara@chromium.org>
    Commit-Queue: Dinesh Kumar Sunkara <dsunkara@chromium.org>
    Trybot-Ready: Dinesh Kumar Sunkara <dsunkara@chromium.org>

--- client/site_tests/touch_ScrollDirection/touch_ScrollDirection.py
+++ client/site_tests/touch_ScrollDirection/touch_ScrollDirection.py
@@ -4,7 +4,6 @@
 
 import os
 import logging
-import shutil
 
 from autotest_lib.client.bin import utils
 from autotest_lib.client.common_lib import error
@@ -16,10 +15,7 @@ class touch_ScrollDirection(touch_playback_test_base.touch_playback_test_base):
     """Plays back scrolls and checks for correct page movement."""
     version = 1
 
-    _VALID_BOARDS = ['squawks', 'nyan_big', 'parrot', 'link', 'peppy', 'daisy',
-                     'peach_pit', 'x86-alex', 'veyron_mighty', 'samus', 'auron_paine']
-
-    def _check_scroll_direction(self, filename, down):
+    def _check_scroll_direction(self, filepath, down):
         """Playback and raise error if scrolling does not match down value.
 
         @param down: True if scrolling is supposed to be down; else False.
@@ -29,7 +25,7 @@ class touch_ScrollDirection(touch_playback_test_base.touch_playback_test_base):
         """
         self._set_default_scroll_position()
         self._wait_for_default_scroll_position()
-        self._playback(filepath=filename)
+        self._playback(filepath)
         self._wait_for_scroll_position_to_settle()
 
         delta = self._get_scroll_position() - self._DEFAULT_SCROLL
@@ -41,29 +37,25 @@ class touch_ScrollDirection(touch_playback_test_base.touch_playback_test_base):
 
     def _verify_scrolling(self):
         """Check scrolling direction for down then up."""
-        self._check_scroll_direction(filename=self._down_file,
+
+        self._check_scroll_direction(filepath=self._down_filepath,
                                      down=not self._australian_state)
-        self._check_scroll_direction(filename=self._up_file,
-                                     down=self._australian_state)
+        self._check_scroll_direction(filepath=self._up_filepath,
+                                     down= self._australian_state)
 
     def run_once(self):
         """Entry point of this test."""
-
-        # Copy playback files to DUT, if available.  Deleted during cleanup.
-        self._copied_files = []
+        # Check if corresponding playback files are available on DUT.
         device = utils.get_board()
-        if device not in self._VALID_BOARDS:
-            logging.info('Aborting test; %s is not supported.', device)
+        gest_dir = os.path.join(self.bindir, 'gestures')
+        self._down_file = '%s_scroll_down' % device
+        self._up_file = '%s_scroll_up' % device
+        self._down_filepath = os.path.join(gest_dir, self._down_file)
+        self._up_filepath = os.path.join(gest_dir, self._up_file)
+        if not (os.path.exists(self._down_filepath) and
+                os.path.exists (self._up_filepath)):
+            logging.info('Missing gesture files, Aborting test')
             return
-        gestures_dir = os.path.join(self.bindir, 'gestures')
-        down = device + '_scroll_down'
-        up = device + '_scroll_up'
-        self._down_file = os.path.join('/tmp', down)
-        self._up_file = os.path.join('/tmp', up)
-        self._copied_files.append(self._down_file)
-        self._copied_files.append(self._up_file)
-        shutil.copyfile(os.path.join(gestures_dir, down), self._down_file)
-        shutil.copyfile(os.path.join(gestures_dir, up), self._up_file)
 
         # Raise error if no touchpad detected.
         if not self._has_touchpad:
@@ -78,7 +70,7 @@ class touch_ScrollDirection(touch_playback_test_base.touch_playback_test_base):
             cr.browser.SetHTTPServerDirectories(self.bindir)
             self._tab = cr.browser.tabs[0]
             self._tab.Navigate(cr.browser.http_server.UrlOf(
-                    os.path.join(self.bindir, 'long_page.html')))
+                               os.path.join(self.bindir, 'long_page.html')))
             self._tab.WaitForDocumentReadyStateToBeComplete()
 
             # Check default scroll - Australian for touchscreens.
@@ -90,13 +82,3 @@ class touch_ScrollDirection(touch_playback_test_base.touch_playback_test_base):
             self._australian_state = not self._australian_state
             self._set_australian_scrolling(value=self._australian_state)
             self._verify_scrolling()
-
-    def cleanup(self):
-        # Remove files, if present.
-        for fh in self._copied_files:
-            try:
-                os.remove(fh)
-            except OSError:
-                pass
-
-
