df0beb3365a733a36ea8532c1d4ac5489d39bf9c
Gabe Black <gabeblack@chromium.org>
2015-04-20 16:15:53 -0700

    mysql: Retry connecting to mysql.
    Retry connecting to mysql on OperationalError exception.
    
    BUG=chromium:478919
    TEST=Ran the smoke suite on moblab. Ran unit tests.
    
    Change-Id: I86b664e7d3c4ab5ec46eacddee24dd3fa671b996
    Reviewed-on: https://chromium-review.googlesource.com/266596
    Tested-by: Gabe Black <gabeblack@chromium.org>
    Commit-Queue: Gabe Black <gabeblack@chromium.org>
    Trybot-Ready: Gabe Black <gabeblack@chromium.org>
    Reviewed-by: Dan Shi <dshi@chromium.org>

--- tko/db_mysql.py
+++ tko/db_mysql.py
@@ -1,8 +1,11 @@
 import common
 import MySQLdb as driver
 import db
+from autotest_lib.client.common_lib.cros import retry
 
 class db_mysql(db.db_sql):
+    @retry.retry(db._get_error_class("OperationalError"), timeout_min=0.5,
+                 delay_sec=0.5)
     def connect(self, host, database, user, password, port):
         connection_args = {
             'host': host,
