55ba2e5be6d73b5ea5d9fc185cf78fb7e9dbd450
Haixia Shi <hshi@chromium.org>
2015-04-20 11:44:36 -0700

    autotest: remove xorg stuff from platform_ToolchainOptions whitelists for freon
    BUG=chromium:413133
    TEST=emerge-link autotest; test_that platform_ToolchainOptions
    
    Change-Id: I8b6a633729dc94d28bb95cd2132f912db377309e
    Reviewed-on: https://chromium-review.googlesource.com/266397
    Commit-Queue: Haixia Shi <hshi@chromium.org>
    Trybot-Ready: Haixia Shi <hshi@chromium.org>
    Tested-by: Haixia Shi <hshi@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Ilja Friedel <ihf@chromium.org>

--- client/site_tests/platform_ToolchainOptions/platform_ToolchainOptions.py
+++ client/site_tests/platform_ToolchainOptions/platform_ToolchainOptions.py
@@ -215,7 +215,10 @@ class platform_ToolchainOptions(test.test):
         now_cmd = ("(%s {} | grep -q statically) ||"
                    "%s -d {} 2>&1 | "
                    "egrep -q \"BIND_NOW\"" % (FILE_CMD, readelf_cmd))
-        now_whitelist = os.path.join(self.bindir, "now_whitelist")
+        if utils.is_freon():
+            now_whitelist = os.path.join(self.bindir, "now_whitelist")
+        else:
+            now_whitelist = os.path.join(self.bindir, "now_whitelist_x")
         option_sets.append(self.create_and_filter("-Wl,-z,now",
                                                   now_cmd,
                                                   now_whitelist))
@@ -251,7 +254,10 @@ class platform_ToolchainOptions(test.test):
         loadwx_cmd = ("%s -lW {} 2>&1 | "
                       "grep \"LOAD\" | egrep -v \"(RW |R E)\" | "
                       "wc -l | grep -q \"^0$\"" % readelf_cmd)
-        loadwx_whitelist = os.path.join(self.bindir, "loadwx_whitelist")
+        if utils.is_freon():
+            loadwx_whitelist = os.path.join(self.bindir, "loadwx_whitelist")
+        else:
+            loadwx_whitelist = os.path.join(self.bindir, "loadwx_whitelist_x")
         option_sets.append(self.create_and_filter("LOAD Writable and Exec",
                                                   loadwx_cmd,
                                                   loadwx_whitelist))
