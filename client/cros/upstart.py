jingpad-bsp/android_external_autotest

commit f748e3beecac2237455f9f0eeb7b502be1a339ef

autotest: wifi: Add upstart to sys_power dependencies
sys_power.py now imports upstart.py.  Import this.  Use os.system
instead of utils.  This reduces the number of dependencies incurred by
sys_power.py.

BUG=chromium-os:20327
TEST=Rerun roaming suite test

Change-Id: Ic7104dbf7d69deddab7df66fd5213d5ac7d7d1d6
Reviewed-on: http://gerrit.chromium.org/gerrit/7569
Reviewed-by: Jason Glasgow <jglasgow@chromium.org>
Tested-by: Paul Stewart <pstew@chromium.org>

client/cros/upstart.py
@@ -2,10 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# NB: this code is downloaded for use by site_system_suspend.py;
+# beware of adding dependencies on client libraries such as utils
+
 """Provides utility methods for interacting with upstart"""
 
-from autotest_lib.client.common_lib import utils
+import os
 
 def ensure_running(service_name):
     cmd = 'initctl status %s | grep start/running' % service_name
-    utils.system(cmd)
+    os.system(cmd)

server/site_wifitest.py
@@ -1218,7 +1218,9 @@ class WiFiTest(object):
         script_client_file = self.install_script('site_system_suspend.py',
                                                  '../client/cros/rtc.py',
                                                  '../client/cros/'
-                                                 'sys_power.py')
+                                                 'sys_power.py',
+                                                 '../client/cros/'
+                                                 'upstart.py')
         result = self.client.run('python "%s" %d' %
             (script_client_file, int(params.get("suspend_time", 5))))
 
@@ -1229,7 +1231,9 @@ class WiFiTest(object):
         script_client_file = self.install_script('site_system_suspend.py',
                                                  '../client/cros/rtc.py',
                                                  '../client/cros/'
-                                                 'sys_power.py')
+                                                 'sys_power.py',
+                                                 '../client/cros/'
+                                                 'upstart.py')
         cmd = ('python "%s" %d %s' %
                (script_client_file,
                 int(params.get("suspend_time", 5)),

commit ec24bc2aaf914ff501c3666cbd2f6bdb4f59e67d

autotest: fix a bug in upstart.ensure_running
Use utils.system instead of os.system: the latter does not report errors
as the code expects, i.e. the function has no effect when using os.system.

The bug was originally introduced by f748e3beecac2237455f9f0eeb7b502be1a339ef^!

BUG=None
TEST=Verify that tests that should fail if a service is not running fail

Change-Id: I90ea81ea9ff19caa36d4aae4879064eea834ae52
Reviewed-on: https://chromium-review.googlesource.com/269569
Tested-by: Andrey Ulanov <andreyu@google.com>
Reviewed-by: Bertrand Simonnet <bsimonnet@chromium.org>
Commit-Queue: Andrey Ulanov <andreyu@google.com>

client/cros/upstart.py
@@ -6,13 +6,15 @@
 
 import os
 
+from autotest_lib.client.common_lib import utils
+
 def ensure_running(service_name):
     """Fails if |service_name| is not running.
 
     @param service_name: name of the service.
     """
     cmd = 'initctl status %s | grep start/running' % service_name
-    os.system(cmd)
+    utils.system(cmd)
 
 
 def has_service(service_name):