58ed26dde0afc9039fed682c0640027e2509efb3
Nick Sanders <nsanders@chromium.org>
2015-05-13 15:48:11 -0700

    hardware_SAT: Add chip decode for samus
    Add a mapping for samus/bdw memory error location to dram chip.
    This is basically the same as for link/ivb.
    
    BUG=chrome-os-partner:40081
    TEST=emerge-samus autotest-tests; ./run_remote_tests.sh --remote 100.96.49.166 \
         '^client/site_tests/hardware_SAT/control$'; \
         heatgun each chip and check error log.
    
    Change-Id: Idb587c461f1d25359be9aa4a9de30981e7c317cf
    Reviewed-on: https://chromium-review.googlesource.com/270815
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Commit-Queue: Nick Sanders <nsanders@chromium.org>
    Trybot-Ready: Nick Sanders <nsanders@chromium.org>
    Tested-by: Nick Sanders <nsanders@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- client/site_tests/hardware_SAT/hardware_SAT.py
+++ client/site_tests/hardware_SAT/hardware_SAT.py
@@ -7,8 +7,9 @@ import logging, re, struct, sys, time
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 
-def memory_channel_args_sandybridge(channel_modules):
+def memory_channel_args_snb_bdw(channel_modules):
     """Add arguments for memory testing.
+    Works on sandybridge, ivybridge, broadwell. Maybe others?
 
     @param channel_modules: channel names.
     """
@@ -109,9 +110,14 @@ class hardware_SAT(test.test):
             args += ' -f sat.diskthread.b'
 
         if utils.get_board() == 'link':
-            args += memory_channel_args_sandybridge([
+            args += memory_channel_args_snb_bdw([
                     ['U1', 'U2', 'U3', 'U4'],
                     ['U6', 'U5', 'U7', 'U8']])  # yes, U6 is actually before U5
+
+        if utils.get_board() == 'samus':
+            args += memory_channel_args_snb_bdw([
+                    ['U11', 'U12'],
+                    ['U13', 'U14']])
 
         # 'stressapptest' is provided by dev-util/stressapptest, pre-installed
         # in test images.
