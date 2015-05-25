882bf236caf34429a8db8434c924a675febe57fe
Cheng-Yi Chiang <cychiang@chromium.org>
2015-05-25 22:34:38 +0800

    chameleon: Add chameleon_info module for MAC address mapping
    chameleon_info handles the mapping from MAC address of Chameleon board
    to Chameleon board information including MAC address of bluetooth module
    on audio board.
    
    BUG=chromium:437678
    TEST=not used yet
    
    Change-Id: I9727da18f00c81285590e7841a2883785d93c24b
    Reviewed-on: https://chromium-review.googlesource.com/273152
    Reviewed-by: Hung-ying Tyan <tyanh@chromium.org>
    Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>

---
+++ client/cros/chameleon/chameleon_info.py
@@ -0,0 +1,46 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""This module provides the information of Chameleon board."""
+
+
+import collections
+import logging
+
+
+# Mapping from Chameleon MAC address to other information including
+# bluetooth MAC address on audio board.
+ChameleonInfo = collections.namedtuple(
+        'ChameleonInfo', ['bluetooth_mac_address'])
+
+_CHAMELEON_BOARD_INFO = {
+        '94:eb:2c:00:00:fb': ChameleonInfo('00:1F:84:01:03:68'),
+        '94:eb:2c:00:01:2b': ChameleonInfo('00:1F:84:01:03:5E'),
+}
+
+class ChameleonInfoError(Exception):
+    """Error in chameleon_info."""
+    pass
+
+
+def get_bluetooth_mac_address(chameleon_board):
+    """Gets bluetooth MAC address of a ChameleonBoard.
+
+    @param chameleon_board: A ChameleonBoard object.
+
+    @returns: A string for bluetooth MAC address of bluetooth module on the
+              audio board.
+
+    @raises: ChameleonInfoError if bluetooth MAC address of this Chameleon
+             board can not be found.
+
+    """
+    chameleon_mac_address = chameleon_board.get_mac_address().lower()
+    if chameleon_mac_address not in _CHAMELEON_BOARD_INFO:
+        raise ChameleonInfoError(
+                'Chameleon info not found for %s' % chameleon_mac_address)
+    board_info = _CHAMELEON_BOARD_INFO[chameleon_mac_address]
+    logging.debug('Chameleon board info: %r', board_info)
+    return board_info.bluetooth_mac_address
