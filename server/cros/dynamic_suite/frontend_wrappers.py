d188984d9aef5a4ff09d8d459b5b3a6c46789fb0
Matthew Sartori <msartori@chromium.org>
2015-06-05 14:36:22 -0700

    [autotest] Retry with backoff for RPCs
    Use chromite.lib.retry_util to run RPCs with exponential
    backoff. The chromite library cannot be safely imported into
    client/common_lib, preventing us from adding to the retry module.
    So, we use chromite's retry_util directly when issuing RPCs to
    get the desired backoff behaviour.
    
    BUG=chromium:493219
    Test=Tested that test suites were runnable with these changes on
    a moblab. Tested the new retry logic with a stubbed function to
    retry.
    
    Change-Id: Ia5aac84d3e475a022867b4ca43d8f6bb0fb05d7a
    Reviewed-on: https://chromium-review.googlesource.com/275699
    Tested-by: Matthew Sartori <msartori@chromium.org>
    Reviewed-by: David James <davidjames@chromium.org>
    Commit-Queue: Matthew Sartori <msartori@chromium.org>

--- server/cros/dynamic_suite/frontend_wrappers.py
+++ server/cros/dynamic_suite/frontend_wrappers.py
@@ -3,10 +3,32 @@
 # found in the LICENSE file.
 
 import common
+import math
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib.cros import retry
 from autotest_lib.frontend.afe.json_rpc import proxy
 from autotest_lib.server import frontend
+from chromite.lib import retry_util
+
+
+def convert_timeout_to_retry(backoff, timeout_min, delay_sec):
+    """Compute the number of retry attempts for use with chromite.retry_util.
+
+    @param backoff: The exponential backoff factor.
+    @param timeout_min: The maximum amount of time (in minutes) to sleep.
+    @param delay_sec: The amount to sleep (in seconds) between each attempt.
+
+    @return: The number of retry attempts in the case of exponential backoff.
+    """
+    # Estimate the max_retry in the case of exponential backoff:
+    # => total_sleep = sleep*sum(r=0..max_retry-1, backoff^r)
+    # => total_sleep = sleep( (1-backoff^max_retry) / (1-backoff) )
+    # => max_retry*ln(backoff) = ln(1-(total_sleep/sleep)*(1-backoff))
+    # => max_retry = ln(1-(total_sleep/sleep)*(1-backoff))/ln(backoff)
+    total_sleep = timeout_min * 60
+    numerator = math.log10(1-(total_sleep/delay_sec)*(1-backoff))
+    denominator = math.log10(backoff)
+    return int(math.ceil(numerator/denominator))
 
 
 class RetryingAFE(frontend.AFE):
@@ -26,13 +48,35 @@ class RetryingAFE(frontend.AFE):
 
 
     def run(self, call, **dargs):
-        @retry.retry_exponential(Exception, timeout_min=self.timeout_min,
-                                 delay_sec=self.delay_sec,
-                                 blacklist=[ImportError, error.RPCException,
-                                            proxy.ValidationError])
+        # exc_retry: We retry if this exception is raised.
+        # blacklist: Exceptions that we raise immediately if caught.
+        exc_retry = Exception
+        blacklist = (ImportError, error.RPCException, proxy.ValidationError)
+        backoff = 2
+        max_retry = convert_timeout_to_retry(backoff, self.timeout_min,
+                                             self.delay_sec)
+
         def _run(self, call, **dargs):
             return super(RetryingAFE, self).run(call, **dargs)
-        return _run(self, call, **dargs)
+
+        def handler(exc):
+            """Check if exc is an exc_retry or if it's blacklisted.
+
+            @param exc: An exception.
+
+            @return: True if exc is an exc_retry and is not
+                     blacklisted. False otherwise.
+            """
+            is_exc_to_check = isinstance(exc, exc_retry)
+            is_blacklisted = isinstance(exc, blacklist)
+            return is_exc_to_check and not is_blacklisted
+
+        # Set the keyword argument for GenericRetry
+        dargs['sleep'] = self.delay_sec
+        dargs['backoff_factor'] = backoff
+
+        return retry_util.GenericRetry(handler, max_retry, _run,
+                                       self, call, **dargs)
 
 
 class RetryingTKO(frontend.TKO):
