85517f25f31d920dc9371c1ad739d936c0efb770
Eric Caruso <ejcaruso@chromium.org>
2015-06-10 16:12:25 -0700

    power_Resume: retry with extra time after SuspendTimeout
    We have been seeing some TPM issues which make suspension take
    a lot longer than usual. In those cases, we still want device
    suspend/resume duration data, but we won't get it because the
    suspend cycle gets aborted early. Try again if we throw a
    warning, but with extra time.
    
    BUG=chrome-os-partner:26289
    TEST=test that both warning and device timings are shown when
      kernel suspend is deliberately delayed by 5 seconds on samus
    
    Change-Id: Ie83cd526edc92a0d1f86051e3d2ddfd500382dee
    Reviewed-on: https://chromium-review.googlesource.com/276783
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Commit-Queue: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Eric Caruso <ejcaruso@chromium.org>

--- client/site_tests/power_Resume/power_Resume.py
+++ client/site_tests/power_Resume/power_Resume.py
@@ -3,7 +3,15 @@
 # found in the LICENSE file.
 
 from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import power_suspend
+
+
+# In cases like crosbug.com/p/26289, we want results, but also want
+# to make sure we are suspending quickly enough. Retry with this amount
+# of extra suspend time to make sure we can get some results, even
+# if we throw a warning.
+EXTRA_TIME = 10
 
 
 class power_Resume(test.test):
@@ -16,6 +24,14 @@ class power_Resume(test.test):
 
 
     def run_once(self, max_devs_returned=10, seconds=0):
+        try:
+            self._suspend_once(max_devs_returned, seconds)
+        except error.TestWarn:
+            self._suspend_once(max_devs_returned, seconds + EXTRA_TIME)
+            raise
+
+
+    def _suspend_once(self, max_devs_returned, seconds):
         (results, device_times) = self._suspender.suspend(seconds)
 
         # return as keyvals the slowest n devices
