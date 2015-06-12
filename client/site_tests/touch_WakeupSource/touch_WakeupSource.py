fc3db80479c7252473925175cc87f4e99fda33e5
Katherine Threlkeld <kathrelkeld@chromium.org>
2015-06-12 11:16:23 -0700

    Autotest: make touch_WakeupSource support HID touchpads.
    Some new devices report whether their touchpads are a wake source differently.
    Rather than a file called wakeup in the folder associated with the touchpad,
    these devices report the wakeup status in the parent bus's folder.
    
    Change the test to search for the parent bus and check for this file if it
    cannot find a file for any of the inputs.
    
    BUG=chromium:499903
    TEST=ran on all existing combinations of touchpad/touchscreen wake
    
    Change-Id: I19baab797f3ded96edbae4dc1aab8e09653affc3
    Reviewed-on: https://chromium-review.googlesource.com/277151
    Reviewed-by: Kris Rambish <krisr@chromium.org>
    Tested-by: Katherine Threlkeld <kathrelkeld@chromium.org>
    Commit-Queue: Katherine Threlkeld <kathrelkeld@chromium.org>

--- client/site_tests/touch_WakeupSource/touch_WakeupSource.py
+++ client/site_tests/touch_WakeupSource/touch_WakeupSource.py
@@ -22,15 +22,16 @@ class touch_WakeupSource(touch_playback_test_base.touch_playback_test_base):
     _INVALID_BOARDS = ['x86-alex', 'x86-alex_he', 'x86-zgb', 'x86-zgb_he',
                        'x86-mario', 'stout', 'rambi']
 
-    def _is_wake_source(self, input_type):
-        """Return True if the given device is a wake source, else False.
+    def _find_wakeup_file(self, input_type):
+        """Return path to wakeup file or None.
 
-        Also, return false if the file does not exist.
+        If the file does not exist, check the parent bus for wakeup rules
+        as well, as is the setup for some devices.
 
         @param input_type: e.g. 'touchpad' or 'mouse'. See parent class for
                 all options.
 
-        @raises: TestError if it cannot find and interpret file contents.
+        @raises: TestError if input_type lacks required information.
 
         """
         device_dir = self.player.device_dirs[input_type]
@@ -40,6 +41,45 @@ class touch_WakeupSource(touch_playback_test_base.touch_playback_test_base):
         filename = os.path.join(device_dir, 'power', 'wakeup')
         if not os.path.isfile(filename):
             logging.info('%s not found for %s', filename, input_type)
+
+            # Look for wakeup file on parent bus instead.
+            event = self.player.nodes[input_type].split('/')[-1]
+
+            parent = None
+            i2c_devices_dir = os.path.join('/', 'sys', 'bus', 'i2c', 'devices')
+            for device_dir in os.listdir(i2c_devices_dir):
+                event_search = os.path.join(i2c_devices_dir, device_dir, '*',
+                                            'input', 'input*', event)
+                match_count = utils.run('ls %s 2>/dev/null | wc -l' % (
+                        event_search)).stdout.strip()
+                if int(match_count) > 0:
+                    parent = os.path.join(i2c_devices_dir, device_dir)
+                    break
+            if parent is None:
+                logging.info('Could not find parent bus for %s.', input_type)
+                return None
+
+            logging.info('Parent bus of %s is %s.', input_type, parent)
+            filename = os.path.join(parent, 'power', 'wakeup')
+            if not os.path.isfile(filename):
+                logging.info('%s not found either.', filename)
+                return None
+
+        return filename
+
+    def _is_wake_source(self, input_type):
+        """Return True if the given device is a wake source, else False.
+
+        If the file does not exist, return False.
+
+        @param input_type: e.g. 'touchpad' or 'mouse'. See parent class for
+                all options.
+
+        @raises: TestError if test cannot interpret the file contents.
+
+        """
+        filename = self._find_wakeup_file(input_type)
+        if filename is None:
             return False
 
         result = utils.run('cat %s' % filename).stdout.strip()
