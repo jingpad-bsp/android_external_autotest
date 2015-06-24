134cb045b40d54d30d6d3f88b596164076d32e4b
Dinesh Kumar Sunkara <dsunkara@google.com>
2015-06-24 16:18:34 -0700

    [autotest] Updated touch_ScrollDirection test to verify horizontal scrolling..
    Changed cursor centering behavior by adding a gesture file exclusively
    for centering the cursor. Now scroll_down, scroll_up, scroll_right
    and scroll_left gesture files only do scroll in respective direction.
    The newly added gesture file "center_cursor" will re-position the cursor
    using emulated mouse.
    Recorded horizontal scrolls on touchpad using two finger scroll.
    Updated touch_ScrollDirection test to verify horizontal scrolling.
    
    Sometimes, on slower machines, the test begins after the DOM has
    loaded (WaitForDocumentReadyStateToBeComplete) but before the page
    has fully loaded.
    Since mouse gestures don't work on the half-loaded page, add in a
    wait until the onLoad function has finished executing.
    
    Also, moved some of the setup code to subfunctions since run_once
    is getting too long.
    
    BUG=chromium:498333
    TEST=Tested on all affected boards.
    
    Change-Id: I8472e80123945cc5a579027716e70506f07b30bd
    Reviewed-on: https://chromium-review.googlesource.com/281781
    Tested-by: Dinesh Kumar Sunkara <dsunkara@chromium.org>
    Reviewed-by: Mussa Kiroga <mussa@chromium.org>
    Commit-Queue: Dinesh Kumar Sunkara <dsunkara@chromium.org>
    Trybot-Ready: Dinesh Kumar Sunkara <dsunkara@chromium.org>

--- client/site_tests/touch_ScrollDirection/touch_ScrollDirection.py
+++ client/site_tests/touch_ScrollDirection/touch_ScrollDirection.py
@@ -15,63 +15,133 @@ class touch_ScrollDirection(touch_playback_test_base.touch_playback_test_base):
     """Plays back scrolls and checks for correct page movement."""
     version = 1
 
-    def _check_scroll_direction(self, filepath, down):
+    _MOUSE_DESCRIPTION = 'apple_mouse.prop'
+    _DIRECTIONS = ['down', 'up', 'right', 'left']
+    _REVERSES = {'down': 'up', 'up': 'down', 'right': 'left', 'left': 'right'}
+
+
+    def _wait_for_page_ready(self):
+        utils.poll_for_condition(
+                lambda: self._tab.EvaluateJavaScript('pageReady'),
+                exception=error.TestError('Test page is not ready!'))
+
+
+    def _check_scroll_direction(self, filepath, expected):
         """Playback and raise error if scrolling does not match down value.
 
-        @param down: True if scrolling is supposed to be down; else False.
+        @param filepath: Gesture file's complete path for playback.
+        @param expected: String, expected direction in which test page scroll
+                         should move for the gesture file being played.
 
         @raises TestFail if actual scrolling did not match expected.
 
         """
-        self._set_default_scroll_position()
-        self._wait_for_default_scroll_position()
-        self._playback(filepath)
-        self._wait_for_scroll_position_to_settle()
+        is_vertical = expected == 'up' or expected == 'down'
+        is_down_or_right = expected == 'down' or expected == 'right'
 
-        delta = self._get_scroll_position() - self._DEFAULT_SCROLL
+        self._set_default_scroll_position(is_vertical)
+        self._wait_for_default_scroll_position(is_vertical)
+        self._playback(filepath)
+        self._wait_for_scroll_position_to_settle(is_vertical)
+        delta = self._get_scroll_position(is_vertical) - self._DEFAULT_SCROLL
         logging.info('Scroll delta was %d', delta)
-        if (down and delta <= 0) or (not down and delta >= 0):
+
+        # Below logic checks if the scroll has occurd in right direction taking
+        # into account Australian_scroll setting and the direction of
+        # scroll's movement and fails the test if scroll occured in wrong direction.
+        if (is_down_or_right and delta <= 0) or (not is_down_or_right and delta >= 0):
             raise error.TestFail('Page scroll was in wrong direction! '
                                  'Delta=%d, Australian=%s'
                                   % (delta, self._australian_state))
 
+
+    def _center_cursor(self):
+        """Playback and check whether cursor moved as recorded. Fail if needed.
+
+        @raises: TestError if cursor movement is not recorded in test_page.html.
+
+        """
+        self._reload_page()
+        self._wait_for_page_ready()
+        self._blocking_playback(self._center_cursor_file,
+                                touch_type='mouse')
+        if not self._tab.EvaluateJavaScript('cursorOnPage'):
+            raise error.TestError('Test page did not see cursor.')
+
+
     def _verify_scrolling(self):
         """Check scrolling direction for down then up."""
 
-        self._check_scroll_direction(filepath=self._down_filepath,
-                                     down=not self._australian_state)
-        self._check_scroll_direction(filepath=self._up_filepath,
-                                     down= self._australian_state)
+        if not self._australian_state:
+            for direction in self._DIRECTIONS:
+                self._check_scroll_direction(self._filepaths[direction],
+                                             direction)
+        else:
+            for direction in self._DIRECTIONS:
+                self._check_scroll_direction(self._filepaths[direction],
+                                             self._REVERSES[direction])
 
-    def run_once(self):
-        """Entry point of this test."""
-        # Check if corresponding playback files are available on DUT.
-        device = utils.get_board()
+
+    def _is_testable(self):
+        """Return True if test can run on this device, else False.
+
+        @raises: TestError if host has no touchpad when it should.
+
+        """
+        # Check if playback files are available on DUT to run test.
+        self._device = utils.get_board()
         gest_dir = os.path.join(self.bindir, 'gestures')
-        self._down_file = '%s_scroll_down' % device
-        self._up_file = '%s_scroll_up' % device
-        self._down_filepath = os.path.join(gest_dir, self._down_file)
-        self._up_filepath = os.path.join(gest_dir, self._up_file)
-        if not (os.path.exists(self._down_filepath) and
-                os.path.exists (self._up_filepath)):
-            logging.info('Missing gesture files, Aborting test')
-            return
+        self._center_cursor_file = os.path.join(gest_dir, 'center_cursor')
+        self._filepaths = {}
+
+        for direction in self._DIRECTIONS:
+            gest_file =  '%s_scroll_%s' % (self._device, direction)
+            self._filepaths[direction] = os.path.join(gest_dir, gest_file)
+            if not os.path.exists(self._filepaths[direction]):
+                logging.info('Missing gesture files, Aborting test')
+                return False
 
         # Raise error if no touchpad detected.
         if not self._has_touchpad:
-            raise error.TestFail('No touchpad found on this %s' % device)
+            raise error.TestError('No touchpad found on this %d' % self._device)
+
+        return True
+
+
+    def _page_setup(self, cr):
+        """Prepare for test by opening test page and centering cursor.
+
+        Navigate to test page, emulate a USB mouse, and center the cursor.
+
+        @raises: TestError if mouse emulation fails.
+
+        """
+        # Open test page.
+        cr.browser.SetHTTPServerDirectories(self.bindir)
+        self._tab = cr.browser.tabs[0]
+        self._tab.Navigate(cr.browser.http_server.UrlOf(
+                os.path.join(self.bindir, 'long_page.html')))
+        self._tab.WaitForDocumentReadyStateToBeComplete()
+        self._wait_for_page_ready()
+
+        # Emulate a USB test mouse and center cursor.
+        mouse_file = os.path.join(self.bindir, self._MOUSE_DESCRIPTION)
+        self._emulate_mouse(property_file=mouse_file)
+        if not self._has_mouse:
+            raise error.TestError('Emulated mouse not found on device.')
+        self._center_cursor()
+
+
+    def run_once(self):
+        """Entry point of this test."""
+        if not self._is_testable():
+            return
 
         # Log in and start test.
         with chrome.Chrome(autotest_ext=True) as cr:
             # Pass in the autotest extension.
             self._set_autotest_ext(cr.autotest_ext)
-
-            # Open test page.
-            cr.browser.SetHTTPServerDirectories(self.bindir)
-            self._tab = cr.browser.tabs[0]
-            self._tab.Navigate(cr.browser.http_server.UrlOf(
-                               os.path.join(self.bindir, 'long_page.html')))
-            self._tab.WaitForDocumentReadyStateToBeComplete()
+            self._page_setup(cr)
 
             # Check default scroll - Australian for touchscreens.
             self._australian_state = self._has_touchscreen
