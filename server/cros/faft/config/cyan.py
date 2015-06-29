a6cf37ea6d933407c391239bc60b2e489e52dd49
Icarus Sparry <icarus.w.sparry@intel.com>
2015-06-29 16:37:17 -0700

    Increase time before inserting USB drive
    Some FAFT tests enter recovery mode via crossystem,
    and these require a longer delay to remove and insert
    the media
    
    BUG=chrome-os-partner:41647
    TEST="test_that -b cyan $IP firmware_UserRequestRecovery" passes
    
    Change-Id: I6b145bfaca7fdd0acb7040970e854e31949f18de
    Signed-off-by: Icarus Sparry <icarus.w.sparry@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/282393
    Reviewed-by: danny chan <dchan@chromium.org>

--- server/cros/faft/config/cyan.py
+++ server/cros/faft/config/cyan.py
@@ -12,6 +12,7 @@ class Values(object):
     ec_capability = ['battery', 'charging', 'keyboard', 'lid', 'x86',
                      'usb', 'smart_usb_charge']
     firmware_screen = 28
+    usb_plug = 28
     long_rec_combo = True
     wp_voltage = 'pp1800'
     spi_voltage = 'pp1800'
