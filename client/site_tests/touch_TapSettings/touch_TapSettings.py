fe34681449531490b815cfd92fd9e9186e842916
Katherine Threlkeld <kathrelkeld@chromium.org>
2015-06-30 19:21:11 -0700

    Autotest: touch_TapSettings flake fix
    Sometimes, on slower machines, the test begins after the DOM has
    loaded (WaitForDocumentReadyStateToBeComplete) but before the page
    has fully loaded.
    Since mouse gestures don't work on the half-loaded page, add in a
    wait until the onLoad function has finished executing.
    
    Also, move some of the setup code to subfunctions since run_once
    is getting too long.
    
    TEST=failed sometimes on Daisy without fix, does not fail with it
    BUG=chromium:488282
    
    Change-Id: Ibb3ec27a4293ef2b785858ee13bb81b5146c66b9
    Reviewed-on: https://chromium-review.googlesource.com/282768
    Reviewed-by: Mussa Kiroga <mussa@chromium.org>
    Commit-Queue: Katherine Threlkeld <kathrelkeld@chromium.org>
    Tested-by: Katherine Threlkeld <kathrelkeld@chromium.org>

--- client/site_tests/touch_TapSettings/touch_TapSettings.py
+++ client/site_tests/touch_TapSettings/touch_TapSettings.py
@@ -21,6 +21,24 @@ class touch_TapSettings(touch_playback_test_base.touch_playback_test_base):
     _MOUSE_DESCRIPTION = 'apple_mouse.prop'
 
 
+    def _wait_for_page_ready(self):
+        utils.poll_for_condition(
+                lambda: self._tab.EvaluateJavaScript('pageReady'),
+                exception=error.TestError('Test page is not ready!'))
+
+    def _center_cursor(self):
+        """Playback and check whether cursor moved as recorded. Fail if needed.
+
+        @raises: TestError if cursor movement is not recorded in test_page.html.
+
+        """
+        self._reload_page()
+        self._wait_for_page_ready()
+        self._blocking_playback(self._center_cursor_file, touch_type='mouse')
+        cursorOnPage = bool(self._tab.EvaluateJavaScript('cursorOnPage'))
+        if not cursorOnPage:
+            raise error.TestError('Test page did not see cursor.')
+
     def _check_for_click(self, expected):
         """Playback and check whether tap-to-click occurred.  Fail if needed.
 
@@ -30,26 +48,13 @@ class touch_TapSettings(touch_playback_test_base.touch_playback_test_base):
         """
         expected_count = 1 if expected else 0
         self._reload_page()
+        self._wait_for_page_ready()
         self._playback(filepath=self._click_filepath)
         time.sleep(self._test_timeout)
         actual_count = int(self._tab.EvaluateJavaScript('clickCount'))
         if actual_count is not expected_count:
             raise error.TestFail('Expected clicks=%s, actual=%s.'
                                  % (expected_count, actual_count))
-
-
-    def _center_cursor(self):
-        """Playback and check whether cursor moved as recorded. Fail if needed.
-
-        @raises: TestError if cursor movement is not recorded in test_page.html.
-
-        """
-        self._reload_page()
-        self._blocking_playback(self._center_cursor_file, touch_type='mouse')
-        cursorOnPage = bool(self._tab.EvaluateJavaScript('cursorOnPage'))
-        if not cursorOnPage:
-            raise error.TestError('Test page did not see cursor.')
-
 
     def _check_for_drag(self, expected):
         """Playback and check whether tap dragging occurred.  Fail if needed.
@@ -59,16 +64,19 @@ class touch_TapSettings(touch_playback_test_base.touch_playback_test_base):
 
         """
         self._reload_page()
+        self._wait_for_page_ready()
         self._blocking_playback(filepath=self._drag_filepath)
         actual = self._tab.EvaluateJavaScript('movementOccurred')
         if actual is not expected:
             raise error.TestFail('Tap dragging movement was %s; expected %s.'
                                  % (actual, expected))
 
+    def _is_testable(self):
+        """Return True if test can run on this device, else False.
 
-    def run_once(self):
-        """Entry point of this test."""
+        @raises: TestError if host has no touchpad when it should.
 
+        """
         # Check if playback files are available on DUT to run test.
         device = utils.get_board()
         gest_dir = os.path.join(self.bindir, 'gestures')
@@ -80,30 +88,48 @@ class touch_TapSettings(touch_playback_test_base.touch_playback_test_base):
         if not (os.path.exists(self._click_filepath) and
                 os.path.exists (self._drag_filepath)):
             logging.info('Missing gesture files, Aborting test')
-            return
+            return False
 
         # Raise error if no touchpad detected.
         if not self._has_touchpad:
-            raise error.TestFail('No touchpad found on this %d' % device)
+            raise error.TestError('No touchpad found on this %d' % device)
+
+        return True
+
+    def _page_setup(self, cr):
+        """Prepare for test by opening test page and centering cursor.
+
+        Navigate to test page, emulate a USB mouse, and center the cursor.
+
+        @raises: TestError if mouse emulation fails.
+
+        """
+        # Open test page.
+        cr.browser.SetHTTPServerDirectories(self.bindir)
+        self._tab = cr.browser.tabs[0]
+        self._tab.Navigate(cr.browser.http_server.UrlOf(
+                os.path.join(self.bindir, 'test_page.html')))
+        self._tab.WaitForDocumentReadyStateToBeComplete()
+        self._wait_for_page_ready()
 
         # Emulate a USB test mouse and raise error if no mouse found.
         mouse_file = os.path.join(self.bindir, self._MOUSE_DESCRIPTION)
         self._emulate_mouse(property_file=mouse_file)
         if not self._has_mouse:
             raise error.TestError('Emulated mouse not found on device.')
+        self._center_cursor()
+
+    def run_once(self):
+        """Entry point of this test."""
+        if not self._is_testable():
+            return
 
         # Log in and start test.
         with chrome.Chrome(autotest_ext=True) as cr:
             # Pass in the autotest extension.
             self._set_autotest_ext(cr.autotest_ext)
 
-            # Open test page and position the cursor to the center of the page.
-            cr.browser.SetHTTPServerDirectories(self.bindir)
-            self._tab = cr.browser.tabs[0]
-            self._tab.Navigate(cr.browser.http_server.UrlOf(
-                    os.path.join(self.bindir, 'test_page.html')))
-            self._tab.WaitForDocumentReadyStateToBeComplete()
-            self._center_cursor()
+            self._page_setup(cr)
 
             # Check default setting values.
             logging.info('Checking for default setting values.')
