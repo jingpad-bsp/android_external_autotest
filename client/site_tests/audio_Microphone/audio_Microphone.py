a74762efe4100e47296800451095eb6597c979d1
Chinyue Chen <chinyue@chromium.org>
2015-07-16 12:26:32 +0800

    autotest: audio_Microphone - Add check for CRAS recording.
    We currently only verify microphone using ALSA utils. Add checking
    of CRAS recording to catch CRAS bug if any.
    
    BUG=chromium:510565
    TEST=Run audio_Microphone test on peppy.
    
    Change-Id: If511389b80ace3ebe3fe899557041c0b28728f11
    Reviewed-on: https://chromium-review.googlesource.com/285797
    Tested-by: Chinyue Chen <chinyue@chromium.org>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Commit-Queue: Chinyue Chen <chinyue@chromium.org>

--- client/site_tests/audio_Microphone/audio_Microphone.py
+++ client/site_tests/audio_Microphone/audio_Microphone.py
@@ -7,7 +7,7 @@ import tempfile
 
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.cros.audio import alsa_utils
+from autotest_lib.client.cros.audio import alsa_utils, cras_utils
 
 DURATION = 3
 TOLERANT_RATIO = 0.1
@@ -17,25 +17,41 @@ class audio_Microphone(test.test):
 
 
     def check_recorded_filesize(
-            self, filesize, duration, channels, bits, rate):
+            self, filesize, duration, channels, rate, bits=16):
         expected = duration * channels * (bits / 8) * rate
         if abs(float(filesize) / expected - 1) > TOLERANT_RATIO:
             raise error.TestFail('File size not correct: %d' % filesize)
 
 
-    def verify_capture(self, channels, rate, bits=16):
+    def verify_alsa_capture(self, channels, rate, bits=16):
         recorded_file = tempfile.NamedTemporaryFile()
         alsa_utils.record(
                 recorded_file.name, duration=DURATION, channels=channels,
                 bits=bits, rate=rate)
         self.check_recorded_filesize(
                 os.path.getsize(recorded_file.name),
-                DURATION, channels, bits, rate)
+                DURATION, channels, rate, bits)
+
+
+    def verify_cras_capture(self, channels, rate):
+        recorded_file = tempfile.NamedTemporaryFile()
+        cras_utils.capture(
+                recorded_file.name, duration=DURATION, channels=channels,
+                rate=rate)
+        self.check_recorded_filesize(
+                os.path.getsize(recorded_file.name),
+                DURATION, channels, rate)
 
 
     def run_once(self):
         # Mono and stereo capturing should work fine @ 44.1KHz and 48KHz.
-        self.verify_capture(1, 44100)
-        self.verify_capture(1, 48000)
-        self.verify_capture(2, 48000)
-        self.verify_capture(2, 44100)
+        # Verify recording using ALSA utils.
+        self.verify_alsa_capture(1, 44100)
+        self.verify_alsa_capture(1, 48000)
+        self.verify_alsa_capture(2, 48000)
+        self.verify_alsa_capture(2, 44100)
+        # Verify recording of CRAS.
+        self.verify_cras_capture(1, 44100)
+        self.verify_cras_capture(1, 48000)
+        self.verify_cras_capture(2, 48000)
+        self.verify_cras_capture(2, 44100)
