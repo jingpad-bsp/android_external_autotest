8f668216f3a43d60fa50f8e85c6ffa028f270d67
mukesh agrawal <quiche@chromium.org>
2015-07-16 10:38:00 -0700

    autotest (wifi): support beacon injection on more APs
    Some APs won't let us inject beacon frames, unless we
    have an AP interface up along with the monitor interface.
    
    To enable beacon injection on such APs, we have
    FrameSender set up an AP interface along-side its monitor
    interface.
    
    The AP interface isn't necessary for other frame types.
    But I'd like to avoid having the code think about that,
    if possible.
    
    BUG=chromium:510883
    TEST=suite:wifi_matfunc (on new hardware)
    TEST=network_WiFi_ConnectionIdentifier (on stumpy)
    TEST=network_WiFi_DisconnectClearsIP (on stumpy)
    TEST=network_WiFi_ProfileBasic (on stumpy)
    TEST=network_WiFi_RoamEndToEnd (on stumpy)
    TEST=network_WiFi_ChannelScanDwellTime (on stumpy)
    
    Change-Id: I9b9c60773fd799dee2df3a21316f068fd3eb85ef
    Reviewed-on: https://chromium-review.googlesource.com/287305
    Reviewed-by: mukesh agrawal <quiche@chromium.org>
    Commit-Queue: mukesh agrawal <quiche@chromium.org>
    Trybot-Ready: mukesh agrawal <quiche@chromium.org>
    Tested-by: mukesh agrawal <quiche@chromium.org>

--- server/cros/network/frame_sender.py
+++ server/cros/network/frame_sender.py
@@ -2,8 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from autotest_lib.server.cros.network import hostap_config
+
 class FrameSender(object):
     """Context manager for sending management frames."""
+
 
     def __init__(self, router, frame_type, channel, ssid_prefix=None,
                  num_bss=None, frame_count=None, delay=None, dest_addr=None,
@@ -22,6 +25,8 @@ class FrameSender(object):
         """
         self._router = router
         self._channel = channel
+        self._frequency = (
+            hostap_config.HostapConfig.get_frequency_for_channel(channel))
         self._frame_type = frame_type
         self._ssid_prefix = ssid_prefix
         self._num_bss = num_bss
@@ -29,23 +34,30 @@ class FrameSender(object):
         self._delay = delay
         self._dest_addr = dest_addr
         self._probe_resp_footer = probe_resp_footer
-        self._interface = None
+        self._ap_interface = None
+        self._injection_interface = None
         self._pid = None
 
+
     def __enter__(self):
-        self._interface = self._router.setup_management_frame_interface(
-                self._channel)
-        self._pid = self._router.send_management_frame(self._interface,
-                self._frame_type, self._channel, ssid_prefix=self._ssid_prefix,
-                num_bss=self._num_bss, frame_count=self._frame_count,
-                delay=self._delay, dest_addr=self._dest_addr,
-                probe_resp_footer=self._probe_resp_footer)
+        # Some chips don't allow beacon or probe response injection,
+        # unless an AP mode interface is present.
+        self._ap_interface = self._router.get_configured_interface(
+            '__ap', frequency=self._frequency)
+        self._injection_interface = self._router.get_configured_interface(
+            'monitor', same_phy_as=self._ap_interface)
+        self._pid = self._router.send_management_frame(
+            self._injection_interface,
+            self._frame_type, self._channel, ssid_prefix=self._ssid_prefix,
+            num_bss=self._num_bss, frame_count=self._frame_count,
+            delay=self._delay, dest_addr=self._dest_addr,
+            probe_resp_footer=self._probe_resp_footer)
         return self
 
 
     def __exit__(self, exception, value, traceback):
-        if self._interface:
-            self._router.release_interface(self._interface)
+        if self._injection_interface:
+            self._router.release_interface(self._injection_interface)
+            self._router.release_interface(self._ap_interface)
         if self._pid:
             self._router.host.run('kill %d' % self._pid, ignore_status=True)
-
