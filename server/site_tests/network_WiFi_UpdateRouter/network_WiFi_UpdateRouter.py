a5b2bc2fb64f1a5de063eeb6c66af64e063bbf03
Rebecca Silberstein <silberst@chromium.org>
2015-07-27 15:46:16 -0700

    autotest (wifi): update AP build for stumpy and panther
    Update the AP image for wifi cells to R46-7298.0.0-b23
    
    BUG=chromium:514437
    TEST=updated stumpy with test_that network_WiFi_UpdateRouter
    
    Change-Id: I8a186bf8e4b1d54ffc0f3cafad7af8c2c9ee0dc5
    Reviewed-on: https://chromium-review.googlesource.com/288754
    Reviewed-by: Kris Rambish <krisr@chromium.org>
    Tested-by: Rebecca Silberstein <silberst@chromium.org>
    Commit-Queue: Rebecca Silberstein <silberst@chromium.org>
    Trybot-Ready: Rebecca Silberstein <silberst@chromium.org>

--- server/site_tests/network_WiFi_UpdateRouter/network_WiFi_UpdateRouter.py
+++ server/site_tests/network_WiFi_UpdateRouter/network_WiFi_UpdateRouter.py
@@ -35,10 +35,10 @@ class network_WiFi_UpdateRouter(test.test):
     version = 1
 
     STABLE_VERSIONS = {
-            'stumpy': StableVersion('trybot-stumpy-test-ap/R45-7115.0.0-b22',
-                                    '7115.0.2015_05_27_1207'),
-            'panther': StableVersion('trybot-panther-test-ap/R45-7115.0.0-b22',
-                                     '7115.0.2015_05_27_1223'),
+            'stumpy': StableVersion('trybot-stumpy-test-ap/R46-7298.0.0-b23',
+                                    '7298.0.2015_07_24_1640'),
+            'panther': StableVersion('trybot-panther-test-ap/R46-7298.0.0-b23',
+                                     '7298.0.2015_07_24_1656'),
     }
 
 
