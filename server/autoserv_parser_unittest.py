888cfcaeeffdf302e25b066d9bef9ba535ebbc20
Dan Shi <dshi@chromium.org>
2015-07-31 15:49:00 -0700

    [autotest] Update autoserv parser to replace optparse with argparse to support unknown arguments
    This fix should avoid lab failures like bug 515339
    
    BUG=chromium:490456
    TEST=local run suite jobs, unittest
    
    Change-Id: I06776c080bac2ff438582637a4b93ef279a5ec8a
    Reviewed-on: https://chromium-review.googlesource.com/290106
    Tested-by: Dan Shi <dshi@chromium.org>
    Reviewed-by: Fang Deng <fdeng@chromium.org>
    Trybot-Ready: Dan Shi <dshi@chromium.org>
    Reviewed-by: Simran Basi <sbasi@chromium.org>
    Commit-Queue: Dan Shi <dshi@chromium.org>

--- server/autoserv_parser_unittest.py
+++ server/autoserv_parser_unittest.py
@@ -10,6 +10,9 @@ from autotest_lib.server import autoserv_parser
 
 
 class autoserv_parser_test(unittest.TestCase):
+    """Test autoserv_parser can parse command line correctly.
+    """
+
     def setUp(self):
         self.orig_sys_argv = sys.argv
 
@@ -17,13 +20,24 @@ class autoserv_parser_test(unittest.TestCase):
         sys.argv = self.orig_sys_argv
 
     def _get_parser(self):
+        """Get the parser class.
+        """
         # We resort to this vile hack because autoserv_parser is bad
         # enough to instantiate itself and replace its own class definition
         # with its instance at module import time.  Disgusting.
         return autoserv_parser.autoserv_parser.__class__()
 
     def test_control_file_args(self):
-        sys.argv = [None, '--args', '-y  -z foo --hello', 'controlfile']
+        """Test the parser can parse args and unknown args correctly.
+        """
+        sys.argv = ['autoserv', '--args', '-y  -z foo --hello', 'controlfile']
+        parser = self._get_parser()
+        parser.parse_args()
+        self.assertEqual(['controlfile', '-y', '-z', 'foo', '--hello'],
+                         parser.args)
+
+        sys.argv = ['autoserv', '--args', '-y  -z foo --hello', '--unknown_arg',
+                    'unknown_arg_val', 'controlfile']
         parser = self._get_parser()
         parser.parse_args()
         self.assertEqual(['controlfile', '-y', '-z', 'foo', '--hello'],
