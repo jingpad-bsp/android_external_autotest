6fb50112a27784eb80746ee84c8840593217b700
Katherine Threlkeld <kathrelkeld@chromium.org>
2015-07-31 16:54:11 -0700

    Autotest: update keyboard definition for input tests
    Playback autotests decide what the various inputs are based on the
    input properties.  Update the checked-against properties with
    ChromeOS's current definition of a "keyboard".  This definition
    currently includes things like the power button, so limit the label
    "keyboard" to qwerty inputs or number pads.  Label the other
    keyboards as such.
    
    TEST=Ran it on a variety of devices. Keyboards found and labeled.
    BUG=None
    
    Change-Id: I712cca54f2c174dde531bf1de64636a1e1bef18e
    Reviewed-on: https://chromium-review.googlesource.com/290079
    Reviewed-by: David Haddock <dhaddock@chromium.org>
    Tested-by: Katherine Threlkeld <kathrelkeld@chromium.org>
    Commit-Queue: Katherine Threlkeld <kathrelkeld@chromium.org>

--- client/cros/input_playback/input_playback.py
+++ client/cros/input_playback/input_playback.py
@@ -28,6 +28,49 @@ class InputPlayback(object):
                                'keyboard' : 'keyboard.prop'}
     _PLAYBACK_COMMAND = 'evemu-play --insert-slot0 %s < %s'
 
+    # Define a keyboard as anything with any keys #2 to #248 inclusive,
+    # as defined in the linux input header.  This definition includes things
+    # like the power button, so reserve the "keyboard" label for things with
+    # letters/numbers and define the rest as "other_keyboard".
+    _MINIMAL_KEYBOARD_KEYS = ['1', 'Q', 'SPACE']
+    _KEYBOARD_KEYS = [
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'MINUS', 'EQUAL',
+            'BACKSPACE', 'TAB', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O',
+            'P', 'LEFTBRACE', 'RIGHTBRACE', 'ENTER', 'LEFTCTRL', 'A', 'S', 'D',
+            'F', 'G', 'H', 'J', 'K', 'L', 'SEMICOLON', 'APOSTROPHE', 'GRAVE',
+            'LEFTSHIFT', 'BACKSLASH', 'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+            'COMMA', 'DOT', 'SLASH', 'RIGHTSHIFT', 'KPASTERISK', 'LEFTALT',
+            'SPACE', 'CAPSLOCK', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8',
+            'F9', 'F10', 'NUMLOCK', 'SCROLLLOCK', 'KP7', 'KP8', 'KP9',
+            'KPMINUS', 'KP4', 'KP5', 'KP6', 'KPPLUS', 'KP1', 'KP2', 'KP3',
+            'KP0', 'KPDOT', 'ZENKAKUHANKAKU', '102ND', 'F11', 'F12', 'RO',
+            'KATAKANA', 'HIRAGANA', 'HENKAN', 'KATAKANAHIRAGANA', 'MUHENKAN',
+            'KPJPCOMMA', 'KPENTER', 'RIGHTCTRL', 'KPSLASH', 'SYSRQ', 'RIGHTALT',
+            'LINEFEED', 'HOME', 'UP', 'PAGEUP', 'LEFT', 'RIGHT', 'END', 'DOWN',
+            'PAGEDOWN', 'INSERT', 'DELETE', 'MACRO', 'MUTE', 'VOLUMEDOWN',
+            'VOLUMEUP', 'POWER', 'KPEQUAL', 'KPPLUSMINUS', 'PAUSE', 'SCALE',
+            'KPCOMMA', 'HANGEUL', 'HANGUEL', 'HANJA', 'YEN', 'LEFTMETA',
+            'RIGHTMETA', 'COMPOSE', 'STOP', 'AGAIN', 'PROPS', 'UNDO', 'FRONT',
+            'COPY', 'OPEN', 'PASTE', 'FIND', 'CUT', 'HELP', 'MENU', 'CALC',
+            'SETUP', 'WAKEUP', 'FILE', 'SENDFILE', 'DELETEFILE', 'XFER',
+            'PROG1', 'PROG2', 'WWW', 'MSDOS', 'COFFEE', 'SCREENLOCK',
+            'DIRECTION', 'CYCLEWINDOWS', 'MAIL', 'BOOKMARKS', 'COMPUTER',
+            'BACK', 'FORWARD', 'CLOSECD', 'EJECTCD', 'EJECTCLOSECD', 'NEXTSONG',
+            'PLAYPAUSE', 'PREVIOUSSONG', 'STOPCD', 'RECORD', 'REWIND', 'PHONE',
+            'ISO', 'CONFIG', 'HOMEPAGE', 'REFRESH', 'EXIT', 'MOVE', 'EDIT',
+            'SCROLLUP', 'SCROLLDOWN', 'KPLEFTPAREN', 'KPRIGHTPAREN', 'NEW',
+            'REDO', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20',
+            'F21', 'F22', 'F23', 'F24', 'PLAYCD', 'PAUSECD', 'PROG3', 'PROG4',
+            'DASHBOARD', 'SUSPEND', 'CLOSE', 'PLAY', 'FASTFORWARD', 'BASSBOOST',
+            'PRINT', 'HP', 'CAMERA', 'SOUND', 'QUESTION', 'EMAIL', 'CHAT',
+            'SEARCH', 'CONNECT', 'FINANCE', 'SPORT', 'SHOP', 'ALTERASE',
+            'CANCEL', 'BRIGHTNESSDOWN', 'BRIGHTNESSUP', 'MEDIA',
+            'SWITCHVIDEOMODE', 'KBDILLUMTOGGLE', 'KBDILLUMDOWN', 'KBDILLUMUP',
+            'SEND', 'REPLY', 'FORWARDMAIL', 'SAVE', 'DOCUMENTS', 'BATTERY',
+            'BLUETOOTH', 'WLAN', 'UWB', 'UNKNOWN', 'VIDEO_NEXT', 'VIDEO_PREV',
+            'BRIGHTNESS_CYCLE', 'BRIGHTNESS_AUTO', 'BRIGHTNESS_ZERO',
+            'DISPLAY_OFF', 'WWAN', 'WIMAX', 'RFKILL', 'MICMUTE']
+
 
     def __init__(self):
         self.nodes = {} # e.g. /dev/input/event4
@@ -146,8 +189,13 @@ class InputPlayback(object):
                     return 'touchscreen'
             if props.find('BTN_LEFT') >= 0:
                 return 'touchscreen'
-        if props.find('KEY_ESC') >= 0:
-            return 'keyboard'
+        if props.find('KEY_') >= 0:
+            for key in self._MINIMAL_KEYBOARD_KEYS:
+                if props.find('KEY_%s' % key) >= 0:
+                    return 'keyboard'
+            for key in self._KEYBOARD_KEYS:
+                if props.find('KEY_%s' % key) >= 0:
+                    return 'other_keyboard'
         return
 
 
