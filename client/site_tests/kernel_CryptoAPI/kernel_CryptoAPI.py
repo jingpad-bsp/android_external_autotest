1a2edd4df71e7f42577797cf201f276a9cda888e
Brian Norris <briannorris@chromium.org>
2015-08-06 17:13:20 -0700

    [autotest] kernel_CryptoAPI: add tests for sha1, md5, and sha512 digests
    Simple sanity tests to make sure the API is working. Compare against
    Python's hashlib for reference.
    
    Run these only >=3.8 kernels, since this API only works when we have
    AF_ALG enabled.
    
    BUG=chromium:462835
    CQ-DEPEND=CL:290237,CL:290342,CL:290324,CL:290158
    TEST=kernel_CryptoAPI
    
    Change-Id: I597da68c12b9f97698f0ed45140b30dffed98e39
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/291424
    Reviewed-by: Kees Cook <keescook@chromium.org>

--- client/site_tests/kernel_CryptoAPI/kernel_CryptoAPI.py
+++ client/site_tests/kernel_CryptoAPI/kernel_CryptoAPI.py
@@ -9,6 +9,40 @@ from autotest_lib.client.common_lib import error
 from autotest_lib.client.bin import test
 from autotest_lib.client.bin import utils
 
+import ctypes
+import hashlib
+import logging
+import binascii
+
+
+AF_ALG = 38
+SOCK_SEQPACKET = 5
+
+class sockaddr_alg(ctypes.Structure):
+    """
+    A python definition of the same struct from <linux/if_alg.h>
+
+    struct sockaddr_alg {
+            __u16   salg_family;
+            __u8    salg_type[14];
+            __u32   salg_feat;
+            __u32   salg_mask;
+            __u8    salg_name[64];
+    };
+    """
+    _fields_ = [
+        ('salg_family', ctypes.c_uint16),
+        ('salg_type', ctypes.c_char * 14),
+        ('salg_feat', ctypes.c_uint32),
+        ('salg_mask', ctypes.c_uint32),
+        ('salg_name', ctypes.c_char * 64),
+    ]
+
+
+    def __init__(self, alg_family, alg_type, alg_name, alg_feat=0, alg_mask=0):
+        super(sockaddr_alg, self).__init__(alg_family, alg_type, alg_feat,
+                                           alg_mask, alg_name)
+
 
 class kernel_CryptoAPI(test.test):
     """
@@ -44,6 +78,109 @@ class kernel_CryptoAPI(test.test):
                                  module)
 
 
+    def do_ifalg_digest(self, name, data, outlen):
+        """
+        Use ctypes to run a digest through one of the available kernel ifalg
+        digest types
+
+        @param name: digest name
+        @param data: string data to digest
+        @param outlen: length of the digest output (e.g., SHA1 is 160-bit, so
+                outlen==20)
+        @param return string containing the output digest, or None if
+                experiencing an error
+        """
+        libc = ctypes.CDLL("libc.so.6", use_errno=True)
+
+        # If you don't specify the function parameters this way, ctypes may try
+        # to treat pointers as 32-bit (and then later sign-extend them)
+        libc.socket.argtypes = [ ctypes.c_int, ctypes.c_int, ctypes.c_int ]
+        libc.bind.argtypes = [ ctypes.c_int, ctypes.c_void_p, ctypes.c_int ]
+        libc.send.argtypes = [ ctypes.c_int, ctypes.c_void_p, ctypes.c_size_t,
+                               ctypes.c_int ]
+        libc.recv.argtypes = [ ctypes.c_int, ctypes.c_void_p, ctypes.c_size_t,
+                               ctypes.c_int ]
+
+        sock = libc.socket(AF_ALG, SOCK_SEQPACKET, 0)
+        if sock == -1:
+            libc.perror("socket")
+            return None
+
+        alg = sockaddr_alg(AF_ALG, "hash", name)
+        if libc.bind(sock, ctypes.addressof(alg), ctypes.sizeof(alg)) == -1:
+            libc.perror("bind")
+            return None
+
+        fd = libc.accept(sock, None, 0)
+        if fd == -1:
+            libc.perror("accept")
+            return None
+
+        message = ctypes.create_string_buffer(data, len(data))
+        if libc.send(fd, ctypes.addressof(message), ctypes.sizeof(message), 0) == -1:
+            libc.perror("send")
+            return None
+
+        out = (ctypes.c_uint8 * outlen)()
+        ret = libc.recv(fd, ctypes.addressof(out), ctypes.sizeof(out), 0)
+        if ret == -1:
+            libc.perror("recv")
+            return None
+
+        h = ctypes.string_at(ctypes.addressof(out), ret)
+
+        libc.close(sock)
+
+        return h
+
+
+    def test_digest(self, name, lib, data):
+        """
+        Run a digest through both the kernel UAPI and through hashlib, throwing
+        an error if the two don't match
+
+        @param name: name of the digest (according to AF_ALG)
+        @param lib: a hashlib digest object
+        @param data: data to digest
+        """
+
+        logging.info("Testing digest %s", name)
+
+        h1 = self.do_ifalg_digest(name, data, lib.digestsize)
+        if h1 is None:
+            raise error.TestFail("ifalg digest %s failed", name)
+
+        lib.update(data)
+        h2 = lib.digest()
+
+        if h1 != h2:
+            logging.error("%s: digests do not match", name)
+            logging.error(" hash 1: %s", binascii.hexlify(h1))
+            logging.error(" hash 2: %s", binascii.hexlify(h2))
+            raise error.TestFail("digest mismatch (%s)" % name)
+
+        logging.debug("hash 1: %s", binascii.hexlify(h1))
+        logging.debug("hash 2: %s", binascii.hexlify(h2))
+
+
+    def test_digests(self, data):
+        """
+        Test several digests, using both the kernel crypto APIs and python
+        hashlib
+
+        @param data: the data to digest
+        """
+
+        digests = [
+                ( "sha1", hashlib.sha1()),
+                ( "md5", hashlib.md5()),
+                ( "sha512", hashlib.sha512()),
+        ]
+
+        for (name, lib) in digests:
+            self.test_digest(name, lib, data)
+
+
     def run_once(self):
         # crypto tests only work with AF_ALG support, so run only on >=3.8
         # kernels
@@ -53,3 +190,5 @@ class kernel_CryptoAPI(test.test):
 
         module = "test_module"
         self.try_load_mod(module)
+
+        self.test_digests("This is a not-so-secret message")
