562a08611915e3c0bd7e0430c3946fbc44907b93
David Haddock <dhaddock@chromium.org>
2015-09-08 18:16:57 -0700

    Allow image comparison diff viewer to run without a web server.
    The results page is just a HTML stored in Google Storage. This change
    allows a single html page to show all the information by encoding images in
    base64 and having the css/js in the html header.
    
    TEST=None
    BUG=None
    
    Change-Id: I5fbb7ec969913633535c6ef72118c1f4a2d875ab
    Reviewed-on: https://chromium-review.googlesource.com/298077
    Commit-Ready: David Haddock <dhaddock@chromium.org>
    Tested-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Katherine Threlkeld <kathrelkeld@chromium.org>

--- client/cros/image_comparison/diffviewer/code.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $(".thumb").click(function() {
-    $("#fullsizeimage").attr("src", $(this).attr("src"));
-  });
-});
--- client/cros/image_comparison/publisher.py
+++ client/cros/image_comparison/publisher.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import base64
 import glob
 import logging
 import os
@@ -17,6 +18,49 @@ class ImageDiffPublisher(object):
 
     VIEWER_FILES = '/usr/local/autotest/cros/image_comparison/diffviewer/*'
 
+    JS = """
+      $(document).ready(function() {
+          $("#fullsizeimage").attr("src", $("#golden").attr("src"));
+          $(".thumb").click(function() {
+              $("#fullsizeimage").attr("src", $(this).attr("src"));
+          });
+      });
+    """
+
+    CSS = """
+     #testrundetails {
+          height: 200px;
+        }
+
+        #thumbnails {
+          top: 250px;
+          left: 10px;
+          width: 260px;
+          margin: 0px;
+          padding: 0px;
+          position: absolute;
+        }
+
+        .thumb {
+          width: 200px;
+          height: auto;
+          cursor: pointer;
+          border: 3px solid black;
+        }
+
+        .thumb:hover {
+          border: 3px solid blue;
+        }
+
+        #content {
+          margin-left: 260px;
+        }
+
+        .tagname {
+           font-weight: bold;
+        }
+    """
+
     def __init__(self, results_folder):
         """
         @param results_folder: path, where to publish to
@@ -26,7 +70,7 @@ class ImageDiffPublisher(object):
     def publish(self, golden_image_path, test_image_path, diff_image_path,
                 tags):
         """
-        Move three images and viewer files to the results folder.
+        Move viewer files to the results folder and base64 encode the images.
         Write tags to HTML file.
 
         @param golden_image_path: path, complete path to a golden image.
@@ -36,34 +80,39 @@ class ImageDiffPublisher(object):
         """
 
         try:
-            # Copy the html, css and js needed for the viewer to resultsdir
+            # Copy the files needed for the viewer to resultsdir
             for diff_viewer_file in glob.glob(self.VIEWER_FILES):
                 shutil.copy(diff_viewer_file, self.results_folder)
 
-            output_folder = self.results_folder
-            shutil.move(golden_image_path, os.path.join(output_folder,
-                                                        'golden.png'))
-            shutil.move(test_image_path, os.path.join(output_folder,
-                                                      'test.png'))
-            shutil.move(diff_image_path, os.path.join(output_folder,
-                                                      'diff.png'))
         except shutil.Error as error:
-            logging.debug('Failed to copy all images and viewer files to '
-                          'results directory.')
+            logging.debug('Failed to copy files to results directory')
             logging.debug(error)
             raise
 
-        self._write_tags_to_html(output_folder, tags)
+        # Encode the images to base64
+        base64_images = []
+        with open(golden_image_path, "rb") as image_file:
+            base64_images.append(base64.b64encode(image_file.read()))
+        with open(test_image_path, "rb") as image_file:
+            base64_images.append(base64.b64encode(image_file.read()))
+        with open(diff_image_path, "rb") as image_file:
+            base64_images.append(base64.b64encode(image_file.read()))
 
-    def _write_tags_to_html(self, output_folder, tags,
-                            html_filename='index.html'):
+        # Append all of the things we push to the html template
+        tags = [self.CSS, self.JS] + tags + base64_images
+
+        html_file_fullpath = os.path.join(self.results_folder, 'index.html')
+        self._write_tags_to_html(tags, html_file_fullpath)
+
+    def _write_tags_to_html(self, tags, html_filename):
         """
         Writes tags to the HTML file
-        """
-        html_file_fullpath = os.path.join(output_folder,
-                                          html_filename)
 
-        with open(html_file_fullpath, 'r+') as f:
+        @param tags the tags to write into the html template
+        @param html_filename the full path to the html template
+        """
+
+        with open(html_filename, 'r+') as f:
             html = f.read()
             formatted_html = html.format(*tags)
             f.seek(0)
