bb2cd4945628d904d357f3e327570243d39e9599
Cheng-Yi Chiang <cychiang@chromium.org>
2015-09-18 18:13:49 -0700

    [autotest] audio_test_utils: Add method to check audio nodes
    Add audio_test_utils module to put audio test utilities shared by
    audio_Audio* tests.
    Add a method to check the audio nodes selected by Cros device are as
    expected.
    
    BUG=chromium:524944
    TEST=not used yet
    
    Change-Id: I11471605704d16b78f89d71cbf116272fdbe7eb6
    Reviewed-on: https://chromium-review.googlesource.com/301250
    Commit-Ready: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@chromium.org>

---
+++ client/cros/chameleon/audio_test_utils.py
@@ -0,0 +1,31 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""This module provides the test utilities for audio tests using chameleon."""
+
+# TODO (cychiang) Move test utilities from chameleon_audio_helpers
+# to this module.
+
+from autotest_lib.client.common_lib import error
+
+def check_audio_nodes(audio_facade, audio_nodes):
+    """Checks the node selected by Cros device is correct.
+
+    @param audio_facade: A RemoteAudioFacade to access audio functions on
+                         Cros device.
+
+    @param audio_nodes: A tuple (out_audio_nodes, in_audio_nodes) containing
+                        expected selected output and input nodes.
+
+    @raises: error.TestFail if the nodes selected by Cros device are not expected.
+
+    """
+    curr_out_nodes, curr_in_nodes = audio_facade.get_selected_node_types()
+    out_audio_nodes, in_audio_nodes = audio_nodes
+    if sorted(curr_in_nodes) != sorted(in_audio_nodes):
+        raise error.TestFail('Wrong input node(s) selected %s '
+                'instead %s!' % (str(curr_in_nodes), str(in_audio_nodes)))
+    if sorted(curr_out_nodes) != sorted(out_audio_nodes):
+        raise error.TestFail('Wrong output node(s) selected %s '
+                'instead %s!' % (str(curr_out_nodes), str(out_audio_nodes)))
