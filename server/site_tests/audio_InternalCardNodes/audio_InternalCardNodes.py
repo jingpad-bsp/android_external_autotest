c4b6cc1332c9bb368c3751d298009a9fecd694bc
Cheng-Yi Chiang <cychiang@chromium.org>
2015-09-20 23:39:43 -0700

    [autotest] audio_InternalCardNodes: Add test to check plugged nodes
    Add a test to check plugged nodes on Cros device before and after
    plugging audio jack.
    
    BUG=chromium:532713
    TEST=run the test
    
    Change-Id: I93cf4193708721f7c9763534fffd3c1e8f60e95a
    Reviewed-on: https://chromium-review.googlesource.com/301177
    Commit-Ready: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@chromium.org>

---
+++ server/site_tests/audio_InternalCardNodes/audio_InternalCardNodes.py
@@ -0,0 +1,62 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""This is a server side test to check nodes created for internal card."""
+
+import logging
+import os
+import time
+
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.cros.chameleon import audio_test_utils
+from autotest_lib.client.cros.chameleon import chameleon_audio_ids
+from autotest_lib.client.cros.chameleon import chameleon_audio_helper
+from autotest_lib.server.cros.audio import audio_test
+from autotest_lib.server.cros.multimedia import remote_facade_factory
+
+
+class audio_InternalCardNodes(audio_test.AudioTest):
+    """Server side test to check audio nodes for internal card.
+
+    This test talks to a Chameleon board and a Cros device to verify
+    audio nodes created for internal cards are correct.
+
+    """
+    version = 1
+    DELAY_AFTER_BINDING = 0.5
+
+    def run_once(self, host):
+        chameleon_board = host.chameleon
+        factory = remote_facade_factory.RemoteFacadeFactory(host)
+        audio_facade = factory.create_audio_facade()
+
+        chameleon_board.reset()
+
+        jack_plugger = chameleon_board.get_audio_board().get_jack_plugger()
+
+        expected_plugged_nodes_without_audio_jack = (
+                ['INTERNAL_SPEAKER'],
+                ['INTERNAL_MIC', 'POST_DSP_LOOPBACK',
+                 'POST_MIX_LOOPBACK'])
+
+        expected_plugged_nodes_with_audio_jack = (
+                ['INTERNAL_SPEAKER', 'HEADPHONE'],
+                ['INTERNAL_MIC', 'MIC', 'POST_DSP_LOOPBACK',
+                 'POST_MIX_LOOPBACK'])
+
+        audio_test_utils.check_plugged_nodes(
+                audio_facade, expected_plugged_nodes_without_audio_jack)
+
+        try:
+            jack_plugger.plug()
+
+            audio_test_utils.check_plugged_nodes(
+                    audio_facade, expected_plugged_nodes_with_audio_jack)
+
+        finally:
+            jack_plugger.unplug()
+
+        audio_test_utils.check_plugged_nodes(
+                audio_facade, expected_plugged_nodes_without_audio_jack)
+
