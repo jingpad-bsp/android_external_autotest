8eb72af6354ef8e3b8cc81b3b2d9f1282412cea8
Gilad Arnold <garnold@chromium.org>
2015-10-20 12:26:20 -0700

    Brillo: Separate test-specific setup from test launching.
    * Pull staging related logic out of brillo_test_launcher.py into a new
      brillo_aue2e_test_setup.py script.
    
    * Pull out common setup logic into a new brillo_common.py, including
      argument parsing, logger initialization and Moblab host allocation.
    
    * Implement payload generation (in addition to staging) in
      brillo_aue2e_test_setup.py.
    
    BUG=b:24774860
    TEST=brillo_aue2e_test_setup.py generates a payload and runs
    brillo_test_launcher.py successfully.
    
    Change-Id: Ia73e41e6be1ab91f9e9b818a87726499c5eb8ada
    Reviewed-on: https://chromium-review.googlesource.com/307835
    Commit-Ready: Gilad Arnold <garnold@chromium.org>
    Tested-by: Gilad Arnold <garnold@chromium.org>
    Reviewed-by: Simran Basi <sbasi@chromium.org>
    Reviewed-by: Dan Shi <dshi@chromium.org>

---
+++ site_utils/brillo_common.py
@@ -0,0 +1,112 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import logging
+
+import common
+from autotest_lib.client.common_lib import error
+from autotest_lib.server import hosts
+from autotest_lib.server.hosts import moblab_host
+
+
+_LOGGING_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+
+# Running against a virtual machine has several intricacies that we need to
+# adjust for. Namely SSH requires the use of 'localhost' while HTTP requires
+# the use of '127.0.0.1'. Also because we are forwarding the ports from the VM
+# to the host system, the ports to use for these services are also different
+# from running on a physical machine.
+_VIRT_MACHINE_SSH_ADDR = 'localhost:9222'
+_VIRT_MACHINE_AFE_ADDR = '127.0.0.1:8888'
+_VIRT_MACHINE_DEVSERVER_PORT = '7777'
+_PHYS_MACHINE_DEVSERVER_PORT = '8080'
+
+
+class BrilloTestError(Exception):
+    """A general error while testing Brillo."""
+
+
+class BrilloMoblabInitializationError(BrilloTestError):
+    """An error during Moblab initialization or handling."""
+
+
+def get_moblab_and_devserver_port(moblab_hostname):
+    """Initializes and returns a MobLab Host Object.
+
+    @params moblab_hostname: The Moblab hostname, None if using a local virtual
+                             machine.
+
+    @returns A pair consisting of a MoblabHost and a devserver port.
+
+    @raise BrilloMoblabInitializationError: Failed to set up the Moblab.
+    """
+    if moblab_hostname:
+        web_address = moblab_hostname
+        devserver_port = _PHYS_MACHINE_DEVSERVER_PORT
+    else:
+        moblab_hostname = _VIRT_MACHINE_SSH_ADDR
+        web_address = _VIRT_MACHINE_AFE_ADDR
+        devserver_port = _VIRT_MACHINE_DEVSERVER_PORT
+
+    try:
+        host = hosts.create_host(moblab_hostname,
+                                 host_class=moblab_host.MoblabHost,
+                                 web_address=web_address,
+                                 retain_image_storage=True)
+    except error.AutoservRunError as e:
+        raise BrilloMoblabInitializationError(
+                'Unable to connect to the MobLab: %s' % e)
+
+    try:
+        host.afe.get_hosts()
+    except Exception as e:
+        raise BrilloMoblabInitializationError(
+                "Unable to communicate with the MobLab's web frontend, "
+                "please verify that it is up and running at http://%s/\n"
+                "Error: %s" % (host.web_address, e))
+
+    return host, devserver_port
+
+
+def parse_args(description, setup_parser=None, validate_args=None):
+    """Parse command-line arguments.
+
+    @param description: The script description in the help message.
+    @param setup_parser: Function that takes a parser object and adds
+                         script-specific options to it.
+    @param validate_args: Function that takes a parser object and the parsed
+                          arguments and validates the arguments. It should use
+                          parser.error() to report errors.
+
+    @return Parsed and validated arguments.
+    """
+    parser = argparse.ArgumentParser(description=description)
+    if setup_parser:
+        setup_parser(parser)
+
+    # Add common options.
+    parser.add_argument('-m', '--moblab_host',
+                        help='MobLab hostname or IP to launch tests. If this '
+                             'argument is not provided, the test launcher '
+                             'will attempt to test a local virtual machine '
+                             'instance of MobLab.')
+    parser.add_argument('-a', '--adb_host',
+                        help='Hostname or IP of the adb_host connected to the '
+                             'Brillo DUT. Default is to assume it is connected '
+                             'directly to the MobLab.')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Print log statements.')
+
+    args = parser.parse_args()
+
+    # Configure the root logger.
+    logging.getLogger().setLevel(logging.DEBUG if args.debug else logging.INFO)
+    for log_handler in logging.getLogger().handlers:
+        log_handler.setFormatter(logging.Formatter(fmt=_LOGGING_FORMAT))
+
+    if validate_args:
+        validate_args(parser, args)
+
+    return args
--- site_utils/brillo_test_launcher.py
+++ site_utils/brillo_test_launcher.py
@@ -3,14 +3,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import argparse
-import httplib
 import logging
 import os
 import sys
 import tempfile
 import time
-import urllib2
 
 import common
 try:
@@ -25,61 +22,35 @@ except ImportError:
     # Restart the script so python now finds the autotest site-packages.
     sys.exit(os.execv(__file__, sys.argv))
 from autotest_lib.client.common_lib import control_data
-from autotest_lib.client.common_lib import error
-from autotest_lib.server import hosts
 from autotest_lib.server import utils
-from autotest_lib.server.hosts import moblab_host
 from autotest_lib.server.cros.dynamic_suite import control_file_getter
 from autotest_lib.server.cros.dynamic_suite import tools
+from autotest_lib.server.hosts import moblab_host
+from autotest_lib.site_utils import brillo_common
 from autotest_lib.site_utils import run_suite
 
 
-LOGGING_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
-MOBLAB_STATIC_DIR = '/mnt/moblab/static'
-MOBLAB_TMP_DIR = os.path.join(MOBLAB_STATIC_DIR, 'tmp')
-TARGET_IMAGE_NAME = 'brillo/target'
-DEVSERVER_STAGE_URL_TEMPLATE = ('http://%(moblab)s:%(port)s/stage?local_path='
-                                '%(staged_dir)s&artifacts=full_payload')
-AFE_JOB_PAGE_TEMPLATE = ('http://%(moblab)s/afe/#tab_id=view_job&object_id='
-                         '%(job_id)s')
-AFE_HOST_PAGE_TEMPLATE = ('http://%(moblab)s/afe/#tab_id=view_host&object_id='
-                          '%(host_id)s')
-
-# Running against a virtual machine has several intricacies that we need to
-# adjust for. Namely SSH requires the use of 'localhost' while HTTP requires
-# the use of '127.0.0.1'. Also because we are forwarding the ports from the VM
-# to the host system, the ports to use for these services are also different
-# from running on a physical machine.
-VIRT_MACHINE_SSH_ADDR = 'localhost:9222'
-VIRT_MACHINE_AFE_ADDR = '127.0.0.1:8888'
-VIRT_MACHINE_DEVSERVER_PORT = '7777'
-PHYS_MACHINE_DEVSERVER_PORT = '8888'
-QUICKMERGE_LIST = ['client/',
-                   'global_config.ini',
-                   'server/',
-                   'site_utils/',
-                   'test_suites/',
-                   'utils/']
+_AFE_JOB_PAGE_TEMPLATE = ('http://%(moblab)s/afe/#tab_id=view_job&'
+                          'object_id=%(job_id)s')
+_AFE_HOST_PAGE_TEMPLATE = ('http://%(moblab)s/afe/#tab_id=view_host&'
+                           'object_id=%(host_id)s')
+_QUICKMERGE_LIST = ('client/',
+                    'global_config.ini',
+                    'server/',
+                    'site_utils/',
+                    'test_suites/',
+                    'utils/')
 
 
-class BrilloTestError(Exception):
-    """A general error during test invocation."""
+class BrilloTestExecutionError(brillo_common.BrilloTestError):
+    """An error while launching and running a test."""
 
 
-class PayloadStagingError(BrilloTestError):
-    """A failure that occurred while staging an update payload."""
+def setup_parser(parser):
+    """Add parser options.
 
-
-def parse_args():
-    """Parse command line arguments"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--debug', action='store_true',
-                        help='Print log statements.')
-    parser.add_argument('-q', '--quickmerge', action='store_true',
-                        help='Rsync over modified Autotest code.')
-    parser.add_argument('-p', '--payload',
-                        help='Path to the update payload for autoupdate '
-                             'testing.')
+    @param parser: argparse.ArgumentParser of the script.
+    """
     parser.add_argument('-t', '--test_name',
                         help="Name of the test to run. This is either the "
                              "name in the test's default control file e.g. "
@@ -88,22 +59,11 @@ def parse_args():
     parser.add_argument('-A', '--test_arg', metavar='NAME=VAL',
                         dest='test_args', default=[], action='append',
                         help='An argument to pass to the test.')
-    parser.add_argument('-m', '--moblab_host',
-                        help='MobLab hostname or IP to launch tests. If this '
-                             'argument is not provided, the test launcher '
-                             'will attempt to test a local virtual machine '
-                             'instance of MobLab.')
-    parser.add_argument('-v', '--virtual_machine', action='store_true',
-                        help='MobLab is running locally on a virtual machine.')
-    parser.add_argument('-a', '--adb_host',
-                        help='Hostname or IP of the adb_host connected to the '
-                             'Brillo DUT. Default is to assume it is connected '
-                             'directly to the MobLab.')
-
-    return parser.parse_args()
+    parser.add_argument('-q', '--quickmerge', action='store_true',
+                        help='Rsync over modified Autotest code.')
 
 
-def add_adbhost(moblab, adb_hostname):
+def add_adb_host(moblab, adb_hostname):
     """Add the ADB host to the MobLab's host list.
 
     @param moblab: MoblabHost representing the MobLab being used to launch the
@@ -120,52 +80,10 @@ def add_adbhost(moblab, adb_hostname):
     return adb_hostname
 
 
-def stage_payload(moblab, payload, vm):
-    """Stage the payload on the MobLab.
-
-    # TODO (sbasi): Add support to stage source payloads.
-
-    @param moblab: MoblabHost representing the MobLab being used to launch the
-                   testing.
-    @param payload: Path to the Brillo payload that will be staged.
-    @param vm: Boolean if True means we are running in a VM.
-
-    @raise PayloadStagingError: If we failed to stage the payload.
-    """
-    if not os.path.exists(payload):
-        raise PayloadStagingError('Payload file %s does not exist.' % payload)
-    stage_tmp_dir = os.path.join(MOBLAB_TMP_DIR, TARGET_IMAGE_NAME)
-    stage_dest_dir = os.path.join(MOBLAB_STATIC_DIR, TARGET_IMAGE_NAME)
-    stage_tmp_file = os.path.join(stage_tmp_dir, 'target_full_.bin')
-    moblab.run('mkdir -p %s' % stage_tmp_dir)
-    moblab.send_file(payload, stage_tmp_file)
-    moblab.run('chown -R moblab:moblab %s' % MOBLAB_TMP_DIR)
-    # Remove any artifacts that were previously staged.
-    moblab.run('rm -rf %s' % stage_dest_dir)
-    port = VIRT_MACHINE_DEVSERVER_PORT if vm else PHYS_MACHINE_DEVSERVER_PORT
-    try:
-        stage_url = DEVSERVER_STAGE_URL_TEMPLATE % dict(
-                moblab=moblab.web_address,
-                port=port,
-                staged_dir=stage_tmp_dir)
-        res = urllib2.urlopen(stage_url).read()
-    except (urllib2.HTTPError, httplib.HTTPException, urllib2.URLError) as e:
-        raise PayloadStagingError('Unable to stage payload on moblab: %s' % e)
-    else:
-        if res == 'Success':
-            logging.debug('Payload is staged on Moblab as %s',
-                          TARGET_IMAGE_NAME)
-        else:
-            raise PayloadStagingError('Staging failed: %s' % res)
-    finally:
-        moblab.run('rm -rf %s' % stage_tmp_dir)
-
-
 def schedule_test(moblab, host, test, test_args):
     """Schedule a Brillo test.
 
-    @param moblab: MoblabHost representing the MobLab being used to launch the
-                   testing.
+    @param moblab: MoblabHost representing the MobLab being used for testing.
     @param host: Hostname of the DUT.
     @param test: Test name.
     @param test_args: Iterable of 'NAME=VAL' (strings) encoding argument
@@ -189,8 +107,8 @@ def schedule_test(moblab, host, test, test_args):
             control_type=control_data.CONTROL_TYPE_NAMES.SERVER,
             hosts=[host], require_ssp=False)
     logging.info('Tests Scheduled. Please wait for results.')
-    job_page = AFE_JOB_PAGE_TEMPLATE % dict(moblab=moblab.web_address,
-                                            job_id=job.id)
+    job_page = _AFE_JOB_PAGE_TEMPLATE % dict(moblab=moblab.web_address,
+                                             job_id=job.id)
     logging.info('Progress can be monitored at %s', job_page)
     logging.info('Please note tests that launch other tests (e.g. sequences) '
                  'might complete quickly, but links to child jobs will appear '
@@ -201,8 +119,7 @@ def schedule_test(moblab, host, test, test_args):
 def get_all_jobs(moblab, parent_job):
     """Generate a list of the parent_job and it's subjobs.
 
-    @param moblab: MoblabHost representing the MobLab being used to launch the
-                   testing.
+    @param moblab: MoblabHost representing the MobLab being used for testing.
     @param host: Hostname of the DUT.
     @param parent_job: autotest_lib.server.frontend.Job object representing the
                        parent job.
@@ -217,8 +134,7 @@ def get_all_jobs(moblab, parent_job):
 def wait_for_test_completion(moblab, host, parent_job):
     """Wait for the parent job and it's subjobs to complete.
 
-    @param moblab: MoblabHost representing the MobLab being used to launch the
-                   testing.
+    @param moblab: MoblabHost representing the MobLab being used for testing.
     @param host: Hostname of the DUT.
     @param parent_job: autotest_lib.server.frontend.Job object representing the
                        test job.
@@ -234,9 +150,9 @@ def wait_for_test_completion(moblab, host, parent_job):
         if afe_host.status == 'Repair Failed':
             moblab.afe.abort_jobs(
                 [j.id for j in get_all_jobs(moblab, parent_job)])
-            host_page = AFE_HOST_PAGE_TEMPLATE % dict(
+            host_page = _AFE_HOST_PAGE_TEMPLATE % dict(
                     moblab=moblab.web_address, host_id=afe_host.id)
-            raise BrilloTestError(
+            raise BrilloTestExecutionError(
                     'ADB dut %s has become Repair Failed. More information '
                     'can be found at %s' % (host, host_page))
         time.sleep(10)
@@ -245,8 +161,7 @@ def wait_for_test_completion(moblab, host, parent_job):
 def output_results(moblab, parent_job):
     """Output the Brillo PTS and it's subjobs results.
 
-    @param moblab: MoblabHost representing the MobLab being used to launch the
-                   testing.
+    @param moblab: MoblabHost representing the MobLab being used for testing.
     @param parent_job: autotest_lib.server.frontend.Job object representing the
                        test job.
     """
@@ -261,8 +176,7 @@ def output_results(moblab, parent_job):
 def copy_results(moblab, parent_job):
     """Copy job results locally.
 
-    @param moblab: MoblabHost representing the MobLab being used to launch the
-                   testing.
+    @param moblab: MoblabHost representing the MobLab being used for testing.
     @param parent_job: autotest_lib.server.frontend.Job object representing the
                        parent job.
 
@@ -275,42 +189,6 @@ def copy_results(moblab, parent_job):
     return tempdir
 
 
-def setup_moblab_host(args):
-    """Setup the MobLab Host Object.
-
-    @params args: argparse.Namespace object representing the parsed args.
-
-    @returns Tuple of (moblab_host.MoblabHost, boolean indicating if the
-                       MobLab is running in a virtual machine).
-
-    @raise BrilloTestError: An error occurred while setting up the Moblab.
-    """
-    is_vm = False
-    web_address = args.moblab_host
-    if not args.moblab_host:
-        logging.info('MobLab host name was not provided. Attempting to '
-                     'launch the test against a local virtual machine.')
-        is_vm = True
-        args.moblab_host = VIRT_MACHINE_SSH_ADDR
-        web_address = VIRT_MACHINE_AFE_ADDR
-    # Create a MoblabHost to interact with the Moblab device.
-    try:
-        moblab = hosts.create_host(args.moblab_host,
-                                   host_class=moblab_host.MoblabHost,
-                                   web_address=web_address)
-    except error.AutoservRunError as e:
-        raise BrilloTestError('Unable to connect to the MobLab: %s' % e)
-
-    try:
-        moblab.afe.get_hosts()
-    except Exception as e:
-        raise BrilloTestError(
-                "Unable to communicate with the MobLab's web frontend, "
-                "please verify that it is up and running at http://%s/\n"
-                "Error: %s" % (moblab.web_address, e))
-    return (moblab, is_vm)
-
-
 def quickmerge(moblab):
     """Transfer over a subset of Autotest directories.
 
@@ -318,13 +196,12 @@ def quickmerge(moblab):
     libraries on their workstation without requiring them to emerge and cros
     deploy the autotest-server package.
 
-    @param moblab: MoblabHost representing the MobLab being used to launch the
-                   testing.
+    @param moblab: MoblabHost representing the MobLab being used for testing.
     """
     autotest_rootdir = os.path.dirname(
             os.path.dirname(os.path.realpath(__file__)))
-    for item in QUICKMERGE_LIST:
-        src = os.path.join(autotest_rootdir,item)
+    for item in _QUICKMERGE_LIST:
+        src = os.path.join(autotest_rootdir, item)
         dest = ':'.join(['moblab@%s' % moblab.hostname,
                          os.path.join(moblab_host.AUTOTEST_INSTALL_DIR, item)])
         rsync_cmd = ['rsync', '-a', '--exclude', '*.pyc', src, dest]
@@ -333,29 +210,15 @@ def quickmerge(moblab):
 
 def main(args):
     """The main function."""
-    args = parse_args()
-    level = logging.DEBUG if args.debug else logging.INFO
-    # Without a full site-packages, logging's root log handler needs to be
-    # manually setup.
-    logger = logging.getLogger()
-    for handler in logger.handlers:
-        logger.removeHandler(handler)
-    logger.setLevel(level)
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
-    logger.addHandler(handler)
-
-    moblab, is_vm = setup_moblab_host(args)
+    args = brillo_common.parse_args('Launch a Brillo test using Moblab.',
+                                    setup_parser=setup_parser)
+    moblab, _ = brillo_common.get_moblab_and_devserver_port(args.moblab_host)
 
     if args.quickmerge:
         quickmerge(moblab)
 
     # Add the adb host object to the MobLab.
-    adb_host = add_adbhost(moblab, args.adb_host)
-
-    # Stage the payload if provided.
-    if args.payload:
-        stage_payload(moblab, args.payload, is_vm)
+    adb_host = add_adb_host(moblab, args.adb_host)
 
     # Schedule the test job.
     test_job = schedule_test(moblab, adb_host, args.test_name, args.test_args)
@@ -372,7 +235,7 @@ if __name__ == '__main__':
     try:
         main(sys.argv)
         sys.exit(0)
-    except BrilloTestError as e:
+    except brillo_common.BrilloTestError as e:
         logging.error('Error: %s', e)
 
     sys.exit(1)
