417395925057cee65779f9663e261aaa22da35f4
Achuith Bhandarkar <achuith@chromium.org>
2015-11-04 13:53:57 -0800

    Use browserBridge.isSandboxedForTesting.
    chrome://gpu page now has browserBridge.isSandboxedForTesting.
    
    Also some minor common code refactoring.
    
    BUG=chromium:549681
    TEST=this is the test.
    
    Change-Id: Ie35e508e8e4bb9a359ae92951bc9a21a4739a639
    Reviewed-on: https://chromium-review.googlesource.com/310875
    Commit-Ready: Achuith Bhandarkar <achuith@chromium.org>
    Tested-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- client/site_tests/security_SandboxStatus/security_SandboxStatus.py
+++ client/site_tests/security_SandboxStatus/security_SandboxStatus.py
@@ -22,50 +22,40 @@ class security_SandboxStatus(test.test):
         except exceptions.EvaluateException:
             return None
 
+    def _CheckSandboxPage(self, url, js):
+        self._tab.Navigate(url)
+
+        return utils.poll_for_condition(
+                lambda: self._EvaluateJavaScript(js),
+                exception=error.TestError('Failed to evaluate in %s "%s"'
+                                          % (url, js)),
+                timeout=30)
+
 
     def _CheckAdequatelySandboxed(self):
-        '''Checks that chrome:///sandbox shows "You are adequately sandboxed."'''
-        sandbox_good_js = "document.getElementsByTagName('p')[0].textContent"
-        sandbox_good = utils.poll_for_condition(
-                lambda: self._EvaluateJavaScript(sandbox_good_js),
-                exception=error.TestError(
-                       'Failed to evaluate in chrome://sandbox "%s"'
-                        % sandbox_good_js),
-                timeout=30)
-        if not re.match('You are adequately sandboxed.', sandbox_good):
-            raise error.TestFail('Could not find "You\'re adequately '
-                                 'sandboxed." in chrome://sandbox')
+        '''Checks that chrome://sandbox shows "You are adequately sandboxed."'''
+        url = 'chrome://sandbox'
+        res = self._CheckSandboxPage(url,
+                "document.getElementsByTagName('p')[0].textContent")
+
+        text = 'You are adequately sandboxed.'
+        if not re.match(text, res):
+            raise error.TestFail('Could not find "%s" in %s' % (text, url))
 
 
-    # TODO(jorgelo): This breaks with changes to the layout of chrome://gpu.
-    # Make it more robust. crbug.com/549681.
-    def _CheckGPUCell(self, table, row, cell, content):
-        '''Checks the content of a cell in chrome://gpu.'''
-
-        gpu_js = ("document.getElementsByTagName('table')"
-                  "[%d].rows[%d].cells[%d].textContent" % (table, row, cell))
-        try:
-            res = utils.poll_for_condition(
-                    lambda: self._EvaluateJavaScript(gpu_js),
-                    timeout=30)
-        except utils.TimeoutError:
-            raise error.TestError('Failed to evaluate in chrome://gpu "%s"'
-                                  % gpu_js)
-
-        return res.find(content) != -1
-
+    def _CheckGPUSandboxed(self):
+        '''
+        Checks that chrome://gpu has "Sandboxed" row, and "Sandboxed" is True.
+        '''
+        url = 'chrome://gpu'
+        res = self._CheckSandboxPage(url,
+                                     "browserBridge.isSandboxedForTesting();")
+        if res is not True:
+            raise error.TestFail('"Sandboxed" not True in %s' % url)
 
     def run_once(self):
         '''Open various sandbox-related pages and test that we are sandboxed.'''
         with chrome.Chrome() as cr:
             self._tab = cr.browser.tabs[0]
-            self._tab.Navigate('chrome://sandbox')
             self._CheckAdequatelySandboxed()
-
-            self._tab.Navigate('chrome://gpu')
-
-            if not self._CheckGPUCell(2, 2, 0, 'Sandboxed'):
-                raise error.TestError('Could not locate "Sandboxed" row in table')
-
-            if not self._CheckGPUCell(2, 2, 1, 'true'):
-                raise error.TestError('GPU not sandboxed')
+            self._CheckGPUSandboxed()
