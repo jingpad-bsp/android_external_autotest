d89dd6cfb740847a97d8626d3ae7ab370ed892e6
Cheng-Yi Chiang <cychiang@chromium.org>
2015-11-19 13:06:24 +0800

    [autotest] audio_AudioBasicInternal*: Use spectral analysis tool
    Use spectral analysis tool to estimate frequency.
    
    BUG=chromium:550395
    TEST=run the tests on link.
    
    Change-Id: Id292c653fd010c4c8612b0c87c59267ec3430e08
    Reviewed-on: https://chromium-review.googlesource.com/313079
    Commit-Ready: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-by: Kalin Stoyanov <kalin@chromium.org>

--- server/site_tests/audio_AudioBasicInternalMicrophone/audio_AudioBasicInternalMicrophone.py
+++ server/site_tests/audio_AudioBasicInternalMicrophone/audio_AudioBasicInternalMicrophone.py
@@ -118,7 +118,5 @@ class audio_AudioBasicInternalMicrophone(audio_test.AudioTest):
         # Comparing data by frequency is more robust than comparing them by
         # correlation, which is suitable for fully-digital audio path like USB
         # and HDMI.
-        if not chameleon_audio_helper.compare_recorded_result(
-                golden_file, recorder, 'frequency'):
-            raise error.TestFail(
-                    'Recorded file does not match playback file')
+        audio_test_utils.check_recorded_frequency(golden_file, recorder,
+                                                  second_peak_ratio=0.1)
--- server/site_tests/audio_AudioBasicInternalSpeaker/audio_AudioBasicInternalSpeaker.py
+++ server/site_tests/audio_AudioBasicInternalSpeaker/audio_AudioBasicInternalSpeaker.py
@@ -107,7 +107,5 @@ class audio_AudioBasicInternalSpeaker(audio_test.AudioTest):
         # Comparing data by frequency is more robust than comparing by
         # correlation, which is suitable for fully-digital audio path like USB
         # and HDMI.
-        if not chameleon_audio_helper.compare_recorded_result(
-                golden_file, recorder, 'frequency'):
-            raise error.TestFail(
-                    'Recorded file does not match playback file')
+        audio_test_utils.check_recorded_frequency(golden_file, recorder,
+                                                  second_peak_ratio=0.1)
