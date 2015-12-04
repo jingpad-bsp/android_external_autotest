99d85e863b4d4c9f615bc23ab95e4acba986b437
Rebecca Silberstein <silberst@google.com>
2015-12-04 13:49:22 -0800

    autotest (wifi): Add loop to check for cleared IP on disconnect
    Add a loop to check for a cleared IP address in the DisconnectClearsIP
    test.  This change is needed due to a variable delay added in the
    DropConnection function.  The delay is a result of switching from an
    async to sync call when killing dhcpcd.
    
    The number of loops for the test was set after running several
    iterations of the test.
    
    BUG=chrome-os-partner:48228
    TEST=test_that -b daisy chromeos1-dev-host5.cros network_WiFi_DisconnectClearsIP
    
    Change-Id: I6e078603051c31dabc910e3c9bb550ae26652d41
    Reviewed-on: https://chromium-review.googlesource.com/315737
    Commit-Ready: Rebecca Silberstein <silberst@chromium.org>
    Tested-by: Rebecca Silberstein <silberst@chromium.org>
    Reviewed-by: Paul Stewart <pstew@chromium.org>

--- server/site_tests/network_WiFi_DisconnectClearsIP/network_WiFi_DisconnectClearsIP.py
+++ server/site_tests/network_WiFi_DisconnectClearsIP/network_WiFi_DisconnectClearsIP.py
@@ -2,6 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
+import time
+
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib.cros.network import xmlrpc_datatypes
 from autotest_lib.server.cros.network import hostap_config
@@ -13,6 +16,7 @@ class network_WiFi_DisconnectClearsIP(wifi_cell_test_base.WiFiCellTestBase):
 
     version = 1
 
+    IP_CHECK_ATTEMPTS = 10
 
     def run_once(self):
         """Test body."""
@@ -27,7 +31,18 @@ class network_WiFi_DisconnectClearsIP(wifi_cell_test_base.WiFiCellTestBase):
             raise error.TestFail('After connecting, we should have an IP.')
 
         self.context.assert_ping_from_dut()
-        self.context.client.shill.disconnect(self.context.router.get_ssid())
-        if self.context.client.wifi_ip is not None:
+        disconnect_check = self.context.client.shill.disconnect(
+            self.context.router.get_ssid())
+        if not disconnect_check:
+            raise error.TestFail('Failed to disconnect from the network')
+        logging.info('Successfully disconnected.')
+
+        for x in range(0, self.IP_CHECK_ATTEMPTS):
+            wifi_ip = self.context.client.wifi_ip
+            if wifi_ip is None:
+                return
+            logging.info('IP was still set: %s', wifi_ip)
+            time.sleep(1)
+        else:
             raise error.TestFail('After disconnecting, we should '
                                  'not have an IP.')
