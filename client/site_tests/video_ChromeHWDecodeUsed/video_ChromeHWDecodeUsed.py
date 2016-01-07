ff11bf627d2bb070e7336cb0e0892044933af472
Mussa <mussa@chromium.org>
2016-01-06 16:17:45 -0800

    Reenable HW Decode test on Sandybridge.
    We disabled HW Decoding on Sandybridge due to crbug/521249
    
    BUG=None.
    TEST=Lab.
    
    Change-Id: I20620301887184c5479d694bf44682f680d6f8a3
    Reviewed-on: https://chromium-review.googlesource.com/320773
    Commit-Ready: Mussa Kiroga <mussa@chromium.org>
    Tested-by: Mussa Kiroga <mussa@chromium.org>
    Reviewed-by: Mussa Kiroga <mussa@chromium.org>

--- client/site_tests/video_ChromeHWDecodeUsed/video_ChromeHWDecodeUsed.py
+++ client/site_tests/video_ChromeHWDecodeUsed/video_ChromeHWDecodeUsed.py
@@ -5,7 +5,6 @@
 import time
 
 from autotest_lib.client.bin import test
-from autotest_lib.client.bin import utils
 from autotest_lib.client.common_lib.cros import chrome
 from autotest_lib.client.cros.video import histogram_verifier
 from autotest_lib.client.cros.video import vda_constants
@@ -17,16 +16,12 @@ class video_ChromeHWDecodeUsed(test.test):
 
 
     def run_once(self, video_file):
-        """Tests whether VDA works by verifying histogram for the loaded video.
+        """
+        Tests whether VDA works by verifying histogram for the loaded video.
 
         @param video_file: Sample video file to be loaded in Chrome.
+
         """
-
-        # TODO(hshi): Remove this once hw decode hang issue is fixed on sandybridge.
-        # See http://crbug.com/521249.
-        if utils.get_board() in ['lumpy', 'stumpy', 'parrot', 'butterfly']:
-            return
-
         with chrome.Chrome() as cr:
             tab1 = cr.browser.tabs[0]
             tab1.Navigate(video_file)
