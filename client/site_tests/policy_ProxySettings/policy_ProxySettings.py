8bfcc27e8f42231df208663ba4273f697df26159
Scott Cunningham <scunningham@chromium.org>
2016-01-22 14:07:06 -0800

    Fix policy_UserProxySettings AutoTest
    Fixes some minor naming issues with the policy_ProxySettings test,
    such as changing its name from policy_UserProxySettings to
    policy_ProxySettings.
    
    BUG=chromium:528504
    TEST=Run test_that <IP>policy_ProxySettings
    
    Change-Id: I8432b4b5cd1f7ebc8d0fe1cd50e2765ca73e110a
    Reviewed-on: https://chromium-review.googlesource.com/323353
    Commit-Ready: Scott Cunningham <scunningham@chromium.org>
    Tested-by: Scott Cunningham <scunningham@chromium.org>
    Reviewed-by: Scott Cunningham <scunningham@chromium.org>

--- client/site_tests/policy_ProxySettings/policy_ProxySettings.py
+++ client/site_tests/policy_ProxySettings/policy_ProxySettings.py
@@ -4,9 +4,7 @@
 
 import logging
 import threading
-import time
 
-##from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import enterprise_policy_base
 from SocketServer import ThreadingTCPServer, StreamRequestHandler
@@ -14,36 +12,43 @@ from SocketServer import ThreadingTCPServer, StreamRequestHandler
 POLICY_NAME = 'ProxySettings'
 PROXY_HOST = 'localhost'
 PROXY_PORT = 3128
-FIXED_PROXY_POLICY_VALUE = """{"ProxyMode":"fixed_servers",
-                              "ProxyServer":"localhost:%s"}""" % str(PROXY_PORT)
+FIXED_PROXY = '''
+{
+  "ProxyMode":"fixed_servers",
+  "ProxyServer":"localhost:%s"
+}
+''' % str(PROXY_PORT)
 
 
 class ProxyHandler(StreamRequestHandler):
-    """Request handler for the ThreadedHitServer
-       that saves every request it recieves.
-    """
+    """Provide a request handler for the Threaded Proxy Server."""
     wbufsize = -1
-    def handle(self):
-        """Reads the first line, up to 40 characters, looking
-           for the URL of the request. If found itsave the URL
-           list.
 
-           All requests receive a HTTP 504 error.
+    def handle(self):
+        """Get URL of request from first line.
+
+        Read the first line of the request, up to 40 characters, and look for
+        the URL of the request. If found, save it to the URL list.
+
+        Note: All requests are sent an HTTP 504 error.
         """
         # Read up to 40 characters of the request to capture the request URL
         data = self.rfile.readline(40).strip()
         logging.info('ProxyHandler::handle(): <%s>', data)
         self.server.store_requests_recieved(data)
-        self.wfile.write("HTTP/1.1 504 Gateway Timeout\r\n" +
-                         "Connection: close\r\n\r\n")
+        self.wfile.write('HTTP/1.1 504 Gateway Timeout\r\n' +
+                         'Connection: close\r\n\r\n')
 
 
 class ThreadedProxyServer(ThreadingTCPServer):
-    """A threaded TCP server which services requests
-       and allows the handler to save recieved requests.
+    """Provide a Threaded TCP Server to service and save requests.
+
+    Define a Threaded TCP Server which services requests, and allows the
+    handler to save all requests.
     """
+
     def __init__(self, server_address, HandlerClass):
-        """Constructor
+        """Constructor.
 
         @param server_address: tuple of server IP and port to listen on.
         @param HandlerClass: the RequestHandler class to instantiate per req.
@@ -51,22 +56,30 @@ class ThreadedProxyServer(ThreadingTCPServer):
         self._requests_recieved = []
         ThreadingTCPServer.__init__(self, server_address, HandlerClass)
 
-    def store_requests_recieved(self, data):
-        self._requests_recieved.append(data)
+    def store_requests_recieved(self, request):
+        """Add receieved request to list.
+
+        @param request: request received by the proxy server.
+        """
+        self._requests_recieved.append(request)
 
     def get_requests_recieved(self):
+        """Get list of received requests."""
         return self._requests_recieved
 
     # TODO(krishnargv) add a method to reset request_recieved_stack
 
 
 class ProxyListener(object):
-    """A fake listener for tracking if an expected CONNECT request is
-       seen at the provided server address. Any requests recieved are
-       exposed to be consumed by the caller.
+    """Provide a Proxy Listener to detect connect requests.
+
+    Defines fake listener for tracking whether an expected CONNECT request is
+    seen at the provided server address. Any requests recieved are exposed to
+    be consumed by the caller.
     """
+
     def __init__(self, server_address):
-        """Constructor
+        """Constructor.
 
         @param server_address: tuple of server IP and port to listen on.
         """
@@ -74,44 +87,44 @@ class ProxyListener(object):
         self._thread = threading.Thread(target=self._server.serve_forever)
 
     def run(self):
-        """Run the server on a thread"""
+        """Run the server on a thread."""
         self._thread.start()
 
     def stop(self):
-        """Stop the server and its threads"""
+        """Stop the server and its threads."""
         self._server.shutdown()
         self._server.socket.close()
         self._thread.join()
-
 
     def get_requests_recieved(self):
         return self._server.get_requests_recieved()
 
 
-class policy_UserProxySettings(enterprise_policy_base.EnterprisePolicyTest):
-    """
-    Test effect of UserProxySettings policy on Chrome OS behavior.
-
-    """
+class policy_ProxySettings(enterprise_policy_base.EnterprisePolicyTest):
+    """Test effect of ProxySettings policy on Chrome OS behavior."""
     version = 1
     TEST_CASES = {
-                 "fixed-proxy": "1"}
-##                 'fixed-proxy': test_fixed_proxy,
-##                 '
+        'FixedProxy': '1'
+    }
     TEST_CASE_DATA = {
-                      'fixed-proxy' : FIXED_PROXY_POLICY_VALUE
-                     }
+        'FixedProxy': FIXED_PROXY
+    }
 
     def initialize(self, args=()):
-        super(policy_UserProxySettings, self).initialize(args)
+        super(policy_ProxySettings, self).initialize(args)
         self._proxy_server = ProxyListener(('', PROXY_PORT))
         self._proxy_server.run()
 
     def cleanup(self):
         self._proxy_server.stop()
-        super(policy_UserProxySettings, self).cleanup()
+        super(policy_ProxySettings, self).cleanup()
 
     def test_fixed_proxy(self, policy_value, policies_json):
+        """Verify CrOS enforces ProxySettings value = fixed-proxy.
+
+        @param policy_value: policy value expected on chrome://policy page.
+        @param policies_json: policy JSON data to send to the fake DM server.
+        """
         proxy_server_requests = []
         matching_requests = []
         url = 'http://www.wired.com/'
@@ -121,14 +134,13 @@ class policy_UserProxySettings(enterprise_policy_base.EnterprisePolicyTest):
         logging.info('Navigating to URL:%s', url)
         tab.Navigate(url, timeout=10)
         proxy_server_requests = self._proxy_server.get_requests_recieved()
-        matching_requests = [request for request in proxy_server_requests \
-                                        if url in request]
+        matching_requests = [request for request in proxy_server_requests
+                             if url in request]
         if not matching_requests:
             raise error.TestFail('Fixed Proxy Policy not applied')
 
     def _run_test_case(self, case):
-        """
-        Setup and run the test configured for the specified test case.
+        """Setup and run the test configured for the specified test case.
 
         Set the expected |policy_value| and |policies_json| data based on the
         test |case|. If the user specified an expected |value| in the command
@@ -149,14 +161,13 @@ class policy_UserProxySettings(enterprise_policy_base.EnterprisePolicyTest):
         else:
             # Otherwise, set expected |policy_value| and setup |policies_json|
             # data to the values required by the specified test |case|.
-
             if not self.TEST_CASES[case]:
                 policy_value = None
             else:
                 policy_value = self.TEST_CASE_DATA[case]
-                policies_json = {'ProxySettings': self.TEST_CASE_DATA[case]}
+                policies_json = {POLICY_NAME: self.TEST_CASE_DATA[case]}
 
-        if case  == "fixed-proxy":
+        if case == 'FixedProxy':
             self.test_fixed_proxy(policy_value, policies_json)
 
     def run_once(self):
