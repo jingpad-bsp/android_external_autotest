b5510c932e485dcff860414e85dae21f07441082
Scott Cunningham <scunningham@chromium.org>
2016-01-26 10:50:35 -0800

    Update and fix policy_CookiesAllowedForUrls test.
    Update policy_CookiesAllowedForUrls AutoTest to use run_once() in
    the base class. Also fix the list of blocked URLs. Also clean up
    some general readability and indentation issues.
    
    BUG=chromium:581410
    TEST=Run test_that <IP> policy_CookiesAllowedForUrls
    
    Change-Id: I303aa2b848cc41afc4bbd16b39435ae41bd5a909
    Reviewed-on: https://chromium-review.googlesource.com/323826
    Commit-Ready: Scott Cunningham <scunningham@chromium.org>
    Tested-by: Scott Cunningham <scunningham@chromium.org>
    Reviewed-by: Rohit Makasana <rohitbm@chromium.org>

--- client/site_tests/policy_CookiesAllowedForUrls/policy_CookiesAllowedForUrls.py
+++ client/site_tests/policy_CookiesAllowedForUrls/policy_CookiesAllowedForUrls.py
@@ -21,60 +21,57 @@ COOKIE_NAME = 'cookie1'
 COOKIE_ALLOWED_SINGLE_FILE_DATA = [URL_HOST]
 COOKIE_ALLOWED_MULTIPLE_FILES_DATA = ['http://google.com', URL_HOST,
                                       'http://doesnotmatter.com']
-COOKIE_BLOCKED_MULTIPLE_FILES_DATA = ['https://testingwebsite.html'
+COOKIE_BLOCKED_MULTIPLE_FILES_DATA = ['https://testingwebsite.html',
                                       'https://somewebsite.com',
                                       'http://doesnotmatter.com']
-#Setting the DefaultCookiesSetting to the value 2, blocks cookies on all sites.
+# Setting DefaultCookiesSetting=2 blocks cookies on all sites.
 SUPPORTING_POLICIES = {'DefaultCookiesSetting': 2}
 
 
 class policy_CookiesAllowedForUrls(enterprise_policy_base.EnterprisePolicyTest):
-    """
-    Test effect of the CookiesAllowedForUrls policy on Chrome OS behavior.
+    """Test effect of the CookiesAllowedForUrls policy on Chrome OS behavior.
 
-    This test implicitly verifies one of the settings of the
-    DefaultCookiesSetting policy as well.
-
-    When the DefaultCookiesSetting policy value is set to 2, cookies for all
-    URLs shall not be stored (ie, shall be blocked), except for the URL patterns
-    specified by the CookiesAllowedForUrls policy value.
+    This test implicitly verifies one value of the DefaultCookiesSetting
+    policy as well. When the DefaultCookiesSetting policy value is set to 2,
+    cookies for all URLs shall not be stored (ie, shall be blocked), except
+    for the URL patterns specified by the CookiesAllowedForUrls policy.
 
     The test verifies ChromeOS behaviour for different values of the
-    CookiesAllowedForUrls policy ie, for the policy value set to Not Set, Set to
-    a single url/host pattern or when the policy is set to multiple url/host
-    patterns. It also excercises an additional scenario ie, it tests that
-    cookies are blocked for urls that are not part of the policy value.
-    The corresponding three test cases are 'NotSet_CookiesBlocked',
-    SingleUrl_CookiesAllowed, MultipleUrls_CookiesAllowed and
+    CookiesAllowedForUrls policy i.e., for the policy value set to Not Set,
+    Set to a single url/host pattern or when the policy is set to multiple
+    url/host patterns. It also excercises an additional scenario i.e., it
+    tests that cookies are blocked for urls that are not part of the policy
+    value.
+
+    The corresponding three test cases are NotSet_CookiesBlocked,
+    SingleUrl_CookiesAllowed, MultipleUrls_CookiesAllowed, and
     and MultipleUrls_CookiesBlocked.
 
     """
     version = 1
     TEST_CASES = {
-            'NotSet_CookiesBlocked': '',
-            'SingleUrl_CookiesAllowed': COOKIE_ALLOWED_SINGLE_FILE_DATA,
-            'MultipleUrls_CookiesAllowed': COOKIE_ALLOWED_MULTIPLE_FILES_DATA,
-            'MultipleUrls_CookiesBlocked' : COOKIE_BLOCKED_MULTIPLE_FILES_DATA
-            }
+        'NotSet_CookiesBlocked': '',
+        'SingleUrl_CookiesAllowed': COOKIE_ALLOWED_SINGLE_FILE_DATA,
+        'MultipleUrls_CookiesAllowed': COOKIE_ALLOWED_MULTIPLE_FILES_DATA,
+        'MultipleUrls_CookiesBlocked': COOKIE_BLOCKED_MULTIPLE_FILES_DATA
+    }
 
     def initialize(self, args=()):
         super(policy_CookiesAllowedForUrls, self).initialize(args)
         self.start_webserver(URL_PORT)
 
     def _is_cookie_blocked(self, url):
-        """
-        Returns True if the cookie is blocked for the URL else returns False.
+        """Return True if cookie is blocked for the URL else return False.
 
         @param url: Url of the page which is loaded to check whether it's
                     cookie is blocked or stored.
 
         """
-        tab =  self.navigate_to_url(url)
+        tab = self.navigate_to_url(url)
         return tab.GetCookieByName(COOKIE_NAME) is None
 
     def _test_CookiesAllowedForUrls(self, policy_value, policies_json):
-        """
-        Verify CrOS enforces CookiesAllowedForUrls policy value.
+        """Verify CrOS enforces CookiesAllowedForUrls policy value.
 
         When the CookiesAllowedForUrls policy is set to one or more urls/hosts,
         check that cookies are not blocked for the urls/urlpatterns listed in
@@ -92,6 +89,7 @@ class policy_CookiesAllowedForUrls(enterprise_policy_base.EnterprisePolicyTest):
         self.setup_case(POLICY_NAME, policy_value, policies_json)
 
         cookie_is_blocked = self._is_cookie_blocked(TEST_URL)
+        logging.info('cookie_is_blocked = %s', cookie_is_blocked)
 
         if policy_value and URL_HOST in policy_value:
             if cookie_is_blocked:
@@ -100,9 +98,8 @@ class policy_CookiesAllowedForUrls(enterprise_policy_base.EnterprisePolicyTest):
             if not cookie_is_blocked:
                 raise error.TestFail('Cookies should be blocked.')
 
-    def _run_test_case(self, case):
-        """
-        Setup and run the test configured for the specified test case.
+    def run_test_case(self, case):
+        """Setup and run the test configured for the specified test case.
 
         Set the expected |policy_value| and |policies_json| data based on the
         test |case|. If the user specified an expected |value| in the command
@@ -124,15 +121,9 @@ class policy_CookiesAllowedForUrls(enterprise_policy_base.EnterprisePolicyTest):
             # Otherwise, set expected |policy_value| and setup |policies_json|
             # data to the values required by the specified test |case|.
             policy_value = ','.join(self.TEST_CASES[case])
-            policy_json = {'CookiesAllowedForUrls': self.TEST_CASES[case]}
+            policy_json = {POLICY_NAME: self.TEST_CASES[case]}
             policies_json = SUPPORTING_POLICIES.copy()
             policies_json.update(policy_json)
 
         # Run test using the values configured for the test case.
         self._test_CookiesAllowedForUrls(policy_value, policies_json)
-
-    def run_once(self):
-        # The run_once() method is required by autotest. We call the base
-        # class run_once_impl() method, which handles command-line run modes,
-        # and pass in the standard _run_test_case() method of this test.
-            self.run_once_impl(self._run_test_case)
