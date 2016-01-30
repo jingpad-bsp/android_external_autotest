3ec6305bbfb42e45a4f08802dffef15752517ace
avinash kodipelli <avkodipelli@chromium.org>
2016-01-29 17:37:11 -0800

    Using local html file for loading video
    To avoid javasscript error, we are using local html file
    
    BUG=chromium:582477
    TEST=manual
    
    Change-Id: I730be624ea562c627f2805e5be566a0ed7d3d834
    Reviewed-on: https://chromium-review.googlesource.com/324635
    Commit-Ready: Mussa Kiroga <mussa@chromium.org>
    Tested-by: Mussa Kiroga <mussa@chromium.org>
    Reviewed-by: Mussa Kiroga <mussa@chromium.org>

--- client/site_tests/video_ChromeHWDecodeUsed/video_ChromeHWDecodeUsed.py
+++ client/site_tests/video_ChromeHWDecodeUsed/video_ChromeHWDecodeUsed.py
@@ -2,12 +2,15 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
 import time
+import shutil
 
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib.cros import chrome
 from autotest_lib.client.cros.video import histogram_verifier
 from autotest_lib.client.cros.video import constants
+from autotest_lib.client.cros.video import native_html5_player
 
 
 class video_ChromeHWDecodeUsed(test.test):
@@ -15,20 +18,38 @@ class video_ChromeHWDecodeUsed(test.test):
     version = 1
 
 
-    def run_once(self, video_file):
+    def run_once(self, is_mse, video_file):
         """
         Tests whether VDA works by verifying histogram for the loaded video.
 
+        @param is_mse: bool, True if the content uses MSE, False otherwise.
         @param video_file: Sample video file to be loaded in Chrome.
 
         """
         with chrome.Chrome() as cr:
-            tab1 = cr.browser.tabs[0]
-            tab1.Navigate(video_file)
-            tab1.WaitForDocumentReadyStateToBeComplete()
-            # Running the test longer to check errors and longer playback for
-            # MSE videos.
-            time.sleep(30)
+            # This will execute for MSE video by accesing shaka player
+            if is_mse:
+                 tab1 = cr.browser.tabs[0]
+                 tab1.Navigate(video_file)
+                 tab1.WaitForDocumentReadyStateToBeComplete()
+                 # Running the test longer to check errors and longer playback
+                 # for MSE videos.
+                 time.sleep(30)
+            else:
+            #This execute for normal video for downloading html file
+                 shutil.copy2(constants.VIDEO_HTML_FILEPATH, self.bindir)
+                 cr.browser.platform.SetHTTPServerDirectories(self.bindir)
+                 tab = cr.browser.tabs[0]
+                 html_fullpath = os.path.join(self.bindir, 'video.html')
+                 url = cr.browser.platform.http_server.UrlOf(html_fullpath)
+                 player = native_html5_player.NativeHtml5Player(
+                         tab,
+                         full_url = url,
+                         video_id = 'video',
+                         video_src_path = video_file,
+                         event_timeout = 120)
+                 player.load_video()
+                 player.play()
 
             # Waits for histogram updated for the test video.
             histogram_verifier.verify(
