001c4dd3fd9026a5b6c621ba1c1913c3c40df100
Fang Deng <fdeng@chromium.org>
2016-02-24 13:55:44 -0800

    [autotest] Only cleanup db on shards.
    Add check which enforce db cleanup to happen only on shards.
    
    TEST=run the script locally w/ and w/o [SHARD] section
    BUG=chromium:589498
    
    Change-Id: I9c77633c2990da94e4668f4287badd5e07645896
    Reviewed-on: https://chromium-review.googlesource.com/329250
    Commit-Ready: Fang Deng <fdeng@chromium.org>
    Tested-by: Fang Deng <fdeng@chromium.org>
    Reviewed-by: Dan Shi <dshi@google.com>

--- contrib/db_optimize.py
+++ contrib/db_optimize.py
@@ -10,6 +10,7 @@ tables and rebuilds indexes. So be careful when running it on production
 systems.
 """
 
+import argparse
 import logging
 import socket
 import subprocess
@@ -19,6 +20,7 @@ import common
 from autotest_lib.client.common_lib.cros.graphite import autotest_stats
 from autotest_lib.frontend import database_settings_helper
 from autotest_lib.scheduler import email_manager
+from autotest_lib.server import utils
 
 # Format Appears as: [Date] [Time] - [Msg Level] - [Message]
 LOGGING_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
@@ -40,9 +42,35 @@ def main_without_exception_handling():
     subprocess.check_call(command)
 
 
+def should_optimize():
+    """Check if the server should run db_optimize.
+
+    Only shard should optimize db.
+
+    @returns: True if it should optimize db otherwise False.
+    """
+    return utils.is_shard()
+
+
+def parse_args():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--check_server', action='store_true',
+                        help='Check if the server should optimize db.')
+    return parser.parse_args()
+
+
 def main():
+    """Main."""
+    args = parse_args()
+
     logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT)
     logging.info('Calling: %s', sys.argv)
+
+    if args.check_server and not should_optimize():
+        print 'Only shard can run db optimization.'
+        return
+
     try:
         main_without_exception_handling()
     except Exception as e:
