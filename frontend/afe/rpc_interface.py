275169775cab95fd5081cb3a0e9e832f5eb6c7c4
Dan Shi <dshi@google.com>
2016-03-16 14:03:41 -0700

    [autotest] Hide ab-version and testbed-version labels from the hosts filter
    BUG=None
    TEST=local instance
    
    Change-Id: Iec350cbcb50dae3af7fdc093bb5fe9f05427fb1f
    Reviewed-on: https://chromium-review.googlesource.com/333102
    Commit-Ready: Dan Shi <dshi@google.com>
    Tested-by: Dan Shi <dshi@google.com>
    Reviewed-by: Kevin Cheng <kevcheng@chromium.org>

--- frontend/afe/rpc_interface.py
+++ frontend/afe/rpc_interface.py
@@ -1593,7 +1593,9 @@ def get_static_data():
     label_exclude_filters = [{'name__startswith': 'cros-version'},
                              {'name__startswith': 'fw-version'},
                              {'name__startswith': 'fwrw-version'},
-                             {'name__startswith': 'fwro-version'}]
+                             {'name__startswith': 'fwro-version'},
+                             {'name__startswith': 'ab-version'},
+                             {'name__startswith': 'testbed-version'}]
     result['labels'] = get_labels(
         label_exclude_filters,
         sort_by=['-platform', 'name'])
