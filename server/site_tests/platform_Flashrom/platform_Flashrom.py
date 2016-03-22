93175f35e8a585432db9c150f666845951a819df
Danny Chan <dchan@google.com>
2016-03-22 09:40:48 -0700

    Exercise flashrom and chromeos-firmwareupdate script.
    - See run_once comment on test steps.
    
    BUG:chrome-os-partner:45203
    TEST=Manually executed on samus
    
    Change-Id: Ie6aa875c05c8c1656917b5c89e5e076eb0e2c117
    Reviewed-on: https://chromium-review.googlesource.com/334255
    Commit-Ready: danny chan <dchan@chromium.org>
    Tested-by: danny chan <dchan@chromium.org>
    Reviewed-by: Kalin Stoyanov <kalin@chromium.org>

---
+++ server/site_tests/platform_Flashrom/platform_Flashrom.py
@@ -0,0 +1,104 @@
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import re
+import os
+
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib import utils
+from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
+
+
+class platform_Flashrom(FirmwareTest):
+    """
+    Test flashrom works correctly by calling
+    chromeos-firmwareupdate --mode=factory.
+    """
+    version = 1
+
+
+    def initialize(self, host, cmdline_args, dev_mode=True):
+        # This test assume the system already have the latest RW from
+        # shellball.  You should run chromeos-firmware --mode=factory.
+        # Device should have WP disable.
+
+        # Parse arguments from command line
+        dict_args = utils.args_to_dict(cmdline_args)
+        super(platform_Flashrom, self).initialize(host, cmdline_args)
+        self.switcher.setup_mode('dev' if dev_mode else 'normal')
+
+    def run_cmd(self, command, checkfor=''):
+        """
+        Log and execute command and return the output.
+
+        @param command: Command to execute on device.
+        @param checkfor: If not emmpty, fail test if checkfor not in output.
+        @returns the output of command.
+        """
+        logging.info('Execute %s', command)
+        output = self.faft_client.system.run_shell_command_get_output(command)
+        logging.info('Output %s', output)
+        if checkfor and checkfor not in ' '.join(output):
+            raise error.TestFail('Expect %s in output of %s' %
+                                 (checkfor, ' '.join(output)))
+        return output
+
+    def _check_wp_disable(self):
+        """Check firmware is write protect disabled."""
+        self.run_cmd('flashrom -p host --wp-status', checkfor='is disabled')
+        if self.faft_config.chrome_ec:
+            self.run_cmd('flashrom -p ec --wp-status', checkfor='is disabled')
+        if self.faft_config.chrome_usbpd:
+            self.run_cmd('flashrom -p ec:dev=1 --wp-status',
+                         checkfor='is disabled')
+
+    def run_once(self, dev_mode=True):
+        # 1) Check SW WP is disabled.
+        self._check_wp_disable()
+
+        # Output location.
+        tmpdir = os.getenv('TMPDIR')
+        if not tmpdir: tmpdir = '/tmp'
+
+        # 2) Save the DUT bios RW section B for restore later if needed.
+        rw_b = os.path.join(tmpdir, 'rw_b.bin')
+        self.run_cmd('flashrom -r -i RW_SECTION_B:%s' % rw_b, 'SUCCESS')
+
+        # 3) Erase RW section B.  Needed CL 329549 starting with R51-7989.0.0.
+        self.run_cmd('flashrom -E -i RW_SECTION_B', 'SUCCESS')
+
+        # 4) Reinstall RW B (Test flashrom)
+        self.run_cmd('chromeos-firmwareupdate --mode=factory',
+                     '(factory_install) completed.')
+
+        # 5) Compare flash section B vs shellball section B
+        # 5.1) Extract shellball RW section B.
+        outdir = self.run_cmd('chromeos-firmwareupdate --sb_extract')[-1]
+        shball_path = outdir.split()[-1]
+        shball_bios = os.path.join(shball_path, 'bios.bin')
+        shball_rw_b = os.path.join(shball_path, 'shball_rw_b.bin')
+
+        # Extract RW B, offset detail
+        # /src/platform/vboot_reference/tests/futility/data_fmap_expect_p.txt
+        self.run_cmd('dd bs=1 skip=3080192 count=983040 if=%s of=%s 2>&1'
+                     % (shball_bios, shball_rw_b), '983040 bytes')
+
+        # 5.2) Extract flash RW section B.
+        rw_b2 = os.path.join(tmpdir, 'rw_b2.bin')
+        self.run_cmd('flashrom -r -i RW_SECTION_B:%s' % rw_b2, 'SUCCESS')
+
+        # 5.3) Compare output of 5.1 vs 5.2
+        result_output = self.run_cmd('cmp %s %s' % (shball_rw_b, rw_b2))
+        logging.info('cmp %s %s == %s', shball_rw_b, rw_b2, result_output)
+
+        # 6) Restore RW section B if different then 2.
+        out = self.run_cmd('cmp %s %s' % (rw_b, rw_b2))
+        logging.info('cmp %s %s == %s', rw_b, rw_b2, out)
+        if ''.join(out) != '':
+          self.run_cmd('flashrom -w -i RW_SECTION_B:%s' % rw_b, 'SUCCESS')
+
+        # 7) Report result.
+        if ''.join(result_output) != '':
+            raise error.TestFail('Mismatch between %s and %s' % (rw_b, rw_b2))
