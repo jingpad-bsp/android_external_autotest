jingpad-bsp/android_external_autotest

commit ea380fb2a314dcfe73b8f7b421bc9407332ba72d

Modify fastboot_serial for TCP
The brillo_BootLoader test is failing for boards that are using
adb and fastboot over TCP because adb and fastboot serials differ.
Prepend "tcp:" to the IP retrieved from adb serial to
obtain fastboot serial

Fastboot devices does not list serials for TCP. In order to chack that
the device is in fastboot mode we check that fastboot getvar
is successful

b:27915323

Change-Id: I7409819b3e0881ca7ddc5f67ccdd6e4a3548681f
Signed-off-by: Alexandru Branciog <alexandru.n.branciog@intel.com>
Reviewed-on: https://chromium-review.googlesource.com/336737
Commit-Ready: Mohammed Habibulla <moch@google.com>
Tested-by: Alexandru N Branciog <alexandru.n.branciog@intel.com>
Reviewed-by: Dan Shi <dshi@google.com>

server/hosts/adb_host.py
@@ -43,9 +43,10 @@ DEVICE_NO_SERIAL_TAG = '<NO_SERIAL>'
 # 0146B5580B01801B    device
 # 018e0ecb20c97a62    device
 # 172.22.75.141:5555  device
-DEVICE_FINDER_REGEX = (r'^(?P<SERIAL>([\w-]+)|(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})|' +
+DEVICE_FINDER_REGEX = (r'^(?P<SERIAL>([\w-]+)|((tcp:)?' +
+                       '\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}([:]5555)?)|' +
                        re.escape(DEVICE_NO_SERIAL_MSG) +
-                       r')([:]5555)?[ \t]+(?:device|fastboot)')
+                       r')[ \t]+(?:device|fastboot)')
 CMD_OUTPUT_PREFIX = 'ADB_CMD_OUTPUT'
 CMD_OUTPUT_REGEX = ('(?P<OUTPUT>[\s\S]*)%s:(?P<EXIT_CODE>\d{1,3})' %
                     CMD_OUTPUT_PREFIX)
@@ -205,7 +206,10 @@ class ADBHost(abstract_ssh.AbstractSSHHost):
         # refactor the serial retrieval.
         adb_serial = adb_serial or self.host_attributes.get('serials', None)
         self.adb_serial = adb_serial
-        self.fastboot_serial = fastboot_serial or adb_serial
+        adb_prefix = any(adb_serial.startswith(p) for p in ADB_DEVICE_PREFIXES)
+        self.fastboot_serial = (fastboot_serial or
+                ('tcp:%s' % adb_serial.split(':')[0] if
+                ':' in adb_serial and not adb_prefix else adb_serial))
         self.teststation = (teststation if teststation
                 else teststation_host.create_teststationhost(hostname=hostname))
 
@@ -216,7 +220,6 @@ class ADBHost(abstract_ssh.AbstractSSHHost):
             msg += ', fastboot serial: %s' % self.fastboot_serial
         logging.debug(msg)
 
-        adb_prefix = any(adb_serial.startswith(p) for p in ADB_DEVICE_PREFIXES)
         self._use_tcpip = ':' in adb_serial and not adb_prefix
         # Try resetting the ADB daemon on the device, however if we are
         # creating the host to do a repair job, the device maybe inaccesible
@@ -242,6 +245,7 @@ class ADBHost(abstract_ssh.AbstractSSHHost):
         self.adb_run('root')
         # TODO(ralphnathan): Remove this sleep once b/19749057 is resolved.
         time.sleep(1)
+        self._connect_over_tcpip_as_needed()
         self.adb_run('wait-for-device')
 
 
@@ -551,10 +555,21 @@ class ADBHost(abstract_ssh.AbstractSSHHost):
         @returns a list of devices attached to the test station.
         """
         if use_adb:
-            result = self.adb_run('devices')
+            result = self.adb_run('devices').stdout
+            if self.adb_serial and self.adb_serial not in result:
+                self._connect_over_tcpip_as_needed()
         else:
-            result = self.fastboot_run('devices')
-        return self.parse_device_serials(result.stdout)
+            result = self.fastboot_run('devices').stdout
+            if (self.fastboot_serial and
+                self.fastboot_serial not in result):
+                # fastboot devices won't list the devices using TCP
+                try:
+                    if 'product' in self.fastboot_run('getvar product',
+                                                      timeout=2).stderr:
+                        result += '\n%s\tfastboot' % self.fastboot_serial
+                except error.AutotestHostRunError:
+                    pass
+        return self.parse_device_serials(result)
 
 
     def adb_devices(self):

commit 3a011ed46a847d711d2f769497073680a1846266

[autotest] Bug fix in adb_host init
For host without adb_serial specified in host attribute, test job will fail.
The bug is introduced in CL 336737
It also cause server-side packaging to fail as host attributes is not retrieved
from AFE while autoserv tries to stage server side package.

BUG=None
TEST=local run a test with server-side packaging.

Change-Id: I31e05a06a7d1d271b9e6b24a5ad971f2db6d2e06
Reviewed-on: https://chromium-review.googlesource.com/340761
Commit-Ready: Dan Shi <dshi@google.com>
Tested-by: Dan Shi <dshi@google.com>
Reviewed-by: Kevin Cheng <kevcheng@chromium.org>

server/hosts/adb_host.py
@@ -200,10 +200,16 @@ class ADBHost(abstract_ssh.AbstractSSHHost):
         # refactor the serial retrieval.
         adb_serial = adb_serial or self.host_attributes.get('serials', None)
         self.adb_serial = adb_serial
-        adb_prefix = any(adb_serial.startswith(p) for p in ADB_DEVICE_PREFIXES)
-        self.fastboot_serial = (fastboot_serial or
-                ('tcp:%s' % adb_serial.split(':')[0] if
-                ':' in adb_serial and not adb_prefix else adb_serial))
+        if adb_serial:
+            adb_prefix = any(adb_serial.startswith(p)
+                             for p in ADB_DEVICE_PREFIXES)
+            self.fastboot_serial = (fastboot_serial or
+                    ('tcp:%s' % adb_serial.split(':')[0] if
+                    ':' in adb_serial and not adb_prefix else adb_serial))
+            self._use_tcpip = ':' in adb_serial and not adb_prefix
+        else:
+            self.fastboot_serial = fastboot_serial or adb_serial
+            self._use_tcpip = False
         self.teststation = (teststation if teststation
                 else teststation_host.create_teststationhost(hostname=hostname))
 
@@ -214,7 +220,6 @@ class ADBHost(abstract_ssh.AbstractSSHHost):
             msg += ', fastboot serial: %s' % self.fastboot_serial
         logging.debug(msg)
 
-        self._use_tcpip = ':' in adb_serial and not adb_prefix
         # Try resetting the ADB daemon on the device, however if we are
         # creating the host to do a repair job, the device maybe inaccesible
         # via ADB.