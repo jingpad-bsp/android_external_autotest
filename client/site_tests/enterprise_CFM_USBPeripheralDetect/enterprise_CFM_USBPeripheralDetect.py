9ca6d5f228632fecf1b9b3f945175e1bdd688494
harpreet <harpreet@google.com>
2016-05-11 18:44:51 -0700

    Fix the CFM usb peripheral detection test
    CFM usb peripheral detection test should rely on telemetry to query for
    attached peripharls as detected by the hotrod app instead of reading the
    jmi data file.
    
    BUG=None
    TEST=Locally
    
    Change-Id: I900c945f99d95f6475d293a4bd8529da9a68c197
    Reviewed-on: https://chromium-review.googlesource.com/344402
    Commit-Ready: harpreet Grewal <harpreet@chromium.org>
    Tested-by: harpreet Grewal <harpreet@chromium.org>
    Reviewed-by: harpreet Grewal <harpreet@chromium.org>
    Reviewed-by: Kalin Stoyanov <kalin@chromium.org>
    Reviewed-by: Mats Nilsson <mnilsson@chromium.org>

---
+++ client/site_tests/enterprise_CFM_USBPeripheralDetect/enterprise_CFM_USBPeripheralDetect.py
@@ -0,0 +1,77 @@
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging, time
+
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib.cros import chrome, cfm_util
+
+SHORT_TIMEOUT = 2
+EXT_ID = 'ikfcpmgefdpheiiomgmhlmmkihchmdlj'
+
+
+class enterprise_CFM_USBPeripheralDetect(test.test):
+    """Tests that the peripherals detected by the hotrod app match those
+    detected by CrOS in the server side test.
+    """
+    version = 1
+
+
+    def peripheral_detection(self, webview_context):
+        """Get attached peripheral information.
+
+        @param webview_context: Context for hangouts window.
+        """
+        cfm_peripheral_dict = {'Microphone': None, 'Speaker': None,
+                               'Camera': None}
+
+        cfm_util.wait_for_telemetry_commands(webview_context)
+        cfm_util.wait_for_oobe_start_page(webview_context)
+
+        if not cfm_util.is_oobe_start_page(webview_context):
+            raise error.TestFail('CFM did not reach oobe screen.')
+
+        cfm_util.skip_oobe_screen(webview_context)
+        time.sleep(SHORT_TIMEOUT)
+
+        cfm_peripheral_dict['Microphone'] = cfm_util.get_preferred_mic(
+                webview_context)
+        cfm_peripheral_dict['Speaker'] = cfm_util.get_preferred_speaker(
+                webview_context)
+        cfm_peripheral_dict['Camera'] = cfm_util.get_preferred_camera(
+                webview_context)
+
+        for device_type, is_found in cfm_peripheral_dict.iteritems():
+            if not is_found:
+                cfm_peripheral_dict[device_type] = 'Not Found'
+
+        return cfm_peripheral_dict
+
+
+    def run_once(self, cros_peripheral_dict):
+        """Runs the test."""
+        with chrome.Chrome(clear_enterprise_policy=False,
+                           dont_override_profile=True,
+                           disable_gaia_services=False,
+                           disable_default_apps=False,
+                           auto_login=False) as cr:
+            cfm_webview_context = cfm_util.get_cfm_webview_context(
+                    cr.browser, EXT_ID)
+            cfm_peripheral_dict = self.peripheral_detection(cfm_webview_context)
+            logging.debug('Peripherals detected by hotrod: %s',
+                          cfm_peripheral_dict)
+
+            cros_peripherals = set(cros_peripheral_dict.iteritems())
+            cfm_peripherals = set(cfm_peripheral_dict.iteritems())
+
+            peripheral_diff = cros_peripherals.difference(cfm_peripherals)
+
+            if peripheral_diff:
+                no_match_list = list()
+                for item in peripheral_diff:
+                    no_match_list.append(item[0])
+
+                raise error.TestFail('Following peripherals do not match: %s' %
+                                     ', '.join(no_match_list))
