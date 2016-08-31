24b242ef09856eb56cc2fc48a07758de215adec3
Scott Cunningham <scunningham@chromium.org>
2016-08-30 17:45:13 -0700

    Copy resultsdir from DUT to Server at end of test.
    Add a file copy method to enterprise_LongevityTrackerServer that
    will copy the results files (i.e., keyval, perf.csv, and
    results-chart.json) from the DUT's results directory (typically at
    /usr/local/autotest/results/default/longevity_Tracker/results/), to
    the server-side results directory (path stored in self.resultsdir,
    in the runtime scope of the server-side test).
    
    My local branch name: longsrvrcopy
    
    BUG=chromium:641762
    TEST=test_that <IP> f:.*enterprise_LongevityTrackerServer/
    control.chromesign
    
    Change-Id: Ie38ba04f66a514c7e4868dc11217201a17f2a476
    Reviewed-on: https://chromium-review.googlesource.com/376867
    Commit-Ready: Scott Cunningham <scunningham@chromium.org>
    Tested-by: Scott Cunningham <scunningham@chromium.org>
    Reviewed-by: Scott Cunningham <scunningham@chromium.org>
    Reviewed-by: Harpreet Grewal <harpreet@chromium.org>

--- server/site_tests/enterprise_LongevityTrackerServer/enterprise_LongevityTrackerServer.py
+++ server/site_tests/enterprise_LongevityTrackerServer/enterprise_LongevityTrackerServer.py
@@ -2,33 +2,63 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# pylint: disable=invalid-name
+# pylint: disable=missing-docstring
+# pylint: disable=module-missing-docstring
+# pylint: disable=docstring-section-name
+# pylint: disable=no-init
+
+import os
+import shutil
+
 from autotest_lib.client.common_lib.cros import tpm_utils
-from autotest_lib.server import test
-from autotest_lib.server import autotest
 from autotest_lib.server import afe_utils
+from autotest_lib.server import autotest
 from autotest_lib.server import site_utils
+from autotest_lib.server import test
 
 
 class enterprise_LongevityTrackerServer(test.test):
-    """A test that runs enterprise_KioskEnrollment and clears the TPM as
-    necessary. After enterprise enrollment is successful, it collects and logs
-    cpu, memory and temperature data from the device under test."""
+    """Run Longevity Test: Collect performance data over long duration.
+
+    Run enterprise_KioskEnrollment and clear the TPM as necessary. After
+    enterprise enrollment is successful, collect and log cpu, memory, and
+    temperature data from the device under test.
+    """
     version = 1
+
+    CLIENT_RESULTS_PATH = 'longevity_Tracker/results/'
+
+    def copy_results_files_to_server(self):
+        host_results = os.path.join(
+                os.path.dirname(self.resultsdir),
+                self.CLIENT_RESULTS_PATH)
+        result_files = os.listdir(host_results)
+        for file_name in result_files:
+            full_file_name = os.path.join(host_results, file_name)
+            if os.path.isfile(full_file_name):
+                shutil.copy(full_file_name, self.resultsdir)
 
     def run_once(self, host=None, kiosk_app_attributes=None):
         self.client = host
         app_config_id = None
         tpm_utils.ClearTPMOwnerRequest(self.client)
+
         app_config_id = site_utils.get_label_from_afe(
                 self.client.hostname, 'app_config_id', afe_utils.AFE)
         if app_config_id and app_config_id.startswith(':'):
             app_config_id = app_config_id[1:]
-        autotest.Autotest(self.client).run_test('enterprise_KioskEnrollment',
+        autotest.Autotest(self.client).run_test(
+                'enterprise_KioskEnrollment',
                 kiosk_app_attributes=kiosk_app_attributes,
                 app_config_id=app_config_id,
                 check_client_result=True)
+
         for cycle in range(5):
-            autotest.Autotest(self.client).run_test('longevity_Tracker',
+            autotest.Autotest(self.client).run_test(
+                    'longevity_Tracker',
                     kiosk_app_attributes=kiosk_app_attributes,
                     check_client_result=True)
+        self.copy_results_files_to_server()
+
         tpm_utils.ClearTPMOwnerRequest(self.client)
