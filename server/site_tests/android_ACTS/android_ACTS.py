7bf05519a83e76ad818cd2ef016f13be6cfc7aaf
Benny Peake <bpeake@google.com>
2016-09-02 16:57:58 -0700

    ACTs now deploys from devserver
    When running any ACTs job on autotest, ACTs along with all test
    files will be deployed from the devserver artifact.
    
    BUG=b:30102263
    TEST=None
    Change-Id: I6cc788ab4122188ebd7456b1d69d93f8d40b4a7a
    Reviewed-on: https://chromium-review.googlesource.com/380829
    Tested-by: Benjamin Peake <bpeake@chromium.org>
    Reviewed-by: Dan Shi <dshi@google.com>
    Commit-Queue: Benjamin Peake <bpeake@chromium.org>

--- server/site_tests/android_ACTS/android_ACTS.py
+++ server/site_tests/android_ACTS/android_ACTS.py
@@ -2,15 +2,17 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging
 import json
+import logging
 import os
 import sys
 
 import common
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib import global_config
+from autotest_lib.client.common_lib.cros import dev_server
 from autotest_lib.server import adb_utils
+from autotest_lib.server import afe_utils
 from autotest_lib.server import constants
 from autotest_lib.server import test
 from autotest_lib.site_utils import sponge_utils
@@ -18,8 +20,8 @@ from autotest_lib.site_utils import sponge_utils
 CONFIG_FOLDER_LOCATION = global_config.global_config.get_config_value(
         'ACTS', 'acts_config_folder', default='')
 
-TEST_CONFIG_FILE_FOLDER = 'autotest_config'
-TEST_CAMPAIGN_FILE_FOLDER = 'autotest_campaign'
+DEFAULT_TEST_RELATIVE_LOG_PATH = 'results/logs'
+
 
 class android_ACTS(test.test):
     """Run an Android CTS test case.
@@ -36,70 +38,317 @@ class android_ACTS(test.test):
         'SKIP': 'ABORT'
     }
 
-
-    def push_file_to_teststation(self, input_path, sub_dir_name):
+    def push_file_to_teststation(self,
+                                 filename,
+                                 input_path=None,
+                                 output_path=None):
         """Ensures the file specified by a path exists on test station. If the
         file specified by input_path does not exist, attempt to locate it in
         ACTS dirctory.
 
-        @param input_path: A string that's the path to a file.
-        @param sub_dir_name: A string that's the subdirectory name of where the
-                             file exists.
-        @returns: The actual path of the file on work station.
+        @param filename: The name of the file relative to both the input and output
+                         path.
+        @param input_path: The base path on the drone to get the file
+                           from. If none, then the folder in the filename
+                           is used.
+        @param output_path: The base path on the test station to put the file.
+                            By default this is the temp folder.
+
+        @returns: The full path on the test station.
         """
-        if os.path.exists(input_path):
-            self.test_station.send_file(input_path, self.ts_tempfolder)
-            return input_path
-        actual_path = os.path.join(CONFIG_FOLDER_LOCATION,
-                                   sub_dir_name,
-                                   input_path)
-        actual_path = os.path.realpath(actual_path)
-        if not os.path.exists(actual_path):
-            raise error.TestFail('File: %s does not exist' % actual_path)
-        self.test_station.send_file(actual_path, self.ts_tempfolder)
-        return actual_path
+        if not input_path:
+            input_path = os.path.dirname(os.path.abspath(filename))
+
+        if not output_path:
+            output_path = os.path.join(self.ts_tempfolder, 'configs')
+
+        # Find the path on the source machine
+        full_input_path = os.path.abspath(input_path)
+        if not os.path.exists(full_input_path):
+            raise error.TestFail('Invalid input path given %s' % input_path)
+        full_src_file = os.path.join(full_input_path, filename)
+        if not os.path.exists(full_src_file):
+            raise error.TestFail(
+                    'Invalid filename, no full path %s exists' % full_src_file)
+
+        # Find the directory part of the file
+        file_dir = os.path.dirname(filename)
+
+        # Find the path on the test station
+        dst_dir = os.path.join(output_path, file_dir)
+
+        self.test_station.send_file(full_src_file, dst_dir)
+        return os.path.join(output_path, filename)
+
+    def install_sl4a_apk(self):
+        """Installs sl4a on all phones connected to the testbed."""
+        for serial, adb_host in self.testbed.get_adb_devices().iteritems():
+            adb_utils.install_apk_from_build(
+                    adb_host,
+                    constants.SL4A_APK,
+                    constants.SL4A_PACKAGE,
+                    package_name=constants.SL4A_PACKAGE)
+
+    def download_acts(self, download_locaiton=None):
+        """Downloads acts onto a test station.
+
+        Pulls down acts.zip from from devserver and unzips it into the temp
+        directory for this test.
+
+        @param download_locaiton: The directory on the test station to download
+                                  acts into.
+
+        @returns: The base directory for acts.
+        """
+        if not download_locaiton:
+            download_locaiton = self.ts_tempfolder
+
+        host = next(v for v in self.testbed.get_adb_devices().values())
+
+        if not host:
+            raise error.TestError(
+                    'No hosts defined for this test, cannot'
+                    ' determine build to grab artifact from.')
+
+        job_repo_url = afe_utils.get_host_attribute(
+                host, host.job_repo_url_attribute)
+        if not job_repo_url:
+            raise error.TestError('No job repo url defined for this DUT.')
+
+        logging.info('Pulling acts from artifact.')
+
+        devserver_url = dev_server.AndroidBuildServer.get_server_url(
+                job_repo_url)
+        devserver = dev_server.AndroidBuildServer(devserver_url)
+        build_info = host.get_build_info_from_build_url(job_repo_url)
+
+        devserver.trigger_download(
+                build_info['target'],
+                build_info['build_id'],
+                build_info['branch'],
+                files='acts.zip',
+                synchronous=True)
+
+        temp_dir = download_locaiton
+
+        logging.info('Downloading from dev server %s', job_repo_url)
+
+        download_dir = '%s/acts' % temp_dir
+        host.download_file(
+                build_url=job_repo_url,
+                file='acts.zip',
+                dest_dir=temp_dir,
+                unzip=True,
+                unzip_dest=download_dir)
+
+        base_acts_dir = os.path.join(
+                download_dir, 'tools/test/connectivity/acts')
 
 
-    def post_act_cmd(self, config_file, testbed_name, test_case):
+        logging.info('ACTs downloaded to %s on test station', base_acts_dir)
+
+        return base_acts_dir
+
+    def setup_configs(self,
+                      config_file,
+                      additional_configs=[],
+                      configs_local_location=None,
+                      configs_remote_location=None):
+        """Setup config files on the test station.
+
+        Takes configuration files and uploads them onto the tests station.
+        Then does any additional setup that is needed.
+
+        @param config_file: The main config for acts to use.
+        @param additional_configs: An additional set of config files to send.
+        @param configs_local_location: Where on the drone are config files
+                                       being found. If none then the default
+                                       location defined in the autotest
+                                       configs is used.
+        @param configs_remote_location: The directory to store configs in,
+                                        by default it is a sub folder of the
+                                        temp folder.
+
+        @returns A list of locations on the test station where the configs where
+                 uploaded. The first element is always the main config.
+        """
+        if not configs_local_location:
+            configs_local_location = CONFIG_FOLDER_LOCATION
+
+        if not configs_remote_location:
+            configs_remote_location = os.path.join(
+                    self.ts_tempfolder,
+                    'configs')
+
+        if not config_file:
+            raise error.TestFail('A config file must be specified.')
+
+        logging.info('Pulling configs from %s.', configs_local_location)
+
+        remote_config_file = self.push_file_to_teststation(
+                config_file,
+                input_path=configs_local_location,
+                output_path=configs_remote_location)
+
+        remote_configs = [remote_config_file]
+
+        for additional_config in additional_configs:
+            remote_location = self.push_file_to_teststation(
+                    additional_config,
+                    input_path=configs_local_location,
+                    output_path=configs_remote_location)
+
+            remote_configs.append(remote_location)
+
+        return remote_configs
+
+    def build_environment(self, base_acts_dir=None, log_path=None):
+        """Builds the environment variables for a run.
+
+        @param base_acts_dir: Where acts is stored. If none then the default
+                              acts download location is used.
+        @param log_path: The path to the log file. If none then a log folder
+                         under the temp folder is used.
+
+        @returns: The enviroment variables as a dictionary.
+        """
+        if not log_path:
+            log_path = '%s/%s' % (
+                    self.ts_tempfolder,
+                    DEFAULT_TEST_RELATIVE_LOG_PATH)
+
+        if not base_acts_dir:
+            base_acts_dir = self.acts_download_dir
+
+        framework_dir = os.path.join(base_acts_dir, 'framework')
+        base_test_dir = os.path.join(base_acts_dir, 'tests')
+
+        walk_iterator = os.walk(base_test_dir, followlinks=True)
+        all_sud_dirs = []
+        for _, child_dirs, _ in walk_iterator:
+            for child_dir in child_dirs:
+                all_sud_dirs.append(child_dir)
+
+        test_search_dirs = all_sud_dirs + [base_test_dir]
+
+        get_path_result = self.test_station.run('echo $PYTHONPATH')
+        remote_path = get_path_result.stdout
+        new_python_path = '%s:%s' % (remote_path, framework_dir)
+
+        env = {'ACTS_TESTPATHS': ':'.join(test_search_dirs),
+               'ACTS_LOGPATH': log_path,
+               'PYTHONPATH': new_python_path}
+
+        return env
+
+    def run_acts(self,
+                 remote_config_file,
+                 testing_working_dir=None,
+                 remote_acts_file='framework/acts/bin/act.py',
+                 test_file=None,
+                 test_case=None,
+                 env={},
+                 testbed_name=None,
+                 timeout=7200):
+        """Runs ACTs on the test station.
+
+        Runs a test on on the test station and handles logging any details
+        about it during runtime.
+
+        @param remote_config_file: The config file on the test station to use.
+        @param testing_working_dir: The working directory to run acts from.
+                                    By default the acts download location is
+                                    used.
+        @param remote_acts_file: The acts file to use relative from the wokring
+                                 directory.
+        @param test_file: The -tf argument for acts.
+        @param test_case: The tc argument for acts.
+        @param env: The enviroment variables to use with acts.
+        @param testbed_name: The name of the test bed to use, if None then the
+                             default testbed name for the test is used.
+        @param timeout: How long to wait for acts.
+        """
+        if not testbed_name:
+            testbed_name = self.testbed_name
+
+        if not testing_working_dir:
+            testing_working_dir = self.acts_download_dir
+
+        exports = []
+        for key, value in env.items():
+            exports.append('export %s="%s"' % (key, str(value)))
+        env_setup = '; '.join(exports)
+
+        command_setup = 'cd %s' % testing_working_dir
+        act_base_cmd = 'python %s -c %s -tb %s ' % (
+                remote_acts_file, remote_config_file, testbed_name)
+
+        if test_case and test_file:
+            raise ValueError(
+                    'test_case and test_file cannot both have a value.')
+        elif test_case:
+            act_cmd = '%s -tc %s' % (act_base_cmd, test_case)
+        elif test_file:
+            act_cmd = '%s -tf %s' % (act_base_cmd, test_file)
+        else:
+            raise error.TestFail('No tests was specified!')
+
+        command_list = [command_setup, env_setup, act_cmd]
+        full_command = '; '.join(command_list)
+
+        try:
+            logging.debug('Running: %s', full_command)
+            act_result = self.test_station.run(full_command, timeout=timeout)
+            logging.debug('ACTS Output:\n%s', act_result.stdout)
+        except:
+            raise error.TestError('Unexpected error: %s', sys.exc_info())
+
+    def post_act_cmd(self, test_case=None, test_file=None, log_path=None):
         """Actions to take after act_cmd is finished or failed.
 
         Actions include collect logs from test station generated by act_cmd
         and record job results based on `test_run_summary.json`.
-
-        @param config_file: Path to config file locally. Required.
-        @param testbed_name: A string that's passed to act.py's -tb option.
-                             If testbed_name is not provided, set it to the
-                             testbed's hostname without the DNS zone.
         @param test_case: A string that's passed to act.py's -tc option.
+        @param test_file: A string that's passed to act.py's -tf option.
+
+        @param log_path: The path to where the log output.
         """
-        # Transport ACTS logs from test station to work station for parsing.
-        with open(config_file, 'r') as f:
-            configs = json.load(f)
-        log_path = os.path.join(configs['logpath'], testbed_name, 'latest')
+        if not log_path:
+            log_path = os.path.join(
+                    self.ts_tempfolder,
+                    DEFAULT_TEST_RELATIVE_LOG_PATH)
+
         # Following call may fail if logs are not generated by act_cmd yet.
         # Anyhow, the test must have failed already in that case.
         self.test_station.get_file(log_path, self.resultsdir)
         # Load summary json file.
-        summary_path = os.path.join(self.resultsdir,
-                                    'latest',
-                                    'test_run_summary.json')
+        summary_path = os.path.join(
+                self.resultsdir, 'latest', 'test_run_summary.json')
         # If the test has failed, test_run_summary.json may not exist.
         if os.path.exists(summary_path):
-            sponge_utils.upload_results_in_test(self, acts_summary=summary_path)
+            sponge_utils.upload_results_in_test(
+                    self, acts_summary=summary_path)
             with open(summary_path, 'r') as f:
                 results = json.load(f)['Results']
             # Report results to Autotest.
             for result in results:
                 verdict = self.acts_result_to_autotest[result['Result']]
                 details = result['Details']
-                self.job.record(verdict, None, test_case,
-                                status=(details or ''))
+                self.job.record(
+                        verdict,
+                        None,
+                        test_case or test_file,
+                        status=(details or ''))
         else:
             logging.debug('summary at path %s does not exist!', summary_path)
 
-
-    def run_once(self, testbed=None, config_file=None, testbed_name=None,
-                 test_case=None, test_file=None, additional_configs=[],
+    def run_once(self,
+                 testbed=None,
+                 config_file=None,
+                 testbed_name=None,
+                 test_case=None,
+                 test_file=None,
+                 additional_configs=[],
                  acts_timeout=7200):
         """Run ACTS on the DUT.
 
@@ -117,42 +366,27 @@ class android_ACTS(test.test):
                                    in the TEST_CONFIG_FILE_FOLDER.
         @param acts_timeout: A timeout for the specific ACTS test.
         """
-        testbed_name = testbed_name or testbed.hostname.split('.')[0]
-        for serial, adb_host in testbed.get_adb_devices().iteritems():
-            adb_utils.install_apk_from_build(
-                    adb_host, constants.SL4A_APK, constants.SL4A_PACKAGE,
-                    package_name=constants.SL4A_PACKAGE)
-        self.test_station = testbed.get_test_station()
-        # Get a tempfolder on the device.
-        self.ts_tempfolder = self.test_station.get_tmp_dir()
-        if not config_file:
-            raise error.TestFail('A config file must be specified.')
-        config_file = self.push_file_to_teststation(config_file,
-                                                    TEST_CONFIG_FILE_FOLDER)
-
-        for additional_config in additional_configs:
-            self.push_file_to_teststation(additional_config,
-                                          TEST_CONFIG_FILE_FOLDER)
-        if test_file:
-            self.push_file_to_teststation(test_file,
-                                          TEST_CAMPAIGN_FILE_FOLDER)
-        act_base_cmd = 'act.py -c %s -tb %s ' % (
-            os.path.join(self.ts_tempfolder, os.path.basename(config_file)),
-            testbed_name)
-        # Run the acts script.
-        if test_case:
-            act_cmd = '%s -tc %s' % (act_base_cmd, test_case)
-        elif test_file:
-            act_cmd = '%s -tf %s' % (act_base_cmd,
-                os.path.join(self.ts_tempfolder, os.path.basename(test_file)))
-        else:
-            raise error.TestFail('No test was specified,  abort!')
         try:
-            logging.debug('Running: %s', act_cmd)
-            # TODO: Change below to be test_bed.teststation_host.run
-            act_result = self.test_station.run(act_cmd, timeout=acts_timeout)
-            logging.debug('ACTS Output:\n%s', act_result.stdout)
-        except:
-            raise error.TestError('Unexpected error: %s', sys.exc_info())
+            # setup properties
+            self.testbed = testbed
+            self.testbed_name = testbed_name or testbed.hostname.split('.')[0]
+            self.test_station = testbed.teststation
+            self.ts_tempfolder = self.test_station.get_tmp_dir()
+
+            # install all needed tools
+            self.install_sl4a_apk()
+            self.acts_download_dir = self.download_acts()
+            remote_config_file = self.setup_configs(
+                    config_file, additional_configs)
+            env = self.build_environment()
+
+            # launch acts
+            self.run_acts(
+                    remote_config_file[0],
+                    test_file=test_file,
+                    test_case=test_case,
+                    env=env,
+                    timeout=acts_timeout)
         finally:
-            self.post_act_cmd(config_file, testbed_name, test_case)
+            # cleanup
+            self.post_act_cmd(test_case=test_case, test_file=test_file)
