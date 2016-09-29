e410ace2bf7e89506912edd943190d6b9a94479f
Bindu Mahadev <bmahadev@google.com>
2016-09-29 16:43:23 -0700

    [Autotest]scapy installation for APF tests.
    Include scapy installation on the router as part of test and
    create a common place for scapy related files and utility methods.
    
    BUG=b:30954813
    TEST=Ran network_WiFi_IPv6RA
    
    Change-Id: I52c24892ba581dabc50b213bc97e369e78308b75
    Reviewed-on: https://chromium-review.googlesource.com/391215
    Tested-by: Bindu Mahadev <bmahadev@chromium.org>
    Reviewed-by: Kris Rambish <krisr@chromium.org>
    Reviewed-by: Bindu Mahadev <bmahadev@chromium.org>

--- server/site_tests/network_WiFi_IPv6RA/network_WiFi_IPv6RA.py
+++ server/site_tests/network_WiFi_IPv6RA/network_WiFi_IPv6RA.py
@@ -3,69 +3,35 @@
 # found in the LICENSE file.
 
 import logging
-import os
 import time
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib.cros.network import xmlrpc_datatypes
-from autotest_lib.server import site_linux_system
 from autotest_lib.server.cros.network import wifi_cell_test_base
 from autotest_lib.server.cros.network import hostap_config
-from autotest_lib.server.hosts import ssh_host
+from autotest_lib.server.cros.packet_generation import IP_utils
 
-SEND_RA_SCRIPT = 'sendra.py'
-INSTALL_PATH = '/usr/local/bin'
-PROC_NET_SNMP6 = '/proc/net/snmp6'
-MULTICAST_ADDR = '33:33:00:00:00:01'
 LIFETIME_FRACTION = 6
 LIFETIME = 180
 INTERVAL = 2
-IFACE = 'managed0'
 
 class network_WiFi_IPv6RA(wifi_cell_test_base.WiFiCellTestBase):
     """Test that we can drop/accept various IPv6 RAs."""
     version = 1
 
-    def _cleanup(self, client_conf):
+    def _cleanup(self, client_conf, router, ip_utils):
         """Deconfigure AP and cleanup test resources.
 
         @param client_conf: association parameters for test.
+        @param router: Router host object.
+        @param ip_utils object to cleanup.
 
         """
         self.context.client.shill.disconnect(client_conf.ssid)
         self.context.client.shill.delete_entries_for_ssid(client_conf.ssid)
         self.context.router.deconfig()
         self.context.capture_host.stop_capture()
-
-
-    def send_ra(self, mac=MULTICAST_ADDR, interval=1, count=None, iface=IFACE,
-                lifetime=LIFETIME):
-        """Invoke scapy and send RA to the device.
-
-        @param mac: string HWAddr/MAC address to send the packets to.
-        @param interval: int Time to sleep between consecutive packets.
-        @param count: int Number of packets to be sent.
-        @param iface: string of the WiFi interface to use for sending packets.
-        @param lifetime: int router lifetime in seconds.
-
-        """
-        scapy_command = os.path.join(INSTALL_PATH, SEND_RA_SCRIPT)
-        options = ' -m %s -i %d -c %d -l %d -in %s' %(mac, interval, count,
-                                                      lifetime, iface)
-        self.context.router.host.run(scapy_command + options)
-
-
-    def get_icmp6intype134(self):
-        """Read the value of Icmp6InType134 and return integer.
-
-        @return integer value >=0; raises TestError if command fails.
-
-        """
-        ra_count_str = self.context.client.host.run(
-                'grep Icmp6InType134 %s' % PROC_NET_SNMP6).stdout
-        if ra_count_str:
-            return int(ra_count_str.split()[1])
-        raise error.TestError('Failed to fetch value of Icmp6InType134')
+        ip_utils.cleanup_scapy()
 
 
     def run_once(self):
@@ -75,7 +41,7 @@ class network_WiFi_IPv6RA(wifi_cell_test_base.WiFiCellTestBase):
         self.context.router.deconfig()
         ap_config = hostap_config.HostapConfig(channel=6,
                 mode=hostap_config.HostapConfig.MODE_11G,
-                ssid='OnHubWiFi_ch5_802.11g')
+                ssid='OnHubWiFi_ch6_802.11g')
         self.context.configure(ap_config)
         self.context.capture_host.start_capture(2437, ht_type='HT20')
         client_conf.ssid = self.context.router.get_ssid()
@@ -91,17 +57,13 @@ class network_WiFi_IPv6RA(wifi_cell_test_base.WiFiCellTestBase):
         # Sleep for 10 seconds to put the phone in WoW mode.
         time.sleep(10)
 
-        # Copy scapy script to the AP.
-        ap_name = self.context.client.host.hostname.split('.')[0] + '-router'
-        ap_sshhost = ssh_host.SSHHost(hostname=ap_name)
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-        send_ra_script = os.path.join(current_dir, SEND_RA_SCRIPT)
-        ap_sshhost.send_file(send_ra_script, INSTALL_PATH)
+        ip_utils = IP_utils.IPutils(self.context.router.host)
+        ip_utils.install_scapy()
+        ra_count = ip_utils.get_icmp6intype134(self.context.client.host)
 
-        ra_count = self.get_icmp6intype134()
         # Start scapy to send RA to the phone's MAC
-        self.send_ra(mac=client_mac, interval=0, count=1)
-        ra_count_latest = self.get_icmp6intype134()
+        ip_utils.send_ra(mac=client_mac, interval=0, count=1)
+        ra_count_latest = ip_utils.get_icmp6intype134(self.context.client.host)
 
         # The phone should accept the first unique RA in sequence.
         if ra_count_latest != ra_count + 1:
@@ -114,10 +76,11 @@ class network_WiFi_IPv6RA(wifi_cell_test_base.WiFiCellTestBase):
         # original RA should be filtered.
         ra_count = ra_count_latest
         count = LIFETIME / LIFETIME_FRACTION / INTERVAL
-        self.send_ra(mac=client_mac, interval=INTERVAL, count=count)
-        ra_count_latest = self.get_icmp6intype134()
+        ip_utils.send_ra(mac=client_mac, interval=INTERVAL, count=count)
+        ra_count_latest = ip_utils.get_icmp6intype134(self.context.client.host)
         pkt_loss = count - (ra_count_latest - ra_count)
         percentage_loss = float(pkt_loss) / count * 100
+
         # Fail test if at least 90% of RAs were not dropped.
         if percentage_loss < 90:
             logging.debug('Device did not filter duplicate RAs correctly.'
@@ -126,11 +89,11 @@ class network_WiFi_IPv6RA(wifi_cell_test_base.WiFiCellTestBase):
             raise error.TestFail('Device accepted a duplicate RA.')
 
         # Any new RA after this should be accepted.
-        self.send_ra(mac=client_mac, interval=INTERVAL, count=2)
-        ra_count_latest = self.get_icmp6intype134()
+        ip_utils.send_ra(mac=client_mac, interval=INTERVAL, count=2)
+        ra_count_latest = ip_utils.get_icmp6intype134(self.context.client.host)
         if ra_count_latest != ra_count + 1:
             logging.debug('Device did not accept new RA after 1/6th time'
                           ' interval.')
             raise error.TestFail('Device dropped a valid RA in sequence.')
 
-        self._cleanup(client_conf)
+        self._cleanup(client_conf, self.context.router.host, ip_utils)
