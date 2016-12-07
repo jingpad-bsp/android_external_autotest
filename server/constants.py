bdbea876455e617450c9f2f5c2c22d313bb86f9c
Benny Peake <bpeake@google.com>
2016-12-07 13:18:09 -0800

    Android apks now installed from any custom artifact
    Previously apks could only be installed from the preloaded
    artifacts. This meant that tests.zip had to be a part of
    provisioning and any apks outside of that could not be
    installed. The adb_host code has been changed to not download
    tests.zip until installing sl4a, and to allow additional
    apks to come form any artifact. By default if one is
    not given it comes form tests.zip.
    
    BUG=b:33214713
    TEST=Ran android_EasySetup
    
    Change-Id: I751ed042189509dad156830903f7c7e8ca1de8b8
    Reviewed-on: https://chromium-review.googlesource.com/417193
    Commit-Ready: Benjamin Peake <bpeake@chromium.org>
    Tested-by: Benjamin Peake <bpeake@chromium.org>
    Reviewed-by: Dan Shi <dshi@google.com>

--- server/constants.py
+++ server/constants.py
@@ -4,3 +4,4 @@ ANDROID_TESTER_FILEFLAG = '/mnt/stateful_partition/.android_tester'
 # Android test environment constants
 SL4A_APK = 'sl4a.apk'
 SL4A_PACKAGE = 'com.googlecode.android_scripting'
+SL4A_ARTIFACT = 'test_zip'
