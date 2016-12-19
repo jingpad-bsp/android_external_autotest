73fa120db9246f7236ea669dd5c8b0c95c04a8fa
Richard Barnette <jrbarnette@chromium.org>
2016-12-19 13:55:24 -0800

    [autotest] Fix dut_status_unittest time comparisons.
    The logic for unit testing `dut-status --until` was wrong.  It was
    supposed to test the "--until" value was "now" in the sense that
    it was in-between two timestamps that bracketed the calculation of
    the value.  One of the comparisons was for "<=" instead of ">=",
    leading to occasional flake.
    
    This changes to use a mock to force `time.time()` to return a
    pre-computed value.
    
    This change also contains other miscellaneous (non-functional)
    clean-up of the unit tests.
    
    BUG=chromium:675667
    TEST=run the unit tests using a patch to provoke the bug.
    
    Change-Id: I3b3a6fc10d7715cf83103f67e272ccc3a216efb0
    Reviewed-on: https://chromium-review.googlesource.com/422427
    Commit-Ready: Richard Barnette <jrbarnette@chromium.org>
    Tested-by: Richard Barnette <jrbarnette@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>

--- site_utils/dut_status_unittest.py
+++ site_utils/dut_status_unittest.py
@@ -3,6 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import mock
 import time
 import unittest
 
@@ -28,115 +29,101 @@ class TimeOptionTests(unittest.TestCase):
 
     """
 
-    def _try_parse(self, options):
-        arguments = dut_status._parse_command(
-                ['mumble.py'] + options + ['hostname'])
-        dut_status._validate_time_range(arguments)
-        return arguments
+    def setUp(self):
+        self.test_time = time.time()
 
+    def _try_parse(self, options):
+        with mock.patch('time.time', return_value=self.test_time):
+            arguments = dut_status._parse_command(
+                    ['mumble.py'] + options + ['hostname'])
+            dut_status._validate_time_range(arguments)
+        return arguments
 
     def _check_duration(self, arguments, duration):
         start_time = (arguments.until - duration * 3600)
-        assert arguments.since == start_time
-
-
-    def _check_default_end(self, arguments, end_time):
-        max_end = int(time.time())
-        assert arguments.until <= end_time
-        assert arguments.until <= max_end
-
+        self.assertEqual(arguments.since, start_time)
 
     def test_default_time_bounds(self):
         """Test time bounds when no options are supplied."""
-        end_time = int(time.time())
+        end_time = int(self.test_time)
         arguments = self._try_parse([])
-        self._check_default_end(arguments, end_time)
+        self.assertEqual(arguments.until, end_time)
         self._check_duration(arguments, dut_status._DEFAULT_DURATION)
-
 
     def test_start_only(self):
         """Test time bounds with --since only.
 
         Also tests that --since and -s are equivalent.
-
         """
-        end_time = int(time.time())
+        end_time = int(self.test_time)
         start_time = end_time - 3600
         start_time_string = time_utils.epoch_time_to_date_string(start_time)
         for option in ['--since', '-s']:
             arguments = self._try_parse([option, start_time_string])
-            self._check_default_end(arguments, end_time)
-            assert arguments.since == start_time
-
+            self.assertEqual(arguments.until, end_time)
+            self.assertEqual(arguments.since, start_time)
 
     def test_end_only(self):
         """Test time bounds with --until only.
 
         Also tests that --until and -u are equivalent.
-
         """
-        end_time = int(time.time()) - 3600
+        end_time = int(self.test_time) - 3600
         end_time_string = time_utils.epoch_time_to_date_string(end_time)
         for option in ['--until', '-u']:
             arguments = self._try_parse([option, end_time_string])
-            assert arguments.until == end_time
+            self.assertEqual(arguments.until, end_time)
             self._check_duration(arguments, dut_status._DEFAULT_DURATION)
-
 
     def test_duration_only(self):
         """Test time bounds with --duration only.
 
         Also tests that --duration and -d are equivalent.
-
         """
         for option in ['--duration', '-d']:
             duration = 4
             duration_string = '%d' % duration
-            end_time = int(time.time())
+            end_time = int(self.test_time)
             arguments = self._try_parse([option, duration_string])
-            self._check_default_end(arguments, end_time)
+            self.assertEqual(arguments.until, end_time)
             self._check_duration(arguments, duration)
-
 
     def test_start_and_end(self):
         """Test time bounds with --since and --until."""
-        start_time = int(time.time()) - 5 * 3600
+        start_time = int(self.test_time) - 5 * 3600
         start_time_string = time_utils.epoch_time_to_date_string(start_time)
         end_time = start_time + 4 * 3600
         end_time_string = time_utils.epoch_time_to_date_string(end_time)
-        arguments = self._try_parse(['--s', start_time_string,
-                                     '--u', end_time_string])
-        assert arguments.since == start_time
-        assert arguments.until == end_time
-
+        arguments = self._try_parse(['-s', start_time_string,
+                                     '-u', end_time_string])
+        self.assertEqual(arguments.since, start_time)
+        self.assertEqual(arguments.until, end_time)
 
     def test_start_and_duration(self):
         """Test time bounds with --since and --duration."""
-        start_time = int(time.time()) - 5 * 3600
+        start_time = int(self.test_time) - 5 * 3600
         start_time_string = time_utils.epoch_time_to_date_string(start_time)
         duration = 4
         duration_string = '%d' % duration
         arguments = self._try_parse(['-s', start_time_string,
                                      '-d', duration_string])
-        assert arguments.since == start_time
+        self.assertEqual(arguments.since, start_time)
         self._check_duration(arguments, duration)
-
 
     def test_end_and_duration(self):
         """Test time bounds with --until and --duration."""
-        end_time = int(time.time()) - 5 * 3600
+        end_time = int(self.test_time) - 5 * 3600
         end_time_string = time_utils.epoch_time_to_date_string(end_time)
         duration = 4
         duration_string = '%d' % duration
         arguments = self._try_parse(['-u', end_time_string,
                                      '-d', duration_string])
-        assert arguments.until == end_time
+        self.assertEqual(arguments.until, end_time)
         self._check_duration(arguments, duration)
-
 
     def test_all_options(self):
         """Test that all three options are a fatal error."""
-        start_time = int(time.time()) - 5 * 3600
+        start_time = int(self.test_time) - 5 * 3600
         start_time_string = time_utils.epoch_time_to_date_string(start_time)
         duration = 4
         duration_string = '%d' % duration
