0b9558c4dca8d835fe8838d1a9f24aaa214b52f9
Kalin Stoyanov <kalin@chromium.org>
2017-01-30 11:54:39 -0800

    Make servo sel1 mux sees servo
    This will disable at least one USB device on dut allowing
    DUT to see more peripherals connected to sel3 mux hub.
    
    BUG=chromium:679944
    TEST=Tested against problematic baords in lab
    
    Change-Id: I595e916a3e867507c01e9740a394a6a36e994f23
    Reviewed-on: https://chromium-review.googlesource.com/434739
    Reviewed-by: Ruchi Jahagirdar <rjahagir@chromium.org>
    Tested-by: Kalin Stoyanov <kalin@chromium.org>

--- server/site_tests/platform_ExternalUsbPeripherals/platform_ExternalUsbPeripherals.py
+++ server/site_tests/platform_ExternalUsbPeripherals/platform_ExternalUsbPeripherals.py
@@ -283,6 +283,7 @@ class platform_ExternalUsbPeripherals(test.test):
         self.action_step = None
 
         self.host.servo.switch_usbkey('dut')
+        self.host.servo.set('usb_mux_sel1', 'servo_sees_usbkey')
         self.host.servo.set('usb_mux_sel3', 'dut_sees_usbkey')
         time.sleep(_WAIT_DELAY)
 
