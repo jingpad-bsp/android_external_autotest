c6fd83859297c962536dac9e4330df0023ab2b2a
Keith Haddow <haddowk@chromium.org>
2017-03-08 18:14:07 -0800

    [moblab] Add moblab id and mac address to the UI
    These details are required for the linking of account to devices.
    
    TEST=None
    BUG=chromium:679769
    
    Change-Id: Id23294814be6c135c2dbbe3e3e480457c47f5894
    Reviewed-on: https://chromium-review.googlesource.com/452002
    Commit-Ready: Keith Haddow <haddowk@chromium.org>
    Tested-by: Keith Haddow <haddowk@chromium.org>
    Reviewed-by: Stephen Lin <stephenlin@google.com>
    Reviewed-by: Keith Haddow <haddowk@chromium.org>
    Reviewed-by: Michael Tang <ntang@chromium.org>

--- frontend/client/src/autotest/moblab/rpc/VersionInfo.java
+++ frontend/client/src/autotest/moblab/rpc/VersionInfo.java
@@ -11,11 +11,15 @@ public class VersionInfo extends JsonRpcEntity {
   private static final String NO_VERSION_FOUND = "NO VERSION FOUND";
   private static final String NO_TRACK_FOUND = "NO TRACK FOUND";
   private static final String NO_DESCRIPTION_FOUND = "NO DESCRIPTION FOUND";
+  private static final String NO_ID_FOUND = "NO ID FOUND";
+  private static final String NO_MAC_ADDRESS_FOUND = "NO MAC ADDRESS FOUND";
 
   private String milestoneInfo;
   private String versionInfo;
   private String releaseTrack;
   private String releaseDescription;
+  private String moblabIdentification;
+  private String moblabMacAddress;
 
   public VersionInfo() { reset(); }
 
@@ -24,12 +28,16 @@ public class VersionInfo extends JsonRpcEntity {
   }
   public String getReleaseTrack() { return releaseTrack; }
   public String getReleaseDescription() { return releaseDescription; }
+  public String getMoblabIdentification() { return moblabIdentification; }
+  public String getMoblabMacAddress() { return moblabMacAddress; }
 
   private void reset() {
     milestoneInfo = new String(NO_MILESTONE_FOUND);
     versionInfo = new String(NO_VERSION_FOUND);
     releaseTrack = new String(NO_TRACK_FOUND);
     releaseDescription = new String(NO_DESCRIPTION_FOUND);
+    moblabIdentification = new String(NO_ID_FOUND);
+    moblabMacAddress = new String(NO_MAC_ADDRESS_FOUND);
   }
 
   @Override
@@ -42,6 +50,10 @@ public class VersionInfo extends JsonRpcEntity {
     releaseTrack = getStringFieldOrDefault(object, "CHROMEOS_RELEASE_TRACK",
         NO_TRACK_FOUND).trim();
     releaseDescription = getStringFieldOrDefault(object, "CHROMEOS_RELEASE_DESCRIPTION",
+        NO_DESCRIPTION_FOUND).trim();
+    moblabIdentification = getStringFieldOrDefault(object, "MOBLAB_ID",
+        NO_DESCRIPTION_FOUND).trim();
+    moblabMacAddress = getStringFieldOrDefault(object, "MOBLAB_MAC_ADDRESS",
         NO_DESCRIPTION_FOUND).trim();
   }
 
