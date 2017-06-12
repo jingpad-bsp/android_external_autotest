157a77a2bac507e3c4c2ae5465f13fc1254a3c06
Prathmesh Prabhu <pprabhu@chromium.org>
2017-06-12 14:35:04 -0700

    [autotest] Fix lsbrelease_utils.is_moblab to respect its argument.
    is_moblab allows one to pass in the content to check, but then promptly
    goes on to first verify that the call is running on a moblab device
    (which basically ends up ignoring the argument).  FixIt.
    
    BUG=b:28453441
    TEST=(new) unittests
    
    Change-Id: Ie16eef3de582720a93ac15b29bc991b6b984ec62
    Reviewed-on: https://chromium-review.googlesource.com/531761
    Commit-Ready: Prathmesh Prabhu <pprabhu@chromium.org>
    Tested-by: Prathmesh Prabhu <pprabhu@chromium.org>
    Reviewed-by: Prathmesh Prabhu <pprabhu@chromium.org>

--- client/common_lib/lsbrelease_utils_unittest.py
+++ client/common_lib/lsbrelease_utils_unittest.py
@@ -107,6 +107,25 @@ class LsbreleaseUtilsTestCase(unittest.TestCase):
         self.assertTrue(lsbrelease_utils.is_jetstream(
             _WHIRLWIND_LSB_RELEASE_REDACTED))
 
+    @unittest.expectedFailure
+    def test_is_moblab_with_empty_lsbrelease(self):
+        """is_moblab correctly validates trivial lsb-release information.
+
+        TODO(pprabhu) This test currently fails because _lsbrelease_search does
+        not respect its arguments. Fix it in a separate CL.
+        """
+        self.assertFalse(lsbrelease_utils.is_moblab(''))
+
+    def test_is_moblab_with_link_lsbrelease(self):
+        """is_moblab correctly validates the contents from some other board."""
+        self.assertFalse(lsbrelease_utils.is_moblab(
+                _LINK_LSB_RELEASE_REDACTED))
+
+    def test_is_moblab_with_moblab_lsbrelease(self):
+        """is_moblab correctly validates the contents from a moblab device."""
+        self.assertTrue(lsbrelease_utils.is_moblab(
+                _GUADO_MOBLAB_LSB_RELEASE_REDACTED))
+
 
 if __name__ == '__main__':
     unittest.main()
