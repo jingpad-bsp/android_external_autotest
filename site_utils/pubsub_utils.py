0f553bd773170c8ddb80d6061cdc0f133ba239b6
Michael Tang <ntang@google.com>
2017-06-16 17:38:45 -0700

    Wrap the pubsub into a cloud console client.
    BUG=chromium:724523
    TEST=unit tests.
    
    Change-Id: I4338cce8c2c983b4cd50b71014d4f1ca5667cd78
    Reviewed-on: https://chromium-review.googlesource.com/540137
    Commit-Ready: Michael Tang <ntang@chromium.org>
    Tested-by: Michael Tang <ntang@chromium.org>
    Reviewed-by: Keith Haddow <haddowk@chromium.org>
    Reviewed-by: Michael Tang <ntang@chromium.org>

--- site_utils/pubsub_utils.py
+++ site_utils/pubsub_utils.py
@@ -8,24 +8,22 @@ Uses gsutil to archive files to the configured Google Storage bucket.
 Upon successful copy, the local results directory is deleted.
 """
 
-import logging
+from __future__ import print_function
+
 import os
 
 from apiclient import discovery
+from apiclient import errors
 from oauth2client.client import ApplicationDefaultCredentialsError
 from oauth2client.client import GoogleCredentials
-from autotest_lib.server.hosts import moblab_host
-
-import common
+from chromite.lib import cros_logging as logging
 
 # Cloud service
-# TODO(ntang): move this to config.
-CLOUD_SERVICE_ACCOUNT_FILE = moblab_host.MOBLAB_SERVICE_ACCOUNT_LOCATION
 PUBSUB_SERVICE_NAME = 'pubsub'
 PUBSUB_VERSION = 'v1beta2'
 PUBSUB_SCOPES = ['https://www.googleapis.com/auth/pubsub']
 # number of retry to publish an event.
-_PUBSUB_NUM_RETRIES = 3
+DEFAULT_PUBSUB_NUM_RETRIES = 3
 
 class PubSubException(Exception):
     """Exception to be raised when the test to push to prod failed."""
@@ -33,14 +31,18 @@ class PubSubException(Exception):
 
 
 class PubSubClient(object):
-    """A generic pubsub client. """
-    def __init__(self, credential_file=CLOUD_SERVICE_ACCOUNT_FILE):
+    """A generic pubsub client."""
+    def __init__(self, credential_file=None):
         """Constructor for PubSubClient.
 
-        @param credential_file: The credential filename.
-        @raises PubSubException if the credential file does not exist or
-            corrupted.
+        Args:
+          credential_file: The credential filename.
+
+        Raises:
+          PubSubException if the credential file does not exist or corrupted.
         """
+        if not credential_file:
+            raise PubSubException('You need to specify a credential file.')
         self.credential_file = credential_file
         self.credential = self._get_credential()
 
@@ -48,30 +50,30 @@ class PubSubClient(object):
         """Gets the pubsub service api handle."""
         if not os.path.isfile(self.credential_file):
             logging.error('No credential file found')
-            raise PubSubException("Credential file does not exists:"
-                    + self.credential_file)
+            raise PubSubException('Credential file does not exist:' +
+                                  self.credential_file)
         try:
             credential = GoogleCredentials.from_stream(self.credential_file)
             if credential.create_scoped_required():
                 credential = credential.create_scoped(PUBSUB_SCOPES)
             return credential
         except ApplicationDefaultCredentialsError as ex:
-            logging.error('Failed to get credential.')
-        except:
-            logging.error('Failed to get the pubsub service handle.')
+            logging.exception('Failed to get credential:%s', ex)
+        except errors.Error as e:
+            logging.exception('Failed to get the pubsub service handle:%s', e)
 
-        raise PubSubException("Credential file does not exists:"
-                + self.credential_file)
+        raise PubSubException('Credential file %s does not exists:' %
+                              self.credential_file)
 
     def _get_pubsub_service(self):
         try:
             return discovery.build(PUBSUB_SERVICE_NAME, PUBSUB_VERSION,
                                    credentials=self.credential)
-        except:
-            logging.error('Failed to get pubsub resource object.')
-            raise PubSubException("Failed to get pubsub resource object")
+        except errors.Error as e:
+            logging.exception('Failed to get pubsub resource object:%s', e)
+            raise PubSubException('Failed to get pubsub resource object')
 
-    def publish_notifications(self, topic, messages=[]):
+    def publish_notifications(self, topic, messages=None):
         """Publishes a test result notification to a given pubsub topic.
 
         @param topic: The Cloud pubsub topic.
@@ -81,16 +83,24 @@ class PubSubClient(object):
 
         @raises PubSubException if failed to publish the notification.
         """
+        if not messages:
+            return None
+
         pubsub = self._get_pubsub_service()
         try:
             body = {'messages': messages}
-            resp = pubsub.projects().topics().publish(topic=topic,
-                    body=body).execute(num_retries=_PUBSUB_NUM_RETRIES)
+            resp = pubsub.projects().topics().publish(
+                topic=topic, body=body).execute(
+                    num_retries=DEFAULT_PUBSUB_NUM_RETRIES)
+            msgIds = []
             if resp:
                 msgIds = resp.get('messageIds')
                 if msgIds:
                     logging.debug('Published notification message')
-                    return msgIds
-        except:
-            logging.error('Failed to publish test result notifiation.')
-            raise PubSubException("Failed to publish the notifiation.")
+                else:
+                    logging.error('Failed to published notification message')
+            return msgIds
+        except errors.Error as e:
+            logging.exception('Failed to publish test result notification:%s',
+                    e)
+            raise PubSubException('Failed to publish the notification')
