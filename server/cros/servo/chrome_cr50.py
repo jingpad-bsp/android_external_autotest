3c5dcf9e3ae57c8fc6dd56851810d539db1247de
Mary Ruthven <mruthven@chromium.org>
2017-06-26 15:55:15 -0700

    chrome_cr50: add check to see if cr50 rolled back
    Cr50 RW_BOOT_MAX_RETRY_COUNT is 5. After the cr50 console command
    reboot, cr50 subtracts one from the retry count. This change modifies
    the rollback command to set the retry counter to RW_BOOT_MAX_RETRY_COUNT
    before rebooting
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I3c2815fab94ed0ccaf9c78c790d5b5eab747b65d
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/558646
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>

--- server/cros/servo/chrome_cr50.py
+++ server/cros/servo/chrome_cr50.py
@@ -39,6 +39,7 @@ class ChromeCr50(chrome_ec.ChromeConsole):
     UNLOCK = ['Unlock sequence starting. Continue until (\S+)']
     FWMP_LOCKED_PROD = ["Managed device console can't be unlocked"]
     FWMP_LOCKED_DBG = ['Ignoring FWMP unlock setting']
+    MAX_RETRY_COUNT = 5
 
 
     def __init__(self, servo):
@@ -109,18 +110,29 @@ class ChromeCr50(chrome_ec.ChromeConsole):
         self.ccd_enable()
 
 
-    def rollback(self):
+    def rollback(self, eraseflashinfo=True):
         """Set the reset counter high enough to force a rollback then reboot"""
         if not self.has_command('rw') or not self.has_command('eraseflashinfo'):
             raise error.TestError("need image with 'rw' and 'eraseflashinfo'")
 
+        inactive_partition = self.get_inactive_version_info()[0]
         # Increase the reset count to above the rollback threshold
         self.send_command('rw 0x40000128 1')
-        self.send_command('rw 0x4000012c 15')
+        self.send_command('rw 0x4000012c %d' % (self.MAX_RETRY_COUNT + 2))
 
-        self.send_command('eraseflashinfo')
+        if eraseflashinfo:
+            self.send_command('eraseflashinfo')
 
         self.reboot()
+
+        running_partition = self.get_active_version_info()[0]
+        if inactive_partition != running_partition:
+            raise error.TestError("Failed to rollback to inactive image")
+
+
+    def rolledback(self):
+        """Returns true if cr50 just rolled back"""
+        return int(self._servo.get('cr50_reset_count')) > self.MAX_RETRY_COUNT
 
 
     def get_version_info(self, regexp):
