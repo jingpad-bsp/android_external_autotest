652ff6a2b182a89194520b59e65bc38714d86008
Kristoffer Erlandsson <kerl@google.com>
2017-08-31 19:43:32 +0200

    Add WebRTC stress test for video source switching
    This test starts 5 high resolution loopback peer connections. Four of
    the video elements are styled to be small, thumbnail sized and one is in
    its native size. This is intended to simulate a video conference with
    one main feed and several smaller feeds.
    
    The test then frequently swaps the sourceObject of the main feed with a
    randomly chosen smaller feed.
    
    Extracted much of the Python test support code to a common location to
    be shared with the other WebRTC peer connection tests.
    
    The intention is to later add some more verifications than only checking
    if Chrome crashes.
    
    BUG=chromium:760983
    TEST=Tested against local DUT
    Change-Id: Ia9661d0f65ad0c7d5c22b7e29b37995cce01223f
    Reviewed-on: https://chromium-review.googlesource.com/647455
    Commit-Ready: Kristoffer Erlandsson <kerl@google.com>
    Tested-by: Kristoffer Erlandsson <kerl@google.com>
    Reviewed-by: Kristoffer Erlandsson <kerl@google.com>
    Reviewed-by: Denis Tosic <dtosic@chromium.org>

---
+++ client/site_tests/video_WebRtcMainFeedSwitching/video_WebRtcMainFeedSwitching.py
@@ -0,0 +1,29 @@
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.client.bin import test
+from autotest_lib.client.common_lib.cros import test_webrtc_peer_connection
+
+
+class video_WebRtcMainFeedSwitching(test.test):
+    """
+    Tests a simulated Video Call with one main and several small feeds.
+
+    Simulates speaker switching by swapping the source video object between the
+    main feed and one randomly chosen small feed.
+    """
+    version = 1
+
+    def run_once(self):
+        """Runs the test."""
+        test = test_webrtc_peer_connection.WebRtcPeerConnectionTest(
+            title = 'Main Feed Switching',
+            own_script = 'main-feed-switching.js',
+            common_script = 'loopback-peerconnection.js',
+            bindir = self.bindir,
+            tmpdir = self.tmpdir,
+            num_peer_connections = 5,
+            iteration_delay_millis = 50)
+        test.run_test()
+
