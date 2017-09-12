jingpad-bsp/android_external_autotest

commit 522c75bc15c21f88733ce3071dc275ff1c6b8821

power_LoadTest: log package C-states under Haswell/Broadwell
We didn't have CPU model groups for most of the Haswell
microarchitecture family, and the one group we did have was marked for
Sandy Bridge, which meant that we wouldn't be able to log package
C-states PC8/9/10 on that CPU. For the other Haswell ID and all
Broadwell IDs we weren't getting any package C-states at all.

BUG=None
TEST=run power_LoadTest and check keyvals

Change-Id: Id6bf85b39444e11fa1725627ed4d5ac79817e63a
Reviewed-on: https://chromium-review.googlesource.com/255937
Tested-by: Eric Caruso <ejcaruso@chromium.org>
Reviewed-by: Todd Broch <tbroch@chromium.org>
Commit-Queue: Eric Caruso <ejcaruso@chromium.org>
Trybot-Ready: Eric Caruso <ejcaruso@chromium.org>

client/cros/power_status.py
@@ -764,6 +764,8 @@ class CPUPackageStats(AbstractStats):
     ATOM         =              {'C2': 0x3F8, 'C4': 0x3F9, 'C6': 0x3FA}
     NEHALEM      =              {'C3': 0x3F8, 'C6': 0x3F9, 'C7': 0x3FA}
     SANDY_BRIDGE = {'C2': 0x60D, 'C3': 0x3F8, 'C6': 0x3F9, 'C7': 0x3FA}
+    HASWELL      = {'C2': 0x60D, 'C3': 0x3F8, 'C6': 0x3F9, 'C7': 0x3FA,
+                                 'C8': 0x630, 'C9': 0x631,'C10': 0x632}
 
     def __init__(self):
         def _get_platform_states():
@@ -794,8 +796,12 @@ class CPUPackageStats(AbstractStats):
                 '002A': self.SANDY_BRIDGE, # SandyBridge (i3/i5/i7/C/X)
                 '002D': self.SANDY_BRIDGE, # SandyBridge-E (i7)
                 '003A': self.SANDY_BRIDGE, # IvyBridge (i3/i5/i7/X)
-                '003C': self.SANDY_BRIDGE, # unclear (Haswell?)
+                '003C': self.HASWELL,      # Haswell (Core/Xeon)
+                '003D': self.HASWELL,      # Broadwell (Core)
                 '003E': self.SANDY_BRIDGE, # IvyBridge (Xeon)
+                '003F': self.HASWELL,      # Haswell-E (Core/Xeon)
+                '004F': self.HASWELL,      # Broadwell (Xeon)
+                '0056': self.HASWELL,      # Broadwell (Xeon D)
                 }.get(values[6], None)
 
         self._platform_states = _get_platform_states()

commit d167441ebfeb14ddee82246f94cb7d7644ebcfdc

power_status: do not read PC8/9/10 residency couters on CPUs in HSW uarch
The package C-state residency counters of PC8/9/10 were first
introduced in CPU model 0x45 of i5-4250U, aka. Haswell ULT.
Reading these MSRs on other CPUs in HSW uarch will break the test
because they do not exist.

To tackle this, regroup the package C-state residency counters of
PC2/3/6/7/8/9/10 to a new name, leave all CPUs in HSW uarch remain
polling PC2/3/6/7. Such that, it maintains better compatibility.

Fixes: 522c75bc15c2 ("power_LoadTest: log package C-states under Haswell/Broadwell")

BUG=b:65172714
TEST=none, as it only impacts the power tests on old CPUs in HSW uarch

Change-Id: Ibe6b8921e267fabcc396312b482a7a819c71ca1e
Signed-off-by: Harry Pan <harry.pan@intel.com>
Reviewed-on: https://chromium-review.googlesource.com/649426
Reviewed-by: Puthikorn Voravootivat <puthik@chromium.org>

client/cros/power_status.py
@@ -774,7 +774,7 @@ class CPUPackageStats(AbstractStats):
     SANDY_BRIDGE = {'C2': 0x60D, 'C3': 0x3F8, 'C6': 0x3F9, 'C7': 0x3FA}
     SILVERMONT   = {'C6': 0x3FA}
     GOLDMONT     = {'C2': 0x60D, 'C3': 0x3F8, 'C6': 0x3F9,'C10': 0x632}
-    HASWELL      = {'C2': 0x60D, 'C3': 0x3F8, 'C6': 0x3F9, 'C7': 0x3FA,
+    BROADWELL    = {'C2': 0x60D, 'C3': 0x3F8, 'C6': 0x3F9, 'C7': 0x3FA,
                                  'C8': 0x630, 'C9': 0x631,'C10': 0x632}
 
     def __init__(self):
@@ -789,18 +789,22 @@ class CPUPackageStats(AbstractStats):
             return {
                 # model groups pulled from Intel SDM, volume 4
                 # Group same package cstate using the older uarch name
+                #
+                # TODO(harry.pan): As the keys represent microarchitecture
+                # names, we could consider to rename the PC state groups
+                # to avoid ambiguity.
                 'Airmont':      self.SILVERMONT,
                 'Atom':         self.ATOM,
-                'Broadwell':    self.HASWELL,
+                'Broadwell':    self.BROADWELL,
                 'Goldmont':     self.GOLDMONT,
-                'Haswell':      self.HASWELL,
+                'Haswell':      self.SANDY_BRIDGE,
                 'Ivy Bridge':   self.SANDY_BRIDGE,
                 'Ivy Bridge-E': self.SANDY_BRIDGE,
-                'Kaby Lake':    self.HASWELL,
+                'Kaby Lake':    self.BROADWELL,
                 'Nehalem':      self.NEHALEM,
                 'Sandy Bridge': self.SANDY_BRIDGE,
                 'Silvermont':   self.SILVERMONT,
-                'Skylake':      self.HASWELL,
+                'Skylake':      self.BROADWELL,
                 'Westmere':     self.NEHALEM,
                 }.get(cpu_uarch, None)
 