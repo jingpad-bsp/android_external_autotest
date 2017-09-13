464220fd402ff265ef2258bf0dea28368df00e61
Allen Li <ayatane@chromium.org>
2017-09-12 17:14:22 -0700

    [autotest] Update HQE state in job_reporter
    BUG=chromium:748234
    TEST=Unittests
    
    Change-Id: I4ea8d4985f4deeb0e7d5c7a78e145efbb376e2ea
    Reviewed-on: https://chromium-review.googlesource.com/666140
    Commit-Ready: Allen Li <ayatane@chromium.org>
    Tested-by: Allen Li <ayatane@chromium.org>
    Reviewed-by: Prathmesh Prabhu <pprabhu@chromium.org>

---
+++ venv/lucifer/autotest_unittest.py
@@ -0,0 +1,56 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Tests for autotest.py."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+
+import mock
+import pytest
+import subprocess32
+
+from lucifer import autotest
+
+
+def test_monkeypatch():
+    """Test monkeypatch()."""
+    common_file = subprocess32.check_output(
+            [sys.executable, '-m', 'lucifer.scripts.autotest_monkeypatcher'])
+    assert common_file.rstrip() == '<removed>'
+
+
+@pytest.mark.parametrize('fullname,expected', [
+        ('autotest_lib.common', True),
+        ('autotest_lib.server.common', True),
+        ('autotest_lib.server', False),
+        ('some_lib.common', False),
+])
+def test__CommonRemovingFinder__is_autotest_common(fullname, expected):
+    """Test _CommonRemovingFinder._is_common()."""
+    finder = autotest._CommonRemovingFinder()
+    assert finder._is_autotest_common(fullname) == expected
+
+
+@pytest.mark.parametrize('name,expected', [
+        ('scheduler.models', '.scheduler.models'),
+        ('...scheduler.models', '.scheduler.models'),
+])
+def test_load(name, expected):
+    """Test load()."""
+    with mock.patch('importlib.import_module', autospec=True) \
+         as import_module, \
+         mock.patch.object(autotest, '_setup_done', True):
+        autotest.load(name)
+        import_module.assert_called_once_with(expected, package='autotest_lib')
+
+
+def test_load_without_patch_fails():
+    """Test load() without patch."""
+    with mock.patch.object(autotest, '_setup_done', False):
+        with pytest.raises(ImportError):
+            autotest.load('asdf')
--- venv/lucifer/scripts/job_reporter.py
+++ venv/lucifer/scripts/job_reporter.py
@@ -2,6 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+"""Run a job against Autotest.
+
+See http://goto.google.com/monitor_db_per_job_refactor
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -10,6 +15,7 @@ import argparse
 import logging
 import sys
 
+from lucifer import autotest
 from lucifer import eventlib
 from lucifer import loglib
 
@@ -25,27 +31,106 @@ def main(args):
 
     @param args: list of command line args
     """
-    parser = argparse.ArgumentParser(prog='job_reporter')
+
+    parser = argparse.ArgumentParser(prog='job_reporter', description=__doc__)
     loglib.add_logging_options(parser)
+    parser.add_argument('--job-id', type=int, default=None)
+    parser.add_argument('--autoserv-exit', type=int, default=None,
+                        help='autoserv exit status')
     args = parser.parse_args(args)
     loglib.configure_logging_with_args(parser, args)
-    return _run_shepherd(_handle_event)
+
+    autotest.monkeypatch()
+    autotest.load('frontend.setup_django_environment')
+    scheduler_models = autotest.load('scheduler.scheduler_models')
+
+    if args.job_id is not None:
+        if args.autoserv_exit is None:
+            # TODO(crbug.com/748234): autoserv not implemented yet.
+            raise NotImplementedError('not implemented yet (crbug.com/748234)')
+        job = scheduler_models.Job.objects.get(id=args.job_id)
+        hqes = list(scheduler_models.HostQueueEntry.objects
+                    .filter(job_id=args.job_id))
+    else:
+        # TODO(crbug.com/748234): Full jobs not implemented yet.
+        raise NotImplementedError('not implemented yet')
+    handler = _EventHandler(job, hqes, autoserv_exit=args.autoserv_exit)
+    return _run_shepherd(handler, job_id=args.job_id)
 
 
-def _run_shepherd(event_handler):
+def _run_shepherd(event_handler, job_id):
     """Run job_shepherd.
 
     Events issues by the job_shepherd will be handled by event_handler.
 
-    @param event_handler: callable that takes an Event.
+    @param event_handler: callable that takes an Event
+    @param job_id: job ID
     """
-    return eventlib.run_event_command(
-            event_handler=event_handler,
-            args=[_JOB_SHEPHERD_PROGRAM])
+    args = [_JOB_SHEPHERD_PROGRAM]
+    args.append('%d' % job_id)
+    return eventlib.run_event_command(event_handler=event_handler, args=args)
 
 
-def _handle_event(event):
-    logger.debug('Received event %r', event.name)
+class _EventHandler(object):
+    """Event handling dispatcher.
+
+    Event handlers are implemented as methods named _handle_<event value>.
+
+    Each handler method must handle its exceptions accordingly.  If an
+    exception escapes, the job dies on the spot.
+    """
+
+    def __init__(self, job, hqes, autoserv_exit):
+        """Initialize instance.
+
+        @param job: Job instance to own
+        @param hqes: list of HostQueueEntry instances for the job
+        @param autoserv_exit: autoserv exit status
+        """
+        self._job = job
+        self._hqes = hqes
+        # TODO(crbug.com/748234): autoserv not implemented yet.
+        self._autoserv_exit = autoserv_exit
+
+    def __call__(self, event):
+        logger.debug('Received event %r', event.name)
+        method_name = '_handle_%s' % event.value
+        try:
+            handler = getattr(self, method_name)
+        except AttributeError:
+            raise NotImplementedError('%s is not implemented for handling %s',
+                                      method_name, event.name)
+        handler(event)
+
+    def _handle_starting(self):
+        # TODO(crbug.com/748234): No event update needed yet.
+        pass
+
+    def _handle_parsing(self):
+        # TODO(crbug.com/748234): monitor_db leaves the HQEs in parsing
+        pass
+
+    def _handle_completed(self):
+        final_status = self._final_status()
+        for hqe in self._hqes:
+            hqe.set_status(final_status)
+
+    def _final_status(self):
+        afe_models = autotest.load('frontend.afe.models')
+        Status = afe_models.HostQueueEntry.Status
+        if self._job_was_aborted():
+            return Status.ABORTED
+
+        if self._autoserv_exit == 0:
+            return Status.COMPLETED
+        return Status.FAILED
+
+    def _job_was_aborted(self):
+        for hqe in self._hqes:
+            hqe.update_from_database()
+            if hqe.aborted:
+                return True
+        return False
 
 
 if __name__ == '__main__':
