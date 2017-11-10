cd36ae0a377f228c69f58d7c5c854ee1a7abf94b
Xixuan Wu <xixuan@chromium.org>
2017-11-10 13:51:00 -0800

    autotest: Enable passing in parameters from create job page.
    BUG=chromium:782501
    TEST=Ran client job & suite job on local autotest. Verify that fast is
    injected into control file.
    Ran unittest.
    
    Change-Id: I061694392fb3b789a5135ad2af241f2b0886123a
    Reviewed-on: https://chromium-review.googlesource.com/764431
    Commit-Ready: Xixuan Wu <xixuan@chromium.org>
    Tested-by: Xixuan Wu <xixuan@chromium.org>
    Reviewed-by: Dan Shi <dshi@google.com>

--- client/common_lib/control_data.py
+++ client/common_lib/control_data.py
@@ -104,6 +104,7 @@ class ControlData(object):
         self.attributes = set()
         self.max_result_size_KB = DEFAULT_MAX_RESULT_SIZE_KB
         self.priority = priorities.Priority.DEFAULT
+        self.fast = False
 
         _validate_control_file_fields(self.path, vars, raise_warnings)
 
@@ -293,6 +294,9 @@ class ControlData(object):
     def set_priority(self, val):
         self._set_int('priority', val)
 
+    def set_fast(self, val):
+        self._set_bool('fast', val)
+
     def set_attributes(self, val):
         # Add subsystem:default if subsystem is not specified.
         self._set_set('attributes', val)
@@ -408,6 +412,7 @@ def finish_parse(mod, path, raise_warnings):
     assert(mod.node.nodes.__class__ == list)
 
     variables = {}
+    injection_variables = {}
     for n in mod.node.nodes:
         if (n.__class__ == compiler.ast.Function and
             re.match('step\d+', n.name)):
@@ -420,6 +425,7 @@ def finish_parse(mod, path, raise_warnings):
                 variables.clear()
                 variables.update(vars_in_step)
         else:
-            _try_extract_assignment(n, variables)
+            _try_extract_assignment(n, injection_variables)
 
+    variables.update(injection_variables)
     return ControlData(variables, path, raise_warnings)
