0624ad5a0be6d043015f5f0958d1661d8b175bea
Kristoffer Erlandsson <kerl@google.com>
2017-11-16 15:08:56 +0100

    Add an action for asserting file contents.
    Adding verification steps as regular actions for flexibility. The user
    can then choose to run verifications in the middle of a scenario, or
    verify everything at the end.
    
    BUG=chromium:785271
    TEST=cfm_ConfigurableCfmTestSanity on local DUT and unit tests.
    
    pre-cq-configs: bluestreak-pre-cq
    Change-Id: Ibaee23b7144893b725cc5e7e23549e54c62e08c9
    Reviewed-on: https://chromium-review.googlesource.com/774798
    Commit-Ready: Kristoffer Erlandsson <kerl@google.com>
    Tested-by: Kristoffer Erlandsson <kerl@google.com>
    Reviewed-by: Kristoffer Erlandsson <kerl@google.com>
    Reviewed-by: Claes Malmnäs <malmnas@google.com>

--- server/cros/cfm/configurable_test/action_context.py
+++ server/cros/cfm/configurable_test/action_context.py
@@ -2,11 +2,16 @@ class ActionContext(object):
     """
     Provides the dependencies actions might need to execute.
     """
-    def __init__(self, cfm_facade=None):
+    def __init__(self, cfm_facade=None, file_contents_collector=None):
         """
         Initializes.
 
         @param cfm_facade CFM facade to use, an instance of
                 CFMFacadeRemoteAdapter.
+        @param file_contents_collector object with a
+                collect_file_contents(file_name) method to get file contents
+                from the specified file on the DUT.
         """
         self.cfm_facade = cfm_facade
+        self.file_contents_collector = file_contents_collector
+
