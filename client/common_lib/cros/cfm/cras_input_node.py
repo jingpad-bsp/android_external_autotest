8926c4c1113859e66e4729c36ad93b7ace25318b
Claes Malmnas <malmnas@google.com>
2017-11-24 15:02:03 +0100

    [CfM autotest] update the DualSpeaker test to use CrasNodeCollector.
    BUG=chromium:788129
    TEST=local setup
    pre-cq-configs: bluestreak-pre-cq
    
    Change-Id: I106101e0bb851f74b130afe4914660348a40d0b8
    Reviewed-on: https://chromium-review.googlesource.com/789036
    Commit-Queue: Claes Malmnäs <malmnas@google.com>
    Tested-by: Claes Malmnäs <malmnas@google.com>
    Trybot-Ready: Claes Malmnäs <malmnas@google.com>
    Reviewed-by: Denis Tosic <dtosic@chromium.org>
    Reviewed-by: Claes Malmnäs <malmnas@google.com>

--- client/common_lib/cros/cfm/cras_input_node.py
+++ client/common_lib/cros/cfm/cras_input_node.py
@@ -4,8 +4,16 @@ class CrasInputNode(object):
     An input node is a node that can pick up audio, e.g. a microphone jack.
     """
 
-    def __init__(self, node_id, name, gain, node_type):
+    def __init__(self, node_id, node_name, gain, node_type, device_id,
+                 device_name):
         self.node_id = node_id
-        self.name = name
-        self.gain = gain
+        self.node_name = node_name
+        self.gain = int(gain)
         self.node_type = node_type
+        self.device_id = device_id
+        self.device_name = device_name
+
+    def __str__(self):
+        return ('Node id: %s, Node name: %s, Device id: %s, Device name: %s '
+                'Gain: %d' % (self.node_id, self.node_name, self.device_id,
+                              self.device_name, self.gain))
