9b18c715f9b5b617495ca409cc8c073698307ef7
Ben Kwa <kenobi@google.com>
2017-12-13 15:38:56 -0800

    [autotest] Enable container pool metrics.
    Step 0: Set up a metrics context, and add a basic tick counter for
    checking service aliveness.
    
    BUG=chromium:772142
    TEST=service_unittest.py
    TEST=Run lxc_pool_service.py locally; verify that the service starts,
         runs, and shuts down normally.
    
    Change-Id: I528b89385efc608b2ff33a306bd20d49e3360c15
    Reviewed-on: https://chromium-review.googlesource.com/825954
    Commit-Ready: Jacob Kopczynski <jkop@chromium.org>
    Tested-by: Ben Kwa <kenobi@chromium.org>
    Reviewed-by: Paul Hobbs <phobbs@google.com>
    Reviewed-by: Jacob Kopczynski <jkop@chromium.org>

--- site_utils/lxc/container_pool/service.py
+++ site_utils/lxc/container_pool/service.py
@@ -8,6 +8,7 @@ import threading
 import time
 
 import common
+from autotest_lib.client.common_lib import utils
 from autotest_lib.site_utils.lxc import base_image
 from autotest_lib.site_utils.lxc import constants
 from autotest_lib.site_utils.lxc import container_factory
@@ -21,6 +22,11 @@ try:
     import cPickle as pickle
 except:
     import pickle
+
+try:
+    from chromite.lib import metrics
+except ImportError:
+    metrics = utils.metrics_mock
 
 
 # The minimum period between polling for new connections, in seconds.
@@ -84,6 +90,7 @@ class Service(object):
             self._handle_incoming_connections()
             self._cleanup_closed_connections()
             # TODO(kenobi): Poll for and log errors from pool.
+            metrics.Counter('chromeos/autotest/container_pool/tick').increment()
             time.sleep(_MIN_POLLING_PERIOD)
 
         logging.debug('Exit event loop.')
