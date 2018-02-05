dc1c523783334339e97c5137e79f8e763a3adcf9
James Cook <jamescook@chromium.org>
2018-02-05 11:29:07 -0800

    autotest: Enable desktopui_MusLogin/MashLogin on KBL boards
    The underlying addFramebuffer2 / DRM page flip issue has been
    fixed. Eve is now green in the lab so these boards should be
    working as well. Add them back to perbuild.
    
    BUG=chromium:784621
    TEST=autotests
    
    Change-Id: Ib5e6ed8cc2616bcfab4d475960ab1cdb320f106e
    Reviewed-on: https://chromium-review.googlesource.com/902406
    Tested-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>

--- client/site_tests/desktopui_MashLogin/desktopui_MashLogin.py
+++ client/site_tests/desktopui_MashLogin/desktopui_MashLogin.py
@@ -18,9 +18,6 @@ class desktopui_MashLogin(test.test):
 
         # Flaky on nyan_* boards. http://crbug.com/717275
         boards_to_skip = ['nyan_big', 'nyan_kitty', 'nyan_blaze']
-        # Crashes on some KBL boards in ui::DrmThread::SchedulePageFlip.
-        # http://crbug.com/784621
-        boards_to_skip += ['coral', 'poppy', 'pyro', 'snappy', 'soraka']
         if utils.get_current_board() in boards_to_skip:
           logging.warning('Skipping test run on this board.')
           return
