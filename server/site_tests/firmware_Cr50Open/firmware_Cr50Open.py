473bda0dba811dd4099e9dee13bcb92ab3da7ba3
Mary Ruthven <mruthven@chromium.org>
2018-02-26 11:28:07 -0800

    cr50_stress_experimental: add arg for expected ccd lockout
    We are about to release mp images that have ccd_v1 unconditionally
    locked out. Add an arg to pass to all the cr50 tests, so they can
    handle locked out ccd appropriately.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: Ie123623a59e0df3c2a0a2a75c7229bcf254ffb08
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/938963
    Reviewed-by: Wai-Hong Tam <waihong@google.com>

--- server/site_tests/firmware_Cr50Open/firmware_Cr50Open.py
+++ server/site_tests/firmware_Cr50Open/firmware_Cr50Open.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
@@ -29,8 +30,24 @@ class firmware_Cr50Open(FirmwareTest):
             raise error.TestNAError('Cannot test on Cr50 with old CCD version')
 
 
-    def run_once(self):
+    def run_once(self, ccd_lockout):
         """Lock CCD and then Open it."""
         self.cr50.set_ccd_level('lock')
-        self.cr50.set_ccd_level('open')
+        try:
+            self.cr50.set_ccd_level('open')
+            success = True
+        except error.TestFail, e:
+            logging.debug(e)
+            if 'Access Denied' in e.message:
+                success = False
+            else:
+                raise
+
+        ccd_status_str =  'locked out' if ccd_lockout else 'accessible'
+        # Make sure we only got 'Access Denied' when ccd is locked out and open
+        # was successful only when ccd is accessible.
+        if success == ccd_lockout:
+            raise error.TestFail('ccd open %sed with ccd %s' % ('succeed'
+                    if success else 'fail', ccd_status_str))
+        logging.info('ccd open is %s', ccd_status_str)
 
