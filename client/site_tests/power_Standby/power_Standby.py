11cc3456c9035b43a0be2207ea5914565faa00cf
Todd Broch <tbroch@chromium.org>
2018-04-11 18:32:23 -0700

    power_Standby: create short (20min) version of test.
    Revisit this test to modernize and create shorter running versions of
    the original 12hr test.  One that expects battery only operation.  The
    other that uses ectool to force discharge similar to other power
    tests.
    
    BUG=chromium:220268
    TEST=power_Standby.fast power_Standby.fast_force_discharge
    
    Running on eve,
    'fast' measurements are reasonable given fuel gauge inaccuracies & S0
    noise.
    'Fast_force_discharge' dont match those of on battery but presumably
    due to some inefficient behavior for charger.  Test can still be
    useful for tracking regression though
    
    Change-Id: I7f47e0aed675c4f74e1054c072662a2b7e2ee6a7
    Reviewed-on: https://chromium-review.googlesource.com/1010470
    Commit-Ready: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Puthikorn Voravootivat <puthik@chromium.org>

--- client/site_tests/power_Standby/power_Standby.py
+++ client/site_tests/power_Standby/power_Standby.py
@@ -7,39 +7,58 @@ import logging, math, time
 from autotest_lib.client.bin import test
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros import rtc
-from autotest_lib.client.cros.power import power_status, sys_power
+from autotest_lib.client.cros.power import power_status, power_utils, sys_power
 
 class power_Standby(test.test):
-    """Measure Standby(S3) power test."""
+    """Measure Standby power test."""
     version = 1
-    _percent_min_charge = 0.1
+    _percent_min_charge = 10
     _min_sample_hours = 0.1
 
-    def run_once(self, test_hours=None, sample_hours=None,
-                 percent_initial_charge_min=0.2, max_milliwatts_standby=None):
+    def initialize(self):
+        self._force_discharge_enabled = False
 
-        if test_hours <= sample_hours:
+    def run_once(self, test_hours=None, sample_hours=None,
+                 max_milliwatts_standby=500, ac_ok=False,
+                 force_discharge=False):
+
+        if test_hours < sample_hours:
             raise error.TestFail("Test hours must be greater than sample hours")
 
-        # If we're measuring <= 6min of S3 then the S0 time is not negligible.
-        # Note, reasonable rule of thumb is S0 idle is ~10-20 times S3 power.
+        # If we're measuring < 6min of standby then the S0 time is not
+        # negligible. Note, reasonable rule of thumb is S0 idle is ~10-20 times
+        # standby power.
         if sample_hours < self._min_sample_hours:
-            raise error.TestFail("Must suspend more than %.2f hours" % \
+            raise error.TestFail("Must standby more than %.2f hours" % \
                                  sample_hours)
 
-        # Query initial power status
         power_stats = power_status.get_status()
-        power_stats.assert_battery_state(percent_initial_charge_min)
+
+        if not ac_ok and power_stats.on_ac():
+            raise error.TestError('On AC, please unplug power supply.')
+
+        if force_discharge:
+            if not power_stats.on_ac():
+                raise error.TestError('Not on AC, please plug in power supply '
+                                      'to attempt force discharge.')
+            if not power_utils.charge_control_by_ectool(False):
+                raise error.TestError('Unable to force discharge.')
+
+            self._force_discharge_enabled = True
+
         charge_start = power_stats.battery[0].charge_now
         voltage_start = power_stats.battery[0].voltage_now
 
-        max_hours = charge_start * voltage_start / \
-            (max_milliwatts_standby / 1000)
+        max_hours = (charge_start * voltage_start /
+                (max_milliwatts_standby / 1000.))
         if max_hours < test_hours:
             raise error.TestFail('Battery not charged adequately for test')
 
         elapsed_hours = 0
 
+        results = {}
+        # TODO (b/68956240): Add wrapper logging perhaps.
+        loop = 0
         while elapsed_hours < test_hours:
             charge_before = power_stats.battery[0].charge_now
             before_suspend_secs = rtc.get_seconds()
@@ -48,40 +67,55 @@ class power_Standby(test.test):
 
             power_stats.refresh()
             if power_stats.percent_current_charge() < self._percent_min_charge:
-                logging.warning("Battery percent = %.2f%%.  Too low to continue")
+                logging.warning("Battery = %.2f%%.  Too low to continue")
                 break
 
             # check that the RTC slept the correct amount of time as there could
             # potentially be another wake source that would spoil the test.
             actual_hours = (after_suspend_secs - before_suspend_secs) / 3600.0
-            logging.debug("actual_hours = %.4f", actual_hours)
             percent_diff = math.fabs((actual_hours - sample_hours) /
                                      ((actual_hours + sample_hours) / 2) * 100)
             if percent_diff > 2:
-                err_str = "Requested S3 time and actual varied by %.2f%%." \
+                err = "Requested standby time and actual varied by %.2f%%." \
                     % percent_diff
-                raise error.TestFail(err_str)
+                raise error.TestFail(err)
 
             # Check resulting charge consumption
             charge_used = charge_before - power_stats.battery[0].charge_now
-            logging.debug("charge_used = %.6f", charge_used)
-
             elapsed_hours += actual_hours
-            logging.debug("elapsed_hours = %.4f", elapsed_hours)
+            logging.debug('loop%d done: loop hours %.3f, elapsed hours %.3f '
+                          'charge used: %.3f', loop, actual_hours,
+                          elapsed_hours, charge_used)
+            loop += 1
 
         charge_end = power_stats.battery[0].charge_now
+        total_charge_used = charge_start - charge_end
+        if total_charge_used <= 0:
+            raise error.TestError("Charge used is suspect.")
+
         voltage_end = power_stats.battery[0].voltage_now
         standby_hours = power_stats.battery[0].charge_full_design / \
-            (charge_start - charge_end) * elapsed_hours
-        energy_used = charge_start * voltage_start - charge_end * voltage_end
-        if energy_used <= 0:
-            raise error.TestError("Energy used reading is suspect.")
-        standby_milliwatts = energy_used / elapsed_hours * 1000
+                        total_charge_used * elapsed_hours
+        energy_used = (voltage_start + voltage_end) / 2 * \
+                      total_charge_used
 
-        results = {}
-        results['milliwatts_standby_power'] = standby_milliwatts
+        results['ah_charge_start'] = charge_start
+        results['ah_charge_now'] = charge_end
+        results['ah_charge_used'] = total_charge_used
+        results['force_discharge'] = self._force_discharge_enabled
         results['hours_standby_time'] = standby_hours
+        results['hours_standby_time_tested'] = elapsed_hours
+        results['v_voltage_start'] = voltage_start
+        results['v_voltage_now'] = voltage_end
+        results['w_energy_rate'] = energy_used / elapsed_hours
+        results['wh_energy_used'] = energy_used
+
+        # TODO(?): add power/chromeperf dashboard uploading.
         self.write_perf_keyval(results)
 
         # need to sleep for some time to allow network connection to return
         time.sleep(10)
+
+    def cleanup(self):
+        if self._force_discharge_enabled:
+            power_utils.charge_control_by_ectool(True)
