jingpad-bsp/android_external_autotest

commit 753498b613de52e58ae9b9e01ce278d1f6a2cd30

autotest: wifi: Add checks to support pure VHT80 testing
As of this change, autotest doesn't have tests with MODE_11AC_PURE. We'd
like to start testing 802.11ac VHT80 rates exclusively. This change
introduces a check to enforce that the client can support these rates.
Pieces involved are:
a. Change the require_vht variable in hostap_config to make it appear
not-private.
b. Check for require_vht in the ap config, in wifi_test_context_manager,
then error out if the client does not support AC rates.
c. Remove duplicate checks from existing wifi tests, since we added one
in  wifi_test_context_manager.
network_WiFi_BluetoothStreamPerf has this check, but it currently only
runs on 802.11a and 802.11b configs. Remove it.
network_WiFi_Perf has this check, but its not the same. It enforces that the
client support 802.11ac VHT only rates, even when the AP is asking for a
"mixed" that is both 802.11ac VHT and 802.11n HT. Add a comment
explaining that, and leave the check intact.
d. Check the require_capabilities function logging in site_linux_system
to not assume that its running on an AP.

BUG=chromium:827829
TEST=Run a modified network_WiFi_SimpleConnect with VHT80 config on a client
that does not support 802.11ac, and confirm that the test passes with TestNA.
See the TEST section in the commit message of CL:1060648 for more details.

Change-Id: I6af1a403981a134c4a6c7a0d3f29456118e89467
Signed-off-by: Kirtika Ruchandani <kirtika@google.com>
Reviewed-on: https://chromium-review.googlesource.com/1065060
Commit-Ready: Kirtika Ruchandani <kirtika@chromium.org>
Tested-by: Kirtika Ruchandani <kirtika@chromium.org>
Reviewed-by: Brian Norris <briannorris@chromium.org>

server/cros/network/wifi_test_context_manager.py
@@ -162,6 +162,9 @@ class WiFiTestContextManager(object):
         if not self.client.is_frequency_supported(ap_config.frequency):
             raise error.TestNAError('DUT does not support frequency: %s' %
                                     ap_config.frequency)
+        if ap_config.require_vht:
+            self.context.client.require_capabilities(
+                    site_linux_system.CAPABILITY_VHT)
         ap_config.security_config.install_router_credentials(self.router.host)
         if is_ibss:
             if multi_interface:

server/cros/network/hostap_config.py
@@ -289,7 +289,7 @@ class HostapConfig(object):
 
 
     @property
-    def _require_vht(self):
+    def require_vht(self):
         """@return True iff clients should be required to support VHT."""
         return self._mode == self.MODE_11AC_PURE
 
@@ -720,7 +720,7 @@ class HostapConfig(object):
             conf['wmm_enabled'] = 1
         if self._require_ht:
             conf['require_ht'] = 1
-        if self._require_vht:
+        if self.require_vht:
             conf['require_vht'] = 1
         if self._beacon_interval:
             conf['beacon_int'] = self._beacon_interval

server/site_linux_system.py
@@ -686,9 +686,8 @@ class LinuxSystem(object):
         """
         missing = [cap for cap in requirements if not cap in self.capabilities]
         if missing:
-            raise error.TestNAError(
-                    'AP on %s is missing required capabilites: %r' %
-                    (self.role, missing))
+            raise error.TestNAError('%s is missing required capabilites: %r'
+                                    % (self.role, missing))
 
 
     def disable_antennas_except(self, permitted_antennas):

server/site_tests/network_WiFi_BluetoothStreamPerf/network_WiFi_BluetoothStreamPerf.py
@@ -135,8 +135,6 @@ class network_WiFi_BluetoothStreamPerf(wifi_cell_test_base.WiFiCellTestBase):
         for ap_config in self._ap_configs:
             # Set up the router and associate the client with it.
             self.context.configure(ap_config)
-            if ap_config.is_11ac and not self.context.client.is_vht_supported():
-                raise error.TestNAError('Client does not have AC support')
             assoc_params = xmlrpc_datatypes.AssociationParameters(
                     ssid=self.context.router.get_ssid(),
                     security_config=ap_config.security_config)

server/site_tests/network_WiFi_Perf/network_WiFi_Perf.py
@@ -82,6 +82,12 @@ class network_WiFi_Perf(wifi_cell_test_base.WiFiCellTestBase):
         for ap_config in self._ap_configs:
             # Set up the router and associate the client with it.
             self.context.configure(ap_config)
+            # self.context.configure has a similar check - but that one only
+            # errors out if the AP *requires* VHT i.e. AP is requesting
+            # MODE_11AC_PURE and the client does not support it.
+            # For wifi_perf, we don't want to run MODE_11AC_MIXED on the AP if
+            # the client does not support VHT, as we are guaranteed to get the
+            # same results at 802.11n/HT40 in that case.
             if ap_config.is_11ac and not self.context.client.is_vht_supported():
                 raise error.TestNAError('Client does not have AC support')
             assoc_params = xmlrpc_datatypes.AssociationParameters(

commit 877a810dc1db5dd85e008d58b221d7447a4d39b6

autotest: wifi: fix broken CAPABILITY_VHT check
Two mistakes:
- 'self.client.require_capabilities', not self.context.client, since 'self' is
the context.
- require_capabilities expects a list

BUG=chromium:827829
TEST=The below passes of grunt instead of throwing an exception -
`test_that -b grunt grunt network_WiFi_SimpleConnect.wifi_check5VHT80_pure`

Fixes: 753498b61 ("autotest: wifi: Add checks to support pure VHT80 testing")
Change-Id: I1620ff77e02c1b98038e141fb7cc7039f43c7895
Signed-off-by: Kirtika Ruchandani <kirtika@google.com>
Reviewed-on: https://chromium-review.googlesource.com/1069413
Commit-Ready: Kirtika Ruchandani <kirtika@chromium.org>
Tested-by: Kirtika Ruchandani <kirtika@chromium.org>
Reviewed-by: Brian Norris <briannorris@chromium.org>

server/cros/network/wifi_test_context_manager.py
@@ -163,8 +163,8 @@ class WiFiTestContextManager(object):
             raise error.TestNAError('DUT does not support frequency: %s' %
                                     ap_config.frequency)
         if ap_config.require_vht:
-            self.context.client.require_capabilities(
-                    site_linux_system.CAPABILITY_VHT)
+            self.client.require_capabilities(
+                    [site_linux_system.LinuxSystem.CAPABILITY_VHT])
         ap_config.security_config.install_router_credentials(self.router.host)
         if is_ibss:
             if multi_interface: