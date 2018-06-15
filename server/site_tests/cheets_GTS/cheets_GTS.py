ffedbef7c7ded4c1fad4b8537e6534a7962ae2d8
Po-Hsien Wang <pwang@chromium.org>
2018-06-14 17:10:35 -0700

    cheets_GTS: refactor to build cmd from generator
    Same with CL:1066451, simplified the runtime by moving
    command precomputation to generator.
    
    BUG=b:77338376
    TEST=test_that -b kevin cheets_GTS.5.1_r4.GtsBackupTestCases
    CQ-DEPEND=CL:*642657
    
    Change-Id: I9ce604cebb802526ec68eeb458c55c5ddad20d9f
    Reviewed-on: https://chromium-review.googlesource.com/1106957
    Commit-Ready: Pohsien Wang <pwang@chromium.org>
    Tested-by: Pohsien Wang <pwang@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>

--- server/site_tests/cheets_GTS/cheets_GTS.py
+++ server/site_tests/cheets_GTS/cheets_GTS.py
@@ -18,40 +18,37 @@ import os
 from autotest_lib.server import utils
 from autotest_lib.server.cros import tradefed_test
 
+# Maximum default time allowed for each individual GTS module.
+_GTS_TIMEOUT_SECONDS = 3600
 _PARTNER_GTS_LOCATION = 'gs://chromeos-partner-gts/gts-5.1_r4-4697968.zip'
 
 
-class cheets_GTS(tradefed_test.TradefedTest):
+class cheets_GTS(tradefed_test.TradefedTest_P):
     """Sets up tradefed to run GTS tests."""
     version = 1
     _SHARD_CMD = '--shard-count'
 
+    def _tradefed_retry_command(self, template, session_id):
+        """Build tradefed 'retry' command from template."""
+        cmd = []
+        for arg in template:
+            cmd.append(arg.format(session_id=session_id))
+        return cmd
+
+    def _tradefed_run_command(self, template):
+        """Build tradefed 'run' command from template."""
+        cmd = template[:]
+        # If we are running outside of the lab we can collect more data.
+        if not utils.is_in_container():
+            logging.info('Running outside of lab, adding extra debug options.')
+            cmd.append('--log-level-display=DEBUG')
+        return cmd
+
     def _get_default_bundle_url(self, bundle):
         return _PARTNER_GTS_LOCATION
 
-
     def _get_tradefed_base_dir(self):
         return 'android-gts'
-
-
-    def _tradefed_run_command(self, target_module=None, plan=None,
-                              session_id=None):
-        """Builds the GTS command line.
-
-        @param target_module: the module to be run.
-        @param plan: the plan to be run.
-        @param session_id: tradfed session id to continue.
-        """
-        args = ['run', 'commandAndExit', 'gts']
-        if target_module is not None:
-            args += ['--module', target_module]
-        elif plan is not None and session_id is not None:
-            args += ['--plan', plan, '--retry', '%d' % session_id]
-        # We occasionly saw business logic service issue at setup stage on our
-        # dashboard, ignore this type of failure and let the test run.
-        args += ['--ignore-business-logic-failure']
-        return args
-
 
     def _run_tradefed(self, commands):
         """Kick off GTS.
@@ -74,17 +71,47 @@ class cheets_GTS(tradefed_test.TradefedTest):
                 logging.info('END: ./gts-tradefed %s\n', ' '.join(command))
         return output
 
+    def run_once(self,
+                 test_name,
+                 run_template,
+                 retry_template=None,
+                 target_module=None,
+                 target_plan=None,
+                 target_class=None,
+                 target_method=None,
+                 needs_push_media=False,
+                 precondition_commands=[],
+                 login_precondition_commands=[],
+                 timeout=_GTS_TIMEOUT_SECONDS):
+        """Runs the specified GTS once, but with several retries.
 
-    def run_once(self, target_package=None, tradefed_args=None):
-        """Runs GTS with either a target module or a custom command line.
+        Run an arbitrary tradefed command.
 
-        @param target_package: the name of test module to be run.
-        @param tradefed_args: used to pass any specific cmd to GTS binary.
+        @param test_name: the name of test. Used for logging.
+        @param run_template: the template to construct the run command.
+                             Example: ['run', 'commandAndExit', 'cts',
+                                       '--skip-media-download']
+        @param retry_template: the template to construct the retry command.
+                               Example: ['run', 'commandAndExit', 'retry',
+                                         '--skip-media-download', '--retry',
+                                         '{session_id}']
+        @param target_module: the name of test module to run.
+        @param target_plan: the name of the test plan to run.
+        @param target_class: the name of the class to be tested.
+        @param target_method: the name of the method to be tested.
+        @param needs_push_media: need to push test media streams.
+        @param timeout: time after which tradefed can be interrupted.
+        @param precondition_commands: a list of scripts to be run on the
+        dut before the test is run, the scripts must already be installed.
+        @param login_precondition_commands: a list of scripts to be run on the
+        dut before the log-in for the test is performed.
         """
-        if tradefed_args:
-            test_command = tradefed_args
-            test_name = ' '.join(tradefed_args)
-        else:
-            test_command = self._tradefed_run_command(target_package)
-            test_name = 'module.%s' % target_package
-        self._run_tradefed_with_retries(target_package, test_command, test_name)
+        self._run_tradefed_with_retries(
+            test_name=test_name,
+            run_template=run_template,
+            retry_template=retry_template,
+            target_module=target_module,
+            target_plan=target_plan,
+            needs_push_media=needs_push_media,
+            login_precondition_commands=login_precondition_commands,
+            precondition_commands=precondition_commands)
