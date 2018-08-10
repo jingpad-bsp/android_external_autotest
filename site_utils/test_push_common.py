84a834fa96d1ea94ce132995905a8ad3a2463322
Xixuan Wu <xixuan@chromium.org>
2018-08-10 15:22:26 -0700

    autotest: Refactor for test_push for future skylab test_push's use.
    BUG=chromium:871968
    TEST=Unittest.
    
    Change-Id: I829f2066db32231b94f853c2fb46a02351af4a51
    Reviewed-on: https://chromium-review.googlesource.com/1180242
    Reviewed-by: Xixuan Wu <xixuan@chromium.org>
    Commit-Queue: Xixuan Wu <xixuan@chromium.org>
    Tested-by: Xixuan Wu <xixuan@chromium.org>

--- site_utils/test_push_common.py
+++ site_utils/test_push_common.py
@@ -8,6 +8,9 @@ autotest: site_utils/test_push.py
 skylab: venv/skylab_staging/test_push.py
 """
 
+import collections
+import re
+
 # Dictionary of test results keyed by test name regular expression.
 EXPECTED_TEST_RESULTS = {'^SERVER_JOB$':                 'GOOD',
                          # This is related to dummy_Fail/control.dependency.
@@ -35,3 +38,82 @@ EXPECTED_TEST_RESULTS_DUMMY = {'^SERVER_JOB$':       'GOOD',
 
 EXPECTED_TEST_RESULTS_POWERWASH = {'platform_Powerwash': 'GOOD',
                                    'SERVER_JOB':         'GOOD'}
+
+_TestPushErrors = collections.namedtuple(
+        '_TestPushErrors',
+        [
+                'mismatch_errors',
+                'unknown_tests',
+                'missing_tests',
+        ]
+)
+
+
+def summarize_push(test_views, expected_results, ignored_tests=[]):
+    """Summarize the test push errors."""
+    test_push_errors = _match_test_results(test_views, expected_results,
+                                           ignored_tests)
+    return _generate_push_summary(test_push_errors)
+
+
+def _match_test_results(test_views, expected_results, ignored_tests):
+    """Match test results with expected results.
+
+    @param test_views: A dictionary of test status keyed by test name, e.g.,
+             {'dummy_Fail.Error': 'ERROR', 'dummy_Fail.NAError': 'TEST_NA'}
+    @param expected_results: A dictionary of test name to expected test result.
+
+    @return: A _TestPushErrors tuple.
+    """
+    mismatch_errors = []
+    unknown_tests = []
+    found_keys = set()
+    for test_name, test_status in test_views.iteritems():
+        test_found = False
+        for test_name_pattern, expected_result in expected_results.items():
+            if re.search(test_name_pattern, test_name):
+                test_found = True
+                found_keys.add(test_name_pattern)
+                if (expected_result != test_status and
+                    _is_significant(test_name, ignored_tests)):
+                    error = ('%s Expected: [%s], Actual: [%s]' %
+                             (test_name, expected_result, test_status))
+                    mismatch_errors.append(error)
+
+        if not test_found and _is_significant(test_name, ignored_tests):
+            unknown_tests.append(test_name)
+
+    missing_tests = set(expected_results.keys()) - found_keys
+    missing_tests = [t for t in missing_tests
+                     if _is_significant(t, ignored_tests)]
+    return _TestPushErrors(mismatch_errors=mismatch_errors,
+                           unknown_tests=unknown_tests,
+                           missing_tests=missing_tests)
+
+
+def _is_significant(test, ignored_tests_patterns):
+    return all([m.search(test) is None for m in ignored_tests_patterns])
+
+
+def _generate_push_summary(test_push_errors):
+    """Generate a list of summary based on the test_push results."""
+    summary = []
+    if test_push_errors.mismatch_errors:
+        summary.append(('Results of %d test(s) do not match expected '
+                        'values:') % len(test_push_errors.mismatch_errors))
+        summary.extend(test_push_errors.mismatch_errors)
+        summary.append('\n')
+
+    if test_push_errors.unknown_tests:
+        summary.append('%d test(s) are not expected to be run:' %
+                       len(test_push_errors.unknown_tests))
+        summary.extend(test_push_errors.unknown_tests)
+        summary.append('\n')
+
+    if test_push_errors.missing_tests:
+        summary.append('%d test(s) are missing from the results:' %
+                       len(test_push_errors.missing_tests))
+        summary.extend(test_push_errors.missing_tests)
+        summary.append('\n')
+
+    return summary
