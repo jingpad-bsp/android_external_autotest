127c084d1f4bac49b564e1ce171577cb84197565
paulhsia <paulhsia@chromium.org>
2018-11-05 14:38:39 +0800

    [autotest] Add customized internal mic and hotword for nami
    For test audio_InternalCardNodes.
    
    We need to set expected internal mic to 'FRONT_MIC' for
    (board, model) = (nami, vayne) and
    add 'HOTWORD' to nami boards.
    
    BUG=chromium:901723
    TEST=Run audio_InternalCardNodes on DUT
    
    Change-Id: I434e9e5f41fdf93339ed32cbba807427f58883a8
    Reviewed-on: https://chromium-review.googlesource.com/1317280
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>

--- client/cros/audio/cras_configs.py
+++ client/cros/audio/cras_configs.py
@@ -38,7 +38,8 @@ def get_internal_mic_node(board, model):
     return INTERNAL_MIC_NODE.get((board, model), 'INTERNAL_MIC')
 
 INTERNAL_MIC_NODES = {
-        ('coral', 'nasher360'): ['FRONT_MIC', 'REAR_MIC']
+        ('coral', 'nasher360'): ['FRONT_MIC', 'REAR_MIC'],
+        ('nami', 'vayne'): ['FRONT_MIC'],
 }
 
 def get_plugged_internal_mics(board, model):
