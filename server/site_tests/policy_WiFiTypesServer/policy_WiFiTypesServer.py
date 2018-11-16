364389d9aee2ce77d826a9e194371e489ffe584f
Max Timkovich <timkovich@chromium.org>
2018-11-16 13:21:25 -0800

    [autotest] policy_WiFiPrecedence
    policy_WiFiPrecedence tests the autoconnect priority hierarchy for
    policy networks. It sets up 2 APs and sets 2 network policies on the DUT
    and then tests which network the DUT connects to.
    
    Also creates NetworkConfig class to store client network configurations,
    and updates WiFiTypes to use this class.
    
    The scenarios tested are:
    * Connectable vs not connectable (connectable wins)
    * Autoconnect vs no autoconnect (autoconnect wins)
    * PSK vs Open (PSK wins)
    * Managed vs unmanaged (managed wins)
    * Device vs user policy (user policy wins)
    
    BUG=chromium:904837
    TEST=on DUT in wificell pool
    CQ-DEPEND=CL:1327728
    
    Change-Id: Ib1f21d19d6bfe01ed1281bb27e9340cb0e649d28
    Reviewed-on: https://chromium-review.googlesource.com/1398346
    Commit-Ready: Max Timkovich <timkovich@chromium.org>
    Tested-by: Max Timkovich <timkovich@chromium.org>
    Reviewed-by: Max Timkovich <timkovich@chromium.org>
    Reviewed-by: Harpreet Grewal <harpreet@chromium.org>

--- server/site_tests/policy_WiFiTypesServer/policy_WiFiTypesServer.py
+++ server/site_tests/policy_WiFiTypesServer/policy_WiFiTypesServer.py
@@ -1,6 +1,8 @@
-# Copyright (c) 2018 The Chromium OS Authors. All rights reserved.
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+import pickle
 
 from autotest_lib.server import autotest
 from autotest_lib.server import site_linux_system
@@ -11,23 +13,14 @@ class policy_WiFiTypesServer(wifi_cell_test_base.WiFiCellTestBase):
     version = 1
 
 
-    def run_once(self, host, ap_config, security=None, eap=None, password=None,
-                 identity=None, ca_cert=None, client_cert=None):
+    def run_once(self, host, ap_config, network):
         """
         Set up an AP for a WiFi authentication type then run the client test.
 
         @param host: A host object representing the DUT.
         @param ap_config: HostapConfig object representing how to configure
             the router.
-        @param security: Security of network. Options are:
-            'None', 'WEP-PSK', 'WEP-8021X', 'WPA-PSK', and 'WPA-EAP'.
-        @param eap: EAP type, required if security is 'WEP-8021X' or 'WPA-EAP'.
-        @param identity: Username, if the network type requires it.
-        @param password: Password, if the network type requires it.
-        @param ca_cert: CA certificate in PEM format. Required
-            for EAP networks.
-        @param client_cert: Client certificate in PKCS#12 format. Required for
-            EAP-TLS networks.
+        @param network: NetworkConfig object of how to configure the client.
 
         """
         self.context.router.require_capabilities(
@@ -36,16 +29,11 @@ class policy_WiFiTypesServer(wifi_cell_test_base.WiFiCellTestBase):
 
         # Configure the AP
         self.context.configure(ap_config)
+        network.ssid = self.context.router.get_ssid()
 
         client_at = autotest.Autotest(host)
         client_at.run_test('policy_WiFiTypes',
-                           ssid=self.context.router.get_ssid(),
-                           security=security,
-                           eap=eap,
-                           password=password,
-                           identity=identity,
-                           ca_cert=ca_cert,
-                           client_cert=client_cert,
+                           network_pickle=pickle.dumps(network),
                            check_client_result=True)
 
         self.context.router.deconfig()
