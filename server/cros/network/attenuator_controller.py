a081489a457c0f47e8314bc71a29417d0a3b436a
harpreet <harpreet@google.com>
2019-03-08 12:26:08 -0800

    [autotest] Grover Setups: Add new attenuator hosts
    Add new attenuator hosts to get new grover setups up and going. Also updated
    calibration data for chromeos15-row3-rack9-host6-attenuator
    
    Some docs for reference:
    go/gale-grover-setup-how-to - How to setup grover cells with gale AP.
    go/gale-grover-calibrations - Calibration data from gale grover cells.
    go/gale-grover-setup-testing - Initial testing data from new grover cells with gale.
    go/grover-setups - Grover setup details including hostnames and DUT mapping.
    
    chromeos15-row3-rack11-host1	Empty cell
    chromeos15-row3-rack11-host2	Empty cell
    chromeos15-row3-rack11-host3	Empty cell
    chromeos15-row3-rack11-host4	Empty cell
    chromeos15-row3-rack11-host5	Atlas
    chromeos15-row3-rack11-host6	Phaser360
    chromeos15-row3-rack12-host1	Careena
    chromeos15-row3-rack12-host2	astronaut
    chromeos15-row3-rack12-host3	nocturne
    chromeos15-row3-rack12-host4	winky
    chromeos15-row3-rack12-host5	big
    chromeos15-row3-rack12-host6	tricky
    
    BUG=b:112390778, b:124072476
    TEST=Tested against new grover setups in lab.
    
    Change-Id: I756499bc72fb4161a0b6c12aeb44e1c3793b5a76
    Reviewed-on: https://chromium-review.googlesource.com/1512057
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Harpreet Grewal <harpreet@chromium.org>
    Reviewed-by: Kirtika Ruchandani <kirtika@chromium.org>

--- server/cros/network/attenuator_controller.py
+++ server/cros/network/attenuator_controller.py
@@ -6,74 +6,11 @@ import logging
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.server.cros.network import attenuator
+from autotest_lib.server.cros.network import attenuator_hosts
 
 from chromite.lib import timeout_util
 
-# This map represents the fixed loss overhead on a given antenna line.
-# The map maps from:
-#     attenuator hostname -> attenuator number -> frequency -> loss in dB.
-HOST_TO_FIXED_ATTENUATIONS = {
-        'chromeos15-row3-rack9-host1-attenuator': {
-                0: {2437: 59, 5220: 59, 5765: 59},
-                1: {2437: 52, 5220: 54, 5765: 54},
-                2: {2437: 59, 5220: 59, 5765: 59},
-                3: {2437: 52, 5220: 54, 5765: 54}},
-        'chromeos15-row3-rack9-host2-attenuator': {
-                0: {2437: 64, 5220: 62, 5765: 62},
-                1: {2437: 58, 5220: 57, 5765: 57},
-                2: {2437: 64, 5220: 62, 5765: 62},
-                3: {2437: 58, 5220: 57, 5765: 57}},
-        'chromeos15-row3-rack9-host3-attenuator': {
-                0: {2437: 60, 5220: 58, 5765: 58},
-                1: {2437: 52, 5220: 57, 5765: 57},
-                2: {2437: 60, 5220: 58, 5765: 58},
-                3: {2437: 52, 5220: 57, 5765: 57}},
-        'chromeos15-row3-rack9-host4-attenuator': {
-                0: {2437: 52, 5220: 58, 5765: 58},
-                1: {2437: 59, 5220: 60, 5765: 60},
-                2: {2437: 52, 5220: 58, 5765: 58},
-                3: {2437: 59, 5220: 60, 5765: 60}},
-        'chromeos15-row3-rack9-host5-attenuator': {
-                0: {2437: 58, 5220: 60, 5765: 60},
-                1: {2437: 53, 5220: 58, 5765: 58},
-                2: {2437: 58, 5220: 60, 5765: 60},
-                3: {2437: 53, 5220: 58, 5765: 58}},
-        'chromeos15-row3-rack9-host6-attenuator': {
-                0: {2437: 61, 5220: 62, 5765: 62},
-                1: {2437: 53, 5220: 60, 5765: 60},
-                2: {2437: 61, 5220: 62, 5765: 62},
-                3: {2437: 53, 5220: 60, 5765: 60}},
-        'chromeos15-row3-rack10-host1-attenuator': {
-                0: {2437: 53, 5220: 56, 5765: 56},
-                1: {2437: 52, 5220: 56, 5765: 56},
-                2: {2437: 53, 5220: 56, 5765: 56},
-                3: {2437: 52, 5220: 56, 5765: 56}},
-        'chromeos15-row3-rack10-host2-attenuator': {
-                0: {2437: 59, 5220: 59, 5765: 59},
-                1: {2437: 59, 5220: 60, 5765: 60},
-                2: {2437: 59, 5220: 59, 5765: 59},
-                3: {2437: 59, 5220: 60, 5765: 60}},
-        'chromeos15-row3-rack10-host3-attenuator': {
-                0: {2437: 52, 5220: 56, 5765: 56},
-                1: {2437: 64, 5220: 63, 5765: 63},
-                2: {2437: 52, 5220: 56, 5765: 56},
-                3: {2437: 64, 5220: 63, 5765: 63}},
-        'chromeos15-row3-rack10-host4-attenuator': {
-                0: {2437: 52, 5220: 55, 5765: 55},
-                1: {2437: 58, 5220: 58, 5765: 58},
-                2: {2437: 52, 5220: 55, 5765: 55},
-                3: {2437: 58, 5220: 58, 5765: 58}},
-        'chromeos15-row3-rack10-host5-attenuator': {
-                0: {2437: 57, 5220: 58, 5765: 58},
-                1: {2437: 52, 5220: 55, 5765: 55},
-                2: {2437: 57, 5220: 58, 5765: 58},
-                3: {2437: 52, 5220: 55, 5765: 55}},
-        'chromeos15-row3-rack10-host6-attenuator': {
-                0: {2437: 57, 5220: 57, 5765: 57},
-                1: {2437: 52, 5220: 55, 5765: 55},
-                2: {2437: 57, 5220: 57, 5765: 57},
-                3: {2437: 52, 5220: 55, 5765: 55}},
-        }
+HOST_TO_FIXED_ATTENUATIONS = attenuator_hosts.HOST_FIXED_ATTENUATIONS
 
 
 class AttenuatorController(object):
