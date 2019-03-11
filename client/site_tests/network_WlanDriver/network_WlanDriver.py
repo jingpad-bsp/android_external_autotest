6d340de88a1288ca60a5c59d7f5ec0ee709fab44
Kirtika Ruchandani <kirtika@google.com>
2019-03-10 22:19:56 -0700

    network_WlanDriver: Add an entry for CcP2
    Add an entry for the new 802.11ax chip.
    
    BUG=b:127855857
    TEST=network_WlanDriver passes on hatch with CcP2
    
    Change-Id: I64aef8b139c09911226dbd5bef27fe467279ff75
    Signed-off-by: Kirtika Ruchandani <kirtika@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1514310
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Kirtika Ruchandani <kirtika@chromium.org>
    Reviewed-by: Luca Coelho <luciano.coelho@intel.com>
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- client/site_tests/network_WlanDriver/network_WlanDriver.py
+++ client/site_tests/network_WlanDriver/network_WlanDriver.py
@@ -55,6 +55,11 @@ class network_WlanDriver(test.test):
                     '4.14': 'wireless/iwl7000/iwlwifi/iwlwifi.ko',
                     '4.19': 'wireless/iwl7000/iwlwifi/iwlwifi.ko',
             },
+            'Intel 22260': {
+                    '4.4': 'wireless/iwl7000/iwlwifi/iwlwifi.ko',
+                    '4.14': 'wireless/iwl7000/iwlwifi/iwlwifi.ko',
+                    '4.19': 'wireless/iwl7000/iwlwifi/iwlwifi.ko',
+            },
             'Atheros AR9462': {
                     '3.4': 'wireless/ath/ath9k_btcoex/ath9k_btcoex.ko',
                     '3.8': 'wireless-3.4/ath/ath9k_btcoex/ath9k_btcoex.ko',
